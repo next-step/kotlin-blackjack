17df74a4d1f9111ecc631aec74fe256ab4bf2ee0
BeokBeok <kekemusa37@nate.com>
2020-08-06 22:12:00 +0900

    feat(step1) : [적용] 익명 클래스를 람다로 전환

--- src/test/kotlin/fp/CarTest.kt
+++ src/test/kotlin/fp/CarTest.kt
@@ -4,34 +4,26 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 data class Car(val name: String, val position: Int) {
-    fun move(moveStrategy: MoveStrategy): Car {
-        if (moveStrategy.isMovable) {
+    fun move(isAvailableMove: () -> Boolean): Car {
+        if (isAvailableMove()) {
             return copy(position = position + 1)
         }
         return this
     }
 }
 
-interface MoveStrategy {
-    val isMovable: Boolean
-}
-
 class CarTest {
     @Test
     fun 이동() {
         val car = Car("jason", 0)
-        val actual: Car = car.move(object : MoveStrategy {
-            override val isMovable: Boolean = true
-        })
+        val actual: Car = car.move { true }
         assertThat(actual).isEqualTo(Car("jason", 1))
     }
 
     @Test
     fun 정지() {
         val car = Car("jason", 0)
-        val actual: Car = car.move(object : MoveStrategy {
-            override val isMovable: Boolean = false
-        })
+        val actual: Car = car.move { false }
         assertThat(actual).isEqualTo(Car("jason", 0))
     }
 }
