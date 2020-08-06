2dff87e35ea8ad7a9384077a65057136d6b71558
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-06 22:25:14 +0900

    feature(kotlin-blackjack): 람다 활용 중복 제거 람다 적용

--- src/test/kotlin/Lambda.kt
+++ src/test/kotlin/Lambda.kt
@@ -1,31 +1,34 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class Lambda {
     val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
-
+    val conditionOdd: (item: Int) -> Boolean = { item -> item % 2 == 0 }
+    val conditionOverThree: (item: Int) -> Boolean = { item -> item > 3 }
     fun sumAll(numbers: List<Int>): Int {
-        var total = 0
-        for (number in numbers) {
-            total += number
-        }
-        return total
+        return numbers.sum()
     }
 
     fun sumAllEven(numbers: List<Int>): Int {
-        var total = 0
-        for (number in numbers) {
-            if (number % 2 == 0) {
-                total += number
-            }
-        }
-        return total
+        return numbers.filter { conditionOdd(it) }.sum()
     }
 
     fun sumAllOverThree(numbers: List<Int>): Int {
-        var total = 0
-        for (number in numbers) {
-            if (number > 3) {
-                total += number
-            }
-        }
-        return total
+        return numbers.filter { conditionOverThree(it) }.sum()
+    }
+
+    @Test
+    fun `sumAllTest`() {
+        assertThat(sumAll(numbers)).isEqualTo(1 + 2 + 3 + 4 + 5 + 6)
+    }
+
+    @Test
+    fun `sumAllEvenTest`() {
+        assertThat(sumAllEven(numbers)).isEqualTo(2 + 4 + 6)
+    }
+
+    @Test
+    fun `sumAllOverThreeTest`() {
+        assertThat(sumAllOverThree(numbers)).isEqualTo(4 + 5 + 6)
     }
 }
