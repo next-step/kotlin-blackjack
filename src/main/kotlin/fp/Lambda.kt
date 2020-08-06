103295499024dce2865e130daa0380cd29ae28c3
bky373 <bo373@naver.com>
2020-08-07 01:18:01 +0900

    refactor(blackjack): avoid duplicate of content in Lambda.kt and LambdaTest.kt

--- src/main/kotlin/fp/Lambda.kt
+++ src/main/kotlin/fp/Lambda.kt
@@ -3,33 +3,6 @@ package fp
 class Lambda {
     val numbers: List<Int> = listOf(1, 2, 3, 4, 5, 6)
 
-    fun sumAll(numbers: List<Int>, isInCondition: (Int) -> Boolean): Int {
-        var total = 0
-        for (number in numbers) {
-            if (isInCondition(number)) {
-                total += number
-            }
-        }
-        return total
-    }
-
-    fun sumAllEven(numbers: List<Int>, inCondition: (Int) -> Boolean): Int {
-        var total = 0
-        for (number in numbers) {
-            if (inCondition(number)) {
-                total += number
-            }
-        }
-        return total
-    }
-
-    fun sumAllOverThree(numbers: List<Int>, inCondition: (Int) -> Boolean): Int {
-        var total = 0
-        for (number in numbers) {
-            if (inCondition(number)) {
-                total += number
-            }
-        }
-        return total
-    }
+    fun sumInCondition(numbers: List<Int>, condition: (Int) -> Boolean) =
+        numbers.filter(condition).sum()
 }
--- src/test/kotlin/fp/LambdaTest.kt
+++ src/test/kotlin/fp/LambdaTest.kt
@@ -5,25 +5,23 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class LambdaTest {
+    val lambda = Lambda()
 
     @Test
     fun `sum all`() {
-        val lambda = Lambda()
-        val actual: Int = lambda.sumAll(lambda.numbers) { true }
+        val actual: Int = lambda.sumInCondition(lambda.numbers) { true }
         assertThat(actual).isEqualTo(21)
     }
 
     @Test
     fun `sum all even`() {
-        val lambda = Lambda()
-        val actual: Int = lambda.sumAll(lambda.numbers) { number -> number % 2 == 0 }
+        val actual: Int = lambda.sumInCondition(lambda.numbers) { number -> number % 2 == 0 }
         assertThat(actual).isEqualTo(12)
     }
 
     @Test
     fun `sum all overt three`() {
-        val lambda = Lambda()
-        val actual: Int = lambda.sumAll(lambda.numbers) { number -> number > 3 }
+        val actual: Int = lambda.sumInCondition(lambda.numbers) { number -> number > 3 }
         assertThat(actual).isEqualTo(15)
     }
 }
