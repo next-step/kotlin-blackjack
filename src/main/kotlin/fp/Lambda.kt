6a9cbfd461007555f0b2ea9cc2b3c2e21456ab28
LeeYounghyeon <leey9494@icloud.com>
2020-08-08 17:17:44 +0900

    refactor(lambda): 람다를 활용해 중복 제거

---
+++ src/main/kotlin/fp/Lambda.kt
@@ -0,0 +1,13 @@
+package fp
+
+class Lambda {
+    fun sumAll(numbers: List<Int>): Int = sum(numbers) { true }
+
+    fun sumAllEven(numbers: List<Int>): Int = sum(numbers) { it % 2 == 0 }
+
+    fun sumAllOverThree(numbers: List<Int>): Int = sum(numbers) { it > 3 }
+
+    private fun sum(numbers: List<Int>, giveCondition: (number: Int) -> Boolean): Int {
+        return numbers.filter(giveCondition).sum()
+    }
+}
