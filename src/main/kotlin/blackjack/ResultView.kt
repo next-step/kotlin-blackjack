738b483dc69fea382c9cf6d16aa775b2ffa71c97
BeokBeok <kekemusa37@nate.com>
2020-08-09 22:39:38 +0900

    feat(blackjack) : [추가] printPlayerHaveDeck 함수

--- src/main/kotlin/blackjack/ResultView.kt
+++ src/main/kotlin/blackjack/ResultView.kt
@@ -4,8 +4,10 @@ object ResultView {
 
     fun printPreGame(players: List<Player>) {
         println("${players.joinToString { it.name }}에게 2장의 카드를 나누었습니다.")
-        repeat(players.size) {
-            println("${players[it].name}카드: ${players[it].myReceivedDeck.first()}, ${players[it].myReceivedDeck.last()}")
-        }
+        players.map { printPlayerHaveDeck(it) }
+    }
+
+    fun printPlayerHaveDeck(player: Player) {
+        println("${player.name}카드: ${player.myReceivedDeck.joinToString()}")
     }
 }
