cfc60a3d2c1254fb2335d8e66bf5bc00aa8ba125
BeokBeok <kekemusa37@nate.com>
2020-08-09 21:12:25 +0900

    feat(blackjack) : [추가] Player.requestDeck 함수 파라미터

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -5,17 +5,7 @@ class Player(name: String) {
     private val _myReceivedDeck = mutableListOf<Deck>()
     val myReceivedDeck: List<Deck> get() = _myReceivedDeck
 
-    init {
-        repeat(INIT_DECK_SIZE) {
-            requestDeck()
-        }
-    }
-
-    fun requestDeck() {
-        _myReceivedDeck.add(Deck.pop())
-    }
-
-    companion object {
-        private const val INIT_DECK_SIZE = 2
+    fun requestDeck(deck: Deck) {
+        _myReceivedDeck.add(deck)
     }
 }
--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -7,7 +7,10 @@ class PlayerTest {
 
     @Test
     fun `게임 참가 시 2장의 카드 받기`() {
-        val player = Player("moshi")
+        val player = Player("moshi").apply {
+            requestDeck(Deck.pop())
+            requestDeck(Deck.pop())
+        }
 
         assertThat(player.myReceivedDeck.size).isEqualTo(2)
     }
@@ -16,7 +19,7 @@ class PlayerTest {
     fun `덱을 한 장 더 받았을 경우`() {
         val player = Player("moshi")
 
-        player.requestDeck()
+        player.requestDeck(Deck.pop())
 
         assertThat(player.myReceivedDeck.size).isEqualTo(3)
     }
