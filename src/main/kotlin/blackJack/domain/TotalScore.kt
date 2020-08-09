6e64cdf45a3bb3b3525aa0baa42b4b544b11300f
Parkjoopika <parkjoohan0603@gmail.com>
2020-08-09 23:03:08 +0900

    feat: TotalScore가 ace를 가지고 있을때 값을 구할수있다.

--- src/main/kotlin/blackJack/domain/TotalScore.kt
+++ src/main/kotlin/blackJack/domain/TotalScore.kt
@@ -2,6 +2,18 @@ package blackJack.domain
 
 object TotalScore {
     fun getScore(cards: List<Int>): Int {
+        if (isAce(cards)) {
+            return hasAce(cards.sum())
+        }
         return cards.sum()
+    }
+
+    private fun isAce(cards: List<Int>): Boolean = cards.contains(1)
+
+    private fun hasAce(totalScore: Int): Int {
+        if (totalScore <= 11) {
+            return totalScore + 10
+        }
+        return totalScore
     }
 }
--- src/test/kotlin/blackJack/domain/TotalScoreTest.kt
+++ src/test/kotlin/blackJack/domain/TotalScoreTest.kt
@@ -7,8 +7,27 @@ class TotalScoreTest {
     @Test
     fun no_ace() {
         val testList = listOf(5, 10)
+
         val totalScore = TotalScore.getScore(testList)
 
         assertThat(totalScore).isEqualTo(15)
+    }
+
+    @Test
+    fun has_ace() {
+        val testList = listOf(1, 10)
+
+        val totalScore = TotalScore.getScore(testList)
+
+        assertThat(totalScore).isEqualTo(21)
+    }
+
+    @Test
+    fun has_ace_but_do_not_plus_10() {
+        val testList = listOf(10,3,1)
+
+        val totalScore = TotalScore.getScore(testList)
+
+        assertThat(totalScore).isEqualTo(14)
     }
 }
