16fcbe7fe8ccdcc8a62f43083b62db00cd1077d3
BeokBeok <kekemusa37@nate.com>
2020-08-09 22:56:47 +0900

    feat(blackjack) : [정리] calculateRank 함수

--- src/main/kotlin/blackjack/Winner.kt
+++ src/main/kotlin/blackjack/Winner.kt
@@ -4,17 +4,31 @@ object Winner {
     const val WINNING_RANK = 21
 
     fun calculateRank(decks: List<Deck>): Int {
-        val deckTypesExceptAceType = decks.map { it.getDeckType() }
-            .filterNot { it.nickName == Card.Type.ACE.nickName }
-        val aceCountInDeck = decks.map { it.getDeckType() }.filter { it.nickName == Card.Type.ACE.nickName }.size
-        val rank = deckTypesExceptAceType.map { it.points.first() }.reduce { acc, i -> acc + i }
+        val (deckTypesExceptAce, decTypeForAce) = divideDeckType(decks)
+        val rank = deckTypesExceptAce
+            .map { it.points.first() }
+            .reduce { acc, i -> acc + i }
 
-        if (deckTypesExceptAceType.size == decks.size) return rank
+        if (deckTypesExceptAce.size == decks.size) return rank
 
-        return if (rank + aceCountInDeck * Card.Type.ACE.points.last() <= WINNING_RANK) {
-            rank + aceCountInDeck * Card.Type.ACE.points.last()
+        return if (rank + decTypeForAce.size * Card.Type.ACE.points.last() <= WINNING_RANK) {
+            rank + decTypeForAce.size * Card.Type.ACE.points.last()
         } else {
-            rank + aceCountInDeck * Card.Type.ACE.points.first()
+            rank + decTypeForAce.size * Card.Type.ACE.points.first()
         }
+    }
+
+    private fun divideDeckType(decks: List<Deck>): Pair<List<Card.Type>, List<Card.Type>> {
+        val deckTypesExceptAceType = decks
+            .asSequence()
+            .map { it.getDeckType() }
+            .filterNot { it.nickName == Card.Type.ACE.nickName }
+            .toList()
+        val aceCountInDeck = decks
+            .asSequence()
+            .map { it.getDeckType() }
+            .filter { it.nickName == Card.Type.ACE.nickName }
+            .toList()
+        return Pair(deckTypesExceptAceType, aceCountInDeck)
     }
 }
