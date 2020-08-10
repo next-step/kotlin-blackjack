c854d2db3bcdbc0280e94e3bf9ad4833e000ff40
BeokBeok <kekemusa37@nate.com>
2020-08-10 22:01:30 +0900

    fix(blackjack) : [제거] 카드를 뽑을 때 shuffled 함수
    review url : https://github.com/next-step/kotlin-blackjack/pull/17#discussion_r467640436

--- src/main/kotlin/blackjack/model/Deck.kt
+++ src/main/kotlin/blackjack/model/Deck.kt
@@ -1,5 +1,8 @@
 package blackjack.model
 
+import java.util.LinkedList
+import java.util.Queue
+
 class Deck(private val type: Card.Type, private val shape: Card.Shape) {
 
     override fun toString(): String = "${type.nickName}${shape.nickName}"
@@ -8,17 +11,20 @@ class Deck(private val type: Card.Type, private val shape: Card.Shape) {
 
     companion object {
         const val TOTAL_DECK_SIZE = 56
-        private val all = mutableListOf<Deck>().apply {
-            for (type in Card.Type.values()) {
-                for (shape in Card.Shape.values()) {
-                    add(Deck(type, shape))
+        private val all: Queue<Deck> = getShuffledAllDeck()
+
+        private fun getShuffledAllDeck(): Queue<Deck> {
+            val totalDeck = mutableListOf<Deck>().apply {
+                for (type in Card.Type.values()) {
+                    for (shape in Card.Shape.values()) {
+                        add(Deck(type, shape))
+                    }
                 }
-            }
+            }.shuffled()
+
+            return LinkedList<Deck>(totalDeck)
         }
 
-        fun pop(): Deck = all
-            .shuffled()
-            .first()
-            .also { all.remove(it) }
+        fun pop(): Deck = all.poll()
     }
 }
