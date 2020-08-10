8d988917600cd5a0cded46e2aeb86bfb8d37a3d6
zion830 <zion830@honglab.org>
2020-08-10 20:47:56 +0900

    test(blackjack): 우승자 찾기 테스트 추가

--- src/test/kotlin/blackjack/domain/player/PlayersTest.kt
+++ src/test/kotlin/blackjack/domain/player/PlayersTest.kt
@@ -1,5 +1,8 @@
 package blackjack.domain.player
 
+import blackjack.domain.deck.Card
+import blackjack.domain.deck.Pip
+import blackjack.domain.deck.Suit
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
@@ -20,5 +23,25 @@ internal class PlayersTest {
     @Test
     fun emptyPlayer() {
         assertThatThrownBy { Players("") }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @DisplayName("가장 점수가 높은 플레이어가 우승자가 된다.")
+    @Test
+    fun findWinner() {
+        val players = Players("a, b, c")
+        players(0).giveCard(Card(Pip.TEN, Suit.HEART))
+        players(1).giveCard(Card(Pip.NINE, Suit.HEART))
+        players(2).giveCard(Card(Pip.ACE, Suit.HEART))
+        assertThat(players.findWinners()).contains(players(2))
+    }
+
+    @DisplayName("우승자는 여러명일 수 있다.")
+    @Test
+    fun findWinners() {
+        val players = Players("a, b, c")
+        players(0).giveCard(Card(Pip.TEN, Suit.HEART))
+        players(1).giveCard(Card(Pip.NINE, Suit.HEART))
+        players(2).giveCard(Card(Pip.TEN, Suit.HEART))
+        assertThat(players.findWinners()).contains(players(0), players(2))
     }
 }
