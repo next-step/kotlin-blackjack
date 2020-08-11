b571c95383eec077b500847dd6e4176c16ff6be1
zion830 <zion830@honglab.org>
2020-08-11 22:52:28 +0900

    refactor(blackjack): Pip이 score도 프로퍼티로 가지도록 수정

--- src/main/kotlin/blackjack/domain/deck/Pip.kt
+++ src/main/kotlin/blackjack/domain/deck/Pip.kt
@@ -2,20 +2,20 @@ package blackjack.domain.deck
 
 import blackjack.BLACKJACK_COUNT
 
-enum class Pip(val displayName: String) {
-    TWO("2"),
-    THREE("3"),
-    FOUR("4"),
-    FIVE("5"),
-    SIX("6"),
-    SEVEN("7"),
-    EIGHT("8"),
-    NINE("9"),
-    TEN("10"),
-    JACK("J"),
-    QUEEN("Q"),
-    KING("K"),
-    ACE("A");
+enum class Pip(val displayName: String, val score: Int) {
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10),
+    ACE("A", 11);
 
     companion object {
         private const val ACE_MIN = 1
@@ -23,8 +23,7 @@ enum class Pip(val displayName: String) {
 
         fun scoreOf(pip: Pip, totalScore: Int = 0) = when (pip) {
             ACE -> getAceScore(totalScore)
-            JACK, QUEEN, KING -> 10
-            else -> pip.displayName.toInt()
+            else -> pip.score
         }
 
         private fun getAceScore(totalScore: Int) = if (totalScore <= BLACKJACK_COUNT - ACE_MAX) ACE_MAX else ACE_MIN
