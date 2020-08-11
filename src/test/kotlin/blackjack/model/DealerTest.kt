4877e7be6b8e82820619bd92833defe0e888a772
BeokBeok <kekemusa37@nate.com>
2020-08-11 22:16:54 +0900

    feat(test) : ADD/APPLY - 딜러가 처음 받은 2장의 합이 17 이상일 경우 TC

--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -14,6 +14,16 @@ class DealerTest {
 
         assertThat(dealer.myReceivedCard.size).isEqualTo(3)
     }
+
+    @Test
+    fun `딜러가 처음 받은 2장의 합이 17이상일 경우`() {
+        val dealer = Dealer().apply {
+            requestCard(Card(Kinds.TEN, Shape.CLOVER))
+            requestCard(Card(Kinds.SEVEN, Shape.CLOVER))
+        }
+
+        assertThat(dealer.myReceivedCard.size).isEqualTo(2)
+    }
 }
 
 class Dealer : Gamer() {
