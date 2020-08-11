865aabb5f24f936e6f9376401dd0fa1e576a01ee
bky373 <bo373@naver.com>
2020-08-12 03:31:33 +0900

    feat(blackjack) : create MainApp.kt

---
+++ src/main/kotlin/blackjack/MainApp.kt
@@ -0,0 +1,31 @@
+package blackjack
+
+import blackjack.domain.Game
+import blackjack.domain.Game.Companion.DEFAULT_CARD_AMOUNT
+import blackjack.domain.Game.Companion.REPLY_NO
+import blackjack.domain.Game.Companion.REPLY_YES
+import blackjack.view.InputView
+import blackjack.view.ResultView
+
+fun main() {
+
+    val playerNames = InputView.readPlayerNames()
+    val game = Game(playerNames)
+    ResultView.showResultOfSetUp(game.setUp())
+
+    val players = game.players
+    players.forEach { player ->
+        var reply = InputView.readReplyToQuestionOfDrawingOrNot(player)
+        while (reply != REPLY_NO && reply.startsWith(REPLY_YES)) {
+            game.drawOnce(player)
+            ResultView.showStateOfCards(player)
+            reply = InputView.readReplyToQuestionOfDrawingOrNot(player)
+        }
+        val currentCards = player.cards.size()
+        if (currentCards == DEFAULT_CARD_AMOUNT) {
+            ResultView.showStateOfCards(player)
+        }
+    }
+
+    ResultView.showFinalResult(players)
+}
