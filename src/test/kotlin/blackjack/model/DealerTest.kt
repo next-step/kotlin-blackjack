dc2059e9187638734d431b5d4de68941abd2a66d
BeokBeok <kekemusa37@nate.com>
2020-08-11 22:15:11 +0900

    feat(test) : APPLY - 딜러가 처음 받은 2장의 합이 16 이하일 경우 TC 성공

--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -1,10 +1,36 @@
 package blackjack.model
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class DealerTest {
 
     @Test
     fun `딜러가 처음 받은 2장의 합이 16이하일 경우`() {
+        val dealer = Dealer().apply {
+            requestCard(Card(Kinds.TEN, Shape.CLOVER))
+            requestCard(Card(Kinds.SIX, Shape.CLOVER))
+        }
+
+        assertThat(dealer.myReceivedCard.size).isEqualTo(3)
+    }
+}
+
+class Dealer : Gamer() {
+
+    override fun requestCard(card: Card) {
+        super.requestCard(card)
+        requestOneOfCardIfAvailable()
+    }
+
+    private fun requestOneOfCardIfAvailable() {
+        if (checkIfGetExtraCardOrNot()) super.requestCard(Card.pop())
+    }
+
+    private fun checkIfGetExtraCardOrNot(): Boolean =
+        myReceivedCard.size == 2 && calculatePoint() <= EXTRA_CARD_AVAILABLE_LIMIT_POINT
+
+    companion object {
+        private const val EXTRA_CARD_AVAILABLE_LIMIT_POINT = 16
     }
 }
