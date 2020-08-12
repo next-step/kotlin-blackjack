a445c1bc8b1e78b8a228fc6974e4ad9124f05ffd
LeeYounghyeon <leey9494@icloud.com>
2020-08-13 01:28:30 +0900

    feat(blackjack): 카드를 더 받을지 입력하는 기능 구현

--- src/main/kotlin/blackjack/Application.kt
+++ src/main/kotlin/blackjack/Application.kt
@@ -3,4 +3,8 @@ package blackjack
 fun main() {
     val players = Players(InputView.inputPlayers())
     OutputView.showPlayersCard(players.players)
+    players.players.forEach {
+        val selectedValue = InputView.selectMoreCard(it.name)
+        it.selectValue(selectedValue)
+    }
 }
