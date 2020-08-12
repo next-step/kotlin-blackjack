24b678e9a5fd88c482e65c83b0d01d744f798068
bky373 <bo373@naver.com>
2020-08-13 02:33:24 +0900

    feat(blackjack) : create Players.kt and PlayersTest.kt

---
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -0,0 +1,24 @@
+package blackjack.domain
+
+import blackjack.domain.Game.Companion.DEFAULT_CARD_AMOUNT
+
+data class Players(private val players: List<Player>) {
+
+    fun setUpWithCards() {
+        players.forEach { player -> (1..DEFAULT_CARD_AMOUNT).map { player.draw(Dealer.giveCard()) } }
+    }
+
+    fun findPlayer(nth: Int): Player {
+        return players[nth]
+    }
+
+    fun size() = players.size
+
+    fun displayCurrentStates(): String {
+        return players.joinToString("\n") { "${it}카드: ${it.displayCards()}" }
+    }
+
+    override fun toString(): String {
+        return players.joinToString()
+    }
+}
