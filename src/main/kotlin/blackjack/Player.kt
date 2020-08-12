4159fb702ca10a646be316cdcc8a96d9248e2c49
developer-donghwan <ycdoit@naver.com>
2020-08-12 16:24:00 +0900

    Player Class 생성

---
+++ src/main/kotlin/blackjack/Player.kt
@@ -0,0 +1,29 @@
+package blackjack
+
+const val WIN_SCORE = 21
+
+class Player(val name: String) {
+    private val _cards = mutableListOf<Card>()
+    val cards: List<Card>
+        get() = _cards.deepCopy()
+    var isAvailable: Boolean = true
+        get() = countingPoint() < WIN_SCORE
+    var isWinner: Boolean = false
+        get() = countingPoint() == WIN_SCORE
+
+    fun pickCard(card: Card) {
+        _cards.add(card)
+    }
+
+    fun countingPoint(): Int {
+        val value1 = _cards.sumBy { it.value.value1 }
+        val value2 = _cards.sumBy { it.value.value2 }
+        return when {
+            value1 == WIN_SCORE -> value1
+            value2 == WIN_SCORE -> value2
+            else -> value1
+        }
+    }
+
+    private fun List<Card>.deepCopy(): List<Card> = map { it.copy() }
+}
