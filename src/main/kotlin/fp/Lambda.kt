516097b3f2a7bbeca1f3440ce2a17caeee3721eb
SunHo Van <vsh123@naver.com>
2020-08-13 01:50:43 +0900

    feat: 람다 실습 2

--- src/main/kotlin/fp/Lambda.kt
+++ src/main/kotlin/fp/Lambda.kt
@@ -1,1 +1,9 @@
 package fp
+
+fun sumAll(numbers: List<Int>): Int = sum(numbers) { true }
+
+fun sumAllEven(numbers: List<Int>): Int = sum(numbers) { number -> number % 2 == 0 }
+
+fun sumAllOverThree(numbers: List<Int>): Int = sum(numbers) { number -> number > 3 }
+
+fun sum(numbers: List<Int>, isAdd: (Int) -> Boolean): Int = numbers.filter { isAdd(it) }.sum()
