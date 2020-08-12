3e484b4894dd3e366f1f45266a7c81fbf021dabf
bky373 <bo373@naver.com>
2020-08-13 02:45:11 +0900

    refactor(domain) : modify initialOfCard function

--- src/main/kotlin/blackjack/domain/CardScore.kt
+++ src/main/kotlin/blackjack/domain/CardScore.kt
@@ -1,27 +1,24 @@
 package blackjack.domain
 
-enum class CardScore(val score: Int) {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+enum class CardScore(val score: Int, val initial: String = "") {
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K");
 
     companion object {
 
         fun initialOfCard(cardScore: CardScore): String {
-            return when (cardScore) {
-                ACE, JACK, QUEEN, KING -> cardScore.name[0].toString()
-                else -> cardScore.score.toString()
-            }
+            return cardScore.initial
         }
     }
 }
