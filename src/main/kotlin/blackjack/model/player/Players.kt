eedfdae242d9f56493b3efba4bd625c5b9e082d5
jejun <jjin.dev@gmail.com>
2020-08-13 21:51:19 +0900

    Refactor(step3): for문을 map, foreach 로 대체

--- src/main/kotlin/blackjack/model/player/Players.kt
+++ src/main/kotlin/blackjack/model/player/Players.kt
@@ -8,40 +8,26 @@ class Players(private val players: List<Player>) {
     }
 
     fun drawCard(cardDeck: CardDeck) {
-        doAction { it.drawCard(cardDeck.pick()) }
+        players.forEach { it.drawCard(cardDeck.draw()) }
     }
 
     fun progressTurn(cardDeck: CardDeck) {
-        doAction { it.progressTurnForEach(cardDeck) }
+        players.forEach { it.progressTurnForEach(cardDeck) }
     }
 
     fun checkWinOrLose() {
-        doAction { it.checkWinOrLose(players) }
+        players.forEach { it.checkWinOrLose(players) }
     }
 
     fun getDrawCardResults(): List<String> {
-        return getResult { "${it.name}카드:${it.getDisplayCards()}" }
+        return players.map { "${it.name}카드:${it.getDisplayCards()}" }
     }
 
     fun getPointResults(): List<String> {
-        return getResult { "${it.name}카드: ${it.getDisplayCards()} - 결과: ${it.getTotalPointForBlackJack()}" }
+        return players.map { "${it.name}카드: ${it.getDisplayCards()} - 결과: ${it.getTotalPointForBlackJack()}" }
     }
 
     fun getWinnerResults(): List<String> {
-        return getResult { "${it.name}:${it.winLoseResult}" }
-    }
-
-    private fun doAction(getAction: (Player) -> Unit) {
-        for (player in players) {
-            getAction(player)
-        }
-    }
-
-    private fun getResult(getDetails: (Player) -> String): List<String> {
-        val results = mutableListOf<String>()
-        for (player in players) {
-            results.add(getDetails(player))
-        }
-        return results
+        return players.map { "${it.name}:${it.winLoseResult}" }
     }
 }
