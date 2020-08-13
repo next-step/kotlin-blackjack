6d5c700f1487f2d169764b5ef9ef5d4cdcb2dbcd
bky373 <bo373@naver.com>
2020-08-13 20:44:29 +0900

    style(view) : rename REPLY_STAY to REPLY_STAND

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -5,7 +5,7 @@ import blackjack.domain.Player
 const val ENTER_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
 const val ENTER_ONLY_AVAILABLE_REPLIES = "대답은 y(예), n(아니오)로만 할 수 있습니다"
 const val REPLY_HIT = "y"
-const val REPLY_STAY = "n"
+const val REPLY_STAND = "n"
 
 object InputView {
 
@@ -15,7 +15,7 @@ object InputView {
 
     fun readReplyToDrawing(player: Player): String {
         var text = readText(message = "${player}는(은) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-        val availableReplies = listOf(REPLY_HIT, REPLY_STAY)
+        val availableReplies = listOf(REPLY_HIT, REPLY_STAND)
         while (!availableReplies.contains(text)) {
             text = readText(message = ENTER_ONLY_AVAILABLE_REPLIES)
         }
