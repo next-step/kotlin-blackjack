bc8fb9a3c5ca67e0e9c058efb5e52f1d777a9181
BeokBeok <kekemusa37@nate.com>
2020-08-14 22:22:24 +0900

    feat(gamble) : ADD - 배팅 금액 요청 함수

--- src/main/kotlin/blackjack/InputView.kt
+++ src/main/kotlin/blackjack/InputView.kt
@@ -6,4 +6,13 @@ object InputView {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
         return readLine()?.split(",") ?: emptyList()
     }
+
+    fun requestGambleMoney(players: List<Player>): List<Int> {
+        val gambleMoneyPerPlayer = mutableListOf<Int>()
+        players.map {
+            println("${it}의 배팅 금액은?")
+            gambleMoneyPerPlayer.add(readLine()?.toInt() ?: 0)
+        }
+        return gambleMoneyPerPlayer
+    }
 }
