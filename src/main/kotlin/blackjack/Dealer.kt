327805be1ee631a026702e919e8cb46c81d8c98a
BeokBeok <kekemusa37@nate.com>
2020-08-15 00:36:43 +0900

    test(dealer) : APPLY - 한 장 더 받기 가능 여부 TC

--- src/main/kotlin/blackjack/Dealer.kt
+++ src/main/kotlin/blackjack/Dealer.kt
@@ -1,3 +1,14 @@
 package blackjack
 
-class Dealer(name: String = "딜러") : Gamer(name)
+class Dealer(name: String = "딜러") : Gamer(name) {
+
+    fun checkIfExtraCardOrNot(): Boolean {
+        val isTwoCard = myCards.size == Constant.FIRST_HAVE_NUMBER_OF_CARD
+        val isAvailableAnotherCard = calculatePoint() <= EXTRA_CARD_AVAILABLE_LIMIT_POINT
+        return isTwoCard && isAvailableAnotherCard
+    }
+
+    companion object {
+        private val EXTRA_CARD_AVAILABLE_LIMIT_POINT = Point(16)
+    }
+}
--- src/test/kotlin/blackjack/DealerTest.kt
+++ src/test/kotlin/blackjack/DealerTest.kt
@@ -1,3 +1,17 @@
 package blackjack
 
-class DealerTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DealerTest {
+
+    @Test
+    fun `한 장 더 받기 가능 여부`() {
+        val dealer = Dealer().apply {
+            requestCard(Card(Denomination.ACE to Shape.DIAMOND))
+            requestCard(Card(Denomination.FIVE to Shape.DIAMOND))
+        }
+
+        assertThat(dealer.checkIfExtraCardOrNot()).isTrue()
+    }
+}
