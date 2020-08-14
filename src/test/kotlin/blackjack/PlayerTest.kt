30a9f8538f6c2168d8dd06ec5dfe3824e6573b41
BeokBeok <kekemusa37@nate.com>
2020-08-14 22:10:39 +0900

    test(player) : ADD - 플레이어 이름 출력하기 TC

--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -1,3 +1,19 @@
 package blackjack
 
-class PlayerTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PlayerTest {
+
+    @Test
+    fun `플레이어 이름 출력하기`() {
+        val player = Player("moshi")
+
+        assertThat(player.toString()).isEqualTo("moshi")
+    }
+}
+
+class Player(private val name: String) {
+
+    override fun toString(): String = name
+}
