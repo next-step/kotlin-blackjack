59c240e2be418d056c617f3f084bad9362a1ceb7
Jonny-Cho <andante2183@gmail.com>
2020-08-15 00:20:31 +0900

    feat(Suits): add Suits enum and test

---
+++ src/test/kotlin/blackjack/domain/SuitsTest.kt
@@ -0,0 +1,15 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SuitsTest {
+    @Test
+    fun `clubs, diamonds, hearts, spades 4가지 종류의 모양을 가진다`() {
+        assertThat(Suits.values().size).isEqualTo(4)
+        assertThat(Suits.CLUBS).isEqualTo(Suits.CLUBS)
+        assertThat(Suits.DIAMONDS).isEqualTo(Suits.DIAMONDS)
+        assertThat(Suits.HEARTS).isEqualTo(Suits.HEARTS)
+        assertThat(Suits.SPADES).isEqualTo(Suits.SPADES)
+    }
+}
