2c3fc2703cf378504517ec39c16cb1499a879e12
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-15 20:51:10 +0900

    feature(kotlin-blackjack): 카드 목록 생성 기능 추가

--- src/main/kotlin/model/Dealer.kt
+++ src/main/kotlin/model/Dealer.kt
@@ -1,5 +1,12 @@
 package model
 
 class Dealer {
-
+    companion object {
+        val CARD_LIST: List<Card>
+            get() {
+                val cardList = mutableListOf<Card>()
+                Suit.values().map { suit -> { Denomination.values().map { denomination -> { Card(suit, denomination) } } } }
+                return cardList.toList()
+            }
+    }
 }
