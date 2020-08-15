954be6d2a030315fbd678a58f80d355d8765994a
BeokBeok <kekemusa37@nate.com>
2020-08-15 20:38:30 +0900

    feat(app) : REFACT - main function

--- src/main/kotlin/blackjack/BlackJackApp.kt
+++ src/main/kotlin/blackjack/BlackJackApp.kt
@@ -1,26 +1,40 @@
 package blackjack
 
 fun main() {
-
     val playerNames = InputView.requestPlayerNames()
-    val (dealer, players) = Dealer() to playerNames.map(::Player)
-    val game = BlackJackGame(dealer, players).apply {
-        initCardForDealer()
-        initCardForPlayers()
-    }
+    val game = registerGame(playerNames)
     val gambleMoneyPerPlayer = InputView.requestGambleMoney(playerNames)
-
     ResultView.printPreview(game)
 
-    players.map {
-        while (!Point.isReachMaxPoint(it.calculatePoint()) && InputView.requestOneOfCard(it) == "y") {
+    drawCard(game)
+
+    showSummary(game)
+    showRevenue(game, gambleMoneyPerPlayer)
+}
+
+private fun showRevenue(game: BlackJackGame, gambleMoneyPerPlayer: List<Int>) {
+    ResultView.printRevenue(game, gambleMoneyPerPlayer)
+}
+
+private fun showSummary(game: BlackJackGame) {
+    (game.dealer as Dealer).requestCardIfPossibleExtraCard(Deck.pop())
+    ResultView.printResult(game)
+}
+
+private fun drawCard(game: BlackJackGame) {
+    game.players.map {
+        while (isContinueDraw(it)) {
             it.requestCard(Deck.pop())
             ResultView.printCard(it)
         }
     }
-
-    dealer.requestCardIfPossibleExtraCard(Deck.pop())
-    ResultView.printResult(game)
-
-    ResultView.printRevenue(game, gambleMoneyPerPlayer)
 }
+
+private fun isContinueDraw(gamer: Gamer) =
+    !Point.isReachMaxPoint(gamer.calculatePoint()) && InputView.requestOneOfCard(gamer) == "y"
+
+private fun registerGame(playerNames: List<String>): BlackJackGame =
+    BlackJackGame(dealer = Dealer(), players = playerNames.map(::Player)).apply {
+        initCardForDealer()
+        initCardForPlayers()
+    }
