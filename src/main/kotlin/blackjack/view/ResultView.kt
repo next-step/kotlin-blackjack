899d3caa7b18c4b206a87ab0b3f1b75f9c957f99
zion830 <zion830@honglab.org>
2020-08-15 22:29:10 +0900

    refactor(blackjack): 우승자가 없을 경우의 메시지 추가

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -7,6 +7,7 @@ import blackjack.domain.player.Players
 object ResultView {
     private const val GIVE_CARD = "에게 2장의 카드를 나누었습니다."
     private const val WINNER = "우승자 :"
+    private const val WINNER_IS_DEALER = "아쉽지만 딜러가 이겼어요...!"
 
     fun printGiveCard(players: Players) {
         println("${players.participants.joinToString { it.name }}$GIVE_CARD")
@@ -19,7 +20,7 @@ object ResultView {
     }
 
     fun printWinners(winners: List<Player>) {
-        println("$WINNER $winners")
+        println(if (winners.isEmpty()) WINNER_IS_DEALER else "$WINNER $winners")
     }
 
     fun printPlayer(player: Player, handStatus: HandStatus) {
