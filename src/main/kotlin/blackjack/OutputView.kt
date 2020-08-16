877b0e4814c5fd1bef0bc8505325db82562626a2
LeeYounghyeon <leey9494@icloud.com>
2020-08-16 18:12:03 +0900

    feat(blackjack): 각 유저 카드 목록과 우승자를 보여주는 view

--- src/main/kotlin/blackjack/OutputView.kt
+++ src/main/kotlin/blackjack/OutputView.kt
@@ -1,10 +1,22 @@
 package blackjack
 
 object OutputView {
+    private const val SEPARATOR = ", "
+
     fun showPlayersCard(players: List<Player>) {
-        println("${players.joinToString(", ") { it.name }}에게 2장의 카드를 나누어 주었습니다.")
+        println("${players.joinToString(SEPARATOR) { it.getName() }}에게 2장의 카드를 나누어 주었습니다.")
         players.forEach {
-            println("${it.name}카드: ${it.getCards().userCards}")
+            getUserCard(it)
+        }
+    }
+
+    fun getUserCard(player: Player) {
+        println("${player.getName()}카드: ${player.getCards()}")
+    }
+
+    fun getWinner(players: List<Player>) {
+        players.forEach {
+            println("${it.getName()}카드: ${it.getCards()} - 결과: ${it.getScore()}")
         }
     }
 }
