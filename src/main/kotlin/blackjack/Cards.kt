de2cf3f4eee2fab12ac8ed87da64cabd8eeaa46e
LeeYounghyeon <leey9494@icloud.com>
2020-08-16 20:03:20 +0900

    feat(blackjack): card 숫자의 합이 21보다 크면 true를 반환하는 기능 및 테스트 구현

--- src/main/kotlin/blackjack/Cards.kt
+++ src/main/kotlin/blackjack/Cards.kt
@@ -1,15 +1,27 @@
 package blackjack
 
-class Cards {
-    var userCards = mutableListOf(Card(), Card())
-        private set
-
-    fun addCard() {
-        userCards.add(Card())
-    }
+data class Cards(private var userCards: MutableList<Card>) {
+    private var score: Int = sumCardNumbers()
 
     fun getCards() = userCards
+    fun getScore() = score
+
+    fun addCard(card: Card) {
+        userCards.add(card)
+        score = sumCardNumbers()
+    }
+
+    fun isGraterThanWinScore(card: Card) = score + card.getCardScore() > WIN_SCORE
+
+    fun sumCardNumbers(): Int {
+        return userCards.map { it.getCardScore() }.sum()
+    }
+
     override fun toString(): String {
         return "$userCards"
+    }
+
+    companion object {
+        const val WIN_SCORE = 21
     }
 }
--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -5,12 +5,24 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class CardsTest {
-    @DisplayName("card가 추가되면 카드 목록의 사이즈는 1 늘어난다.")
+    private val card = Cards(mutableListOf(Card(Symbol.HEART, Numbers.SEVEN), Card(Symbol.HEART, Numbers.JACK)))
+
+    @DisplayName("카드 추가되면 카드 목록의 사이즈는 1 늘어난다.")
     @Test
     fun `card size added`() {
-        val card = Cards()
-        card.addCard()
-        println(card.getCards().size)
+        card.addCard(Card(Symbol.HEART, Numbers.FOUR))
         assertThat(card.getCards().size).isEqualTo(3)
+    }
+
+    @DisplayName("카드 번호들의 숫자를 더한다.")
+    @Test
+    fun `add card numbers`() {
+        assertThat(card.sumCardNumbers()).isEqualTo(17)
+    }
+
+    @DisplayName("카드 번호의 합이 21보다 크면 true를 반환한다.")
+    @Test
+    fun `when card sum is greater than 21 than return true`() {
+        assertThat(card.isGraterThanWinScore(Card(Symbol.HEART, Numbers.JACK))).isTrue()
     }
 }
