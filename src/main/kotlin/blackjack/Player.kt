4068afc97effb1cfc6de175ad65425a68500fb44
LeeYounghyeon <leey9494@icloud.com>
2020-08-16 20:04:40 +0900

    feat(blackjack): player가 y를 선택하면 카드를 추가하는 기능, y를 선택했는데 점수가 21보다 크면 exception을 던지는 기능 및 테스트 구현

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -1,12 +1,21 @@
 package blackjack
 
-class Player(val name: String) {
-    private val cards = Cards()
+data class Player(private val name: String, private val cards: Cards) {
+    fun getName() = name
     fun getCards() = cards
 
-    fun selectValue(selectValue: String) {
-        if (selectValue == "y") {
-            cards.addCard()
+    fun addMoreCards(selectValue: String, card: Card): Boolean {
+        return when {
+            selectValue == NO -> true
+            cards.isGraterThanWinScore(card) -> throw IllegalArgumentException("21을 초과하여 카드를 입력할 수 없습니다.")
+            else -> {
+                cards.addCard(card)
+                false
+            }
         }
+    }
+
+    companion object {
+        const val NO = "n"
     }
 }
