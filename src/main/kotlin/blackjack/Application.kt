581a971d801c818b44ba80e86f7cd8154e313742
LeeYounghyeon <leey9494@icloud.com>
2020-08-16 20:06:32 +0900

    feat(blackjack): Application.kt 구현

--- src/main/kotlin/blackjack/Application.kt
+++ src/main/kotlin/blackjack/Application.kt
@@ -2,9 +2,7 @@ package blackjack
 
 fun main() {
     val players = Players(InputView.inputPlayers())
-    OutputView.showPlayersCard(players.players)
-    players.players.forEach {
-        val selectedValue = InputView.selectMoreCard(it.name)
-        it.selectValue(selectedValue)
-    }
+    OutputView.showPlayersCard(players.getPlayers())
+    BlackjackGame.playGame(players.getPlayers())
+    OutputView.getWinner(players.getPlayers())
 }
