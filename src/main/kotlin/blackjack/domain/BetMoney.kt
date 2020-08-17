9c7e56ee5e64f4dd2fe9fe974807a6f3f81af267
EthanChae <boschae36@gmail.com>
2020-08-18 02:34:12 +0900

    refactor(blackjack): Remove "run" and "asSequence"

--- src/main/kotlin/blackjack/domain/BetMoney.kt
+++ src/main/kotlin/blackjack/domain/BetMoney.kt
@@ -4,7 +4,7 @@ private val MONEY_REGEX = "(\\d+)".toRegex()
 const val MIN_MONEY = 1
 const val MAX_MONEY = 100_000
 
-class BetMoney(val money: Int) {
+data class BetMoney(val money: Int) {
 
     companion object {
         fun newInstance(moneyString: String): BetMoney? {
@@ -13,20 +13,5 @@ class BetMoney(val money: Int) {
             if (money in MIN_MONEY..MAX_MONEY) return BetMoney(money)
             return null
         }
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as BetMoney
-
-        if (money != other.money) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return money
     }
 }
