21003a3f4901443f90f79169bcfaf7e082730e97
jejun <jjin.dev@gmail.com>
2020-08-17 17:22:12 +0900

    Refactor(step4): 사용하지 않는 코드 삭제

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -13,7 +13,6 @@ object InputView {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
 
         val names = readLine().toString().split(PLAYER_NAME_DELIMITER)
-        val gamers = names.map { Gamer(it) }
 
         return names.map { Gamer(it) }
     }
