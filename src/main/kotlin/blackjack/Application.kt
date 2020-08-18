efa777e03e24a807feea297d7ca3d985c1f61886
LeeYounghyeon <leey9494@icloud.com>
2020-08-19 01:08:52 +0900

    refactor(blackjack): application.kt 가 view 로직을 처리하도록 구현

--- src/main/kotlin/blackjack/Application.kt
+++ src/main/kotlin/blackjack/Application.kt
@@ -3,6 +3,27 @@ package blackjack
 fun main() {
     val players = Players(InputView.inputPlayers())
     OutputView.showPlayersCard(players.getPlayers())
-    BlackjackGame.playGame(players.getPlayers())
+    playGame(players.getPlayers())
     OutputView.getWinner(players.getPlayers())
+}
+
+fun playGame(players: List<Player>) {
+    players.forEach {
+        val score = it.getCards().sumCardNumbers()
+        if (score > Cards.WIN_SCORE) return
+        addUserCards(it)
+    }
+}
+
+private fun addUserCards(player: Player) {
+    loop@ while (true) {
+        try {
+            val selectedValue = InputView.selectMoreCard(player.getName())
+            if (player.addMoreCards(selectedValue, Card.getInstances())) break@loop
+            OutputView.getUserCard(player)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            break@loop
+        }
+    }
 }
