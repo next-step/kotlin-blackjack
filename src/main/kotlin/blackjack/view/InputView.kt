de7836bd59d3596533ebda751139fe03f59dbed6
Jonny-Cho <andante2183@gmail.com>
2020-08-19 22:44:26 +0900

    refactor(InputView): change to tailrec

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,17 +1,22 @@
 package blackjack.view
 
 import blackjack.domain.Player
-import blackjack.domain.Players
 
 object InputView {
-    fun getPlayers(): Players {
-        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        val names = readLine()!!.split(",")
-        return Players(names)
+    private const val INPUT_PLAYERS = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val DELIMITER = ","
+
+    tailrec fun getPlayers(): List<String> {
+        println(INPUT_PLAYERS)
+        return readLine()
+            ?.split(DELIMITER)
+            ?.takeIf { it.isNotEmpty() }
+            ?: getPlayers()
     }
 
-    fun getMoreCard(it: Player): String {
+    tailrec fun getMoreCard(it: Player): String {
         println("${it.name}님은 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-        return readLine()!!
+        return readLine()
+            ?: getMoreCard(it)
     }
 }
