8fc01679f7f29b4b4b63e1d27d4106c91d655413
Jonny-Cho <andante2183@gmail.com>
2020-08-19 23:23:58 +0900

    refactor(Main): split private methods

--- src/main/kotlin/blackjack/Main.kt
+++ src/main/kotlin/blackjack/Main.kt
@@ -1,23 +1,36 @@
 package blackjack
 
-import blackjack.domain.Deck
-import blackjack.domain.Players
+import blackjack.domain.Participants
+import blackjack.domain.deck.Deck
 import blackjack.view.InputView
 import blackjack.view.ResultView
 
+const val FIRST_SETTING_CARD_NUMBER = 2
+
 fun main() {
-    val players = InputView.getPlayers()
-    players.addCards(Deck, 2)
-    ResultView.printPlayersCard(players)
-    playBlackjack(players)
-    ResultView.printResult(players)
+    val participants = getParticipants()
+    playGame(participants)
 }
 
-fun playBlackjack(players: Players) {
-    players.players.forEach {
+private fun getParticipants(): Participants {
+    val playerNames = InputView.getPlayers()
+    return Participants(playerNames)
+}
+
+private fun playGame(participants: Participants) {
+    Deck.reset()
+    participants.addCards(FIRST_SETTING_CARD_NUMBER)
+    ResultView.printPlayersCard(participants)
+
+    playBlackjack(participants)
+    ResultView.printResult(participants)
+}
+
+fun playBlackjack(participants: Participants) {
+    participants.players.forEach {
         while (it.isGaming()) {
-            val yOrN = InputView.getMoreCard(it)
-            it.choose(yOrN)
+            val yesOrNo = InputView.getMoreCard(it)
+            it.choose(yesOrNo)
         }
         ResultView.printPlayerCard(it)
     }
