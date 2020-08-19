9365ae22f159e5e47e45e35e06b34299f6a3b3b4
LeeYounghyeon <leey9494@icloud.com>
2020-08-19 22:34:06 +0900

    refactor(blackjack): players에 딜러와 player를 add 한다

--- src/main/kotlin/blackjack/Players.kt
+++ src/main/kotlin/blackjack/Players.kt
@@ -1,8 +1,17 @@
 package blackjack
 
 class Players(private val playerNames: List<String>) {
-    private val players: List<Player> =
-        playerNames.map { Player(it, Cards((0..1).map { Card.getInstances() }.toMutableList())) }
+    private val players: List<Gamer> = addPlayers(playerNames, Cards(mutableListOf(Card.getInstances())))
+
+    private fun addPlayers(playerNames: List<String>, cards: Cards): List<Gamer> {
+        val players = mutableListOf<Gamer>(Dealer(cards))
+
+        playerNames.forEach {
+            players.add(Player(it, Cards((0..1).map { Card.getInstances() }.toMutableList())))
+        }
+
+        return players.toList()
+    }
 
     fun getPlayers() = players
 }
