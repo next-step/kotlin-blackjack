d5f38f6588f42ecf2278640a927ea9b04e60bc24
Jonny-Cho <andante2183@gmail.com>
2020-08-19 23:20:20 +0900

    refactor(Player): change to use getScore method of card

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -2,12 +2,12 @@ package blackjack.domain
 
 import blackjack.domain.deck.Card
 import blackjack.domain.deck.Deck
-import blackjack.domain.deck.Denomination
+import blackjack.domain.deck.Denomination.Companion.BLACKJACK_COUNT
 
 class Player(val name: String) {
     val cards = mutableListOf<Card>()
-    val total get() = cards.sortedBy { it.denomination }.fold(0) { total, card -> total + Denomination.scoreOf(card.denomination, total) }
-    private val isBust get() = total > 21
+    val total get() = cards.sortedBy { it.denomination }.fold(0) { total, card -> total + card.getScore(total) }
+    private val isBust get() = total > BLACKJACK_COUNT
     var isStay = false
 
     fun addCard(card: Card): List<Card> {
