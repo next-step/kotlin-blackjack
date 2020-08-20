7b1930d5c6b83bc3f86e1f456b6fc2643075f539
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-20 19:26:19 +0900

    feature(kotlin-blackjack): 플레이어 딜러 생성

---
+++ src/main/kotlin/model/AbstractPlayer.kt
@@ -0,0 +1,47 @@
+package model
+
+import kotlin.math.abs
+
+abstract class AbstractPlayer(val name: PlayerName, val playerType: PlayerType) {
+    private val cardMutableList: MutableList<Card> = mutableListOf()
+
+    constructor(name: String) : this(PlayerName(name), PlayerType.PLAYER)
+
+    val cards: List<Card>
+        get() = cardMutableList.toList()
+
+    fun receive(card: Card) = cardMutableList.add(card)
+
+    fun score(): Int {
+        val sum = cards.sumBy { it.denomination.score }
+        return if (isAceBonus(sum)) {
+            sum + ACE_BONUS
+        } else {
+            sum
+        }
+    }
+
+    private fun isAceBonus(sum: Int) = hasAce() && sum <= ACE_BONUS_CONDITION
+
+    private fun hasAce(): Boolean {
+        return cards.any { it.denomination.isAce() }
+    }
+
+    fun isOver(): Boolean = score() >= WINNING_POINT
+
+    fun scoreGap(): Int = abs(WINNING_POINT - score())
+
+    fun isReceiveCard(isReceive: Boolean): Boolean {
+        return isReceive && !isOver()
+    }
+
+    fun cardCount(): Int {
+        return cardMutableList.size
+    }
+
+    companion object {
+        const val WINNING_POINT = 21
+        const val ACE_BONUS = 10
+        const val ACE_BONUS_CONDITION = 11
+    }
+}
---
+++ src/main/kotlin/model/DealerPlayer.kt
@@ -0,0 +1,5 @@
+package model
+
+class DealerPlayer(name: PlayerName = PlayerName("딜러")): AbstractPlayer(name, PlayerType.DEALER) {
+
+}
