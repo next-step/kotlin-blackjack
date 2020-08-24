6e14f74760a588c62159c99a7aeffa3c016ff798
Parkjoopika <parkjoohan0603@gmail.com>
2020-08-25 01:06:00 +0900

    fix: BlackJack Class add method readyGame

--- src/main/kotlin/blackJack/domain/BlackJack.kt
+++ src/main/kotlin/blackJack/domain/BlackJack.kt
@@ -2,4 +2,17 @@ package blackJack.domain
 
 class BlackJack(val players: Players) {
     val dealer = Dealer()
+
+    init {
+        readyGame()
+    }
+
+    private fun readyGame() {
+        repeat(2) { giveCardAllPerson() }
+    }
+
+    private fun giveCardAllPerson() {
+        players.players.forEach { dealer.giveCard(it) }
+        dealer.giveCard(dealer)
+    }
 }
--- src/test/kotlin/blackJack/domain/BlackJackTest.kt
+++ src/test/kotlin/blackJack/domain/BlackJackTest.kt
@@ -12,4 +12,15 @@ class BlackJackTest() {
 
         assertThat(blackJack.players.players).hasSize(2)
     }
+
+    @Test
+    fun ready_game() {
+        val players = Players(listOf("joo", "han"))
+
+        val blackJack = BlackJack(players)
+
+        assertThat(blackJack.players.players[0].hands).hasSize(2)
+        assertThat(blackJack.players.players[1].hands).hasSize(2)
+        assertThat(blackJack.dealer.hands).hasSize(2)
+    }
 }
