a9108a38a22510e3db3db66ed2d170f2fea46d7e
Parkjoopika <parkjoohan0603@gmail.com>
2020-08-24 23:22:30 +0900

    feat: Dealer Class add method shuffleDeck
    cards를 주면 그 cards를 shuffle 한다 cards를 주지 않으면 DEFAULT_DECK을 shuffle 한다.

--- src/main/kotlin/blackJack/domain/Dealer.kt
+++ src/main/kotlin/blackJack/domain/Dealer.kt
@@ -2,4 +2,8 @@ package blackJack.domain
 
 class Dealer : Person("딜러") {
     val deck = Deck()
+
+    fun shuffleDeck(cards: List<Card> = Deck.DEFAULT_DECK) {
+        deck.shuffle(cards)
+    }
 }
--- src/test/kotlin/blackJack/domain/DealerTest.kt
+++ src/test/kotlin/blackJack/domain/DealerTest.kt
@@ -8,8 +8,18 @@ class DealerTest {
     fun make_dealer() {
         val dealer = Dealer()
 
-        assertThat(dealer.deck.cards).isNotEmpty()
+        assertThat(dealer.deck.cards).hasSize(52)
         assertThat(dealer.name).isEqualTo("딜러")
         assertThat(dealer.hands).isEmpty()
+    }
+
+    @Test
+    fun shuffle_deck() {
+        val dealer = Dealer()
+        val cards = listOf(SPADE_JACK, SPADE_QUEEN)
+
+        dealer.shuffleDeck(cards)
+
+        assertThat(dealer.deck.cards).hasSize(2)
     }
 }
