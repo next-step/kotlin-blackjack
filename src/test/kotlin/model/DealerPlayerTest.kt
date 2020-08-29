50b5f21183e736f4cdaabcc70cfd9b3c313b733e
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-08-29 19:27:36 +0900

    feature(kotlin-blackjack): step3 카드 dealer

---
+++ src/test/kotlin/model/DealerPlayerTest.kt
@@ -0,0 +1,25 @@
+package model
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class DealerPlayerTest {
+    @Test
+    @DisplayName("dealer 21점 이상이면 lose 를 반환한다")
+    fun `over21ScoreLose`() {
+        val dealer = DealerPlayer()
+        val player = Player("hello")
+        val cards = listOf(Card(Suit.CLUBS, Denomination.ACE), Card(Suit.CLUBS, Denomination.QUEEN), Card(Suit.CLUBS, Denomination.QUEEN), Card(Suit.CLUBS, Denomination.QUEEN))
+        val cardDealer = listOf(Card(Suit.CLUBS, Denomination.ACE), Card(Suit.CLUBS, Denomination.QUEEN), Card(Suit.CLUBS, Denomination.QUEEN), Card(Suit.CLUBS, Denomination.QUEEN))
+        receiveCard(dealer, cardDealer)
+        receiveCard(player, cards)
+        Assertions.assertThat(dealer.compareResult(player)).isEqualTo(BlackJackWinner.LOSE)
+    }
+
+    private fun receiveCard(player: AbstractPlayer, list:List<Card>) {
+        for(card in list) {
+            player.receive(card)
+        }
+    }
+}
