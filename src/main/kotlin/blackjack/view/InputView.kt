d95dfe917a633323318664c55870461ed5c1248d
bky373 <bo373@naver.com>
2020-08-30 20:11:42 +0900

    refactor(view) : modify InputView, ResultView for better readability

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,29 +1,26 @@
 package blackjack.view
 
-import blackjack.domain.Player
-import blackjack.domain.NO
-import blackjack.domain.YES
+import blackjack.model.participant.Player
 
-const val ENTER_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
-const val ENTER_ONLY_AVAILABLE_REPLIES = "대답은 y(예), n(아니오)로만 할 수 있습니다"
+private const val ENTER_PLAYER_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+private const val ENTER_ONLY_AVAILABLE_REPLIES = "대답은 y(예), n(아니오)로만 할 수 있습니다"
+private const val ENTER_BET_MONEY = "의 배팅 금액은?"
+private const val MINIMUM_BET_MONEY = 10
+private const val RE_ENTER_BET_MONEY = "${MINIMUM_BET_MONEY}원 이상 입력해주세요"
+val POSSIBLE_REPLIES: List<String> = listOf("y", "n")
 
 object InputView {
 
-    fun readPlayerNames(): String {
-        return readText(message = ENTER_PLAYER_NAMES)
-    }
+    fun readPlayerNames(): String = readText(message = ENTER_PLAYER_NAMES)
 
-    fun readReplyToDrawing(player: Player): String {
+    fun readBetMoney(name: String): Int = readNumber("\n${name}$ENTER_BET_MONEY?")
+
+    fun readReplyToHit(player: Player): String {
         var text = readText(message = "${player}는(은) 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-        val availableReplies = listOf(YES, NO)
-        while (!availableReplies.contains(text)) {
+        while (!POSSIBLE_REPLIES.contains(text)) {
             text = readText(message = ENTER_ONLY_AVAILABLE_REPLIES)
         }
         return text
-    }
-
-    fun readBetMoney(player: Player): Int {
-        return readNumber("\n${player}의 배팅 금액은?")
     }
 
     private fun readText(message: String = ""): String {
@@ -39,8 +36,8 @@ object InputView {
     private fun readNumber(message: String = ""): Int {
         println(message)
         var number = readLine()?.toIntOrNull()
-        while (number == null) {
-            println(message)
+        while (number == null || number < MINIMUM_BET_MONEY) {
+            println(RE_ENTER_BET_MONEY)
             number = readLine()?.toIntOrNull()
         }
         return number
--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -1,59 +1,40 @@
 package blackjack.view
 
-import blackjack.domain.Dealer
-import blackjack.domain.Game.Companion.DEFAULT_CARD_COUNT
-import blackjack.domain.Player
-import blackjack.domain.Players
-import blackjack.domain.YES
-import blackjack.domain.NO
+import blackjack.model.participant.Dealer
+import blackjack.model.participant.Player
+import blackjack.model.participant.Players
+import blackjack.model.profit.Profits
 
-const val DEALER_GETS_ONE_MORE_CARD = "딜러는 합계가 16이하라 한장의 카드를 더 받았습니다."
-const val DEALER_GETS_NO_CARD = "딜러는 합계가 17이상이라 카드를 더 받지 않았습니다."
-const val TITLE_OF_MATCH_RESULT = "## 최종 수익"
+const val TOOK_ANOTHER_CARD = "는 합계가 16이하라 한장의 카드를 더 받았습니다."
+const val TITLE_OF_PROFIT_RESULT = "## 최종 수익"
 
 object ResultView {
 
-    fun showResultOfSetUp(dealer: Dealer, players: Players) {
+    fun showSetUpResult(dealer: Dealer, players: Players) {
         println("\n${dealer}와 ${players}에게 2장의 카드를 나누었습니다")
-        println("$dealer: ${dealer.stateOfCards().substringAfter(",")}")
-        println(players.statesOfCards() + "\n")
+        println("$dealer: ${dealer.cardsDetail().substringBefore(",")}")
+        println("${players.cardsDetails()}\n")
     }
 
-    fun showCardsDetail(player: Player, reply: String) {
-        if (YES == reply) {
-            println("${player}카드: ${player.stateOfCards()}")
-        }
-        if (NO == reply && player.cardCount() == DEFAULT_CARD_COUNT) {
-            println("${player}카드: ${player.stateOfCards()}")
-        }
+    fun showCardsOf(player: Player) {
+        println("${player}카드: ${player.cardsDetail()}")
     }
 
-    fun showPlayOfDealer(dealer: Dealer) {
-        if (dealer.cardCount() != DEFAULT_CARD_COUNT) {
-            println("\n$DEALER_GETS_ONE_MORE_CARD")
-            return
-        }
-
-        println("\n$DEALER_GETS_NO_CARD")
+    fun showMessageOfGettingCard(dealer: Dealer) {
+        println("\n-- $dealer$TOOK_ANOTHER_CARD")
     }
 
     fun showScoreResult(dealer: Dealer, players: Players) {
-        println("\n$dealer 카드: ${dealer.stateOfCards()} - 결과: ${dealer.score()}")
-        val scoreResult =
-            (0 until players.size()).map { players.findPlayer(it) }
-                .joinToString("\n") { "${it}카드: ${it.stateOfCards()} - 결과: ${it.score()}" }
-
-        println(scoreResult)
+        println("\n$dealer 카드: ${dealer.cardsDetail()} - 결과: ${dealer.score()}")
+        println("${players.scoreDetails()}\n")
     }
 
-    fun showMatchResult(dealer: Dealer, players: Players) {
-        println("\n$TITLE_OF_MATCH_RESULT")
+    fun showProfitResult(profits: Profits) {
+        println(TITLE_OF_PROFIT_RESULT)
 
-        println("$dealer: ${dealer.result}")
+        val dealerProfit = profits.dealerProfit()
+        println("${dealerProfit.participantName}: ${dealerProfit.profit}")
 
-        val resultOfPlayers =
-            (0 until players.size()).map { players.findPlayer(it) }
-                .joinToString("\n") { "$it: ${it.profit}" }
-        println(resultOfPlayers)
+        println(profits)
     }
 }
