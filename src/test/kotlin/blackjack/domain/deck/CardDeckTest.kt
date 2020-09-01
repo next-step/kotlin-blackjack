80221ef29a5016959dfe7bf3794dd193535d9b30
zion830 <zion830@honglab.org>
2020-09-01 18:09:03 +0900

    refactor(blackjack): 카드덱을 생성자로 받을 수 있도록 수정

--- src/test/kotlin/blackjack/domain/deck/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/deck/CardDeckTest.kt
@@ -2,20 +2,17 @@ package blackjack.domain.deck
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Test
+import java.util.LinkedList
 
 class CardDeckTest {
-    private val cardDeck = CardDeck()
 
     @DisplayName("카드덱이 비면 덱을 자동으로 초기화한다")
-    @ParameterizedTest
-    @ValueSource(ints = [52])
-    fun initCardDeck(count: Int) {
-        var card = cardDeck.pop()
-        for (i in 0 until count) {
-            card = cardDeck.pop()
-        }
-        assertThat(card).isNotNull
+    @Test
+    fun resetCardDeck() {
+        val cardDeck = CardDeck(LinkedList(listOf(Card(Pip.ACE, Suit.CLUB))))
+        cardDeck.pop()
+        cardDeck.pop()
+        assertThat(cardDeck.size()).isEqualTo(51)
     }
 }
