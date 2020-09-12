2b07b1d186c8749c76277f05c0be20e19975e136
Jiwhun Kim <jiwhun.kim@outlook.com>
2020-09-12 14:03:53 +0900

    feature(kotlin-blackjack): 프로퍼티 접근 수정

--- src/main/kotlin/game/GameResultStatus.kt
+++ src/main/kotlin/game/GameResultStatus.kt
@@ -5,6 +5,7 @@ import model.PlayerName
 
 abstract class GameResultStatus(player: AbstractPlayer) {
     var playerName: PlayerName = player.name
+        private set
     var win: Int = 0
     var draw: Int = 0
     var lose: Int = 0
