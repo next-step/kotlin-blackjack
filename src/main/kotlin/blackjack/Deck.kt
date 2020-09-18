bfedf80dffcc72e7a4238a7cbdae4bda2ab611f3
myeongjang <jacobmjjo@gmail.com>
2020-09-19 00:53:39 +0900

    덱 클래스 에서 카드 뽑기 추가

--- src/main/kotlin/blackjack/Deck.kt
+++ src/main/kotlin/blackjack/Deck.kt
@@ -3,6 +3,12 @@ package blackjack
 class Deck {
     var deck = DEFAULT_DECK
 
+    fun shuffled() {
+        deck = deck.shuffled()
+    }
+
+    fun getCard(): Card = deck.get(0)
+
     companion object {
         val DEFAULT_DECK =
             Suit.values().flatMap { shape ->
--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -8,4 +8,9 @@ class DeckTest {
     fun `덱 사이즈가 52가 맞는지 확인`() {
         assertThat(Deck().deck.size).isEqualTo(52)
     }
+
+    @Test
+    fun `덱에서 카드가 뽑히는지 확인`() {
+        assertThat(Deck().getCard()).isEqualTo(Card(Denomination.ACE, Suit.CLUB))
+    }
 }
