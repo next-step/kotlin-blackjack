b49821726d784b91e072d234eb46a611af36ee2a
myeongjang <jacobmjjo@gmail.com>
2020-10-04 21:17:28 +0900

    create method add card to cards in cardsTest class

--- src/main/kotlin/domain/Cards.kt
+++ src/main/kotlin/domain/Cards.kt
@@ -1,7 +1,19 @@
 package domain
 
 data class Cards(private val cards: MutableList<Card>) {
+
+    fun getCards() = cards
+
     fun getTotalScore(): Int {
         return cards.map { it.score() }.sum()
+    }
+
+    fun addCard(card: Card): MutableList<Card> {
+        cards.add(card)
+        return getCards()
+    }
+
+    override fun toString(): String {
+        return cards.map { it }.toString()
     }
 }
--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -7,12 +7,20 @@ import domain.Suit
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+val sampleCard1 = Card(Pair(Suit.CLUB, Denomination.FIVE))
+val sampleCard2 = Card(Pair(Suit.DIAMOND, Denomination.FOUR))
+val sampleCards = Cards(mutableListOf(sampleCard1, sampleCard2))
+
 class CardsTest {
     @Test
     fun `카드 뭉치들 점수 확인`() {
-        val sampleCard1 = Card(Pair(Suit.CLUB, Denomination.FIVE))
-        val sampleCard2 = Card(Pair(Suit.DIAMOND, Denomination.FOUR))
-        val sampleCards = Cards(mutableListOf(sampleCard1, sampleCard2))
+
         assertThat(sampleCards.getTotalScore()).isEqualTo(9)
+    }
+
+    @Test
+    fun `카드 뭉치에 카드 추가`() {
+        val sampleAddCard = Card(Pair(Suit.HEART, Denomination.FIVE))
+        assertThat(sampleCards.addCard(sampleAddCard)).isEqualTo(mutableListOf(sampleCard1, sampleCard2, sampleAddCard))
     }
 }
