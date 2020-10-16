0ed5b1aa997810f10dc5410bf8aabd75c6482e8e
ldcc <jacobmjjo@gmail.com>
2020-10-16 18:02:45 +0900

    try deck test

--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -7,6 +7,14 @@ import org.junit.jupiter.api.Test
 class DeckTest {
     @Test
     fun `덱 사이즈가 52가 맞는지 확인`() {
-        assertThat(Deck.defaultDeck().size).isEqualTo(52)
+        val deck = Deck()
+        assertThat(deck.size).isEqualTo(52)
+    }
+
+    @Test
+    fun `덱이 카드 주면 카드가 하나 없어 져야 함`(){
+        val deck = Deck()
+        deck.giveCard(deck.shuffled())
+        assertThat(deck.shuffled().size).isEqualTo(51)
     }
 }
