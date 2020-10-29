bd3b21216fdf5206b18f60610834d3f6993b2af4
ldcc <jacobmjjo@gmail.com>
2020-10-29 19:10:47 +0900

    add Players Class

--- src/main/kotlin/blackjack/domain/Game.kt
+++ src/main/kotlin/blackjack/domain/Game.kt
@@ -1,3 +1,16 @@
 package blackjack.domain
 
-class Game(plyers: List<Player>, private val deck: Deck)
+class Game(players: List<Player>, private val deck: Deck) {
+    private val players = Players(players)
+    private var turn = 0
+
+    fun result(): List<Player> {
+        return (0 until players.size()).map { players.findPlayer(it) }
+    }
+
+    companion object {
+        private const val PLAYER_NAMES_DELIMITER = ","
+        const val MAXIMUM_GAME_SCORE = 21
+        const val DEFAULT_CARD_AMOUNT = 2
+    }
+}
--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -2,6 +2,20 @@ package blackjack.domain
 
 data class Players(private val players: List<Player>) {
     override fun toString(): String {
-        return players.toString()
+        return players.joinToString()
+    }
+
+    fun setUpWithCards(deck: Deck) {
+        players.forEach { player -> (1 until 21).map { player.draw(deck) } }
+    }
+
+    fun findPlayer(nth: Int): Player {
+        return players[nth]
+    }
+
+    fun size() = players.size
+
+    fun stateOfCards(): String {
+        return players.joinToString("/n") { "${it}카드: ${it.stateOfCards()}" }
     }
 }
