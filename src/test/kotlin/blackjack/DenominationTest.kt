1d4af501780169631372809d305182867cc26722
myeongjang <jacobmjjo@gmail.com>
2020-11-29 22:16:38 +0900

    created Denomination and tested

--- src/test/kotlin/blackjack/DenominationTest.kt
+++ src/test/kotlin/blackjack/DenominationTest.kt
@@ -6,18 +6,19 @@ import org.junit.jupiter.api.Test
 class DenominationTest {
 
     @Test
-    fun `Denomination 점수 출력`(){
+    fun `Denomination 점수 출력`() {
         val sample = Denomination.ACE
         assertThat(Denomination.getScore(sample)).isEqualTo(1)
     }
+
     @Test
-    fun `Denomination symbol print`(){
+    fun `Denomination symbol print`() {
         val sample = Denomination.QUEEN
         assertThat(Denomination.getSymbol(sample)).isEqualTo("QUEEN")
     }
 
     @Test
-    fun `Denomination Ace Calculator`(){
+    fun `Denomination Ace Calculator`() {
         assertThat(Denomination.AceCalculator(13)).isEqualTo(1)
         assertThat(Denomination.AceCalculator(9)).isEqualTo(11)
     }
