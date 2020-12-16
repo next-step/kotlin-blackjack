06a26c0fadd7d38796617630b0559626b2d8f4fb
ldcc <jacobmjjo@gmail.com>
2020-12-16 13:11:43 +0900

    deleted fp project

--- src/main/kotlin/fp/Car.kt
+++
@@ -1,9 +0,0 @@
-package fp
-
-data class Car(val name: String, val position: Int) {
-    fun move(moveornot: () -> Boolean): Car {
-        return if (moveornot()) {
-            copy(position = position + 1)
-        } else this
-    }
-}
