399e88527223736757c191d6dbc43d6ea59ac541
vsh123 <vsh123@naver.com>
2021-02-20 01:58:03 +0900

    step1(feat): 람다 실습 1 - 익명 클래스를 람다로 전환

---
+++ src/test/kotlin/lambda/CarTest.kt
@@ -0,0 +1,20 @@
+package lambda
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CarTest {
+    @Test
+    fun 이동() {
+        val car = Car("jason", 0)
+        val actual: Car = car.move { true }
+        assertThat(actual).isEqualTo(Car("jason", 1))
+    }
+
+    @Test
+    fun 정지() {
+        val car = Car("jason", 0)
+        val actual: Car = car.move { false }
+        assertThat(actual).isEqualTo(Car("jason", 0))
+    }
+}
