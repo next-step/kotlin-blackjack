23dfd73e3764c97f3ec277168aa59816cfda0e35
vsh123 <vsh123@naver.com>
2021-02-21 20:50:23 +0900

    revert : 처음 구현했던 것 리버트

--- src/main/kotlin/blackjack/domain/rate/EarningRate.kt
+++
@@ -1,9 +0,0 @@
-package blackjack.domain.rate
-
-import blackjack.domain.player.Dealer
-import blackjack.domain.player.Player
-
-interface EarningRate {
-    fun isAssignable(dealer: Dealer, player: Player): Boolean
-    fun getEarningRate(dealer: Dealer, player: Player): Double
-}
