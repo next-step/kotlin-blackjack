47df121852a3fc488104806d92a97b938c79c680
kit(김광수) <kit.t@kakaocorp.com>
2021-03-11 22:44:11 +0900

    feat: company & skills

--- src/test/kotlin/resume/ResumeTest.kt
+++ src/test/kotlin/resume/ResumeTest.kt
@@ -26,6 +26,28 @@ class ResumeTest {
         assertThat(person.name).isEqualTo("김광수")
     }
 
+    @Test
+    internal fun company() {
+        val person: Person = introduce {
+            company("마진좋아")
+        }
+        assertThat(person.company).isEqualTo("마진좋아")
+    }
+
+    @Test
+    internal fun skills() {
+        val person: Person = introduce {
+            skills {
+                hard("kotlin")
+                soft("Good communication skills")
+            }
+        }
+        assertThat(person.skills.toList()).containsExactlyInAnyOrder(
+            Hard("kotlin"),
+            Soft("Good communication skills")
+        )
+    }
+
     private fun introduce(initializer: Person.() -> Unit): Person {
         return Person().apply(initializer)
     }
@@ -34,8 +56,43 @@ class ResumeTest {
         lateinit var name: String
             private set
 
+        var company: String? = null
+            private set
+
+        lateinit var skills: Skills
+
         fun name(name: String) {
             this.name = name
         }
+
+        fun company(company: String) {
+            this.company = company
+        }
+
+        fun skills(initializer: Skills.() -> Unit) {
+            skills = Skills().apply(initializer)
+        }
+    }
+
+    class Skills {
+        private val skills: MutableList<Skill> = mutableListOf()
+
+        fun hard(name: String) {
+            skills.add(Hard(name))
+        }
+
+        fun soft(name: String) {
+            skills.add(Soft(name))
+        }
+
+        fun toList(): List<Skill> {
+            return skills
+        }
     }
 }
+
+sealed class Skill
+
+data class Hard(val name: String) : Skill()
+
+data class Soft(val name: String) : Skill()
