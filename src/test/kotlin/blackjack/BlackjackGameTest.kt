adf58b6e6769d14b9f2a2e98026c6823a617bf3f
kit(김광수) <kit.t@kakaocorp.com>
2021-03-13 17:58:47 +0900

    test: 처음엔 두장씩 준다

--- src/test/kotlin/blackjack/BlackjackGameTest.kt
+++ src/test/kotlin/blackjack/BlackjackGameTest.kt
@@ -1,12 +1,23 @@
 package blackjack
 
 import blackjack.PlayerTest.Player
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BlackjackGameTest {
     @Test
     internal fun `플레이어가 있다`() {
         BlackJackGame(listOf(Player("pobi"), Player("json")))
+    }
+
+    @Test
+    internal fun `처음엔 두장씩 준다`() {
+        val prepare = BlackJackGame(listOf(Player("pobi"), Player("json")), Dealer())
+            .prepare()
+        prepare.draw()
+        assertThat(listOf<Int>()).allSatisfy {
+            assertThat(it.cards.size).isEqualTo(2)
+        }
     }
 
     class BlackJackGame(private val players: List<Player>)
