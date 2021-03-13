53b1027b116058cbd7e9382d84fea84f44458117
kit(김광수) <kit.t@kakaocorp.com>
2021-03-13 23:12:46 +0900

    fix: 딜러삭제
    미션을 잘못확인하여 추가한 딜러 구현을 제거한다

--- src/test/kotlin/blackjack/BlackjackGameTest.kt
+++ src/test/kotlin/blackjack/BlackjackGameTest.kt
@@ -14,9 +14,9 @@ class BlackjackGameTest {
 
     @Test
     internal fun `처음엔 두장씩 준다`() {
-        val game = BlackJackGame(listOf(Player.Person("pobi"), Player.Person("json")), Dealer())
+        val game = BlackJackGame(listOf(Player.Person("pobi"), Player.Person("json")))
         game.prepareDraw()
-        assertThat(game.allPlayers()).allSatisfy {
+        assertThat(game.players).allSatisfy {
             assertThat(it.cards.size).isEqualTo(2)
         }
     }
@@ -24,7 +24,7 @@ class BlackjackGameTest {
     @Test
     internal fun `한장을 받는다`() {
         val pobi = Player.Person("pobi")
-        val game = BlackJackGame(listOf(pobi), Dealer())
+        val game = BlackJackGame(listOf(pobi))
         assertThat(pobi.cards.size).isEqualTo(0)
         game.draw({ true }) { player: Player ->
             assertThat(player).isEqualTo(pobi)
@@ -32,19 +32,12 @@ class BlackjackGameTest {
         }
     }
 
-    class Dealer(private val player: Player = Player.Person("dealer")) : Player by player
-
-    class BlackJackGame(
-        private val players: List<Player>,
-        private val dealer: Dealer = Dealer()
-    ) {
+    class BlackJackGame(val players: List<Player>) {
         fun prepareDraw() {
             repeat(2) {
-                allPlayers().forEach { it.draw(Card("A", Symbol.CLUBS)) }
+                players.forEach { it.draw(Card("A", Symbol.CLUBS)) }
             }
         }
-
-        fun allPlayers(): List<Player> = players + dealer
 
         fun draw(isDraw: (name: String) -> Boolean, result: (Player) -> Unit) {
             players.forEach {
