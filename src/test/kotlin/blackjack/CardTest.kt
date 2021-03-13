c970c0f4773ed9fe96a121efe3278821ae0e332c
kit(김광수) <kit.t@kakaocorp.com>
2021-03-13 09:22:45 +0900

    feat: make it pass

--- src/test/kotlin/blackjack/CardTest.kt
+++ src/test/kotlin/blackjack/CardTest.kt
@@ -1,5 +1,6 @@
 package blackjack
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CardTest {
@@ -16,6 +17,31 @@ class CardTest {
         val blackjack = blackjack {
             ace(1, 10)
         }
-        assertThat(blackjack.cards).contains(Card("A", "하트"), Card("A", "클로버"), Card("A", "스페이드"), Card("A", "다이아몬드"))
+        assertThat(blackjack.deck).contains(
+            Card("A", "하트", listOf(1, 10)),
+            Card("A", "클로버", listOf(1, 10)),
+            Card("A", "스페이드", listOf(1, 10)),
+            Card("A", "다이아몬드", listOf(1, 10))
+        )
     }
+
+    private fun blackjack(initializer: BlackjackBuilder.() -> Unit): BlackjackBuilder {
+        return BlackjackBuilder().apply(initializer)
+    }
+
+    class BlackjackBuilder {
+
+        var deck: List<Card> = emptyList()
+
+        fun ace(vararg numbers: Int) {
+            deck = deck + listOf(
+                Card("A", "하트", numbers.asList()),
+                Card("A", "클로버", numbers.asList()),
+                Card("A", "스페이드", numbers.asList()),
+                Card("A", "다이아몬드", numbers.asList())
+            )
+        }
+    }
+
+    data class Card(private val name: String, private val shape: String, private val number: List<Int>)
 }
