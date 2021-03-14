921203710e953ee296f6aa91dec0ef55bc53773c
kit(김광수) <kit.t@kakaocorp.com>
2021-03-14 18:46:44 +0900

    refactor: 구현 클래스 이동

---
+++ src/main/kotlin/blackjack/PlayerResult.kt
@@ -0,0 +1,56 @@
+package blackjack
+
+data class PlayerResult(
+    private val player: Player,
+    var wins: Int = 0,
+    var losses: Int = 0,
+    var draws: Int = 0
+) {
+    fun update(playResult: Player.Person.PlayResult) {
+        when (playResult) {
+            Player.Person.PlayResult.WIN -> wins += 1
+            Player.Person.PlayResult.LOSSES -> losses += 1
+            Player.Person.PlayResult.DRAWS -> draws += 1
+        }
+    }
+}
+
+fun result(builder: PlayResultBuilder.() -> Unit): PlayResultBuilder {
+    return PlayResultBuilder().apply(builder)
+}
+
+class PlayResultBuilder {
+    private val playerResult: MutableList<PlayerResult> = mutableListOf()
+
+    fun update(list: List<PlayerResult>) {
+        playerResult.addAll(list)
+    }
+
+    fun update(result: PlayerResult) {
+        playerResult.add(result)
+    }
+
+    fun build(): List<PlayerResult> {
+        return playerResult
+    }
+}
+
+infix fun List<Player>.vs(other: Player): List<PlayerResult> {
+    return map { it to (it vs other) }
+        .map { (player, playResult) ->
+            PlayerResult(player)
+                .apply {
+                    update(playResult)
+                }
+        } + (other vs this)
+}
+
+private infix fun Player.vs(players: List<Player>): PlayerResult {
+    val thisPlayer = this
+    return PlayerResult(this)
+        .apply {
+            for (player in players) {
+                update(thisPlayer vs player)
+            }
+        }
+}
---
+++ src/test/kotlin/blackjack/PlayResultTest.kt
@@ -0,0 +1,35 @@
+package blackjack
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PlayResultTest {
+    private val pobi = Player.Person("pobi")
+        .apply {
+            accept(Card("A", Symbol.DIAMONDS))
+            accept(Card("K", Symbol.DIAMONDS))
+        }
+    private val jason = Player.Person("jason")
+        .apply {
+            accept(Card("K", Symbol.DIAMONDS))
+            accept(Card("2", Symbol.DIAMONDS))
+        }
+    private val dealer = Player.Dealer()
+        .apply {
+            accept(Card("K", Symbol.DIAMONDS))
+            accept(Card("K", Symbol.DIAMONDS))
+        }
+
+    @Test
+    internal fun `승패를 계산한다`() {
+        val result = result {
+            update(listOf(pobi, jason) vs dealer)
+        }.build()
+
+        assertThat(result).contains(
+            PlayerResult(dealer, wins = 1, losses = 1),
+            PlayerResult(pobi, wins = 1),
+            PlayerResult(jason, losses = 1)
+        )
+    }
+}
