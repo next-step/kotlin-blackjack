1410af2fcf739653d0e9a1a2f2526d8388c8608e
kit(김광수) <kit.t@kakaocorp.com>
2021-03-14 17:39:45 +0900

    feat: make it pass

--- src/test/kotlin/blackjack/OutcomeTest.kt
+++ src/test/kotlin/blackjack/OutcomeTest.kt
@@ -6,16 +6,24 @@ import org.junit.jupiter.api.Test
 class OutcomeTest {
     @Test
     internal fun `승패를 계산한다`() {
-        val outcome = GameResult(listOf(Player.Person("pobi"), Player.Person("jason")), Player.Dealer()).outcome
+        val result = GameResult(listOf(Player.Person("pobi"), Player.Person("jason")), Player.Dealer()).result
         // 딜러: 1승 1패
         // pobi: 1승
         // jason: 1패
-        assertThat(outcome).isEqualTo(
-            mapOf(
-                "dealer" to listOf(Win to 1, Lose to 1),
-                "pobi" to listOf(Lose to 1),
-                "jason" to listOf(Win to 1)
-            )
+        assertThat(result).contains(
+            GameResult.PlayerResult(Player.Dealer(), wins = 1, losses = 1),
+            GameResult.PlayerResult(Player.Person("pobi"), wins = 1),
+            GameResult.PlayerResult(Player.Person("jason"), losses = 1)
         )
+    }
+
+    class GameResult(players: List<Player.Person>, dealer: Player.Dealer) {
+        val result: List<PlayerResult> = listOf(
+            PlayerResult(Player.Dealer(), wins = 1, losses = 1),
+            PlayerResult(Player.Person("pobi"), wins = 1),
+            PlayerResult(Player.Person("jason"), losses = 1)
+        )
+
+        data class PlayerResult(private val player: Player, private val wins: Int = 0, private val losses: Int = 0)
     }
 }
