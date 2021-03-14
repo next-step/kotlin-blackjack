7e025329ae56901a5bfd6ba6b68fe1ce29748f81
kit(김광수) <kit.t@kakaocorp.com>
2021-03-14 18:45:00 +0900

    refactor: builder 패턴 적용

--- src/test/kotlin/blackjack/OutcomeTest.kt
+++ src/test/kotlin/blackjack/OutcomeTest.kt
@@ -1,5 +1,6 @@
 package blackjack
 
+import blackjack.OutcomeTest.PlayerResult
 import blackjack.Player.Person.PlayResult
 import blackjack.Player.Person.PlayResult.DRAWS
 import blackjack.Player.Person.PlayResult.LOSSES
@@ -8,89 +9,87 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class OutcomeTest {
+    private val pobi = Player.Person("pobi")
+        .apply {
+            accept(Card("A", Symbol.DIAMONDS))
+            accept(Card("K", Symbol.DIAMONDS))
+        }
+    private val jason = Player.Person("jason")
+        .apply {
+            accept(Card("K", Symbol.DIAMONDS))
+            accept(Card("2", Symbol.DIAMONDS))
+        }
+    private val dealer = Player.Dealer()
+        .apply {
+            accept(Card("K", Symbol.DIAMONDS))
+            accept(Card("K", Symbol.DIAMONDS))
+        }
+
     @Test
     internal fun `승패를 계산한다`() {
-        val pobi = Player.Person("pobi")
-            .apply {
-                accept(Card("A", Symbol.DIAMONDS))
-                accept(Card("K", Symbol.DIAMONDS))
-            }
-        val jason = Player.Person("jason")
-            .apply {
-                accept(Card("K", Symbol.DIAMONDS))
-                accept(Card("2", Symbol.DIAMONDS))
-            }
-        val dealer = Player.Dealer()
-            .apply {
-                accept(Card("K", Symbol.DIAMONDS))
-                accept(Card("K", Symbol.DIAMONDS))
-            }
-
-        val result = GameResult(listOf(pobi, jason), dealer).result()
+        val result = result {
+            update(listOf(pobi, jason) vs dealer)
+        }.build()
 
         assertThat(result).contains(
-            GameResult.PlayerResult(dealer, wins = 1, losses = 1),
-            GameResult.PlayerResult(pobi, wins = 1),
-            GameResult.PlayerResult(jason, losses = 1)
+            PlayerResult(dealer, wins = 1, losses = 1),
+            PlayerResult(pobi, wins = 1),
+            PlayerResult(jason, losses = 1)
         )
     }
 
-    class GameResult(private val players: List<Player.Person>, private val dealer: Player.Dealer) {
-        fun result(): List<PlayerResult> {
-            return players.map { it to (it vs dealer) }
-                .map { (player, playResult) ->
-                    PlayerResult(player)
-                        .apply {
-                            update(playResult)
-                        }
-                } + PlayerResult(dealer)
-                .apply {
-                    for (player in players) {
-                        update(dealer vs player)
-                    }
-                }
+    private fun result(builder: PlayResultBuilder.() -> Unit): PlayResultBuilder {
+        return PlayResultBuilder().apply(builder)
+    }
+
+    class PlayResultBuilder {
+        private val playerResult: MutableList<PlayerResult> = mutableListOf()
+
+        fun update(list: List<PlayerResult>) {
+            playerResult.addAll(list)
         }
 
-        class PlayerResult(
-            private val player: Player,
-            var wins: Int = 0,
-            var losses: Int = 0,
-            var draws: Int = 0
-        ) {
+        fun update(result: PlayerResult) {
+            playerResult.add(result)
+        }
 
-            fun update(playResult: PlayResult) {
-                when (playResult) {
-                    WIN -> wins += 1
-                    LOSSES -> losses += 1
-                    DRAWS -> draws += 1
-                }
-            }
+        fun build(): List<PlayerResult> {
+            return playerResult
+        }
+    }
 
-            override fun equals(other: Any?): Boolean {
-                if (this === other) return true
-                if (javaClass != other?.javaClass) return false
-
-                other as PlayerResult
-
-                if (player != other.player) return false
-                if (wins != other.wins) return false
-                if (losses != other.losses) return false
-                if (draws != other.draws) return false
-
-                return true
-            }
-
-            override fun hashCode(): Int {
-                var result = player.hashCode()
-                result = 31 * result + wins
-                result = 31 * result + losses
-                result = 31 * result + draws
-                return result
-            }
-
-            override fun toString(): String {
-                return "PlayerResult(player=${player.name}, wins=$wins, losses=$losses, draws=$draws)"
+    data class PlayerResult(
+        private val player: Player,
+        var wins: Int = 0,
+        var losses: Int = 0,
+        var draws: Int = 0
+    ) {
+        fun update(playResult: PlayResult) {
+            when (playResult) {
+                WIN -> wins += 1
+                LOSSES -> losses += 1
+                DRAWS -> draws += 1
             }
         }
     }
+}
+
+infix fun List<Player>.vs(other: Player): List<PlayerResult> {
+    return map { it to (it vs other) }
+        .map { (player, playResult) ->
+            PlayerResult(player)
+                .apply {
+                    update(playResult)
+                }
+        } + (other vs this)
+}
+
+infix fun Player.vs(players: List<Player>): PlayerResult {
+    val thisPlayer = this
+    return PlayerResult(this)
+        .apply {
+            for (player in players) {
+                update(thisPlayer vs player)
+            }
+        }
 }
