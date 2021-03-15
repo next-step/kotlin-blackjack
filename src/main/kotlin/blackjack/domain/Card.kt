62b619ede6e69f4fdc8442f447f4fa0d4a719d22
Joseph <wjsdudtkd95@gmail.com>
2021-03-15 18:26:31 +0900

    refactor(blackjack): Card와 CardDeck 분리

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -24,50 +24,26 @@ class Card private constructor(val value: Pair<String, Pattern>) {
     }
 
     companion object {
-        private const val AVAILABLE_CARD = true
-        private const val DUPLICATED_CARD = false
-        private val NUMBERS =
+        val NUMBERS =
             listOf<String>("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "J", "Q", "K")
-        private val PATTERNS = listOf(
+        val PATTERNS = listOf(
             Pattern.HEART,
             Pattern.DIAMOND,
             Pattern.SPADE,
             Pattern.CLOVER
         )
-        private val HEARTS = NUMBERS.map { number ->
-            Card(number to Pattern.HEART)
-        }
-        private val DIAMONDS = NUMBERS.map { number ->
-            Card(number to Pattern.DIAMOND)
-        }
-        private val SPADES = NUMBERS.map { number ->
-            Card(number to Pattern.SPADE)
-        }
-        private val CLOVERS = NUMBERS.map { number ->
-            Card(number to Pattern.CLOVER)
-        }
-        val CARDS =
-            (HEARTS + DIAMONDS + SPADES + CLOVERS)
-                .associateWith { card -> AVAILABLE_CARD }
-                .toMutableMap()
 
-        fun drawCard(): Card {
-            val randomPattern  = PATTERNS[Random.nextInt(0, PATTERNS.size - 1)]
-            val randomNumber = NUMBERS[Random.nextInt(0, NUMBERS.size - 1)]
-            val card = Card(randomNumber to randomPattern)
-
-            if (!isDuplicateCard(card)) {
-                CARDS[card] = false
-                return card
-            }
-            return drawCard()
+        fun makeCard(number: String, pattern: Pattern): Card {
+            checkValidateNumber(number)
+            checkValidatePattern(pattern)
+            return Card(number to pattern)
         }
 
-        private fun isDuplicateCard(card: Card?): Boolean {
-            if(CARDS[card] == DUPLICATED_CARD) {
-                return true
-            }
-            return false
+        private fun checkValidateNumber(number: String) {
+            if(!NUMBERS.contains(number)) throw RuntimeException("올바르지 않은 카드번호입니다.")
+        }
+        private fun checkValidatePattern(pattern: Pattern) {
+            if(!PATTERNS.contains(pattern)) throw RuntimeException("올바르지 않은 카드무늬입니다.")
         }
     }
 }
