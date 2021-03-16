30959a9826d7c651509ff09d490f921f84cd0340
ka0oll <jjyoung0216@woowahan.com>
2021-03-16 20:20:45 +0900

    refactor step1 반영

--- src/main/kotlin/study/Languages.kt
+++ src/main/kotlin/study/Languages.kt
@@ -5,7 +5,7 @@ class Languages {
     private var _values: MutableMap<String, Int> = mutableMapOf()
 
     val values: Map<String, Int>
-        get() = _values.toMap()
+        get() = _values
 
     infix fun String.level(value: Int) {
         _values[this] = value
--- src/main/kotlin/study/Skills.kt
+++ src/main/kotlin/study/Skills.kt
@@ -4,9 +4,7 @@ class Skills {
     private val _skills: MutableList<Skill> = mutableListOf()
 
     val skills: List<Skill>
-        get() {
-            return _skills.toList()
-        }
+        get() = _skills
 
     fun soft(name: String) {
         this._skills.add(Skill.Soft(name))
