591a912e3228f9dba08c943beec1bd37b17bfe70
남윤서 <devysnam@gmail.com>
2021-03-17 21:40:07 +0900

    feat: 플레이어의 이름을 검증하는 기능 추가

---
+++ src/main/kotlin/blackjack/domain/PlayerName.kt
@@ -0,0 +1,22 @@
+package blackjack.domain
+
+data class PlayerName(private val name: String) {
+    init {
+        validateLength()
+        validateContainBlank()
+    }
+
+    private fun validateLength() {
+        require(name.isNotBlank()) { "이름은 공백이 될 수 없습니다." }
+        require(name.length < MAX_LENGTH) { "이름은 ${MAX_LENGTH}글자 이상이 될 수 없습니다." }
+    }
+
+    private fun validateContainBlank() {
+        require(!name.contains(BLANK)) { "이름엔 공백이 포함될 수 없습니다." }
+    }
+
+    companion object {
+        private const val MAX_LENGTH = 10
+        private const val BLANK = " "
+    }
+}
---
+++ src/test/kotlin/blackjack/domain/PlayerNameTest.kt
@@ -0,0 +1,31 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class PlayerNameTest {
+    @DisplayName("이름 생성")
+    @ParameterizedTest
+    @ValueSource(strings = ["1", "123456789", "name123"])
+    fun constructor(input: String) {
+        assertDoesNotThrow { PlayerName(input) }.also { assertThat(it).isNotNull }
+    }
+
+    @DisplayName("이름이 10글자 이상인 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = ["", "1234567890"])
+    fun validateLength(input: String) {
+        assertThrows<IllegalArgumentException> { PlayerName(input) }
+    }
+
+    @DisplayName("이름에 공백이 포함된 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = [" ", "a b"])
+    fun validateContainBlank(input: String) {
+        assertThrows<IllegalArgumentException> { PlayerName(input) }
+    }
+}
