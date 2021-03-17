0bee1644ff3e32ed67fc7c9be9e09d75cfd7bf65
남윤서 <devysnam@gmail.com>
2021-03-18 01:06:29 +0900

    refactor: 플레이어 생성 예외 케이스 추가

--- src/main/kotlin/blackjack/domain/player/PlayerFactory.kt
+++ src/main/kotlin/blackjack/domain/player/PlayerFactory.kt
@@ -5,7 +5,14 @@ class PlayerFactory {
         private const val DELIMITERS = ","
 
         fun create(input: String): Players {
-            return Players(input.split(DELIMITERS).map { Player(PlayerName(it)) })
+            val names = input.split(DELIMITERS)
+            validateDuplicatedName(names)
+            return Players(names.map { Player(PlayerName(it)) })
+        }
+
+        private fun validateDuplicatedName(names: List<String>) {
+            val toSet = names.toSet()
+            require(names.size == toSet.size) { "플레이어끼리 이름이 중복될 수 없습니다." }
         }
     }
 }
--- src/test/kotlin/blackjack/domain/player/PlayerFactoryTest.kt
+++ src/test/kotlin/blackjack/domain/player/PlayerFactoryTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 
 internal class PlayerFactoryTest {
     @DisplayName("플레이어 생성")
@@ -16,5 +17,12 @@ internal class PlayerFactoryTest {
             { assertThat(users.players.size).isEqualTo(2) },
             { assertThat(users.players.map { it.playerName }).containsExactly(PlayerName("pobi"), PlayerName("jason")) }
         )
+    }
+
+    @DisplayName("중복된 이름의 플레이어가 있는 경우 예외 발생")
+    @Test
+    fun validateDuplicatedName() {
+        val duplicatedInput = "pobi,pobi"
+        assertThrows<IllegalArgumentException> { PlayerFactory.create(duplicatedInput) }
     }
 }
