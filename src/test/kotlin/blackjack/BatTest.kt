d988dc8643ad6ee88e1651a6860a01fcce763f24
kit(김광수) <kit.t@kakaocorp.com>
2021-03-19 17:50:06 +0900

    feat: make it pass

--- src/test/kotlin/blackjack/BatTest.kt
+++ src/test/kotlin/blackjack/BatTest.kt
@@ -1,10 +1,13 @@
 package blackjack
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BatTest {
     @Test
     internal fun `배팅금액이 있다`() {
-        assertThat(Bat(Player("pobi"), 10_000)).isEqualTo(Bat(Player("pobi"), 10_000))
+        assertThat(Bat(CardPlayer.Player("pobi"), 10_000)).isEqualTo(Bat(CardPlayer.Player("pobi"), 10_000))
     }
+
+    data class Bat(private val player: CardPlayer, private val money: Int)
 }
