cf30ee074e33eb085b8f48d85c832aee38cbd365
kit(김광수) <kit.t@kakaocorp.com>
2021-03-19 17:21:10 +0900

    refactor: 딜러 21 초과 테스트 이동

--- src/test/kotlin/blackjack/PlayResultTest.kt
+++ src/test/kotlin/blackjack/PlayResultTest.kt
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test
 
 class PlayResultTest {
     private val pobi = CardPlayer.Player("pobi")
-        .apply {
-            accept(Card("A", Symbol.DIAMONDS))
-            accept(Card("K", Symbol.DIAMONDS))
-        }
     private val jason = CardPlayer.Player("jason")
-        .apply {
-            accept(Card("K", Symbol.DIAMONDS))
-            accept(Card("2", Symbol.DIAMONDS))
-        }
     private val dealer = CardPlayer.Dealer()
-        .apply {
-            accept(Card("K", Symbol.DIAMONDS))
-            accept(Card("K", Symbol.DIAMONDS))
-        }
 
     @Test
     internal fun `승패를 계산한다`() {
+        pobi.apply {
+            accept(Card("A", Symbol.DIAMONDS))
+            accept(Card("K", Symbol.DIAMONDS))
+        }
+        jason.apply {
+            accept(Card("K", Symbol.DIAMONDS))
+            accept(Card("2", Symbol.DIAMONDS))
+        }
+        dealer.apply {
+            accept(Card("K", Symbol.DIAMONDS))
+            accept(Card("K", Symbol.DIAMONDS))
+        }
         val result = Players(dealer, pobi, jason).gameResult()
 
         assertThat(result).contains(
@@ -29,5 +29,24 @@ class PlayResultTest {
             PlayerResult(pobi, wins = 1),
             PlayerResult(jason, losses = 1)
         )
+    }
+
+    @Test
+    fun `딜러가 21을 초과하면 플레이어가 승리한다`() {
+        dealer.apply {
+            accept(Card("K", Symbol.HEARTS))
+            accept(Card("K", Symbol.HEARTS))
+            accept(Card("K", Symbol.HEARTS))
+        }
+
+        val list = Players(dealer, pobi).gameResult()
+        assertThat(list)
+            .hasSize(2)
+            .filteredOnAssertions { assertThat(it).isEqualTo(PlayerResult(pobi, wins = 1)) }
+            .hasSize(1)
+
+        assertThat(list)
+            .filteredOnAssertions { assertThat(it).isEqualTo(PlayerResult(dealer, losses = 1)) }
+            .hasSize(1)
     }
 }
