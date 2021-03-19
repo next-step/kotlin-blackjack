c8e5c0ccdced3525e862b66625b46d768f0af70c
kit(김광수) <kit.t@kakaocorp.com>
2021-03-19 17:55:09 +0900

    test: 승패에 따라 돈이 오간다

--- src/test/kotlin/blackjack/BatTest.kt
+++ src/test/kotlin/blackjack/BatTest.kt
@@ -9,5 +9,14 @@ class BatTest {
         assertThat(Bat(CardPlayer.Player("pobi"), 10_000)).isEqualTo(Bat(CardPlayer.Player("pobi"), 10_000))
     }
 
+    @Test
+    internal fun `승패에 따라 돈이 오간다`() {
+        val pobi = Bat(CardPlayer.Player("pobi"), 10_000)
+        val dealer = Bat(CardPlayer.Dealer())
+        pobi beats dealer
+        assertThat(pobi).isEqualTo(pobi.copy(money = 20_000))
+        assertThat(dealer).isEqualTo(dealer.copy(money = -20_000))
+    }
+
     data class Bat(private val player: CardPlayer, private val money: Int)
 }
