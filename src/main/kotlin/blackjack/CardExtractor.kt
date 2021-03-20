10b9d5d8a2be9180cefafb8e108d6aa9a641c74a
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-20 23:10:30 +0900

    allCards를 static으로 전환

--- src/main/kotlin/blackjack/CardExtractor.kt
+++ src/main/kotlin/blackjack/CardExtractor.kt
@@ -3,14 +3,17 @@ package blackjack
 import java.util.Random
 
 class CardExtractor {
-    private val allCards: MutableList<Card> = CardType.values().flatMap { cardType ->
-        CardNumber.values().map { cardNumber ->
-            Card(cardType, cardNumber)
-        }
-    }.toMutableList()
     private val random = Random()
 
     fun getCard(): Card {
         return allCards.removeAt(random.nextInt(allCards.size))
+    }
+
+    companion object {
+        private val allCards: MutableList<Card> = CardType.values().flatMap { cardType ->
+            CardNumber.values().map { cardNumber ->
+                Card(cardType, cardNumber)
+            }
+        }.toMutableList()
     }
 }
