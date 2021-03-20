1a9e19c444b7779b56e9acd0d1fad9d817377df9
bactoria <bactoria@gmail.com>
2021-03-20 20:54:40 +0900

    test: 테스트 케이스 보강

--- src/test/kotlin/blackjack/domain/DenominationTest.kt
+++ src/test/kotlin/blackjack/domain/DenominationTest.kt
@@ -2,6 +2,10 @@ package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class DenominationTest {
     @Test
@@ -15,25 +19,38 @@ internal class DenominationTest {
         assertThat(result).isEqualTo(Score(9))
     }
 
-    @Test
-    fun `에이스는 기본적으로 11점이다 11 + 2 + 2 = 15`() {
-        val result = Denomination.ACE.calculateScore(
-            listOf(
-                Denomination.TWO,
-                Denomination.TWO
-            )
-        )
-        assertThat(result).isEqualTo(Score(15))
+    @ParameterizedTest
+    @MethodSource("provideDenominationsWithAce")
+    fun `에이스는 기본적으로 11점이다`(denomination: Denomination, others: List<Denomination>, expected: Score) {
+        val result = denomination.calculateScore(others)
+        assertThat(result).isEqualTo(expected)
     }
 
-    @Test
-    fun `점수 결과가 21이 넘는 경우 에이스는 1점으로 계산한다 1 + 2 + 9 = 12`() {
-        val result = Denomination.ACE.calculateScore(
-            listOf(
-                Denomination.TWO,
-                Denomination.NINE
+    @ParameterizedTest
+    @MethodSource("provideDenominationsWithAceOver21")
+    fun `점수 결과가 21이 넘는 경우 에이스는 1점으로 계산한다`(denomination: Denomination, others: List<Denomination>, expected: Score) {
+        val result = denomination.calculateScore(others)
+        assertThat(result).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun provideDenominationsWithAce(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(Denomination.TWO, listOf(Denomination.ACE), Score(13)),
+                Arguments.of(Denomination.TWO, listOf(Denomination.ACE, Denomination.THREE), Score(16)),
+                Arguments.of(Denomination.ACE, listOf(Denomination.TEN), Score(21))
             )
-        )
-        assertThat(result).isEqualTo(Score(12))
+        }
+
+        @JvmStatic
+        private fun provideDenominationsWithAceOver21(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(Denomination.ACE, listOf(Denomination.ACE), Score(12)),
+                Arguments.of(Denomination.TWO, listOf(Denomination.ACE, Denomination.ACE), Score(14)),
+                Arguments.of(Denomination.ACE, listOf(Denomination.ACE, Denomination.ACE, Denomination.ACE), Score(14)),
+                Arguments.of(Denomination.FIVE, listOf(Denomination.SIX, Denomination.ACE), Score(12))
+            )
+        }
     }
 }
