9466e2bc7ee59d4125147eee59a1f2d4c55b9568
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-20 23:27:58 +0900

    enum 각 요소 개행 추가

--- src/main/kotlin/blackjack/CardType.kt
+++ src/main/kotlin/blackjack/CardType.kt
@@ -1,5 +1,8 @@
 package blackjack
 
 enum class CardType(val showName: String) {
-    SPADE("스페이드"), CLOVER("클로버"), HEART("하트"), DIAMOND("다이아")
+    SPADE("스페이드"),
+    CLOVER("클로버"),
+    HEART("하트"),
+    DIAMOND("다이아")
 }
