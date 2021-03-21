196012cc5218077e8faf25faed51cd98338c9698
hyunssooo <jhs695@gmail.com>
2021-03-22 02:41:11 +0900

    feat: Denomination, Suit 추가

---
+++ src/main/kotlin/domain/Denomination.kt
@@ -0,0 +1,17 @@
+package domain
+
+enum class Denomination(private val score: Score) {
+    ACE(Score(11)),
+    TWO(Score(2)),
+    THREE(Score(3)),
+    FOUR(Score(4)),
+    FIVE(Score(5)),
+    SIX(Score(6)),
+    SEVEN(Score(7)),
+    EIGHT(Score(8)),
+    NINE(Score(9)),
+    TEN(Score(10)),
+    JACK(Score(10)),
+    QUEEN(Score(10)),
+    KING(Score(10));
+}
---
+++ src/main/kotlin/domain/Suit.kt
@@ -0,0 +1,5 @@
+package domain
+
+enum class Suit {
+    SPADE, HEART, CLUB, DIAMOND;
+}
