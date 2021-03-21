7f0977faebfc6e918bde799e97e1ac0a9e4f8e46
hyunssooo <jhs695@gmail.com>
2021-03-22 03:12:03 +0900

    feat: card.calculateScore() 추가

--- src/main/kotlin/domain/Card.kt
+++ src/main/kotlin/domain/Card.kt
@@ -1,3 +1,11 @@
 package domain
 
-data class Card(private val suit: Suit, private val denomination: Denomination)
+data class Card(
+    private val suit: Suit,
+    private val denomination: Denomination
+) {
+
+    fun calculateScore(otherScore: Score): Score {
+        return denomination.calculateScore(otherScore)
+    }
+}
---
+++ src/test/kotlin/domain/CardTest.kt
@@ -0,0 +1,24 @@
+package domain
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class CardTest {
+    @Test
+    fun `card로 점수 계산이 가능하다 TWO(2) + 1 = 3`() {
+        val card = Card(Suit.CLUB, Denomination.TWO)
+        Assertions.assertThat(card.calculateScore(Score.of(1))).isEqualTo(Score.of(3))
+    }
+
+    @Test
+    fun `card로 점수 계산이 가능하다 ACE(11) + 10 = 21`() {
+        val card = Card(Suit.CLUB, Denomination.ACE)
+        Assertions.assertThat(card.calculateScore(Score.of(10))).isEqualTo(Score.of(21))
+    }
+
+    @Test
+    fun `card ACE 일 때 합이 21점을 초과 한다면 ACE는 1점으로 취급한다 ACE(1) + 11 = 12`() {
+        val card = Card(Suit.CLUB, Denomination.ACE)
+        Assertions.assertThat(card.calculateScore(Score.of(11))).isEqualTo(Score.of(12))
+    }
+}
