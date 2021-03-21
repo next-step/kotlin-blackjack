727494d47da00b3a7af0f24ee96453e8b6f4a841
SeoJinHyuk14 <suhjh14@gmail.com>
2021-03-22 00:04:28 +0900

    addExecute 함수를 extension으로 추출

--- src/main/kotlin/blackjack/CardNumber.kt
+++ src/main/kotlin/blackjack/CardNumber.kt
@@ -2,18 +2,23 @@ package blackjack
 
 import blackjack.Card.Companion.BLACK_JACK_NUM
 
-enum class CardNumber(val addExecute: (Int) -> Int, val score: Int) {
-    ACE({ num -> if (num + ACE.score > BLACK_JACK_NUM) num + 1 else num + ACE.score }, 11),
-    TWO({ num -> num + TWO.score }, 2),
-    THREE({ num -> num + THREE.score }, 3),
-    FOUR({ num -> num + FOUR.score }, 4),
-    FIVE({ num -> num + FIVE.score }, 5),
-    SIX({ num -> num + SIX.score }, 6),
-    SEVEN({ num -> num + SEVEN.score }, 7),
-    EIGHT({ num -> num + EIGHT.score }, 8),
-    NINE({ num -> num + NINE.score }, 9),
-    TEN({ num -> num + TEN.score }, 10),
-    JACK({ num -> num + JACK.score }, 10),
-    QUEEN({ num -> num + QUEEN.score }, 10),
-    KING({ num -> num + KING.score }, 10)
+enum class CardNumber(val score: Int) {
+    ACE(11),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10)
+}
+
+fun CardNumber.addExecute(_score: Int): Int = when (this) {
+    CardNumber.ACE -> if (_score + score > BLACK_JACK_NUM) _score + 1 else _score + score
+    else -> _score + score
 }
