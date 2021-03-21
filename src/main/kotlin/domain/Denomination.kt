b0d759301d44e6061928cde5919c6e55de2c1346
hyunssooo <jhs695@gmail.com>
2021-03-22 03:08:14 +0900

    feat: Denomination.calculateScore() 추가

--- src/main/kotlin/domain/Denomination.kt
+++ src/main/kotlin/domain/Denomination.kt
@@ -1,17 +1,25 @@
 package domain
 
 enum class Denomination(private val score: Score) {
-    ACE(Score(11)),
-    TWO(Score(2)),
-    THREE(Score(3)),
-    FOUR(Score(4)),
-    FIVE(Score(5)),
-    SIX(Score(6)),
-    SEVEN(Score(7)),
-    EIGHT(Score(8)),
-    NINE(Score(9)),
-    TEN(Score(10)),
-    JACK(Score(10)),
-    QUEEN(Score(10)),
-    KING(Score(10));
+    ACE(Score.of(11)),
+    TWO(Score.of(2)),
+    THREE(Score.of(3)),
+    FOUR(Score.of(4)),
+    FIVE(Score.of(5)),
+    SIX(Score.of(6)),
+    SEVEN(Score.of(7)),
+    EIGHT(Score.of(8)),
+    NINE(Score.of(9)),
+    TEN(Score.of(10)),
+    JACK(Score.of(10)),
+    QUEEN(Score.of(10)),
+    KING(Score.of(10));
+
+    fun calculateScore(otherScore: Score): Score {
+        return otherScore +
+            when (this) {
+                ACE -> if (score + otherScore > Score.of(21)) Score.of(1) else score
+                else -> score
+            }
+    }
 }
---
+++ src/test/kotlin/domain/DenominationTest.kt
@@ -0,0 +1,22 @@
+package domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class DenominationTest {
+
+    @Test
+    fun `denomination 으로 점수 계산이 가능하다 TWO(2) + 1 = 3`() {
+        assertThat(Denomination.TWO.calculateScore(Score.of(1))).isEqualTo(Score.of(3))
+    }
+
+    @Test
+    fun `denomination 으로 점수 계산이 가능하다 ACE(11) + 10 = 21`() {
+        assertThat(Denomination.ACE.calculateScore(Score.of(10))).isEqualTo(Score.of(21))
+    }
+
+    @Test
+    fun `denomination 이 ACE 일 때 합이 21점을 초과 한다면 ACE는 1점으로 취급한다 ACE(1) + 11 = 12`() {
+        assertThat(Denomination.ACE.calculateScore(Score.of(11))).isEqualTo(Score.of(12))
+    }
+}
