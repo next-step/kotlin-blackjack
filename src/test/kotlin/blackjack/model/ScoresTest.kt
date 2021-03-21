7f625e34cb1e0a2a4be9363e0fbbe8f6f82c8510
sw.oh <sangw0804@likelion.org>
2021-03-21 22:38:20 +0900

    test(ScoresTest): 큰점수/낮은점수 반환 기능 테스트 추가

---
+++ src/test/kotlin/blackjack/model/ScoresTest.kt
@@ -0,0 +1,31 @@
+package blackjack.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+internal class ScoresTest {
+    @ParameterizedTest
+    @MethodSource("scoreListProvider")
+    fun `가장 큰 점수와 낮은 점수를 돌려준다`(scores: List<Int>) {
+        val result = Scores.Builder().scoresFromInt(scores).build()
+
+        assertThat(result.size).isEqualTo(scores.size)
+        assertThat(result.highest()).isEqualTo(Score(7))
+        assertThat(result.lowest()).isEqualTo(Score(1))
+    }
+
+    companion object {
+        @JvmStatic
+        fun scoreListProvider(): List<Arguments> {
+            return listOf(
+                Arguments {
+                    arrayOf(
+                        listOf(1, 3, 5, 7)
+                    )
+                }
+            )
+        }
+    }
+}
