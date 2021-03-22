a8097b9e4761cf3e57b1450e16359fb6d388f01a
kit(김광수) <kit.t@kakaocorp.com>
2021-03-22 23:12:12 +0900

    refactor: 라인 분리

--- src/main/kotlin/blackjack/domain/DealerAdjustment.kt
+++ src/main/kotlin/blackjack/domain/DealerAdjustment.kt
@@ -2,7 +2,8 @@ package blackjack.domain
 
 class DealerAdjustment(private val playerResult: List<PlayerResult>, private val bet: List<Bet>) {
     fun income(): Int {
-        return playerResult.sortedBy { it.name }.zip(bet.sortedBy { it.name })
+        return playerResult.sortedBy { it.name }
+            .zip(bet.sortedBy { it.name })
             .onEach { require(it.first.name == it.second.name) }
             .map { (pr, bet) -> pr.income(bet) }
             .sumBy { -it }
