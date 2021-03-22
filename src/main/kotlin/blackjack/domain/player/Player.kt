89fe5926fadffa7e2a523efd6bb506c8dd9e1d47
hyunssooo <jhs695@gmail.com>
2021-03-22 20:04:59 +0900

    feat: Player.take() 추가

--- src/main/kotlin/blackjack/domain/player/Player.kt
+++ src/main/kotlin/blackjack/domain/player/Player.kt
@@ -1,8 +1,16 @@
 package blackjack.domain.player
 
+import blackjack.domain.Card
 import blackjack.domain.Cards
+import blackjack.domain.Score
 
 class Player(val name: Name, val cards: Cards) {
+    val score: Score
+        get() = cards.score
+
+    fun take(card: Card) {
+        cards.add(card)
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
