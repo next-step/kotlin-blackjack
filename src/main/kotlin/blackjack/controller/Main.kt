165b6d3a9cad9040984a3fe42f079f7746e0a9e8
sw.oh <sangw0804@likelion.org>
2021-03-24 01:28:15 +0900

    refactor(view, controller): View 에 과도하게 집중된 책임을 Controller 로 이동

--- src/main/kotlin/blackjack/controller/Main.kt
+++ src/main/kotlin/blackjack/controller/Main.kt
@@ -1,16 +1,29 @@
 package blackjack.controller
 
+import blackjack.model.Player
+import blackjack.model.Players
 import blackjack.view.InputView
 import blackjack.view.OutputView
+import blackjack.view.ViewUtil
 
 fun main() {
-    val players = InputView.readPlayers()
+    val players = Players.Builder().playerNames(InputView.readNames()).build()
 
     OutputView.printFirstDraw(players)
 
     players.forEach {
-        InputView.drawCards(it)
+        drawUntilUserStop(it)
     }
 
     OutputView.printResult(players)
+}
+
+private fun drawUntilUserStop(player: Player) {
+    while (true) {
+        if (InputView.readUserResponse(player.name) != "y") {
+            break
+        }
+        player.draw()
+        println("${player.name}카드: ${ViewUtil.toString(player.cards)}")
+    }
 }
--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,28 +1,19 @@
 package blackjack.view
 
-import blackjack.model.Player
-import blackjack.model.Players
-import blackjack.view.ViewUtil.toString
-
 object InputView {
-    fun readPlayers(): Players {
+    fun readNames(): List<String> {
         var input: String? = null
         while (input.isNullOrBlank()) {
             println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
             input = readLine()
         }
 
-        return Players.Builder().playerNames(input.split(",")).build()
+        return input.split(",")
     }
 
-    fun drawCards(player: Player) {
-        while (true) {
-            println("${player.name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-            if (readLine()?.trim()?.toLowerCase() != "y") {
-                break
-            }
-            player.draw()
-            println("${player.name}카드: ${toString(player.cards)}")
-        }
+    fun readUserResponse(playerName: String): String? {
+        println("${playerName}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+
+        return readLine()?.trim()?.toLowerCase()
     }
 }
