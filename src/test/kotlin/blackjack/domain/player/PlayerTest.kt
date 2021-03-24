29bc0264cd96eaad42a6968fbe9922ef11c67def
bactoria <bactoria@gmail.com>
2021-03-25 01:11:35 +0900

    test: Player가 카드 더 받을 수 있는지 여부
    리뷰: https://github.com/next-step/kotlin-blackjack/pull/82\#discussion_r598114787

--- src/test/kotlin/blackjack/domain/player/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/player/PlayerTest.kt
@@ -8,6 +8,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class PlayerTest {
 
@@ -31,5 +35,55 @@ internal class PlayerTest {
     @Test
     fun `이름이 같을 경우 같은 플레이어로 취급한다`() {
         assertThat(Player("자손", cards)).isEqualTo(Player("자손", cards))
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCards")
+    fun `플레이어 카드의 점수가 블랙잭(21점)보다 높거나 같은 경우 카드를 더이상 가질 수 없다`(cards: Cards, expected: Boolean) {
+        val player = Player("자손", cards)
+        val result = player.isNotTakeable()
+        assertThat(result).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun provideCards(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    makeCards(
+                        Card(Suit.HEART, Denomination.JACK),
+                        Card(Suit.DIAMOND, Denomination.JACK)
+                    ),
+                    false
+                ),
+                Arguments.of(
+                    makeCards(
+                        Card(Suit.HEART, Denomination.ACE),
+                        Card(Suit.HEART, Denomination.JACK),
+                        Card(Suit.DIAMOND, Denomination.JACK)
+                    ),
+                    true
+                ),
+                Arguments.of(
+                    makeCards(
+                        Card(Suit.HEART, Denomination.TWO),
+                        Card(Suit.HEART, Denomination.JACK),
+                        Card(Suit.DIAMOND, Denomination.JACK)
+                    ),
+                    true
+                )
+            )
+        }
+
+        private fun makeCards(vararg cards: Card): Cards {
+            val cardInitSize = 2
+
+            val initCards = cards.take(cardInitSize)
+            val restCards = cards.drop(cardInitSize)
+
+            return Cards(*initCards.toTypedArray()).apply {
+                restCards.forEach(this::add)
+            }
+        }
     }
 }
