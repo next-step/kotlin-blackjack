8d42d705eea9bddd3311cc7eb630c9c8b50ea1a4
sanggu <tkdrn8578@naver.com>
2021-03-25 23:57:44 +0900

    test: 숫자를 가진 카드넘버는 그 숫자와 일치해야 한다 에 대한 테스트 코드 추가

--- src/test/kotlin/blackjack/CardNumberTests.kt
+++ src/test/kotlin/blackjack/CardNumberTests.kt
@@ -2,6 +2,7 @@ package blackjack
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class CardNumberTests {
@@ -9,5 +10,11 @@ class CardNumberTests {
     @ValueSource(strings = ["JACK", "QUEEN", "KING"])
     fun `King, Queen, Jack 은 10을 의미한다`(cardNumber: CardNumber) {
         assertThat(cardNumber.value).isEqualTo(10)
+    }
+
+    @ParameterizedTest
+    @CsvSource("NUMBER_2, 2", "NUMBER_3, 3", "NUMBER_4, 4", "NUMBER_5, 5", "NUMBER_6, 6", "NUMBER_7, 7", "NUMBER_8, 8", "NUMBER_9, 9", "NUMBER_10, 10")
+    fun `숫자를 가진 카드넘버는 그 숫자와 일치해야 한다`(cardNumber: CardNumber, exceptedValue: Int) {
+        assertThat(cardNumber.value).isEqualTo(exceptedValue)
     }
 }
