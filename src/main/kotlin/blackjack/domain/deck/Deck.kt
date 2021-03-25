f1c631d856ee1f9e1749d46e6f75cdcfa34f9bea
hyunssooo <jhs695@gmail.com>
2021-03-25 21:45:08 +0900

    refactor: 피드백 반영

--- src/main/kotlin/blackjack/domain/deck/Deck.kt
+++ src/main/kotlin/blackjack/domain/deck/Deck.kt
@@ -3,10 +3,10 @@ package blackjack.domain.deck
 import blackjack.domain.Card
 
 class Deck(elements: List<Card>) {
-    private val _elements = elements.toMutableList()
+    private val elements = elements.toMutableList()
 
     fun draw(): Card {
-        require(_elements.isNotEmpty())
-        return _elements.removeAt(0)
+        require(elements.isNotEmpty())
+        return elements.removeAt(0)
     }
 }
