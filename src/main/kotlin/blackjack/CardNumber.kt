ffd34f6eaa3af63beb13904e9a83c2a2442d9a02
sanggu <tkdrn8578@naver.com>
2021-03-25 23:58:10 +0900

    feat: NUMBER 2 ~ 10까지의 숫자를 가지는 CardNumbers 추가

--- src/main/kotlin/blackjack/CardNumber.kt
+++ src/main/kotlin/blackjack/CardNumber.kt
@@ -1,6 +1,15 @@
 package blackjack
 
 enum class CardNumber(val value: Int) {
+    NUMBER_2(2),
+    NUMBER_3(3),
+    NUMBER_4(4),
+    NUMBER_5(5),
+    NUMBER_6(6),
+    NUMBER_7(7),
+    NUMBER_8(8),
+    NUMBER_9(9),
+    NUMBER_10(10),
     JACK(10),
     QUEEN(10),
     KING(10)
