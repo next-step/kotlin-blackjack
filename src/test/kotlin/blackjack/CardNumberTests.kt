1a6fe1488e5a9de1a8d9b115d10630104e8d176a
sanggu <tkdrn8578@naver.com>
2021-03-26 00:16:13 +0900

    refactor: CardNumber 가변인자로 받음에 따른 테스트 코드 수정

--- src/test/kotlin/blackjack/CardNumberTests.kt
+++ src/test/kotlin/blackjack/CardNumberTests.kt
@@ -1,6 +1,7 @@
 package blackjack
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
@@ -16,5 +17,10 @@ class CardNumberTests {
     @CsvSource("NUMBER_2, 2", "NUMBER_3, 3", "NUMBER_4, 4", "NUMBER_5, 5", "NUMBER_6, 6", "NUMBER_7, 7", "NUMBER_8, 8", "NUMBER_9, 9", "NUMBER_10, 10")
     fun `숫자를 가진 카드넘버는 그 숫자와 일치해야 한다`(cardNumber: CardNumber, exceptedValue: Int) {
         assertThat(cardNumber.value).contains(exceptedValue)
+    }
+
+    @Test
+    fun `Ace는 1 또는 11이다`() {
+        assertThat(CardNumber.ACE.value).containsExactlyInAnyOrder(1, 11)
     }
 }
