0c0a927d1f4c6d3a49aed2d40465cea653fea05b
bactoria <bactoria@gmail.com>
2021-03-27 20:10:51 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/kotlin/blackjack/domain/CardsFactory.kt
+++
@@ -1,9 +0,0 @@
-package blackjack.domain
-
-object CardsFactory {
-
-    fun makeCards(cards: Pair<Card, Card>): Cards {
-        require(cards.first != cards.second) { "동일한 카드로는 생성할 수 없습니다. cards.first: ${cards.first}, cards.second: ${cards.second}" }
-        return Cards(listOf(cards.first, cards.second))
-    }
-}
