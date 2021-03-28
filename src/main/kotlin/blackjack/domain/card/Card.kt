613e55e0eeb1233d59885cfde881babf0fff693e
Joseph <wjsdudtkd95@gmail.com>
2021-03-29 00:41:59 +0900

    feat(blackjack): 카드덱 구현

--- src/main/kotlin/blackjack/domain/card/Card.kt
+++ src/main/kotlin/blackjack/domain/card/Card.kt
@@ -1,32 +1,17 @@
 package blackjack.domain.card
 
-import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
+import java.util.*
+import kotlin.IllegalArgumentException
+import kotlin.collections.HashMap
 
-class Card private constructor(private val suit: Suit, private val denomination: Denomination) {
+class Card(val suit: Suit,  val denomination: Denomination) {
     val isAce: Boolean
         get() = denomination.isAce
 
     val score: Int
         get() = denomination.score
 
-    companion object {
-        private val CARDS: MutableMap<String, Card> = HashMap()
-
-        fun of(suit: Suit, denomination: Denomination): Card {
-            return CARDS[toKey(suit, denomination)] ?: throw IllegalArgumentException("해당 카드는 존재할 수 없습니다.")
-        }
-
-        private fun toKey(suit: Suit, denomination: Denomination): String {
-            return suit.name + denomination.name
-        }
-
-        init {
-            for(suit in Suit.values()) {
-                for(denomination in Denomination.values()) {
-                    CARDS[toKey(suit, denomination)] = Card(suit, denomination)
-                }
-            }
-        }
-    }
+    val displayName: String
+        get() = suit.name + denomination.name
 }
--- src/test/kotlin/blackjack/domain/Fixtures.kt
+++ src/test/kotlin/blackjack/domain/Fixtures.kt
@@ -1,10 +1,11 @@
 package blackjack.domain
 
 import blackjack.domain.card.Card
+import blackjack.domain.card.CardDeck
 import blackjack.domain.card.Denomination
 import blackjack.domain.card.Suit
 
-val CLUB_ACE = Card.of(Suit.CLUBS, Denomination.ACE)
-val CLUB_TWO = Card.of(Suit.CLUBS, Denomination.TWO)
-val CLUB_TEN = Card.of(Suit.CLUBS, Denomination.TEN)
-val CLUB_KING = Card.of(Suit.CLUBS, Denomination.KING)
+val CLUB_ACE = Card(Suit.CLUBS, Denomination.ACE)
+val CLUB_TWO = Card(Suit.CLUBS, Denomination.TWO)
+val CLUB_TEN = Card(Suit.CLUBS, Denomination.TEN)
+val CLUB_KING = Card(Suit.CLUBS, Denomination.KING)
