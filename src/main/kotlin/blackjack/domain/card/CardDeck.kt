94c2d30760018670e415e1a155f98c93675553be
Joseph <wjsdudtkd95@gmail.com>
2021-03-29 00:42:18 +0900

    feat(blackjack): 카드덱 구현

---
+++ src/main/kotlin/blackjack/domain/card/CardDeck.kt
@@ -0,0 +1,26 @@
+package blackjack.domain.card
+
+import java.util.*
+
+object CardDeck {
+
+    private val CARDS: LinkedList<Card> = LinkedList<Card>()
+
+    fun drawCard():Card {
+        return CARDS.pop()
+    }
+
+    fun of(suit: Suit, denomination: Denomination): Card {
+        return CARDS.find { it.suit == suit && it.denomination == denomination }
+            ?: throw IllegalArgumentException("해당 카드를 찾을 수 없습니다.")
+    }
+
+    init {
+        for (suit in Suit.values()) {
+            for (denomination in Denomination.values()) {
+                CARDS.push(Card(suit, denomination))
+            }
+        }
+        CARDS.shuffle()
+    }
+}
