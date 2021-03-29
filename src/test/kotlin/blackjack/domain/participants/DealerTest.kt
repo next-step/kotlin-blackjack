b1e48b70387e374a865d06122ebe8fd46e3ea273
Joseph <wjsdudtkd95@gmail.com>
2021-03-29 22:58:32 +0900

    test(blackjack): 딜러 테스트

---
+++ src/test/kotlin/blackjack/domain/participants/DealerTest.kt
@@ -0,0 +1,28 @@
+package blackjack.domain.participants
+
+import blackjack.domain.CLUB_ACE
+import blackjack.domain.CLUB_KING
+import blackjack.domain.CLUB_TWO
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DealerTest {
+    @Test
+    fun `17이상이면 카드를 드로우 하지 않는다`() {
+        val dealer = Dealer(initCards = arrayListOf(CLUB_KING, CLUB_KING))
+        val beforeDrawScore = dealer.getScore()
+        dealer.drawCard()
+        val afterDrawScore = dealer.getScore()
+        assertThat(beforeDrawScore).isEqualTo(afterDrawScore)
+    }
+
+    @Test
+    fun `16이하이면 카드를 드로우 하지 않는다`() {
+        val dealer = Dealer(initCards = arrayListOf(CLUB_TWO, CLUB_KING))
+        val beforeDrawScore = dealer.getScore()
+        dealer.drawCard()
+        val afterDrawScore = dealer.getScore()
+        Assertions.assertThat(beforeDrawScore).isNotEqualTo(afterDrawScore)
+    }
+}
