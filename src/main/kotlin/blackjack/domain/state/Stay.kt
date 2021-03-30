1fbb6f959d8bedbd83912b4fdd74ce0085c22f19
Joseph <wjsdudtkd95@gmail.com>
2021-03-31 01:07:42 +0900

    fix(blackjack): 딜러가 Bust인 경우 승리하도록 수정

--- src/main/kotlin/blackjack/domain/state/Stay.kt
+++ src/main/kotlin/blackjack/domain/state/Stay.kt
@@ -6,6 +6,7 @@ import blackjack.domain.winning.GameResult
 
 class Stay : Finished() {
     override fun isWinning(player: Player, dealer: Dealer): GameResult {
+        if(dealer.state is Bust) return GameResult.WIN
         if (player.getScore() < dealer.getScore()) return GameResult.LOSE
         if (player.getScore() > dealer.getScore()) return GameResult.WIN
         return GameResult.DRAW
