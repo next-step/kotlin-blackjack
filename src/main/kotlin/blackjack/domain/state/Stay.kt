87d43c97869b02ec452a69c2e417450e390a0b85
Joseph <wjsdudtkd95@gmail.com>
2021-03-31 00:56:45 +0900

    fix(blackjack): 승부에서 잘못된 내용 개선

--- src/main/kotlin/blackjack/domain/state/Stay.kt
+++ src/main/kotlin/blackjack/domain/state/Stay.kt
@@ -6,8 +6,8 @@ import blackjack.domain.winning.GameResult
 
 class Stay : Finished() {
     override fun isWinning(player: Player, dealer: Dealer): GameResult {
-        if (player.getScore() < dealer.getScore()) return GameResult.WIN
-        if (player.getScore() > dealer.getScore()) return GameResult.LOSE
+        if (player.getScore() < dealer.getScore()) return GameResult.LOSE
+        if (player.getScore() > dealer.getScore()) return GameResult.WIN
         return GameResult.DRAW
     }
 
