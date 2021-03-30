d7982bbf181aa584ec546ec2578e76021d74ea0f
sanggu <tkdrn8578@naver.com>
2021-03-30 23:58:40 +0900

    refactor: 매직넘버에 대한 추출 작업

--- src/test/kotlin/blackjack/GameTest.kt
+++ src/test/kotlin/blackjack/GameTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class GameTest {
     private val HIT = 1
+    private val BLACK_JACK = 3
 
     @Test
     fun `블랙잭은 처음에 카드를 2장을 가져가면 현재 상태를 리턴한다`() {
@@ -30,5 +31,13 @@ class GameTest {
         val state = game.state
 
         assertThat(state).isEqualTo(HIT)
+    }
+
+    @Test
+    fun `처음 받은 2개의 두 카드의 숫자 합이 21이면 Blackjack 이다`() {
+        val game = Game(Card(CardSuite.HEART, CardNumber.ACE), Card(CardSuite.SPADE, CardNumber.JACK))
+        val state = game.state
+
+        assertThat(state).isEqualTo(BLACK_JACK)
     }
 }
