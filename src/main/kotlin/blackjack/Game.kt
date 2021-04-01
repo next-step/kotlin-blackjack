2d9bf5576e3e7cc5e6a09c13d3f63163277bf591
sanggu <tkdrn8578@naver.com>
2021-04-01 23:15:58 +0900

    refactor: List<Card> 를 일급컬렉션인 Cards 로 추출

--- src/main/kotlin/blackjack/Game.kt
+++ src/main/kotlin/blackjack/Game.kt
@@ -1,6 +1,6 @@
 package blackjack
 
-data class Game(private val cards: MutableList<Card>) {
+data class Game(private var cards: Cards) {
     val state: States
         get() {
             var score = cards.sumBy { it.number.value }
@@ -18,19 +18,11 @@ data class Game(private val cards: MutableList<Card>) {
             return States.BUST
         }
 
-    private fun calculateAce(card: Card, score: Int): Int {
-        if (card.number == CardNumber.ACE && score + SUBTRACT_FIRST_AND_SECONDARY_ACE_SCORE <= BLACK_JACK_SCORE) {
-            return SUBTRACT_FIRST_AND_SECONDARY_ACE_SCORE + score
-        }
-
-        return score
-    }
-
-    constructor(firstCard: Card, secondCard: Card) : this(mutableListOf(firstCard, secondCard))
-
     fun draw(card: Card) {
-        cards.add(card)
+        cards = Cards(cards + card)
     }
+
+    constructor(firstCard: Card, secondCard: Card) : this(Cards(listOf(firstCard, secondCard)))
 
     companion object {
         private const val BLACK_JACK_SCORE = 21
