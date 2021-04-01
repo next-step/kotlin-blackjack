0ab783580d2825401ecaede43b9a26f0c3a282d5
Joseph <wjsdudtkd95@gmail.com>
2021-04-02 00:17:03 +0900

    refactor(blackjack): 객체지향 생활체조 원칙에 의한 수정

--- src/main/kotlin/blackjack/domain/winning/BettingResult.kt
+++ src/main/kotlin/blackjack/domain/winning/BettingResult.kt
@@ -5,7 +5,8 @@ import blackjack.domain.participants.Player
 class BettingResult(val results: Map<Player, Double>) {
 
     fun getDealerProfit(): Int {
-        val playersLoss = results.values.filter { it < 0 }.sum() * -1
+        val money = results.values
+        val playersLoss = money.filter { it < 0 }.sum() * -1
         if (playersLoss == 0.0) playersLoss * -1
         return playersLoss.toInt()
     }
