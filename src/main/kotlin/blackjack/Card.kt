b0a50f00aa17cb78ed0703ac930afa41f24c2441
sanggu <tkdrn8578@naver.com>
2021-04-04 23:53:51 +0900

    feat: 52장의 카드를 미리 생성해놓기 위한 캐시된 CARDS 추가

--- src/main/kotlin/blackjack/Card.kt
+++ src/main/kotlin/blackjack/Card.kt
@@ -1,3 +1,19 @@
 package blackjack
 
-data class Card(val suite: CardSuite, val number: CardNumber)
+data class Card(val suite: CardSuite, val number: CardNumber) {
+
+    companion object {
+        val CARDS: Map<Pair<CardSuite, CardNumber>, Card> = CardNumber.values()
+            .flatMap { number -> generateCards(number) }
+            .associateWith { Card(it.first, it.second) }
+
+        val ALL_CARDS
+            get() = CARDS.values.toSet()
+
+        private fun generateCards(number: CardNumber) = CardSuite.values().map { suite -> Pair(suite, number) }
+
+        fun from(suite: CardSuite, number: CardNumber): Card {
+            return CARDS[Pair(suite, number)] ?: throw IllegalArgumentException("존재하지 않는 카드입니다.")
+        }
+    }
+}
