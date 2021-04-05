19ac9bcb963e946f945e6554629d5b2e899af229
sanggu <tkdrn8578@naver.com>
2021-04-05 23:16:04 +0900

    feat: 게임에서는 카드를 한장을 뽑아서 플레이어에게 줄 수 있는 경우와 아닌 경우에 대한 테스트 코드 및 기능 추가

--- src/main/kotlin/blackjack/Game.kt
+++ src/main/kotlin/blackjack/Game.kt
@@ -10,6 +10,12 @@ data class Game(private val players: Players, private var cards: GameCards = Gam
             return GameStates.PLAYING
         }
 
+    fun draw(target: Player) {
+        target.throwExceptionIfIsNotPlayingState()
+
+        val card = cards.poll()
+        target.draw(card)
+    }
     private fun isEndState() = players.countOfPlayingState == NO_PLAYING_COUNT
 
     companion object {
--- src/test/kotlin/blackjack/PlayerCardsTest.kt
+++ src/test/kotlin/blackjack/PlayerCardsTest.kt
@@ -2,11 +2,19 @@ package blackjack
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class PlayerCardsTest {
     @Test
     fun `카드를 맨 앞의 카드 한장을 뽑아서 리턴 해야 한다`() {
-        val cards = GameCards()
+        val cards = GameCards(
+            LinkedList(
+                mutableListOf(
+                    Card(CardSuite.DIAMOND, CardNumber.ACE),
+                    Card(CardSuite.DIAMOND, CardNumber.TWO)
+                )
+            )
+        )
 
         val card = cards.poll()
 
