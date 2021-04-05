cf8fe03bc154973e5ab8b6fcc24fe80888bf9a2e
sanggu <tkdrn8578@naver.com>
2021-04-05 23:49:03 +0900

    refactor: 카드를 뽑을 수 없는 상태일 때에 대한 예외처리 추가

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -1,9 +1,12 @@
 package blackjack
 
-class Player(private var playerCards: PlayerCards) {
+class Player(val name: String, cards: PlayerCards) {
+    var cards: PlayerCards = cards
+        private set
+
     var state: States = States.HIT
         get() {
-            return States.valueOf(playerCards.score, field)
+            return States.valueOf(cards.score, field)
         }
 
     val isPlayingState: Boolean
@@ -12,11 +15,9 @@ class Player(private var playerCards: PlayerCards) {
         }
 
     fun draw(card: Card) {
-        if (state == States.STAY) {
-            throw IllegalStateException("STAY이므로 draw 할 수 없습니다.")
-        }
+        throwExceptionIfIsNotPlayingState()
 
-        playerCards = playerCards.addCard(card)
+        cards = cards.addCard(card)
     }
 
     fun stop() {
