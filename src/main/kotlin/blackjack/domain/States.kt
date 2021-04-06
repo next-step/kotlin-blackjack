4a1a8b96fd0513ef3c6d4682edc49ec5068ba825
sanggu <tkdrn8578@naver.com>
2021-04-06 22:56:08 +0900

    refactor: States 가 score 에 따라 상태를 리턴하는게 맞지 않늗나고 생각하여 Player로 추출

--- src/main/kotlin/blackjack/domain/States.kt
+++ src/main/kotlin/blackjack/domain/States.kt
@@ -1,30 +1,8 @@
 package blackjack.domain
 
-enum class States(val state: Int) {
-    HIT(1),
-    BUST(2),
-    BLACK_JACK(3),
-    STAY(4);
-
-    companion object {
-        fun valueOf(score: Int, sizeOfCards: Int, state: States): States {
-            if (state == STAY) {
-                return STAY
-            }
-
-            return valueOf(score, sizeOfCards)
-        }
-
-        private fun valueOf(score: Int, sizeOfCards: Int): States {
-            if (score < Game.BLACK_JACK_SCORE) {
-                return HIT
-            } else if (score > Game.BLACK_JACK_SCORE) {
-                return BUST
-            } else if (score == Game.BLACK_JACK_SCORE && sizeOfCards == Game.BLACK_JACK_CARD_COUNT) {
-                return BLACK_JACK
-            }
-
-            return STAY
-        }
-    }
+enum class States {
+    HIT,
+    BUST,
+    BLACK_JACK,
+    STAY;
 }
