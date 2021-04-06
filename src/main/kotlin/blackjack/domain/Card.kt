a6d69895ab24fecfd74d926ddad56d2f09b719ae
sanggu <tkdrn8578@naver.com>
2021-04-06 22:54:49 +0900

    refactor: 사용하지 않는 Map으로된 CARDS 제거

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -3,17 +3,10 @@ package blackjack.domain
 data class Card(val suite: CardSuite, val number: CardNumber) {
 
     companion object {
-        val CARDS: Map<Pair<CardSuite, CardNumber>, Card> = CardNumber.values()
+        val CARDS = CardNumber.values()
             .flatMap { number -> generateCards(number) }
-            .associateWith { Card(it.first, it.second) }
+            .toSet()
 
-        val ALL_CARDS
-            get() = CARDS.values.toSet()
-
-        private fun generateCards(number: CardNumber) = CardSuite.values().map { suite -> Pair(suite, number) }
-
-        fun from(suite: CardSuite, number: CardNumber): Card {
-            return CARDS[Pair(suite, number)] ?: throw IllegalArgumentException("존재하지 않는 카드입니다.")
-        }
+        private fun generateCards(number: CardNumber) = CardSuite.values().map { suite -> Card(suite, number) }
     }
 }
