b5b347ba8a16e2f425475b572fa037f94ba90f43
sanggu <tkdrn8578@naver.com>
2021-04-06 22:56:08 +0900

    refactor: States 가 score 에 따라 상태를 리턴하는게 맞지 않늗나고 생각하여 Player로 추출

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -6,13 +6,39 @@ class Player(val name: String, cards: PlayerCards) {
 
     var state: States = States.HIT
         get() {
-            return States.valueOf(cards.score, cards.size, field)
+            return valueOf(cards.score, field)
         }
 
-    val isPlayingState: Boolean
+    val isPlaying: Boolean
         get() {
             return state == States.HIT
         }
+
+    init {
+        if (cards.score == Game.BLACK_JACK_SCORE) {
+            state = States.BLACK_JACK
+        }
+    }
+
+    constructor(name: String, firstCard: Card, secondCard: Card) : this(name, PlayerCards(firstCard, secondCard))
+
+    private fun valueOf(score: Int, state: States): States {
+        if (state == States.STAY || state == States.BLACK_JACK) {
+            return state
+        }
+
+        return valueOf(score)
+    }
+
+    private fun valueOf(score: Int): States {
+        if (score < Game.BLACK_JACK_SCORE) {
+            return States.HIT
+        } else if (score > Game.BLACK_JACK_SCORE) {
+            return States.BUST
+        }
+
+        return States.STAY
+    }
 
     fun draw(card: Card) {
         throwExceptionIfIsNotPlayingState()
@@ -25,10 +51,8 @@ class Player(val name: String, cards: PlayerCards) {
     }
 
     fun throwExceptionIfIsNotPlayingState() {
-        if (isPlayingState.not()) {
+        if (isPlaying.not()) {
             throw IllegalStateException("게임이 진행 불가능한 상태입니다. : $state")
         }
     }
-
-    constructor(name: String, firstCard: Card, secondCard: Card) : this(name, PlayerCards(firstCard, secondCard))
 }
