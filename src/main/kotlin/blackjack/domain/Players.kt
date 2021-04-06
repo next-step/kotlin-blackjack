7ef4ddb809900771344db189857af84eec0d906c
sanggu <tkdrn8578@naver.com>
2021-04-06 22:54:26 +0900

    refactor: isPlayingState 를 isPlaying 으로 네미밍 수정

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -2,5 +2,5 @@ package blackjack.domain
 
 class Players(private val players: Set<Player>) : Set<Player> by players {
     val countOfPlayingState: Int
-        get() = players.count { it.isPlayingState }
+        get() = players.count { it.isPlaying }
 }
