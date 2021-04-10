6abb2eef52917529e81f03b88931fd3feef95a71
Nam Dongmin <nam.dongmin@htbeyond.com>
2021-04-11 00:22:34 +0900

    feat(blackjack): 플에이어는 덱에서 카드를 뽑아 손패에 추가할 수 있다

--- src/test/kotlin/blackjack/player/PlayerTest.kt
+++ src/test/kotlin/blackjack/player/PlayerTest.kt
@@ -1,8 +1,14 @@
 package blackjack.player
 
+import blackjack.playingcard.Card
+import blackjack.playingcard.Deck
+import blackjack.playingcard.Suit
+import blackjack.playingcard.Symbol
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class PlayerTest {
     @Test
@@ -33,5 +39,24 @@ internal class PlayerTest {
 
         // then
         assertThat(hand.cards.toList()).isEmpty()
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "SPADES, ACE",
+        "DIAMONDS, SEVEN"
+    )
+    internal fun `플레이어는 덱에서 카드를 뽑아 손패에 추가할 수 있다`(suit: Suit, symbol: Symbol) {
+        // given
+        val deck = Deck(
+            listOf(Card.of(suit, symbol))
+        )
+        val player = Player(Name("Russ Hamilton"))
+
+        // when
+        player.drawOneFrom(deck)
+
+        // then
+        assertThat(player.hand.cards.toList()).containsExactly(Card.of(suit, symbol))
     }
 }
