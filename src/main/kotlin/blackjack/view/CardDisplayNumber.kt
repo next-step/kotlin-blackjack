d5e1ab86feb16c79142664f1b44c66484a5cd450
sanggu <tkdrn8578@naver.com>
2021-04-29 21:42:57 +0900

    feat: CardNumber로 DisplayName을 가져오는 함수 추가

--- src/main/kotlin/blackjack/view/CardDisplayNumber.kt
+++ src/main/kotlin/blackjack/view/CardDisplayNumber.kt
@@ -1,19 +1,38 @@
 package blackjack.view
 
 import blackjack.domain.CardNumber
+import blackjack.domain.CardNumber.ACE
+import blackjack.domain.CardNumber.EIGHT
+import blackjack.domain.CardNumber.FIVE
+import blackjack.domain.CardNumber.FOUR
+import blackjack.domain.CardNumber.JACK
+import blackjack.domain.CardNumber.KING
+import blackjack.domain.CardNumber.NINE
+import blackjack.domain.CardNumber.QUEEN
+import blackjack.domain.CardNumber.SEVEN
+import blackjack.domain.CardNumber.SIX
+import blackjack.domain.CardNumber.TEN
+import blackjack.domain.CardNumber.THREE
+import blackjack.domain.CardNumber.TWO
 
 enum class CardDisplayNumber(val value: CardNumber, val displayName: String) {
-    TWO(CardNumber.TWO, "2"),
-    THREE(CardNumber.THREE, "3"),
-    FOUR(CardNumber.FOUR, "4"),
-    FIVE(CardNumber.FIVE, "5"),
-    SIX(CardNumber.SIX, "6"),
-    SEVEN(CardNumber.SEVEN, "7"),
-    EIGHT(CardNumber.EIGHT, "8"),
-    NINE(CardNumber.NINE, "9"),
-    TEN(CardNumber.TEN, "10"),
-    JACK(CardNumber.JACK, "J"),
-    QUEEN(CardNumber.QUEEN, "Q"),
-    KING(CardNumber.KING, "K"),
-    ACE(CardNumber.ACE, "A")
+    TWO_VIEW(TWO, "2"),
+    THREE_VIEW(THREE, "3"),
+    FOUR_VIEW(FOUR, "4"),
+    FIVE_VIEW(FIVE, "5"),
+    SIX_VIEW(SIX, "6"),
+    SEVEN_VIEW(SEVEN, "7"),
+    EIGHT_VIEW(EIGHT, "8"),
+    NINE_VIEW(NINE, "9"),
+    TEN_VIEW(TEN, "10"),
+    JACK_VIEW(JACK, "J"),
+    QUEEN_VIEW(QUEEN, "Q"),
+    KING_VIEW(KING, "K"),
+    ACE_VIEW(ACE, "A");
+
+    companion object {
+        fun retrieveDisplayName(value: CardNumber): String {
+            return values().first { it.value == value }.displayName
+        }
+    }
 }
