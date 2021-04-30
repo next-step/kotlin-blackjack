3f8d15ecd25a80a3223e1ebd69d62e1627c77ad1
sanggu <tkdrn8578@naver.com>
2021-04-30 09:25:48 +0900

    refactor: Participant라는 추상 클래스 상속에 따른 중복 메서드 제거

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,37 +1,16 @@
 package blackjack.domain
 
-class Player(val name: String, cards: PlayerCards) {
-    var cards: PlayerCards = cards
-        private set
+class Player : Participant {
 
-    var state: States = States.HIT
+    override val isEnd: Boolean
         get() {
-            if (name == "dealer" && cards.score > 21) {
-                return States.WIN
-            }
-
-            if (field == States.STAY || field == States.BLACK_JACK) {
-                return field
-            }
-
-            return findStateByScore(cards.score)
+            return state == States.BLACK_JACK
         }
 
-    val isPlaying: Boolean
-        get() {
-            return state == States.HIT
-        }
-
-    init {
-        if (cards.score == Game.BLACK_JACK_SCORE) {
-            state = States.BLACK_JACK
-        }
-    }
-
-    constructor(name: String, cards: Set<Card>) : this(name, PlayerCards(cards))
+    constructor(name: String, cards: Set<Card>) : super(name, PlayerCards(cards))
     constructor(name: String, vararg cards: Card) : this(name, PlayerCards(cards.toSet()))
 
-    private fun findStateByScore(score: Int): States {
+    override fun findStateByScore(score: Int): States {
         return when {
             score < Game.BLACK_JACK_SCORE -> { States.HIT }
             score > Game.BLACK_JACK_SCORE -> { States.BUST }
@@ -39,31 +18,7 @@ class Player(val name: String, cards: PlayerCards) {
         }
     }
 
-    fun draw(card: Card) {
-        throwExceptionIfIsNotPlayingState()
-
-        cards = cards.addCard(card)
-    }
-
-    fun stop() {
-        state = States.STAY
-    }
-
-    fun throwExceptionIfIsNotPlayingState() {
-        if (isPlaying.not()) {
-            throw IllegalStateException("게임이 진행 불가능한 상태입니다. : $state")
-        }
-    }
-
-    fun isSmallerThanMinimumDealerScore(): Boolean {
+    override fun isSmallerThanMinimumDealerScore(): Boolean {
         return cards.score <= 16
-    }
-
-    fun isEnd(): Boolean {
-        if (name == "dealer") {
-            return state == States.WIN
-        }
-
-        return state == States.BLACK_JACK
     }
 }
