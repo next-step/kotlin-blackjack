184f03d5c932878344c4c47ebceed940b3239fa7
ohgillwhan <soora_genius@naver.com>
2021-05-03 20:51:58 +0900

    test : PlayerCards의 score를 구해오는 코드를 테스트

--- src/test/kotlin/blackjack/domain/PlayerCardsTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerCardsTest.kt
@@ -2,6 +2,8 @@ package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.util.LinkedList
 
 class PlayerCardsTest {
@@ -20,4 +22,29 @@ class PlayerCardsTest {
 
         assertThat(card).isEqualTo(Card(CardSuite.DIAMOND, CardNumber.ACE))
     }
+
+    @ParameterizedTest
+    @MethodSource("cardGenerate")
+    fun `카드에 맞게 점수를 가져야 한다`(cardAndScore: Pair<Set<Card>, Int>) {
+        val playerCards: PlayerCards = PlayerCards(cardAndScore.first)
+
+        assertThat(playerCards.score)
+            .isEqualTo(cardAndScore.second)
+    }
+
+    companion object {
+        @JvmStatic
+        fun cardGenerate(): List<Pair<Set<Card>, Int>> {
+            return listOf(
+                Pair(setOf(Card(CardSuite.CLOVER, CardNumber.QUEEN), Card(CardSuite.CLOVER, CardNumber.KING)), 20),
+                Pair(setOf(Card(CardSuite.CLOVER, CardNumber.QUEEN), Card(CardSuite.CLOVER, CardNumber.KING), Card(CardSuite.CLOVER, CardNumber.ACE)), 21),
+                Pair(setOf(Card(CardSuite.CLOVER, CardNumber.ACE), Card(CardSuite.CLOVER, CardNumber.KING)), 21),
+                Pair(setOf(Card(CardSuite.CLOVER, CardNumber.TWO), Card(CardSuite.CLOVER, CardNumber.THREE), Card(CardSuite.CLOVER, CardNumber.NINE)), 14)
+
+            )
+        }
+    }
 }
+
+// [kotlin.Pair<? extends java.util.Set<blackjack.domain.Card>, java.lang.Integer> arg0]
+// kotlin.Pair<? extends java.util.Set<blackjack.domain.Card>, java.lang.Integer>)].
