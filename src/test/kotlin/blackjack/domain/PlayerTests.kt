e0e0f021e54a54c82729bb716321c34db0aa1438
sanggu <tkdrn8578@naver.com>
2021-05-12 22:42:43 +0900

    refactor: Dealer로 생성하도록 수정

--- src/test/kotlin/blackjack/domain/PlayerTests.kt
+++ src/test/kotlin/blackjack/domain/PlayerTests.kt
@@ -96,7 +96,7 @@ class PlayerTests {
 
     @Test
     fun `딜러가 21을 초과하면 그 시점까지 남아 있던 플레이어들은 가지고 있는 패에 상관 없이 승리한다`() {
-        val game = Player("dealer", Card(CardSuite.HEART, CardNumber.QUEEN), Card(CardSuite.SPADE, CardNumber.JACK))
+        val game = Dealer(setOf(Card(CardSuite.HEART, CardNumber.QUEEN), Card(CardSuite.SPADE, CardNumber.JACK)))
 
         game.draw(Card(CardSuite.DIAMOND, CardNumber.QUEEN))
 
