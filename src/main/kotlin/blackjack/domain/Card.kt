1b4d88949a9ad09a8ccb6595799e949f4d940708
kimsanggu <tkdrn8578@paymint.co.kr>
2021-05-12 17:54:27 +0900

    refactor: cards를 뽑을 때 shuffled 하도록 수정

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -7,6 +7,7 @@ data class Card(val suite: CardSuite, val number: CardNumber) {
     companion object {
         val CARDS = CardNumber.values()
             .flatMap { number -> generateCards(number) }
+            .shuffled()
             .toSet()
 
         private fun generateCards(number: CardNumber) = CardSuite.values().map { suite -> Card(suite, number) }
