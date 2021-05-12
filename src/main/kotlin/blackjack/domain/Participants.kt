064df0c28c03d104f0242346d39bbfc05853a94e
sanggu <tkdrn8578@naver.com>
2021-05-12 23:20:22 +0900

    refactor: 불필요한 메서드 제거

--- src/main/kotlin/blackjack/domain/Participants.kt
+++ src/main/kotlin/blackjack/domain/Participants.kt
@@ -1,7 +1,3 @@
 package blackjack.domain
 
-class Participants(private val participants: Set<Participant>) : Set<Participant> by participants {
-
-    val countOfPlayingState: Int
-        get() = participants.count { it.isPlaying }
-}
+class Participants(private val participants: Set<Participant>) : Set<Participant> by participants
