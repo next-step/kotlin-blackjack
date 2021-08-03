84f336d5a114cedf06c2c616708f1e76f6b23cc8
sanggu <tkdrn8578@naver.com>
2021-08-03 23:34:30 +0900

    refactor: findWinnerScore, makeWinners의 책임을 Participants로 이동 및 테스트코드 추가

--- src/main/kotlin/blackjack/domain/Participants.kt
+++ src/main/kotlin/blackjack/domain/Participants.kt
@@ -1,3 +1,12 @@
 package blackjack.domain
 
-class Participants(private val participants: Set<Participant>) : Set<Participant> by participants
+class Participants(private val participants: Set<Participant>) : Set<Participant> by participants {
+
+    fun findWinnerScore(): Participant {
+        return participants.minBy { it.calculateToFindWinner() } ?: throw IllegalArgumentException("무승부입니다. : $this")
+    }
+
+    fun makeWinners(winner: Participant) {
+        participants.filter { it.isSameScore(winner) }.forEach { it.win() }
+    }
+}
