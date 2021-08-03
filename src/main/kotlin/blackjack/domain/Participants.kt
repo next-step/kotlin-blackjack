477fbf90848a950496ad98e365235f7c1c68d435
sanggu <tkdrn8578@naver.com>
2021-08-04 00:06:44 +0900

    refactor: 무승부가 아닌 존재하지 않는 플레이어인 경우로 예외 변경

--- src/main/kotlin/blackjack/domain/Participants.kt
+++ src/main/kotlin/blackjack/domain/Participants.kt
@@ -3,7 +3,7 @@ package blackjack.domain
 class Participants(private val participants: Set<Participant>) : Set<Participant> by participants {
 
     fun findWinnerScore(): Participant {
-        return participants.minBy { it.calculateToFindWinner() } ?: throw IllegalArgumentException("무승부입니다. : $this")
+        return participants.minBy { it.calculateToFindWinner() } ?: throw IllegalStateException("플레이어가 존재하지 않습니다.")
     }
 
     fun makeWinners(winner: Participant) {
