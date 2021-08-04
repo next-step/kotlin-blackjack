1a8f7159a3e7023945d0ca86f81ddf3e36ed6ec4
sanggu <tkdrn8578@naver.com>
2021-08-05 00:18:31 +0900

    refactor: toChar()인 경우 공백이 출력되는 문제로 toString으로 변경

--- src/main/kotlin/blackjack/view/CardDisplayNumber.kt
+++ src/main/kotlin/blackjack/view/CardDisplayNumber.kt
@@ -4,18 +4,18 @@ import blackjack.domain.CardNumber
 
 object CardDisplayNumber {
     private const val INDEX_OF_DISPLAY_NAME = 0
-    private const val ACE_DISPLAY_NAME = 'A'
+    private const val ACE_DISPLAY_NAME = "A"
 
-    val CardNumber.displayName: Char
+    val CardNumber.displayName: String
         get() {
             if (this == CardNumber.ACE) {
                 return ACE_DISPLAY_NAME
             }
 
             if (this == CardNumber.JACK || this == CardNumber.QUEEN || this == CardNumber.KING) {
-                return this.name[INDEX_OF_DISPLAY_NAME]
+                return this.name[INDEX_OF_DISPLAY_NAME].toString()
             }
 
-            return this.value.toChar()
+            return this.value.toString()
         }
 }
