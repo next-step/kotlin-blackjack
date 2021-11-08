a18031a36d79a6b74b93c001de065469fb2c8ab3
ninjasul <ninjasul@gmail.com>
2021-11-09 01:00:28 +0900

    feat: Dealer 구현

---
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -0,0 +1,31 @@
+package blackjack.domain
+
+import blackjack.view.input.InputView
+import blackjack.view.result.ResultView
+
+class Dealer(private val deck: CardDeck) {
+    fun deliverBasicCards(players: Players) {
+        players.players
+            .forEach { player -> repeat(BASIC_CARD_NUMBER) { player.receiveCard(deck.getNextCard()) } }
+    }
+
+    fun deliverAdditionalCards(players: Players, inputView: InputView, resultView: ResultView) {
+        players.players
+            .forEach { player -> deliverAdditionalCards(inputView, resultView, player) }
+    }
+
+    private fun deliverAdditionalCards(inputView: InputView, resultView: ResultView, player: Player) {
+        while (player.canReceiveAdditionalCard() && inputView.askToReceiveAdditionalCardOrNot(player)) {
+            deliverAdditionalCard(player)
+            resultView.showPlayerCards(player, true)
+        }
+    }
+
+    private fun deliverAdditionalCard(player: Player) {
+        player.receiveCard(deck.getNextCard())
+    }
+
+    companion object {
+        const val BASIC_CARD_NUMBER = 2
+    }
+}
---
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -0,0 +1,27 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
+
+internal class DealerTest {
+    @Test
+    fun `deliverBasicCards() 메소드로 Players들에게 카드를 나눠주면 각 Player들은 카드를 2장씩 가지게 된다`() {
+        val players = Players(listOf(Player("aaa"), Player("bbb")))
+
+        var cards = listOf(
+            Card(CardSymbol.SPADE, CardNumber.ACE),
+            Card(CardSymbol.HEART, CardNumber.KING),
+            Card(CardSymbol.DIAMOND, CardNumber.JACK),
+            Card(CardSymbol.CLUBS, CardNumber.QUEEN),
+        )
+        val cardDeck = CardDeck(Stack<Card>().also { it.addAll(cards) })
+        val dealer = Dealer(cardDeck)
+
+        dealer.deliverBasicCards(players)
+
+        players.players.forEach {
+            assertThat(it.getCards().size).isEqualTo(2)
+        }
+    }
+}
