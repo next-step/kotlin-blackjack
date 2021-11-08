f6a59fd2bca4fbf1d16e2b5754959595a6bb6f5a
ninjasul <ninjasul@gmail.com>
2021-11-09 00:07:48 +0900

    feat: CardDeck 구현

---
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -0,0 +1,32 @@
+package blackjack.domain
+
+import java.util.*
+
+data class CardDeck(val cards: Stack<Card>) {
+    fun getNextCard(): Card {
+        check(cards.isNotEmpty()) { NO_CARD_LEFT_MESSAGE }
+        return cards.pop()!!
+    }
+
+    companion object {
+        private const val NO_CARD_LEFT_MESSAGE = "덱에 카드가 남아있지 않습니다."
+
+        private val cardPool: Map<CardSymbol, List<Card>> = buildCardPool()
+
+        private fun buildCardPool(): Map<CardSymbol, List<Card>> {
+            return CardSymbol.values().associateWith { buildCardsPerSymbol(it) }
+        }
+
+        private fun buildCardsPerSymbol(symbol: CardSymbol) =
+            CardNumber.values().map { Card(symbol, it) }.toList()
+
+        fun getShuffledCards(): Stack<Card> {
+            return Stack<Card>().also { it.addAll(getShuffledCardList()) }
+        }
+
+        private fun getShuffledCardList() = cardPool.values
+            .flatten()
+            .shuffled()
+            .toList()
+    }
+}
