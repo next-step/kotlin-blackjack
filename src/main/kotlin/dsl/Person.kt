8058d43965e45e7b85770a3a7d40ceee82d4509b
thgus5614@naver.com <thgus5614@naver.com>
2021-11-10 00:10:35 +0900

    improve: 사용하지 않는 코드 삭제

--- src/main/kotlin/dsl/Person.kt
+++ src/main/kotlin/dsl/Person.kt
@@ -4,25 +4,3 @@ import dsl.languages.Languages
 import dsl.skills.Skills
 
 data class Person(val name: String?, val company: String?, val skills: Skills?, val language: Languages?)
-// {
-//     lateinit var name: String
-//     lateinit var company: String
-//     lateinit var skills: Skills
-//     lateinit var language: Languages
-//
-//     fun name(name: String) {
-//         this.name = name
-//     }
-//
-//     fun company(company: String) {
-//         this.company = company
-//     }
-//
-//     fun skills(init: Skills.() -> Unit) {
-//         this.skills = Skills().apply(init)
-//     }
-//
-//     fun languages(init: Languages.() -> Unit) {
-//         this.language = Languages().apply(init)
-//     }
-// }
