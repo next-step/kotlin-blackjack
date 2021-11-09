df47c9b6b1c633fc6a868394f76709368f7943d9
LimHanGyeol <hang19663@gmail.com>
2021-11-10 00:29:10 +0900

    feat: Kotlin DSL 실습

---
+++ src/main/kotlin/study/resume/Languages.kt
@@ -0,0 +1,10 @@
+package study.resume
+
+class Languages(
+    private val languages: MutableList<Language> = mutableListOf(),
+) : List<Language> by languages {
+
+    infix fun String.level(level: Int) {
+        languages.add(Language(this, level))
+    }
+}
---
+++ src/main/kotlin/study/resume/Skill.kt
@@ -0,0 +1,7 @@
+package study.resume
+
+sealed class Skill
+
+data class Hard(val name: String) : Skill()
+
+data class Soft(val name: String) : Skill()
