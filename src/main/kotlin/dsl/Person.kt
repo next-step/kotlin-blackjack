c7b322182afad6453e401cec814f3ef9fbeb5415
ninjasul <ninjasul@gmail.com>
2021-11-10 18:22:33 +0900

    [Step1] kotlin dsl 실습 (#114)
    * feat: step1 구현
    
    * test: 테스트 메소드 변경(containsExactlyInAnyOrder -> containsExactly)
    
    * style: reformat codes

---
+++ src/main/kotlin/dsl/Person.kt
@@ -0,0 +1,28 @@
+package dsl
+
+fun introduce(initializer: Person.() -> Unit): Person {
+    return Person().apply(initializer)
+}
+
+class Person {
+    lateinit var name: String
+    var company: String? = null
+    lateinit var skills: Skills
+    lateinit var languages: Languages
+
+    fun name(name: String) {
+        this.name = name
+    }
+
+    fun company(company: String?) {
+        this.company = company
+    }
+
+    fun skills(initializer: Skills.() -> Unit): Skills {
+        return Skills().apply(initializer).also { this.skills = it }
+    }
+
+    fun languages(initializer: Languages.() -> Unit): Languages {
+        return Languages().apply(initializer).also { this.languages = it }
+    }
+}
---
+++ src/test/kotlin/dsl/KotlinDslTest.kt
@@ -0,0 +1,48 @@
+package dsl
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class KotlinDslTest {
+
+    @Test
+    internal fun person() {
+        val person = introduce {
+            name("ninjasul")
+            company("KakaoPay")
+            skills {
+                soft("communication")
+                hard("kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+
+        assertThat(person.name).isEqualTo("ninjasul")
+        assertThat(person.company).isEqualTo("KakaoPay")
+        assertThat(person.skills.items).containsExactly(Soft("communication"), Hard("kotlin"))
+        assertThat(person.languages.items).containsExactly(Language("Korean", 5), Language("English", 3))
+    }
+
+    @Test
+    internal fun skills() {
+        val skills = Person().skills {
+            soft("communication")
+            hard("kotlin")
+        }
+
+        assertThat(skills.items).containsExactly(Soft("communication"), Hard("kotlin"))
+    }
+
+    @Test
+    internal fun languages() {
+        val languages = Person().languages {
+            "Korean" level 5
+            "English" level 3
+        }
+
+        assertThat(languages.items).containsExactly(Language("Korean", 5), Language("English", 3))
+    }
+}
