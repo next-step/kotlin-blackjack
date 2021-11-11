50fa9762ee0295508b433bf7626a32cf04f0a98c
김우재 <kwj1270@naver.com>
2021-11-11 16:09:57 +0900

    test: 이름과 회사를 가진 사람을 만든다

--- src/test/kotlin/dsl/PersonTest.kt
+++ src/test/kotlin/dsl/PersonTest.kt
@@ -12,4 +12,14 @@ class PersonTest {
         }
         Assertions.assertThat(person.name).isEqualTo("김우재")
     }
+
+    @Test
+    fun `이름과 회사를 가진 사람을 만든다`() {
+        val person: Person = introduce {
+            name("김우재")
+            company("우아한형제들")
+        }
+        Assertions.assertThat(person.name).isEqualTo("김우재")
+        Assertions.assertThat(person.company).isEqualTo("우아한형제들")
+    }
 }
