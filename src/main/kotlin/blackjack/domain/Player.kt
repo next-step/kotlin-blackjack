1970dd7c6a94c903fcce630bcb315f0934d9e62e
nachwon <nachwon@gmail.com>
2021-11-12 01:49:43 +0900

    feat: Add takeCards to Player

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,13 @@
 package blackjack.domain
 
-data class Player(val name: String)
+data class Player(val name: String) {
+    private val _cards: MutableList<Card> = mutableListOf()
+    val cards: List<Card>
+        get() = _cards.deepCopy()
+
+    fun takeCards(vararg cards: Card) {
+        _cards.addAll(cards)
+    }
+
+    private fun List<Card>.deepCopy() = this.map { it.copy() }
+}
--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -2,6 +2,8 @@ package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class PlayerTest {
     @Test
@@ -9,5 +11,16 @@ class PlayerTest {
         val player = Player(name = "che1")
 
         assertThat(player.name).isEqualTo("che1")
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 5, 13])
+    fun `플레이어는 카드를 받아 가지고 있을 수 있다`(cardCount: Int) {
+        val player = Player(name = "che1")
+        val cards = (1..cardCount).map { Card(CardSymbol.CLOVER, CardNumber(it)) }
+
+        player.takeCards(*cards.toTypedArray())
+
+        assertThat(player.cards.size).isEqualTo(cardCount)
     }
 }
