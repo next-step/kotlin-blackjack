6e18c3dc580d52624b28886da76a61b2c9de9788
김우재 <kwj1270@naver.com>
2021-11-11 16:22:41 +0900

    test: 이름과 회사 그리고 기술을 가진 사람을 만든다

--- src/test/kotlin/dsl/PersonTest.kt
+++ src/test/kotlin/dsl/PersonTest.kt
@@ -22,4 +22,24 @@ class PersonTest {
         Assertions.assertThat(person.name.value).isEqualTo("김우재")
         Assertions.assertThat(person.company.value).isEqualTo("우아한형제들")
     }
+
+    @Test
+    fun `이름과 회사 그리고 기술을 가진 사람을 만든다`() {
+        val person: Person = introduce {
+            name("김우재")
+            company("우아한형제들")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+        Assertions.assertThat(person.name.value).isEqualTo("김우재")
+        Assertions.assertThat(person.company.value).isEqualTo("우아한형제들")
+        Assertions.assertThat(person.skills).contains(
+            Soft("A passion for problem solving"),
+            Soft("Good communication skills"),
+            Hard("Kotlin")
+        )
+    }
 }
