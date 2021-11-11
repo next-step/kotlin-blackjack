d9a43d2e8aeb745da57168de3bcb7077e1f5748a
ninjasul <ninjasul@gmail.com>
2021-11-12 08:15:02 +0900

    test: removed unuseful tests from CardNumberTest

--- src/test/kotlin/blackjack/domain/CardNumberTest.kt
+++ src/test/kotlin/blackjack/domain/CardNumberTest.kt
@@ -7,12 +7,6 @@ import org.junit.jupiter.params.provider.ValueSource
 
 internal class CardNumberTest {
     @ParameterizedTest
-    @ValueSource(strings = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
-    fun `J, Q, K 외 문자열을 isRoyaFamily() 메소드의 인자로 입력하면 false를 리턴한다`(text: String) {
-        assertThat(CardNumber.isRoyalFamily(text)).isFalse
-    }
-
-    @ParameterizedTest
     @ValueSource(strings = ["J", "Q", "K"])
     fun `J, Q, K 를 isRoyaFamily() 메소드의 인자로 입력하면 true를 리턴한다`(text: String) {
         assertThat(CardNumber.isRoyalFamily(text)).isTrue
@@ -21,11 +15,5 @@ internal class CardNumberTest {
     @Test
     fun `A 문자열을 isAce() 메소드의 인자로 입력하면 true를 리턴한다`() {
         assertThat(CardNumber.isAce("A")).isTrue
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"])
-    fun `A외의 문자열을 isAce() 메소드의 인자로 입력하면 false를 리턴한다`(text: String) {
-        assertThat(CardNumber.isAce(text)).isFalse
     }
 }
