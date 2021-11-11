fa60c451560d64cb2092f7a050c779fd317ebf8f
김우재 <kwj1270@naver.com>
2021-11-11 16:11:49 +0900

    feat: 이름과 회사를 가진 사람을 만든다

--- src/main/kotlin/dsl/Person.kt
+++ src/main/kotlin/dsl/Person.kt
@@ -2,8 +2,10 @@ package dsl
 
 class Person {
     lateinit var name: String
+    lateinit var company: String
 
     fun name(name: String) = name.also { this.name = it }
+    fun company(company: String) = company.also { this.company = it }
 }
 
 fun introduce(initializer: Person.() -> Unit): Person {
