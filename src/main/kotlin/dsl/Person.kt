c1eca4d291dd8ed704f670fc3962649ddc0452ad
김우재 <kwj1270@naver.com>
2021-11-11 16:08:59 +0900

    feat: 이름을 가진 사람을 만든다

--- src/main/kotlin/dsl/Person.kt
+++ src/main/kotlin/dsl/Person.kt
@@ -1,3 +1,13 @@
 package dsl
 
-class Person
+class Person {
+    lateinit var name: String
+
+    fun name(name: String) = name.also { this.name = it }
+}
+
+fun introduce(initializer: Person.() -> Unit): Person {
+    val person = Person()
+    person.initializer()
+    return person
+}
