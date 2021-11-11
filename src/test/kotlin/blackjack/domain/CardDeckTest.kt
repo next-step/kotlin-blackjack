4b79ebf3efa800a8da595ec00e06db06c3cfe572
ninjasul <ninjasul@gmail.com>
2021-11-12 08:17:33 +0900

    test: sanitize CardDeckTest

--- src/test/kotlin/blackjack/domain/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/CardDeckTest.kt
@@ -17,38 +17,8 @@ internal class CardDeckTest {
     }
 
     @Test
-    fun `카드가 한 장 들어있는 CardDeck에 getNextCard() 메소드를 두번 호출하면 IllegalStateException이 발생한다`() {
-        var cards = listOf(
-            Card(CardSymbol.SPADE, CardNumber.ACE),
-        )
-
-        val cardDeck = CardDeck(Stack<Card>().also { it.addAll(cards) })
-        cardDeck.getNextCard()
-
-        assertThrows<IllegalStateException> {
-            cardDeck.getNextCard()
-        }
-    }
-
-    @Test
-    fun `카드가 두 장 들어있는 CardDeck에 getNextCard() 메소드를 세번 호출하면 IllegalStateException이 발생한다`() {
-        var cards = listOf(
-            Card(CardSymbol.SPADE, CardNumber.ACE),
-            Card(CardSymbol.CLUBS, CardNumber.KING),
-        )
-
-        val cardDeck = CardDeck(Stack<Card>().also { it.addAll(cards) })
-        cardDeck.getNextCard()
-        cardDeck.getNextCard()
-
-        assertThrows<IllegalStateException> {
-            cardDeck.getNextCard()
-        }
-    }
-
-    @Test
     fun `카드가 한 장 들어있는 CardDeck에 getNextCard() 메소드를 호출하면 Deck에 들어가 있는 카드를 얻을 수 있다`() {
-        var cards = listOf(
+        val cards = listOf(
             Card(CardSymbol.SPADE, CardNumber.ACE),
         )
         val cardDeck = CardDeck(Stack<Card>().also { it.addAll(cards) })
@@ -58,7 +28,7 @@ internal class CardDeckTest {
 
     @Test
     fun `카드가 두 장 들어있는 CardDeck에 getNextCard() 메소드를 호출하면 두번째로 Deck에 들어간 카드를 얻을 수 있다`() {
-        var cards = listOf(
+        val cards = listOf(
             Card(CardSymbol.SPADE, CardNumber.ACE),
             Card(CardSymbol.CLUBS, CardNumber.KING),
         )
