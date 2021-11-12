6a0f32b5caaf4484e2ecb044c06fb378245bbc09
thgus5614@naver.com <thgus5614@naver.com>
2021-11-12 23:32:16 +0900

    improve: 테스트 코드 이름 변경

--- src/test/kotlin/blackject/ParticipantTest.kt
+++ src/test/kotlin/blackject/ParticipantTest.kt
@@ -12,7 +12,7 @@ class ParticipantTest {
 
     @ValueSource(strings = ["sohyun,hiii"])
     @ParameterizedTest
-    @DisplayName("참가자 ")
+    @DisplayName("참가자 리스트 확인")
     fun `check participant list`(name: String) {
         val participant = Participant.addPerson(name)
         val expectedList = listOf("sohyun", "hiii")
@@ -23,7 +23,7 @@ class ParticipantTest {
 
     @ParameterizedTest
     @NullAndEmptySource
-    @DisplayName("")
+    @DisplayName("참가자 리스트의 빈칸 혹은 null 값이 들어온 경우")
     fun `check participant list null or empty`(name: String?) {
         assertThrows<IllegalArgumentException> {
             Participant.addPerson(name)
