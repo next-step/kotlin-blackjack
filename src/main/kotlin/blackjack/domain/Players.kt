c38722eaebab5737182899e1a8772d091f575b81
ddaaac <skyblue300a@gmail.com>
2021-11-13 18:27:16 +0900

    Player 구현

---
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -0,0 +1,16 @@
+package blackjack.domain
+
+private const val GAME_START_DRAW_COUNT = 2
+
+class Players(val players: List<Player>) {
+
+    fun drawAtGameStart(deck: Deck) {
+        repeat(GAME_START_DRAW_COUNT) {
+            players.forEach { it.draw(deck) }
+        }
+    }
+
+    fun forEach(action: (Player) -> Unit) {
+        players.forEach(action)
+    }
+}
