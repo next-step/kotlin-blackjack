b70c4794d0c95b0e5003a5c009b16ef28f4221f8
thgus5614@naver.com <thgus5614@naver.com>
2021-11-14 15:58:09 +0900

    add: 카드 숫자 클래스 테스트 코드 추가

--- src/test/kotlin/blackject/CardNumberTest.kt
+++ src/test/kotlin/blackject/CardNumberTest.kt
@@ -4,8 +4,101 @@ import blackject.model.card.CardNumber
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class CardNumberTest {
+
+    @Test
+    @DisplayName("2번 카드 값 구하기")
+    fun `check 2 card number`() {
+        val card = CardNumber.TWO
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(2)
+    }
+
+    @Test
+    @DisplayName("2번 카드 값 구하기")
+    fun `check 3 card number`() {
+        val card = CardNumber.THREE
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(3)
+    }
+
+    @Test
+    @DisplayName("2번 카드 값 구하기")
+    fun `check 4 card number`() {
+        val card = CardNumber.FOUR
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(4)
+    }
+
+    @Test
+    @DisplayName("2번 카드 값 구하기")
+    fun `check 5 card number`() {
+        val card = CardNumber.FIVE
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(5)
+    }
+
+    @Test
+    @DisplayName("6번 카드 값 구하기")
+    fun `check 6 card number`() {
+        val card = CardNumber.SIX
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(6)
+    }
+
+    @Test
+    @DisplayName("7번 카드 값 구하기")
+    fun `check 7 card number`() {
+        val card = CardNumber.SEVEN
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(7)
+    }
+
+    @Test
+    @DisplayName("8번 카드 값 구하기")
+    fun `check 8 card number`() {
+        val card = CardNumber.EIGHT
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(8)
+    }
+
+    @Test
+    @DisplayName("9번 카드 값 구하기")
+    fun `check 9 card number`() {
+        val card = CardNumber.NINE
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(9)
+    }
+
+    @Test
+    @DisplayName("10번 카드 값 구하기")
+    fun `check 10 card number`() {
+        val card = CardNumber.TEN
+
+        val cardNumber = CardNumber.getNumberValue(card)
+
+        Assertions.assertThat(cardNumber).isEqualTo(10)
+    }
+
     @Test
     @DisplayName("Ace 카드의 최소 숫자 구하기")
     fun `check ace number of min`() {
@@ -59,5 +152,23 @@ class CardNumberTest {
         val number = CardNumber.getNumberValue(king)
 
         Assertions.assertThat(number).isEqualTo(expectedMinNumber)
+    }
+
+    @EnumSource(names = ["QUEEN", "JACK", "KING"])
+    @ParameterizedTest
+    @DisplayName("local 종류의 카드 확인")
+    fun `check loyal card`(cardNumber: CardNumber) {
+        val number = CardNumber.isRoyal(cardNumber.numberName)
+
+        Assertions.assertThat(number).isEqualTo(true)
+    }
+
+    @EnumSource(names = ["ACE"])
+    @ParameterizedTest
+    @DisplayName("ace 종류의 카드 확인")
+    fun `check ace card`(cardNumber: CardNumber) {
+        val number = CardNumber.isAce(cardNumber.numberName)
+
+        Assertions.assertThat(number).isEqualTo(true)
     }
 }
