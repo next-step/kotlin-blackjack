2930295d3b6f11a5b5deae492f81977bda45c3a6
김우재 <kwj1270@naver.com>
2021-11-15 23:06:49 +0900

    test: PlayingCards, 카드들에는 새로운 카드가 추가될 수 있다

--- src/test/kotlin/blackjack/domain/state/PlayingCardsTest.kt
+++ src/test/kotlin/blackjack/domain/state/PlayingCardsTest.kt
@@ -1,5 +1,8 @@
 package blackjack.domain.state
 
+import blackjack.domain.Denomination
+import blackjack.domain.PlayingCard
+import blackjack.domain.Suit
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.DisplayName
@@ -16,5 +19,14 @@ internal class PlayingCardsTest {
             { assertThat(playingCards).isNotNull },
             { assertThat(playingCards).isExactlyInstanceOf(PlayingCards::class.java) },
         )
+    }
+
+    @Test
+    fun `카드들에는 새로운 카드가 추가될 수 있다`() {
+        val expected = PlayingCards.from(listOf(PlayingCard(Suit.SPADE, Denomination.ACE)))
+        val playingCards: PlayingCards = PlayingCards.initialize()
+        val actual = playingCards.add(PlayingCard(Suit.SPADE, Denomination.ACE))
+
+        assertThat(actual).isEqualTo(expected)
     }
 }
