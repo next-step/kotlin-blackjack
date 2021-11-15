5032180a8d008708c465fb96d0c984e709dd859e
김우재 <kwj1270@naver.com>
2021-11-15 15:41:02 +0900

    refactor: Score, 0~32 사이의 숫자에 캐싱 적용

--- src/main/kotlin/blackjack/domain/Score.kt
+++ src/main/kotlin/blackjack/domain/Score.kt
@@ -3,6 +3,10 @@ package blackjack.domain
 class Score(val score: Int) {
 
     companion object {
-        fun from(score: Int): Score = Score(score)
+        private const val MINIMUM_SCORE = 0
+        private const val MAXIMUM_SCORE = 32
+        private val CACHE: Map<Int, Score> = (MINIMUM_SCORE..MAXIMUM_SCORE).associateWith { Score(it) }
+
+        fun from(score: Int): Score = CACHE[score] ?: throw IllegalArgumentException()
     }
 }
