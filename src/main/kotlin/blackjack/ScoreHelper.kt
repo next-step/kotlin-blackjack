ce40b34548a040f3b249b6d3f83f1f6e222f1973
laco-dev <laco2951@gmail.com>
2021-11-15 11:12:16 +0900

    ftr - Ace를 제외한 나머지 점수들의 합이 10보다 크다면 Ace는 1점으로 계산한다

--- src/main/kotlin/blackjack/ScoreHelper.kt
+++ src/main/kotlin/blackjack/ScoreHelper.kt
@@ -6,6 +6,7 @@ object ScoreHelper {
     private const val ACE_BONUS_SCORE = 11
 
     fun sum(cards: List<Card>): Int = cards.asSequence()
+        .sortedByDescending { it.denomination.defaultScore }
         .map { it.denomination }
         .fold(0) { acc, denomination ->
             if (denomination.isAce() && acc + ACE_BONUS_SCORE <= TWENTY_ONE) {
--- src/test/kotlin/blackjack/ScoreHelperTest.kt
+++ src/test/kotlin/blackjack/ScoreHelperTest.kt
@@ -16,10 +16,20 @@ class ScoreHelperTest {
     }
 
     @Test
-    fun `Ace를 제외한 나머지 점수들의 합이 10이하 일경우 Ace는 11점으로 계산한다`() {
+    fun `Ace를 제외한 나머지 점수들의 합이 10이하라면 Ace는 11점으로 계산한다`() {
         val cards = listOf(
             Card(Denomination.ACE, Suit.HEART),
             Card(Denomination.TEN, Suit.HEART)
+        )
+        assertThat(ScoreHelper.sum(cards)).isEqualTo(21)
+    }
+
+    @Test
+    fun `Ace를 제외한 나머지 점수들의 합이 10보다 크다면 Ace는 1점으로 계산한다`() {
+        val cards = listOf(
+            Card(Denomination.ACE, Suit.HEART),
+            Card(Denomination.TEN, Suit.HEART),
+            Card(Denomination.TEN, Suit.CLOVER),
         )
         assertThat(ScoreHelper.sum(cards)).isEqualTo(21)
     }
