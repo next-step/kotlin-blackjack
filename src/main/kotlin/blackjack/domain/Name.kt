e683d924267c43807e67de3d8de12e5a758c295f
김우재 <kwj1270@naver.com>
2021-11-15 14:31:54 +0900

    feat: Name, 공백이거나 널이 아닌 문자열로 이름을 만들 수 있다

--- src/main/kotlin/blackjack/domain/Name.kt
+++ src/main/kotlin/blackjack/domain/Name.kt
@@ -1,4 +1,4 @@
 package blackjack.domain
 
-class Name {
+class Name(name: String) {
 }
--- src/test/kotlin/blackjack/domain/NameTest.kt
+++ src/test/kotlin/blackjack/domain/NameTest.kt
@@ -1,15 +1,17 @@
 package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
+@DisplayName("이름(Name)")
 internal class NameTest {
 
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력값 : {0}")
     @ValueSource(strings = ["a", "ab", "abc", "abcdefghijkmlnopqrstuvwxyz"])
-    fun `공백이거나 널이 아닌 문자열로 이름을 만들 수 있다`(nameString: String) {
+    internal fun `공백이거나 널이 아닌 문자열로 이름을 만들 수 있다`(nameString: String) {
         val name = Name(nameString)
 
         assertAll(
