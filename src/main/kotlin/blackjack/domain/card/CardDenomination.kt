78273a44a4bfc9796f3d96c92f398c6520463942
sangyongchoi <csy7792@naver.com>
2021-11-17 00:32:20 +0900

    step3 modify code review content - remove values

--- src/main/kotlin/blackjack/domain/card/CardDenomination.kt
+++ src/main/kotlin/blackjack/domain/card/CardDenomination.kt
@@ -2,29 +2,23 @@ package blackjack.domain.card
 
 enum class CardDenomination(
     val denomination: String,
-    private val value: List<Int>,
     val order: Int,
-    val getValue: (Int) -> Int = { _ -> value[BASIC_VALUE_INDEX] },
+    val getValue: (Int) -> Int,
 ) {
-    ACE("ACE", listOf(1, 11), 1, { sum ->
-        if (sum + 11 > 21) ACE.value[BASIC_VALUE_INDEX]
-        else ACE.value[SPECIAL_VALUE_INDEX]
+    ACE("ACE", 1, { sum ->
+        if (sum + 11 > 21) 1
+        else 11
     }),
-    TWO("2", listOf(2), 2),
-    THREE("3", listOf(3), 3),
-    FOUR("4", listOf(4), 4),
-    FIVE("5", listOf(5), 5),
-    SIX("6", listOf(6), 6),
-    SEVEN("7", listOf(7), 7),
-    EIGHT("8", listOf(8), 8),
-    NINE("9", listOf(9), 9),
-    TEN("10", listOf(10), 10),
-    JACK("J", listOf(10), 11),
-    QUEEN("Q", listOf(10), 12),
-    KING("K", listOf(10), 13);
-
-    companion object {
-        private const val BASIC_VALUE_INDEX = 0
-        private const val SPECIAL_VALUE_INDEX = 1
-    }
+    TWO("2", 2, { _ -> 2 }),
+    THREE("3", 3, { _ -> 3 }),
+    FOUR("4", 4, { _ -> 4 }),
+    FIVE("5", 5, { _ -> 5 }),
+    SIX("6", 6, { _ -> 6 }),
+    SEVEN("7", 7, { _ -> 7 }),
+    EIGHT("8", 8, { _ -> 8 }),
+    NINE("9", 9, { _ -> 9 }),
+    TEN("10", 10, { _ -> 10 }),
+    JACK("J", 11, { _ -> 10 }),
+    QUEEN("Q", 12, { _ -> 10 }),
+    KING("K", 13, { _ -> 10 });
 }
