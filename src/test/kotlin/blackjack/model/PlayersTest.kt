5774a74aec2d90efbe32b35cc37d60df226663ca
laco-dev <laco2951@gmail.com>
2021-11-18 18:19:23 +0900

    test - 플레이어들 모두에게 카드를 줄 수 있다.

--- src/test/kotlin/blackjack/model/PlayersTest.kt
+++ src/test/kotlin/blackjack/model/PlayersTest.kt
@@ -2,6 +2,7 @@ package blackjack.model
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class PlayersTest {
 
@@ -10,5 +11,33 @@ class PlayersTest {
         val names = Names.from("laco")
         val players = Players.from(names)
         assertThat(players.toNames()).isEqualTo(names)
+    }
+
+    @Test
+    fun `플레이어들 모두에게 카드를 줄 수 있다`() {
+        // given
+        val names = Names.from("laco", "pobi")
+        val players = Players.from(names)
+        val cards = listOf(
+            Card(Denomination.TEN, Suit.HEART),
+            Card(Denomination.TEN, Suit.SPADE),
+            Card(Denomination.TEN, Suit.DIAMOND),
+            Card(Denomination.TEN, Suit.CLOVER),
+        )
+
+        // when
+        var count = 0
+        val (player1, player2) = players.receiveAll(
+            count = 2,
+            next = { cards.getOrNull(count++) }
+        )
+            .toList()
+            .map { it.cards }
+
+        // then
+        assertAll(
+            { assertThat(player1).isEqualTo(Cards(cards.take(2))) },
+            { assertThat(player2).isEqualTo(Cards(cards.takeLast(2))) }
+        )
     }
 }
