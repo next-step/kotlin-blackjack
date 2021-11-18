5b8f8e9150f0b59fe68004989bbc89b58caa2fa9
seunghwan <slflfl12@naver.com>
2021-11-19 00:16:00 +0900

    feat (step1): 코틀린 DSL 실습

---
+++ src/main/kotlin/study/Languages.kt
@@ -0,0 +1,8 @@
+package study
+
+class Languages(val languages: MutableList<Language> = mutableListOf()) : List<Language> by languages {
+
+    infix fun String.level(level: Int) {
+        languages.add(Language(this, level))
+    }
+}
