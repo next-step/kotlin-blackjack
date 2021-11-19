fa9c930200c4646ebd506b89211858d4c5a6a15f
thgus5614@naver.com <thgus5614@naver.com>
2021-11-19 23:30:11 +0900

    improve: Person 클래스의 메소드 open

--- src/main/kotlin/blackject/model/Person.kt
+++ src/main/kotlin/blackject/model/Person.kt
@@ -7,19 +7,19 @@ import blackject.model.card.Cards
 /**
  * 참가자 정보 관리 클래스
  * */
-data class Person(
+open class Person(
     val type: PersonType,
     val name: String,
     val cards: Cards = Cards()
 ) {
-    fun isTakeMoreCard(maxInt: Int, dealerMaxNumber: Int, exceptCard: CardNumber): Boolean {
+    open fun isTakeMoreCard(maxInt: Int, dealerMaxNumber: Int, exceptCard: CardNumber): Boolean {
         if (type == PersonType.DEALER) {
             return dealerMaxNumber >= cards.getResultNumber(maxInt, exceptCard)
         }
         return maxInt > cards.getResultNumber(maxInt, exceptCard)
     }
 
-    fun giveCard(newCards: List<Card>) {
+    open fun giveCard(newCards: List<Card>) {
         cards.addCard(newCards)
     }
 }
