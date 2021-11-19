1fa3493acc2bcd84efa1038c6244a37df08247b5
Pyro <rhwjddhks@gmail.com>
2021-11-19 17:56:37 +0900

    feat: 포장되지 않은 원시값 제거

--- src/main/kotlin/domain/player/BetAmount.kt
+++ src/main/kotlin/domain/player/BetAmount.kt
@@ -3,9 +3,9 @@ package domain.player
 import exception.IllegalBetException
 
 @JvmInline
-value class BetAmount(val money: Int = DEFAULT_MONEY) {
+value class BetAmount(val money: Int = ZERO_MONEY) {
     init {
-        if (money < 0) {
+        if (money < ZERO_MONEY) {
             throw IllegalBetException()
         }
     }
@@ -13,6 +13,6 @@ value class BetAmount(val money: Int = DEFAULT_MONEY) {
     operator fun times(earningRate: Double): Double = earningRate * money
 
     companion object {
-        private const val DEFAULT_MONEY = 0
+        private const val ZERO_MONEY = 0
     }
 }
