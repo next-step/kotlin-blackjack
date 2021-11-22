30d36be99665022c4e24ae0c1009f5e3da7ee0bd
nachwon <nachwon@naver.com>
2021-11-22 22:07:08 +0900

    refactor: Refactor output view

--- src/main/kotlin/blackjack/views/OutputView.kt
+++ src/main/kotlin/blackjack/views/OutputView.kt
@@ -1,6 +1,8 @@
 package blackjack.views
 
 import blackjack.domain.Card
+import blackjack.domain.CardNumber
+import blackjack.domain.CardSymbol
 import blackjack.domain.Player
 
 class OutputView {
@@ -27,29 +29,43 @@ class OutputView {
     }
 
     private fun buildCardOutput(card: Card): String {
-        val cardNumberText = when (card.number.value) {
-            ACE_NUMBER -> ACE_TEXT
-            JACK_NUMBER -> JACK_TEXT
-            QUEEN_NUMBER -> QUEEN_TEXT
-            KING_NUMBER -> KING_TEXT
-            else -> card.number.value.toString()
-        }
+        return CARD_OUTPUT_TEMPLATE.format(
+            convertCardNumberToString(card.number),
+            convertSymbolToString(card.symbol)
+        )
+    }
 
-        return CARD_OUTPUT_TEMPLATE.format(cardNumberText, card.symbol.displayName)
+    private fun convertCardNumberToString(cardNumber: CardNumber): String {
+        return when (cardNumber) {
+            CardNumber.ACE -> ACE_TEXT
+            CardNumber.KING -> KING_TEXT
+            CardNumber.QUEEN -> QUEEN_TEXT
+            CardNumber.JACK -> JACK_TEXT
+            else -> cardNumber.value.toString()
+        }
+    }
+
+    private fun convertSymbolToString(cardSymbol: CardSymbol): String {
+        return when (cardSymbol) {
+            CardSymbol.HEART -> HEART_TEXT
+            CardSymbol.CLOVER -> CLOVER_TEXT
+            CardSymbol.DIAMOND -> DIAMOND
+            CardSymbol.SPADE -> SPADE
+        }
     }
 
     companion object {
         private const val INITIAL_DRAW_RESULT_MSG = "%s 에게 각각 2장의 카드를 전달하였습니다."
         private const val PLAYER_STATUS_MSG = "%s 카드: %s"
         private const val GAME_RESULT_MSG = " - 결과: %s"
-        private const val ACE_NUMBER = 1
-        private const val KING_NUMBER = 13
-        private const val QUEEN_NUMBER = 12
-        private const val JACK_NUMBER = 11
         private const val ACE_TEXT = "A"
         private const val KING_TEXT = "K"
         private const val QUEEN_TEXT = "Q"
         private const val JACK_TEXT = "J"
+        private const val HEART_TEXT = "하트"
+        private const val CLOVER_TEXT = "클로버"
+        private const val DIAMOND = "다이아몬드"
+        private const val SPADE = "스페이드"
         private const val CARD_OUTPUT_TEMPLATE = "[%s-%s]"
     }
 }
