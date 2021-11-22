d753fe0f0ebd3a0f9a787d9de08e7e87fb481888
seunghwan <slflfl12@naver.com>
2021-11-23 01:15:02 +0900

    test (step2) : CardDeckTest 추가

---
+++ src/test/kotlin/blackjack/CardDeckTest.kt
@@ -0,0 +1,34 @@
+package blackjack
+
+import blackjack.domain.Card
+import blackjack.domain.CardDeck
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class CardDeckTest {
+
+    @DisplayName(value = "CardDeck으로 부터 Card를 받아올 수 있다")
+    @Test
+    fun `CardDeck으로 부터 Card를 받아올 수 있다`() {
+        val cardDeck = CardDeck()
+
+        assertThat(cardDeck.next()).isInstanceOf(Card::class.java)
+    }
+
+    @DisplayName(value = "CardDeck에 Card가 다 떨어지면 Card 52장을 새로 추가한다")
+    @Test
+    fun `CardDeck에 Card가 다 떨어지면 Card 52장을 새로 추가한다`() {
+        val cardDeck = CardDeck()
+
+        assertThat(cardDeck.size).isEqualTo(52)
+
+        repeat(cardDeck.size) {
+            cardDeck.next()
+        }
+        assertThat(cardDeck.size).isEqualTo(0)
+
+        cardDeck.next()
+        assertThat(cardDeck.size).isEqualTo(51)
+    }
+}
