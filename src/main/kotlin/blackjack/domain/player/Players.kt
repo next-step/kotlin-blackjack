cf14bd3c33239571e5b9336238373ccc467857d8
김우재 <kwj1270@naver.com>
2021-11-23 19:49:33 +0900

    feat: Players, 이름과 분리 전략을 사용해서 플레이어들을 생성할 수 있다

--- src/main/kotlin/blackjack/domain/player/Players.kt
+++ src/main/kotlin/blackjack/domain/player/Players.kt
@@ -1,4 +1,17 @@
 package blackjack.domain.player
 
+import blackjack.domain.player.name.Name
+import blackjack.strategy.split.SplitStrategy
+
 @JvmInline
-value class Players(private val players: List<Player>)
+value class Players(private val players: List<Player>) {
+
+    companion object {
+        fun of(name: String, splitStrategy: SplitStrategy): Players =
+            splitStrategy.split(name)
+                .map { GamePlayer(Name(it)) }
+                .let { from(it) }
+
+        fun from(players: List<Player>): Players = Players(players.toList())
+    }
+}
--- src/test/kotlin/blackjack/domain/player/PlayersTest.kt
+++ src/test/kotlin/blackjack/domain/player/PlayersTest.kt
@@ -1,8 +1,8 @@
 package blackjack.domain.player
 
+import blackjack.domain.player.name.Name
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
@@ -12,11 +12,11 @@ internal class PlayersTest {
     @ParameterizedTest(name = "입력 값 : {0}")
     @ValueSource(strings = ["a", "a,b", "a,b,c"])
     fun `이름과 분리 전략을 사용해서 플레이어들을 생성할 수 있다`(names: String) {
-        val players: Players = Players.of(names) { it.split(",") }
+        val expectedGamePlayers = names.split(",").map { GamePlayer(Name(it)) }
+        val expected = Players.from(expectedGamePlayers)
 
-        assertAll(
-            { assertThat(players).isNotNull },
-            { assertThat(players).isExactlyInstanceOf(Players::class.java) },
-        )
+        val players = Players.of(names) { it.split(",") }
+
+        assertThat(players).isEqualTo(expected)
     }
 }
