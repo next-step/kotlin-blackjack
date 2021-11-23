1f97e01d31bd2852c0e852f8d76fddad3c5f1091
김우재 <kwj1270@naver.com>
2021-11-23 16:29:08 +0900

    test: GamePlayer, 카드 전략에 따라 드로우를 할 수 있다

--- src/test/kotlin/blackjack/domain/player/GamePlayerTest.kt
+++ src/test/kotlin/blackjack/domain/player/GamePlayerTest.kt
@@ -1,5 +1,7 @@
 package blackjack.domain.player
 
+import blackjack.domain.card.Deck
+import blackjack.domain.player.state.Ready
 import org.assertj.core.api.AssertionsForClassTypes
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.DisplayName
@@ -18,5 +20,14 @@ internal class GamePlayerTest {
         )
     }
 
+    @Test
+    fun `카드 전략에 따라 드로우를 할 수 있다`() {
+        val extraCard = Deck.initialize { it }.pop()
+        val expected = GamePlayer(Name("김우재"), Ready().draw(extraCard))
+
+        val dealer = GamePlayer(Name("김우재")).draw(Deck.initialize { it }) { listOf(it.pop()) }
+
+        AssertionsForClassTypes.assertThat(dealer).isEqualTo(expected)
+    }
 }
 
