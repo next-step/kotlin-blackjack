ae89b531e73f9f6cf1347241a7024a2592a9de25
seunghwan <slflfl12@naver.com>
2021-11-25 20:57:47 +0900

    refactor (step2) : CardDeckTest 불필요한 테스트 제거

--- src/test/kotlin/blackjack/CardDeckTest.kt
+++ src/test/kotlin/blackjack/CardDeckTest.kt
@@ -13,19 +13,4 @@ class CardDeckTest {
 
         assertThat(cardDeck.next()).isInstanceOf(Card::class.java)
     }
-
-    @Test
-    fun `CardDeck에 Card가 다 떨어지면 Card 52장을 새로 추가한다`() {
-        val cardDeck = CardDeck()
-
-        assertThat(cardDeck.size).isEqualTo(52)
-
-        repeat(cardDeck.size) {
-            cardDeck.next()
-        }
-        assertThat(cardDeck.size).isEqualTo(0)
-
-        cardDeck.next()
-        assertThat(cardDeck.size).isEqualTo(51)
-    }
 }
