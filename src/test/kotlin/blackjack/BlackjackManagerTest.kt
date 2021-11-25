892385f617dd034c778b8ea1fbb3d84dca5862c2
seunghwan <slflfl12@naver.com>
2021-11-25 21:29:57 +0900

    test (step2) : BlackjackManager giveInitialCards() 테스트 추가

---
+++ src/test/kotlin/blackjack/BlackjackManagerTest.kt
@@ -0,0 +1,24 @@
+package blackjack
+
+import blackjack.domain.BlackJackManager
+import blackjack.domain.CardDeck
+import blackjack.domain.Player
+import blackjack.domain.Players
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class BlackjackManagerTest {
+
+    @Test
+    fun `giveInitialCards()을 통해 cardDeck으로 부터 초기 2장의 카드를 나눠줄 수 있다`() {
+        val cardDeck = CardDeck()
+        val players = Players.of(Player("pobi"), Player("jason"))
+        val blackJackManager = BlackJackManager(players)
+
+        blackJackManager.giveInitialCards(cardDeck)
+
+        players.forEach { player ->
+            assertThat(player.cards.size).isEqualTo(2)
+        }
+    }
+}
