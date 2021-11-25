5ffe153e05f968746d3f9ff23d5376076eb253ed
flamme <flamme1004@gmail.com>
2021-11-25 23:52:47 +0900

    refactor(blackJack): BettingResult 리팩토링

--- src/main/kotlin/blackJack/domain/result/BettingResult.kt
+++ src/main/kotlin/blackJack/domain/result/BettingResult.kt
@@ -8,13 +8,18 @@ class BettingResult(val profit: Int) {
     companion object {
         fun of(player: Player, dealer: Dealer, winDrawLose: WinDrawLose): BettingResult {
             if (player.isBlackJackPlayer() && !dealer.isBlackJackPlayer()) {
-                return BettingResult((player.getBettingMoney() * 1.5).toInt())
+                return BettingResult((player.getBettingMoney() * TIMES).toInt())
             }
             return when (winDrawLose) {
-                WinDrawLose.WIN -> BettingResult(player.getBettingMoney())
-                WinDrawLose.DRAW -> BettingResult(0)
-                WinDrawLose.LOSE -> BettingResult(-player.getBettingMoney())
+                WinDrawLose.WIN -> player.getBettingMoney()
+                WinDrawLose.DRAW -> NOT_PROFIT
+                WinDrawLose.LOSE -> -player.getBettingMoney()
+            }.run {
+                BettingResult(this)
             }
         }
+
+        private const val TIMES = 1.5
+        private const val NOT_PROFIT = 0
     }
 }
