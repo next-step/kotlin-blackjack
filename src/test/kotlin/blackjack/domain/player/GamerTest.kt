f840173b58db9059a9b1dcb73c8454ba60e0fa6e
etff <wish2rich@gmail.com>
2021-11-28 23:28:47 +0900

    style: 주석을 제거하라

--- src/test/kotlin/blackjack/domain/player/GamerTest.kt
+++ src/test/kotlin/blackjack/domain/player/GamerTest.kt
@@ -57,31 +57,4 @@ internal class GamerTest {
 
         assertThat(givenGamer.isBurst()).isTrue
     }
-
-    // @Test
-    // fun `플레이어를 입력받아 게임 결과를 리턴한다`() {
-    //     val card1 = Card(Suit.HEART, Denomination.SIX)
-    //     val card2 = Card(Suit.CLUB, Denomination.NINE)
-    //     val card3 = Card(Suit.DIAMOND, Denomination.JACK)
-    //     val card4 = Card(Suit.DIAMOND, Denomination.FIVE)
-    //     val card5 = Card(Suit.DIAMOND, Denomination.QUEEN)
-    //     val card6 = Card(Suit.HEART, Denomination.QUEEN)
-    //     val card7 = Card(Suit.HEART, Denomination.EIGHT)
-    //
-    //     val profile1 = Profile.from(Name("player1"))
-    //     val profile2 = Profile.from(Name("player2"))
-    //     val profile3 = Profile.from(Name("dealer"))
-    //     val givenGamer1 = Gamer(profile1, Cards(listOf(card1, card2, card3)))
-    //     val givenGamer2 = Gamer(profile2, Cards(listOf(card4, card5)))
-    //     val dealer = Dealer(profile3, Cards(listOf(card6, card7)))
-    //     val gamers = Players(listOf(givenGamer1, givenGamer2))
-    //
-    //     val actual = dealer.judge(bettings, gamers)
-    //
-    //     Assertions.assertAll(
-    //         { assertThat(actual[dealer]).contains(Score.WIN) },
-    //         { assertThat(actual[givenGamer1]).contains(Score.LOSE) },
-    //         { assertThat(actual[givenGamer2]).contains(Score.LOSE) }
-    //     )
-    // }
 }
