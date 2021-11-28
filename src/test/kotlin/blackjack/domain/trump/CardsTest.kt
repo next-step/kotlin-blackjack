bf714499db1b92bec0b19eca5a6049260880a1a4
ssibongee <ssibongee@gmail.com>
2021-11-29 03:27:06 +0900

    test: 트럼프 카드 Fixture 확장 및 오탈자 수정

--- src/test/kotlin/blackjack/domain/trump/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/trump/CardsTest.kt
@@ -1,6 +1,6 @@
 package blackjack.domain.trump
 
-import blackjack.fixture.CardFixture.Companion.CLOB_FOUR
+import blackjack.fixture.CardFixture.Companion.CLUB_FOUR
 import blackjack.fixture.CardFixture.Companion.DIAMOND_KING
 import blackjack.fixture.CardFixture.Companion.HEART_ACE
 import blackjack.fixture.CardFixture.Companion.SPADE_THREE
@@ -14,7 +14,7 @@ class CardsTest {
     @Test
     fun `ACE를 포함한 카드덱의 점수 총합이 11점 이하면 ACE를 11점으로 계산한다`() {
         // given
-        val cards = Cards(listOf(HEART_ACE, SPADE_THREE, CLOB_FOUR))
+        val cards = Cards(listOf(HEART_ACE, SPADE_THREE, CLUB_FOUR))
         val expected = 18
 
         // when
@@ -27,7 +27,7 @@ class CardsTest {
     @Test
     fun `ACE를 포함한 카드덱의 점수 총합이 11점 초과면 ACE를 1점으로 계산한다`() {
         // given
-        val cards = Cards(listOf(HEART_ACE, SPADE_THREE, CLOB_FOUR, DIAMOND_KING))
+        val cards = Cards(listOf(HEART_ACE, SPADE_THREE, CLUB_FOUR, DIAMOND_KING))
         val expected = 18
 
         // when
@@ -40,7 +40,7 @@ class CardsTest {
     @Test
     fun `카드를 추가할 수 있다`() {
         // given
-        val before = Cards(listOf(HEART_ACE, SPADE_THREE, CLOB_FOUR))
+        val before = Cards(listOf(HEART_ACE, SPADE_THREE, CLUB_FOUR))
 
         // when
         val after = before.add(DIAMOND_KING)
@@ -48,7 +48,7 @@ class CardsTest {
         // then
         assertAll(
             { assertThat(before.values.size).isLessThan(after.values.size) },
-            { assertThat (after.values).contains(DIAMOND_KING, HEART_ACE, SPADE_THREE, CLOB_FOUR)},
+            { assertThat (after.values).contains(DIAMOND_KING, HEART_ACE, SPADE_THREE, CLUB_FOUR)},
             { assertThat (before.values).isNotIn(DIAMOND_KING)}
         )
     }
--- src/test/kotlin/blackjack/fixture/CardFixture.kt
+++ src/test/kotlin/blackjack/fixture/CardFixture.kt
@@ -10,9 +10,12 @@ class CardFixture {
     companion object {
         val HEART_ACE = Card(Pattern.HEART, Number.ACE)
         val SPADE_THREE = Card(Pattern.SPADE, Number.THREE)
-        val CLOB_FOUR = Card(Pattern.CLUB, Number.FOUR)
+        val CLUB_FOUR = Card(Pattern.CLUB, Number.FOUR)
         val DIAMOND_KING = Card(Pattern.DIAMOND, Number.KING)
+        val HEART_QUEEN = Card(Pattern.HEART, Number.QUEEN)
+        val CLUB_JACK = Card(Pattern.CLUB, Number.JACK)
 
-        val CARDS_WITH_SUM_18 = Cards(listOf(HEART_ACE, SPADE_THREE, CLOB_FOUR, DIAMOND_KING))
+        val CARDS_WITH_SUM_18 = Cards(listOf(HEART_ACE, SPADE_THREE, CLUB_FOUR, DIAMOND_KING))
+        val CARDS_WITH_SUM_30 = Cards(listOf(DIAMOND_KING, HEART_QUEEN, CLUB_JACK))
     }
 }
