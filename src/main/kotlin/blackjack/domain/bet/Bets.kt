b4dc269bc3952d51a22378f67d71c7f47eb64874
김우재 <kwj1270@naver.com>
2021-11-29 01:27:08 +0900

    feat: Bets, 이름으로 배팅 금액을 반환 받을 수 있다

--- src/main/kotlin/blackjack/domain/bet/Bets.kt
+++ src/main/kotlin/blackjack/domain/bet/Bets.kt
@@ -5,6 +5,8 @@ import blackjack.domain.player.name.Name
 @JvmInline
 value class Bets(private val bets: Map<Name, Money>) {
 
+    fun betMoney(name: Name): Money = bets.getValue(name)
+
     companion object {
         fun of(bets: Map<Name, Money>): Bets = Bets(bets.toMap())
     }
