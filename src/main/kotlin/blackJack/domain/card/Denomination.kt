26136ed8e20d1e862f7e7b44c7ccf01003fb04e4
flamme <flamme1004@gmail.com>
2021-11-30 00:46:13 +0900

    refactor(blackJack): Denomination의 score 계산을 고차함수로 변경

--- src/main/kotlin/blackJack/domain/card/Denomination.kt
+++ src/main/kotlin/blackJack/domain/card/Denomination.kt
@@ -2,51 +2,26 @@ package blackJack.domain.card
 
 import blackJack.domain.card.Signal.Companion.MAX_NUMBER
 
-enum class Denomination(val value: String, val orderBy: Int) : CardScore {
-    ACE("A", 1) {
-        override fun score(sum: Int): Int =
-            if (sum + DOWN_SCORE_ACE > MAX_NUMBER) {
-                TOP_SCORE_ACE
-            } else {
-                DOWN_SCORE_ACE
-            }
-    },
-    TWO("2", 2) {
-        override fun score(sum: Int): Int = 2
-    },
-    THREE("3", 3) {
-        override fun score(sum: Int): Int = 3
-    },
-    FOUR("4", 4) {
-        override fun score(sum: Int): Int = 4
-    },
-    FIVE("5", 5) {
-        override fun score(sum: Int): Int = 5
-    },
-    SIX("6", 6) {
-        override fun score(sum: Int): Int = 6
-    },
-    SEVEN("7", 7) {
-        override fun score(sum: Int): Int = 7
-    },
-    EIGHT("8", 8) {
-        override fun score(sum: Int): Int = 8
-    },
-    NINE("9", 9) {
-        override fun score(sum: Int): Int = 9
-    },
-    TEN("10", 10) {
-        override fun score(sum: Int): Int = 10
-    },
-    JACK("J", 11) {
-        override fun score(sum: Int): Int = 10
-    },
-    QUEEN("Q", 12) {
-        override fun score(sum: Int): Int = 10
-    },
-    KING("K", 13) {
-        override fun score(sum: Int): Int = 10
-    };
+enum class Denomination(val value: String, val orderBy: Int, val score: (sum: Int) -> Int) {
+    ACE("A", 1, {
+        if (it + DOWN_SCORE_ACE > MAX_NUMBER) {
+            TOP_SCORE_ACE
+        } else {
+            DOWN_SCORE_ACE
+        }
+    }),
+    TWO("2", 2, { 2 }),
+    THREE("3", 3, { 3 }),
+    FOUR("4", 4, { 4 }),
+    FIVE("5", 5, { 5 }),
+    SIX("6", 6, { 6 }),
+    SEVEN("7", 7, { 7 }),
+    EIGHT("8", 8, { 8 }),
+    NINE("9", 9, { 9 }),
+    TEN("10", 10, { 10 }),
+    JACK("J", 11, { 10 }),
+    QUEEN("Q", 12, { 10 }),
+    KING("K", 13, { 10 });
 
     companion object {
         private const val DOWN_SCORE_ACE = 11
