629b81d1cab4faba4aa904d06fbff78652260ec7
bangjom <kbj7227@gmail.com>
2021-12-04 16:10:22 +0900

    feat: 카드 숫자, 카드 모양 나타내는 enum 추가

---
+++ src/main/kotlin/blackjack/domain/Score.kt
@@ -0,0 +1,15 @@
+package blackjack.domain
+
+class Score private constructor(val value: Int) {
+    override fun toString(): String {
+        return "$value"
+    }
+
+    companion object {
+        private val numbers = mutableMapOf<Int, Score>()
+
+        fun of(value: Int): Score {
+            return numbers[value] ?: Score(value).also { numbers[value] = it }
+        }
+    }
+}
