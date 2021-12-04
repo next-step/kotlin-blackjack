3e86bd4bb117f37268ac95a4cda11948e416ac33
seunghwan <slflfl12@naver.com>
2021-12-05 01:46:23 +0900

    feat, refactor (step3) : Players 리팩토링, plus operator 추가

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,20 +1,42 @@
 package blackjack.domain
 
-data class Players(private val players: List<Player> = listOf()) : List<Player> by players {
+@JvmInline
+value class Players private constructor(private val _players: MutableList<Player> = mutableListOf()) {
+
+    val players: List<Player>
+        get() = _players.sortedWith(compareBy {
+            when (it) {
+                is Dealer -> -1
+                else -> 0
+            }
+        }).map { it.copy() }
+
+    val playersExceptedDealer: List<Player>
+        get() = _players.filter { it !is Dealer }.map { it.copy() }
+
+    val dealer: Dealer?
+        get() = _players.filterIsInstance(Dealer::class.java).firstOrNull()
+
+    fun isContainDealer(): Boolean {
+        return dealer != null
+    }
 
     fun eachAcceptCards(cardDeck: CardDeck) {
-        players.forEach {
+        _players.forEach {
             it.hit(cardDeck.next())
         }
     }
 
+    operator fun plus(other: Player): Players = from(_players + other)
+
     companion object {
+
         fun getPlayerListByNames(names: List<String>): List<Player> {
             return names.map { Player.of(Name.from(it)) }
         }
 
-        fun of(vararg player: Player): Players {
-            return Players(listOf(*player))
+        fun from(list: List<Player>): Players {
+            return Players(list.toMutableList())
         }
     }
 }
