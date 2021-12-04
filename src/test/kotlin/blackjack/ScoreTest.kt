cdf908e4f6739222465af90bc99f2f710b4251ec
seunghwan <slflfl12@naver.com>
2021-12-05 02:57:23 +0900

    test (step3): ScoreTest 추가

---
+++ src/test/kotlin/blackjack/ScoreTest.kt
@@ -0,0 +1,32 @@
+package blackjack
+
+import blackjack.domain.Score
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class ScoreTest {
+
+    @ParameterizedTest(name = "Score는 더할 수 있다")
+    @CsvSource(value = ["1, 2, 3", "2, 2, 4", "100, 10, 110"])
+    fun `Score는 더할 수 있다`(score1: Int, score2: Int, sum: Int) {
+        val firstScore = Score.from(score1)
+        val secondScore = Score.from(score2)
+        assertThat(firstScore + secondScore).isEqualTo(Score.from(sum))
+    }
+
+    @ParameterizedTest(name = "Score는 비교할 수 있다")
+    @CsvSource(value = ["1, 2, false", "3, 2, true", "100, 100, false"])
+    fun `Score는 비교할 수 있다`(score1: Int, score2: Int, expected: Boolean) {
+        val firstScore = Score.from(score1)
+        val secondScore = Score.from(score2)
+        assertThat(firstScore > secondScore).isEqualTo(expected)
+    }
+
+    @ParameterizedTest(name = "Score는 Bust를 알 수 있다")
+    @CsvSource(value = ["22, true", "4, false", "8, false", "32, true"])
+    fun `Score는 Bust를 알 수 있다`(score: Int, expected: Boolean) {
+        val firstScore = Score.from(score)
+        assertThat(firstScore.isBust()).isEqualTo(expected)
+    }
+}
