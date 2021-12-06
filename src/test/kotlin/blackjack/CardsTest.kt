e66952f3d155c43ec35c04bbbf13d9c9926cfc0e
seunghwan <slflfl12@naver.com>
2021-12-06 15:11:41 +0900

    test (step3): Cards getScore() 테스트 추가

--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -3,6 +3,7 @@ package blackjack
 import blackjack.domain.Card
 import blackjack.domain.Cards
 import blackjack.domain.Denomination
+import blackjack.domain.Score
 import blackjack.domain.SuitType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -27,5 +28,35 @@ class CardsTest {
         cards.addCard(Card(suitType = SuitType.SPADE, denomination = Denomination.THREE))
 
         assertThat(cards.getScore().score).isEqualTo(15)
+    }
+
+    @Test
+    fun `Cards는 ACE의 score를 이전 score와 11을 더해서 21을 넘지 않을 경우 11로 사용한다`() {
+        val cards = Cards.from(emptyList())
+
+        val tenCard = Card(SuitType.SPADE, Denomination.TEN)
+        cards.addCard(tenCard)
+
+        val aceCard = Card(SuitType.SPADE, Denomination.ACE)
+        cards.addCard(aceCard)
+
+        cards.getScore()
+        assertThat(cards.getScore()).isEqualTo(Score.from(21))
+    }
+
+    @Test
+    fun `Cards는 ACE의 score를 이전 score와 11을 더해서 21을 넘을 경우 1로 사용한다`() {
+        val cards = Cards.from(emptyList())
+
+        val tenCard = Card(SuitType.SPADE, Denomination.TEN)
+        cards.addCard(tenCard)
+
+        val twoCard = Card(SuitType.SPADE, Denomination.TWO)
+        cards.addCard(twoCard)
+
+        val aceCard = Card(SuitType.SPADE, Denomination.ACE)
+        cards.addCard(aceCard)
+
+        assertThat(cards.getScore()).isEqualTo(Score.from(13))
     }
 }
