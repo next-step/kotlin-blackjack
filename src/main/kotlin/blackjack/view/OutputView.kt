de7b9c26c7747bce9db2a158437a8c0723523d49
pursue503 <pursue503@naver.com>
2021-12-12 00:34:55 +0900

    Add: OutputView

---
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -0,0 +1,23 @@
+package blackjack.view
+
+import blackjack.entity.Player
+import blackjack.util.PlayerCardCalculation
+
+object OutputView {
+
+    private const val SET_PLAY_MESSAGE = "에게 2장의 카드를 나누었습니다."
+
+    fun printSettingPlayer(players: List<Player>) {
+        println("$players $SET_PLAY_MESSAGE")
+        println()
+        players.forEach { println("${it.name}카드 : ${it.cards}") }
+    }
+
+    fun printPlayCard(player: Player) {
+        println("${player.name}카드 : ${player.cards}")
+    }
+
+    fun gameEnd(players: List<Player>) {
+        players.forEach { println("${it.name}카드: ${it.cards} - 결과: ${PlayerCardCalculation.calculation(it)}") }
+    }
+}
