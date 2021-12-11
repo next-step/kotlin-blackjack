a80e2eb196cf9488cfdb513280bfba9d1b38870d
pursue503 <pursue503@naver.com>
2021-12-12 00:35:14 +0900

    Fix: InputView

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -8,13 +8,13 @@ object InputView {
     private const val INPUT_PLAYER = "게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리)"
     private const val HITS_MESSAGE = "는 한장의 카드를 더 받겠습니까?(예 y, 아니오는 n)"
 
-    fun setPlayer(): String {
+    fun setPlayer(): List<String> {
         println(INPUT_PLAYER)
-        return readLine() ?: throw BizException(InputError.NOT_INPUT)
+        return readLine()?.split(",") ?: throw BizException(InputError.NOT_INPUT)
     }
 
-    fun hitsCard(name: String): String {
+    fun hitsAndStay(name: String): Boolean {
         println("$name $HITS_MESSAGE")
-        return readLine() ?: throw BizException(InputError.NOT_INPUT)
+        return ("y" == readLine())
     }
 }
