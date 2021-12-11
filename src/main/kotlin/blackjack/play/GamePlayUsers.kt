c2dc327df758bdd179dbfde27adad3ec8bc7181a
pursue503 <pursue503@naver.com>
2021-12-12 00:31:56 +0900

    Add: 게임을 진행하는 Player 들을 담아둘 클래스 추가

---
+++ src/main/kotlin/blackjack/play/GamePlayUsers.kt
@@ -0,0 +1,22 @@
+package blackjack.play
+
+import blackjack.entity.Player
+
+class GamePlayUsers(
+    players: List<Player>
+) {
+
+    private val _playUsers = players
+    val playUsers get() = _playUsers.toList()
+
+    override fun toString(): String {
+        return "$playUsers"
+    }
+
+    fun playing() {
+        for (player in playUsers) {
+            Play.playing(player)
+        }
+    }
+
+}
