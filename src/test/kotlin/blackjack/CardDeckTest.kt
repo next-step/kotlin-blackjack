f780f21207da11ac13fc319d7089fa29f8db8165
seunghwan <slflfl12@naver.com>
2021-12-21 12:44:20 +0900

    test (step4): CardDeck next() test코드 추가

--- src/test/kotlin/blackjack/CardDeckTest.kt
+++ src/test/kotlin/blackjack/CardDeckTest.kt
@@ -2,8 +2,13 @@ package blackjack
 
 import blackjack.domain.card.Card
 import blackjack.domain.card.CardDeck
+import blackjack.state.CARD_HEART_ACE
+import blackjack.state.CARD_HEART_KING
+import blackjack.state.CARD_HEART_TWO
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import java.util.LinkedList
 
 class CardDeckTest {
 
@@ -12,5 +17,22 @@ class CardDeckTest {
         val cardDeck = CardDeck()
 
         assertThat(cardDeck.next()).isInstanceOf(Card::class.java)
+    }
+
+    @Test
+    fun `CardDeck에 있는 카드를 순서대로 Card를 뽑을 수 있다`() {
+        val cardDeck = CardDeck(cardQueue = LinkedList(listOf(CARD_HEART_ACE, CARD_HEART_TWO, CARD_HEART_KING)))
+
+        assertAll(
+            {
+                assertThat(cardDeck.next()).isEqualTo(CARD_HEART_ACE)
+            },
+            {
+                assertThat(cardDeck.next()).isEqualTo(CARD_HEART_TWO)
+            },
+            {
+                assertThat(cardDeck.next()).isEqualTo(CARD_HEART_KING)
+            }
+        )
     }
 }
