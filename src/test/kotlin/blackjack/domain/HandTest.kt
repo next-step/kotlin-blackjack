7ced91211801988e6b7672d6b82cb6dd88ab543c
Jake Son <sonjeabin@gmail.com>
2022-05-29 21:58:26 +0900

    test: 손패에 카드 추가하기 테스트 추가

--- src/test/kotlin/blackjack/domain/HandTest.kt
+++ src/test/kotlin/blackjack/domain/HandTest.kt
@@ -4,15 +4,23 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 internal class HandTest : FreeSpec({
+    val cards = listOf(
+        Card(Suite.CLUBS, Denomination.JACK),
+        Card(Suite.DIAMONDS, Denomination.KING)
+    )
 
     "주어진 카드를 통해 인스턴스를 생성한다" {
-        val cards = listOf(
-            Card(Suite.CLUBS, Denomination.JACK),
-            Card(Suite.DIAMONDS, Denomination.KING)
-        )
-
         val hand = Hand(cards)
 
         hand.count shouldBe cards.size
+    }
+
+    "새로운 카드를 더해 새로운 손패를 생성한다" {
+        val hand = Hand(cards)
+        val newCard = Card(Suite.HEARTS, Denomination.ACE)
+
+        val newHand = hand.addCard(newCard)
+
+        newHand.cards shouldBe cards + newCard
     }
 })
