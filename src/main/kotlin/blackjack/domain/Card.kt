10e5c56aa98bad0e8d8010e3ce6d860c8d36beaa
Jake Son <sonjeabin@gmail.com>
2022-05-30 08:37:23 +0900

    feat: 카드의 ace 여부확인 메소드 구현

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -1,3 +1,7 @@
 package blackjack.domain
 
-data class Card(val suite: Suite, val denomination: Denomination)
+data class Card(val suite: Suite, val denomination: Denomination) {
+
+    val isAce: Boolean
+        get() = denomination == Denomination.ACE
+}
--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -1,6 +1,8 @@
 package blackjack.domain
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 
 internal class CardTest : StringSpec({
@@ -10,5 +12,17 @@ internal class CardTest : StringSpec({
 
         card.suite shouldBe Suite.SPADES
         card.denomination shouldBe Denomination.ACE
+    }
+
+    "Denomination 가 Ace 이면 isAce 결과는 true 이다" {
+        val card = Card(Suite.SPADES, Denomination.ACE)
+
+        card.isAce.shouldBeTrue()
+    }
+
+    "Denomination 가 Ace 가 아니면 isAce 결과는 false 이다" {
+        val card = Card(Suite.SPADES, Denomination.TWO)
+
+        card.isAce.shouldBeFalse()
     }
 })
