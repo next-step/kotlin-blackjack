52c12a4f636022c7edacecdddcdab8bd814cadfe
Jake Son <sonjeabin@gmail.com>
2022-05-29 20:56:04 +0900

    test: 반복되는 로직 확장함수로 분리

--- src/test/kotlin/blackjack/domain/DeckTest.kt
+++ src/test/kotlin/blackjack/domain/DeckTest.kt
@@ -5,39 +5,42 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 internal class DeckTest : StringSpec({
+    val cards = listOf(
+        Card(Suite.DIAMONDS, Denomination.TEN),
+        Card(Suite.HEARTS, Denomination.FOUR),
+    )
 
     "카드 한 벌을 생성한다" {
-        val cards = listOf(
-            Card(Suite.DIAMONDS, Denomination.TEN),
-            Card(Suite.HEARTS, Denomination.FOUR),
-        )
-
         val deck = Deck(cards)
 
         deck.count shouldBe cards.size
     }
 
     "카드를 뽑을 수 있다" {
-        val cards = listOf(
-            Card(Suite.DIAMONDS, Denomination.TEN),
-            Card(Suite.HEARTS, Denomination.FOUR),
-        )
-
         val deck = Deck(cards)
 
-        val cardOne = deck.draw()
-        cardOne shouldBe cards[0]
-        val cardTwo = deck.draw()
-        cardTwo shouldBe cards[1]
+        val allCard = deck.drawAll()
+
+        allCard shouldBe cards
     }
 
     "남아있는 카드가 없는경우 뽑을 때 에러가 발생한다" {
-        val cards = listOf(Card(Suite.CLUBS, Denomination.QUEEN))
         val deck = Deck(cards)
-        deck.draw()
+        deck.drawAll()
 
         shouldThrow<IllegalStateException> {
             deck.draw()
         }
     }
 })
+
+fun Deck.drawAll(): List<Card> {
+    val cards = mutableListOf<Card>()
+
+    while (count > 0) {
+        val card = draw()
+        cards.add(card)
+    }
+
+    return cards
+}
