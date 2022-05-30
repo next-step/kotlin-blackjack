ef734045c4bc66393f638836f069f905a7405e6f
Jake Son <sonjeabin@gmail.com>
2022-05-30 09:18:23 +0900

    test: ace 가 있는 경우 점수계산 테스트 추가

--- src/test/kotlin/blackjack/domain/ScoreTest.kt
+++ src/test/kotlin/blackjack/domain/ScoreTest.kt
@@ -59,4 +59,79 @@ internal class ScoreTest : FreeSpec({
             }
         }
     }
+
+    "Ace 의 개수에 따른 점수계산" - {
+        "4개면 4점으로 계산된다" {
+            val score = Score(
+                listOf(
+                    Card(Suite.DIAMONDS, Denomination.ACE),
+                    Card(Suite.CLUBS, Denomination.ACE),
+                    Card(Suite.HEARTS, Denomination.ACE),
+                    Card(Suite.CLUBS, Denomination.ACE),
+                    Card(Suite.CLUBS, Denomination.JACK),
+                )
+            )
+            score.sum shouldBe 14
+        }
+
+        "3개면 3점으로 계산된다" {
+            val score = Score(
+                listOf(
+                    Card(Suite.DIAMONDS, Denomination.ACE),
+                    Card(Suite.CLUBS, Denomination.ACE),
+                    Card(Suite.HEARTS, Denomination.ACE),
+                    Card(Suite.CLUBS, Denomination.NINE),
+                )
+            )
+            score.sum shouldBe 12
+        }
+
+        "2개인 경우" - {
+            "2로 판단" {
+                val score = Score(
+                    listOf(
+                        Card(Suite.SPADES, Denomination.ACE),
+                        Card(Suite.HEARTS, Denomination.ACE),
+                        Card(Suite.CLUBS, Denomination.TWO),
+                        Card(Suite.HEARTS, Denomination.FIVE),
+                    )
+                )
+                score.sum shouldBe 9
+            }
+
+            "12로 판단" {
+                val score = Score(
+                    listOf(
+                        Card(Suite.SPADES, Denomination.ACE),
+                        Card(Suite.HEARTS, Denomination.ACE),
+                        Card(Suite.CLUBS, Denomination.NINE),
+                    )
+                )
+                score.sum shouldBe 21
+            }
+        }
+
+        "1개인 경우" - {
+            "1로 판단" {
+                val score = Score(
+                    listOf(
+                        Card(Suite.CLUBS, Denomination.KING),
+                        Card(Suite.HEARTS, Denomination.SIX),
+                        Card(Suite.SPADES, Denomination.ACE),
+                    )
+                )
+                score.sum shouldBe 17
+            }
+
+            "11로 판단" {
+                val score = Score(
+                    listOf(
+                        Card(Suite.DIAMONDS, Denomination.ACE),
+                        Card(Suite.CLUBS, Denomination.TEN),
+                    )
+                )
+                score.sum shouldBe 21
+            }
+        }
+    }
 })
