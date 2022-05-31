ad6862829105aa71b003340e4b891bf42e76e479
larry.p <larry.p@kakaocorp.com>
2022-05-31 23:15:53 +0900

    chore: 테스트 코드 정리

--- src/test/kotlin/dsl/ResumeTest.kt
+++ src/test/kotlin/dsl/ResumeTest.kt
@@ -7,9 +7,11 @@ class ResumeTest : StringSpec({
 
     "내 이름을 포함한 이력서를 만든다" {
         val expected = "김해중"
+
         val resume: Resume = introduce {
-            name(expected)
+            name("김해중")
         }
+
         resume.name shouldBe expected
     }
 
