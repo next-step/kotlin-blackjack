b2c48b4e9b92ccc80b34f15b307a8b9ba4a8fa1d
larry.p <larry.p@kakaocorp.com>
2022-05-31 23:51:44 +0900

    test: 이력서에 언어 능력 레벨이 있는 테스트 추가

--- src/test/kotlin/dsl/ResumeTest.kt
+++ src/test/kotlin/dsl/ResumeTest.kt
@@ -50,4 +50,34 @@ class ResumeTest : StringSpec({
         resume.skills.soft shouldContainExactly expectedSoftSkills
         resume.skills.hard shouldContainExactly expectedHardSkills
     }
+
+    "내 이름, 회사, 기술, 언어를 포함한 이력서를 만든다" {
+        val expectedName = "김해중"
+        val expectedCompany = "오리백숙집"
+        val expectedSoftSkills = listOf("잠자기", "숨시기")
+        val expectedHardSkills = listOf("먹기")
+        val expectedKoreanLevel = "한국어" to 5
+        val expectedEnglishLeve = "영어" to -3
+
+        val resume: Resume = introduce {
+            name("김해중")
+            company("오리백숙집")
+            skills {
+                soft("잠자기")
+                soft("숨시기")
+                hard("먹기")
+            }
+            languages {
+                "한국어" level 5
+                "영어" level -3
+            }
+        }
+
+        resume.name shouldBe expectedName
+        resume.company shouldBe expectedCompany
+        resume.skills.soft shouldContainExactly expectedSoftSkills
+        resume.skills.hard shouldContainExactly expectedHardSkills
+        resume.languages["한국어"] shouldBe 5
+        resume.languages["영어"] shouldBe -3
+    }
 })
