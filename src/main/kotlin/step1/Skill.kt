cba71c6b30894ecd33a72c639f64b38787f41e7d
SeokHo-Im <questknight26@gmail.com>
2022-05-31 23:18:03 +0900

    feat : Dsl 학습 (#177)

---
+++ src/main/kotlin/step1/Skill.kt
@@ -0,0 +1,21 @@
+package step1
+
+data class Skill(val name: String, val type: SkillType)
+
+enum class SkillType {
+    Soft, Hard
+}
+
+class SkillBuilder {
+    private val skills: MutableList<Skill> = mutableListOf()
+
+    fun soft(name: String) {
+        skills.add(Skill(name, SkillType.Soft))
+    }
+
+    fun hard(name: String) {
+        skills.add(Skill(name, SkillType.Hard))
+    }
+
+    fun build(): List<Skill> = skills.toList()
+}
