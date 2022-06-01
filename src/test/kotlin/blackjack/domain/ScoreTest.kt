9e346fb7f93788be3ba479deb71db69165037305
dongchul <sorkehdcjf@gmail.com>
2022-06-01 14:19:13 +0900

    test: Card, Score 테스트

---
+++ src/test/kotlin/blackjack/domain/ScoreTest.kt
@@ -0,0 +1,15 @@
+package blackjack.domain
+
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+
+class ScoreTest : StringSpec({
+    "점수 객체를 생성할 수 있다." {
+        shouldNotThrow<Throwable> { Score(1) }
+    }
+
+    "1 미만의 점수는 생성할수 없다." {
+        shouldThrow<IllegalArgumentException> { Score(0) }
+    }
+})
---
+++ src/test/kotlin/blackjack/domain/card/CardTest.kt
@@ -0,0 +1,12 @@
+package blackjack.domain.card
+
+import blackjack.domain.card.type.Denomination
+import blackjack.domain.card.type.Suit
+import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.core.spec.style.StringSpec
+
+class CardTest : StringSpec({
+    "카드 객체를 생성할수 있다." {
+        shouldNotThrow<Throwable> { Card(Denomination.ACE, Suit.CLUB) }
+    }
+})
