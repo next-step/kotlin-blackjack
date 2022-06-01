31e7439ef6409d240dbd9c6cd18136cd1dbabb44
leeyohan93 <good_1411@naver.com>
2022-06-01 14:53:55 +0900

    test: 이력서 전체 테스트 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -5,26 +5,40 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-/*
-introduce {
-  name("이요한")
-  company("카카오")
-  skills {
-    soft("A passion for problem solving")
-    soft("Good communication skills")
-    hard("Kotlin")
-  }
-  languages {
-    "Korean" level 5
-    "English" level 3
-  }
-}
- */
 class DslTest {
+
+    @Test
+    fun introduce() {
+        val resume = introduce {
+            name("이요한")
+            company("카카오")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+
+        assertThat(resume.name).isEqualTo("이요한")
+        assertThat(resume.company).isEqualTo("카카오")
+        assertThat(resume.skills).containsExactly(
+            Skill("A passion for problem solving", Skill.SkillType.SOFT),
+            Skill("Good communication skills", Skill.SkillType.SOFT),
+            Skill("Kotlin", Skill.SkillType.HARD),
+        )
+        assertThat(resume.languages).containsExactly(
+            Language("Korean", 5),
+            Language("English", 3),
+        )
+    }
 
     @ValueSource(strings = ["이요한", "yohan"])
     @ParameterizedTest
-    fun introduce(value: String) {
+    fun name(value: String) {
         val resume = introduce {
             name(value)
         }
