28fc2cd728d92fb41942c2384fce4780c5468523
dongchul <sorkehdcjf@gmail.com>
2022-06-03 00:24:28 +0900

    feat: 플레이어 Stay 구현 및 테스트 코드 작성

--- src/main/kotlin/blackjack/domain/player/Player.kt
+++ src/main/kotlin/blackjack/domain/player/Player.kt
@@ -1,14 +1,26 @@
 package blackjack.domain.player
 
+import blackjack.domain.Score
 import blackjack.domain.card.CardDeck
 
 class Player private constructor(
     val name: Name,
-    private val cardsInHand: CardsInHand
+    val cardsInHand: CardsInHand
 ) {
+    var isStay: Boolean = false
+        get() = field || cardsInHand.calculateScore().isGreaterThan(Score.BLACK_JACK)
 
     fun hit(cardDeck: CardDeck) {
+        require(!isStay) { "Stay를 선언하였다면 카드를 뽑을수 없습니다." }
+        require(cardsInHand.calculateScore().isLess(Score.BLACK_JACK)) {
+            "현재 점수가 ${Score.BLACK_JACK.value} 보다 크거나 같으면 카드를 뽑지 못합니다."
+        }
+
         cardsInHand.add(cardDeck.draw())
+    }
+
+    fun stay() {
+        isStay = true
     }
 
     companion object {
--- src/test/kotlin/blackjack/domain/player/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/player/PlayerTest.kt
@@ -1,10 +1,63 @@
 package blackjack.domain.player
 
+import blackjack.domain.card.Card
+import blackjack.domain.card.CardDeckTest
+import blackjack.domain.card.type.Ace
+import blackjack.domain.card.type.Suit
+import blackjack.domain.card.type.Ten
 import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
 
 class PlayerTest : StringSpec({
     "참가자 객체를 생성할수 있다." {
         shouldNotThrow<Throwable> { Player.sit(Name("dean")) }
     }
-})
+
+    "Stay 상태로 변경할수 있다" {
+        val player = player()
+
+        player.stay()
+
+        player.isStay shouldBe true
+    }
+
+    "카드를 뽑을수 있다." {
+        val player = player()
+        val cardDeck = CardDeckTest.cardDeck()
+
+        player.hit(cardDeck)
+
+        player.cardsInHand.cards.size shouldBe 1
+    }
+
+    "Stay 선언시 카드를 뽑는 경우 Exception을 던진다." {
+        val player = player()
+        val cardDeck = CardDeckTest.cardDeck()
+
+        player.stay()
+
+        shouldThrow<IllegalArgumentException> { player.hit(cardDeck) }
+    }
+
+    "플레이어의 점수가 21보다 크거나 같을때 카드를 뽑으면 Exception을 던진다." {
+        val player = player()
+        val cardDeck = CardDeckTest.cardDeck()
+
+        val values = listOf(
+            listOf(Card(Ten(), Suit.DIAMOND), Card(Ten(), Suit.DIAMOND), Card(Ten(), Suit.DIAMOND)),
+            listOf(Card(Ten(), Suit.DIAMOND), Card(Ten(), Suit.DIAMOND), Card(Ace(), Suit.DIAMOND))
+        )
+
+        values.forAll { cards ->
+            cards.forEach { player.cardsInHand.add(it) }
+            shouldThrow<IllegalArgumentException> { player.hit(cardDeck) }
+        }
+    }
+}) {
+    companion object {
+        private fun player() = Player.sit(Name("dean"))
+    }
+}
