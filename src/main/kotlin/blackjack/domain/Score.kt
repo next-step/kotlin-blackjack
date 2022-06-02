69c307f4351ed3354dd3e4117240fdcff217e297
dongchul <sorkehdcjf@gmail.com>
2022-06-03 00:10:29 +0900

    feat: 점수 비교 로직 추가

--- src/main/kotlin/blackjack/domain/Score.kt
+++ src/main/kotlin/blackjack/domain/Score.kt
@@ -10,9 +10,14 @@ value class Score(
         require(value > MIN_VALUE) { "점수는 $MIN_VALUE 보다 커야합니다." }
     }
 
+    fun isGreaterThan(other: Score): Boolean = value >= other.value
+
     fun isLessThan(other: Score): Boolean = value <= other.value
 
+    fun isLess(other: Score): Boolean = value < other.value
+
     operator fun plus(other: Score): Score = Score(value.plus(other.value))
+
 
     companion object {
         val BLACK_JACK = Score(21)
--- src/test/kotlin/blackjack/domain/ScoreTest.kt
+++ src/test/kotlin/blackjack/domain/ScoreTest.kt
@@ -3,6 +3,8 @@ package blackjack.domain
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
 
 class ScoreTest : StringSpec({
     "점수 객체를 생성할 수 있다." {
@@ -12,4 +14,34 @@ class ScoreTest : StringSpec({
     "음수 점수는 생성할수 없다." {
         shouldThrow<IllegalArgumentException> { Score(-1) }
     }
-})
+
+    "점수가 크거나 같음을 비교할수 있다." {
+        val values = listOf(NINE, EIGHT)
+        values.forAll {
+            NINE.isGreaterThan(it) shouldBe true
+        }
+        NINE.isGreaterThan(TEN) shouldBe false
+    }
+
+    "점수가 작거나 같음을 비교할수 있다." {
+        val values = listOf(NINE, TEN)
+        values.forAll {
+            NINE.isLessThan(it) shouldBe true
+        }
+        NINE.isLessThan(EIGHT) shouldBe false
+    }
+
+    "점수가 작음을 비교할수 있다." {
+        val values = listOf(NINE, EIGHT)
+        values.forAll {
+            NINE.isLess(it) shouldBe false
+        }
+        NINE.isLessThan(TEN) shouldBe true
+    }
+}) {
+    companion object {
+        val TEN = Score(10)
+        val NINE = Score(9)
+        val EIGHT = Score(8)
+    }
+}
