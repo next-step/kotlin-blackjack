cbc7f61620e0803007ddae2890a8ff676970023f
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-06-02 22:35:38 +0900

    refactor: 모든카드 만들기 함수분리

--- src/main/kotlin/blackjack/Dealer.kt
+++ src/main/kotlin/blackjack/Dealer.kt
@@ -1,18 +1,23 @@
 package blackjack
 
 object Dealer {
-    private val cards = mutableListOf<Card>().apply {
-        val numbers = CardNumber.values()
-        val shapes = CardShape.values()
-
-        numbers.forEach { number ->
-            shapes.forEach { shape ->
-                add(Card(number, shape))
-            }
-        }
-    }.shuffled().toMutableList()
+    private val cards = makeAllCards().toMutableList()
 
     fun popOneCard(): Card {
         return cards.removeLast()
+    }
+
+    private fun makeAllCards(): List<Card> {
+        val numbers = CardNumber.values()
+        val shapes = CardShape.values()
+        val allCards = mutableListOf<Card>()
+
+        numbers.forEach { number ->
+            shapes.forEach { shape ->
+                allCards.add(Card(number, shape))
+            }
+        }
+
+        return allCards
     }
 }
