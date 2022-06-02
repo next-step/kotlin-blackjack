7037a2b6f26d6c5b67e1ed757fe799e6ebf3c807
Jungyu Choi <devwithpug@kakao.com>
2022-06-02 21:43:47 +0900

    step1 - Kotlin DSL (#180)
    * feat: step1 - 코틀린 DSL
    
    * chore: 테스트 파일 제거
    
    * feat: step1 - assert 구문 추가
    
    * refactor: 불필요한 주석 제거
    
    * refactor: 코드 정리
    
    Co-authored-by: pug <pug.gg@kakaocorp.com>

---
+++ src/main/kotlin/step1/language/LanguagesBuilder.kt
@@ -0,0 +1,11 @@
+package step1.language
+
+class LanguagesBuilder {
+    private val languages = mutableListOf<Language>()
+
+    infix fun String.level(level: Int) {
+        languages.add(Language(this, level))
+    }
+
+    fun build() = Languages(languages)
+}
