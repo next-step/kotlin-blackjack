a73a981a81d18bfa48c0853122cb6dbe30843830
pug <pug.gg@kakaocorp.com>
2022-06-03 20:44:40 +0900

    test: Player, Hand 테스트 케이스 생성

---
+++ src/test/kotlin/blackjack/player/HandTest.kt
@@ -0,0 +1,16 @@
+package blackjack.player
+
+import org.junit.jupiter.api.Test
+
+internal class HandTest {
+
+    @Test
+    fun `플레이어의 핸드에 카드를 추가할 수 있다`() {
+        TODO()
+    }
+
+    @Test
+    fun `플레이어의 핸드에 있는 카드의 총 점수를 계산할 수 있다`() {
+        TODO()
+    }
+}
--- src/test/kotlin/blackjack/player/PlayerTest.kt
+++ src/test/kotlin/blackjack/player/PlayerTest.kt
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test
 internal class PlayerTest {
 
     @Test
-    fun `플레이어는 자신의 패를 가질 수 있다`() {
+    fun `플레이어는 카드의 점수가 21을 초과하면 BUST 된다`() {
         TODO()
     }
 
+    @Test
+    fun `플레이어는 카드의 점수가 21을 초과하지 않으면 HIT 또는 STAND 상태를 가질 수 있다`() {
+        TODO()
+    }
 }
