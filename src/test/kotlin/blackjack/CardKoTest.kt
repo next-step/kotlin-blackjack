abb99b89d8a1e04be29292a64b02b8363db1d44d
d000l <questknight26@gmail.com>
2022-06-03 15:18:33 +0900

    feat : 범위 밖의 카드 생성시 예외 처리

--- src/test/kotlin/blackjack/CardKoTest.kt
+++ src/test/kotlin/blackjack/CardKoTest.kt
@@ -1,9 +1,9 @@
 package blackjack
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.inspectors.forAll
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNot
 import io.kotest.matchers.shouldNotBe
 
 class CardKoTest : DescribeSpec({
@@ -28,9 +28,33 @@ class CardKoTest : DescribeSpec({
                 }
             }
         }
+
+        context("허용하지 않는 문자가 들어오면") {
+            it("IllegalArgumentException 예외가 발생한다.") {
+                listOf(
+                    Symbol.Club to 1,
+                    Symbol.Heart to 11,
+                    Symbol.Spade to 20
+                ).forAll { (symbol, number) ->
+                    shouldThrow<IllegalArgumentException> {
+                        NumberCard(symbol, number)
+                    }
+                }
+
+                listOf(
+                    Symbol.Club to 'T',
+                    Symbol.Diamond to 'F',
+                    Symbol.Spade to 'O'
+                ).forAll { (symbol, number) ->
+                    shouldThrow<IllegalArgumentException> {
+                        AlphabetCard(symbol, number)
+                    }
+                }
+            }
+        }
     }
 
-    describe("score 함수"){
+    describe("score 함수") {
         context("NumberCard 인 경우") {
             it("숫자를 점수로 리턴한다") {
                 (2..10).forEach {
