840c9719a6febf42346e4a78131e6a9dd4bda1e5
jngcii <concotree@gmail.com>
2022-06-03 18:38:23 +0900

    feat : 이름리스트가 2개가 아니면 null 반환 구현

---
+++ src/main/kotlin/com/nextstep/jngcii/blackjack/view/InputValidator.kt
@@ -0,0 +1,17 @@
+package com.nextstep.jngcii.blackjack.view
+
+object InputValidator {
+    private const val COMMA = ","
+
+    fun parseNames(input: String?): List<String>? {
+        if (input == null) return null
+
+        val names = input
+            .split(COMMA)
+            .map { it.trim() }
+
+        if (names.size != 2) return null
+
+        return names
+    }
+}
