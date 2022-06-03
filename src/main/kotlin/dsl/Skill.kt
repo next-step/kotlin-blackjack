cfff0d8e64db4b5eb98cc27a3de032cbf5c56195
justin.sg <justin.sg@kakaocorp.com>
2022-06-03 15:04:35 +0900

    refactor : Skills backing property 적용

--- src/main/kotlin/dsl/Skill.kt
+++ src/main/kotlin/dsl/Skill.kt
@@ -1,8 +1,22 @@
 package dsl
 
-data class Skills(
-    val soft: List<Skill> = emptyList(),
-    val hard: List<Skill> = emptyList()
-)
+class Skills(
+    soft: List<Skill> = emptyList(),
+    hard: List<Skill> = emptyList()
+) {
+    private val _soft = soft.toMutableList()
+    val soft: List<Skill> get() = _soft.toList()
+
+    private val _hard = hard.toMutableList()
+    val hard: List<Skill> get() = _hard.toList()
+
+    fun soft(value: Skill) {
+        _soft.add(value)
+    }
+
+    fun hard(value: Skill) {
+        _hard.add(value)
+    }
+}
 
 data class Skill(val title: String)
--- src/main/kotlin/dsl/SkillsBuilder.kt
+++ src/main/kotlin/dsl/SkillsBuilder.kt
@@ -1,21 +1,17 @@
 package dsl
 
 class SkillsBuilder {
-    private val soft: MutableList<Skill> = mutableListOf()
-    private val hard: MutableList<Skill> = mutableListOf()
+    private val skills = Skills()
 
     fun soft(value: String) {
-        soft.add(Skill(value))
+        skills.soft(Skill(value))
     }
 
     fun hard(value: String) {
-        hard.add(Skill(value))
+        skills.hard(Skill(value))
     }
 
     fun build(): Skills {
-        return Skills(
-            soft = soft.toList(),
-            hard = hard.toList()
-        )
+        return skills
     }
 }
