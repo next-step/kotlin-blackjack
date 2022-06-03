95feb65749c6dd8b09b2d31786f1769091dea7e0
jngcii <concotree@gmail.com>
2022-06-03 19:16:46 +0900

    test : true를 전달받고 할당받은 카드 총합이 21이 넘으면 false 반환 테스트

--- src/test/kotlin/com/nextstep/jngcii/blackjack/domain/GameRunnerTest.kt
+++ src/test/kotlin/com/nextstep/jngcii/blackjack/domain/GameRunnerTest.kt
@@ -29,4 +29,20 @@ class GameRunnerTest {
         assertThat(result).isFalse
         assertThat(player.cards.size).isEqualTo(0)
     }
+
+    @Test
+    fun `true를 전달받은 후 유저의 할당 총합이 21이 넘으면 false를 반환`() {
+        val deck = CardDeck()
+        val player = PlayerBoard("peter")
+
+        player.addCard(Card(Card.Shape.SPADE, Card.SYMBOL.TEN))
+        player.addCard(Card(Card.Shape.CLOVER, Card.SYMBOL.ACE))
+        assertThat(player.cards.size).isEqualTo(2)
+        assertThat(player.total).isEqualTo(21)
+
+        val result: Boolean = GameRunner.run(deck, player, true)
+
+        assertThat(result).isFalse
+        assertThat(player.cards.size).isEqualTo(3)
+    }
 }
