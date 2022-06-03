1e91f724ae01e75b00c1206b4b40f172fd37014a
jngcii <concotree@gmail.com>
2022-06-03 18:03:33 +0900

    feat : PlayerBoard 카드추가 기능 구현

--- src/main/kotlin/com/nextstep/jngcii/blackjack/domain/PlayerBoard.kt
+++ src/main/kotlin/com/nextstep/jngcii/blackjack/domain/PlayerBoard.kt
@@ -3,4 +3,11 @@ package com.nextstep.jngcii.blackjack.domain
 class PlayerBoard {
     private val _cards = mutableListOf<Card>()
     val cards get() = _cards.toList()
+
+    fun addCard(card: Card) {
+        if (_cards.contains(card)) {
+            throw IllegalArgumentException("이미 존재하는 카드입니다")
+        }
+        _cards.add(card)
+    }
 }
