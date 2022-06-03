75dfa09fc60f419d094b27d20113af75be6346ab
d000l <questknight26@gmail.com>
2022-06-03 15:13:33 +0900

    feat : NumberCard score 함수 정의

--- src/main/kotlin/blackjack/Card.kt
+++ src/main/kotlin/blackjack/Card.kt
@@ -2,7 +2,15 @@ package blackjack
 
 interface Card {
     val symbol: Symbol
+    fun score(): Int
 }
 
-data class NumberCard(override val symbol: Symbol, val number: Int) : Card
-data class AlphabetCard(override val symbol: Symbol, val alphabet: Char) : Card
+data class NumberCard(override val symbol: Symbol, val number: Int) : Card {
+    override fun score(): Int = number
+}
+
+data class AlphabetCard(override val symbol: Symbol, val alphabet: Char) : Card {
+    override fun score(): Int {
+        TODO("Not yet implemented")
+    }
+}
--- src/test/kotlin/blackjack/CardKoTest.kt
+++ src/test/kotlin/blackjack/CardKoTest.kt
@@ -2,6 +2,9 @@ package blackjack
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNot
+import io.kotest.matchers.shouldNotBe
 
 class CardKoTest : DescribeSpec({
 
@@ -13,7 +16,7 @@ class CardKoTest : DescribeSpec({
                     Symbol.Heart to 2,
                     Symbol.Spade to 10
                 ).forAll { (symbol, number) ->
-                    NumberCard(symbol, number)
+                    NumberCard(symbol, number) shouldNotBe null
                 }
 
                 listOf(
@@ -21,7 +24,17 @@ class CardKoTest : DescribeSpec({
                     Symbol.Diamond to 'K',
                     Symbol.Spade to 'J'
                 ).forAll { (symbol, number) ->
-                    AlphabetCard(symbol, number)
+                    AlphabetCard(symbol, number) shouldNotBe null
+                }
+            }
+        }
+    }
+
+    describe("score 함수"){
+        context("NumberCard 인 경우") {
+            it("숫자를 점수로 리턴한다") {
+                (2..10).forEach {
+                    NumberCard(Symbol.Heart, it).score() shouldBe it
                 }
             }
         }
