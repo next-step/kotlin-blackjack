632f83b495d2e4803c35fcd1d5e2fab62b7d32ad
pug <pug.gg@kakaocorp.com>
2022-06-03 17:04:05 +0900

    test: 테스트 케이스 추가 및 수정

--- src/test/kotlin/blackjack/GameTest.kt
+++ src/test/kotlin/blackjack/GameTest.kt
@@ -1,26 +1,8 @@
 package blackjack
 
-import blackjack.game.Game
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class GameTest {
-
-    private val game = Game()
-
-    @Test
-    fun `플레이어는 (,)로 구분하여 여러명이 게임에 참여할 수 있다`() {
-        // given
-        val playerRequest = "pug,jason,pobi"
-
-        // when
-        val result = game.enrollPlayers(playerRequest)
-
-        // then
-        assertThat(result.players).hasSize(3)
-        assertThat(result.players).extracting("name")
-            .containsExactly("pug", "jason", "pobi")
-    }
 
     @Test
     fun `카드 숫자의 합이 21을 초과하면 패배한다`() {
---
+++ src/test/kotlin/blackjack/player/PlayerTest.kt
@@ -0,0 +1,12 @@
+package blackjack.player
+
+import org.junit.jupiter.api.Test
+
+internal class PlayerTest {
+
+    @Test
+    fun `플레이어는 자신의 패를 가질 수 있다`() {
+        TODO()
+    }
+
+}
