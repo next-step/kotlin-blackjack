b6a428ed71cdf3efea7244b7578458392048af91
jngcii <concotree@gmail.com>
2022-06-03 18:01:32 +0900

    test : PlayerBoard 카드 추가 테스트 작성

--- src/test/kotlin/com/nextstep/jngcii/blackjack/domain/PlayerBoardTest.kt
+++ src/test/kotlin/com/nextstep/jngcii/blackjack/domain/PlayerBoardTest.kt
@@ -2,6 +2,7 @@ package com.nextstep.jngcii.blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class PlayerBoardTest {
     @Test
@@ -9,5 +10,33 @@ class PlayerBoardTest {
         val cards: List<Card> = PlayerBoard().cards
 
         assertThat(cards).isEmpty()
+    }
+
+    @Test
+    fun `같은 카드가 들어오면 예외를 발생한다`() {
+        val playerBoard = PlayerBoard()
+
+        val spadeTwo = Card(Card.Shape.SPADE, Card.SYMBOL.TWO)
+
+        playerBoard.addCard(spadeTwo)
+
+        assertThrows<IllegalArgumentException>("이미 존재하는 카드입니다") {
+            playerBoard.addCard(spadeTwo)
+        }
+    }
+
+    @Test
+    fun `들어온 카드 확인`() {
+        val playerBoard = PlayerBoard()
+
+        val cards = listOf(
+            Card(Card.Shape.SPADE, Card.SYMBOL.TWO),
+            Card(Card.Shape.CLOVER, Card.SYMBOL.TEN),
+            Card(Card.Shape.HEART, Card.SYMBOL.ACE)
+        )
+
+        cards.forEach { playerBoard.addCard(it) }
+
+        assertThat(playerBoard.cards).isEqualTo(cards)
     }
 }
