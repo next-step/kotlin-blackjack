5456305f499e60a4f58aa2654f16eb0311540fc7
justin.sg <justin.sg@kakaocorp.com>
2022-06-03 14:30:15 +0900

    test : Person은 name 정보를 갖으며 DSL로 생성한다

--- src/test/kotlin/dsl/PersonTest.kt
+++ src/test/kotlin/dsl/PersonTest.kt
@@ -10,4 +10,13 @@ internal class PersonTest {
         val person = Person()
         assertThat(person).isNotNull
     }
+
+    @Test
+    fun `Person은 name 정보를 갖으며 DSL로 생성한다`() {
+        val person: Person = introduce {
+            name = "김승갑"
+        }
+
+        assertThat(person.name).isEqualTo("김승갑")
+    }
 }
