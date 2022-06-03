201f3af6a1697345b6b9cb157df0f8bf131f8d8f
jngcii <concotree@gmail.com>
2022-06-03 19:19:14 +0900

    feat : true를 전달받고 할당받은 카드 총합이 21이 넘으면 false 반환 구현

--- src/main/kotlin/com/nextstep/jngcii/blackjack/domain/GameRunner.kt
+++ src/main/kotlin/com/nextstep/jngcii/blackjack/domain/GameRunner.kt
@@ -1,10 +1,14 @@
 package com.nextstep.jngcii.blackjack.domain
 
 object GameRunner {
+    private const val LIMIT = 21
+
     fun run(deck: CardDeck, player: PlayerBoard, moreable: Boolean): Boolean {
         if (!moreable) return false
 
         player.addCard(deck.pop())
+
+        if (player.total > LIMIT) return false
 
         return true
     }
