26ade9e02ccbe4845faf7dc99d7e5f74bd8d4f6a
Jake Son <sonjeabin@gmail.com>
2022-06-04 15:16:43 +0900

    feat: 카드추가 수락여부 로직구현

--- src/main/kotlin/blackjack/view/GameView.kt
+++ src/main/kotlin/blackjack/view/GameView.kt
@@ -14,9 +14,22 @@ class GameView(
         io.print("")
     }
 
-    private fun play(player: Player) {
+    private tailrec fun play(player: Player) {
         if (!dealer.checkCardDrawable(player)) {
             return
         }
+
+        val shouldGiveCard = getDrawChoice(player.name)
+        if (shouldGiveCard) {
+            dealer.giveCard(player)
+            io.print(player.text())
+            play(player)
+        }
+    }
+
+    private fun getDrawChoice(name: String): Boolean {
+        io.print("${name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+
+        return io.read().lowercase().trim() == "y"
     }
 }
