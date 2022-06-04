fa461f5094dbd0b5c953041233365a0cbc68a7a1
Jake Son <sonjeabin@gmail.com>
2022-06-04 15:28:19 +0900

    test: 게임 결과 view 테스트 추가

---
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -0,0 +1,13 @@
+package blackjack.view
+
+import blackjack.domain.Player
+
+class ResultView(
+    private val io: IO,
+    private val players: List<Player>,
+) {
+
+    fun run() {
+        TODO()
+    }
+}
--- src/test/kotlin/blackjack/view/ResultViewTest.kt
+++ src/test/kotlin/blackjack/view/ResultViewTest.kt
@@ -6,31 +6,30 @@ import blackjack.domain.Suite
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-internal class PlayerViewTest : StringSpec({
+class ResultViewTest : StringSpec({
 
-    "참가자 현황을 출력한다" {
+    "게임 결과를 출력한다" {
         val io = StubIO()
         val players = listOf(
             createPlayer(
                 "pobi",
                 Card(Suite.HEARTS, Denomination.TWO),
                 Card(Suite.SPADES, Denomination.EIGHT),
+                Card(Suite.CLUBS, Denomination.ACE),
             ),
             createPlayer(
                 "jason",
                 Card(Suite.CLUBS, Denomination.SEVEN),
                 Card(Suite.SPADES, Denomination.KING),
-            ),
+            )
         )
-        val playerView = PlayerView(io, players)
+        val resultView = ResultView(io, players)
 
-        playerView.run()
+        resultView.run()
 
         io.printed shouldBe listOf(
-            "pobi, jason에게 2장의 나누었습니다.",
-            "pobi카드: 2하트, 8스페이드",
-            "jason카드: 7클로버, K스페이드",
-            "",
+            "pobi카드: 2하트, 8스페이드, A클로버 - 결과: 21",
+            "jason카드: 7클로버, K스페이드 - 결과: 17",
         )
     }
 })
