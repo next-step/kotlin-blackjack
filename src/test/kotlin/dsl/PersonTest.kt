0ff184a24cc145f62e290536ce1968dd7ae724f2
YooInKeun <keun0390@naver.com>
2022-06-04 20:18:12 +0900

    test: person 소개 테스트 추가

--- src/test/kotlin/dsl/PersonTest.kt
+++ src/test/kotlin/dsl/PersonTest.kt
@@ -59,6 +59,35 @@ class PersonTest {
         assertEquals(person.languages[1], Language("English", 3))
     }
 
+    @Test
+    fun introduceTest() {
+        // given, when
+        val person = introduce {
+            name("유인근")
+            company("카카오")
+            skills {
+                soft("Good communication skills")
+                soft("Documentation")
+                hard("Programming")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+
+        // then
+        assertEquals(person.name, "유인근")
+        assertEquals(person.company, "카카오")
+
+        assertEquals(person.skills[0], Skill.Soft("Good communication skills"))
+        assertEquals(person.skills[1], Skill.Soft("Documentation"))
+        assertEquals(person.skills[2], Skill.Hard("Programming"))
+
+        assertEquals(person.languages[0], Language("Korean", 5))
+        assertEquals(person.languages[1], Language("English", 3))
+    }
+
     private fun introduce(block: PersonBuilder.() -> Unit): Person {
         return PersonBuilder().apply(block).build()
     }
