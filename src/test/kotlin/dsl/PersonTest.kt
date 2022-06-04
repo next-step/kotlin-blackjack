9d338866184c4f78b10afe583381165851282caa
YooInKeun <keun0390@naver.com>
2022-06-04 20:13:15 +0900

    test: 회사 테스트 추가

--- src/test/kotlin/dsl/PersonTest.kt
+++ src/test/kotlin/dsl/PersonTest.kt
@@ -14,6 +14,17 @@ class PersonTest {
         assertEquals(person.name, "유인근")
     }
 
+    @Test
+    fun companyTest() {
+        val person = introduce {
+            name("유인근")
+            company("카카오")
+        }
+
+        assertEquals(person.name, "유인근")
+        assertEquals(person.company, "카카오")
+    }
+
     private fun introduce(block: PersonBuilder.() -> Unit): Person {
         return PersonBuilder().apply(block).build()
     }
