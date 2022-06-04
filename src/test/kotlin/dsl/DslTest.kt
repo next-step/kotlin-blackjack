aab1cff8e48624a90e6cb76e054d387ee0801e80
hyeon9mak <jinha3507@gmail.com>
2022-06-04 17:59:07 +0900

    refactor: 불필요한 주석 제거

--- src/test/kotlin/dsl/DslTest.kt
+++ src/test/kotlin/dsl/DslTest.kt
@@ -3,22 +3,6 @@ package dsl
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-/*
-introduce {
-  name("박재성")
-  company("우아한형제들")
-  skills {
-    soft("A passion for problem solving")
-    soft("Good communication skills")
-    hard("Kotlin")
-  }
-  languages {
-    "Korean" level 5
-    "English" level 3
-  }
-}
- */
-
 class DslTest {
 
     @Test
