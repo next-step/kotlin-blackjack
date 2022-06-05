a1015536bd43e997d73541eb6a1c317d094c0835
molly <molly.d@kakaocorp.com>
2022-06-05 23:38:37 +0900

    fix: 리뷰반영 - 테스트 명칭 수정

--- src/test/kotlin/blackjack/domain/ParticipantTest.kt
+++ src/test/kotlin/blackjack/domain/ParticipantTest.kt
@@ -8,7 +8,7 @@ class ParticipantTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["molly,jayce"])
-    fun `참가자 생성 테스트`(input: String) {
+    fun `참가자는 이름을 가진다`(input: String) {
         val players = input.split(",")
         val participants = players.map { Participant.of(it, CardDeck(emptyList())) }
         assertThat(participants.count()).isEqualTo(2)
@@ -16,14 +16,14 @@ class ParticipantTest {
 
     @ParameterizedTest
     @ValueSource(strings = ["molly"])
-    fun `스코어 테스트`(input: String) {
+    fun `참가자 최초 생성 시 스코어는 0점`(input: String) {
         val participant = Participant.of(input, CardDeck(emptyList()))
         assertThat(participant.score()).isEqualTo(0)
     }
 
     @ParameterizedTest
     @ValueSource(strings = ["molly"])
-    fun `카드 갯수 테스트`(input: String) {
+    fun `참가자 생성 직구 카드 갯수 0개`(input: String) {
         val participant = Participant.of(input, CardDeck(emptyList()))
         assertThat(participant.cards.count()).isEqualTo(0)
     }
