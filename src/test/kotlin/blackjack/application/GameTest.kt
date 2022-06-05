602ed8d0fedf56c1b334abdc4514edefaae51169
qyu.st <qyu.st@kakaocorp.com>
2022-06-06 02:18:16 +0900

    refactor: GameTest.kt 패키지 위치 변경

--- src/test/kotlin/blackjack/application/GameTest.kt
+++ src/test/kotlin/blackjack/application/GameTest.kt
@@ -1,6 +1,5 @@
-package application
+package blackjack.application
 
-import blackjack.application.Game
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
