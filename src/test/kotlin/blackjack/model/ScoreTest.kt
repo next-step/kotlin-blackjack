d4c95f1d7acfee7f89ce8876743c3c6da8f9c42a
austen.lee <austen.lee@kakaocorp.com>
2022-06-05 22:41:14 +0900

    step2 test : 테스트 세분화

--- src/test/kotlin/blackjack/model/ScoreTest.kt
+++ src/test/kotlin/blackjack/model/ScoreTest.kt
@@ -8,10 +8,34 @@ class ScoreTest {
     fun `Score은 PlayingCards를 받아 현재 점수를 계산해 보관한다`() {
         val cards = listOf(
             PlayingCard.of(Suit.CLUBS, CardNumber.NINE),
+            PlayingCard.of(Suit.HEARTS, CardNumber.EIGHT)
+        )
+        val playingCards = PlayingCards.from(cards)
+
+        assertThat(Score.from(playingCards).value).isEqualTo(17)
+    }
+
+    @Test
+    fun `ACE 카드의 점수를 11로 계산했을 때 총점이 21을 초과하지 않는다면, ACE는 11점으로 계산된다`() {
+        val cards = listOf(
+            PlayingCard.of(Suit.CLUBS, CardNumber.THREE),
+            PlayingCard.of(Suit.HEARTS, CardNumber.FOUR),
             PlayingCard.of(Suit.HEARTS, CardNumber.ACE)
         )
         val playingCards = PlayingCards.from(cards)
 
-        assertThat(Score.from(playingCards).value).isEqualTo(20)
+        assertThat(Score.from(playingCards).value).isEqualTo(18)
+    }
+
+    @Test
+    fun `ACE 카드의 점수를 11로 계산했을 때 총점이 21을 초과하면, ACE는 1점으로 계산된다`() {
+        val cards = listOf(
+            PlayingCard.of(Suit.CLUBS, CardNumber.KING),
+            PlayingCard.of(Suit.HEARTS, CardNumber.JACK),
+            PlayingCard.of(Suit.HEARTS, CardNumber.ACE)
+        )
+        val playingCards = PlayingCards.from(cards)
+
+        assertThat(Score.from(playingCards).value).isEqualTo(21)
     }
 }
