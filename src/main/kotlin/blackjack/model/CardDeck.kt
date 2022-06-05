8df5a9ed717be0579ef37dc76f9795b0e06775d7
austen.lee <austen.lee@kakaocorp.com>
2022-06-05 15:26:41 +0900

    step2 refactor : 고쳐야 할 부분 Todo 달기

--- src/main/kotlin/blackjack/model/CardDeck.kt
+++ src/main/kotlin/blackjack/model/CardDeck.kt
@@ -13,6 +13,7 @@ class CardDeck private constructor(cards: List<PlayingCard>) {
 
     companion object {
         fun from(cards: List<PlayingCard>): CardDeck {
+            // Todo : distinct와 shuffled는 외부에서 하는 것으로
             return CardDeck(cards.distinct().shuffled())
         }
     }
