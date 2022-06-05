f3cce2b245a153c6308b88cba51ea4ce8b9ca2f7
austen.lee <austen.lee@kakaocorp.com>
2022-06-05 16:45:46 +0900

    step2 refactor : cards MutableList에서 List로

--- src/main/kotlin/blackjack/model/PlayingCards.kt
+++ src/main/kotlin/blackjack/model/PlayingCards.kt
@@ -1,13 +1,13 @@
 package blackjack.model
 
-class PlayingCards private constructor(private val cards: MutableList<PlayingCard>) : List<PlayingCard> by cards {
+class PlayingCards private constructor(private val cards: List<PlayingCard>) : List<PlayingCard> by cards {
     operator fun plus(other: PlayingCards): PlayingCards {
         return from(cards + other.cards)
     }
 
     companion object {
         fun from(cards: List<PlayingCard>): PlayingCards {
-            return PlayingCards(cards.toMutableList())
+            return PlayingCards(cards)
         }
     }
 }
