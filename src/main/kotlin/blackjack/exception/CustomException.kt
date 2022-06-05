822e00dec7cdc155ea8959662f3bf7e3ca5d8e9d
molly <molly.d@kakaocorp.com>
2022-06-06 00:34:04 +0900

    fix: 리뷰반영 - CustomException.kt 수정

--- src/main/kotlin/blackjack/exception/CustomException.kt
+++ src/main/kotlin/blackjack/exception/CustomException.kt
@@ -2,4 +2,4 @@ package blackjack.exception
 
 class InvalidInputValueException : IllegalArgumentException("입력값이 올바르지 않습니다.")
 
-class CardDeckIsEmptyException : RuntimeException()
+class CardDeckIsEmptyException : RuntimeException("카드 덱이 비어있습니다. 게임을 다시 진행해주세요.")
