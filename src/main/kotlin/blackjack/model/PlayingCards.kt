e0568abfce3b6137a51032694ac3b8e8f9b68031
austen.lee <austen.lee@kakaocorp.com>
2022-06-05 22:18:21 +0900

    step2 feat : 특정 번호의 카드가 있는지 확인하는 hasCardOf

--- src/main/kotlin/blackjack/model/PlayingCards.kt
+++ src/main/kotlin/blackjack/model/PlayingCards.kt
@@ -1,6 +1,10 @@
 package blackjack.model
 
 class PlayingCards private constructor(private val cards: List<PlayingCard>) : List<PlayingCard> by cards {
+    fun hasCardOf(number: CardNumber): Boolean = any { playingCard ->
+        playingCard.isCardOf(number)
+    }
+
     operator fun plus(other: PlayingCards): PlayingCards {
         return from(cards + other.cards)
     }
--- src/test/kotlin/blackjack/model/PlayingCardsTest.kt
+++ src/test/kotlin/blackjack/model/PlayingCardsTest.kt
@@ -11,6 +11,7 @@ class PlayingCardsTest {
             PlayingCard.of(Suit.CLUBS, CardNumber.NINE),
             PlayingCard.of(Suit.HEARTS, CardNumber.ACE)
         )
+
         assertThat(PlayingCards.from(cards)).containsAll(cards)
     }
 
@@ -24,11 +25,30 @@ class PlayingCardsTest {
             PlayingCard.of(Suit.DIAMONDS, CardNumber.TEN),
             PlayingCard.of(Suit.SPADES, CardNumber.KING)
         )
-
         val playingCards = PlayingCards.from(cards1) + PlayingCards.from(cards2)
+
         assertAll(
             { assertThat(playingCards).containsAll(cards1) },
             { assertThat(playingCards).containsAll(cards2) }
+        )
+    }
+
+    @Test
+    fun `hasCardOf를 통해 해당 번호를 가진 PlayingCard가 있는지 확인할 수 있다`() {
+        val cards = listOf(
+            PlayingCard.of(Suit.CLUBS, CardNumber.NINE),
+            PlayingCard.of(Suit.HEARTS, CardNumber.ACE),
+            PlayingCard.of(Suit.DIAMONDS, CardNumber.TEN),
+            PlayingCard.of(Suit.SPADES, CardNumber.KING)
+        )
+        val playingCards = PlayingCards.from(cards)
+
+        assertAll(
+            { assertThat(playingCards.hasCardOf(CardNumber.NINE)).isTrue },
+            { assertThat(playingCards.hasCardOf(CardNumber.ACE)).isTrue },
+            { assertThat(playingCards.hasCardOf(CardNumber.TEN)).isTrue },
+            { assertThat(playingCards.hasCardOf(CardNumber.KING)).isTrue },
+            { assertThat(playingCards.hasCardOf(CardNumber.EIGHT)).isFalse }
         )
     }
 }
