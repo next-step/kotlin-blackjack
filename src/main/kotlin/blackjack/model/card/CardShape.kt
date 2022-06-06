c693d95903261edad2d2222c8290456fbbea221c
Juatae-Kim <jutaekim@sk.com>
2022-06-07 07:43:03 +0900

    refactor(Card): Remove needless property (CardShape.score) (apply feedback)

--- src/main/kotlin/blackjack/model/card/CardShape.kt
+++ src/main/kotlin/blackjack/model/card/CardShape.kt
@@ -1,10 +1,10 @@
 package blackjack.model.card
 
-enum class CardShape(val displayName: String, val score: Int) {
-    SPADES("스페이드", 1),
-    DIAMONDS("다이이몬드", 2),
-    HEARTS("하트", 3),
-    CLUBS("클로버", 4);
+enum class CardShape(val displayName: String) {
+    SPADES("스페이드"),
+    DIAMONDS("다이이몬드"),
+    HEARTS("하트"),
+    CLUBS("클로버");
 
     companion object {
         val count: Int = CardShape.values().size
