b1107381729d37b35e814b7a4525c456907e95e1
pug <pug.gg@kakaocorp.com>
2022-06-07 20:25:15 +0900

    test: TDD 테스트 케이스 선 반영

--- src/test/kotlin/blackjack/domain/game/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/game/DealerTest.kt
@@ -31,4 +31,14 @@ internal class DealerTest {
         // then
         assertThat(card).isInstanceOf(Card::class.java)
     }
+
+    @Test
+    fun `딜러는 가진 패의 합계가 16이하이면 1장의 카드를 추가로 받는다`() {
+        TODO()
+    }
+
+    @Test
+    fun `딜러는 가진 패의 합계가 17이상이면 카드를 추가로 받을 수 없다`() {
+        TODO()
+    }
 }
