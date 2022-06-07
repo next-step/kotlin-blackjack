eb4b8f1a540b41181bd1fa4f983c0fc37d3dd3b3
pug <pug.gg@kakaocorp.com>
2022-06-07 21:04:57 +0900

    refactor: 테스트 클래스 위치 변경

--- src/test/kotlin/blackjack/domain/participant/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/participant/DealerTest.kt
@@ -1,7 +1,6 @@
-package blackjack.domain.game
+package blackjack.domain.participant
 
 import blackjack.domain.card.Card
-import blackjack.domain.participant.Dealer
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
