a298352837fb2062736e35e42bc70991b41ea413
pug <pug.gg@kakaocorp.com>
2022-06-07 21:24:13 +0900

    test: Dealer 테스트 케이스 구현

--- src/test/kotlin/blackjack/domain/TestFixtures.kt
+++ src/test/kotlin/blackjack/domain/TestFixtures.kt
@@ -7,4 +7,6 @@ import blackjack.domain.card.CardSymbol
 val DIAMOND_ACE = Card(CardSuit.DIAMOND, CardSymbol.ACE)
 val CLUB_KING = Card(CardSuit.CLUB, CardSymbol.KING)
 val SPADE_TEN = Card(CardSuit.SPADE, CardSymbol.TEN)
+val SPADE_FIVE = Card(CardSuit.SPADE, CardSymbol.FIVE)
+val HEART_SIX = Card(CardSuit.HEART, CardSymbol.SIX)
 val HEART_TWO = Card(CardSuit.HEART, CardSymbol.TWO)
--- src/test/kotlin/blackjack/domain/participant/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/participant/DealerTest.kt
@@ -1,8 +1,18 @@
 package blackjack.domain.participant
 
+import blackjack.domain.CLUB_KING
+import blackjack.domain.DIAMOND_ACE
+import blackjack.domain.HEART_SIX
+import blackjack.domain.HEART_TWO
+import blackjack.domain.SPADE_FIVE
+import blackjack.domain.SPADE_TEN
 import blackjack.domain.card.Card
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class DealerTest {
 
@@ -32,13 +42,45 @@ internal class DealerTest {
         assertThat(card).isInstanceOf(Card::class.java)
     }
 
-    @Test
-    fun `딜러는 가진 패의 합계가 16이하이면 1장의 카드를 추가로 받는다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("패의 합계가 16이하인 케이스")
+    fun `딜러는 가진 패의 합계가 16이하이면 1장의 카드를 추가로 받는다`(cards: Array<Card>) {
+        // given
+        val dealer = Dealer()
+
+        // when
+        dealer.addCards(*cards)
+
+        // then
+        assertThat(dealer.isDrawable()).isTrue
     }
 
-    @Test
-    fun `딜러는 가진 패의 합계가 17이상이면 카드를 추가로 받을 수 없다`() {
-        TODO()
+    @ParameterizedTest
+    @MethodSource("패의 합계가 17이상인 케이스")
+    fun `딜러는 가진 패의 합계가 17이상이면 카드를 추가로 받을 수 없다`(cards: Array<Card>) {
+        // given
+        val dealer = Dealer()
+
+        // when
+        dealer.addCards(*cards)
+
+        // then
+        assertThat(dealer.isDrawable()).isFalse
+    }
+
+    companion object {
+        @JvmStatic
+        fun `패의 합계가 16이하인 케이스`() = Stream.of(
+            Arguments.of(arrayOf(SPADE_TEN, HEART_SIX)),
+            Arguments.of(arrayOf(DIAMOND_ACE, SPADE_FIVE, SPADE_TEN)),
+            Arguments.of(arrayOf(HEART_TWO))
+        )
+
+        @JvmStatic
+        fun `패의 합계가 17이상인 케이스`() = Stream.of(
+            Arguments.of(arrayOf(SPADE_TEN, CLUB_KING)),
+            Arguments.of(arrayOf(SPADE_TEN, CLUB_KING, DIAMOND_ACE)),
+            Arguments.of(arrayOf(CLUB_KING, SPADE_FIVE, HEART_TWO))
+        )
     }
 }
