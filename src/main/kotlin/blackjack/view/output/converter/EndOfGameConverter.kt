0f927a891df48077b2a69166cdd26923eba0c413
austen.lee <austen.lee@kakaocorp.com>
2022-06-08 01:34:02 +0900

    step2 feat : 게임의 결과를 출력 문자로 변환해 주는 EndOfGameConverter

---
+++ src/main/kotlin/blackjack/view/output/converter/EndOfGameConverter.kt
@@ -0,0 +1,11 @@
+package blackjack.view.output.converter
+
+import blackjack.domain.Player
+
+object EndOfGameConverter : OutputConverter<List<Player>> {
+    override fun convert(printable: List<Player>): String {
+        return printable.joinToString("\n") { player ->
+            "${PlayerConverter.convert(player)} - 결과: ${player.hands.score.value}"
+        }
+    }
+}
---
+++ src/test/kotlin/blackjack/view/output/converter/EndOfGameConverterTest.kt
@@ -0,0 +1,39 @@
+package blackjack.view.output.converter
+
+import blackjack.domain.CardNumber
+import blackjack.domain.Player
+import blackjack.domain.PlayingCard
+import blackjack.domain.PlayingCards
+import blackjack.domain.Suit
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class EndOfGameConverterTest {
+    @Test
+    fun `EndOfGameConverter는 게임의 결과를 출력을 위한 문자열로 변환해 반환한다`() {
+        val player1 = Player.from("panther").apply {
+            receive(
+                PlayingCards.from(
+                    listOf(
+                        PlayingCard.of(Suit.SPADES, CardNumber.TWO),
+                        PlayingCard.of(Suit.HEARTS, CardNumber.ACE),
+                        PlayingCard.of(Suit.SPADES, CardNumber.EIGHT)
+                    )
+                )
+            )
+        }
+        val player2 = Player.from("fox").apply {
+            receive(
+                PlayingCards.from(
+                    listOf(
+                        PlayingCard.of(Suit.DIAMONDS, CardNumber.JACK),
+                        PlayingCard.of(Suit.CLUBS, CardNumber.SEVEN)
+                    )
+                )
+            )
+        }
+
+        val expected = "panther카드: 2스페이드, A하트, 8스페이드 - 결과: 21\nfox카드: J다이아몬드, 7클로버 - 결과: 17"
+        Assertions.assertThat(EndOfGameConverter.convert(listOf(player1, player2))).isEqualTo(expected)
+    }
+}
