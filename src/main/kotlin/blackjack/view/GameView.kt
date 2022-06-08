1678a242d0d2bc0c649c216020f31ed18ae6c254
ujusy <lsyeon1997@ajou.ac.kr>
2022-06-09 00:04:49 +0900

    refactor: 잘못 입력한 경우 예외처리 추가

--- src/main/kotlin/blackjack/view/GameView.kt
+++ src/main/kotlin/blackjack/view/GameView.kt
@@ -23,13 +23,20 @@ class GameView(
     private fun choiceCard(player: Player) {
         println("${player.name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
         val input = readlnOrNull() ?: throw IllegalArgumentException()
-        if (input == "y") {
-            blackJack.giveCard(player)
-            playerCards(player)
-            choiceCard(player)
-        } else if (input == "n") {
-            playerCards(player)
-            return
+        when (input) {
+            "y" -> {
+                blackJack.giveCard(player)
+                playerCards(player)
+                choiceCard(player)
+            }
+            "n" -> {
+                playerCards(player)
+                return
+            }
+            else -> {
+                println("다시 입력해주세요. (예는 y, 아니오는 n)")
+                choiceCard(player)
+            }
         }
     }
 
