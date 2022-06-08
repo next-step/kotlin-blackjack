7775f058bca92399e2f2660a91ed042c591941a3
ujusy <lsyeon1997@ajou.ac.kr>
2022-06-08 19:01:05 +0900

    refactor: cardDeck fake 객체 생성

--- src/main/kotlin/blackjack/domain/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -1,21 +1,11 @@
 package blackjack.domain
 
-class CardDeck {
+interface CardDeck {
+    fun getOne(): Card
 
-    private val shape = listOf("하트", "스페이스", "클로버", "다이아몬드")
-    private val number = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K")
-
-    private var cardPool: MutableList<Card> = mutableListOf()
-
-    init {
-        for (i in shape) {
-            for (j in number) cardPool.add(Card(i, j))
+    class Fake(private val card: Card) : CardDeck {
+        override fun getOne(): Card {
+            return card
         }
-
-        cardPool.shuffle()
-    }
-
-    fun getOne(): Card {
-        return cardPool.removeFirst()
     }
 }
--- src/test/kotlin/blackjack/CardGeneratorTest.kt
+++ src/test/kotlin/blackjack/CardGeneratorTest.kt
@@ -1,7 +1,7 @@
 package blackjack
 
 import blackjack.domain.Card
-import blackjack.domain.CardDeck
+import blackjack.domain.CardDeckImpl
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
@@ -11,15 +11,15 @@ class CardGeneratorTest : FreeSpec({
     "getOne" - {
 
         "랜덤한 카드 한 장이 나와야한다." {
-            val cardDeck = CardDeck()
+            val cardDeckImpl = CardDeckImpl()
 
-            cardDeck.getOne().shouldBeInstanceOf<Card>()
+            cardDeckImpl.getOne().shouldBeInstanceOf<Card>()
         }
 
         "카드를 모두 뽑았을 때 중복되는 카드가 없어야한다." {
-            var cardDeck = CardDeck()
+            var cardDeckImpl = CardDeckImpl()
             val cards = List(48) {
-                cardDeck.getOne()
+                cardDeckImpl.getOne()
             }
 
             cards.distinct().size shouldBe 48
