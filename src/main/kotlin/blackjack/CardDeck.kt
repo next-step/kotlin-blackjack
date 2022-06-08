66e5e2fad4a4e18ed4ae3445b0483e8bce0c1876
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-06-08 17:25:35 +0900

    refactor: 카드 덱에서 카드뭉치 생성

---
+++ src/main/kotlin/blackjack/CardDeck.kt
@@ -0,0 +1,17 @@
+package blackjack
+
+object CardDeck {
+    fun makeAllCards(): List<Card> {
+        val numbers = CardNumber.values()
+        val shapes = CardShape.values()
+        val allCards = mutableListOf<Card>()
+
+        numbers.forEach { number ->
+            shapes.forEach { shape ->
+                allCards.add(Card(number, shape))
+            }
+        }
+
+        return allCards
+    }
+}
--- src/main/kotlin/blackjack/Dealer.kt
+++ src/main/kotlin/blackjack/Dealer.kt
@@ -1,23 +1,9 @@
 package blackjack
 
 object Dealer {
-    private val cards = makeAllCards().shuffled().toMutableList()
+    private val cards = CardDeck.makeAllCards().shuffled().toMutableList()
 
     fun popOneCard(): Card {
         return cards.removeLast()
-    }
-
-    private fun makeAllCards(): List<Card> {
-        val numbers = CardNumber.values()
-        val shapes = CardShape.values()
-        val allCards = mutableListOf<Card>()
-
-        numbers.forEach { number ->
-            shapes.forEach { shape ->
-                allCards.add(Card(number, shape))
-            }
-        }
-
-        return allCards
     }
 }
