39718e6d431182ee24fcadea4cf4807787ca2dc7
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-06-08 17:18:17 +0900

    refactor: 카드클래스 데이터클래스로

--- src/main/kotlin/blackjack/Card.kt
+++ src/main/kotlin/blackjack/Card.kt
@@ -1,16 +1,7 @@
 package blackjack
 
-class Card(val cardNumber: CardNumber, private val cardShape: CardShape) {
+data class Card(val cardNumber: CardNumber, private val cardShape: CardShape) {
     override fun toString(): String {
         return "${cardNumber.cardName}${cardShape.shapeName}"
-    }
-
-    override fun hashCode(): Int {
-        return cardNumber.name.hashCode() + cardShape.name.hashCode()
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other !is Card) return false
-        return cardNumber == other.cardNumber && cardShape == other.cardShape
     }
 }
