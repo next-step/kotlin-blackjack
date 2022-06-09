68c0a1a9214480e317d065efcb996ab3f0cfe52a
glenn.kgp <glenn.kgp@kakaocorp.com>
2022-06-10 00:01:52 +0900

    test: 카드합계 테스트의 케이스 추가

--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 class CardsTest {
     @Test
     fun `카드의 합계를 모든 경우의수가 결과가 맞는지 테스트`() {
-        val cards = Cards(
+        val cards1 = Cards(
             listOf(
                 Card(CardNumber.ACE, CardShape.CLOVER),
                 Card(CardNumber.THREE, CardShape.HEART),
@@ -15,10 +15,39 @@ class CardsTest {
             )
         )
 
-        val answer = cards.possibleResults
+        val answer1 = cards1.possibleResults
 
-        assertThat(answer.contains(14)).isTrue
-        assertThat(answer.contains(23)).isTrue
+        assertThat(answer1.contains(14)).isTrue
+        assertThat(answer1.contains(23)).isTrue
+
+        val cards2 = Cards(
+            listOf(
+                Card(CardNumber.ACE, CardShape.CLOVER),
+                Card(CardNumber.ACE, CardShape.HEART),
+                Card(CardNumber.ACE, CardShape.CLOVER),
+            )
+        )
+
+        val answer2 = cards2.possibleResults
+
+        assertThat(answer2.contains(3)).isTrue
+        assertThat(answer2.contains(12)).isTrue
+        assertThat(answer2.contains(21)).isTrue
+        assertThat(answer2.contains(30)).isTrue
+
+        val cards3 = Cards(
+            listOf(
+                Card(CardNumber.ACE, CardShape.CLOVER),
+                Card(CardNumber.ACE, CardShape.HEART),
+                Card(CardNumber.JACK, CardShape.CLOVER),
+            )
+        )
+
+        val answer3 = cards3.possibleResults
+
+        assertThat(answer3.contains(12)).isTrue
+        assertThat(answer3.contains(21)).isTrue
+        assertThat(answer3.contains(30)).isTrue
     }
 
     @Test
