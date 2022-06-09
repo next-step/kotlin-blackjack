ef3e7ff1c57e1853cce157b2a1127f6985ae2f8b
pug <pug.gg@kakaocorp.com>
2022-06-09 20:21:59 +0900

    refactor: viewResolver 구조 개선

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -2,9 +2,10 @@ package blackjack.view
 
 import blackjack.domain.participant.Dealer
 import blackjack.domain.participant.Player
+import blackjack.domain.game.Result
 
 object ResultView {
-    fun printResult(dealer: Dealer, players: List<Player>, result: blackjack.domain.game.Result) {
+    fun printResult(dealer: Dealer, players: List<Player>, result: Result) {
         println("\n${ParticipantView.parseParticipantInfoToString(dealer)} - 결과: ${dealer.score()}")
         players.forEach { player -> println("${ParticipantView.parseParticipantInfoToString(player)} - 결과: ${player.score()}") }
 
--- src/main/kotlin/blackjack/view/ViewResolver.kt
+++ src/main/kotlin/blackjack/view/ViewResolver.kt
@@ -1,7 +1,14 @@
 package blackjack.view
 
-data class ViewResolver(
-    val inputView: InputView,
-    val participantView: ParticipantView,
-    val resultView: ResultView
-)
+import blackjack.domain.participant.Dealer
+import blackjack.domain.participant.Participant
+import blackjack.domain.participant.Player
+import blackjack.domain.game.Result
+
+class ViewResolver {
+    val decidePlayerHitDecision: (player: Player) -> Boolean = InputView::decidePlayerHitDecision
+    val printInitialHand: (dealer: Dealer, players: List<Player>) -> Unit = ParticipantView::printInitialHand
+    val printParticipantInfo: (participant: Participant) -> Unit = ParticipantView::printParticipantInfo
+    val printDealerDrawOneCard: () -> Unit = ParticipantView::printDealerDrawOneCard
+    val printResult: (dealer: Dealer, players: List<Player>, result: Result) -> Unit = ResultView::printResult
+}
