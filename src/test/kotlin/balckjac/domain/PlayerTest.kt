1e5d254616fd127c97312acb4808814951f6703b
justin.sg <justin.sg@kakaocorp.com>
2022-06-10 20:03:01 +0900

    test : 플레이어는 카드 정보를 추가할 수 있다

--- src/test/kotlin/balckjac/domain/PlayerTest.kt
+++ src/test/kotlin/balckjac/domain/PlayerTest.kt
@@ -1,5 +1,6 @@
 package balckjac.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class PlayerTest {
@@ -10,5 +11,17 @@ internal class PlayerTest {
             name = "justin",
             cards = listOf("justin")
         )
+    }
+
+    @Test
+    fun `플레이어는 카드 정보를 추가할 수 있다`() {
+        val player = Player(
+            name = "justin",
+            cards = listOf("card")
+        )
+        player.addCard(listOf("newCard"))
+
+        assertThat(player.cards).contains("newCard")
+
     }
 }
