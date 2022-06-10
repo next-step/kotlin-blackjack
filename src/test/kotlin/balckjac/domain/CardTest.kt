1c1610171f10c8fa4eefce43307535e2b3767047
justin.sg <justin.sg@kakaocorp.com>
2022-06-10 20:23:53 +0900

    test : 카드는 suit, denomination 을 조합하여 실제 카드 이름을 생성한다

--- src/test/kotlin/balckjac/domain/CardTest.kt
+++ src/test/kotlin/balckjac/domain/CardTest.kt
@@ -15,4 +15,14 @@ internal class CardTest {
         assertThat(card.suit).isEqualTo(Suit.DIA)
         assertThat(card.denomination).isEqualTo(Denomination.ACE)
     }
+
+    @Test
+    fun `카드는 suit, denomination 을 조합하여 실제 카드 이름을 생성한다`() {
+        val card = Card(
+            suit = Suit.DIA,
+            denomination = Denomination.FIVE
+        )
+
+        assertThat(card.fullName).isEqualTo("다이아몬드(◆)5")
+    }
 }
