8fb8ac3182548010e18133c619f7f378d6059e00
justin.sg <justin.sg@kakaocorp.com>
2022-06-10 20:07:04 +0900

    refactor : 플레이어 1급 콜렉션 backin property 반영

--- src/main/kotlin/balckjac/domain/Players.kt
+++ src/main/kotlin/balckjac/domain/Players.kt
@@ -2,4 +2,9 @@ package balckjac.domain
 
 class Players(
     players: List<Player> = emptyList()
-)
+) {
+    private val _payers = players.toMutableList()
+    val players: List<Player> get() = _payers.toList()
+}
+
+fun List<String>.toPlayers() = Players(this.map { Player(it) })
