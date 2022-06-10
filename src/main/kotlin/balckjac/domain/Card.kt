5d7afcc375b777f6312f596beefa41f3d1023d35
justin.sg <justin.sg@kakaocorp.com>
2022-06-10 20:24:16 +0900

    feat : 카드는 suit, denomination 을 조합하여 실제 카드 이름을 생성한다

--- src/main/kotlin/balckjac/domain/Card.kt
+++ src/main/kotlin/balckjac/domain/Card.kt
@@ -3,4 +3,8 @@ package balckjac.domain
 data class Card(
     val suit: Suit,
     val denomination: Denomination
-)
+) {
+    val fullName = "${suit.cardName}${denomination.cardName}"
+}
+
+fun List<Card>.asText() = this.joinToString { it.fullName }
