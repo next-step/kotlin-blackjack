0caf0c418a46fa6b463d68e4626e396250da47ed
justin.sg <justin.sg@kakaocorp.com>
2022-06-10 20:10:22 +0900

    test : Denomination 은 카드 이름과 스코어를 갖는다

---
+++ src/test/kotlin/balckjac/domain/DenominationTest.kt
@@ -0,0 +1,14 @@
+package balckjac.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DenominationTest {
+
+    @Test
+    fun `Denomination 은 카드 이름과 스코어를 갖는다`() {
+        assertThat(Denomination.ACE.cardName).isEqualTo("Ace")
+        assertThat(Denomination.ACE.score).isEqualTo(1)
+        assertThat(Denomination.ACE.multipleScore).isEqualTo(listOf(1, 11))
+    }
+}
