06b87a0e109967d1f3b0fe0cd02c39ef6fe89c1a
justin.sg <justin.sg@kakaocorp.com>
2022-06-10 20:33:27 +0900

    feat : Deck 은 카드를 분배할 수 있으며 분배된 카드는 Deck 에서 제거된다.

--- src/main/kotlin/balckjac/domain/Deck.kt
+++ src/main/kotlin/balckjac/domain/Deck.kt
@@ -2,8 +2,9 @@ package balckjac.domain
 
 import balckjac.domain.GameRule.TOTAL_CARD_COUNT
 
+
 class Deck(
-    cards: List<Card> = emptyList()
+    cards: List<Card> = CARD_LIST
 ) {
     private val _cards = cards.toMutableList()
     val cards: List<Card> get() = _cards.toList()
@@ -12,5 +13,24 @@ class Deck(
         require(cards.size == TOTAL_CARD_COUNT) {
             "카드는 ${TOTAL_CARD_COUNT}으로 구성되어야 합니다."
         }
+    }
+
+
+    fun draw(): Card {
+        require(cards.isNotEmpty()) {
+            "사용할 카드가 없습니다."
+        }
+
+        return cards.first().apply {
+            _cards.remove(this)
+        }
+    }
+
+    companion object {
+        val CARD_LIST: List<Card> = Suit.values().map { character ->
+            Denomination.values().map { number ->
+                Card(suit = character, denomination = number)
+            }
+        }.flatten().shuffled()
     }
 }
