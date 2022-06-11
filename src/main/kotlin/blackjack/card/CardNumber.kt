ecd75b25e4f399665123d51a6f2696e31d0a0dcc
jojoldu <jojoldu@gmail.com>
2022-06-12 00:46:53 +0900

    CardNumber 카드명 요구사항에 맞게 축약어 적용

--- src/main/kotlin/blackjack/card/CardNumber.kt
+++ src/main/kotlin/blackjack/card/CardNumber.kt
@@ -1,7 +1,7 @@
 package blackjack.card
 
 enum class CardNumber(val noName: String, val primaryPoint: Int, val secondaryPoint: Int) {
-    ACE("Ace", 11, 1),
+    ACE("A", 11, 1),
     TWO("2", 2, 0),
     THREE("3", 3, 0),
     FOUR("4", 4, 0),
@@ -10,7 +10,7 @@ enum class CardNumber(val noName: String, val primaryPoint: Int, val secondaryPo
     SEVEN("7", 7, 0),
     EIGHT("8", 8, 0),
     NINE("9", 9, 0),
-    JACK("Jack", 10, 0),
-    QUEEN("Queen", 10, 0),
-    KING("King", 10, 0);
+    JACK("J", 10, 0),
+    QUEEN("Q", 10, 0),
+    KING("K", 10, 0);
 }
