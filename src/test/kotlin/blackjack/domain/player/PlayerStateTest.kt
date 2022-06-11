5a202fc57b5a9f9bcc6a56d80d9d40e5412b9c0c
vivian.bae <vivian.bae@kakaocorp.com>
2022-06-12 01:27:09 +0900

    사용자가 게임을 진행할 수 있는 상태이고 hit 를 해 카드를 받은 후 총 숫자합이 21 이상일 경우 게임 불가 상태가 된다

--- src/test/kotlin/blackjack/domain/player/PlayerStateTest.kt
+++ src/test/kotlin/blackjack/domain/player/PlayerStateTest.kt
@@ -64,4 +64,23 @@ class PlayerStateTest {
         val newPlayerState = (playerState as PlayerState.Playing).hit(additionalCard)
         assertThat(newPlayerState).isExactlyInstanceOf(PlayerState.Playing::class.java)
     }
+
+    @Test
+    fun `사용자가 게임을 진행할 수 있는 상태이고 hit 를 해 카드를 받은 후 총 숫자합이 21 이상일 경우 게임 불가 상태가 된다`() {
+        val player = Player("vivian")
+
+        val cards = listOf(
+            Card.Six(CardSuit.CLOVER),
+            Card.Six(CardSuit.HEART),
+        )
+        val additionalCard = Card.King(CardSuit.CLOVER)
+
+        cards.forEach { player.addCardToHand(it) }
+
+        val playerState = PlayerState.of(player)
+        assertThat(playerState).isExactlyInstanceOf(PlayerState.Playing::class.java)
+
+        val newPlayerState = (playerState as PlayerState.Playing).hit(additionalCard)
+        assertThat(newPlayerState).isExactlyInstanceOf(PlayerState.Done::class.java)
+    }
 }
