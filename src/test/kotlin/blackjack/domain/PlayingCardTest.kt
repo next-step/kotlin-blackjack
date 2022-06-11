f52ab40ac744a2ecc3ec5a501d7914f83a12915f
austen.lee <austen.lee@kakaocorp.com>
2022-06-11 21:29:55 +0900

    step2 refactor : 테스트 수정

--- src/test/kotlin/blackjack/domain/PlayingCardTest.kt
+++ src/test/kotlin/blackjack/domain/PlayingCardTest.kt
@@ -9,21 +9,20 @@ class PlayingCardTest {
     fun `PlayingCard는 카드 모양과 카드 번호로 구성된다`() {
         val playingCard = PlayingCard.of(Suit.CLUBS, CardNumber.ACE)
 
-        assertThat(playingCard.suit).isEqualTo(Suit.CLUBS)
-        assertThat(playingCard.number).isEqualTo(CardNumber.ACE)
+        assertAll(
+            { assertThat(playingCard.suit).isEqualTo(Suit.CLUBS) },
+            { assertThat(playingCard.number).isEqualTo(CardNumber.ACE) }
+        )
     }
 
     @Test
-    fun `all을 통해 모든 카드 목록을 가져올 수 있다`() {
+    fun `all을 호출해 얻은 전체 카드는 52개의 유니크한 카드 리스트이다`() {
         val playingCards = PlayingCard.all()
 
-        Suit.values().forEach { suit ->
-            CardNumber.values().forEach { number ->
-                val playingCard = PlayingCard.of(suit, number)
-
-                assertThat(playingCards).contains(playingCard)
-            }
-        }
+        assertAll(
+            { assertThat(playingCards).hasSize(52) },
+            { assertThat(playingCards.distinct()).containsAll(playingCards) }
+        )
     }
 
     @Test
