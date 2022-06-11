7f7d3293e319bf39354c99e74ab93f3ceb561f97
vivian.bae <vivian.bae@kakaocorp.com>
2022-06-12 01:00:49 +0900

    사용자가 카드를 더 받을지 안 받을지 입력받을 수 있다

---
+++ src/main/kotlin/blackjack/PlayerDecision.kt
@@ -0,0 +1,13 @@
+package blackjack
+
+enum class PlayerDecision {
+    HIT, STAND;
+
+    companion object {
+        fun ofText(playerDecision: String): PlayerDecision? = when (playerDecision) {
+            "y" -> HIT
+            "n" -> STAND
+            else -> null
+        }
+    }
+}
---
+++ src/test/kotlin/blackjack/PlayerDecisionTest.kt
@@ -0,0 +1,21 @@
+package blackjack
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PlayerDecisionTest {
+    @Test
+    fun `입력값이 y 일 경우 hit 라고 판단한다`() {
+        assertThat(PlayerDecision.ofText("y")).isEqualTo(PlayerDecision.HIT)
+    }
+
+    @Test
+    fun `입력값이 n 일 경우 stand 라고 판단한다`() {
+        assertThat(PlayerDecision.ofText("n")).isEqualTo(PlayerDecision.STAND)
+    }
+
+    @Test
+    fun `입력값이 y 이나 n 이 아닐 경우 null 을 반환한다`() {
+        assertThat(PlayerDecision.ofText("invalid!")).isNull()
+    }
+}
