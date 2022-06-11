4a5a8938a958a83b9e4164aaf23bea2b89444b79
saintbeller96 <rlawhdtjd9@naver.com>
2022-06-11 16:13:25 +0900

    feature: 플레이어는  상태가 아니라면 카드를 받을 수 없다

--- src/test/kotlin/blackjack/domain/PlayerSpecs.kt
+++ src/test/kotlin/blackjack/domain/PlayerSpecs.kt
@@ -3,7 +3,9 @@ package blackjack.domain
 import blackjack.domain.Denomination.KING
 import blackjack.domain.State.HITTABLE
 import blackjack.domain.Suit.SPADE
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.inspectors.forAll
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
 import io.kotest.matchers.shouldBe
 
@@ -20,6 +22,20 @@ class PlayerSpecs : DescribeSpec({
                 val card = Card(KING, SPADE)
                 player.receive(card)
                 player.hand shouldBeEqualToComparingFields hand(card)
+            }
+        }
+
+        context("Hittable 이 아닌 상태라면") {
+            it("카드를 받을 수 없다") {
+                State.values()
+                    .filterNot { it == HITTABLE }
+                    .forAll {
+                        val player = Player("이름", state = it)
+                        val card = Card(KING, SPADE)
+                        shouldThrowExactly<IllegalStateException> {
+                            player.receive(card)
+                        }
+                    }
             }
         }
     }
