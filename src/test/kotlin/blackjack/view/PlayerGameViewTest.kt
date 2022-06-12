09f1f8c0afb17b38a12e6a6e0cc4372b7da331f3
Jake Son <sonjeabin@gmail.com>
2022-06-12 09:54:56 +0900

    test: 참가자 view 테스트 추가

--- src/test/kotlin/blackjack/view/PlayerGameViewTest.kt
+++ src/test/kotlin/blackjack/view/PlayerGameViewTest.kt
@@ -1,13 +1,75 @@
 package blackjack.view
 
 import blackjack.domain.Card
-import blackjack.domain.Dealer
-import blackjack.domain.Deck
-import blackjack.domain.Hand
-import blackjack.domain.Player
+import blackjack.domain.Denomination
+import blackjack.domain.Suite
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
 
-class GameViewTest : FreeSpec({
+class PlayerGameViewTest : FreeSpec({
+
+    "주어진 참가자의 카드정보를 출력한다" {
+        val io = StubIO()
+        val player = createPlayer(
+            "player",
+            Card(Suite.CLUBS, Denomination.QUEEN),
+            Card(Suite.CLUBS, Denomination.ACE),
+        )
+        val playerGameView = PlayerGameView(io)
+
+        playerGameView.printStatus(player)
+
+        io.printed shouldBe listOf("player카드: Q클로버, A클로버")
+    }
+
+    "카드 추가여부" - {
+        "안내문구를 출력한다" {
+            val io = StubIO()
+            io.addInput("y")
+            val player = createPlayer("player")
+            val playerGameView = PlayerGameView(io)
+
+            playerGameView.getDrawChoice(player)
+
+            io.printed shouldBe listOf("player는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        }
+
+        "y를 입력하는 경우 true 를 반환한다" {
+            val io = StubIO()
+            io.addInput("y")
+            val player = createPlayer("player")
+            val playerGameView = PlayerGameView(io)
+
+            val result = playerGameView.getDrawChoice(player)
+
+            result.shouldBeTrue()
+        }
+
+        "n를 입력하는 경우 false 를 반환한다" {
+            val io = StubIO()
+            io.addInput("n")
+            val player = createPlayer("player")
+            val playerGameView = PlayerGameView(io)
+
+            val result = playerGameView.getDrawChoice(player)
+
+            result.shouldBeFalse()
+        }
+
+        "잘못 입력하는 경우 다시 입력받는다" {
+            val io = StubIO()
+            io.addInput("invalid")
+            io.addInput("N")
+            val player = createPlayer("player")
+            val playerGameView = PlayerGameView(io)
+
+            playerGameView.getDrawChoice(player)
+
+            io.printed.last() shouldBe "잘못된 입력입니다. 다시 입력해주세요."
+        }
+    }
 
 //    "아무도 카드를 더 받을 수 없는 경우 게임을 종료한다" {
 //        val dealer = createDealer()
@@ -99,7 +161,3 @@ class GameViewTest : FreeSpec({
 //        )
 //    }
 })
-
-fun createDealer(vararg cards: Card): Dealer = Dealer(Deck(cards.toList()))
-
-fun createPlayer(name: String, vararg cards: Card): Player = Player(name, Hand(cards.toList()))
