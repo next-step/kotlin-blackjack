e3e7a9feb4a984d1e9dabf6f2af0d09c22908e13
salphaon <salphaon@gmail.com>
2022-06-12 14:17:55 +0900

    feat: 플레이어에 카드를 추가할 수 있다

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,12 @@
 package blackjack.domain
 
-class Player(val name: String)
+class Player(val name: String) {
+    var cards: List<Card> = listOf()
+
+    fun addCard(card: Card) {
+        cards = cards + card
+    }
+    fun addCards(cards: List<Card>) {
+        this.cards = this.cards.plus(cards)
+    }
+}
--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -1,5 +1,8 @@
 package blackjack.domain
 
+import blackjack.domain.Denomination.ACE
+import blackjack.domain.Denomination.TWO
+import blackjack.domain.Suit.SPADE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -8,9 +11,46 @@ internal class PlayerTest {
     @Test
     fun `Player는 이름을 가진다`() {
         val name = "han"
-
         val player = Player(name)
 
         assertThat(player.name).isEqualTo(name)
+    }
+
+    @Test
+    fun `Player는 카드를 가진다`() {
+        val name = "han"
+        val player = Player(name)
+
+        assertThat(player.name).isEqualTo(name)
+        assertThat(player.cards).hasSize(0)
+    }
+
+    @Test
+    fun `Player에 카드를 1장 추가할 수 있다`() {
+        val name = "han"
+        val player = Player(name)
+
+        assertThat(player.name).isEqualTo(name)
+        assertThat(player.cards).hasSize(0)
+
+        player.addCard(Card(SPADE, ACE))
+        assertThat(player.cards).hasSize(1)
+    }
+
+    @Test
+    fun `Player에 카드를 2장 추가할 수 있다`() {
+        val name = "han"
+        val player = Player(name)
+
+        assertThat(player.name).isEqualTo(name)
+        assertThat(player.cards).hasSize(0)
+
+        player.addCards(
+            listOf(
+                Card(SPADE, ACE),
+                Card(SPADE, TWO)
+            )
+        )
+        assertThat(player.cards).hasSize(2)
     }
 }
