9faf563b953e1a79b5a1843f89ec6a486267ea7f
ellen.youn <ellen.youn@kakaocorp.com>
2022-06-13 23:06:19 +0900

    feat: 블랙잭 게임 구현

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,5 +1,8 @@
 package blackjack.view
 
+import blackjack.domain.Card
+import blackjack.domain.CardDeck
+
 class InputView {
     fun getPlayers(): List<String> {
         println(INPUT_PLAYERS)
@@ -8,8 +11,21 @@ class InputView {
         return players.trim().split(SEPARATE_DELIMITER)
     }
 
+    fun askThePlayer(player: String): Card? {
+        println(String.format(INPUT_ASK_HIT, player))
+        return when (readln()) {
+            YES -> CardDeck.hit()
+            NO -> null
+            else -> throw IllegalArgumentException()
+        }
+    }
+
     companion object {
         private const val INPUT_PLAYERS = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+        private const val INPUT_ASK_HIT = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
+
+        private const val YES = "y"
+        private const val NO = "n"
 
         private const val SEPARATE_DELIMITER = ","
     }
