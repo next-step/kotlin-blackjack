71704edbfebe83d9adcc2751816f59b50fa7e471
Juatae-Kim <jutaekim@sk.com>
2022-06-13 11:47:28 +0900

    refactor(InputView): Improve readability

--- src/main/kotlin/blackjack/view/input/ConsolePlayerProvider.kt
+++ src/main/kotlin/blackjack/view/input/ConsolePlayerProvider.kt
@@ -11,13 +11,16 @@ class ConsolePlayerProvider : PlayerProvider {
 
     private val playNamesInputParser = PlayerNamesInputParser()
     private val hitDecisionMaker = ConsoleHitDecisionMaker()
+    private val betMoneyInputParser = IntInputParser(minValue = Player.MIN_BET_MONEY)
 
     override fun createPlayers(): Players =
         ConsoleReader.read(MESSAGE_INPUT_PLAYERS, playNamesInputParser)
             .map { name ->
-                val betMoney = ConsoleReader.read("${name}의 배팅 금액은?", IntInputParser(minValue = Player.MIN_BET_MONEY))
-                Player.Guest(name, hitDecisionMaker, betMoney = betMoney)
+                Player.Guest(name = name, hitDecisionMaker = hitDecisionMaker, betMoney = readBetMoneyFor(name))
             }.toPlayers()
+
+    private fun readBetMoneyFor(name: String): Int =
+        ConsoleReader.read("${name}의 배팅 금액은?", betMoneyInputParser)
 
     companion object {
         private const val MESSAGE_INPUT_PLAYERS = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
