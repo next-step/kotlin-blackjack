389b577df3c55ae6fd1f14c8d65b07655e19c33a
ellen.youn <ellen.youn@kakaocorp.com>
2022-06-13 22:48:00 +0900

    test: 52개의 카드 중 랜덤하게 하나를 가져온다.

--- src/test/kotlin/blackjack/domain/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/CardDeckTest.kt
@@ -8,4 +8,9 @@ class CardDeckTest : FreeSpec({
     "처음 생성된 덱은 총 52개의 카드를 가진다" {
         CardDeck.all().size shouldBe 52
     }
+
+    "랜덤하게 카드를 한개 가져온다" {
+        takeCards.add(CardDeck.hit())
+        takeCards.size() shouldBe 3
+    }
 })
