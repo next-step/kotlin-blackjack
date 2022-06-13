e599388eb8728f6222514c15c7aa661d67f4c870
ellen.youn <ellen.youn@kakaocorp.com>
2022-06-13 23:20:23 +0900

    bug: 테스트 순서가 변경되면서 깨지는 부분 수정

--- src/test/kotlin/blackjack/domain/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/CardDeckTest.kt
@@ -7,18 +7,19 @@ import io.kotest.matchers.shouldBe
 
 class CardDeckTest : FreeSpec({
     lateinit var takeCards: Cards
+
     "처음 생성된 덱은 총 52개의 카드를 가진다" {
         CardDeck.all().size shouldBe 52
-    }
-
-    "랜덤하게 카드를 한개 가져온다" {
-        takeCards.add(CardDeck.hit())
-        takeCards.size() shouldBe 3
     }
 
     "시작 시 랜덤하게 카드 두개를 가져온다" {
         takeCards = CardDeck.start()
         takeCards.size() shouldBe 2
+    }
+
+    "랜덤하게 카드를 한개 가져온다" {
+        takeCards.add(CardDeck.hit())
+        takeCards.size() shouldBe 3
     }
 
     "한번 가져온 카드는 덱에 존재하지 않는다" {
