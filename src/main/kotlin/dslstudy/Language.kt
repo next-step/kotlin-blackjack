53eba84182ff7a25e6f7790ff549efa4940a7692
Gyeonglok Kim <goodboy302@naver.com>
2022-06-13 19:31:50 +0900

    Step1 - 코틀린 DSL (#246)
    * feat: Kotlin DSL 실습 코드
    
    * chore: gitkeep 파일 제거
    
    * feat: Skill 요소 추가
    
    * feat: Language 요소 추가
    
    * refactor: Language, Skill 객체 클래스 분리
    
    * refactor: 패키지명 코틀린 코딩 컨벤션에 맞춰 수정
    
    * refactor: 불필요한 코드 제거

---
+++ src/main/kotlin/dslstudy/Language.kt
@@ -0,0 +1,17 @@
+package dslstudy
+
+data class Language(
+    val name: String,
+    val level: Int,
+)
+
+class LanguagesBuilder(
+    private val languages: MutableList<Language> = mutableListOf(),
+) {
+    infix fun String.level(level: Int) {
+        val language = Language(this, level)
+        languages.add(language)
+    }
+
+    fun build(): List<Language> = languages.toList()
+}
---
+++ src/test/kotlin/dslstudy/PersonTest.kt
@@ -0,0 +1,66 @@
+package dslstudy
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+class PersonTest : StringSpec({
+    "introduce" {
+        listOf("김경록", "로키").forAll {
+            val person = introduce {
+                name(it)
+            }
+
+            person.name shouldBe it
+        }
+    }
+
+    "company" {
+        val person = introduce {
+            name("김경록")
+            company("맘편한세상")
+        }
+
+        person.name shouldBe "김경록"
+        person.company shouldBe "맘편한세상"
+    }
+
+    "skills" {
+        val person = introduce {
+            name("김경록")
+            company("맘편한세상")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+
+        person.skills[0].type shouldBe Skill.Type.SOFT
+        person.skills[0].name shouldBe "A passion for problem solving"
+        person.skills[1].type shouldBe Skill.Type.SOFT
+        person.skills[1].name shouldBe "Good communication skills"
+        person.skills[2].type shouldBe Skill.Type.HARD
+        person.skills[2].name shouldBe "Kotlin"
+    }
+
+    "languages" {
+        val person = introduce {
+            name("김경록")
+            company("맘편한세상")
+            skills {
+                soft("White lie")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+
+        person.languages[0].name shouldBe "Korean"
+        person.languages[0].level shouldBe 5
+        person.languages[1].name shouldBe "English"
+        person.languages[1].level shouldBe 3
+    }
+})
