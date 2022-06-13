8d269c5527ca3f43096521d9053f98ead762a4a3
ellen.youn <ellen.youn@kakaocorp.com>
2022-06-13 22:52:05 +0900

    test: 카드덱에서 가져온 카드는 중복되지 않는다.

--- src/test/kotlin/blackjack/domain/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/CardDeckTest.kt
@@ -1,6 +1,8 @@
 package blackjack.domain
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldBeUnique
+import io.kotest.matchers.collections.shouldNotContain
 import io.kotest.matchers.shouldBe
 
 class CardDeckTest : FreeSpec({
@@ -17,5 +19,13 @@ class CardDeckTest : FreeSpec({
     "시작 시 랜덤하게 카드 두개를 가져온다" {
         takeCards = CardDeck.start()
         takeCards.size() shouldBe 2
+    }
+
+    "한번 가져온 카드는 덱에 존재하지 않는다" {
+        CardDeck.all() shouldNotContain takeCards.cards[0]
+    }
+
+    "꺼내온 카드는 중복되지 않는다" {
+        takeCards.cards.shouldBeUnique()
     }
 })
