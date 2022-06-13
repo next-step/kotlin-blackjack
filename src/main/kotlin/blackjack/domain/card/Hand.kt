efa1d5ebe3389218776c6d6d4ad35e3d18db0f91
saintbeller96 <rlawhdtjd9@naver.com>
2022-06-13 21:06:57 +0900

    fix: Hand#isNotEmpty() 제거

--- src/main/kotlin/blackjack/domain/card/Hand.kt
+++ src/main/kotlin/blackjack/domain/card/Hand.kt
@@ -29,8 +29,6 @@ data class Hand(
         _cards.add(card)
     }
 
-    fun isNotEmpty(): Boolean = _cards.isNotEmpty()
-
     fun first(): Card = _cards.first()
 
     override fun toString(): String {
