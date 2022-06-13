cb14fa639d4b22a5f70e2a8d23d4a88841f6a56d
ellen.youn <ellen.youn@kakaocorp.com>
2022-06-13 23:00:45 +0900

    feat: 여러명의 플레이어는 Players에서 관리된다.

---
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -0,0 +1,11 @@
+package blackjack.domain
+
+class Players(private val players: List<Player>) {
+    fun names(): List<String> = players.map { it.name }
+
+    fun all() = players
+
+    companion object {
+        fun of(names: List<String>): Players = Players(List(names.size) { Player(names[it]).start() })
+    }
+}
