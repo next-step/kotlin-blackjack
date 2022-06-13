c0c078bc99be3671cc802e571f3814d6c2261142
YooInKeun <keun0390@naver.com>
2022-06-13 21:23:53 +0900

    chore: 카드 문양 생성 기능 추가

---
+++ src/main/kotlin/blackjack/CardSymbol.kt
@@ -0,0 +1,15 @@
+package blackjack
+
+@JvmInline
+value class CardSymbol private constructor(
+    private val symbol: String,
+) {
+
+    companion object {
+        private val SYMBOLS = listOf("하트", "클로버", "다이아", "스페이드")
+
+        fun generateAllKinds() = SYMBOLS.map { CardSymbol(it) }
+
+        fun from(symbols: List<String>) = symbols.map { CardSymbol(it) }
+    }
+}
