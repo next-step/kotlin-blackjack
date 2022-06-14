23dce98dd3fb494830e303b9daddc54f447aeac2
Jake Son <sonjeabin@gmail.com>
2022-06-15 06:01:30 +0900

    refactor: 수익률 계산을 GameResult 에서 하도록 변경

--- src/main/kotlin/blackjack/domain/GameResult.kt
+++ src/main/kotlin/blackjack/domain/GameResult.kt
@@ -6,7 +6,16 @@ enum class GameResult {
     DEALER_WON,
     DRAW;
 
+    fun getProfit(betAmount: Double): Double =
+        when (this) {
+            PLAYER_BLACKJACK_WON -> betAmount * BLACKJACK_WIN_BONUS_RATE
+            PLAYER_WON -> betAmount
+            DEALER_WON -> -betAmount
+            else -> 0.0
+        }
+
     companion object {
+        private const val BLACKJACK_WIN_BONUS_RATE = 1.5
 
         fun from(dealerScore: Score, playerScore: Score): GameResult =
             when {
--- src/main/kotlin/blackjack/domain/GameStatus.kt
+++ src/main/kotlin/blackjack/domain/GameStatus.kt
@@ -8,24 +8,14 @@ data class GameStatus(
 ) {
 
     fun toProfitResult(): List<ProfitResult> {
-        val players = players.map { ProfitResult(it.name, getPlayerProfit(it)) }
+        val players = players.map { ProfitResult(it.name, it.profit) }
         val dealer = ProfitResult(dealer.name, -players.sumOf { it.profit })
 
         return listOf(dealer) + players
     }
 
-    private fun getPlayerProfit(player: Player): Double {
-        val betAmount = player.hand.betAmount.toDouble()
-
-        return when (GameResult.from(dealer.score, player.score)) {
-            GameResult.PLAYER_BLACKJACK_WON -> betAmount * BLACKJACK_WIN_BONUS_RATE
-            GameResult.PLAYER_WON -> betAmount
-            GameResult.DEALER_WON -> -betAmount
-            else -> 0.0
-        }
-    }
-
-    companion object {
-        private const val BLACKJACK_WIN_BONUS_RATE = 1.5
-    }
+    private val Player.profit: Double
+        get() = GameResult
+            .from(dealer.score, score)
+            .getProfit(hand.betAmount.toDouble())
 }
