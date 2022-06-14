84dc9460d4ead88696de998d298f7192e5c32c92
Juatae-Kim <jutaekim@sk.com>
2022-06-14 19:16:41 +0900

    refactor: Remove needless method

--- src/main/kotlin/blackjack/model/player/PlayerBet.kt
+++ src/main/kotlin/blackjack/model/player/PlayerBet.kt
@@ -9,8 +9,6 @@ data class PlayerBet(val player: Player.Guest, val betMoney: Int = MIN_BET_MONEY
 
 data class PlayerBets(val playerBetList: List<PlayerBet>) : List<PlayerBet> by playerBetList {
 
-    fun Player.findBetMoney(): Int = playerBetList.find { it.player == this }?.betMoney ?: 0
-
     companion object {
 
         fun List<PlayerBet>.toPlayersBets(): PlayerBets = PlayerBets(this)
