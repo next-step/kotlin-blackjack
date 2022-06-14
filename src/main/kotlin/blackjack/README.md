c18d1f368d8aa8631b746dcd1dbbf1378ab83f0a
vivian.bae <vivian.bae@kakaocorp.com>
2022-06-14 23:20:55 +0900

    리뷰 반영 - 점수를 계산할 때 ace가 있고 isBusted가 아닐경우 bonus 를 더하는 식으로 변경

--- src/main/kotlin/blackjack/README.md
+++ src/main/kotlin/blackjack/README.md
@@ -65,11 +65,11 @@ jason카드: 7클로버, K스페이드 - 결과: 17
 - [x] CardTotalTest 에서 상수로 검증하도록 변경
 - [x] isAboveTwentyOne 을 isBusted 로 변경
 - [x] isBlackjack 제거
-- [ ] 점수를 계산할 때 ace가 있고 isBusted가 아닐경우 bonus 를 더하는 식으로 변경
+- [x] 점수를 계산할 때 ace가 있고 isBusted가 아닐경우 bonus 를 더하는 식으로 변경
 - [x] Cards.getAll 메소드 리팩토링
 - [x] CardDeck 생성자에 총 52장의 중복된 카드가 없다는 검증 진행하기
 - [x] 하나 이상의 element 를 가지는 List 생성
 - [x] object 을 비교할 때에는 isEqualTo 사용하기
 - [x] Blackjack은 InputView, Outputview 에 의존하지 않도록 변경
-- [ ] PlayerState 에 isPlaying 필드 추가
-- [ ] 테스트 간결하게 리팩토링
+- [ ] ~~PlayerState 에 isPlaying 필드 추가~~
+- [x] 테스트 간결하게 리팩토링
--- src/main/kotlin/blackjack/domain/card/CardTotal.kt
+++ src/main/kotlin/blackjack/domain/card/CardTotal.kt
@@ -7,24 +7,15 @@ class CardTotal(private val cards: List<Card>) {
     val value: Int
         get() = if (isBusted) getSmallestTotal() else getLargestTotalNotExceedingTwentyOne()
 
-    private fun getAllNonZeroBonuses(): List<Int> = cards
-        .map { it.bonusValue }
-        .filter { it > 0 }
+    private fun getBonus(): Int = cards.firstOrNull { it is Card.Ace }?.bonusValue ?: 0
 
     private fun getSmallestTotal(): Int = cards.sumOf { it.value }
 
     private fun getLargestTotalNotExceedingTwentyOne(): Int {
         val smallestTotal = getSmallestTotal()
-        val bonuses = getAllNonZeroBonuses()
+        val bonus = getBonus()
 
-        var total = smallestTotal
-
-        bonuses.forEach { bonus ->
-            if ((total + bonus).isAboveTwentyOne()) return@forEach
-            total += bonus
-        }
-
-        return total
+        return (smallestTotal + bonus).takeUnless { it.isAboveTwentyOne() } ?: smallestTotal
     }
 
     private fun Int.isAboveTwentyOne() = this > TWENTY_ONE
