ec62ce474fda86ee74cf7e7774ca3438de7415be
pplenty <newtype26@naver.com>
2022-06-15 22:26:50 +0900

    refactor: 피드백반영 - 테스트에서만 사용되는 프로퍼티 제거

--- src/test/kotlin/game/blackjack/domain/DeckTest.kt
+++ src/test/kotlin/game/blackjack/domain/DeckTest.kt
@@ -3,25 +3,32 @@ package game.blackjack.domain
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("카드 덱")
 internal class DeckTest {
 
     @Test
-    fun `덱 초기화 하면 52개 카드 생성`() {
+    fun `덱은 52장의 중복없는 카드를 가진다`() {
         val deck = Deck()
-        Assertions.assertThat(deck.cards).hasSize(52)
+        val drawCard = mutableSetOf<Card>()
+
+        repeat(52) {
+            drawCard.add(deck.draw())
+        }
+
+        Assertions.assertThat(drawCard).hasSize(52)
     }
 
     @Test
-    fun `덱에서 카드 가져오기`() {
-        val expected = 10
+    fun `덱의 모든 카드를 draw 하면 예외가 발생한다`() {
         val deck = Deck()
+        val drawCard = mutableSetOf<Card>()
 
-        repeat(expected) {
-            deck.draw()
+        assertThrows<RuntimeException> {
+            repeat(53) {
+                drawCard.add(deck.draw())
+            }
         }
-
-        Assertions.assertThat(deck.cards).hasSize(52 - expected)
     }
 }
