ccf459dff787139a78e50c1d560bfffeb3c22e7c
austen.lee <austen.lee@kakaocorp.com>
2022-06-16 02:14:30 +0900

    step2 refactor : CardNumberConverterTest 수정

--- src/test/kotlin/blackjack/view/output/converter/CardNumberConverterTest.kt
+++ src/test/kotlin/blackjack/view/output/converter/CardNumberConverterTest.kt
@@ -3,28 +3,26 @@ package blackjack.view.output.converter
 import blackjack.domain.CardNumber
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.EnumSource
+import org.junit.jupiter.params.provider.CsvSource
 
 class CardNumberConverterTest {
     @ParameterizedTest
-    @EnumSource(CardNumber::class)
-    fun `CardNumberConverter는 CardNumber를 출력을 위한 문자열로 변환해 반환한다`(input: CardNumber) {
-        val expected = when (input) {
-            CardNumber.ACE -> "A"
-            CardNumber.TWO -> "2"
-            CardNumber.THREE -> "3"
-            CardNumber.FOUR -> "4"
-            CardNumber.FIVE -> "5"
-            CardNumber.SIX -> "6"
-            CardNumber.SEVEN -> "7"
-            CardNumber.EIGHT -> "8"
-            CardNumber.NINE -> "9"
-            CardNumber.TEN -> "10"
-            CardNumber.JACK -> "J"
-            CardNumber.QUEEN -> "Q"
-            CardNumber.KING -> "K"
-        }
-
-        assertThat(CardNumberConverter.convert(input)).isEqualTo(expected)
+    @CsvSource(
+        "ACE,A",
+        "TWO,2",
+        "THREE,3",
+        "FOUR,4",
+        "FIVE,5",
+        "SIX,6",
+        "SEVEN,7",
+        "EIGHT,8",
+        "NINE,9",
+        "TEN,10",
+        "JACK,J",
+        "QUEEN,Q",
+        "KING,K"
+    )
+    fun `CardNumberConverter는 CardNumber를 출력을 위한 문자열로 변환해 반환한다`(cardNumber: CardNumber, expected: String) {
+        assertThat(CardNumberConverter.convert(cardNumber)).isEqualTo(expected)
     }
 }
