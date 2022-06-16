139dae84de028a13eed4a05b978cb67ee436a46c
justin-s <justin.s@kakaocorp.com>
2022-06-16 22:52:42 +0900

    refactor : 스코어 객체 메시지 전달로 구현

--- src/main/kotlin/blackjack/PlayerDeck.kt
+++ src/main/kotlin/blackjack/PlayerDeck.kt
@@ -10,4 +10,22 @@ class PlayerDeck(cards: List<Card> = emptyList()) {
 
     val cards: List<Card>
         get() = _cards.toList()
+
+    fun calculateScore(): Int {
+        var sumOf = cards.sumOf { it.cardSymbol.score }
+        val aceCount = cards.count { it.cardSymbol == CardSymbol.ACE }
+
+        repeat(aceCount) {
+            if (sumOf + CORRECTION_NUMBER <= BLACKJACK) {
+                sumOf += CORRECTION_NUMBER
+            }
+        }
+
+        return sumOf
+    }
+
+    companion object {
+        private const val CORRECTION_NUMBER = 10
+        private const val BLACKJACK = 21
+    }
 }
--- src/main/kotlin/blackjack/Score.kt
+++ src/main/kotlin/blackjack/Score.kt
@@ -3,33 +3,6 @@ package blackjack
 class Score(val cards: List<Card>) {
 
     fun getScore(): Int {
-
-        val reversed = cards.sortedBy { it.cardSymbol.score }
-            .reversed()
-        var sum = 0
-        reversed.forEach {
-            sum += it.cardSymbol.score
-            if (it.cardSymbol == CardSymbol.ACE) {
-                if (sum + CORRECTION_NUMBER <= BLACKJACK) {
-                    sum += CORRECTION_NUMBER
-                }
-            }
-        }
-
-        var sumOf = cards.sumOf { it.cardSymbol.score }
-        val aceCount = cards.count { it.cardSymbol == CardSymbol.ACE }
-
-        repeat(aceCount) {
-            if (sumOf + CORRECTION_NUMBER <= BLACKJACK) {
-                sumOf += CORRECTION_NUMBER
-            }
-        }
-
-        return sumOf
-    }
-
-    companion object {
-        private const val CORRECTION_NUMBER = 10
-        private const val BLACKJACK = 21
+        return PlayerDeck(cards).calculateScore()
     }
 }
