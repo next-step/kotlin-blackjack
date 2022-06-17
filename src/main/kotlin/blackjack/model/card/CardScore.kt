bc19b078ceb0ca9b7a55c8f0f7030e0293afdbdd
YooInKeun <keun0390@naver.com>
2022-06-17 20:48:42 +0900

    chore: 카드 최소 점수 검증 기능 추가

--- src/main/kotlin/blackjack/model/card/CardScore.kt
+++ src/main/kotlin/blackjack/model/card/CardScore.kt
@@ -5,9 +5,22 @@ class CardScore(
     val otherScore: Int,
 ) {
 
+    init {
+        validateMinScore(score)
+        validateMinScore(otherScore)
+    }
+
     fun plus(other: CardScore): CardScore {
         val sumOfScore = score.plus(other.score)
         val sumOfOtherScore = otherScore.plus(other.otherScore)
         return CardScore(sumOfScore, sumOfOtherScore)
+    }
+
+    private fun validateMinScore(score: Int) {
+        require(score >= MIN_SCORE) { "카드 점수는 최소 ${MIN_SCORE}점 이상이어야 합니다." }
+    }
+
+    companion object {
+        private const val MIN_SCORE = 1
     }
 }
--- src/test/kotlin/blackjack/model/card/CardScoreTest.kt
+++ src/test/kotlin/blackjack/model/card/CardScoreTest.kt
@@ -3,6 +3,8 @@ package blackjack.model.card
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("카드 점수 테스트")
 class CardScoreTest {
@@ -19,5 +21,21 @@ class CardScoreTest {
         // then
         assertThat(sumOfScore.score).isEqualTo(4)
         assertThat(sumOfScore.otherScore).isEqualTo(6)
+    }
+
+    @Test
+    fun `카드 점수가 1점 미만이면 예외 발생`() {
+        // given, when, then
+        assertAll(
+            "validate card min score",
+            {
+                val exception = assertThrows<IllegalArgumentException> { CardScore(0, 1) }
+                assertThat(exception.message).isEqualTo("카드 점수는 최소 1점 이상이어야 합니다.")
+            },
+            {
+                val exception = assertThrows<IllegalArgumentException> { CardScore(1, 0) }
+                assertThat(exception.message).isEqualTo("카드 점수는 최소 1점 이상이어야 합니다.")
+            }
+        )
     }
 }
