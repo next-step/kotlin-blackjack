25f43fd6ffc2a238cd0e5a4afe1bea631cc165bf
YooInKeun <keun0390@naver.com>
2022-06-17 18:58:14 +0900

    chore: 불필요한 import문 제거

--- src/test/kotlin/blackjack/model/CardSymbolTest.kt
+++ src/test/kotlin/blackjack/model/CardSymbolTest.kt
@@ -1,6 +1,5 @@
 package blackjack.model
 
-import blackjack.model.CardSymbol
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
