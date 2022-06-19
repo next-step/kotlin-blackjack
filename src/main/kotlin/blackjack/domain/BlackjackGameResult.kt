0284b9d11d8cf10dcf8ba4be9c16792bc15bb783
austen.lee <austen.lee@kakaocorp.com>
2022-06-20 00:33:27 +0900

    step3 feat : 참가자들을 받아 그들의 블랙잭 게임 결과를 보관하는 BlackjackGameResult와 테스트

---
+++ src/main/kotlin/blackjack/domain/BlackjackGameResult.kt
@@ -0,0 +1,41 @@
+package blackjack.domain
+
+@JvmInline
+value class BlackjackGameResult private constructor(val value: List<BlackjackParticipantResult>) {
+    companion object {
+        fun from(participants: Participants): BlackjackGameResult {
+            val playerResults = participants.players.toResults(participants.dealer)
+            val dealerResult = participants.dealer.toResult(playerResults)
+
+            val results = mutableListOf(dealerResult).apply {
+                addAll(playerResults)
+            }
+
+            return BlackjackGameResult(results)
+        }
+
+        private fun List<Participant.Player>.toResults(dealer: Participant.Dealer): List<BlackjackParticipantResult> {
+            return map { player ->
+                BlackjackParticipantResult(
+                    name = player.name,
+                    hands = player.hands,
+                    matchStatus = player matchWith dealer
+                )
+            }
+        }
+
+        private fun Participant.Dealer.toResult(playerResults: List<BlackjackParticipantResult>): BlackjackParticipantResult {
+            val dealerMatchStatusMap = playerResults.map { playerResult ->
+                playerResult.matchStatus.inverse()
+            }.groupingBy { matchStatus ->
+                matchStatus
+            }.eachCount()
+
+            return BlackjackParticipantResult(
+                name = name,
+                hands = hands,
+                matchStatus = MatchStatus.Dealer.from(dealerMatchStatusMap)
+            )
+        }
+    }
+}
