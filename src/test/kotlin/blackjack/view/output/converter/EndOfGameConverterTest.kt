86a27f9a0e0a75279f08c048989e309d6c99f39f
austen.lee <austen.lee@kakaocorp.com>
2022-06-20 01:23:12 +0900

    step3 refactor : EndOfGameConverter가 BlackjackGameResult를 받도록

--- src/test/kotlin/blackjack/view/output/converter/EndOfGameConverterTest.kt
+++ src/test/kotlin/blackjack/view/output/converter/EndOfGameConverterTest.kt
@@ -33,7 +33,8 @@ class EndOfGameConverterTest {
         val participants = Participants(dealer, listOf(player1, player2))
         val blackjackGameResult = BlackjackGameResult.from(participants)
 
-        val cardText = "딜러카드: 9다이아몬드, Q하트 - 결과: 19\npanther카드: 2스페이드, A하트, 8스페이드 - 결과: 21\nfox카드: J다이아몬드, 7클로버, 10클로버 - 결과: 27"
+        val cardText =
+            "딜러카드: 9다이아몬드, Q하트 - 결과: 19\npanther카드: 2스페이드, A하트, 8스페이드 - 결과: 21\nfox카드: J다이아몬드, 7클로버, 10클로버 - 결과: 27"
         val matchStatusText = "## 최종 승패\n딜러: 1승 0무 1패\npanther: 승\nfox: 패"
         val expected = "$cardText\n\n$matchStatusText"
         Assertions.assertThat(EndOfGameConverter.convert(blackjackGameResult)).isEqualTo(expected)
