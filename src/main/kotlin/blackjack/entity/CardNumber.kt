0a80bbaa23131a4cc6ac49fd016ee5d7f88dfe28
erie0210 <2j00923@gmail.com>
2022-06-19 16:19:59 +0900

    feat: card number 추가

---
+++ src/main/kotlin/blackjack/entity/CardNumber.kt
@@ -0,0 +1,8 @@
+package blackjack.entity
+
+enum class CardNumber(val value: Int) {
+  ACE(listOf<Int>(1,11).shuffled().first()),
+  TWO(2), THREE(3), FOUR(4), FIVE(5),
+  SIX(6), SEVEN(7), EIGHT(8), NINE(9),
+  TEN(10), JACK(10), QUEEN(10), KING(10);
+}
