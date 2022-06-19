a31cca652867e58301a745d3da9f2d7b64eea017
erie0210 <2j00923@gmail.com>
2022-06-19 16:21:57 +0900

    feat: person 추가

--- src/main/kotlin/blackjack/entity/Person.kt
+++ src/main/kotlin/blackjack/entity/Person.kt
@@ -1,4 +1,9 @@
 package blackjack.entity
 
 interface Person {
+  val name: String
+  val wallet:Wallet
+  val limit: Int
+  fun draw(answer: String): Wallet
+
 }
--- src/main/kotlin/blackjack/entity/Player.kt
+++ src/main/kotlin/blackjack/entity/Player.kt
@@ -1,2 +1,11 @@
-package blackjack.entity 
+package blackjack.entity
 
+//todo: limit 이 static 한 변수일 때, inferface에서 선언하는 방법
+class Player(override val name: String, override val wallet: Wallet, override val limit: Int =21) : Person {
+  override fun draw(answer: String): Wallet {
+    if (answer == "n") return this.wallet
+    val cards = this.wallet.cards.toMutableList()
+    cards.add(CardDrawer.drawSingleCard())
+    return Wallet(cards)
+  }
+}
