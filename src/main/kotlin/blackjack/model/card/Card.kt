6f4d09775282c0f3ed22d2d7ca5c6c15d1038c7b
YooInKeun <keun0390@naver.com>
2022-06-19 17:22:31 +0900

    refactor: Card 클래스, data class로 변경

--- src/main/kotlin/blackjack/model/card/Card.kt
+++ src/main/kotlin/blackjack/model/card/Card.kt
@@ -1,28 +1,12 @@
 package blackjack.model.card
 
-class Card(
+data class Card(
     private val symbol: CardSymbol,
     private val number: CardNumber,
 ) {
 
     val score
         get() = number.score
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is Card) return false
-
-        if (symbol != other.symbol) return false
-        if (number != other.number) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = symbol.hashCode()
-        result = 31 * result + number.hashCode()
-        return result
-    }
 
     override fun toString() = "${number.mark}$symbol"
 }
