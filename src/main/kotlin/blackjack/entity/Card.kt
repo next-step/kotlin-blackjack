c3302f97f800575ca942e8318d3fde12b0c3697e
erie0210 <2j00923@gmail.com>
2022-06-19 16:20:35 +0900

    feat: card 추가

--- src/main/kotlin/blackjack/entity/Card.kt
+++ src/main/kotlin/blackjack/entity/Card.kt
@@ -1,6 +1,6 @@
 package blackjack.entity
 
-class Card (val shape:Shape, val number: Int){
+class Card (val shape:Shape, val number: CardNumber){
   companion object{
     const val MINIMUM_CARD_NUMBER = 1
     const val MAXIMUM_CARD_NUMBER = 14
--- src/test/kotlin/blackjack/entity/CardTest.kt
+++ src/test/kotlin/blackjack/entity/CardTest.kt
@@ -9,23 +9,28 @@ import org.junit.jupiter.api.Assertions.*
 internal class CardTest {
 
   @Test
-  fun `DIAMOND 와3을 넣고 Card를 생성하면 number는 3이다`() {
+  fun `DIAMOND 와3을 넣고 Card를 생성하면 Shape는 DIAMON이다`() {
     // given
     val testShape = Shape.DIAMOND
+    val testNumber = CardNumber.NINE
 
     // when
-    val testCard = Card(Shape.DIAMOND, 3)
+    val testCard = Card(Shape.DIAMOND, testNumber)
 
     // then
     Assertions.assertThat(testCard.shape).hasSameClassAs(testShape)
   }
 
   @Test
-  fun `DIAMOND 와3을 넣고 Card를 생성하면 Shape는 DIAMON이다`() {
+  fun `DIAMOND 와 3을 넣고 Card를 생성하면 number는 3이다`() {
+    // given
+    val testShape = Shape.DIAMOND
+    val testNumber = CardNumber.NINE
+
     // when
-    val testCard = Card(Shape.DIAMOND, 3)
+    val testCard = Card(testShape, CardNumber.NINE)
 
     // then
-    Assertions.assertThat(testCard.number).isEqualTo(3)
+    Assertions.assertThat(testCard.number).isEqualTo(9)
   }
 }
