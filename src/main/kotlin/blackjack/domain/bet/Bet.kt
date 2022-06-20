7e4859b4174298d9cee10718744e5b011626b540
vivian.bae <vivian.bae@kakaocorp.com>
2022-06-20 22:42:44 +0900

    게임 결과에 따라 베팅 이익이 달라진다

--- src/main/kotlin/blackjack/domain/bet/Bet.kt
+++ src/main/kotlin/blackjack/domain/bet/Bet.kt
@@ -6,7 +6,16 @@ value class Bet(private val bet: Double) {
         require(bet > 0) { BET_SHOULD_BE_ABOVE_ZERO }
     }
 
+    fun blackjackWin(): Profit = Profit(bet * BLACKJACK_WIN_PROFIT_MULTIPLIER)
+
+    fun win(): Profit = Profit(bet)
+
+    fun push(): Profit = Profit(0.0)
+
+    fun lose(): Profit = Profit(-bet)
+
     companion object {
         private const val BET_SHOULD_BE_ABOVE_ZERO = "베팅 금액은 0보다 큰 숫자이여야 합니다."
+        private const val BLACKJACK_WIN_PROFIT_MULTIPLIER = 1.5
     }
 }
--- src/test/kotlin/blackjack/domain/bet/BetTest.kt
+++ src/test/kotlin/blackjack/domain/bet/BetTest.kt
@@ -1,6 +1,8 @@
 package blackjack.domain.bet
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.assertj.core.api.Assertions.assertThatNoException
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
@@ -8,17 +10,35 @@ import org.junit.jupiter.params.provider.ValueSource
 class BetTest {
     @Test
     fun `베팅 금액은 0 보다 큰 숫자이다`() {
-        Assertions.assertThatNoException().isThrownBy { Bet(0.1) }
+        assertThatNoException().isThrownBy { Bet(0.1) }
     }
 
     @ParameterizedTest
     @ValueSource(doubles = [0.0, -0.1])
     fun `베팅 금액이 0보다 크지 않을 경우 IllegalArgumentException 이 발생한다`(bet: Double) {
-        Assertions.assertThatIllegalArgumentException().isThrownBy { Bet(bet) }
+        assertThatIllegalArgumentException().isThrownBy { Bet(bet) }
     }
 
-//    @Test
-//    fun `블랙잭으로 승리했을 경우 베팅 금액의 1.5배의 이익이 발생한다`() {
-//
-//    }
+    @Test
+    fun `블랙잭으로 승리했을 경우 베팅 금액의 1,5배의 이익이 발생한다`() {
+        val bet = 1000.0
+        assertThat(Bet(bet).blackjackWin().value).isEqualTo(bet * 1.5)
+    }
+
+    @Test
+    fun `블랙잭이 아닌 승리일 경우 베팅 금액만큼의 이익이 발생한다`() {
+        val bet = 1000.0
+        assertThat(Bet(bet).win().value).isEqualTo(bet)
+    }
+
+    @Test
+    fun `푸시가 발생할 경우(무승부) 총 이익은 0 이다`() {
+        assertThat(Bet(1000.0).push().value).isEqualTo(0.0)
+    }
+
+    @Test
+    fun `질 경우 베팅 금액을 잃는다`() {
+        val bet = 1000.0
+        assertThat(Bet(bet).lose().value).isEqualTo(-bet)
+    }
 }
