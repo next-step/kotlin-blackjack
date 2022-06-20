7b309114c15a61ae4f922bab911d79ca33632e0c
pang-yo <pang.yo@kakaocorp.com>
2022-06-21 00:20:25 +0900

    [step2] refactor: BlackJackGame.kt refactoring ( 메서드 위치, 불필요 메서드 삭졔 )

--- src/main/kotlin/blackjack/BlackJackGame.kt
+++ src/main/kotlin/blackjack/BlackJackGame.kt
@@ -20,24 +20,6 @@ class BlackJackGame(
         players.forEach { dealer.giveCards(it, INIT_DRAW_CARD_COUNT) }
     }
 
-    private fun giveCard(player: Player) {
-        player.getCard()
-    }
-
-    private fun Player.getCard() {
-        dealer.giveCard(this)
-    }
-
-    fun getPlayablePlayer(): Player? {
-        return this.players
-            .filterNot { it.bust }
-            .firstOrNull { it.wantToPlay() }
-    }
-
-    private fun stopPlayerBetting(player: Player) {
-        player.stopBetting()
-    }
-
     fun ask(player: Player, needMoreCard: Boolean) {
         require(player in players)
         if (needMoreCard) {
@@ -45,6 +27,20 @@ class BlackJackGame(
         } else {
             stopPlayerBetting(player)
         }
+    }
+
+    private fun giveCard(player: Player) {
+        dealer.giveCard(player)
+    }
+
+    private fun stopPlayerBetting(player: Player) {
+        player.stopBetting()
+    }
+
+    fun getPlayablePlayer(): Player? {
+        return this.players
+            .filterNot { it.bust }
+            .firstOrNull { it.wantToPlay() }
     }
 
     companion object {
