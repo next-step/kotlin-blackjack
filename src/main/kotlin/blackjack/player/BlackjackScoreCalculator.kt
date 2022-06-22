cd0ae4858f56f6714079f588d182f41fa3b50393
pang-yo <pang.yo@kakaocorp.com>
2022-06-22 17:10:37 +0900

    [step2] refactor: BlackjackScoreCalculator.kt 에서 스코어 계산 시 Card에 ace 여부 판단 메세지 보내도록 변경

--- src/main/kotlin/blackjack/player/BlackjackScoreCalculator.kt
+++ src/main/kotlin/blackjack/player/BlackjackScoreCalculator.kt
@@ -1,14 +1,13 @@
 package blackjack.player
 
 import blackjack.card.Card
-import blackjack.card.CardSymbol
 
 object BlackjackScoreCalculator {
     private const val ACE_DIVISION_VALUE = 11
     private const val ACE_ADDITIONAL_VALUE = 10
 
     fun getScore(cards: List<Card>): Int {
-        val aces = cards.filter { it.symbol == CardSymbol.ACE }
+        val aces = cards.filter { it.isAceCard() }
 
         var score = cards.sumOf { it.symbol.value }
 
--- src/test/kotlin/blackjack/card/CardTest.kt
+++ src/test/kotlin/blackjack/card/CardTest.kt
@@ -1,6 +1,8 @@
 package blackjack.card
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertDoesNotThrow
 
 class CardTest {
@@ -8,5 +10,16 @@ class CardTest {
     @Test
     fun createCard() {
         assertDoesNotThrow { Card(Suit.CLUB, CardSymbol.ACE) }
+    }
+
+    @Test
+    fun `카드가 에이스카드인지 알 수 있다`() {
+        val card1 = Card(Suit.CLUB, CardSymbol.ACE)
+        val card2 = Card(Suit.CLUB, CardSymbol.KING)
+
+        assertAll(
+            { assertThat(card1.isAceCard()).isEqualTo(true) },
+            { assertThat(card2.isAceCard()).isEqualTo(false) },
+        )
     }
 }
