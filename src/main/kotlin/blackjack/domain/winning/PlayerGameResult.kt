047658b4dd092632a90017cf2fe43b9e9a483c75
ujusy <lsyeon1997@ajou.ac.kr>
2022-06-22 20:49:30 +0900

    refactor: 플레이어 승리 상태에 따라 게임 결과 반환하도록 수정

--- src/main/kotlin/blackjack/domain/winning/PlayerGameResult.kt
+++ src/main/kotlin/blackjack/domain/winning/PlayerGameResult.kt
@@ -4,7 +4,5 @@ import blackjack.domain.player.Player
 
 data class PlayerGameResult(
     val player: Player,
-    val winCount: Int,
-    val looseCount: Int,
-    val tieCount: Int
+    val playerWinningState: WinningState
 )
--- src/main/kotlin/blackjack/domain/winning/WinningStat.kt
+++ src/main/kotlin/blackjack/domain/winning/WinningStat.kt
@@ -1,40 +1,51 @@
 package blackjack.domain.winning
 
+import blackjack.domain.player.Player
 import blackjack.domain.score.PlayerScore
 
 class WinningStat(
     playerScores: List<PlayerScore>,
     dealerScore: PlayerScore,
 ) {
-    private val scores: List<PlayerScore> = playerScores + dealerScore
+    private val scores: List<PlayerScore> = playerScores
     private val dealerScore: Int = dealerScore.score
+    private val dealer: Player = dealerScore.player
+    val result = playerGameResult()
 
     fun playerGameResult(): List<PlayerGameResult> {
         return scores.map {
-            PlayerGameResult(it.player, win(it.score), loose(it.score), tie(it.score))
+            PlayerGameResult(
+                it.player,
+                playerWinningState(it)
+            )
         }
     }
 
-    fun dealerWinning(): Boolean {
-        if (dealerScore >= BLACK_JACK_SCORE) return false
-        return true
+    private fun playerWinningState(playerScore: PlayerScore): WinningState {
+        return when {
+            dealerBust() -> WinningState.DEALER_BUST
+            playerBust(playerScore) -> WinningState.PLAYER_BUST
+            playerBlackJack(playerScore) -> WinningState.PLAYER_BLACKJACK
+            playerBlackJack(playerScore) && dealerBlackJack() -> WinningState.PLAYER_TIE
+            else -> when (compareTo(playerScore.score)) {
+                0 -> WinningState.PLAYER_TIE
+                1 -> WinningState.PLAYER_WIN
+                else -> WinningState.PLAYER_LOOSE
+            }
+        }
     }
 
-    private fun win(score: Int): Int {
-        return scores.count() { it -> it.score > score }
-    }
+    private fun dealerBust(): Boolean = dealerScore > BLACK_JACK_SCORE
 
-    private fun loose(score: Int): Int {
-        return scores.count() { it -> it.score < score }
-    }
+    private fun dealerBlackJack(): Boolean = (dealerScore == BLACK_JACK_SCORE && dealer.cards.size == 2)
 
-    private fun tie(score: Int): Int {
-        val sameScoreCount = scores.count() { it -> it.score == score }
-        return sameScoreCount - SELF_SAME_COUNT
-    }
+    private fun playerBust(playerScore: PlayerScore): Boolean = playerScore.score > BLACK_JACK_SCORE
+
+    private fun playerBlackJack(playerScore: PlayerScore): Boolean = (playerScore.score == BLACK_JACK_SCORE && playerScore.player.cards.size == 2)
+
+    private fun compareTo(score: Int): Int = score.compareTo(dealerScore)
 
     companion object {
         private const val BLACK_JACK_SCORE = 21
-        private const val SELF_SAME_COUNT = 1
     }
 }
