757f9ea3e57d24016f7c9b6758870391531919ea
pang-yo <pang.yo@kakaocorp.com>
2022-06-23 17:10:24 +0900

    [step2] refactor: CardViewMapper.kt 매핑 변경

--- src/main/kotlin/blackjack/ui/output/CardViewMapper.kt
+++ src/main/kotlin/blackjack/ui/output/CardViewMapper.kt
@@ -5,35 +5,30 @@ import blackjack.card.CardSymbol
 import blackjack.card.Suit
 
 object CardViewMapper {
+    private val suitBy = mapOf(
+        Suit.HEART to "하트",
+        Suit.SPADE to "스페이드",
+        Suit.DIAMOND to "다이아몬드",
+        Suit.CLUB to "클로버",
+    )
+
+    private val symbolBy = mapOf(
+        CardSymbol.ACE to "A",
+        CardSymbol.TWO to "2",
+        CardSymbol.THREE to "3",
+        CardSymbol.FOUR to "4",
+        CardSymbol.FIVE to "5",
+        CardSymbol.SIX to "6",
+        CardSymbol.SEVEN to "7",
+        CardSymbol.EIGHT to "8",
+        CardSymbol.NINE to "9",
+        CardSymbol.TEN to "10",
+        CardSymbol.JACK to "J",
+        CardSymbol.QUEEN to "Q",
+        CardSymbol.KING to "K",
+    )
 
     fun toView(card: Card): String {
-        return card.symbol.toView() + card.suit.toView()
-    }
-
-    private fun Suit.toView(): String {
-        return when (this) {
-            Suit.HEART -> "하트"
-            Suit.SPADE -> "스페이드"
-            Suit.DIAMOND -> "다이아몬드"
-            Suit.CLUB -> "클로버"
-        }
-    }
-
-    private fun CardSymbol.toView(): String {
-        return when (this) {
-            CardSymbol.ACE -> "A"
-            CardSymbol.TWO -> "2"
-            CardSymbol.THREE -> "3"
-            CardSymbol.FOUR -> "4"
-            CardSymbol.FIVE -> "5"
-            CardSymbol.SIX -> "6"
-            CardSymbol.SEVEN -> "7"
-            CardSymbol.EIGHT -> "8"
-            CardSymbol.NINE -> "9"
-            CardSymbol.TEN -> "10"
-            CardSymbol.JACK -> "J"
-            CardSymbol.QUEEN -> "Q"
-            CardSymbol.KING -> "K"
-        }
+        return symbolBy[card.symbol] + suitBy[card.suit]
     }
 }
