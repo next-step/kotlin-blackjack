b29036ae0595485bc1ec7c5174c533696438ece2
justin.sg <justin.sg@kakaocorp.com>
2022-06-24 12:32:33 +0900

    test : Score을 이용하여 plus, equal, greater을 판단한다.

---
+++ src/test/kotlin/blackjack/domain/ScoreTest.kt
@@ -0,0 +1,23 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class ScoreTest {
+
+    @Test
+    fun `Score 덧셈 확인`() {
+        assertThat(Score(10) + Score(20)).isEqualTo(Score(30))
+    }
+
+    @Test
+    fun `isEqual 확인`() {
+        assertThat(Score(10) == Score(10)).isEqualTo(true)
+    }
+
+    @Test
+    fun `Score 초과 비교 확인`() {
+        assertThat(Score(10).isGreater(Score(5))).isEqualTo(true)
+    }
+}
