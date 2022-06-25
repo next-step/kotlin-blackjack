0863ab4a699075441791acfa236aecd78f6da115
austen.lee <austen.lee@kakaocorp.com>
2022-06-25 20:46:01 +0900

    step4 refactor : BlackjackLayout에서 PlayerInfo를 받도록

--- src/main/kotlin/blackjack/view/BlackjackLayout.kt
+++ src/main/kotlin/blackjack/view/BlackjackLayout.kt
@@ -1,12 +1,15 @@
 package blackjack.view
 
+import blackjack.domain.BetAmount
 import blackjack.domain.BlackjackGameTurn
 import blackjack.domain.Dealer
 import blackjack.domain.Participant
 import blackjack.domain.Player
+import blackjack.domain.PlayerInfo
 import blackjack.domain.PlayerName
 import blackjack.view.input.InputView
 import blackjack.view.input.UserInputRequest
+import blackjack.view.input.converter.BetAmountConverter
 import blackjack.view.input.converter.PlayerNamesConverter
 import blackjack.view.input.converter.YesOrNoConverter
 import blackjack.view.output.OutputView
@@ -17,12 +20,16 @@ import blackjack.viewmodel.BlackjackViewModel
 
 object BlackjackLayout {
     private const val GUIDANCE_MESSAGE_PLAYERS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val GUIDANCE_MESSAGE_BET_AMOUNT = "의 배팅 금액은?"
     private const val GUIDANCE_MESSAGE_HIT = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
     private const val GUIDANCE_MESSAGE_DEALER_HIT = "딜러는 16이하라 한 장의 카드를 더 받았습니다."
     private val DEALER_NAME = PlayerName("딜러")
 
     fun execute() {
-        val viewModel = BlackjackViewModel.from(DEALER_NAME, getPlayerNames())
+        val playerInfos = getPlayerNames().map { playerName ->
+            playerName.toPlayerInfo()
+        }
+        val viewModel = BlackjackViewModel.from(DEALER_NAME, playerInfos.map { it.name })
         OutputView.println(viewModel.participants.all, StartOfGameConverter)
 
         viewModel.currentTurn.observe { turn ->
@@ -41,6 +48,19 @@ object BlackjackLayout {
         val userInputRequest = UserInputRequest(
             message = GUIDANCE_MESSAGE_PLAYERS_NAME,
             inputConverter = PlayerNamesConverter
+        )
+
+        return InputView.receiveUserInput(userInputRequest)
+    }
+
+    private fun PlayerName.toPlayerInfo(): PlayerInfo {
+        return PlayerInfo(this, getBetAmount(this))
+    }
+
+    private fun getBetAmount(playerName: PlayerName): BetAmount {
+        val userInputRequest = UserInputRequest(
+            message = "${playerName.value}$GUIDANCE_MESSAGE_BET_AMOUNT",
+            inputConverter = BetAmountConverter
         )
 
         return InputView.receiveUserInput(userInputRequest)
