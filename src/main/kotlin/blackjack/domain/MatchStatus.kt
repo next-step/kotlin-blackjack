66d8cdb720465dddedbfe7a045a9f07de847b35a
austen.lee <austen.lee@kakaocorp.com>
2022-06-26 19:21:25 +0900

    step4 refactor : opposite와 Dealer는 이제 필요 없음

--- src/main/kotlin/blackjack/domain/MatchStatus.kt
+++ src/main/kotlin/blackjack/domain/MatchStatus.kt
@@ -1,47 +1,25 @@
 package blackjack.domain
 
 sealed interface MatchStatus {
-    data class Dealer(val win: Int, val push: Int, val lose: Int) : MatchStatus {
-        companion object {
-            fun from(matchStatusMap: Map<Player, Int>): Dealer {
-                return Dealer(
-                    win = matchStatusMap[Win] ?: 0,
-                    push = matchStatusMap[Push] ?: 0,
-                    lose = matchStatusMap[Lose] ?: 0
-                )
-            }
-        }
-    }
-
     sealed interface Player : MatchStatus {
         val revenueRatio: Double
-
-        fun opposite(): Player
 
         fun getRevenueFrom(betAmount: BetAmount): Revenue = Revenue((betAmount.value * revenueRatio).toInt())
     }
 
     object Blackjack : Player {
         override val revenueRatio: Double = 1.5
-
-        override fun opposite(): Player = this
     }
 
     object Win : Player {
         override val revenueRatio: Double = 1.0
-
-        override fun opposite(): Player = Lose
     }
 
     object Lose : Player {
         override val revenueRatio: Double = -1.0
-
-        override fun opposite(): Player = Win
     }
 
     object Push : Player {
         override val revenueRatio: Double = 0.0
-
-        override fun opposite(): Player = this
     }
 }
