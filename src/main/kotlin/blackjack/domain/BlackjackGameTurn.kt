49ed1e74625f093698dc20f547b5a1b53ca04237
austen.lee <austen.lee@kakaocorp.com>
2022-06-27 02:23:29 +0900

    step4 feat : 다시 생각해 보니 필요 없을 듯...

--- src/main/kotlin/blackjack/domain/BlackjackGameTurn.kt
+++ src/main/kotlin/blackjack/domain/BlackjackGameTurn.kt
@@ -4,14 +4,6 @@ package blackjack.domain
 value class BlackjackGameTurn private constructor(val participant: Participant) {
     fun isTurnEnd(): Boolean = !participant.isReceivable()
 
-    sealed interface Result {
-        data class Hit(val player: Participant) : Result
-
-        object Stay : Result
-
-        object Dealer : Result
-    }
-
     companion object {
         fun from(participants: Participants): BlackjackGameTurn {
             val participant = participants.players.find { player ->
