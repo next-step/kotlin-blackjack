b85604445f2cd5b277889ee502011cad596a55b4
austen.lee <austen.lee@kakaocorp.com>
2022-06-26 20:58:33 +0900

    step4 refactor : 필요 없는 거 같아서 다시 지움...

--- src/main/kotlin/blackjack/view/output/converter/PlayerNameConverter.kt
+++
@@ -1,9 +0,0 @@
-package blackjack.view.output.converter
-
-import blackjack.domain.PlayerName
-
-object PlayerNameConverter : OutputConverter<PlayerName> {
-    override fun convert(printable: PlayerName): String {
-        return "${printable.value} 카드"
-    }
-}
