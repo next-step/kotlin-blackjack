6a32d363d9297b7433ab2fdc9a734f321dbce753
austen.lee <austen.lee@kakaocorp.com>
2022-06-26 19:00:53 +0900

    step4 feat : Player에 수익률과 Revenue를 구하는 기능 추가

--- src/main/kotlin/blackjack/domain/MatchStatus.kt
+++ src/main/kotlin/blackjack/domain/MatchStatus.kt
@@ -14,18 +14,34 @@ sealed interface MatchStatus {
     }
 
     sealed interface Player : MatchStatus {
+        val revenueRatio: Double
+
         fun opposite(): Player
+
+        fun getRevenueFrom(betAmount: BetAmount): Revenue = Revenue((betAmount.value * revenueRatio).toInt())
+    }
+
+    object Blackjack : Player {
+        override val revenueRatio: Double = 1.5
+
+        override fun opposite(): Player = this
     }
 
     object Win : Player {
+        override val revenueRatio: Double = 1.0
+
         override fun opposite(): Player = Lose
     }
 
     object Lose : Player {
+        override val revenueRatio: Double = -1.0
+
         override fun opposite(): Player = Win
     }
 
     object Push : Player {
+        override val revenueRatio: Double = 0.0
+
         override fun opposite(): Player = this
     }
 }
