acabe7d4e6775a00c14e3935b247e8b3664d58d0
tim.aeom <tim.aeom@kakaocorp.com>
2022-06-28 22:18:32 +0900

    refactor: DEALER_STAY_NUMBER 상수화

--- src/main/kotlin/camp/nextstep/blackjack/game/DealerActionProducer.kt
+++ src/main/kotlin/camp/nextstep/blackjack/game/DealerActionProducer.kt
@@ -4,8 +4,10 @@ import camp.nextstep.blackjack.player.Dealer
 
 object DealerActionProducer {
 
+    const val DEALER_STAY_NUMBER = 17
+
     fun produce(dealer: Dealer): Action {
-        return if (Score.of(dealer.hand) < Score.of(17)) Action.HIT
+        return if (Score.of(dealer.hand) < Score.of(DEALER_STAY_NUMBER)) Action.HIT
         else Action.STAY
     }
 }
