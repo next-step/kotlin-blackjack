84776abe66096555b439ba0319a9af459f4758ee
tim.aeom <tim.aeom@kakaocorp.com>
2022-06-28 21:46:37 +0900

    refactor: Gambler init block 위로 이동

--- src/main/kotlin/camp/nextstep/blackjack/player/Gambler.kt
+++ src/main/kotlin/camp/nextstep/blackjack/player/Gambler.kt
@@ -6,13 +6,13 @@ import camp.nextstep.blackjack.card.Hand
 
 class Gambler(override val name: String) : Player {
 
+    init {
+        require(name.isNotBlank()) { "올바른 플레이어 이름을 입력해주세요. ($name)" }
+    }
+
     override val hand = Hand()
 
     override fun receive(card: Card) {
         hand.add(DrawnCard(card).apply { turnUp() })
-    }
-
-    init {
-        require(name.isNotBlank()) { "올바른 플레이어 이름을 입력해주세요. ($name)" }
     }
 }
