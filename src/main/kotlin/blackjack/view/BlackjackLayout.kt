d9773f432e17ab78b40b862ef2dde52f01334303
austen.lee <austen.lee@kakaocorp.com>
2022-06-28 20:02:48 +0900

    step4 refactor : BlackjackLayout가 Contract.View를 구현

--- src/main/kotlin/blackjack/view/BlackjackLayout.kt
+++ src/main/kotlin/blackjack/view/BlackjackLayout.kt
@@ -1,7 +1,10 @@
 package blackjack.view
 
+import blackjack.Contract
 import blackjack.domain.BetAmount
+import blackjack.domain.BlackjackGameResult
 import blackjack.domain.Dealer
+import blackjack.domain.Participants
 import blackjack.domain.Player
 import blackjack.domain.PlayerInfo
 import blackjack.domain.PlayerName
@@ -16,7 +19,7 @@ import blackjack.view.output.converter.PlayerConverter
 import blackjack.view.output.converter.StartOfGameConverter
 import blackjack.viewmodel.BlackjackViewModel
 
-object BlackjackLayout {
+object BlackjackLayout : Contract.View {
     private const val GUIDANCE_MESSAGE_PLAYERS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     private const val GUIDANCE_MESSAGE_BET_AMOUNT = "의 배팅 금액은?"
     private const val GUIDANCE_MESSAGE_HIT = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)"
@@ -42,7 +45,7 @@ object BlackjackLayout {
         OutputView.print(viewModel.getBlackjackGameResult(), EndOfGameConverter)
     }
 
-    private fun getPlayerNames(): List<PlayerName> {
+    override fun getPlayerNames(): List<PlayerName> {
         val userInputRequest = UserInputRequest(
             message = GUIDANCE_MESSAGE_PLAYERS_NAME,
             inputConverter = PlayerNamesConverter
@@ -51,11 +54,11 @@ object BlackjackLayout {
         return InputView.receiveUserInput(userInputRequest)
     }
 
-    private fun PlayerName.toPlayerInfo(): PlayerInfo {
+    fun PlayerName.toPlayerInfo(): PlayerInfo {
         return PlayerInfo(this, getBetAmount(this))
     }
 
-    private fun getBetAmount(playerName: PlayerName): BetAmount {
+    override fun getBetAmount(playerName: PlayerName): BetAmount {
         val userInputRequest = UserInputRequest(
             message = "${playerName.value}$GUIDANCE_MESSAGE_BET_AMOUNT",
             inputConverter = BetAmountConverter
@@ -64,12 +67,28 @@ object BlackjackLayout {
         return InputView.receiveUserInput(userInputRequest)
     }
 
-    private fun isPlayerWannaHit(player: Player): Boolean {
+    override fun isPlayerWannaHit(player: Player): Boolean {
         val userInputRequest = UserInputRequest(
             message = "${player.name.value}$GUIDANCE_MESSAGE_HIT",
             inputConverter = YesOrNoConverter
         )
 
         return InputView.receiveUserInput(userInputRequest)
+    }
+
+    override fun showStartOfGameInfo(participants: Participants) {
+        OutputView.println(participants, StartOfGameConverter)
+    }
+
+    override fun showPlayersHitEvent(player: Player) {
+        OutputView.print(player, PlayerConverter)
+    }
+
+    override fun showDealerHitEvent() {
+        OutputView.printlnOnlyMessage(GUIDANCE_MESSAGE_DEALER_HIT)
+    }
+
+    override fun showEndOfGameInfo(blackjackGameResult: BlackjackGameResult) {
+        OutputView.print(blackjackGameResult, EndOfGameConverter)
     }
 }
