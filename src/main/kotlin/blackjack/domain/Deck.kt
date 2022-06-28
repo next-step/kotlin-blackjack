508f8898a0422f97d3ea282809d2a8f93b0811b9
Rok93 <kr.kim@mfort.co.kr>
2022-06-29 02:06:27 +0900

    feat: 덱에 카드가 부족할 때, 첫 턴의 카드를 뽑으면 예외를 발생시킨다

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -2,12 +2,16 @@ package blackjack.domain
 
 import blackjack.domain.card.Card
 
+private const val NEED_FIRST_TURN_CARD_NUMBER = 2
+private const val NEED_MIN_DRAW_CARD_NUMBER = 1
+
 class Deck(
     cards: List<Card>,
 ) {
     private val _cards: MutableList<Card> = cards.toMutableList()
 
     fun pullOutFirstTurn(): List<Card> {
+        require(_cards.size >= NEED_FIRST_TURN_CARD_NUMBER) { "나눠줄 덱의 카드가 부족합니다. 현재 카드 수 : ${_cards.size}장" }
         return listOf(
             _cards.removeFirst(),
             _cards.removeFirst(),
@@ -15,6 +19,7 @@ class Deck(
     }
 
     fun pullOut(): Card {
+        require(_cards.size >= NEED_MIN_DRAW_CARD_NUMBER) { "덱의 카드가 비었습니다." }
         return _cards.removeFirst()
     }
 
--- src/test/kotlin/blackjack/domain/DeckTest.kt
+++ src/test/kotlin/blackjack/domain/DeckTest.kt
@@ -1,5 +1,9 @@
 package blackjack.domain
 
+import blackjack.domain.card.Card
+import blackjack.domain.card.Face
+import blackjack.domain.card.Suit
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -13,5 +17,21 @@ class DeckTest : FunSpec({
 
         // then
         actual.size shouldBe 2
+    }
+
+    test("덱에 카드가 부족할 때, 첫 턴의 카드를 뽑으면 예외를 발생시킨다.") {
+        // given
+        val deck = Deck(listOf(Card(Suit.DIAMOND, Face.NINE)))
+
+        // when // then
+        shouldThrowExactly<IllegalArgumentException> { deck.pullOutFirstTurn() }
+    }
+
+    test("덱에 카드가 부족할 때, 카드를 뽑으면 예외를 발생시킨다.") {
+        // given
+        val deck = Deck(emptyList())
+
+        // when // then
+        shouldThrowExactly<IllegalArgumentException> { deck.pullOut() }
     }
 })
