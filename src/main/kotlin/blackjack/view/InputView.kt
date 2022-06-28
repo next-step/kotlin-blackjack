f13ba816bcacfba6b9ab322b13edf190bab91c41
Rok93 <kr.kim@mfort.co.kr>
2022-06-29 02:14:52 +0900

    refactor: 매직넘버 및 문자열 상수로 치환

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,17 +1,21 @@
 package blackjack.view
 
 object InputView {
+    private const val NAME_DELIMITER = ","
+    private const val YES = "y"
+    private const val NO = "n"
+
     fun inputPlayerNames(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        return readLine()!!.split(",")
+        return readln().split(NAME_DELIMITER)
             .map { it.trim() }
     }
 
     fun askDrawCard(playerName: String): Boolean {
         println("${playerName}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-        return when (readLine()!!) {
-            "y" -> true
-            "n" -> false
+        return when (readln()) {
+            YES -> true
+            NO -> false
             else -> askDrawCard(playerName)
         }
     }
