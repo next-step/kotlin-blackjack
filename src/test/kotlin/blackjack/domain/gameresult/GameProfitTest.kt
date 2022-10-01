af40c39286077e9d7e890d0313c1e5eaa7bc4596
GyeongrokKim <kr.kim@mfort.co.kr>
2022-10-01 21:48:36 +0900

    test: GameProfit 연산자 테스트 케이스 추가

--- src/test/kotlin/blackjack/domain/gameresult/GameProfitTest.kt
+++ src/test/kotlin/blackjack/domain/gameresult/GameProfitTest.kt
@@ -1,8 +1,31 @@
 package blackjack.domain.gameresult
 
+import blackjack.domain.GameProfit.GameProfit
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import java.math.BigDecimal
 
-// TODO 2022-10-01 경록: 좀 더, 세분화된 테스트코드 짜기!
 class GameProfitTest : StringSpec({
-    // TODO 2022-10-01 경록: operator test 추가하기!
+    "게임 이득 객체의 값을 음의 값으로 바꾼다." {
+        // given
+        val gameProfit = GameProfit(BigDecimal.valueOf(1_000L))
+
+        // when
+        val result = !gameProfit
+
+        // then
+        result.value shouldBe BigDecimal(-1_000L)
+    }
+
+    "게임 이득 객체를 더한다." {
+        // given
+        val gameProfit = GameProfit(BigDecimal.valueOf(1_000L))
+        val additionalGameProfit = GameProfit(BigDecimal.valueOf(2_000L))
+
+        // when
+        val result = gameProfit + additionalGameProfit
+
+        // then
+        result.value shouldBe BigDecimal(3_000L)
+    }
 })
