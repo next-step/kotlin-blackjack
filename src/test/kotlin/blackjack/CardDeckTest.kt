60c5268d0557a09094ce5eb377f7e8887be4f668
harris.yi <harris.yi@kakaostyle.com>
2022-11-24 20:24:46 +0900

    test: Validate CardDeck deal more than 52

---
+++ src/test/kotlin/blackjack/CardDeckTest.kt
@@ -0,0 +1,15 @@
+package blackjack
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+
+class CardDeckTest : StringSpec({
+
+    "카드를 52개를 초과하여 deal하면 IllegalStateException이 발생한다." {
+        shouldThrow<IllegalStateException> { CardDeck().deal(53) }
+            .shouldHaveMessage("더 이상 카드가 없습니다.")
+    }
+
+})
