d4fdd173dc0389650239b02a70a5ee8e9ab24b8c
harris.yi <harris.yi@kakaostyle.com>
2022-11-28 21:04:18 +0900

    feat: Change view classes to object

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -3,30 +3,28 @@ package blackjack.view
 import blackjack.Card
 import blackjack.Player
 
-class OutputView {
-    companion object {
-        fun printFirstDeal(players: List<String>) {
-            println("${players.joinToString(", ")} 에게 2장의 카드를 나누었습니다.")
-        }
+object OutputView {
+    fun printFirstDeal(players: List<String>) {
+        println("${players.joinToString(", ")} 에게 2장의 카드를 나누었습니다.")
+    }
 
-        fun printCardsByPlayer(playerDtos: List<PlayerDto>) {
-            playerDtos.forEach { printCardsByPlayer(it) }
-        }
+    fun printCardsByPlayer(playerDtos: List<PlayerDto>) {
+        playerDtos.forEach { printCardsByPlayer(it) }
+    }
 
-        fun printCardsByPlayer(playerDto: PlayerDto) {
-            print("${playerDto.name}카드: ")
-            println(playerDto.cards.joinToString(", ") { it.korean })
-        }
+    fun printCardsByPlayer(playerDto: PlayerDto) {
+        print("${playerDto.name}카드: ")
+        println(playerDto.cards.joinToString(", ") { it.korean })
+    }
 
-        fun printResult(playerDtos: List<PlayerDto>) {
-            playerDtos.forEach { printResultByPlayer(it) }
-        }
+    fun printResult(playerDtos: List<PlayerDto>) {
+        playerDtos.forEach { printResultByPlayer(it) }
+    }
 
-        private fun printResultByPlayer(playerDto: PlayerDto) {
-            print("${playerDto.name}카드: ")
-            print(playerDto.cards.joinToString(", ") { it.korean })
-            println(" - 결과: ${playerDto.totalPoint}")
-        }
+    private fun printResultByPlayer(playerDto: PlayerDto) {
+        print("${playerDto.name}카드: ")
+        print(playerDto.cards.joinToString(", ") { it.korean })
+        println(" - 결과: ${playerDto.totalPoint}")
     }
 }
 
@@ -35,5 +33,9 @@ data class PlayerDto(
     val cards: Set<Card>,
     val totalPoint: Int,
 ) {
-    constructor(player: Player) : this(player.name, player.playingCards.cards, player.playingCards.calculatePoint().value)
+    constructor(player: Player) : this(
+        player.name,
+        player.playingCards.cards,
+        player.playingCards.calculatePoint().value
+    )
 }
