068002a2958e348a3b7e5524d9ed4aaa3077bedc
EP <kjuioqq8@gmail.com>
2022-11-29 23:04:33 +0900

    feat: DSL 테스트 구현

---
+++ src/test/kotlin/study/DslTest.kt
@@ -0,0 +1,116 @@
+package study
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+/**
+ introduce {
+ name("김동인")
+ company("ep")
+ skills {
+ soft("A passion for problem solving")
+ soft("Good communication skills")
+ hard("Kotlin")
+ }
+ languages {
+ "Korean" level 5
+ "English" level 3
+ }
+}
+ */
+class DslTest {
+    @Test
+    fun introduce() {
+        val person: Person = introduce {
+            name("김동인")
+            company("ep")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.name shouldBe "김동인"
+        person.company shouldBe "ep"
+        person.skills shouldBe Skills(soft = listOf("A passion for problem solving", "Good communication skills"), hard = listOf("Kotlin"))
+        person.languages shouldBe Languages(languages = listOf(Language("Korean", 5), Language("English", 3)))
+    }
+}
+
+fun introduce(block: PersonBuilder.() -> Unit): Person {
+    val person = PersonBuilder().apply {
+        block()
+    }
+    return person.build()
+}
+
+class PersonBuilder {
+    private lateinit var name: String
+    private lateinit var company: String
+    private lateinit var skills: Skills
+    private lateinit var languages: Languages
+
+    fun name(value: String) {
+        this.name = value
+    }
+
+    fun company(value: String) {
+        this.company = value
+    }
+
+    fun skills(block: SkillBuilder.() -> Unit) {
+        val skills = SkillBuilder().apply {
+            block()
+        }
+        this.skills = skills.build()
+    }
+
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        val languages = LanguagesBuilder().apply {
+            block()
+        }
+        this.languages = languages.build()
+    }
+
+    fun build(): Person {
+        return Person(name, company, skills, languages)
+    }
+}
+
+class SkillBuilder {
+    private val soft: MutableList<String> = mutableListOf()
+    private val hard: MutableList<String> = mutableListOf()
+
+    fun soft(value: String) {
+        soft.add(value)
+    }
+
+    fun hard(value: String) {
+        hard.add(value)
+    }
+
+    fun build(): Skills {
+        return Skills(soft.toList(), hard.toList())
+    }
+}
+
+class LanguagesBuilder {
+    private val languages: MutableList<Language> = mutableListOf()
+
+    infix fun String.level(level: Int) {
+        languages.add(Language(language = this, level = level))
+    }
+
+    fun build(): Languages {
+        return Languages(languages = languages.toList())
+    }
+}
+
+data class Person(val name: String, val company: String, val skills: Skills, val languages: Languages)
+data class Skills(val soft: List<String>, val hard: List<String>)
+data class Languages(val languages: List<Language>)
+data class Language(val language: String, val level: Int)
