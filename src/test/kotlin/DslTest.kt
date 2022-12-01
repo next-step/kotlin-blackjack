241684c384106de6411aeebf111c3daeb30383a8
chayoung-choi <chayoung.dev@gmail.com>
2022-12-01 22:32:10 +0900

    test: Dsl Test

---
+++ src/test/kotlin/DslTest.kt
@@ -0,0 +1,63 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+/*
+introduce {
+  name("최차영")
+  company("회사")
+  skills {
+    soft("A passion for problem solving")
+    soft("Good communication skills")
+    hard("Kotlin")
+  }
+  languages {
+    "Korean" level 5
+    "English" level 3
+  }
+}
+ */
+class DslTest {
+    @ValueSource(strings = ["최차영", "이든"])
+    @ParameterizedTest
+    fun introduce(value: String) {
+        val person = introduce {
+            name(value)
+        }
+        assertThat(person.name).isEqualTo(value)
+    }
+
+    @Test
+    fun company() {
+        val person = introduce {
+            name("최차영")
+            company("회사")
+        }
+        assertThat(person.name).isEqualTo("최차영")
+        assertThat(person.company).isEqualTo("회사")
+    }
+}
+
+fun introduce(block: PersonBuilder.() -> Unit): Person {
+    return PersonBuilder().apply(block).build()
+}
+
+class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+
+    fun name(value: String) {
+        name = value
+    }
+
+    fun company(value: String) {
+        company = value
+    }
+
+    fun build(): Person {
+        return Person(name, company)
+    }
+}
+
+data class Person(val name: String, val company: String?)
