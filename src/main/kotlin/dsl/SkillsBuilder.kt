ce74091a79a8a42c4e4737923c162105932a01ed
chayoung-choi <chayoung.dev@gmail.com>
2022-12-02 23:29:56 +0900

    feat: SkillsBuilder

---
+++ src/main/kotlin/dsl/SkillsBuilder.kt
@@ -0,0 +1,20 @@
+package dsl
+
+class SkillsBuilder {
+    private val soft = mutableListOf<String>()
+    private val hard = mutableListOf<String>()
+
+    fun soft(value: String) {
+        soft.add(value)
+    }
+
+    fun hard(value: String) {
+        hard.add(value)
+    }
+
+    fun build(): Skills {
+        return Skills(soft, hard)
+    }
+}
+
+data class Skills(val soft: List<String>, val hard: List<String>)
