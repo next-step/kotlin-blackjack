c801344c9d917482d31b3714de7bfb62a4c4d5b2
chayoung-choi <chayoung.dev@gmail.com>
2022-12-02 23:29:24 +0900

    feat: LanguagesBuilder

---
+++ src/main/kotlin/dtl/LanguagesBuilder.kt
@@ -0,0 +1,14 @@
+package dtl
+
+class LanguagesBuilder {
+    private val languages = mutableListOf<Language>()
+
+    infix fun String.level(value: Int) = languages.add(Language(this, value))
+
+    fun build(): Languages {
+        return Languages(languages)
+    }
+}
+
+data class Language(val type: String, val level: Int)
+data class Languages(val list: List<Language>)
