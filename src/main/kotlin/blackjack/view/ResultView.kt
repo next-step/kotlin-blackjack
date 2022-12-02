6b484cdbe0e3ca691b398c4703001ee7ab4626cc
shinaaaa <shin029abc@gmail.com>
2022-12-02 22:43:25 +0900

    feat: 카드 2장 나누어 주는 ResultView 추가

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -1,4 +1,11 @@
 package blackjack.view
 
+import blackjack.domain.Participant
+
 object ResultView {
+
+    fun handingOutCards(participants: List<Participant>) {
+        val names = participants.joinToString { it.name }
+        println("${names}에게 2장의 나누었습니다.")
+    }
 }
