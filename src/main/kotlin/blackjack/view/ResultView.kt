b08e635702810946b931c62dac413c4d2c296b4c
shinaaaa <shin029abc@gmail.com>
2022-12-02 23:30:18 +0900

    feat: 선수의 현재 상태를 조회 추가

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -1,11 +1,18 @@
 package blackjack.view
 
 import blackjack.domain.Participant
+import blackjack.domain.Player
 
 object ResultView {
 
     fun handingOutCards(participants: List<Participant>) {
         val names = participants.joinToString { it.name }
         println("${names}에게 2장의 나누었습니다.")
+    }
+
+    fun getCurrentStatus(vararg player: Player) {
+        player.forEach {
+            println("${it.name.name}카드: ${it.cards.joinToString { card -> "${card.number.number}${card.type.text}" }}")
+        }
     }
 }
