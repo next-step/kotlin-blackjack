7408a01378ecbeabd7bd5d6bf317edc36481139f
Wonjun <aa3991@office.uc.ac.kr>
2022-12-04 00:11:02 +0900

    feature(dsl): implements step1 kotlinDSL (#324)

---
+++ src/test/kotlin/dsl/KotlinTest.kt
@@ -0,0 +1,38 @@
+package dsl
+
+import io.kotest.matchers.shouldBe
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class KotlinTest {
+    @Test
+    fun stringTest() {
+        assertThat("Kotlin".lastChar()).isEqualTo('n')
+    }
+
+    @Test
+    fun infix() {
+        1 to "1"
+    }
+
+    @Test
+    fun operator() {
+        val actual = Point(0, 1) + Point(1, 2)
+        actual shouldBe Point(1, 3)
+    }
+
+//    @Test
+//    fun operator2() {
+//        val actual = !5
+//        actual shouldBe 5 * 4 * 3 * 2 * 1
+//    }
+
+    data class Point(val x: Int, val y: Int) {
+        operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
+    }
+}
+
+fun String.lastChar(): Char {
+    return get(length - 1)
+}
