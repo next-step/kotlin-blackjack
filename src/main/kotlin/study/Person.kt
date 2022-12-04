39698ae00d0467bffe2a84ebd5d92bd0ea2d8aa2
giyeon95 <giyeon15@gmail.com>
2022-12-04 12:39:00 +0900

    🚀 1단계 - 코틀린 DSL (#323)
    * feat: 1단계 요구사항 name, company, skills 구현
    
    * feat: 1단계 요구사항 languages 구현

---
+++ src/main/kotlin/study/Person.kt
@@ -0,0 +1,36 @@
+package study
+
+class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+    private var skills: Skills? = null
+    private var languages: List<Language> = emptyList()
+
+    fun name(value: String) {
+        name = value
+    }
+
+    fun company(value: String) {
+        company = value
+    }
+
+    fun skills(block: SkillBuilder.() -> Unit) {
+        skills = SkillBuilder().apply(block).build()
+    }
+
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        languages = LanguagesBuilder().apply(block).build()
+    }
+
+
+    fun build(): Person {
+        return Person(name, company, skills, languages)
+    }
+}
+
+data class Person(
+    val name: String,
+    val company: String?,
+    val skills: Skills?,
+    val languages: List<Language>
+)
