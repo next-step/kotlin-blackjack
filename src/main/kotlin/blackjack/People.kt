231d51b5eceb7d24ef03941fa62d37859fdf00fe
SangHyunGil <zizon5941@gmail.com>
2022-12-05 00:00:58 +0900

    feat: person의 리스트를 가지는 People 구현

---
+++ src/main/kotlin/blackjack/People.kt
@@ -0,0 +1,17 @@
+package blackjack
+
+import blackjack.util.Parser
+
+data class People(
+    private val nameStr: String,
+    val people: List<Person> = Parser.parse(nameStr).map { name -> Person(name) }
+) : List<Person> by people {
+
+    override fun toString(): String {
+        return people.joinToString(DELIMITER) { "$it - ${it.getScore()}" }
+    }
+
+    companion object {
+        private const val DELIMITER = "\n"
+    }
+}
