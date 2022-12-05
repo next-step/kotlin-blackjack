6087fa5a7906b4bd71be8517cb490f381d6bb1d7
이상인/wefu0000 <wefu.child@gmail.com>
2022-12-06 01:33:57 +0900

    feat: 카드 숫자 중 1은 Ace로 표시한다

--- src/main/kotlin/blackjack/CardNumber.kt
+++ src/main/kotlin/blackjack/CardNumber.kt
@@ -1,3 +1,12 @@
 package blackjack
 
-class CardNumber(val value: Int = (1..13).random())
+class CardNumber(val value: Int = (1..13).random()) {
+
+    fun toName(): String = when (value) {
+        1 -> "Ace"
+        11 -> "Jack"
+        12 -> "Queen"
+        13 -> "Jack"
+        else -> value.toString()
+    }
+}
--- src/test/kotlin/blackjack/CardTest.kt
+++ src/test/kotlin/blackjack/CardTest.kt
@@ -3,6 +3,7 @@ package blackjack
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.ints.shouldBeInRange
+import io.kotest.matchers.shouldBe
 
 class CardTest : StringSpec({
 
@@ -16,7 +17,12 @@ class CardTest : StringSpec({
     "카드의 숫자는 1-13까지 부여 받을 수 있다" {
         val card = Card()
 
-        println(card.cardShape.name)
         card.number.value shouldBeInRange (1..13)
+    }
+
+    "카드 숫자 중 1은 Ace로 표시한다" {
+        val card = Card(CARD_SHAPE.Clover, CardNumber(1))
+
+        card.number.toName() shouldBe "Ace"
     }
 })
