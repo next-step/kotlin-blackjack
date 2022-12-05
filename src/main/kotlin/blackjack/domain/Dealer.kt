6d56d428765eef0214d93a35895d07b0b01f396b
chayoung-choi <chayoung.dev@gmail.com>
2022-12-05 23:01:20 +0900

    feat: Dealer divede, shuffle

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,10 +1,14 @@
 package blackjack.domain
 
-class Dealer(val cards: Cards = initCards()) {
+class Dealer(deck: Cards = Deck().cards, val cards: Cards = Cards()) {
+    private var _list: MutableList<Card> = deck.list.toMutableList()
 
-    companion object {
-        fun initCards(): Cards {
-            return Cards()
-        }
+    val deck: Cards
+        get() = Cards(_list.toList())
+
+    fun divede(): Card = _list.removeFirst()
+
+    fun shuffle() {
+        _list = _list.shuffled().toMutableList()
     }
 }
