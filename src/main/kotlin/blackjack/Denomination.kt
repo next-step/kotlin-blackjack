6a7efde416124c56dd776567b869e2fd73671041
wonjun3991 <wonjun.choi@meshkorea.net>
2022-12-06 23:14:55 +0900

    feature(Denomination): create Denomination

---
+++ src/main/kotlin/blackjack/Denomination.kt
@@ -0,0 +1,16 @@
+package blackjack
+
+enum class Denomination(val score: Int) {
+    ACE(1),
+    SECOND(2),
+    THIRD(3),
+    FOURTH(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    JACK(10),
+    KING(10),
+    QUEEN(10);
+}
---
+++ src/test/kotlin/blackjack/DenominationTest.kt
@@ -0,0 +1,30 @@
+package blackjack
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class DenominationTest {
+    @Test
+    fun `2부터 9까지는 숫자 그대로 따라간다`() {
+        Denomination.SECOND.score shouldBe 2
+        Denomination.THIRD.score shouldBe 3
+        Denomination.FOURTH.score shouldBe 4
+        Denomination.FIVE.score shouldBe 5
+        Denomination.SIX.score shouldBe 6
+        Denomination.SEVEN.score shouldBe 7
+        Denomination.EIGHT.score shouldBe 8
+        Denomination.NINE.score shouldBe 9
+    }
+
+    @Test
+    fun `Jack, King, Queen은 10으로 취급한다`() {
+        Denomination.KING shouldBe 10
+        Denomination.QUEEN shouldBe 10
+        Denomination.JACK shouldBe 10
+    }
+
+    @Test
+    fun `Ace는 1로 취급한다`() {
+        Denomination.ACE shouldBe 1
+    }
+}
