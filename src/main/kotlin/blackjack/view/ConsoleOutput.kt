a22b9466cc51b32da81fa574074ba150512dd86b
chayoung-choi <chayoung.dev@gmail.com>
2022-12-06 17:57:21 +0900

    feat: ConsoleOutput

--- src/main/kotlin/blackjack/view/ConsoleOutput.kt
+++ src/main/kotlin/blackjack/view/ConsoleOutput.kt
@@ -1,6 +1,21 @@
 package blackjack.view
 
+import blackjack.domain.Constants.INITIAL_CARD_COUNT
+import blackjack.domain.Player
+import blackjack.domain.Players
+
 class ConsoleOutput {
-    fun printInitialCard() {
+    fun printInitialCards(players: Players) {
+        println("${players.list.joinToString { it.name }}에게 ${INITIAL_CARD_COUNT}장의 카드를 나누었습니다.")
+        players.list.map { println(getPlayerInfo(it)) }
+        println()
     }
+
+    fun printPlayerCards(player: Player) = getPlayerInfo(player)
+
+    fun printGameResult(players: Players) {
+        players.list.map { println("${getPlayerInfo(it)} - 결과:") }
+    }
+
+    private fun getPlayerInfo(player: Player) = "${player.name}카드: ${player.cards.toString()}"
 }
