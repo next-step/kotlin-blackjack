525582e7a73f5aba7dc92364e3a0df941a69e6a0
chayoung-choi <chayoung.dev@gmail.com>
2022-12-06 17:56:08 +0900

    feat: Cards toString()

--- src/main/kotlin/blackjack/domain/Cards.kt
+++ src/main/kotlin/blackjack/domain/Cards.kt
@@ -4,11 +4,13 @@ class Cards(list: List<Card> = listOf()) {
     private val _list: MutableList<Card> = list.toMutableList()
 
     val list: List<Card>
-        get() = _list.toList()
+        get() = _list.toList().sortedBy { it.number.ordinal }
+
+    override fun toString(): String = list.joinToString()
 
     fun add(card: Card) {
         _list.add(card)
     }
 
-    fun count(): Int = _list.size
+    fun count(): Int = list.size
 }
