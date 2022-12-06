f63d517750dc7c119dd6d85b009098cdbae0aa0d
chayoung-choi <chayoung.dev@gmail.com>
2022-12-06 18:00:26 +0900

    feat: blackjack game method

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,7 +1,15 @@
 package blackjack.domain
 
+import blackjack.domain.Constants.BLACKJACK_SCORE
+
 class Player(val name: String, val cards: Cards = Cards()) {
     init {
         require(!name.isNullOrBlank()) { "이름은 빈값이 될 수 없습니다." }
     }
+
+    fun hit(card: Card) = cards.add(card)
+
+    fun countingCard(): Int = Score.countingCard(cards)
+
+    fun isBurst(): Boolean = countingCard() > BLACKJACK_SCORE
 }
