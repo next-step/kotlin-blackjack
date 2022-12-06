f44dfa57636c5dead6c5c13e8f5e037f3f735af4
chayoung-choi <chayoung.dev@gmail.com>
2022-12-06 22:56:31 +0900

    test: Card & Score test case

--- src/test/kotlin/blackjack/domain/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -4,12 +4,22 @@ import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class CardsTest {
+
+    object FakeCards {
+        val cards = Cards(
+            listOf(
+                Card(CardNumber.of("K"), CardShape.HEART),
+                Card(CardNumber.of("4"), CardShape.DIAMOND),
+                Card(CardNumber.of("7"), CardShape.SPADE)
+            )
+        )
+    }
+
     @Test
     fun `카드를 추가할 수 있다`() {
         val cards = Cards()
         cards.count() shouldBe 0
-        cards.add(Card(CardNumber.of("K"), CardShape.HEART))
-        cards.add(Card(CardNumber.of("4"), CardShape.DIAMOND))
-        cards.count() shouldBe 2
+        FakeCards.cards.list.map { cards.add(it) }
+        cards.count() shouldBe FakeCards.cards.count()
     }
 }
