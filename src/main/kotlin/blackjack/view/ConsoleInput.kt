5be6fba8ff6d9a564001b59a79c703675086e2e2
chayoung-choi <chayoung.dev@gmail.com>
2022-12-06 17:57:03 +0900

    feat: ConsoleInput inputScratch()

--- src/main/kotlin/blackjack/view/ConsoleInput.kt
+++ src/main/kotlin/blackjack/view/ConsoleInput.kt
@@ -3,7 +3,6 @@ package blackjack.view
 import blackjack.domain.Player
 import blackjack.domain.Players
 
-const val DELIMITER_NAMES = ","
 
 class ConsoleInput {
 
@@ -13,10 +12,22 @@ class ConsoleInput {
         val names = readln()
         require(names.isNotBlank()) { "잘못된 입력값입니다." }
 
-        val players = Players(splitNames(names).map { Player(it) })
+        val players = getPlayers(names)
         println()
         return players
     }
 
+    fun inputScratch(player: Player): Boolean {
+        println("${player.name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        val isHit = readln()
+        require(isHit in listOf("y", "n")) { "y 또는 n으로 의사를 입력해주세요." }
+        return "y" == isHit
+    }
+
+    private fun getPlayers(names: String): Players = Players(splitNames(names).map { Player(it) })
     private fun splitNames(names: String): List<String> = names.split(DELIMITER_NAMES).map { it.trim() }
+
+    companion object {
+        const val DELIMITER_NAMES = ","
+    }
 }
