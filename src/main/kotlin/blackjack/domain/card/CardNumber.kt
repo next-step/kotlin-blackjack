5887fdadfe51e6390c985202a1a8cf623dce6935
shinaaaa <shin029abc@gmail.com>
2022-12-06 16:24:56 +0900

    refactor: 카드 숫자를 enum으로 변경 및 카드 총합 계산 추가

--- src/main/kotlin/blackjack/domain/card/CardNumber.kt
+++ src/main/kotlin/blackjack/domain/card/CardNumber.kt
@@ -1,4 +1,32 @@
 package blackjack.domain.card
 
-@JvmInline
-value class CardNumber(val number: Int)
+enum class CardNumber(val text: String, vararg val value: Int) {
+    ACE("A", 1, 11),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEEN("10", 10),
+    KING("K", 10),
+    QUEEN("Q", 10),
+    JACK("J", 10);
+
+    companion object {
+        fun getRandomNumber() = values().random()
+
+        fun calculate(numbers: List<CardNumber>): List<Int> {
+            val sum = numbers.sumOf { it.value.first() }
+            val countOfAce = numbers.count { it == ACE }
+
+            val result = mutableListOf(sum)
+            repeat(countOfAce) {
+                result.add(result[it] + ACE.value[1] - ACE.value[0])
+            }
+            return result
+        }
+    }
+}
