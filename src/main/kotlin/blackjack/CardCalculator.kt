76df9ed5321c2ea4a4625432c64fa53c3fa2ca35
이상인/wefu0000 <wefu.child@gmail.com>
2022-12-07 00:44:52 +0900

    feat: Ace는 1 또는 11로 계산할 수 있다

--- src/main/kotlin/blackjack/CardCalculator.kt
+++ src/main/kotlin/blackjack/CardCalculator.kt
@@ -1,5 +1,25 @@
 package blackjack
 
+import kotlin.math.abs
+
 class CardCalculator(private val cards: List<Card>) {
-    fun sum() = cards.sumOf { it.number.value }
+    fun sum(): Int {
+        val sum = cards.sumOf { it.number.value }
+        return sumChangeAce(sum)
+    }
+
+    private fun sumChangeAce(sum: Int): Int {
+        var changeSum = sum
+        cards.filter { it.number.value == 1 }.forEach { _ ->
+            val sumDiff = abs(STD_NUMBER - sum)
+            val changeSumDiff = abs(STD_NUMBER - (changeSum + 10))
+            if (sumDiff > changeSumDiff)
+                changeSum += 10
+        }
+        return changeSum
+    }
+
+    companion object {
+        const val STD_NUMBER = 21
+    }
 }
--- src/test/kotlin/blackjack/CalculatorTest.kt
+++ src/test/kotlin/blackjack/CalculatorTest.kt
@@ -13,4 +13,22 @@ class CalculatorTest : StringSpec({
 
         cardCalculator.sum() shouldBe inputValues.sumOf { it.value }
     }
+
+    "Ace는 1로 계산할 수 있다" {
+        val inputValues = listOf(CardNumber(1), CardNumber(11), CardNumber(10))
+        val cardCalculator = CardCalculator(
+            inputValues.map { Card(CARD_SHAPE.Clover, it) }
+        )
+
+        cardCalculator.sum() shouldBe inputValues.sumOf { it.value }
+    }
+
+    "Ace는 11로 계산할 수 있다" {
+        val inputValues = listOf(CardNumber(1), CardNumber(2), CardNumber(3))
+        val cardCalculator = CardCalculator(
+            inputValues.map { Card(CARD_SHAPE.Clover, it) }
+        )
+
+        cardCalculator.sum() shouldBe inputValues.sumOf { it.value } + 10
+    }
 })
