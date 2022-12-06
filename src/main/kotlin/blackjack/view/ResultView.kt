a3c410a3b05a5ee0a70b49896620363cf4bcdf19
shinaaaa <shin029abc@gmail.com>
2022-12-06 17:28:30 +0900

    feat: 최종 결과 ResultView 추가

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -1,18 +1,25 @@
 package blackjack.view
 
-import blackjack.domain.Participant
 import blackjack.domain.Player
 
 object ResultView {
 
-    fun handingOutCards(participants: List<Participant>) {
-        val names = participants.joinToString { it.name }
+    fun handingOutCards(players: List<Player>) {
+        println()
+        val names = players.joinToString { it.name }
         println("${names}에게 2장의 나누었습니다.")
     }
 
-    fun getCurrentStatus(vararg player: Player) {
-        player.forEach {
-            println("${it.name.name}카드: ${it.cards.joinToString { card -> "${card.number.number}${card.type.text}" }}")
+    fun getCurrentStatus(players: List<Player>) {
+        players.forEach {
+            println("${it.name}카드: ${it.cards.joinToString { card -> "${card.number.text}${card.type.text}" }}")
+        }
+        println()
+    }
+
+    fun getTotalScore(players: List<Player>) {
+        players.forEach {
+            println("${it.name}카드: ${it.cards.joinToString { card -> "${card.number.text}${card.type.text}" }} - 결과: ${it.score}")
         }
     }
 }
