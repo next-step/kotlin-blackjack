a849f848d775934e83a8a27213d97c3771a1da8c
chayoung-choi <chayoung.dev@gmail.com>
2022-12-06 18:01:03 +0900

    style: Players companion object

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,9 +1,11 @@
 package blackjack.domain
 
-const val MAX_PLAYER_COUNT = 8
-
 class Players(val list: List<Player>) {
     init {
         require(list.size <= MAX_PLAYER_COUNT) { "게임 정원을 초과하였습니다." }
+    }
+
+    companion object {
+        const val MAX_PLAYER_COUNT = 8
     }
 }
