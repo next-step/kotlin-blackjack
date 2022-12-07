a119aec0cd12371111395e3df58768b084d22d72
이상인/wefu0000 <wefu.child@gmail.com>
2022-12-08 00:32:17 +0900

    refactor: Ace 조건 수정

--- src/main/kotlin/blackjack/CardCalculator.kt
+++ src/main/kotlin/blackjack/CardCalculator.kt
@@ -4,13 +4,13 @@ import kotlin.math.abs
 
 class CardCalculator(private val cards: List<Card>) {
     fun sum(): Int {
-        val sum = cards.sumOf { if (it.number.value > 10) 10 else it.number.value }
+        val sum = cards.sumOf { it.number.value }
         return sumChangeAce(sum)
     }
 
     private fun sumChangeAce(sum: Int): Int {
         var changeSum = sum
-        cards.filter { it.number.value == 1 }.forEach { _ ->
+        cards.filter { it.number is CardNumber.Ace }.forEach { _ ->
             val sumDiff = abs(STD_NUMBER - sum)
             val changeSumDiff = abs(STD_NUMBER - (changeSum + 10))
             if (sumDiff > changeSumDiff)
