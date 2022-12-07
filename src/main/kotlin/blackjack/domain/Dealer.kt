f0857b4da095fde4b1cfbe0d227f738cfb91dbda
dreamboy1339 <dreamboy1339@gmail.com>
2022-12-07 23:42:17 +0900

    study(step2): 플레이어 출력 시 처리 수정

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,8 +1,15 @@
 package blackjack.domain
 
-data class Dealer(override val name: String = "dealer", override val cards: Cards = Cards()) : Participant(name, cards) {
+data class Dealer(
+    override val name: String = "dealer",
+    override val cards: Cards = Cards()
+) : Participant(name, cards) {
 
     private val dealerCards: DealerCards = DealerCards()
+
+    override fun toString(): String {
+        return "${name}카드: $cards"
+    }
 
     fun extractCard(): Card = dealerCards.getCard()
 }
--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,11 @@
 package blackjack.domain
 
-data class Player(override val name: String, override val cards: Cards = Cards()) : Participant(name, cards)
+data class Player(
+    override val name: String,
+    override val cards: Cards = Cards()
+) : Participant(name, cards) {
+
+    override fun toString(): String {
+        return "${name}카드: $cards"
+    }
+}
