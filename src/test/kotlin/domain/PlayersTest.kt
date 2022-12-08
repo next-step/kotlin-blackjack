922badf98dbff000da23191d4ce7a682d2146af0
devcs96 <katd6@naver.com>
2022-12-09 00:06:39 +0900

    :art: style(lint) : 린트 적용

--- src/test/kotlin/domain/PlayersTest.kt
+++ src/test/kotlin/domain/PlayersTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class PlayersTest {
 
     @Test
-    fun `���� ������ �÷��̾ ��ȯ�˴ϴ�`() {
+    fun `현재 차례의 플레이어가 반환됩니다`() {
         val players = Players(listOf(Player.withName("pobi"), Player.withName("crong")))
 
         players.currentPlayer() shouldBe Player.withName("pobi")
@@ -14,7 +14,7 @@ class PlayersTest {
     }
 
     @Test
-    fun `�÷��̾ ������ �׸��� �� �ֽ��ϴ�`() {
+    fun `플레이어가 게임을 그만둘 수 있습니다`() {
         val players = Players(listOf(Player.withName("pobi"), Player.withName("crong")))
         val currentPlayer = players.currentPlayer()
 
