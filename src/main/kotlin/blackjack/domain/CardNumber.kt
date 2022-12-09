805fefffb1976cd5cc3961559ca493905767d684
chayoung-choi <chayoung.dev@gmail.com>
2022-12-09 15:08:48 +0900

    refactor: CardNumber score 포함

--- src/main/kotlin/blackjack/domain/CardNumber.kt
+++ src/main/kotlin/blackjack/domain/CardNumber.kt
@@ -1,19 +1,23 @@
 package blackjack.domain
 
-enum class CardNumber(val value: String) {
-    ACE("A"),
-    TWO("2"),
-    THREE("3"),
-    FOUR("4"),
-    FIVE("5"),
-    SIX("6"),
-    SEVEN("7"),
-    EIGHT("8"),
-    NINE("9"),
-    TEN("10"),
-    JACK("J"),
-    QUEEN("Q"),
-    KING("K");
+const val SPECIAL_CARD_SCORE = 10
+const val ACE_CARD_MIN_SCORE = 1
+const val ACE_CARD_MAX_SCORE = 11
+
+enum class CardNumber(val value: String, val score: Int) {
+    ACE("A", ACE_CARD_MIN_SCORE),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", SPECIAL_CARD_SCORE),
+    QUEEN("Q", SPECIAL_CARD_SCORE),
+    KING("K", SPECIAL_CARD_SCORE);
 
     companion object {
         fun of(value: String): CardNumber {
