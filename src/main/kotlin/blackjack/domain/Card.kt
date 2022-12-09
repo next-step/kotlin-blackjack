350f3ab4f98f5c1b2a673acd9e2e20a7b4e5dab0
dreamboy1339 <dreamboy1339@gmail.com>
2022-12-09 14:48:49 +0900

    study(step2): CardNumber 에서 점수를 가져오도록 변경

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -3,11 +3,7 @@ package blackjack.domain
 data class Card(private val cardNumber: CardNumber, private val suit: Suit) {
 
     fun getScore(isAceEleven: Boolean = false): Int {
-        return when (val number = cardNumber) {
-            CardNumber.Ace -> if (isAceEleven) 11 else 1
-            CardNumber.King, CardNumber.Queen, CardNumber.Jack -> 10
-            else -> number.rawValue.toInt()
-        }
+        return cardNumber.getScore(isAceEleven)
     }
 
     override fun toString(): String {
--- src/main/kotlin/blackjack/domain/CardNumber.kt
+++ src/main/kotlin/blackjack/domain/CardNumber.kt
@@ -1,19 +1,19 @@
 package blackjack.domain
 
-enum class CardNumber(val rawValue: String) {
+enum class CardNumber(val rawValue: String, val getScore: (Boolean) -> Int) {
 
-    Ace("A"),
-    Two("2"),
-    Three("3"),
-    Four("4"),
-    Five("5"),
-    Six("6"),
-    Seven("7"),
-    Eight("8"),
-    Nine("9"),
-    TEN("10"),
-    King("K"),
-    Queen("Q"),
-    Jack("J")
+    Ace("A", { if (it) 11 else 1 }),
+    Two("2", { 2 }),
+    Three("3", { 3 }),
+    Four("4", { 4 }),
+    Five("5", { 5 }),
+    Six("6", { 6 }),
+    Seven("7", { 7 }),
+    Eight("8", { 8 }),
+    Nine("9", { 9 }),
+    TEN("10", { 10 }),
+    King("K", { 10 }),
+    Queen("Q", { 10 }),
+    Jack("J", { 10 })
     ;
 }
