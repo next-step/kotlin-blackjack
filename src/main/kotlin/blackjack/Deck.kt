c283440b4153c37462e06578f2101b4f34379a8f
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-10 15:11:37 +0900

    feature(Deck): 랜덤으로 1장의 카드를 뽑을 수 있고 뽑으면 덱의 카드수가 줄어든다.

--- src/main/kotlin/blackjack/Deck.kt
+++ src/main/kotlin/blackjack/Deck.kt
@@ -1,3 +1,13 @@
 package blackjack
 
-data class Deck(val cards: List<Card> = Card.DECK)
+class Deck(cards: List<Card> = Card.DECK) {
+    private val _cards: MutableList<Card> = cards.toMutableList()
+    val cards: List<Card>
+        get() = _cards.toList()
+    fun pick(): Card {
+        val shuffledCards = _cards.shuffled()
+        val card = shuffledCards.toMutableList().removeFirst()
+        _cards.remove(card)
+        return card
+    }
+}
--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -6,9 +6,19 @@ import io.kotest.matchers.shouldBe
 internal class DeckTest : BehaviorSpec({
     given("블랙잭에서는") {
         `when`("최초로 게임을 시작했을 때") {
-            val initDeck = Deck()
+            val deck = Deck()
             then("52장의 카드 덱을 가진다.") {
-                initDeck.cards.size shouldBe 52
+                deck.cards.size shouldBe 52
+            }
+        }
+    }
+
+    given("게임을 최초로 시작했을 때 또는 게임 중간에") {
+        val deck = Deck()
+        `when`("플레이어에게 한 장을 주면") {
+            deck.pick()
+            then("덱의 카드 수가 1 줄어든다.") {
+                deck.cards.size shouldBe 51
             }
         }
     }
