bc6cb2a8bd8ff1e2297190f72a975ba64a84ca2d
dreamboy1339 <dreamboy1339@gmail.com>
2022-12-10 11:58:50 +0900

    study(step2): CardTest 수정

--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -6,27 +6,12 @@ import org.junit.jupiter.api.Test
 
 internal class CardTest {
 
-    @DisplayName("숫자 카드가 올바르게 생성된다")
+    @DisplayName("카드는 숫자 1종과 무늬 1종을 가진다")
     @Test
     fun numberCard() {
         val card = Card(CardNumber.Seven, Suit.Heart)
 
-        "$card" shouldBe "7하트"
-    }
-
-    @DisplayName("K, J, Q 메이저 카드가 올바르게 생성된다")
-    @Test
-    fun majorCard() {
-        val card = Card(CardNumber.Queen, Suit.Spade)
-
-        "$card" shouldBe "Q스페이드"
-    }
-
-    @DisplayName("Ace 카드가 올바르게 생성된다")
-    @Test
-    fun aceCard() {
-        val card = Card(CardNumber.Ace, Suit.Diamond)
-
-        "$card" shouldBe "A다이아몬드"
+        card.cardNumber shouldBe CardNumber.Seven
+        card.suit shouldBe Suit.Heart
     }
 }
