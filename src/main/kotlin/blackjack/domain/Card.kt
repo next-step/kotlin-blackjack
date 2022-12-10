a7dc3c40aa128b2d77eef5c0e4b385dccf442d48
jay.lee <stkment@gmail.com>
2022-12-11 01:27:13 +0900

    :white_check_mark: Card 생성, 테스트 시나리오 및 설정 코드 추가

---
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -0,0 +1,7 @@
+package blackjack.domain
+
+import blackjack.model.CardShape
+import blackjack.model.CardType
+
+class Card(val type: CardType, val shape: CardShape) {
+}
---
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -0,0 +1,28 @@
+package blackjack.domain
+
+import blackjack.model.CardShape
+import blackjack.model.CardType
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+internal class CardTest {
+
+    @DisplayName("각 카드 타입 별로 유효한 값(value)과 점수 및 모양을 가지고 있다.")
+    @ParameterizedTest
+    @MethodSource("provideValidCard")
+    fun validate(card: Card, value: String, score: Int, shape: String) {
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideValidCard(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(Card(CardType.ACE, CardShape.CLOVER), "A", 1, "클로버"),
+                Arguments.of(Card(CardType.NINE, CardShape.SPADE), "9", 9, "스페이드"),
+                Arguments.of(Card(CardType.JACK, CardShape.HEART), "J", 10, "하트")
+            )
+    }
+}
