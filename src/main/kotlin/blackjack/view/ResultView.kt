057c1d06ce454060187f650641e1a69512927b21
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-10 21:27:09 +0900

    feature(View): 게임 UI 뷰 작성

--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -6,7 +6,14 @@ import blackjack.domain.Player
 object ResultView {
     fun printInitialStatus() {
         printPlayersName(Game.players)
-        printPlayersCardStatus(Game.players)
+        printPlayersStatus(Game.players)
+    }
+
+    fun printResult() {
+        println()
+        Game.players.forEach {
+            println("${it.name} 카드: ${it.cards} - 결과: ${it.cards.getScore()}")
+        }
     }
 
     private fun printPlayersName(players: List<Player>) {
@@ -15,11 +22,11 @@ object ResultView {
         println("\n$names 에게 2장의 카드를 나누었습니다.")
     }
 
-    private fun printPlayersCardStatus(players: List<Player>) {
-        players.forEach { player ->
-            val cards = player.cards.values
-                .joinToString(", ") { it.toString() }
-            println("${player.name} 카드: $cards")
-        }
+    private fun printPlayersStatus(players: List<Player>) {
+        players.forEach { printPlayerStatus(it) }
+    }
+
+    fun printPlayerStatus(player: Player) {
+        println("${player.name} 카드: ${player.cards}")
     }
 }
