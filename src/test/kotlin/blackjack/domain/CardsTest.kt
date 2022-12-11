478c0e738de07ead773270255d93edd49942c0c4
jay.lee <stkment@gmail.com>
2022-12-11 20:39:01 +0900

    :white_check_mark: Cards 테스트코드 리팩토링

--- src/test/kotlin/blackjack/domain/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -7,10 +7,10 @@ import org.junit.jupiter.params.provider.ValueSource
 
 internal class CardsTest {
 
-    @DisplayName("카드 모양, 종류를 조합하여 총 52장의 카드(덱) 생성된다.")
+    @DisplayName("카드 모양, 종류를 조합하여 총 52장의 카드(덱) 생성할 수 있다.")
     @ParameterizedTest
     @ValueSource(ints = [52])
     fun cardDeck(cardDeckCount: Int) {
-        assertThat(Cards.CARD_DECK.size).isEqualTo(cardDeckCount)
+        assertThat(Cards(DEFAULT_CARD_DECK).value.size).isEqualTo(cardDeckCount)
     }
 }
