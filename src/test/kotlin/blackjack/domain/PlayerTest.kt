4a6c8539d479993c34d2108013d419f2cf114075
jay.lee <stkment@gmail.com>
2022-12-11 21:50:40 +0900

    :white_check_mark: (누락) Player 이름 입력 확인 테스트 케이스 추가

--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 
 internal class PlayerTest {
@@ -16,6 +17,13 @@ internal class PlayerTest {
     @EmptySource
     fun emptyName(name: String) {
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy { Player(name) }
+    }
+
+    @DisplayName("Player 이름을 가질 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = ["고니", "아귀", "팽 경장", "너구리", "정 마담", "고광렬"])
+    fun name(name: String) {
+        assertThat(Player(name).name).isEqualTo(name)
     }
 
     @DisplayName("Player 게임 시작 전 2개의 카드를 받는다.")
