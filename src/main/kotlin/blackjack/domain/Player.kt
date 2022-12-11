4e6656ab5b8d8f5ff354424079d1c9988fb6971e
jay.lee <stkment@gmail.com>
2022-12-11 21:50:05 +0900

    :necktie: Player 게임 카드 초기화 로직 구현

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,7 +1,16 @@
 package blackjack.domain
 
-class Player(val name: String) {
+class Player(val name: String, val cards: Cards = Cards()) {
     init {
         require(name.isNotBlank()) { "Player 이름은 공백이 될 수 없습니다. 유효한 Player 명을 입력해주세요." }
+    }
+
+    fun readyToPlay(initialCards: List<Card>) {
+        require(initialCards.size == INITIAL_CARDS_COUNT) { "잘못된 초기 카드 개수 입니다. 최초 2장만 카드를 받을 수 있습니다." }
+        initialCards.forEach(cards::add)
+    }
+
+    companion object {
+        private const val INITIAL_CARDS_COUNT = 2
     }
 }
