8e1ca03e41774918a4809a79b20b58ab7e8694c6
jay.lee <stkment@gmail.com>
2022-12-11 21:30:14 +0900

    :white_check_mark: Player 게임 시작 전 카드 초기화 관련 테스트 코드 구현

--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -1,9 +1,14 @@
 package blackjack.domain
 
+import blackjack.model.CardShape
+import blackjack.model.CardType
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 internal class PlayerTest {
     @DisplayName("Player 이름은 공백이 될 수 없다")
@@ -11,5 +16,19 @@ internal class PlayerTest {
     @EmptySource
     fun emptyName(name: String) {
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy { Player(name) }
+    }
+
+    @DisplayName("Player 게임 시작 전 2개의 카드를 받는다.")
+    @ParameterizedTest
+    @MethodSource("provideInitialCardS")
+    fun readyToPlayGameWithInitialCards(cards: List<Card>) {
+        val player = Player("고니").apply { readyToPlay(cards) }
+        assertThat(player.cards.size).isEqualTo(cards.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideInitialCardS(): Stream<List<Card>> =
+            Stream.of(listOf(Card(CardType.JACK, CardShape.CLOVER), Card(CardType.ACE, CardShape.CLOVER)))
     }
 }
