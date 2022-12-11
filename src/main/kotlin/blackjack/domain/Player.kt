f0a2c339a527a2372c0ea0d8141116b2a128c8b6
jay.lee <stkment@gmail.com>
2022-12-11 21:07:17 +0900

    :necktie: Player 이름 유효성 체크 로직 추가

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,7 @@
 package blackjack.domain
 
-class Player(val name: String)
+class Player(val name: String) {
+    init {
+        require(name.isNotBlank()) { "Player 이름은 공백이 될 수 없습니다. 유효한 Player 명을 입력해주세요." }
+    }
+}
