7206988893924e9552fb41193fd3c46371ae1929
jay.lee <stkment@gmail.com>
2022-12-13 00:02:38 +0900

    :necktie: Players 초기화 코드 추가

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,6 +1,11 @@
 package blackjack.domain
 
-class Players(val player: List<Player>) {
+class Players(val value: List<Player>) {
+
+    init {
+        require(value.size >= MIN_NUMBER_PLAYERS) { "우효하지 않는 플레이어 인원 수 입니다. 최소 2명 이상 입력해주세요." }
+        require(value.size <= MAX_NUMBER_PLAYERS) { "우효하지 않는 플레이어 인원 수 입니다. 최대 8명 까지 가능합니다." }
+    }
 
     companion object {
         const val MIN_NUMBER_PLAYERS = 2
