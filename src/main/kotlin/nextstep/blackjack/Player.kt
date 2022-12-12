5d2848673b84dbac5a399b60bb4e442e5f74fc0d
kimseonghun <seonghun127@woowahan.com>
2022-12-12 21:09:57 +0900

    feat: 플레이어는 가진 카드의 총합 점수를 계산할 수 있다.

--- src/main/kotlin/nextstep/blackjack/Player.kt
+++ src/main/kotlin/nextstep/blackjack/Player.kt
@@ -5,4 +5,6 @@ class Player(cards: Set<Card>) {
     private val _cards: MutableSet<Card> = cards.toMutableSet()
     val cards: Set<Card>
         get() = _cards.toSet()
+
+    fun calculateTotalPoint(): Int = _cards.fold(0) { acc: Int, card: Card -> acc + card.getPoint(acc) }
 }
--- src/test/kotlin/nextstep/blackjack/PlayerTest.kt
+++ src/test/kotlin/nextstep/blackjack/PlayerTest.kt
@@ -11,4 +11,10 @@ class PlayerTest : StringSpec({
 
         player.cards shouldBe setOf(SPADE_ACE, HEART_ACE, DIAMOND_ACE, CLOVER_ACE)
     }
+
+    "플레이어는 가진 카드의 총합 점수를 계산할 수 있다." {
+        val player = Player(setOf(CLOVER_JACK, CLOVER_ACE))
+
+        player.calculateTotalPoint() shouldBe 21
+    }
 })
