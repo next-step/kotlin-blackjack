c84dede2e4232bb632c3bb2d014004c661d45735
jay.lee <stkment@gmail.com>
2022-12-13 00:44:28 +0900

    :necktie: Game 초기화 코드 추가

--- src/main/kotlin/blackjack/domain/Game.kt
+++ src/main/kotlin/blackjack/domain/Game.kt
@@ -1,6 +1,24 @@
 package blackjack.domain
 
+import blackjack.model.Card
+
 class Game(val players: Players, val dealer: Dealer) {
+    init {
+        dealer.shuffle()
+        deliverInitialCards()
+    }
+
+    private fun deliverInitialCards() {
+        players.value.forEach {
+            val cards = mutableListOf<Card>().apply {
+                repeat(INITIAL_CARDS_COUNT) {
+                    add(dealer.deliverCard())
+                }
+            }
+            it.readyToPlay(cards)
+        }
+    }
+
     companion object {
         const val INITIAL_CARDS_COUNT = 2
     }
