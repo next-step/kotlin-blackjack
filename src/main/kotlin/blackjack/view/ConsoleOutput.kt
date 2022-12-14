8b04dd2d0cefb3b34260451bfc5dd906df0bef0f
chayoung-choi <chayoung.dev@gmail.com>
2022-12-14 23:25:53 +0900

    refactor: 함수 인자 변경

--- src/main/kotlin/blackjack/view/ConsoleOutput.kt
+++ src/main/kotlin/blackjack/view/ConsoleOutput.kt
@@ -1,26 +1,26 @@
 package blackjack.view
 
 import blackjack.domain.Deck.Companion.INITIAL_CARD_COUNT
-import blackjack.domain.Game
 import blackjack.domain.Player
 import blackjack.domain.Players
 
 object ConsoleOutput {
-    fun printInitialCards(game: Game) {
-        println("달러와 ${game.players.list.joinToString { it.name.value }}에게 ${INITIAL_CARD_COUNT}장의 카드를 나누었습니다.")
-        printPlayerCards(game.getDealer())
-        game.players.list.map { printPlayerCards(it) }
+    fun printInitialCards(dealer: Player, players: Players) {
+        println("달러와 ${players.list.joinToString { it.name.value }}에게 ${INITIAL_CARD_COUNT}장의 카드를 나누었습니다.")
+        printPlayerCards(dealer)
+        players.list.map { printPlayerCards(it) }
         println()
     }
 
     fun printPlayerCards(player: Player) = println(getPlayerInfo(player))
 
-    fun printGameResult(dealer: Player, players: Players) {
-        printGameResult(dealer)
-        players.list.map { printGameResult(it) }
+    fun printResultCards(dealer: Player, players: Players) {
+        printResultCards(dealer)
+        players.list.map { printResultCards(it) }
+        println()
     }
 
-    private fun printGameResult(player: Player) {
+    private fun printResultCards(player: Player) {
         println("${getPlayerInfo(player)} - 결과: ${player.countingCard()}")
     }
 
