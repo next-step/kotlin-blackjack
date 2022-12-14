5711edb0182181198cdecc17e133f529adb2a6ea
wrallee <wrallee@naver.com>
2022-12-14 20:40:16 +0900

    docs(Blackjack): 카드 생성 요구사항 추가

--- src/test/kotlin/blackjack/model/CardDeckTest.kt
+++ src/test/kotlin/blackjack/model/CardDeckTest.kt
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test
 
 class CardDeckTest {
     @Test
+    internal fun `하나의 덱은 52개의 카드로 구성된다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
     internal fun `두개의 카드를 지급한다`() {
         TODO("Not yet implemented")
     }
@@ -22,5 +27,4 @@ class CardDeckTest {
     internal fun `덱에 카드가 없으면 뽑을 수 없다`() {
         TODO("Not yet implemented")
     }
-
 }
---
+++ src/test/kotlin/blackjack/model/CardTest.kt
@@ -0,0 +1,10 @@
+package blackjack.model
+
+import org.junit.jupiter.api.Test
+
+class CardTest {
+    @Test
+    internal fun `카드는 슈트와 끗수로 구성된다`() {
+        TODO("Not yet implemented")
+    }
+}
