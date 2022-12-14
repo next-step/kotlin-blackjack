4d88e67b81c78512d9ecfe01b0406478cbc78a38
kimseonghun <seonghun127@woowahan.com>
2022-12-14 21:26:25 +0900

    feat: 플레이어가 원하면 특정 플레이어에게 카드 1장을 추가로 지급한다.

--- src/main/kotlin/nextstep/blackjack/Game.kt
+++ src/main/kotlin/nextstep/blackjack/Game.kt
@@ -1,12 +1,17 @@
 package nextstep.blackjack
 
-class Game(
-    val players: List<Player>,
-    val cardPack: CardPack = CardPack()
-) {
+private const val INIT_DRAW_CARD_COUNT = 2
+
+class Game(val players: List<Player>) {
+    private val cardPack: CardPack = CardPack()
+
     init {
         players.forEach { player ->
-            repeat(2) { player.hit(cardPack.draw()) }
+            repeat(INIT_DRAW_CARD_COUNT) { player.hit(cardPack.draw()) }
         }
+    }
+
+    fun drawTo(playerName: String) {
+        players.first { it.name == playerName }.also { it.hit(cardPack.draw()) }
     }
 }
--- src/test/kotlin/nextstep/blackjack/GameTest.kt
+++ src/test/kotlin/nextstep/blackjack/GameTest.kt
@@ -9,6 +9,14 @@ class GameTest : StringSpec({
     "게임 라운드가 시작되면 모든 플레이어는 카드 2장을 지급받는다." {
         val game = Game(listOf(Player("jack"), Player("pobi")))
 
-        game.players.forAll { it.cards.size shouldBe  2 }
+        game.players.forAll { it.cards.size shouldBe 2 }
+    }
+
+    "플레이어가 원하면 특정 플레이어에게 카드 1장을 추가로 지급한다." {
+        val game = Game(listOf(Player("jack"), Player("pobi")))
+
+        game.drawTo("jack")
+
+        game.players.first { it.name == "jack" }.cards.size shouldBe 3
     }
 })
