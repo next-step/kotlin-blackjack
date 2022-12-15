8bd7fd8dbe9b931ad239f5b583fbafbc488aa7af
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-15 23:45:05 +0900

    test(DeckTest): 요구 사항에 맞게 덱에서 카드를 뽑을 수 있다.

--- src/test/kotlin/blackjack/DeckTest.kt
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -2,20 +2,42 @@ package blackjack
 
 import blackjack.domain.Card
 import blackjack.domain.Deck
-import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-internal class DeckTest : BehaviorSpec({
-    given("블랙잭에서는") {
-        `when`("최초로 게임을 시작했을 때") {
-            val deck = Deck()
-            then("52장의 카드 덱을 가진다.") {
-                deck.values.values.size shouldBe 52
-            }
+internal class DeckTest : StringSpec() {
+    private lateinit var deck: Deck
+    init {
+        beforeEach {
+            deck = Deck()
+        }
 
-            then("중복없는 플레잉 카드를 가진다.") {
-                deck.values.values shouldBe Card.ALL_CARDS
-            }
+        "최초로 게임을 시작했을 때 52장의 카드 덱을 가진다." {
+            deck.values.values.size shouldBe 52
+        }
+
+        "카드 덱은 중복 없는 플레잉 카드를 가진다." {
+            deck.values.values shouldBe Card.ALL_CARDS
+        }
+
+        "52장의 카드 중에서 한 장을 뽑으면 카드 덱의 사이즈는 1 줄어든다." {
+            // given
+            deck.values.values.size shouldBe 52
+
+            // when
+            deck.draw()
+
+            // then
+            deck.values.values.size shouldBe 51
+        }
+
+        "플레이어들에게 최초로 지급되는 카드는 2장이다." {
+            // given
+            // when
+            val result = deck.drawInitCards()
+
+            // then
+            result.values.size shouldBe 2
         }
     }
-})
+}
