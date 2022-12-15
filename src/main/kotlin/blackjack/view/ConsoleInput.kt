e196c16f2ad2f1e920ca7d78646b1dae2d1ffd3f
chayoung-choi <chayoung.dev@gmail.com>
2022-12-15 17:32:12 +0900

    refactor: 입력값 오류시 재입력 로직 적용

--- src/main/kotlin/blackjack/view/ConsoleInput.kt
+++ src/main/kotlin/blackjack/view/ConsoleInput.kt
@@ -10,19 +10,32 @@ object ConsoleInput {
     fun inputPlayers(): Players {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
 
-        val names = readln()
-        require(names.isNotBlank()) { "잘못된 입력값입니다." }
+        return runCatching {
+            val names = readln()
+            require(names.isNotBlank()) { "잘못된 입력값입니다." }
 
-        val players = getPlayers(names)
-        println()
-        return players
+            val players = getPlayers(names)
+            println()
+            players
+        }
+            .fold(
+                onSuccess = { it }, onFailure = { e ->
+                    println(e.message)
+                    inputPlayers()
+                }
+            )
     }
 
-    fun inputScratch(player: Player): Boolean {
+    tailrec fun inputScratch(player: Player): Boolean {
         println("${player.name.value}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+
         val isHit = readln().lowercase()
-        require(isHit in listOf("y", "n")) { "y 또는 n으로 의사를 입력해주세요." }
-        return "y" == isHit
+        return if (isHit in listOf("y", "n")) {
+            "y" == isHit
+        } else {
+            println("y 또는 n으로 의사를 입력해주세요.")
+            inputScratch(player)
+        }
     }
 
     private fun getPlayers(names: String): Players = Players(splitNames(names).map { GamePlayer(it) })
