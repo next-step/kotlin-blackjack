4526ab8a3bc645b7e81f9cf3504f8253169c8796
jay.lee <stkment@gmail.com>
2022-12-18 04:14:36 +0900

    :necktie: Dealer GamePlay 구현 선언

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -4,11 +4,30 @@ import blackjack.model.Card
 
 class Dealer(
     private val _deck: CardDeck = CardDeckImpl(),
-) {
+): GamePlay {
     val deck: CardDeck
         get() = _deck
 
     fun shuffle() = _deck.shuffle()
 
     fun deliverCard(): Card = _deck.takeOutFirstCard()
+    override fun readyToPlay(initialCards: List<Card>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun hit(card: Card): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun sumCards(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun burst(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun blackjack(): Boolean {
+        TODO("Not yet implemented")
+    }
 }
