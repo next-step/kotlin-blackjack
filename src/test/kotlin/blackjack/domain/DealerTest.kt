8dd1bad6f7cb4af3c227ca4c6be4fea4a23338b1
jay.lee <stkment@gmail.com>
2022-12-18 04:23:49 +0900

    :white_check_mark: 딜러 게임플레이 테스트 코드 추가

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -1,8 +1,14 @@
 package blackjack.domain
 
+import blackjack.model.Card
+import blackjack.model.CardShape
+import blackjack.model.CardType
 import blackjack.model.DEFAULT_CARD_DECK
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class DealerTest {
     @Test
@@ -24,5 +30,58 @@ internal class DealerTest {
         val dealer = Dealer(CardDeckImpl(cardDeck))
         assertThat(dealer.deliverCard()).isEqualTo(firstCard)
         assertThat(dealer.deck.size).isEqualTo(resultCount)
+    }
+
+    // Dealer GamePlay 경우 Player 동일해야 한다.
+    @ParameterizedTest
+    @MethodSource("provideInitialCardS")
+    fun `Dealer 게임 시작 전 2개의 카드를 받는다`(cards: List<Card>) {
+        val player = Player("고니").apply { readyToPlay(cards) }
+        assertThat(player.cards.size).isEqualTo(cards.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInitialInvalidCardS")
+    fun `Dealer 게임 시작 전 2개의 카드를 받지 않으면 에러가 발생한다`(cards: List<Card>) {
+        val dealer = Dealer()
+        Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { dealer.readyToPlay(cards) }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideHitCard")
+    fun `Dealer 히트를 외치면 카드 한장을 더 받는다`(initialCards: List<Card>, hitCard: Card) {
+        val dealer = Dealer().apply {
+            readyToPlay(initialCards)
+            hit(hitCard)
+        }
+        assertThat(dealer.cards.value.last()).isEqualTo(hitCard)
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideBurstCards")
+    fun `Dealer 카드 합산이 21 초과 burst 상태가 되어 게임을 더이상 참가할 수 없다`(initialCards: List<Card>, hitCard: Card) {
+        val dealer = Dealer().apply {
+            readyToPlay(initialCards)
+            hit(hitCard)
+        }
+        assertThat(dealer.burst()).isTrue
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNotBurstCards")
+    fun `Dealer 카드 합산이 21 이상일 떄 burst 상태가 되어 게임을 계속 할 수있다`(initialCards: List<Card>, hitCard: Card) {
+        val dealer = Dealer().apply {
+            readyToPlay(initialCards)
+            hit(hitCard)
+        }
+        assertThat(dealer.burst()).isFalse
+    }
+
+    @Test
+    fun `Dealer 카드 합산이 21이면 블랙잭 완성`() {
+        val cards = Cards(mutableListOf(Card(CardType.KING, CardShape.HEART), Card(CardType.ACE, CardShape.DIAMOND)))
+        val dealer = Dealer(cards = cards)
+        assertThat(dealer.blackjack()).isTrue
     }
 }
