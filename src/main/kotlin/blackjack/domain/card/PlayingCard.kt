be9630ad9c128df437614b00939d4d2071b143a2
namonak <namonak@gmail.com>
2022-12-17 13:08:01 +0900

    refactor(step2): update the Suit enum class
    The following have been changed:
     - replace toString() override method with symbol property in Suit enum class

--- src/main/kotlin/blackjack/domain/card/PlayingCard.kt
+++ src/main/kotlin/blackjack/domain/card/PlayingCard.kt
@@ -2,7 +2,7 @@ package blackjack.domain.card
 
 data class PlayingCard(private val suit: Suit, private val denomination: Denomination) {
     override fun toString(): String {
-        return denomination.toString() + suit.toString()
+        return denomination.toString() + suit.symbol
     }
 
     fun score(): Int {
