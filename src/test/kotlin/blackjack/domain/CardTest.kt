ff815584381ef8f1a2ed79ec0f5d181d1b230b37
jay.lee <stkment@gmail.com>
2022-12-18 03:25:56 +0900

    :white_check_mark: 카트 테스트 코드 수정

--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -3,7 +3,7 @@ package blackjack.domain
 import blackjack.model.Card
 import blackjack.model.CardShape
 import blackjack.model.CardType
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -13,19 +13,19 @@ internal class CardTest {
 
     @ParameterizedTest
     @MethodSource("provideValidCard")
-    fun `각 카드 타입 별로 유효한 값(value)과 점수 및 모양을 가지고 있다`(card: Card, value: String, score: Int, shape: String) {
-        Assertions.assertThat(card.type.value).isEqualTo(value)
-        Assertions.assertThat(card.type.score).isEqualTo(score)
-        Assertions.assertThat(card.shape.text).isEqualTo(shape)
+    fun `각 카드 타입 별로 유효한 값(value)과 점수 및 모양을 생성할 수 있다`(cardType: CardType, cardShape: CardShape) {
+        val card = Card(cardType, cardShape)
+        assertThat(card.type).isEqualTo(cardType)
+        assertThat(card.shape).isEqualTo(cardShape)
     }
 
     companion object {
         @JvmStatic
         fun provideValidCard(): Stream<Arguments> =
             Stream.of(
-                Arguments.of(Card(CardType.ACE, CardShape.CLOVER), "A", 1, "클로버"),
-                Arguments.of(Card(CardType.NINE, CardShape.SPADE), "9", 9, "스페이드"),
-                Arguments.of(Card(CardType.JACK, CardShape.HEART), "J", 10, "하트")
+                Arguments.of(CardType.ACE, CardShape.CLOVER),
+                Arguments.of(CardType.NINE, CardShape.SPADE),
+                Arguments.of(CardType.JACK, CardShape.HEART)
             )
     }
 }
