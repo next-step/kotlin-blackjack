ff6fb7e59ffed8f366d36b7cbcd0bcacb3bd6c9f
jay.lee <stkment@gmail.com>
2022-12-18 00:52:41 +0900

    :necktie: unwrap main 함수
    arges 받을 필요 없어서 companin object에서 제외

--- src/main/kotlin/blackjack/Blackjack.kt
+++ src/main/kotlin/blackjack/Blackjack.kt
@@ -3,31 +3,26 @@ package blackjack
 import blackjack.domain.Dealer
 import blackjack.domain.Game
 
-class Blackjack {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val players = InputView.inputPlayers()
-            val dealer = Dealer()
-            val game = Game(players, dealer)
-            ResultView.printInitialCards(game.players)
-            playBlackJack(game)
-            ResultView.printResult(game.players)
-        }
+fun main() {
+    val players = InputView.inputPlayers()
+    val dealer = Dealer()
+    val game = Game(players, dealer)
+    ResultView.printInitialCards(game.players)
+    playBlackJack(game)
+    ResultView.printResult(game.players)
+}
 
-        private fun playBlackJack(game: Game) {
-            game.players
-                .value
-                .forEach { player ->
-                    while (!player.blackjack() &&
-                        !player.burst() &&
-                        InputView.inputHitOrStop(player)
-                    ) {
-                        player.hit(game.dealer.deliverCard())
-                        ResultView.printPlayerCards(player)
-                    }
-                }
-            println()
+private fun playBlackJack(game: Game) {
+    game.players
+        .value
+        .forEach { player ->
+            while (!player.blackjack() &&
+                !player.burst() &&
+                InputView.inputHitOrStop(player)
+            ) {
+                player.hit(game.dealer.deliverCard())
+                ResultView.printPlayerCards(player)
+            }
         }
-    }
+    println()
 }
