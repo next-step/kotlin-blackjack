a3acbd87d393861fd5c2e0aa855488d3bfae8257
EP <kjuioqq8@gmail.com>
2022-12-18 09:40:46 +0900

    feat: 블랙잭 게임 기능 구현

---
+++ src/main/kotlin/application/BlackJackGame.kt
@@ -0,0 +1,28 @@
+package application
+
+import domain.Cards
+import domain.Dealer
+import domain.Participants
+import domain.Player
+
+class BlackJackGame(
+    private val dealer: Dealer = Dealer(),
+    private val players: Participants
+) {
+    fun init() {
+        repeat(INIT_CARD_COUNT) { players.receiveCard(dealer) }
+    }
+
+    fun receiveCard(player: Player) {
+        dealer.giveCard(player = player)
+    }
+
+    fun hands(player: Player): Cards = player.hands
+    fun allPlayers(): List<Player> {
+        return this.players.allPlayers()
+    }
+
+    private companion object {
+        const val INIT_CARD_COUNT = 2
+    }
+}
---
+++ src/test/kotlin/blackjack/application/BlackJackGameTest.kt
@@ -0,0 +1,51 @@
+package blackjack.application
+
+import application.BlackJackGame
+import domain.Dealer
+import domain.Participants
+import domain.Player
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+
+class BlackJackGameTest : BehaviorSpec({
+    given("블랙잭 게임은 딜러와 참여자가 있어야 만들 수 있다") {
+        val dealer = Dealer()
+        val playerA = Player("ep")
+        val playerB = Player("kim")
+        val participants = Participants(listOf(playerA, playerB))
+
+        val blackJackGame = BlackJackGame(dealer = dealer, players = participants)
+        blackJackGame.shouldNotBeNull()
+
+        `when`("게임이 시작하면") {
+            blackJackGame.init()
+            then("플레이어는 카드를 두장씩 받는다") {
+                playerA.handsCardCount shouldBe 2
+            }
+        }
+
+        `when`("플레이어는") {
+            blackJackGame.receiveCard(playerA)
+            then("카드를 한 장씩 받을 수 있다") {
+                playerA.handsCardCount shouldBe 3
+            }
+        }
+
+        `when`("모든 플레이어의 정보를") {
+            val allPlayers = blackJackGame.allPlayers()
+            then("확인할 수 있다") {
+                allPlayers.size shouldBe 2
+            }
+        }
+
+        `when`("플레이어의 카드 패 정보를") {
+            val playerAHands = blackJackGame.hands(playerA)
+            val playerBHands = blackJackGame.hands(playerB)
+            then("확인할 수 있다") {
+                playerAHands.count shouldBe 3
+                playerBHands.count shouldBe 2
+            }
+        }
+    }
+})
