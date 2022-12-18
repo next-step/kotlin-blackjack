c4bcb8661cd438647ef0d258848d78db2e7487a0
dk-ko <dev.dkko@gmail.com>
2022-12-18 13:21:37 +0900

    feat: languages 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -1,6 +1,7 @@
 package study
 
 import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.maps.shouldContainAll
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
@@ -64,6 +65,25 @@ class DslTest {
         person.skills.soft shouldBe listOf("A passion for problem solving", "Good communication skills")
         person.skills.hard shouldBe listOf("Kotlin")
     }
+
+    @Test
+    fun languages() {
+        val person = introduce {
+            name("이름")
+            company("회사")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.languages.languages shouldContainAll mapOf("Korean" to 5)
+        person.languages.languages shouldContainAll mapOf("English" to 3)
+    }
 }
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
@@ -74,6 +94,7 @@ class PersonBuilder {
     private lateinit var name: String
     private var company: String? = null
     private lateinit var skills: Skills
+    private lateinit var languages: Languages
 
     fun name(value: String) {
         this.name = value
@@ -87,8 +108,12 @@ class PersonBuilder {
         skills =  SkillsBuilder().apply(block).build()
     }
 
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        languages = LanguagesBuilder().apply(block).build()
+    }
+
     fun build(): Person {
-        return Person(name, company, skills)
+        return Person(name, company, skills, languages)
     }
 }
 
@@ -109,6 +134,20 @@ class SkillsBuilder {
     }
 }
 
-data class Person(val name: String, val company: String?, val skills: Skills)
+class LanguagesBuilder {
+    private var languages: MutableMap<String, Int> = mutableMapOf()
+
+    infix fun String.level(level: Int) {
+        languages[this] = level
+    }
+
+    fun build(): Languages {
+        return Languages(languages)
+    }
+}
+
+data class Person(val name: String, val company: String?, val skills: Skills, val languages: Languages)
 
 data class Skills(val soft: List<String>, val hard: List<String>)
+
+data class Languages(val languages: Map<String, Int>)
