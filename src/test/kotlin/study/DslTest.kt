a8ea004a987265ff36b10abe0791d0ca9427355d
dk-ko <dev.dkko@gmail.com>
2022-12-18 12:55:12 +0900

    feat: skills 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -24,6 +24,11 @@ class DslTest {
     fun introduce() {
         val person: Person = introduce {
             name("이름")
+            skills {
+                soft("test")
+                soft("test")
+                hard("test")
+            }
         }
         person.name shouldBe "이름"
         person.company.shouldBeNull()
@@ -34,19 +39,40 @@ class DslTest {
         val person: Person = introduce {
             name("이름")
             company("회사")
+            skills {
+                soft("test")
+                soft("test")
+                hard("test")
+            }
         }
         person.name shouldBe "이름"
         person.company shouldBe "회사"
     }
+
+    @Test
+    fun skills() {
+        introduce {
+            name("이름")
+            company("회사")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+    }
 }
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
-    return PersonBuilder().apply(block).build()
+    val personBuilder: PersonBuilder = PersonBuilder()
+    personBuilder.block()
+    return personBuilder.build()
 }
 
 class PersonBuilder {
     private lateinit var name: String
     private var company: String? = null
+    private lateinit var skills: Skills
 
     fun name(value: String) {
         this.name = value
@@ -56,9 +82,32 @@ class PersonBuilder {
         this.company = value
     }
 
+    fun skills(block: SkillsBuilder.() -> Unit) {
+        skills =  SkillsBuilder().apply(block).build()
+    }
+
     fun build(): Person {
-        return Person(name, company)
+        return Person(name, company, skills)
     }
 }
 
-data class Person(val name: String, val company: String?)
+class SkillsBuilder {
+    private lateinit var soft: String
+    private lateinit var hard: String
+
+    fun soft(value: String) {
+        this.soft = value
+    }
+
+    fun hard(value: String) {
+        this.hard = value
+    }
+
+    fun build(): Skills {
+        return Skills(soft, hard)
+    }
+}
+
+data class Person(val name: String, val company: String?, val skills: Skills)
+
+data class Skills(val soft: String, val hard: String)
