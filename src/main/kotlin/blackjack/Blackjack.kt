270d244991219a83845f0b7a6dc9cc955375951f
jay.lee <stkment@gmail.com>
2022-12-19 22:32:37 +0900

    :recycle: Game play 로직 리팩토링

--- src/main/kotlin/blackjack/Blackjack.kt
+++ src/main/kotlin/blackjack/Blackjack.kt
@@ -2,40 +2,13 @@ package blackjack
 
 import blackjack.domain.Dealer
 import blackjack.domain.Game
-import blackjack.domain.Players
 
 fun main() {
     val players = InputView.inputPlayers()
     val dealer = Dealer()
     val game = Game(players, dealer)
     ResultView.printInitialCards(game.players, game.dealer)
-    playBlackJack(game)
+    game.play()
     ResultView.printResult(game.players, game.dealer)
-}
-
-private fun playBlackJack(game: Game) {
-    playPlayers(game.players, game.dealer)
-    playDealer(game.dealer)
-}
-
-private fun playPlayers(players: Players, dealer: Dealer) {
-    players.value
-        .forEach { player ->
-            while (!player.blackjack() &&
-                !player.burst() &&
-                InputView.shouldHit(player)
-            ) {
-                player.hit(dealer.deliverCard())
-                ResultView.printPlayerCards(player)
-            }
-        }
-    println()
-}
-
-private fun playDealer(dealer: Dealer) {
-    while (!dealer.blackjack() && !dealer.stay() && !dealer.burst()) {
-        InputView.printDealerHit()
-        dealer.hit(dealer.deliverCard())
-    }
 }
 
--- src/main/kotlin/blackjack/domain/Game.kt
+++ src/main/kotlin/blackjack/domain/Game.kt
@@ -1,5 +1,8 @@
 package blackjack.domain
 
+import blackjack.InputView
+import blackjack.ResultView
+
 class Game(val players: Players, val dealer: Dealer) {
     init {
         dealer.shuffle()
@@ -10,6 +13,32 @@ class Game(val players: Players, val dealer: Dealer) {
         players.value.forEach {
             List(INITIAL_CARDS_COUNT) { dealer.deliverCard() }
                 .let(it::readyToPlay)
+        }
+    }
+
+    fun play() {
+        playPlayers(players, dealer)
+        playDealer(dealer)
+    }
+
+    private fun playPlayers(players: Players, dealer: Dealer) {
+        players.value
+            .forEach { player ->
+                while (!player.blackjack() &&
+                    !player.burst() &&
+                    InputView.shouldHit(player)
+                ) {
+                    player.hit(dealer.deliverCard())
+                    ResultView.printPlayerCards(player)
+                }
+            }
+        println()
+    }
+
+    private fun playDealer(dealer: Dealer) {
+        while (!dealer.blackjack() && !dealer.stay() && !dealer.burst()) {
+            InputView.printDealerHit()
+            dealer.hit(dealer.deliverCard())
         }
     }
 
