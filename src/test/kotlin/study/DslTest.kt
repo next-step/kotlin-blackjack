f057a1d6715c06857e6c04f4473d5fa1c55df9a6
Jade <dev.dkko@gmail.com>
2022-12-19 09:24:22 +0900

    Step1 코틀린 DSL (#363)
    * feat: dsl test
    
    * feat: skills 추가
    
    * feat: skills list로 변경
    
    * feat: languages 추가
    
    * chore: remove import
    
    * fix: test

---
+++ src/test/kotlin/study/DslTest.kt
@@ -0,0 +1,164 @@
+package study
+
+import io.kotest.matchers.maps.shouldContainAll
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+/*
+introduce {
+  name("이름")
+  company("회사")
+  skills {
+    soft("A passion for problem solving")
+    soft("Good communication skills")
+    hard("Kotlin")
+  }
+  languages {
+    "Korean" level 5
+    "English" level 3
+  }
+}
+ */
+class DslTest {
+    @Test
+    fun introduce() {
+        val person: Person = introduce {
+            name("이름")
+            skills {
+                soft("test")
+                soft("test")
+                hard("test")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.name shouldBe "이름"
+        person.company.shouldBeNull()
+    }
+
+    @Test
+    fun company() {
+        val person: Person = introduce {
+            name("이름")
+            company("회사")
+            skills {
+                soft("test")
+                soft("test")
+                hard("test")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.name shouldBe "이름"
+        person.company shouldBe "회사"
+    }
+
+    @Test
+    fun skills() {
+        val person = introduce {
+            name("이름")
+            company("회사")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.skills.soft shouldBe listOf("A passion for problem solving", "Good communication skills")
+        person.skills.hard shouldBe listOf("Kotlin")
+    }
+
+    @Test
+    fun languages() {
+        val person = introduce {
+            name("이름")
+            company("회사")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.languages.languages shouldContainAll mapOf("Korean" to 5)
+        person.languages.languages shouldContainAll mapOf("English" to 3)
+    }
+}
+
+fun introduce(block: PersonBuilder.() -> Unit): Person {
+    return PersonBuilder().apply(block).build()
+}
+
+class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+    private lateinit var skills: Skills
+    private lateinit var languages: Languages
+
+    fun name(value: String) {
+        this.name = value
+    }
+
+    fun company(value: String) {
+        this.company = value
+    }
+
+    fun skills(block: SkillsBuilder.() -> Unit) {
+        skills =  SkillsBuilder().apply(block).build()
+    }
+
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        languages = LanguagesBuilder().apply(block).build()
+    }
+
+    fun build(): Person {
+        return Person(name, company, skills, languages)
+    }
+}
+
+class SkillsBuilder {
+    private var soft: MutableList<String> = mutableListOf()
+    private var hard: MutableList<String> = mutableListOf()
+
+    fun soft(value: String) {
+        this.soft.add(value)
+    }
+
+    fun hard(value: String) {
+        this.hard.add(value)
+    }
+
+    fun build(): Skills {
+        return Skills(soft, hard)
+    }
+}
+
+class LanguagesBuilder {
+    private var languages: MutableMap<String, Int> = mutableMapOf()
+
+    infix fun String.level(level: Int) {
+        languages[this] = level
+    }
+
+    fun build(): Languages {
+        return Languages(languages)
+    }
+}
+
+data class Person(val name: String, val company: String?, val skills: Skills, val languages: Languages)
+
+data class Skills(val soft: List<String>, val hard: List<String>)
+
+data class Languages(val languages: Map<String, Int>)
