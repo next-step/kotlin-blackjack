51a1cb2e3cb4a47d51a599055d73e8f887c29553
jay.lee <stkment@gmail.com>
2022-12-20 21:33:30 +0900

    :white_check_mark: 게임 초기화, 딜러 카드 수여 관련 테스트 코드 수정

--- src/test/kotlin/blackjack/domain/GameTest.kt
+++ src/test/kotlin/blackjack/domain/GameTest.kt
@@ -2,12 +2,11 @@ package blackjack.domain
 
 import blackjack.domain.Game.Companion.INITIAL_CARDS_COUNT
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 internal class GameTest {
     @Test
-    fun `게임이 생성되면 딜러는 각 플에이어에게 2장의 카드를 나눠준다`() {
+    fun `게임이 생성되면 딜러는 각 플에이어에게 2장의 카드 전달하고 본인 자신도 2장의 카드를 가진다`() {
         val firstPlayer = Player("고니")
         val secondPlayer = Player("아귀")
 
@@ -15,5 +14,6 @@ internal class GameTest {
         val dealer = Dealer()
         val game = Game(players, dealer)
         assertThat(game.players.value.all { it.cards.size == INITIAL_CARDS_COUNT }).isTrue
+        assertThat(game.dealer.cards.size).isEqualTo(INITIAL_CARDS_COUNT)
     }
 }
