cef1667360f388bebcbdfdd02775173053a2e7c1
jay.lee <stkment@gmail.com>
2022-12-20 21:50:11 +0900

    :necktie: fake player, dealer 추가

---
+++ src/test/kotlin/blackjack/domain/FakePlayer.kt
@@ -0,0 +1,45 @@
+package blackjack.domain
+
+import blackjack.model.Card
+import java.util.UUID
+
+class FakePlayer(
+    val name: String = UUID.randomUUID().toString(),
+    private val hit: Boolean = false,
+    private val sumCards: Int = 0,
+    private val burst: Boolean = false,
+    private val blackjack: Boolean = false,
+) : GamePlay {
+
+    override fun readyToPlay(initialCards: List<Card>) {
+        // do nothing
+    }
+
+    override fun hit(card: Card): Boolean = hit
+
+    override fun sumCards(): Int = sumCards
+
+    override fun burst(): Boolean = burst
+
+    override fun blackjack(): Boolean = blackjack
+}
+
+class FakeDealer(
+    private val hit: Boolean = false,
+    private val sumCards: Int = 0,
+    private val burst: Boolean = false,
+    private val blackjack: Boolean = false,
+) : GamePlay {
+
+    override fun readyToPlay(initialCards: List<Card>) {
+        // do nothing
+    }
+
+    override fun hit(card: Card): Boolean = hit
+
+    override fun sumCards(): Int = sumCards
+
+    override fun burst(): Boolean = burst
+
+    override fun blackjack(): Boolean = blackjack
+}
