4baa4554e8573be1bd4cbe6f407937108d674da3
jay.lee <stkment@gmail.com>
2022-12-25 05:27:16 +0900

    :white_check_mark: Players 내에 player 게임 준비 상태 테스트 코드 추가

--- src/test/kotlin/blackjack/domain/PlayersTest.kt
+++ src/test/kotlin/blackjack/domain/PlayersTest.kt
@@ -1,5 +1,9 @@
 package blackjack.domain
 
+import blackjack.domain.Game.Companion.INITIAL_CARDS_COUNT
+import blackjack.model.Card
+import blackjack.model.CardShape
+import blackjack.model.CardType
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
@@ -38,5 +42,37 @@ internal class PlayersTest {
         assertThatExceptionOfType(IllegalArgumentException::class.java).isThrownBy {
             val players = Players(participants)
         }
+    }
+
+    @Test
+    fun `모든 블랙잭 참가들이 카드를 2장씩 받으면 게임 시작할 준비가 되었다`() {
+        val participants = mutableListOf<GamePlayer>().apply {
+            repeat(2) { count ->
+                add(GamePlayer("$count").apply {
+                    repeat(INITIAL_CARDS_COUNT) {
+                        draw(Card(CardType.SIX, CardShape.CLOVER))
+                    }
+                })
+            }
+        }
+        val players = Players(participants)
+        assertThat(players.allReadyToPlay()).isTrue
+    }
+
+    @Test
+    fun `모든 블랙잭 참가들 중 한명이라도 카드를 2장 미만으로 카드 받으면 게임 시작할 준비가 되어있지 않다`() {
+        val participants = mutableListOf<GamePlayer>().apply {
+            repeat(2) { count ->
+                repeat(2) { count ->
+                    add(GamePlayer("$count").apply {
+                        repeat(INITIAL_CARDS_COUNT) {
+                            if (count == 1) draw(Card(CardType.SIX, CardShape.CLOVER))
+                        }
+                    })
+                }
+            }
+        }
+        val players = Players(participants)
+        assertThat(players.allReadyToPlay()).isFalse
     }
 }
