813e9a474852828c957da77576aad3c344e43862
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-24 19:34:14 +0900

    refactor: Player 를 인터페이스로 적용

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,10 +1,16 @@
 package blackjack.domain
 
-class Dealer(results: List<ResultStatus> = emptyList()) : Player("딜러") {
-    private var _results: MutableList<ResultStatus> = results.toMutableList()
-    val results: List<ResultStatus>
-        get() = _results.toList()
+class Dealer(
+    override val name: Name = DEALER_NAME,
+    override var cards: Cards = Cards(),
+) : Player {
+    var results: MutableList<ResultStatus> = mutableListOf()
 
+    override fun isHit() = this.score <= DEALER_HIT_SCORE
+
+    override fun copy(name: Name, cards: Cards): Player {
+        return Dealer(cards = cards)
+    }
     fun getMatchResult(player: Player): ResultStatus {
         val playerResult = getPlayerResult(player)
         calculateResult(playerResult)
@@ -12,21 +18,21 @@ class Dealer(results: List<ResultStatus> = emptyList()) : Player("딜러") {
         return playerResult
     }
 
-    fun isHit() = this.score <= DEALER_HIT_SCORE
+    private fun getPlayerResult(player: Player): ResultStatus {
+        if (this.isBlackJack()) return getPlayerResultWhenDealerBlackJack(player)
+        if (this.isBust()) return getPlayerResultWhenDealerBust(player)
+        if (player.isBust()) return ResultStatus.LOSE
 
-    fun calculateResult(result: ResultStatus) {
-        val dealerResult = when (result) {
+        return player.score match this.score
+    }
+
+    fun calculateResult(playerResult: ResultStatus) {
+        val dealerResult = when (playerResult) {
             ResultStatus.WIN -> ResultStatus.LOSE
             ResultStatus.LOSE -> ResultStatus.WIN
             else -> ResultStatus.DRAW
         }
-        _results.add(dealerResult)
-    }
-
-    private fun getPlayerResult(player: Player): ResultStatus {
-        if (this.isBlackJack()) return getPlayerResultWhenDealerBlackJack(player)
-        if (this.isBust()) return ResultStatus.WIN
-        return player.score match this.score
+        results.add(dealerResult)
     }
 
     private fun getPlayerResultWhenDealerBlackJack(player: Player): ResultStatus {
@@ -34,7 +40,13 @@ class Dealer(results: List<ResultStatus> = emptyList()) : Player("딜러") {
         return ResultStatus.LOSE
     }
 
+    private fun getPlayerResultWhenDealerBust(player: Player): ResultStatus {
+        if (player.isBust()) return ResultStatus.DRAW
+        return ResultStatus.WIN
+    }
+
     companion object {
         private const val DEALER_HIT_SCORE = 16
+        private val DEALER_NAME = Name("딜러")
     }
 }
--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,15 +1,25 @@
 package blackjack.domain
 
-open class Player(val name: String, var cards: Cards = Cards()) {
+interface Player {
+    val name: Name
+    val cards: Cards
     val score: Int
         get() = cards.getScore()
+
+    fun drawInitialCards(deck: Deck): Player {
+        return this.copy(cards = deck.drawInitCards())
+    }
     fun hit(card: Card) {
         cards.add(card)
     }
 
+    fun isHit() =  cards.getScore() < TWENTY_ONE
+
     fun isBust() = cards.getScore() > TWENTY_ONE
 
     fun isBlackJack() = cards.isBlackJack()
+
+    fun copy(name: Name = this.name, cards: Cards): Player
 
     companion object {
         private const val TWENTY_ONE = 21
