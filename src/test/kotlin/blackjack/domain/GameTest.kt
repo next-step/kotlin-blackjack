6456ca4ed8e08a6decbc96fccf239e15352c783a
jay.lee <stkment@gmail.com>
2022-12-25 05:31:42 +0900

    :white_check_mark: Gmae play 시작할 때 players 상태 확인 및 예외 테스트 코드 추가

--- src/test/kotlin/blackjack/domain/GameTest.kt
+++ src/test/kotlin/blackjack/domain/GameTest.kt
@@ -1,7 +1,10 @@
 package blackjack.domain
 
+import blackjack.InputView
+import blackjack.ResultView
 import blackjack.domain.Game.Companion.INITIAL_CARDS_COUNT
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
 
 internal class GameTest {
@@ -15,5 +18,31 @@ internal class GameTest {
         val game = Game(players, dealer)
         assertThat(game.players.value.all { it.cards.size == INITIAL_CARDS_COUNT }).isTrue
         assertThat(game.dealer.cards.size).isEqualTo(INITIAL_CARDS_COUNT)
+    }
+
+    @Test
+    fun `각 플에이어에게 2장의 카드 전달하지 않고 게임을 시작하면 에러가 발생한다`() {
+        val firstGamePlayer = GamePlayer("고니")
+        val secondGamePlayer = FakePlayer("아귀", readyToPlay = false)
+
+        val players = Players(listOf(firstGamePlayer, secondGamePlayer))
+        val dealer = GameDealer()
+        val game = Game(players, dealer)
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { game.playPlayers(InputView::shouldHit, ResultView::printPlayerCards) }
+            .withMessage("모든 플레이어가 게임을 시작할 준비가 되어야 합니다.")
+    }
+
+    @Test
+    fun `딜러가 2장의 카드 받지 않고 게임을 시작하면 에러가 발생한다`() {
+        val firstGamePlayer = GamePlayer("고니")
+        val secondGamePlayer = GamePlayer("아귀")
+
+        val players = Players(listOf(firstGamePlayer, secondGamePlayer))
+        val dealer = FakeDealer(readyToPlay = false)
+        val game = Game(players, dealer)
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { game.playDealer { } }
+            .withMessage("딜러가 게임을 시작할 준비가 되어야 합니다.")
     }
 }
