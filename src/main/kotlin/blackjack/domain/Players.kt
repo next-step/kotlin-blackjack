f0792275754a6267407de6b370db4984d8b91f7b
jay.lee <stkment@gmail.com>
2022-12-25 05:28:35 +0900

    :necktie: Players 카드 초기화 및 초기화 상태 확인 로직 구현

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,5 +1,7 @@
 package blackjack.domain
 
+import blackjack.domain.Game.Companion.INITIAL_CARDS_COUNT
+
 class Players(val value: List<Player>) {
 
     init {
@@ -7,6 +9,16 @@ class Players(val value: List<Player>) {
         require(value.size <= MAX_NUMBER_PLAYERS) { "유효하지 않는 플레이어 참가 인원 수 입니다. 최대 ${MAX_NUMBER_PLAYERS}명 까지 가능합니다." }
     }
 
+    fun drawInitialCards(dealer: Dealer) {
+        repeat(INITIAL_CARDS_COUNT) {
+            value.forEach { player ->
+                dealer.deliverCard()
+                    .let(player::draw)
+            }
+        }
+    }
+
+    fun allReadyToPlay(): Boolean = value.all { it.shouldBeReadyToPlay() }
     companion object {
         const val MIN_NUMBER_PLAYERS = 2
         const val MAX_NUMBER_PLAYERS = 8
