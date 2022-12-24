6c297ba63acba0ba6c3b0cdaf3c2690a02b42766
jay.lee <stkment@gmail.com>
2022-12-25 04:06:36 +0900

    :necktie: GameResult 계산 구현

--- src/main/kotlin/blackjack/domain/Game.kt
+++ src/main/kotlin/blackjack/domain/Game.kt
@@ -3,10 +3,13 @@ package blackjack.domain
 import blackjack.InputView
 import blackjack.ResultView
 import blackjack.model.GameResult
-import blackjack.model.PlayerGameResult
 import blackjack.model.PlayerGameResults
 
-class Game(val players: Players, val dealer: Dealer) {
+class Game(
+    val players: Players,
+    val dealer: Dealer,
+    private val resultCalculator: ResultCalculator = GameResultCalculator()
+) {
     init {
         dealer.shuffle()
         deliverInitialCards()
@@ -27,42 +30,8 @@ class Game(val players: Players, val dealer: Dealer) {
         println()
     }
 
-    fun results(): PlayerGameResults {
-        val playerResults = calculatePlayerResult(players, dealer)
-        val gameDealerGameResult = calculateDealerResult(playerResults)
-        return PlayerGameResults(listOf(gameDealerGameResult) + playerResults)
-    }
-
-    private fun calculatePlayerResult(players: Players, dealer: Dealer): List<PlayerGameResult> {
-        val playerResults = mutableListOf<PlayerGameResult>()
-        players.value.map { player ->
-            val result = calculateGameResult(player, dealer)
-            playerResults.add(PlayerGameResult.Player(player.name, result))
-        }
-        return playerResults
-    }
-
-    private fun calculateDealerResult(playerResults: List<PlayerGameResult>): PlayerGameResult.Dealer =
-        PlayerGameResult.Dealer(
-            name = dealer.name,
-            win = playerResults.count { it is PlayerGameResult.Player && it.gameResult == GameResult.LOSE },
-            push = playerResults.count { it is PlayerGameResult.Player && it.gameResult == GameResult.PUSH },
-            lose = playerResults.count { it is PlayerGameResult.Player && it.gameResult == GameResult.WIN },
-        )
-
-    private fun calculateGameResult(player: Player, dealer: Dealer): GameResult {
-        val playerSum = player.sumCards()
-        val dealerSum = dealer.sumCards()
-        return when {
-            player.blackjack() && dealer.blackjack() -> GameResult.PUSH
-            dealer.blackjack() -> GameResult.LOSE
-            player.blackjack() || (dealer.bust() && !player.bust()) -> GameResult.WIN
-            player.bust() -> GameResult.LOSE
-            playerSum == dealerSum -> GameResult.PUSH
-            playerSum > dealerSum -> GameResult.WIN
-            else -> GameResult.LOSE
-        }
-    }
+    fun results(): PlayerGameResults =
+        resultCalculator.calculate(dealer, players)
 
     private fun playPlayers(players: Players, dealer: Dealer) {
         players.value
