3d7d3c00a0611124fc662a6efa35adbe43aeb3f1
seongbeen.kim <seongbeen.kim@linecorp.com>
2022-12-24 18:58:24 +0900

    feat: Add feature to pick a card

--- src/main/kotlin/blackjack/domain/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -8,4 +8,12 @@ class CardDeck(
         .toSet()
 ) {
     private val cards: MutableSet<Card> = cards.toMutableSet()
+    fun pick(): Card {
+        if (cards.isEmpty()) throw IllegalStateException("there are no card left")
+
+        val card = cards.first()
+        cards.remove(card)
+
+        return card
+    }
 }
--- src/test/kotlin/blackjack/domain/CardDeckTest.kt
+++ src/test/kotlin/blackjack/domain/CardDeckTest.kt
@@ -1,13 +1,31 @@
 package blackjack.domain
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.types.shouldBeInstanceOf
+import java.lang.IllegalStateException
 
 class CardDeckTest : FunSpec({
     context("객체 생성") {
         test("카드 덱을 생성한다.") {
             shouldNotThrowAny {
                 CardDeck()
+            }
+        }
+    }
+
+    context("pick()") {
+        test("카드 1장을 뽑는다.") {
+            val cardDeck = CardDeck()
+
+            val actual = cardDeck.pick()
+
+            actual.shouldBeInstanceOf<Card>()
+        }
+        test("뽑을 카드가 없을 경우, 예외가 발생한다.") {
+            shouldThrow<IllegalStateException> {
+                CardDeck(emptySet()).pick()
             }
         }
     }
