aa6faab931f1ea7764f29d17a282832723803a23
harris.yi <harris.yi@kakaostyle.com>
2022-12-24 13:29:23 +0900

    feat: Change test as dealer has only one card

--- src/test/kotlin/blackjack/BlackJackGameTest.kt
+++ src/test/kotlin/blackjack/BlackJackGameTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class BlackJackGameTest : StringSpec({
-    "게임을 시작하면 카드를 두장씩 나눠준다." {
+    "게임을 시작하면 딜러에게 카드를 한장 플레이어에게 카드를 두장씩 나눠준다." {
         //given
         val game = BlackJackGame(
             listOf(Player("harris")),
@@ -17,7 +17,7 @@ class BlackJackGameTest : StringSpec({
 
         //then
         playerDtos.size shouldBe 2
-        playerDtos[0].cards.size shouldBe 2
+        playerDtos[0].cards.size shouldBe 1
         playerDtos[1].cards.size shouldBe 2
     }
     "딜러의 카드 점수가 16점일 때 hit을 한 번 수행한다." {
