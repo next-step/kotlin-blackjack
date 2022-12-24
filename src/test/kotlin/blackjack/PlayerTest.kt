c0a8e48a5c9ca666cea32ebba06aa17bc0ce6bb2
harris.yi <harris.yi@kakaostyle.com>
2022-12-24 13:35:18 +0900

    test: Validate blackJack winning rule of bust and tie

--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -17,17 +17,53 @@ class PlayerTest : StringSpec({
         harris.cardPoint() shouldBe Point(10)
     }
 
-    "19점인 플레이어와 21점인 딜러가 있을 때 플레이어가 flip 함수를 호출하면 플레이어의 losingCount와 딜러의 winningCount가 1씩 증가한다." {
+    "19점인 플레이어와 21점인 딜러가 있을 때 플레이어가 flip 함수를 호출한 결과는 LOSE이다." {
         //given
         val dealer = Dealer(playingCards = PlayingCards(mutableSetOf(Card.CLOVER_10, Card.CLOVER_A)))
         val harris = Player("harris", playingCards = PlayingCards(mutableSetOf(Card.DIAMOND_10, Card.CLOVER_9)))
-        //when
-        harris.losingCount shouldBe 0
-        dealer.winningCount shouldBe 0
-        harris.flip(dealer)
-        //then
-        harris.losingCount shouldBe 1
-        dealer.winningCount shouldBe 1
 
+        //when
+        val result = harris.flip(dealer)
+
+        //then
+        result shouldBe GameResult.LOSE
+    }
+
+    "bust인 플레이어와 bust인 딜러가 있을 때 플레이어는 항상 진다." {
+        //given
+        val dealer = Dealer(playingCards = PlayingCards(mutableSetOf(Card.CLOVER_5, Card.CLOVER_10, Card.CLOVER_K)))
+        val harris = Player("harris", playingCards = PlayingCards(mutableSetOf(Card.DIAMOND_10, Card.CLOVER_9, Card.DIAMOND_3)))
+
+        //when
+        val result = harris.flip(dealer)
+
+        //then
+        dealer.bust() shouldBe true
+        harris.bust() shouldBe true
+        result shouldBe GameResult.LOSE
+    }
+
+    "19점인 플레이어와 bust인 딜러가 있을 때 플레이어는 승리한다." {
+        //given
+        val dealer = Dealer(playingCards = PlayingCards(mutableSetOf(Card.CLOVER_5, Card.CLOVER_10, Card.CLOVER_K)))
+        val harris = Player("harris", playingCards = PlayingCards(mutableSetOf(Card.DIAMOND_2, Card.CLOVER_2)))
+
+        //when
+        val result = harris.flip(dealer)
+
+        //then
+        dealer.bust() shouldBe true
+        result shouldBe GameResult.WIN
+    }
+    "19점인 플레이어와 19점인 딜러가 있을 때 결과는 무승부이다.." {
+        //given
+        val dealer = Dealer(playingCards = PlayingCards(mutableSetOf(Card.CLOVER_9, Card.CLOVER_10)))
+        val harris = Player("harris", playingCards = PlayingCards(mutableSetOf(Card.DIAMOND_9, Card.DIAMOND_10)))
+
+        //when
+        val result = harris.flip(dealer)
+
+        //then
+        result shouldBe GameResult.TIE
     }
 })
