921868ea1cda26ad1b4a48d09f0eaf0d413a2aef
jay.lee <stkment@gmail.com>
2022-12-25 02:12:31 +0900

    :white_check_mark: GamePlayer 리팩토링

--- src/main/kotlin/blackjack/domain/GamePlayer.kt
+++ src/main/kotlin/blackjack/domain/GamePlayer.kt
@@ -1,24 +1,34 @@
 package blackjack.domain
 
-import blackjack.domain.Game.Companion.INITIAL_CARDS_COUNT
 import blackjack.model.Card
 
-class GamePlayer(override val name: String, override val cards: Cards = Cards()) : Player {
+class GamePlayer(
+    override val name: String,
+    initialState: State = Started()
+) : Player {
     init {
         require(name.isNotBlank()) { "Player 이름은 공백이 될 수 없습니다. 유효한 Player 명을 입력해주세요." }
     }
 
-    override fun readyToPlay(initialCards: List<Card>) {
-        require(initialCards.size == INITIAL_CARDS_COUNT) { "잘못된 초기 카드 개수 입니다. 최초 2장만 카드를 받을 수 있습니다." }
-        initialCards.forEach(cards::add)
+    private var _state: State = initialState
+    override val cards: Cards
+        get() = _state.cards
+    override val state: State
+        get() = _state
+
+    override val finished: Boolean
+        get() = state.finished
+
+    override fun shouldBeReadyToPlay(): Boolean =
+        cards.size == Game.INITIAL_CARDS_COUNT && state !is Started
+
+    override fun draw(card: Card) {
+        _state = state.draw(card)
     }
 
-    override fun hit(card: Card) = cards.add(card)
+    override fun stay() {
+        _state = state.stay()
+    }
 
-    override fun sumCards(): Int = cards.sum()
-
-    override fun bust(): Boolean = cards.sum() > BLACKJACK_SCORE
-
-    override fun blackjack(): Boolean =
-        cards.size == INITIAL_CARDS_COUNT && cards.sum() == BLACKJACK_SCORE
+    override fun sumCards(): Int = state.cards.sum()
 }
