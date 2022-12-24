57041b1eccf1f5b9066f01a12cc674a1bfaba461
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-24 20:10:17 +0900

    refactor(Name): Name 값객체 생성

--- src/main/kotlin/blackjack/domain/User.kt
+++ src/main/kotlin/blackjack/domain/User.kt
@@ -3,8 +3,8 @@ package blackjack.domain
 class User(
     override val name: Name,
     override val cards: Cards = Cards()
-): Player {
-    constructor(name: String): this(name = Name(name))
+) : Player {
+    constructor(name: String) : this(name = Name(name))
 
     override fun copy(name: Name, cards: Cards): Player {
         return User(name = name, cards = cards)
