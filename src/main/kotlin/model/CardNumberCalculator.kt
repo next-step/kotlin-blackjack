2d89a7f0677592c151020d677479aeb00162731c
James <bookwriterseo@gmail.com>
2022-12-26 23:31:12 +0900

    feat: ACE 카드 처리 로직 구현

--- src/main/kotlin/model/CardNumberCalculator.kt
+++ src/main/kotlin/model/CardNumberCalculator.kt
@@ -1,12 +1,30 @@
 package model
 
+import kotlin.math.abs
+
 class CardNumberCalculator {
     fun isUnderTwentyOne(cards: List<Card>): Boolean {
+        return totalNumber(cards) < BLACK_JACK
+    }
+
+    private fun totalNumber(cards: List<Card>): Int {
         var total = 0
         cards.forEach {
-            total += it.cardNumber.number
+            if (it.cardNumber != CardNumber.ACE) {
+                total += it.cardNumber.number
+            }
         }
-        return total < BLACK_JACK
+        total += decideAceCardNumber(total)
+        return total
+    }
+
+    fun decideAceCardNumber(semiTotal: Int): Int {
+        val ace = if (abs(BLACK_JACK - (semiTotal + 1)) <= abs(BLACK_JACK - (semiTotal + 11))) {
+            1
+        } else {
+            11
+        }
+        return ace
     }
 
     companion object {
--- src/test/kotlin/model/CardNumberCalculatorTest.kt
+++ src/test/kotlin/model/CardNumberCalculatorTest.kt
@@ -33,4 +33,19 @@ internal class CardNumberCalculatorTest {
             }
         )
     }
+
+    @Test
+    fun `적절한 ACE 카드 값을 결정한다`() {
+        assertAll(
+            {
+                assertThat(CardNumberCalculator().decideAceCardNumber(20)).isSameAs(1)
+            },
+            {
+                assertThat(CardNumberCalculator().decideAceCardNumber(14)).isSameAs(11)
+            },
+            {
+                assertThat(CardNumberCalculator().decideAceCardNumber(15)).isSameAs(1)
+            }
+        )
+    }
 }
