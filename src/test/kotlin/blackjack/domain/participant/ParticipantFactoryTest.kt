decde3c62aa90da4d4648f7e9c492ead4bab34de
namonak <namonak@gmail.com>
2022-12-26 23:12:27 +0900

    feat(step2): add the ParticipantFactory test code

---
+++ src/test/kotlin/blackjack/domain/participant/ParticipantFactoryTest.kt
@@ -0,0 +1,47 @@
+package blackjack.domain.participant
+
+import blackjack.application.Deck
+import blackjack.domain.card.PlayingCards
+import blackjack.domain.card.SpadeAce
+import blackjack.domain.card.SpadeJack
+import blackjack.domain.card.strategy.RandomShuffleStrategy
+import blackjack.domain.participant.state.Name
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ParticipantFactoryTest {
+    @Test
+    fun `참가자 - 생성 테스트`() {
+        // given
+        val cards = PlayingCards(SpadeAce, SpadeJack)
+        val name = Name("pobi")
+
+        // when
+        val actual = ParticipantFactory.create(name, cards)
+
+        // then
+        assertThat(actual).isInstanceOf(Player::class.java)
+        assertThat(actual.name).isEqualTo(name)
+        assertThat(actual.state.cards).isEqualTo(cards)
+    }
+
+    @Test
+    fun `참가자 - 복수 생성 테스트`() {
+        // given
+        val cards = PlayingCards.shuffle(RandomShuffleStrategy())
+        val deck = Deck(cards.toMutableList())
+        val names = listOf("pobi", "jason").map { Name(it) }
+
+        // when
+        val actual = ParticipantFactory.create(names.toTypedArray(), deck)
+
+        // then
+        assertThat(actual.getDealer()).isInstanceOf(Dealer::class.java)
+        assertThat(actual.getPlayers().map { it.name }).containsAll(names)
+        assertThat(actual.getPlayers().map { it.state }).allMatch { it.cards.size() == NUMBER_OF_STARTING_CARDS }
+    }
+
+    companion object {
+        private const val NUMBER_OF_STARTING_CARDS = 2
+    }
+}
