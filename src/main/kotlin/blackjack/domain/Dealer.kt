8fc525f045b8a096a31f881e59bc0656806b1a6d
harris.yi <harris.yi@kakaostyle.com>
2022-12-28 19:31:11 +0900

    feat: Add properties in Dealer class

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -3,7 +3,11 @@ package blackjack.domain
 private val DEALER_MIN_POINT = Point(17)
 
 class Dealer(playingCards: PlayingCards = PlayingCards()) {
-    val player = Player("딜러", playingCards)
+    private val player = Player("딜러", playingCards)
+    val name: String
+        get() = player.name
+    val playingCards: PlayingCards
+        get() = player.playingCards
 
     fun hitUntil(deck: CardDeck): Int {
         var count = 0
@@ -17,10 +21,6 @@ class Dealer(playingCards: PlayingCards = PlayingCards()) {
     fun cardPoint(): Point = player.cardPoint()
     fun blackJack(): Boolean = player.blackJack()
     fun bust(): Boolean = player.bust()
-
-    fun addCard(card: Card) {
-        player.addCard(card)
-    }
 
     fun addCard(cards: Set<Card>) {
         player.addCard(cards)
