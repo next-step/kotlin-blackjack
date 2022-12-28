14334d9657d2c4cefb6a56aa02591bb7a9e9cf6e
harris.yi <harris.yi@kakaostyle.com>
2022-12-28 19:12:28 +0900

    test: Change test name and field name

--- src/test/kotlin/blackjack/BlackJackGameTest.kt
+++ src/test/kotlin/blackjack/BlackJackGameTest.kt
@@ -1,8 +1,11 @@
 package blackjack
 
 import blackjack.domain.*
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+import java.lang.IllegalArgumentException
 
 class BlackJackGameTest : StringSpec({
     "게임을 시작하면 딜러에게 카드를 두장 (딜러는 한 장만 보여준다) 플레이어에게 카드를 두장씩 나눠준다." {
@@ -21,6 +24,23 @@ class BlackJackGameTest : StringSpec({
         playerDtos[0].cards.size shouldBe 1
         playerDtos[1].cards.size shouldBe 2
     }
+
+    "유효하지 않은 플레이어 이름으로 addCard 함수를 호출하면 IllegalArgumentException이 발생한다." {
+        //given
+        val game = BlackJackGame(
+            listOf(Player("harris")),
+            Dealer(),
+            CardDeck(),
+        )
+
+        //when
+        game.start()
+
+        //then
+        shouldThrow<IllegalArgumentException> { game.addCard("hurris") }
+            .shouldHaveMessage("존재하지 않는 플레이어 이름입니다.")
+    }
+
     "딜러의 카드 점수가 16점일 때 hit을 한 번 수행한다." {
         //given
         val game = BlackJackGame(
@@ -34,10 +54,10 @@ class BlackJackGameTest : StringSpec({
         hitCount shouldBe 1
     }
 
-    "harris가 딜러에게 이겼을 때 winningCount는 1, losingCount는 0이다." {
+    "harris가 베팅금액 10000으로 딜러에게 이겼을 때 수익은 10000이다." {
         //given
         val game = BlackJackGame(
-            listOf(Player("harris", playingCards = PlayingCards(mutableSetOf(Card.CLOVER_10, Card.DIAMOND_8)))),
+            listOf(Player("harris", playingCards = PlayingCards(mutableSetOf(Card.CLOVER_10, Card.DIAMOND_8)), 10000)),
             Dealer(playingCards = PlayingCards(mutableSetOf(Card.CLOVER_10, Card.DIAMOND_7))),
             CardDeck(),
         )
@@ -49,10 +69,8 @@ class BlackJackGameTest : StringSpec({
 
         //then
         playerDtos.size shouldBe 2
-        dealer.losingCount shouldBe 1
-        dealer.winningCount shouldBe 0
-        harris.losingCount shouldBe 0
-        harris.winningCount shouldBe 1
+        harris.winningAmount shouldBe 10000
+        dealer.winningAmount shouldBe -10000
     }
 
 })
