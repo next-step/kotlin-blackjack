40effcd861a3e39450cb2b6df175557341467fdd
harris.yi <harris.yi@kakaostyle.com>
2022-12-28 19:30:12 +0900

    feat: Move directory

---
+++ src/main/kotlin/blackjack/util/BlackjackResultMaker.kt
@@ -0,0 +1,13 @@
+package blackjack.util
+
+import blackjack.domain.Dealer
+import blackjack.domain.Player
+import blackjack.view.dto.PlayerGameResult
+
+object BlackjackResultMaker {
+    fun result(dealer: Dealer, players: List<Player>): List<PlayerGameResult> {
+        val playerGameResults = players.map { PlayerGameResult(it, it.flip(dealer).winningAmount) }
+        val dealerBettingAmount = -playerGameResults.sumOf { it.winningAmount }
+        return listOf(PlayerGameResult(dealer, dealerBettingAmount)) + playerGameResults
+    }
+}
