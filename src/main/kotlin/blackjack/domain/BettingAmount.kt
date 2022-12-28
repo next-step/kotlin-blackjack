c9466825c98e0a4ab1465a38e1b834228b15f2a1
harris.yi <harris.yi@kakaostyle.com>
2022-12-29 00:16:14 +0900

    feat: Change WinningAmountCalculator.kt to BettingAmount class

---
+++ src/main/kotlin/blackjack/domain/BettingAmount.kt
@@ -0,0 +1,11 @@
+package blackjack.domain
+
+private const val BLACK_JACK_WINNING_AMOUNT_RATE = 1.5
+private const val NO_AMOUNT = 0
+
+class BettingAmount(private val amount: Int) {
+    fun win() = amount
+    fun blackJack() = (amount * BLACK_JACK_WINNING_AMOUNT_RATE).toInt()
+    fun lose() = -amount
+    fun tie() = NO_AMOUNT
+}
