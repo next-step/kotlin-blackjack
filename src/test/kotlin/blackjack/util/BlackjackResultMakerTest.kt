d6cab640eabce68e02bf1cf35b4e33f1b1d16a6c
harris.yi <harris.yi@kakaostyle.com>
2022-12-29 00:32:51 +0900

    test: Validate if all bust, dealer win

--- src/test/kotlin/blackjack/util/BlackjackResultMakerTest.kt
+++ src/test/kotlin/blackjack/util/BlackjackResultMakerTest.kt
@@ -1,6 +1,7 @@
 package blackjack.util
 
 import blackjack.domain.*
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.ints.shouldBeInRange
 import io.kotest.matchers.shouldBe
@@ -80,6 +81,35 @@ class BlackjackResultMakerTest : StringSpec({
         result[2].totalPoint shouldBe Point(21)
         result[3].winningAmount shouldBe 0
         result[3].totalPoint shouldBe Point(19)
+    }
 
+    "(플레이어 다수) 딜러 bust, 플레이어1 bust 베팅 10000, 플레이어2 bust 20000 일 때 딜러의 상금은 30000원이다." {
+        //given
+        val dealer = Dealer(PlayingCards(mutableSetOf(Card.DIAMOND_6, Card.DIAMOND_7, Card.DIAMOND_K)))
+        val players =
+            listOf(
+                Player(
+                    "harris",
+                    PlayingCards(mutableSetOf(Card.CLOVER_J, Card.CLOVER_4, Card.CLOVER_8)),
+                    BettingAmount(10000)
+                ),
+                Player(
+                    "hurris",
+                    PlayingCards(mutableSetOf(Card.CLOVER_K, Card.CLOVER_6, Card.CLOVER_10)),
+                    BettingAmount(20000)
+                ),
+            )
+        //when
+        val result = BlackjackResultMaker.result(dealer, players)
+
+        //then
+        assertSoftly {
+            result[0].winningAmount shouldBe 30000
+            result[0].totalPoint shouldBe Point(0)
+            result[1].winningAmount shouldBe -10000
+            result[1].totalPoint shouldBe Point(0)
+            result[2].winningAmount shouldBe -20000
+            result[2].totalPoint shouldBe Point(0)
+        }
     }
 })
