755474c4c744cdd97fad390b74f4effe497ab200
harris.yi <harris.yi@kakaostyle.com>
2022-12-28 17:29:40 +0900

    feat: Add askBettingAmount in Input

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,5 +1,7 @@
 package blackjack.view
 
+import java.lang.IllegalArgumentException
+
 object InputView {
     fun askNames(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
@@ -12,5 +14,13 @@ object InputView {
             return true
         }
         return false
+    }
+
+    fun askBettingAmount(name: String): Int {
+        println("${name}의 베팅 금액은?")
+        val amount = readln().toIntOrNull()
+        requireNotNull(amount) { "베팅 금액은 숫자로 입력해야 합니다." }
+        require(amount > 0) { "베팅 금액은 0보다 큰 숫자여야 합니다." }
+        return amount
     }
 }
