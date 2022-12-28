3ae8f0570beb6669b154b26934d1993e59d524a9
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-29 07:10:44 +0900

    test(ProfitCalculator): 플레이어가 승리하면 베팅 금액의 2배를 받는다.

--- src/test/kotlin/blackjack/domain/ProfitCalculatorTest.kt
+++ src/test/kotlin/blackjack/domain/ProfitCalculatorTest.kt
@@ -3,7 +3,7 @@ package blackjack.domain
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-internal class ProfitCalculatorTest: StringSpec({
+internal class ProfitCalculatorTest : StringSpec({
     "딜러와 플레이어가 무승부라면 베팅금액과 상관없이 플레이어의 수익은 0원이다." {
         // 12 점
         val user = User(Card(Suite.SPADE, Denomination.JACK), Card(Suite.DIAMOND, Denomination.TWO))
@@ -25,5 +25,16 @@ internal class ProfitCalculatorTest: StringSpec({
 
         val result = ProfitCalculator().calculate(user, userResultStatus)
         result.value shouldBe user.betAmount.value * 2.5
+    }
+
+    "플레이어가 승리하면 베팅금액의 2배를 받는다." {
+        val user = User(Card(Suite.SPADE, Denomination.ACE), Card(Suite.CLOVER, Denomination.NINE))
+        val dealer = Dealer(Card(Suite.CLOVER, Denomination.TWO), Card(Suite.HEART, Denomination.NINE))
+
+        val userResultStatus = dealer.getMatchResult(user)
+        userResultStatus shouldBe ResultStatus.WIN
+
+        val result = ProfitCalculator().calculate(user, userResultStatus)
+        result.value shouldBe user.betAmount.value * 2
     }
 })
