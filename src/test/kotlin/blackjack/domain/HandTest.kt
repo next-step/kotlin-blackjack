b4fcc2d1b02fa1026f5c43cfc81081feb2e365db
wonjun3991 <wonjun.choi@meshkorea.net>
2022-12-31 08:02:45 +0900

    feature(Hand): add Test Case when caculate Ace

--- src/test/kotlin/blackjack/domain/HandTest.kt
+++ src/test/kotlin/blackjack/domain/HandTest.kt
@@ -14,6 +14,16 @@ internal class HandTest {
     }
 
     @Test
+    fun `기존 카드의 합계가 11 이상이면 Ace카드는 1로 계산된다`() {
+        val hand = Hand()
+        hand.addCard(Card(Shape.CLUB, Denomination.TWO))
+        hand.addCard(Card(Shape.CLUB, Denomination.NINE))
+        hand.addCard(Card(Shape.CLUB, Denomination.ACE))
+
+        hand.score().value shouldBe 12
+    }
+
+    @Test
     fun `Ace카드는 11로 계산될 수 있다`() {
         val hand = Hand()
         hand.addCard(Card(Shape.CLUB, Denomination.TEN))
