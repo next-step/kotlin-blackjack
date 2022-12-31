fce3609014e60978f15b565515837de3a76c2217
seyun(Marco) <ksy90101@gmail.com>
2022-12-31 15:39:59 +0900

    feat: kotlin dsl 1단계

---
+++ src/main/kotlin/study/Skill.kt
@@ -0,0 +1,5 @@
+package study
+
+sealed class Skill(open val name: String)
+data class HardSkill(override val name: String) : Skill(name)
+data class SoftSkill(override val name: String) : Skill(name)
