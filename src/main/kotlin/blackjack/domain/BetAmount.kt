a2e1c9fed5ffcba1e819df4ef2228d3e46ceb821
youngbin-yun <youngbin@koreaspacedata.com>
2022-12-31 19:36:07 +0900

    refactor: 상수화 적용

--- src/main/kotlin/blackjack/domain/BetAmount.kt
+++ src/main/kotlin/blackjack/domain/BetAmount.kt
@@ -3,6 +3,10 @@ package blackjack.domain
 @JvmInline
 value class BetAmount(val value: Int) {
     init {
-        require(value >= 0) { "베팅 금액은 0원 이상이어야 합니다." }
+        require(value >= MIN) { "베팅 금액은 $MIN 원 이상이어야 합니다." }
+    }
+
+    companion object {
+        private const val MIN = 0
     }
 }
