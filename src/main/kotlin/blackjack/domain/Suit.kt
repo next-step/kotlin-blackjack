b1d2e7d15ee240f16fef7aa1d1b445861ecd4cd7
Choiyounho <maxosa78@gmail.com>
2022-12-31 21:34:57 +0900

    feat : Suit 랜덤 생성 기능 추가

--- src/main/kotlin/blackjack/domain/Suit.kt
+++ src/main/kotlin/blackjack/domain/Suit.kt
@@ -7,5 +7,11 @@ enum class Suit(
     SPADE("스페이드"),
     HEART("하트"),
     DIAMOND("다이아몬드"),
-    CLOVER("클로버")
+    CLOVER("클로버");
+
+    companion object {
+        fun random(): Suit {
+            return values().random()
+        }
+    }
 }
