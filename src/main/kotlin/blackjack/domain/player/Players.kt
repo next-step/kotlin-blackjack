a0cea5e8027b7bd0eba42f88287395856e514200
김신영 <rolroralra@gmail.com>
2022-12-31 20:27:44 +0900

    feat: Step3: 블랙잭(딜러) - 코드리뷰 반영
    - List<Player>.toPlayers 확장함수 추가

--- src/main/kotlin/blackjack/domain/player/Players.kt
+++ src/main/kotlin/blackjack/domain/player/Players.kt
@@ -2,9 +2,12 @@ package blackjack.domain.player
 
 data class Players(
     private val players: List<Player>
-): List<Player> by players {
+) : List<Player> by players {
 
     fun names(): List<String> = players.map { it.name }
 
-    fun notFinishedPlayers(): List<Player> = players.filter { it.isNotFinished() }
+    fun notFinishedPlayers(): Players = players.filter { it.isNotFinished() }.toPlayers()
 }
+
+fun List<Player>.toPlayers(): Players =
+    Players(this)
