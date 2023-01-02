0f1ca44d031d87616b3c796290c72912dab25919
wrallee <wrallee@naver.com>
2023-01-02 21:52:52 +0900

    refactor(Blackjack): 딜러 STOP_SCORE 변수명 변경(PLAYER -> DEALER)

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -3,10 +3,10 @@ package blackjack.model
 class Dealer(
     override var cards: Cards = Cards()
 ) : Gambler {
-    override val stopScore = PLAYER_STOP_SCORE
+    override val stopScore = DEALER_STOP_SCORE
 
     companion object {
-        const val PLAYER_STOP_SCORE = 16
+        const val DEALER_STOP_SCORE = 16
 
         fun init(cardDeck: CardDeck, initCardCount: Int): Dealer {
             return Dealer(cardDeck.drawCards(initCardCount))
