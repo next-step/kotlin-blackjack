866bf2bf5227fabf11261756f92d85c7568956ed
wrallee <wrallee@naver.com>
2023-01-02 18:59:45 +0900

    refactor(Blackjack): DualResult.of 메서드 내 분기로직 인스턴스 변수화

--- src/main/kotlin/blackjack/model/DualResult.kt
+++ src/main/kotlin/blackjack/model/DualResult.kt
@@ -1,17 +1,14 @@
 package blackjack.model
 
-enum class DualResult {
-    WIN, PUSH, LOSE;
+enum class DualResult(private val compare: (Int, Int) -> Boolean) {
+    WIN({ score, otherScore -> score > otherScore }),
+    PUSH({ score, otherScore -> score == otherScore }),
+    LOSE({ score, otherScore -> score < otherScore });
 
     companion object {
         fun of(score: Int, otherScore: Int): DualResult {
-            if (score == otherScore) {
-                return PUSH
-            }
-            if (score > otherScore) {
-                return WIN
-            }
-            return LOSE
+            return values().find { it.compare(score, otherScore) }
+                ?: throw IllegalArgumentException("해당되는 듀얼 결과가 없습니다.")
         }
     }
 }
