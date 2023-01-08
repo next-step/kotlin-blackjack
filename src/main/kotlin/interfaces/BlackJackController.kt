08db26e1825f3cd46f9dc4f5ceb745bdfa009dd3
EP <kjuioqq8@gmail.com>
2023-01-09 07:10:18 +0900

    refactor: 프린트 view 책임으로 변경

--- src/main/kotlin/interfaces/BlackJackController.kt
+++ src/main/kotlin/interfaces/BlackJackController.kt
@@ -3,8 +3,6 @@ package interfaces
 import application.BlackJackGame
 import common.Executable
 import domain.player.Participants
-import domain.player.Playable
-import interfaces.ui.CardInfo
 import interfaces.ui.InputConsole
 import interfaces.ui.OutputConsole
 
@@ -25,45 +23,30 @@ class BlackJackController : Executable {
         showFinalResult(blackJackGame)
     }
 
-    private fun initGame(blackJackGame: BlackJackGame) {
-        blackJackGame.init()
-        val playerNames = blackJackGame.allPlayers().map { it.name }
-        OutputConsole.printInit(playerNames = playerNames)
-    }
-
     private fun queryParticipants(): Participants {
         val nameList = InputConsole.queryParticipantName()
         return Participants.from(names = nameList)
     }
 
-    private fun showAllParticipantsHands(blackJackGame: BlackJackGame) {
-        val dealer = blackJackGame.dealer
-        val dealerCardInfo = cardInfo(dealer)
-        OutputConsole.printCard(playerName = "딜러", cardInfo = dealerCardInfo)
-
-        val players = blackJackGame.allPlayers()
-        players.forEach { player ->
-            val cardInfo = cardInfo(player)
-            OutputConsole.printCard(playerName = player.name, cardInfo = cardInfo)
-        }
+    private fun initGame(blackJackGame: BlackJackGame) {
+        blackJackGame.init()
+        val allPlayers = blackJackGame.allPlayers()
+        OutputConsole.printInit(allPlayers = allPlayers)
     }
 
-    private fun cardInfo(playable: Playable): List<CardInfo> {
-        return playable.hands.cardList().map {
-            CardInfo(
-                number = it.number.nameValue,
-                name = it.shape.nameValue
-            )
-        }
+    private fun showAllParticipantsHands(blackJackGame: BlackJackGame) {
+        val dealer = blackJackGame.dealer
+        OutputConsole.printDealerCard(dealer = dealer)
+
+        val players = blackJackGame.allPlayers()
+        OutputConsole.printPlayerCard(players)
     }
 
     private fun queryReceiveCard(blackJackGame: BlackJackGame) {
-        OutputConsole.printNewLine()
         val allPlayers = blackJackGame.allPlayers()
         allPlayers.forEach {
             blackJackGame.playsTurn(player = it) { player -> InputConsole.queryReceiveCard(player.name) }
-            val cardInfo = cardInfo(it)
-            OutputConsole.printCard(playerName = it.name, cardInfo = cardInfo)
+            OutputConsole.printCard(it)
         }
     }
 
@@ -75,24 +58,15 @@ class BlackJackController : Executable {
     }
 
     private fun showFinalResult(blackJackGame: BlackJackGame) {
-        OutputConsole.printNewLine()
-
         val dealer = blackJackGame.dealer
-        val dealerCardInfo = cardInfo(dealer)
-        val resultDealerScore = dealer.handsCardScore()
-        OutputConsole.printCardWithResult(playerName = "딜러", cardInfo = dealerCardInfo, result = resultDealerScore.toString())
+        OutputConsole.printCardWithDealerResult(dealer = dealer)
 
         val players = blackJackGame.allPlayers()
-        players.forEach { player ->
-            val cardInfo = cardInfo(player)
-            val resultScore = player.handsCardScore()
-            OutputConsole.printCardWithResult(playerName = player.name, cardInfo = cardInfo, result = resultScore.toString())
-        }
+        OutputConsole.printCardWithResult(players)
 
         val gameResult = blackJackGame.complete()
         val resultPlayerBoards = gameResult.resultPlayerBoards
         val resultDealerBoard = gameResult.resultDealerBoard
-
         OutputConsole.printFinalResult(resultDealerBoard, resultPlayerBoards)
     }
 }
--- src/main/kotlin/interfaces/ui/OutputConsole.kt
+++ src/main/kotlin/interfaces/ui/OutputConsole.kt
@@ -1,25 +1,38 @@
 package interfaces.ui
 
 import application.BlackJackGame
+import domain.player.Dealer
+import domain.player.Playable
+import domain.player.Player
 
 object OutputConsole {
 
     private const val NAME_DELIMITER = ", "
 
-    fun printCard(playerName: String, cardInfo: List<CardInfo>) {
+    fun printDealerCard(dealer: Dealer) {
+        val cardInfo = dealer.cardInfo()
+        printCard(playerName = "딜러", cardInfo = cardInfo)
+    }
+
+    private fun printCard(playerName: String, cardInfo: List<CardInfo>) {
         println("${playerName}카드: ${cardInfo.toStr()}")
     }
 
-    fun printNewLine() {
-        println()
+    fun printCardWithResult(allPlayers: List<Player>) {
+        allPlayers.forEach {
+            val playerName = it.name
+            val cardInfo = it.cardInfo()
+            val resultScore = it.handsCardScore()
+            printCardWithResult(playerName, cardInfo, resultScore.toString())
+        }
     }
 
-    fun printCardWithResult(playerName: String, cardInfo: List<CardInfo>, result: String) {
+    private fun printCardWithResult(playerName: String, cardInfo: List<CardInfo>, result: String) {
         println("${playerName}카드: ${cardInfo.toStr()} - 결과: $result")
     }
 
-    fun printInit(playerNames: List<String>) {
-        val playerNameString = playerNames.joinToString(NAME_DELIMITER)
+    fun printInit(allPlayers: List<Player>) {
+        val playerNameString = allPlayers.joinToString(NAME_DELIMITER) { it.name }
         println()
         println("딜러와 ${playerNameString}에게 2장의 카드를 나누었습니다.")
     }
@@ -40,6 +53,35 @@ object OutputConsole {
             val result = if (it.isWin) "승" else "패"
             println("${it.name}: $result")
         }
+    }
+
+
+    fun printPlayerCard(players: List<Player>) {
+        players.forEach {
+            printCard(it)
+        }
+        println()
+    }
+
+    private fun Playable.cardInfo(): List<CardInfo> {
+        return this.hands.cardList().map {
+            CardInfo(
+                number = it.number.nameValue,
+                name = it.shape.nameValue
+            )
+        }
+    }
+
+    fun printCard(player: Player) {
+        val cardInfo = player.cardInfo()
+        printCard(playerName = player.name, cardInfo = cardInfo)
+    }
+
+    fun printCardWithDealerResult(dealer: Dealer) {
+        println()
+        val dealerCardInfo = dealer.cardInfo()
+        val resultDealerScore = dealer.handsCardScore()
+        printCardWithResult(playerName = "딜러", cardInfo = dealerCardInfo, result = resultDealerScore.toString())
     }
 }
 
