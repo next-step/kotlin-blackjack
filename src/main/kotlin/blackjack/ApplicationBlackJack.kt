09ba35852dbf27894b6e4ef22a3f5f76cc9dc4f3
hojihun5516 <hojihun5516@daum.net>
2023-01-18 19:34:52 +0900

    Application 로직 추가

---
+++ src/main/kotlin/blackjack/ApplicationBlackJack.kt
@@ -0,0 +1,22 @@
+import blackjack.GameRule
+import blackjack.ResultCalculator
+import blackjack.domains.deck.Deck
+import blackjack.domains.deck.PokerNumber
+import blackjack.domains.deck.PokerShape
+import blackjack.views.Input.getPlayerNames
+import blackjack.views.Output.printFirstDrawCards
+import blackjack.views.Output.printVictoryOrDefeat
+
+fun main() {
+    val playerNames = getPlayerNames()
+    val deck = Deck(pokerNumbers = PokerNumber.values().toList(), pokerShapes = PokerShape.values().toList())
+
+    val gameRule = GameRule(deck = deck)
+    printFirstDrawCards(playerNames, GameRule.FIRST_DRAW_COUNT)
+    val gamers = gameRule.initUsers(playerNames)
+
+    gamers.drawCard(gameRule = gameRule)
+    gamers.calculateScore(gameRule = gameRule)
+    ResultCalculator(gamers).setUserRanks()
+    printVictoryOrDefeat(gamers.getDealer(), gamers.getPlayers())
+}
