4b5322ed310d0f5742e5a359670088835110d9aa
ljw0829 <olo_jinwon@naver.com>
2023-06-17 01:11:36 +0900

    test: 플레이 카드 기능 요구사항 테스트

---
+++ src/test/kotlin/blackjack/domain/card/PlayingCardsTest.kt
@@ -0,0 +1,67 @@
+package blackjack.domain.card
+
+import blackjack.domain.deck.Denomination
+import blackjack.domain.deck.Suit
+import blackjack.domain.model.BlackJackErrorCode
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
+
+class PlayingCardsTest : BehaviorSpec({
+
+    Given(name = "게임에서 사용할 카드를 생성할 수 있다.") {
+        val expect = mutableSetOf(
+            Card(denomination = Denomination.ACE, suit = Suit.CLUBS),
+            Card(denomination = Denomination.QUEEN, suit = Suit.DIAMONDS),
+        )
+
+        When(name = "주어진 카드 리스트를 등록하게 되면") {
+            val playingCards = PlayingCards(cards = expect)
+
+            Then(name = "해당 카드 리스트가 들어가있다.") {
+                playingCards shouldBe expect
+            }
+        }
+
+        val addExpect = Card(denomination = Denomination.FIVE, suit = Suit.CLUBS)
+        val playingCards = PlayingCards(cards = expect)
+
+        When(name = "새로운 카드를 추가하면") {
+            playingCards.add(card = addExpect)
+
+            Then(name = "추가한 카드가 들어가있다.") {
+                playingCards shouldContain addExpect
+            }
+        }
+
+        When(name = "이미 추가된 카드를 추가하면") {
+            val exception = shouldThrow<IllegalStateException> {
+                playingCards.add(card = addExpect)
+            }
+
+            Then(name = "중복된 카드를 등록할 수 없다는 에러가 발생한다.") {
+                exception shouldHaveMessage BlackJackErrorCode.CAN_NOT_ADD_DUPLICATE_CARD.message(arrayOf(addExpect))
+            }
+        }
+
+        When(name = "정해진 버스트 값보다 작으면") {
+            val actual = playingCards.isBust()
+
+            Then(name = "거짓을 반환한다.") {
+                actual shouldBe false
+            }
+        }
+
+        playingCards.add(card = Card(denomination = Denomination.JACK, suit = Suit.DIAMONDS))
+
+        When(name = "정해진 버스트 값을 넘으면") {
+            val actual = playingCards.isBust()
+
+            Then(name = "참을 반환한다.") {
+                actual shouldBe true
+            }
+        }
+    }
+})
