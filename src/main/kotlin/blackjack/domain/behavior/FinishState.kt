ff1d8689d1d2dd92453d55ba6a4ac9482cc2a26a
ljw0829 <olo_jinwon@naver.com>
2023-06-17 15:19:13 +0900

    refactor: state 불필요한 메서드 정의 제거

--- src/main/kotlin/blackjack/domain/behavior/FinishState.kt
+++ src/main/kotlin/blackjack/domain/behavior/FinishState.kt
@@ -1,13 +1,8 @@
 package blackjack.domain.behavior
 
-import blackjack.domain.card.Card
 import blackjack.domain.card.PlayingCards
 
 class FinishState(playingCards: PlayingCards) : State(playingCards = playingCards) {
-
-    override fun hit(card: Card): State = throwDoneInState()
-
-    override fun stay(): FinishState = throwDoneInState()
 
     override fun resultScore(): Int = playingCards.calculateOptimalScore()
 }
