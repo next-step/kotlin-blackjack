bd8eb08f334f3e416aa7543ae796feba2c9f5faf
yoo-jaein <y_jaein@naver.com>
2023-06-21 00:56:50 +0900

    test: Skills 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-//introduce {
+// introduce {
 //    name("홍길동")
 //    company("활빈당")
 //    skills {
@@ -18,7 +18,7 @@ import org.junit.jupiter.params.provider.ValueSource
 //        "Korean" level 5
 //        "English" level 3
 //    }
-//}
+// }
 class DslTest {
     @ValueSource(strings = ["홍길동", "유재인"])
     @ParameterizedTest
@@ -40,17 +40,45 @@ class DslTest {
         person.name shouldBe "홍길동"
         person.company shouldBe "활빈당"
     }
+
+    @Test
+    fun skills() {
+        val person = introduce {
+            name("홍길동")
+            company("활빈당")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+
+        person.name shouldBe "홍길동"
+        person.company shouldBe "활빈당"
+
+        val skills = person.skills.skills
+        skills.size shouldBe 3
+        skills[0].description shouldBe "A passion for problem solving"
+        skills[1].description shouldBe "Good communication skills"
+        skills[2].description shouldBe "Kotlin"
+    }
 }
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
     return PersonBuilder().apply(block).build()
 }
 
-class Person(val name: String, val company: String?)
+class Person(val name: String, val company: String?, val skills: Skills)
+
+@JvmInline
+value class Skills(val skills: List<Skill>)
+
+class Skill(val description: String, val type: String)
 
 class PersonBuilder {
-    lateinit var name: String
-    var company: String? = null
+    private lateinit var name: String
+    private var company: String? = null
+    private val skillsList = mutableListOf<Skill>()
 
     fun name(value: String) {
         name = value
@@ -60,8 +88,31 @@ class PersonBuilder {
         company = value
     }
 
+    fun skills(block: SkillsBuilder.() -> Unit) {
+        val skillsBuilder = SkillsBuilder()
+        skillsBuilder.block()
+        skillsList.addAll(skillsBuilder.skillsList)
+    }
+
     fun build(): Person {
-        return Person(name, company)
+        val skills = Skills(skillsList)
+        return Person(name, company, skills)
     }
 }
 
+class SkillsBuilder {
+    val skillsList = mutableListOf<Skill>()
+
+    fun soft(description: String) {
+        skillsList.add(Skill(description, SkillType.SOFT.type))
+    }
+
+    fun hard(description: String) {
+        skillsList.add(Skill(description, SkillType.HARD.type))
+    }
+}
+
+enum class SkillType(val type: String) {
+    SOFT("soft"),
+    HARD("hard")
+}
