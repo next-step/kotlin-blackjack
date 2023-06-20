6b80ba27d30694810e7be4f630458780efa6dc79
genos-lee <genos.lee@kakaocorp.com>
2023-06-20 23:47:58 +0900

    feature : Languages dsl 테스트 코드 작성과 구현

---
+++ src/main/kotlin/next/step/dsl/Languages.kt
@@ -0,0 +1,20 @@
+package next.step.dsl
+
+fun languages(block: LanguagesBuilder.() -> Unit): Languages = LanguagesBuilder().apply(block).build()
+
+@JvmInline
+value class Languages(private val languagesMap: Map<String, Int> = emptyMap()) {
+    fun get(language: String): Int? {
+        return languagesMap[language]
+    }
+}
+
+class LanguagesBuilder() {
+    var languagesMap: MutableMap<String, Int> = mutableMapOf()
+    infix fun String.level(level: Int) {
+        languagesMap[this] = level
+    }
+
+    fun build(): Languages = Languages(languagesMap)
+
+}
