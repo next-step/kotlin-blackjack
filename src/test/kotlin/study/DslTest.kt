cec76f24b28c9dcb1d1dd6f9283bb31848b5bb5b
yoo-jaein <y_jaein@naver.com>
2023-06-21 00:58:21 +0900

    test: Languages 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -62,23 +62,62 @@ class DslTest {
         skills[1].description shouldBe "Good communication skills"
         skills[2].description shouldBe "Kotlin"
     }
+
+    @Test
+    fun languages() {
+        val person = introduce {
+            name("홍길동")
+            company("활빈당")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+
+        person.name shouldBe "홍길동"
+        person.company shouldBe "활빈당"
+
+        val skills = person.skills.skills
+        skills.size shouldBe 3
+        skills[0].description shouldBe "A passion for problem solving"
+        skills[1].description shouldBe "Good communication skills"
+        skills[2].description shouldBe "Kotlin"
+
+        val languages = person.languages.languages
+        languages.size shouldBe 2
+        languages[0].country shouldBe "Korean"
+        languages[0].level shouldBe 5
+        languages[1].country shouldBe "English"
+        languages[1].level shouldBe 3
+    }
 }
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
     return PersonBuilder().apply(block).build()
 }
 
-class Person(val name: String, val company: String?, val skills: Skills)
+class Person(val name: String, val company: String?, val skills: Skills, val languages: Languages)
 
 @JvmInline
 value class Skills(val skills: List<Skill>)
 
 class Skill(val description: String, val type: String)
 
+@JvmInline
+value class Languages(val languages: List<LanguageLevel>)
+
+class LanguageLevel(val country: String, val level: Int)
+
 class PersonBuilder {
     private lateinit var name: String
     private var company: String? = null
     private val skillsList = mutableListOf<Skill>()
+    private val languagesList = mutableListOf<LanguageLevel>()
 
     fun name(value: String) {
         name = value
@@ -94,9 +133,17 @@ class PersonBuilder {
         skillsList.addAll(skillsBuilder.skillsList)
     }
 
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        val languagesBuilder = LanguagesBuilder()
+        languagesBuilder.block()
+        languagesList.addAll(languagesBuilder.languagesList)
+    }
+
+
     fun build(): Person {
         val skills = Skills(skillsList)
-        return Person(name, company, skills)
+        val languages = Languages(languagesList)
+        return Person(name, company, skills, languages)
     }
 }
 
@@ -115,4 +162,12 @@ class SkillsBuilder {
 enum class SkillType(val type: String) {
     SOFT("soft"),
     HARD("hard")
+}
+
+class LanguagesBuilder {
+    val languagesList = mutableListOf<LanguageLevel>()
+
+    infix fun String.level(level: Int) {
+        languagesList.add(LanguageLevel(this, level))
+    }
 }
