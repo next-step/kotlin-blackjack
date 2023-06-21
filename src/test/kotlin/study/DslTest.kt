d03885befdec75a84c6af5471508f05ac8a7e6cf
pavlo.v <pavlo.v@kakaocorp.com>
2023-06-21 16:15:58 +0900

    추가 : 라이브강의 코드

---
+++ src/test/kotlin/study/DslTest.kt
@@ -0,0 +1,59 @@
+package study
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+/**
+ * introduce {
+ *   name("박재성")
+ *   company("우아한형제들")
+ *   skills {
+ *     soft("A passion for problem solving")
+ *     soft("Good communication skills")
+ *     hard("Kotlin")
+ *   }
+ *   languages {
+ *     "Korean" level 5
+ *     "English" level 3
+ *   }
+ * }
+ */
+class DslTest {
+    @ValueSource(strings = ["홍길동", "최환"])
+    @ParameterizedTest
+    fun introduce(value: String) {
+        val person: Person = introduce {
+            name(value)
+        }
+        person.name shouldBe value
+        person.company shouldBe null
+    }
+
+    @Test
+    fun company() {
+        val person = introduce {
+            name("홍길동")
+            company("활빈당")
+        }
+        person.name shouldBe "홍길동"
+        person.company shouldBe "활빈당"
+    }
+}
+
+fun introduce(block: Person.() -> Unit): Person {
+    return Person().apply(block)
+}
+
+class Person {
+    lateinit var name: String
+    var company: String? = null
+    fun name(value: String) {
+        name = value
+    }
+
+    fun company(value: String) {
+        company = value
+    }
+}
