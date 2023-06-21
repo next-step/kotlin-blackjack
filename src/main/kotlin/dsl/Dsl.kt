306693ea9ecd1ddaf85d5d76688e15b311e2d431
oliveunji <eunjikim2u@gmail.com>
2023-06-21 20:42:14 +0900

    feat: Initialize DSL code

---
+++ src/main/kotlin/dsl/Dsl.kt
@@ -0,0 +1,24 @@
+package dsl
+
+fun introduce(block: PersonBuilder.() -> Unit): Person {
+    return PersonBuilder().apply(block).build()
+}
+
+class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+
+    fun name(value: String) {
+        name = value
+    }
+
+    fun company(value: String) {
+        company = value
+    }
+
+    fun build(): Person {
+        return Person(name, company)
+    }
+}
+
+data class Person(val name: String, val company: String?)
---
+++ src/test/kotlin/dsl/DslTest.kt
@@ -0,0 +1,27 @@
+package dsl
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class DslTest {
+    @ValueSource(strings = ["EJ", "DJ"])
+    @ParameterizedTest
+    fun introduce(value: String) {
+        val person = introduce {
+            name(value)
+        }
+        assertThat(person.name).isEqualTo(value)
+    }
+
+    @Test
+    fun company() {
+        val person = introduce {
+            name("EJ")
+            company("Microsoft")
+        }
+        assertThat(person.name).isEqualTo("EJ")
+        assertThat(person.company).isEqualTo("Microsoft")
+    }
+}
