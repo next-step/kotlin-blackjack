736617211b5ff25c7ce753dd65c7fce999a07e30
Wordbe <seonghojin3@gmail.com>
2023-06-22 01:11:31 +0900

    feat(Card): 카드 52장을 메모리에 캐싱한다.

--- src/main/kotlin/blackjack/Card.kt
+++ src/main/kotlin/blackjack/Card.kt
@@ -1,6 +1,20 @@
 package blackjack
 
-data class Card(
+data class Card private constructor(
     val denomination: Denomination,
     val suit: Suit,
-)
+) {
+    companion object {
+        val cards = Denomination.values()
+            .flatMap { number ->
+                Suit.values()
+                    .map { suit -> Card(number, suit) }
+            }
+            .associateBy { Pair(it.denomination, it.suit) }
+
+        operator fun invoke(denomination: Denomination, suit: Suit): Card {
+            return cards[Pair(denomination, suit)]
+                ?: throw IllegalArgumentException("카드가 존재하지 않습니다: $denomination, $suit")
+        }
+    }
+}
