be2604beae8cd44068aa14e61643b8bc54f0c751
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-22 01:11:37 +0900

    feat: Cards 생성 성공테스트 추가

--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -1,9 +1,11 @@
 package blackjack
 
 import blackjack.Denomination.ACE
+import blackjack.Denomination.TWO
 import blackjack.Suit.SPADE
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 
 class CardsTest : FunSpec({
@@ -13,6 +15,11 @@ class CardsTest : FunSpec({
             val givenCards = listOf(Card(SPADE, ACE), Card(SPADE, ACE))
             val exception = shouldThrowExactly<IllegalArgumentException> { Cards(givenCards) }
             exception.message shouldBe "중복된 카드가 저장될 수 없다."
+        }
+
+        test("카드 리스트를 생성한다.") {
+            val actual = Cards(listOf(Card(SPADE, ACE), Card(SPADE, TWO)))
+            actual.values shouldHaveSize 2
         }
     }
 })
