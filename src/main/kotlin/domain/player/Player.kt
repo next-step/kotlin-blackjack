dea6a0a99f5ecaaf6b900b73a61282b8a1f4c6ff
nam-sangyun <nsun9505@naver.com>
2023-06-22 23:51:20 +0900

    feature: 플레이어는 카드를 받거나 안 받을 수 있다

--- src/main/kotlin/domain/player/Player.kt
+++ src/main/kotlin/domain/player/Player.kt
@@ -1,5 +1,24 @@
 package domain.player
 
+import domain.card.BlackjackCard
+import domain.card.BlackjackCards
+import domain.state.StartState
 import domain.state.State
 
-class Player(val state: State)
+class Player(name: String, card1: BlackjackCard, card2: BlackjackCard) {
+
+    val name: String
+    var state: State
+        private set
+    val cards: BlackjackCards
+        get() = state.getCards()
+
+    init {
+        this.name = name
+        this.state = StartState.start(card1, card2)
+    }
+
+    fun draw(card: BlackjackCard): State = state.draw(card)
+
+    fun stop(): State = state.stop()
+}
