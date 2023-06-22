e0715f563d79570f502c23dece6b949f4b0bba7c
nam-sangyun <nsun9505@naver.com>
2023-06-23 00:49:05 +0900

    feature: 덱의 카드 수는 312개다

---
+++ src/main/kotlin/domain/card/Deck.kt
@@ -0,0 +1,31 @@
+package domain.card
+
+class Deck {
+    private val cards: MutableList<BlackjackCard>
+    val cardCount
+        get() = cards.size
+
+    init {
+        cards = (1..DECK_SIZE)
+            .map { makeDeck() }
+            .flatten()
+            .shuffled()
+            .toMutableList()
+    }
+
+    fun getCard(): BlackjackCard = cards.removeAt(0)
+
+    private fun makeDeck(): List<BlackjackCard> {
+        return Suit.values().map {
+            makeCards(it, CardNumber.values())
+        }.flatten()
+    }
+
+    private fun makeCards(suit: Suit, cardNumbers: Array<CardNumber>): List<BlackjackCard> {
+        return cardNumbers.map { BlackjackCard(suit, it) }
+    }
+
+    companion object {
+        private const val DECK_SIZE = 6
+    }
+}
