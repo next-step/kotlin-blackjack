4295e2c83ba72e9999498b2104ca1ddf8c7a482d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2023-06-23 07:05:43 +0900

    feat: 카드 deal할 때 보유중인 카드가 없는 경우 예외 추가 및 테스트

--- src/main/kotlin/blackjack/CardDeck.kt
+++ src/main/kotlin/blackjack/CardDeck.kt
@@ -5,9 +5,13 @@ import java.util.Queue
 
 @JvmInline
 value class CardDeck(
-    private val values: Queue<Card>,
+    private val values: Queue<Card> = LinkedList(),
 ) {
     fun size() = values.size
+
+    fun deal() {
+        check(values.isNotEmpty()) { "남은 카드가 없을 땐 deal할 수 없다" }
+    }
 
     companion object {
         private val INIT_CARD_DECK =
--- src/test/kotlin/blackjack/CardDeckTest.kt
+++ src/test/kotlin/blackjack/CardDeckTest.kt
@@ -1,9 +1,17 @@
 package blackjack
 
+import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class CardDeckTest : FunSpec({
+
+    context("deal") {
+        test("카드를 deal할 때 보유중인 카드가 더 없는 경우 예외가 발생한다.") {
+            val exception = shouldThrowExactly<IllegalStateException> { CardDeck().deal() }
+            exception.message shouldBe "남은 카드가 없을 땐 deal할 수 없다"
+        }
+    }
 
     context("randomCardDeck") {
         test("랜덤한 카드 52장을 받을 수 있다.") {
