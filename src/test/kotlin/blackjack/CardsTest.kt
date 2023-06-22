06dffa6c9b2876d6c88b0aaaa961adf14f65b9df
yoo-jaein <y_jaein@naver.com>
2023-06-22 23:48:52 +0900

    test: Ace가 포함된 카드들의 숫자 합을 계산하는 테스트 케이스 추가

--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -18,4 +18,22 @@ class CardsTest {
 
         assertThat(cards.sum()).isEqualTo(20)
     }
+
+    @Test
+    fun `Ace는 1 또는 11로 계산된다`() {
+        val aceOfClubs = Card.createCard(Ranks.ACE, Suits.CLUBS)
+        val tenOfHearts = Card.createCard(Ranks.TEN, Suits.HEARTS)
+        val cards = Cards.empty()
+            .add(aceOfClubs)
+            .add(tenOfHearts)
+
+        assertThat(cards.sum()).isEqualTo(21)
+
+        val twoOfHearts = Card.createCard(Ranks.TWO, Suits.HEARTS)
+        val cards2 = Cards.empty()
+            .add(aceOfClubs)
+            .add(twoOfHearts)
+
+        assertThat(cards2.sum()).isEqualTo(13)
+    }
 }
