8fda32aa9feccd0842fca19c4c29d8cb856ef667
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2023-06-23 07:35:12 +0900

    feat: Bust에서 stay 시 예외 추가 및 테스트

---
+++ src/test/kotlin/blackjack/gamestate/BustTest.kt
@@ -0,0 +1,15 @@
+package blackjack.gamestate
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class BustTest : FunSpec({
+
+    context("stay") {
+        test("카드를 그만 받으려하는 경우 예외가 발생한다.") {
+            val exception = shouldThrowExactly<IllegalStateException> { Bust().stay() }
+            exception.message shouldBe "종료된 게임은 stay할 수 없다."
+        }
+    }
+})
