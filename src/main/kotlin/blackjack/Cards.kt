43e271b3760afccf024b34bae27b930282221c0d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2023-06-23 07:54:13 +0900

    feat: 카드 추가방식 변경

--- src/main/kotlin/blackjack/Cards.kt
+++ src/main/kotlin/blackjack/Cards.kt
@@ -2,36 +2,18 @@ package blackjack
 
 @JvmInline
 value class Cards(
-    private val values: MutableList<Card> = mutableListOf(),
+    val values: List<Card> = listOf()
 ) {
     init {
         require(values.size == values.toSet().size) { "중복된 카드가 저장될 수 없다." }
     }
 
-    fun deal(card: Card) {
-        check(values.size < CAN_DEAL_CARDS_SIZE) { "이미 2장이상 가지고 있어 deal할 수 없다." }
-        addCard(card)
-    }
-
-    fun hit(card: Card) {
-        check(values.size >= CAN_DEAL_CARDS_SIZE) { "카드가 deal이 완료되지 않아 hit할 수 없다." }
-        check(calculateScore() <= BUST_SCORE) { "카드가 bust되어 더이상 hit할 수 없다." }
-        addCard(card)
-    }
-
-    fun values() = values.toList()
-
-    private fun addCard(card: Card) {
+    fun addCard(card: Card): Cards {
         require(values.contains(card).not()) { "이미 존재하는 카드를 추가할 수 없다." }
-        values.add(card)
+        return Cards(values + card)
     }
-
-    private fun calculateScore() = values.sumOf { it.denomination.score.min() }
 
     companion object {
-        private const val CAN_DEAL_CARDS_SIZE = 2
-        private const val BUST_SCORE = 21
-
         fun of(vararg card: Card) = Cards(card.toMutableList())
     }
 }
--- src/test/kotlin/blackjack/CardsTest.kt
+++ src/test/kotlin/blackjack/CardsTest.kt
@@ -21,60 +21,28 @@ class CardsTest : FunSpec({
 
         test("카드 리스트를 생성한다.") {
             val actual = Cards(mutableListOf(SPADE_ACE, SPADE_TWO))
-            actual.values() shouldHaveSize 2
+            actual.values shouldHaveSize 2
         }
     }
 
-    context("deal") {
-        test("2장이상의 카드가 있는데 deal하는 경우 예외가 발생한다.") {
-            val cards = Cards.of(SPADE_ACE, SPADE_TWO)
-            val exception = shouldThrowExactly<IllegalStateException> { cards.deal(SPADE_KING) }
-            exception.message shouldBe "이미 2장이상 가지고 있어 deal할 수 없다."
-        }
-
-        test("중복된 카드를 deal하는 경우 예외가 발생한다.") {
+    context("add") {
+        test("중복된 카드를 더하는 경우 예외가 발생한다.") {
             val cards = Cards.of(SPADE_ACE)
-            val exception = shouldThrowExactly<IllegalArgumentException> { cards.deal(SPADE_ACE) }
+            val exception = shouldThrowExactly<IllegalArgumentException> { cards.addCard(SPADE_ACE) }
             exception.message shouldBe "이미 존재하는 카드를 추가할 수 없다."
         }
 
-        test("카드를 deal할 수 있다.") {
+        test("카드를 추가할 수 있다.") {
             val cards = Cards()
-            cards.deal(SPADE_TWO)
-            cards.values() shouldHaveSize 1
-        }
-    }
-
-    context("hit") {
-        test("2장미만의 카드가 있는데 hit하는 경우 예외가 발생한다") {
-            val cards = Cards.of(SPADE_ACE)
-            val exception = shouldThrowExactly<IllegalStateException> { cards.hit(SPADE_TWO) }
-            exception.message shouldBe "카드가 deal이 완료되지 않아 hit할 수 없다."
-        }
-
-        test("최소 계산된 점수가 21점 초과되는데 hit하는 경우 예외가 발생한다") {
-            val cards = Cards.of(SPADE_KING, SPADE_QUEEN, SPADE_JACK)
-            val exception = shouldThrowExactly<IllegalStateException> { cards.hit(SPADE_ACE) }
-            exception.message shouldBe "카드가 bust되어 더이상 hit할 수 없다."
-        }
-
-        test("중복된 카드를 hit하는 경우 예외가 발생한다.") {
-            val cards = Cards.of(SPADE_ACE, SPADE_TWO)
-            val exception = shouldThrowExactly<IllegalArgumentException> { cards.hit(SPADE_ACE) }
-            exception.message shouldBe "이미 존재하는 카드를 추가할 수 없다."
-        }
-
-        test("카드를 hit 할 수 있다.") {
-            val cards = Cards.of(SPADE_ACE, SPADE_KING, SPADE_JACK)
-            cards.hit(SPADE_QUEEN)
-            cards.values() shouldHaveSize 4
+            val actual = cards.addCard(SPADE_TWO)
+            actual.values shouldHaveSize 1
         }
     }
 
     context("of") {
         test("card를 받아 생성한다.") {
             val actual = Cards.of(SPADE_ACE, SPADE_KING)
-            actual.values() shouldHaveSize 2
+            actual.values shouldHaveSize 2
         }
     }
 })
