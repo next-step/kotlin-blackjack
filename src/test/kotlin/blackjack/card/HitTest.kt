a570dd1b1683562e932fdfeb65186a9a7daea554
nam-sangyun <nsun9505@naver.com>
2023-06-22 22:41:07 +0900

    feature: 플레이어는 Hit 상태에서 카드를 더 받았을 때 합이 21 이하라면 Hit 이라는 진행 상태가 된다

---
+++ src/test/kotlin/blackjack/card/HitTest.kt
@@ -0,0 +1,55 @@
+package blackjack.card
+
+import domain.card.BlackjackCard
+import domain.card.BlackjackCards
+import domain.card.CardNumber
+import domain.card.Suit
+import domain.state.Hit
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class HitTest {
+
+    @ParameterizedTest
+    @MethodSource("getHitStateTestData")
+    fun `플레이어는 Hit 상태에서 카드를 더 받았을 때 합이 21 이하라면 Hit 이라는 진행 상태가 된다`(
+        cards: BlackjackCards,
+        newCard: BlackjackCard,
+    ) {
+        val hit = Hit(cards)
+
+        val newState = hit.draw(newCard)
+
+        (newState is Hit) shouldBe true
+    }
+
+    companion object {
+        @JvmStatic
+        fun getHitStateTestData(): List<Arguments> = listOf(
+            Arguments.of(
+                BlackjackCards(listOf(BlackjackCard(suit = Suit.CLUB, number = CardNumber.KING))),
+                BlackjackCard(suit = Suit.SPADE, number = CardNumber.FIVE),
+            ),
+            Arguments.of(
+                BlackjackCards(
+                    listOf(
+                        BlackjackCard(suit = Suit.CLUB, number = CardNumber.ACE),
+                        BlackjackCard(suit = Suit.CLUB, number = CardNumber.TWO),
+                    ),
+                ),
+                BlackjackCard(suit = Suit.SPADE, number = CardNumber.FIVE),
+            ),
+            Arguments.of(
+                BlackjackCards(
+                    listOf(
+                        BlackjackCard(suit = Suit.CLUB, number = CardNumber.TEN),
+                        BlackjackCard(suit = Suit.HEART, number = CardNumber.TEN),
+                    ),
+                ),
+                BlackjackCard(suit = Suit.SPADE, number = CardNumber.ACE),
+            ),
+        )
+    }
+}
