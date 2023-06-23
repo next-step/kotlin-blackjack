bd02fc2845117ca6992f089825dfd084893ba044
genos-lee <genos.lee@kakaocorp.com>
2023-06-24 01:00:36 +0900

    feature : Card of생성자 추가

--- src/main/kotlin/next/step/blackjack/domain/Card.kt
+++ src/main/kotlin/next/step/blackjack/domain/Card.kt
@@ -1,5 +1,9 @@
 package next.step.blackjack.domain
 
 data class Card(val face: CardFace, val symbol: CardSymbol) {
-    fun desc() = "${face.desc}${symbol.desc}"
+    fun desc(): String = "${face.desc}${symbol.desc}"
+
+    companion object {
+        fun of(face: CardFace, symbol: CardSymbol): Card = Card(face, symbol)
+    }
 }
