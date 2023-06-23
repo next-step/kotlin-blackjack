979c8bef53e11ba6ffc618e080a1d44fbdda89dc
박세준 <sjpark@selab.co.kr>
2023-06-23 11:23:38 +0900

    refactor: 딜러는 더이상 덱의 상태에 관심을 두지 않음.

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,22 +1,14 @@
 package blackjack.domain
 
 class Dealer(private val deck: Deck = Deck()) {
-    fun startRound(players: Array<Player>) {
+    fun initializeRound(players: Array<Player>) {
         for (player in players) {
-            require(deck.canDrawFromDeck(DOUBLE_DRAW)) { EMPTY_DECK_ERROR }
             val firstRoundCards = arrayOf(deck.drawCard(), deck.drawCard())
             player.hit(*firstRoundCards)
         }
     }
 
     fun draw(): PokerCard {
-        require(deck.canDrawFromDeck(SINGLE_DRAW)) { EMPTY_DECK_ERROR }
         return deck.drawCard()
-    }
-
-    companion object {
-        private const val EMPTY_DECK_ERROR = "모든 덱이 소진되었습니다."
-        private const val SINGLE_DRAW = 1
-        private const val DOUBLE_DRAW = 2
     }
 }
