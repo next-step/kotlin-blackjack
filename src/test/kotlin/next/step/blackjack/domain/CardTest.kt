325833b98f6b33e5dbc68b2a2211fad56224fc53
genos-lee <genos.lee@kakaocorp.com>
2023-06-24 01:01:06 +0900

    test : Card of생성자 테스트 코드에 반영

--- src/test/kotlin/next/step/blackjack/domain/CardTest.kt
+++ src/test/kotlin/next/step/blackjack/domain/CardTest.kt
@@ -7,7 +7,7 @@ class CardTest : DescribeSpec({
     describe("card method") {
         context("desc를 호출하면") {
             it("card face와 card symbol을 붙여서 제공한다.") {
-                Card(CardFace.ACE, CardSymbol.CLUB).desc() shouldBe "A클로버"
+                Card.of(CardFace.ACE, CardSymbol.CLUB).desc() shouldBe "A클로버"
             }
         }
     }
