f656d0d079e70bd1791f60aa2360325b7fc4e2d4
박세준 <sjpark@selab.co.kr>
2023-06-23 11:24:07 +0900

    refactor: 무덤 대신 카드 52장을 생성하여 사용하도록 변경

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -1,37 +1,32 @@
 package blackjack.domain
 
-import blackjack.drawNewCard
+import blackjack.pokerCardOf
 import kotlin.random.Random
 
 class Deck {
-
-    private val graveyard = Array(DECK_SIZE) { false }
-    private val random = Random
-
-    fun drawCard(): PokerCard {
-        var drawIndex: Int
-        do {
-            drawIndex = random.nextInt(DECK_SIZE)
-        } while (graveyard[drawIndex])
-
-        graveyard[drawIndex] = true
-        val pokerSymbol = PokerSymbol.getSymbolByDrawNumber(drawIndex)
-        val cardValue = (drawIndex + REALIZATION_NUMBER) % SYMBOL_COUNT
-
-        return drawNewCard {
+    private val deck: MutableList<PokerCard> = List(DECK_SIZE) { index ->
+        val symbolValue = index / SYMBOL_COUNT
+        val cardValue = index % SYMBOL_COUNT + ADJUSTED_INDEX
+        val pokerSymbol = PokerSymbol.getSymbolByDrawNumber(symbolValue)
+        pokerCardOf {
             symbol(pokerSymbol)
             value(cardValue)
             rank(cardValue)
         }
-    }
+    }.toMutableList()
 
-    fun canDrawFromDeck(n: Int): Boolean {
-        return graveyard.filter { !it }.size >= n
+    private val random = Random.Default
+
+    fun drawCard(): PokerCard {
+        check(deck.isNotEmpty()) { EMPTY_DECK_ERROR }
+        val drawIndex = random.nextInt(deck.size)
+        return deck.removeAt(drawIndex)
     }
 
     companion object {
         private const val DECK_SIZE = 52
         private const val SYMBOL_COUNT = 13
-        private const val REALIZATION_NUMBER = 1
+        private const val ADJUSTED_INDEX = 1
+        private const val EMPTY_DECK_ERROR = "모든 덱이 소진되었습니다."
     }
 }
