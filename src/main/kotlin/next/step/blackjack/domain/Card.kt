6ea1a37192aacf3bbe53cbd0ada0514f436d4606
genos-lee <genos.lee@kakaocorp.com>
2023-06-24 02:49:16 +0900

    feature : Card에 최대 최소 포인트 제공하는 래퍼 메소드 추가하고 테스트 작성

--- src/main/kotlin/next/step/blackjack/domain/Card.kt
+++ src/main/kotlin/next/step/blackjack/domain/Card.kt
@@ -1,8 +1,11 @@
 package next.step.blackjack.domain
 
 data class Card(val face: CardFace, val symbol: CardSymbol) {
-    
+
     fun desc(): String = "${face.desc}${symbol.desc}"
+
+    fun maxPoint(): Int = face.maxPoint()
+    fun minPoint(): Int = face.minPoint()
 
     companion object {
         fun of(face: CardFace, symbol: CardSymbol): Card = Card(face, symbol)
