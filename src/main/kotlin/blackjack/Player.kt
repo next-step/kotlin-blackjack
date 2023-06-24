85d0be9ad4963e31a1cca5a74a942531e0304d1c
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-24 15:47:21 +0900

    feat: 새 플레이어 생성 추가 및 테스트

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -1,8 +1,15 @@
 package blackjack
 
+import blackjack.gamestate.GameState
+import blackjack.gamestate.InitialHand
+
 class Player(
     val name: String,
+    gameState: GameState = InitialHand(),
 ) {
+    var gameState: GameState = gameState
+        private set
+
     init {
         require(name.length <= NAME_LENGTH_LIMIT) { "플레이어 이름은 5자를 초과할 수 없다." }
     }
--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -1,8 +1,10 @@
 package blackjack
 
+import blackjack.gamestate.InitialHand
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
 
 class PlayerTest : FunSpec({
 
@@ -10,6 +12,12 @@ class PlayerTest : FunSpec({
         test("이름이 5자 초과 시 예외가 한다.") {
             val exception = shouldThrowExactly<IllegalArgumentException> { Player("123456") }
             exception.message shouldBe "플레이어 이름은 5자를 초과할 수 없다."
+        }
+
+        test("새 플레이어를 생성한다.") {
+            val actual = Player("최진영")
+            actual.name shouldBe "최진영"
+            actual.gameState.shouldBeTypeOf<InitialHand>()
         }
     }
 })
