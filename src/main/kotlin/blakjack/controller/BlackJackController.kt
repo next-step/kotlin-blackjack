23240585200b0aacde27107b87901a38fa39092d
hjjae2 <hjjae2@gmail.com>
2023-06-25 03:15:14 +0900

    [STEP2] main, controller, view 코드 작성

---
+++ src/main/kotlin/blakjack/controller/BlackJackController.kt
@@ -0,0 +1,33 @@
+package blakjack.controller
+
+import blakjack.domain.Dealer
+import blakjack.domain.Player
+import blakjack.view.InputView
+import blakjack.view.OutputView
+
+class BlackJackController {
+    private val dealer = Dealer()
+
+    fun start() {
+        val playerNames = InputView.readPlayerNames()
+        val players = playerNames.map { Player(name = it) }.also { initialDraw(it) }
+
+        OutputView.printInitialPlayerCards(players)
+        players.forEach(::hitOrStand)
+        OutputView.printPlayerCardsWithScore(players)
+    }
+
+    private fun initialDraw(players: List<Player>) {
+        players.forEach { player -> player.add(dealer.drawTwoCards()) }
+    }
+
+    private fun hitOrStand(player: Player) {
+        var isHit = InputView.readHitOrStand(player.name)
+        while (isHit && player.isUnderBlackjackScore) {
+            player.add(dealer.drawOneCard())
+            OutputView.printPlayerCards(player)
+
+            isHit = InputView.readHitOrStand(player.name)
+        }
+    }
+}
---
+++ src/main/kotlin/blakjack/view/OutputView.kt
@@ -0,0 +1,24 @@
+package blakjack.view
+
+import blakjack.domain.Player
+
+object OutputView {
+    fun printInitialPlayerCards(players: List<Player>) {
+        println("${players.joinToString { it.name }}에게 2장의 나누었습니다.")
+        players.forEach(this::printPlayerCards)
+    }
+
+    fun printPlayerCards(player: Player) {
+        println(getPrintPlayerCards(player))
+    }
+
+    fun printPlayerCardsWithScore(players: List<Player>) {
+        players.forEach {
+            println("${getPrintPlayerCards(it)} - 결과: ${it.score}")
+        }
+    }
+
+    private fun getPrintPlayerCards(player: Player): String {
+        return "${player.name}카드: ${player.cards.values.joinToString(",") { it.korean }}"
+    }
+}
