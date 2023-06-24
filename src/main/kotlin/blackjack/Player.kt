685238c6afd18aa2bb05067445e7240e211cc474
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-24 15:31:02 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -4,6 +4,10 @@ class Player(
     val name: String,
 ) {
     init {
-        require(name.length <= 5) { "플레이어 이름은 5자를 초과할 수 없다." }
+        require(name.length <= NAME_LENGTH_LIMIT) { "플레이어 이름은 5자를 초과할 수 없다." }
+    }
+
+    companion object {
+        private const val NAME_LENGTH_LIMIT = 5
     }
 }
