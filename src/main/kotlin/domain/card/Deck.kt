7e1693e5e8d89c6ce533bede01d3d4185049647d
nam-sangyun <nsun9505@naver.com>
2023-06-24 21:14:29 +0900

    refactor: 덱에 카드가 0개 일 때 처리

--- src/main/kotlin/domain/card/Deck.kt
+++ src/main/kotlin/domain/card/Deck.kt
@@ -1,10 +1,23 @@
 package domain.card
 
+import domain.card.util.DeckGenerator
+import domain.game.BlackjackGame
+
 fun MutableList<BlackjackCard>.pop(): BlackjackCard = this.removeAt(0)
 
 class Deck(private val cards: MutableList<BlackjackCard>) {
     val cardCount
         get() = cards.size
 
-    fun issueCard(): BlackjackCard = cards.pop()
+    fun issueCard(): BlackjackCard {
+        if (cards.isEmpty()) {
+            reloadCards()
+        }
+        return cards.pop()
+    }
+
+    private fun reloadCards() {
+        val newDeck = DeckGenerator.makeDeck(BlackjackGame.BLACKJACK_GAME_DECK_SIZE)
+        cards.addAll(newDeck.cards)
+    }
 }
