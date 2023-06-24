0a7c841a0e0173af2933ed29d6bdeeced802417a
neity16 <neity16@daum.net>
2023-06-25 02:51:14 +0900

    [step3] refactor: 메서드 기본 파라미터 추가로 불필요한 매직넘버 제거

--- src/main/kotlin/blackjack/domain/card/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/card/CardDeck.kt
@@ -2,14 +2,14 @@ package blackjack.domain.card
 
 class CardDeck {
     val cards: Cards = Cards(cardDeck.shuffled().toMutableList())
-    fun getRandomCards(count: Int): Cards {
+    fun getRandomCards(count: Int = 1): Cards {
         val randomCards = cards.getRandomCard(count)
         cards.removeAllCards(randomCards)
         return randomCards
     }
 
     fun getRandomCard(): Card {
-        return getRandomCards(1).getFirstCard()
+        return getRandomCards().getFirstCard()
     }
 
     companion object {
