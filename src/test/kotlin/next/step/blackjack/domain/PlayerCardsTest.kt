073d974791f907a1b751cd93b2cb52051da7920d
genos-lee <genos.lee@kakaocorp.com>
2023-06-24 21:23:55 +0900

    test : Ace가 여러개 들어간 경우, 가능한 조합으로 점수 계산하는 로직 테스트 코드 작성

--- src/test/kotlin/next/step/blackjack/domain/PlayerCardsTest.kt
+++ src/test/kotlin/next/step/blackjack/domain/PlayerCardsTest.kt
@@ -7,6 +7,7 @@ import next.step.blackjack.domain.card.Card
 import next.step.blackjack.domain.card.CardFace
 import next.step.blackjack.domain.card.CardSymbol
 import next.step.blackjack.domain.player.PlayerCards
+import next.step.blackjack.util.CombinationUtils
 
 class PlayerCardsTest : BehaviorSpec({
 
@@ -36,9 +37,11 @@ class PlayerCardsTest : BehaviorSpec({
             Then("burst하지 않음") {
                 playerCards.isBurst() shouldBe false
             }
-
             Then("블랙잭이 아님") {
                 playerCards.isBlackJack() shouldBe false
+            }
+            Then("20점") {
+                playerCards.point() shouldBe 20
             }
         }
         When("카드 총 개수가 2개이고, 총 점수가 21점이면") {
@@ -64,8 +67,11 @@ class PlayerCardsTest : BehaviorSpec({
             Then("블랙잭이 아님") {
                 playerCards.isBlackJack() shouldBe false
             }
+            Then("22점") {
+                playerCards.point() shouldBe 22
+            }
         }
-        When("Ace가 포함되어 최대점수가 블랙잭점수보다 더 작으면") {
+        When("Ace가 포함되어 최대점수가 21점보다 더 작으면") {
             val playerCards = PlayerCards.of(
                 listOf(
                     Card.of(CardFace.ACE, CardSymbol.CLUB),
@@ -82,16 +88,20 @@ class PlayerCardsTest : BehaviorSpec({
                 playerCards.isBlackJack() shouldBe false
             }
         }
-        When("Ace가 포함되어 최소점수가 블랙잭점수보다 더 작고 최대점수가 블랙잭점수보다 더 크면") {
+        When("Ace가 포함되어 최소점수가 21점보다 더 작고 최대점수가 21점보다 더 크면서 21점이 가능하지 않으면") {
             val playerCards = PlayerCards.of(
                 listOf(
+                    Card.of(CardFace.ACE, CardSymbol.CLUB),
                     Card.of(CardFace.ACE, CardSymbol.CLUB),
                     Card.of(CardFace.NINE, CardSymbol.HEART),
                     Card.of(CardFace.NINE, CardSymbol.CLUB),
                 )
             )
-            Then("최소점수를 점수로 제공함") {
-                playerCards.point() shouldBe 19
+            Then("가능한 점수는 20, 30, 40") {
+                CombinationUtils.possiblePoints(playerCards.cards) shouldBe setOf(20, 30, 40)
+            }
+            Then("21점보다 작으면서 가장 큰 점수를 점수로 제공함") {
+                playerCards.point() shouldBe 20
             }
             Then("burst 하지 않음") {
                 playerCards.isBurst() shouldBe false
@@ -100,7 +110,7 @@ class PlayerCardsTest : BehaviorSpec({
                 playerCards.isBlackJack() shouldBe false
             }
         }
-        When("Ace가 포함되어 최소점수가 블랙잭점수보다 더 크면") {
+        When("Ace가 포함되어 최소점수가 21점보다 더 크면") {
             val playerCards = PlayerCards.of(
                 listOf(
                     Card.of(CardFace.ACE, CardSymbol.CLUB),
@@ -119,7 +129,7 @@ class PlayerCardsTest : BehaviorSpec({
                 playerCards.isBlackJack() shouldBe false
             }
         }
-        When("Ace가 포함되어 최대점수가 종료점수와 같으면") {
+        When("Ace가 포함되어 최대점수가 21점과 같으면") {
             val playerCards = PlayerCards.of(
                 listOf(
                     Card.of(CardFace.ACE, CardSymbol.CLUB),
@@ -137,7 +147,7 @@ class PlayerCardsTest : BehaviorSpec({
                 playerCards.isFinished() shouldBe true
             }
         }
-        When("Ace가 포함되어 최소점수가 종료점수와 같으면") {
+        When("Ace가 포함되어 최소점수가 21점과 같으면") {
             val playerCards = PlayerCards.of(
                 listOf(
                     Card.of(CardFace.ACE, CardSymbol.CLUB),
@@ -157,6 +167,26 @@ class PlayerCardsTest : BehaviorSpec({
             }
             Then("카드 설명 제공") {
                 playerCards.descs() shouldBe listOf("A클로버", "9하트", "9클로버", "2하트")
+            }
+        }
+        When("Ace가 포함되어 21점이 가능하면") {
+            val playerCards = PlayerCards.of(
+                listOf(
+                    Card.of(CardFace.ACE, CardSymbol.CLUB),
+                    Card.of(CardFace.NINE, CardSymbol.HEART),
+                    Card.of(CardFace.NINE, CardSymbol.CLUB),
+                    Card.of(CardFace.ONE, CardSymbol.HEART),
+                    Card.of(CardFace.ACE, CardSymbol.CLUB),
+                )
+            )
+            Then("블랙잭 점수를 점수로 제공함") {
+                playerCards.point() shouldBe 21
+            }
+            Then("burst 안함") {
+                playerCards.isBurst() shouldBe false
+            }
+            Then("종료 상태임") {
+                playerCards.isFinished() shouldBe true
             }
         }
     }
