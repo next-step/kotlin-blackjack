a2405d5983866eeeadf4399f59860fe21e046188
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-24 15:08:59 +0900

    refactor: 초기핸드 확인메소드로 리팩터링

--- src/main/kotlin/blackjack/gamestate/InitialHand.kt
+++ src/main/kotlin/blackjack/gamestate/InitialHand.kt
@@ -8,20 +8,16 @@ class InitialHand(
     val cards: Cards = Cards(),
 ) : GameState {
     init {
-        require(cards.values.size < INITIAL_HAND_CARD_LIMIT_SIZE) { "초기 핸드는 ${INITIAL_HAND_CARD_LIMIT_SIZE}장 이상 가질 수 없다." }
+        require(cards.isInitialHand()) { "초기 핸드는 2장 이상 가질 수 없다." }
     }
 
     override fun draw(card: Card): GameState {
         val cards = this.cards.addCard(card)
-        if (cards.values.size == INITIAL_HAND_CARD_LIMIT_SIZE) {
+        if (cards.isInitialHand().not()) {
             return Hit(cards)
         }
         return InitialHand(cards)
     }
 
     override fun stay() = throw IllegalStateException("2장을 받기전에는 카드를 그만받을 수 없다.")
-
-    companion object {
-        private const val INITIAL_HAND_CARD_LIMIT_SIZE = 2
-    }
 }
