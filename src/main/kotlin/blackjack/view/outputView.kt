d3616264da9ec81306dd70111640c507abddc5ee
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-24 17:34:07 +0900

    feat: 첫드로우 출력뷰 추가

---
+++ src/main/kotlin/blackjack/view/outputView.kt
@@ -0,0 +1,18 @@
+package blackjack.view
+
+import blackjack.domain.DrawResult
+
+private const val PLAYER_NAME_DELIMITER = ", "
+
+fun printFirstDrawResult(drawResults: List<DrawResult>) {
+    println("${parsePlayerNames(drawResults)}에게 2장의 카드를 나누었습니다.")
+    drawResults.forEach { printPlayerDrawResult(it) }
+}
+
+private fun parsePlayerNames(drawResults: List<DrawResult>) =
+    drawResults.joinToString(PLAYER_NAME_DELIMITER) { it.playerName }
+
+private fun printPlayerDrawResult(drawResult: DrawResult) {
+    println("${drawResult.playerName}카드: ${drawResult.cards}}")
+}
+
