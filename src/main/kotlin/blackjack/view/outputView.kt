8af837b8be03c4d5584cd9e21aac5ed7572d6956
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-25 01:00:58 +0900

    feat: 출력 뷰 수정

--- src/main/kotlin/blackjack/view/outputView.kt
+++ src/main/kotlin/blackjack/view/outputView.kt
@@ -1,30 +1,27 @@
 package blackjack.view
 
-import blackjack.domain.Hands
 import blackjack.domain.GameResult
+import blackjack.domain.Hands
+import blackjack.domain.card.Card
 
 private const val PLAYER_NAME_DELIMITER = ", "
+private const val CARD_DELIMITER = ", "
 
 fun printFirstDrawResult(hands: List<Hands>) {
     println("${parsePlayerNames(hands)}에게 2장의 카드를 나누었습니다.")
     hands.forEach { printPlayerDrawResult(it) }
 }
 
-fun printCurrentDrawResult(hands: Hands) {
-    println("${hands.playerName}카드: ${hands.cards}")
-}
+fun printCurrentDrawResult(hands: Hands) = println("${hands.playerName}카드: ${parseCardsResult(hands.cards)}")
 
-fun printGameResults(gameResults: List<GameResult>) {
-    gameResults.forEach { printGameResult(it) }
-}
+fun printGameResults(gameResults: List<GameResult>) = gameResults.forEach { printGameResult(it) }
 
-private fun parsePlayerNames(hands: List<Hands>) =
-    hands.joinToString(PLAYER_NAME_DELIMITER) { it.playerName }
+private fun parsePlayerNames(hands: List<Hands>) = hands.joinToString(PLAYER_NAME_DELIMITER) { it.playerName }
 
-private fun printPlayerDrawResult(hands: Hands) {
-    println("${hands.playerName}카드: ${hands.cards}}")
-}
+private fun printPlayerDrawResult(hands: Hands) = println("${hands.playerName}카드: ${parseCardsResult(hands.cards)}}")
 
 private fun printGameResult(gameResult: GameResult) {
-    println("${gameResult.playerName}카드: ${gameResult.cards} - 결과: ${gameResult.score}")
+    println("${gameResult.playerName}카드: ${parseCardsResult(gameResult.cards)}} - 결과: ${gameResult.score}")
 }
+
+private fun parseCardsResult(cards: List<Card>) = cards.joinToString(CARD_DELIMITER) { parseCardView(it) }
