03fb1d8c2c29773b83382b516086b40fdf8b6f8a
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-06-24 15:19:32 +0900

    feat: Bust 생성시 제한기능 추가 및 테스트

--- src/main/kotlin/blackjack/gamestate/Bust.kt
+++ src/main/kotlin/blackjack/gamestate/Bust.kt
@@ -6,6 +6,11 @@ import blackjack.Cards
 class Bust(
     val cards: Cards,
 ) : GameState {
+    init {
+        require(cards.isInitialHand().not()) { "2장 미만의 카드로 생성될 수 없다." }
+        require(cards.isBust()) { "버스트 아닌 카드로 생성될 수 없다." }
+    }
+
     override fun draw(card: Card) = throw IllegalStateException("종료된 게임은 draw할 수 없다.")
 
     override fun stay() = throw IllegalStateException("종료된 게임은 stay할 수 없다.")
--- src/test/kotlin/blackjack/gamestate/BustTest.kt
+++ src/test/kotlin/blackjack/gamestate/BustTest.kt
@@ -11,6 +11,19 @@ import io.kotest.matchers.shouldBe
 
 class BustTest : FunSpec({
 
+    context("init") {
+        test("생성시 카드가 initialhand면 예외가 발생한다.") {
+            val exception = shouldThrowExactly<IllegalArgumentException> { Bust(Cards()) }
+            exception.message shouldBe "2장 미만의 카드로 생성될 수 없다."
+        }
+
+        test("생성 시 카드가 bust면 예외가 발생한다.") {
+            val exception =
+                shouldThrowExactly<IllegalArgumentException> { Bust(Cards.of(SPADE_KING, SPADE_JACK)) }
+            exception.message shouldBe "버스트 아닌 카드로 생성될 수 없다."
+        }
+    }
+
     context("draw") {
         test("카드를 드로우하려는 경우 예외가 발생한다.") {
             val exception = shouldThrowExactly<IllegalStateException> { Bust(BUST_CARDS).draw(SPADE_ACE) }
