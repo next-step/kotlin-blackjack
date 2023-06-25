9a09424585834fd17c5b249b02c5a9afafd374ef
sjparkk <sjparkk0820@naver.com>
2023-06-25 17:33:33 +0900

    test : Cards 클래스에 대한 테스트 추가

---
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -0,0 +1,64 @@
+package blackjack.domain
+
+import blackjack.enums.Rank
+import blackjack.enums.Symbol
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class CardsTest {
+
+    private lateinit var cards: Cards
+    private lateinit var aceCardValueElevenCards: Cards
+    private lateinit var aceCardValueOneCards: Cards
+
+    @BeforeEach
+    fun setup() {
+        cards = Cards(
+            listOf(
+                Card(rank = Rank.JACK, symbol = Symbol.SPADES),
+                Card(rank = Rank.FIVE, symbol = Symbol.HEARTS)
+            )
+        )
+
+        aceCardValueElevenCards = Cards(
+            listOf(
+                Card(rank = Rank.ACE, symbol = Symbol.SPADES),
+                Card(rank = Rank.FIVE, symbol = Symbol.HEARTS),
+                Card(rank = Rank.FIVE, symbol = Symbol.CLUBS),
+            )
+        )
+
+        aceCardValueOneCards = Cards(
+            listOf(
+                Card(rank = Rank.ACE, symbol = Symbol.SPADES),
+                Card(rank = Rank.FIVE, symbol = Symbol.HEARTS),
+                Card(rank = Rank.KING, symbol = Symbol.HEARTS)
+            )
+        )
+    }
+
+    @Test
+    fun `카드 뭉치에 대한 정보를 문자열 값으로 추출한다`() {
+        val actual = cards.extractCardsInfoAsString()
+        actual shouldBe "10스페이드, 5하트"
+    }
+
+    @Test
+    fun `카드 뭉치에 대한 숫자 합계를 구한다`() {
+        val actual = cards.calculateCardsTotalValue()
+        actual shouldBe 15
+    }
+
+    @Test
+    fun `에이스를 제외한 모든 숫자의 합이 10 미만 일 때 에이스는 11로 계산된다`() {
+        val actual = aceCardValueElevenCards.calculateCardsTotalValue()
+        actual shouldBe 21
+    }
+
+    @Test
+    fun `에이스를 제외한 모든 숫자의 합이 10 이상 일 때 에이스는 1로 계산된다`() {
+        val actual = aceCardValueOneCards.calculateCardsTotalValue()
+        actual shouldBe 16
+    }
+}
