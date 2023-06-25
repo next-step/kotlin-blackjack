7e0c14dd8eb48f58b86dd2771fa0aac0f82ed3d9
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-25 11:04:21 +0900

    refactor: 덱을 한번만 섞고 첫번째 값들을 뽑도록 변경

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -12,16 +12,14 @@ class Deck {
             value(cardValue)
             rank(cardValue)
         }
-    }.toMutableList()
+    }.shuffled().toMutableList()
 
     fun drawCard(): PokerCard {
         check(deck.isNotEmpty()) { EMPTY_DECK_ERROR }
-        deck.shuffle()
-        return deck.removeAt(FIRST_INDEX)
+        return deck.removeFirst()
     }
 
     companion object {
-        private const val FIRST_INDEX = 0
         private const val ADJUSTED_INDEX = 1
         private const val SYMBOL_COUNT = 13
         private const val DECK_SIZE = 52
