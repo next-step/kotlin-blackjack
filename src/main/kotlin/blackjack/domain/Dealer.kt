6dccc4e78a2016e2b92405e79a676b50cffa1b24
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-06-25 11:04:45 +0900

    refactor: 딜러만의 메소드를 제거하고 플레이어의 메소드를 사용하도록변경

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,20 +1,20 @@
 package blackjack.domain
 
 import blackjack.DeckManager
-import blackjack.GameResultChecker
 import blackjack.view.PlayerStatus
 
 class Dealer : Player(name = "딜러") {
 
     fun initializeRound(deckManager: DeckManager, players: Array<Player>) {
         this.hit(*deckManager.drawTwoCards())
+        if (this.optimalValue() >= STAND_THRESHOLD) this.stand()
         for (player in players) {
             player.hit(*deckManager.drawTwoCards())
         }
     }
 
-    fun getAllParticipantsStatus(players: Array<Player>): List<PlayerStatus> {
-        val playerStatus = mutableListOf(PlayerStatus.of(this))
+    fun getParticipantInitialStatus(players: Array<Player>): List<PlayerStatus> {
+        val playerStatus = mutableListOf(PlayerStatus.upCard(this))
         for (player in players) {
             playerStatus.add(PlayerStatus.of(player))
         }
@@ -22,27 +22,13 @@ class Dealer : Player(name = "딜러") {
         return playerStatus
     }
 
-    fun drawAdditionalCard(deckManager: DeckManager, dealerAddNotice: () -> Unit) {
-        if (this.optimalValue() <= DEALER_MIN_VALUE_TO_DRAW) {
-            this.hit(deckManager.draw())
-            dealerAddNotice.invoke()
-        }
-    }
-
-    override fun gameResult(): String {
-        return scoreBoard().resultForDealer()
-    }
-
-    fun determineWinner(players: Array<Player>, checker: GameResultChecker) {
-        players.forEach {
-            val playerValue = it.optimalValue()
-            val dealerValue = this.optimalValue()
-            val winner = checker.determineWinner(playerValue, dealerValue)
-            checker.updateScores(winner, it)
-        }
+    fun getParticipantStatus(players: Array<Player>): Pair<PlayerStatus, List<PlayerStatus>> {
+        val dealerStatus = PlayerStatus.of(this)
+        val playerStatusList = players.map(PlayerStatus::of)
+        return Pair(dealerStatus, playerStatusList)
     }
 
     companion object {
-        private const val DEALER_MIN_VALUE_TO_DRAW = 16
+        private const val STAND_THRESHOLD = 17
     }
 }
