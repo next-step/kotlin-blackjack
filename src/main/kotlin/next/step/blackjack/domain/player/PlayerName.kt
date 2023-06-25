29ba2b7a492ab7edee26abf224092c15a5aa8a7d
genos-lee <genos.lee@kakaocorp.com>
2023-06-25 14:59:32 +0900

    refactor : 플레이어 이름을 메소드로 제공하는 로직 제거

--- src/main/kotlin/next/step/blackjack/domain/player/PlayerName.kt
+++ src/main/kotlin/next/step/blackjack/domain/player/PlayerName.kt
@@ -1,12 +1,10 @@
 package next.step.blackjack.domain.player
 
 @JvmInline
-value class PlayerName(private val name: String) {
+value class PlayerName(val name: String) {
     init {
         require(name.isNotBlank()) { "플레이러 이름은 공백이 아닌 글자여야 합니다." }
     }
-
-    fun name(): String = name
 
     companion object {
         fun of(name: String) = PlayerName(name)
--- src/test/kotlin/next/step/blackjack/domain/player/PlayerNameTest.kt
+++ src/test/kotlin/next/step/blackjack/domain/player/PlayerNameTest.kt
@@ -2,7 +2,6 @@ package next.step.blackjack.domain.player
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
-import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.assertThrows
 
 class PlayerNameTest : DescribeSpec({
@@ -16,14 +15,6 @@ class PlayerNameTest : DescribeSpec({
                 "    "
             ) { name ->
                 assertThrows<IllegalArgumentException> { PlayerName.of(name) }
-            }
-        }
-    }
-
-    describe("PlayerName method") {
-        context("name() 호출하면") {
-            it("문자열로 이름 제공") {
-                PlayerName.of("test").name() shouldBe "test"
             }
         }
     }
