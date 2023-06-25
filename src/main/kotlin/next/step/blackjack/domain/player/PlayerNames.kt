04d1619aa849843fec3974a0aa1d70f5c0892456
genos-lee <genos.lee@kakaocorp.com>
2023-06-25 15:17:09 +0900

    refactor : PlayerNames에 delegation 사용

--- src/main/kotlin/next/step/blackjack/domain/player/PlayerNames.kt
+++ src/main/kotlin/next/step/blackjack/domain/player/PlayerNames.kt
@@ -1,7 +1,7 @@
 package next.step.blackjack.domain.player
 
 @JvmInline
-value class PlayerNames(val names: Set<PlayerName>) {
+value class PlayerNames(private val names: Set<PlayerName>) : Set<PlayerName> by names {
     init {
         require(names.isNotEmpty()) { "반드시 한 명 이상의 플레이어 이름이 있어야 합니다." }
     }
