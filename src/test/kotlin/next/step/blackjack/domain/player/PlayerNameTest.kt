34720ac65e0d4fb5548d6a9a8fcdf333939534c0
genos-lee <genos.lee@kakaocorp.com>
2023-06-25 14:30:49 +0900

    refactor : PlayerName 제공메소드 추가하고 value class로 수정 후 테스트코드 추가

--- src/test/kotlin/next/step/blackjack/domain/player/PlayerNameTest.kt
+++ src/test/kotlin/next/step/blackjack/domain/player/PlayerNameTest.kt
@@ -2,6 +2,7 @@ package next.step.blackjack.domain.player
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.assertThrows
 
 class PlayerNameTest : DescribeSpec({
@@ -15,6 +16,14 @@ class PlayerNameTest : DescribeSpec({
                 "    "
             ) { name ->
                 assertThrows<IllegalArgumentException> { PlayerName.of(name) }
+            }
+        }
+    }
+
+    describe("PlayerName method") {
+        context("name() 호출하면") {
+            it("문자열로 이름 제공") {
+                PlayerName.of("test").name() shouldBe "test"
             }
         }
     }
