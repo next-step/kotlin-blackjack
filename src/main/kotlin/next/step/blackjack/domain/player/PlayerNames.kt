13bf637ee61453240f742bcc1550b961a53ca46b
genos-lee <genos.lee@kakaocorp.com>
2023-06-25 15:00:07 +0900

    refactor : 플레이어 이름을 String set으로 제공하는 로직 제거하고, validation 추가 및 테스트코드 작성

--- src/main/kotlin/next/step/blackjack/domain/player/PlayerNames.kt
+++ src/main/kotlin/next/step/blackjack/domain/player/PlayerNames.kt
@@ -1,9 +1,10 @@
 package next.step.blackjack.domain.player
 
 @JvmInline
-value class PlayerNames(private val names: Set<PlayerName>) {
-
-    fun names(): Set<String> = names.map { it.name() }.toSet()
+value class PlayerNames(val names: Set<PlayerName>) {
+    init {
+        require(names.isNotEmpty()) { "반드시 한 명 이상의 플레이어 이름이 있어야 합니다." }
+    }
 
     companion object {
         fun of(names: Set<PlayerName>): PlayerNames = PlayerNames(names)
--- src/test/kotlin/next/step/blackjack/domain/player/PlayerNamesTest.kt
+++ src/test/kotlin/next/step/blackjack/domain/player/PlayerNamesTest.kt
@@ -1,14 +1,14 @@
 package next.step.blackjack.domain.player
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertThrows
 
 class PlayerNamesTest : DescribeSpec({
-    
-    describe("PlayerNames method") {
-        context("names()") {
-            it("문자열 셋으로 이름 제공") {
-                PlayerNames.of(setOf(PlayerName("name1"), PlayerName("name2"))).names() shouldBe setOf("name1", "name2")
+
+    describe("PlayerNames 생성") {
+        context("빈 셋으로 생성하면") {
+            it("예외 발생") {
+                assertThrows<IllegalArgumentException> { PlayerNames.of(emptySet()) }
             }
         }
     }
