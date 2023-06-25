081b6be5e09098a076dd5cf2e34bc3584dc0744b
konempty <jameswebb@naver.com>
2023-06-25 19:38:32 +0900

    refactor: 불필요한 override메소드 삭제

--- src/main/kotlin/blackjack/domain/Users.kt
+++ src/main/kotlin/blackjack/domain/Users.kt
@@ -7,10 +7,6 @@ data class Users(private val userList: Set<User>) : Iterable<User> by userList {
         require(userList.isNotEmpty()) { EMPTY_USER_LIST_ERROR_MESSAGE }
     }
 
-    override fun iterator(): Iterator<User> {
-        return userList.iterator()
-    }
-
     companion object {
         private const val EMPTY_USER_LIST_ERROR_MESSAGE = "1명 이상의 유저가 필요합니다"
     }
