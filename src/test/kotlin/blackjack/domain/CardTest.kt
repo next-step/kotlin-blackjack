9bd71e716ff9ee92d99e5d4730a8e68c7e1bf5c2
sjparkk <sjparkk0820@naver.com>
2023-06-25 11:34:24 +0900

    test : 카드의 숫자는 Ace (1, 11), 2, 3, ... 10, King(10), Queen(10), Jack(10) 총 13 종류를 가진다

--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -1,5 +1,6 @@
 package blackjack.domain
 
+import blackjack.enums.Rank
 import blackjack.enums.Symbol
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
@@ -14,4 +15,12 @@ class CardTest {
         symbolList.size shouldBe 4
         symbolList shouldContainAll listOf(Symbol.HEARTS, Symbol.DIAMONDS, Symbol.CLUBS, Symbol.SPADES)
     }
+
+    @Test
+    fun `카드의 숫자는 Ace (1 or 11), 2 ~ 10, King(10), Queen(10), Jack(10) 총 13 종류를 가진다`() {
+        val rankList = Rank.values()
+        rankList.size shouldBe 13
+    }
+
+
 }
