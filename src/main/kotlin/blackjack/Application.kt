aa17e7f34ef0b483329f4194ebc961aaf71b3487
oliveunji <eunjikim2u@gmail.com>
2023-06-26 08:42:34 +0900

    feat: blackjack 로직 구현

--- src/main/kotlin/blackjack/Application.kt
+++ src/main/kotlin/blackjack/Application.kt
@@ -1,7 +1,23 @@
 package blackjack
+
+import blackjack.domain.Cards
+import blackjack.domain.Player
 import blackjack.view.InputView
 
 fun main() {
-    InputView.getPlayersName()
-    InputView.getDecisionForMoreCards("Test")
+    val names = InputView.getPlayersName().split(",")
+    val cards = Cards()
+
+    var players = mutableListOf<Player>()
+    for (name in names) {
+        players.add(Player(name))
+    }
+
+    players.forEach {
+        it.addCard(cards.takeCard())
+        it.addCard(cards.takeCard())
+        println(it.getCards())
+    }
+
+    // 플레이어가 카드를 그만 받는다고 하거나 블랙잭일때 까지
 }
