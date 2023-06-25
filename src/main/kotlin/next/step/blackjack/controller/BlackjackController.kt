84e83a48743f32d22985fed353581dcab3c858ef
genos-lee <genos.lee@kakaocorp.com>
2023-06-25 22:00:54 +0900

    feature : main, OutputView에 딜러 추가

--- src/main/kotlin/next/step/blackjack/controller/BlackjackController.kt
+++ src/main/kotlin/next/step/blackjack/controller/BlackjackController.kt
@@ -1,6 +1,8 @@
 package next.step.blackjack.controller
 
+import next.step.blackjack.domain.card.Cards
 import next.step.blackjack.domain.card.GameCards
+import next.step.blackjack.domain.dealer.Dealer
 import next.step.blackjack.domain.game.GameRule
 import next.step.blackjack.domain.player.Players
 import next.step.racing.view.InputView
@@ -9,10 +11,13 @@ import next.step.racing.view.OutputView
 fun main() {
     runCatching {
         val gameCards = GameCards.shuffled()
+        val dealer = Dealer.of(Cards.of(gameCards.pop(GameRule.INIT_CARD_CNT)))
         val players = Players.of(InputView.readPlayerNames()) { gameCards.pop(GameRule.INIT_CARD_CNT) }
-        OutputView.showStart(players, GameRule.INIT_CARD_CNT)
+        OutputView.showStart(dealer, players, GameRule.INIT_CARD_CNT)
         players.turn(InputView::readTurn, { gameCards.pop() }) { player -> OutputView.showPlayerCards(player) }
-        OutputView.showResult(players)
+        dealer.turn({ gameCards.pop() }) { OutputView.showDealerHit(it) }
+        OutputView.showCardsWithPoints(dealer, players)
+        OutputView.showGameResult(dealer.name(), dealer.fight(players))
     }.onFailure { e ->
         OutputView.showError(e.message)
         main()
