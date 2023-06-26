e1519f8553539638e36f815f06d7ee6f2ce45443
neity16 <neity16@daum.net>
2023-06-27 02:14:53 +0900

    [step4] refactor: 딜러의 카드는 한장만 출력되도록 수정

--- src/main/kotlin/blackjack/view/output/GamePlayersOutputView.kt
+++ src/main/kotlin/blackjack/view/output/GamePlayersOutputView.kt
@@ -1,6 +1,7 @@
 package blackjack.view.output
 
-import blackjack.domain.player.GamePlayer
+import blackjack.domain.card.Card
+import blackjack.domain.player.Dealer
 import blackjack.domain.player.GamePlayers
 import blackjack.domain.player.Player
 
@@ -8,24 +9,34 @@ class GamePlayersOutputView(gamePlayers: GamePlayers, private val isResult: Bool
     init {
         with(gamePlayers) {
             val hasDealer = dealer.cards.getValue().isNotEmpty()
-            val gamePlayers = if(hasDealer) listOf(dealer).plus(players) else players
-            gamePlayers.forEach { gamePlayer -> renderMessage(gamePlayer) }
+            if (hasDealer) renderDealerMessage(dealer)
+            players.forEach { gamePlayer -> renderPlayerMessage(gamePlayer) }
         }
     }
 
-    private fun renderMessage(gamePlayer: GamePlayer) {
-        with(gamePlayer) {
-            val resultScoreMessage = if (isResult) " - 결과: ${gamePlayer.cards.getOptimizedScore()}" else ""
-            val message = "${getName(this)} 카드: "
+    private fun renderDealerMessage(dealer: Dealer) {
+        val resultScoreMessage = getScoreMessage(dealer.cards.getOptimizedScore())
+        val cardMessage = getCardStr(dealer.cards.getFirstCard())
+        val message = "딜러: $cardMessage"
+        println(message + resultScoreMessage)
+    }
+
+    private fun renderPlayerMessage(player: Player) {
+        with(player) {
+            val resultScoreMessage = getScoreMessage(player.cards.getOptimizedScore())
+            val message = "${this.name.value} 카드: "
             val cardsStr =
-                this.cards.getValue().joinToString(SEPARATOR) { card -> card.denom.symbol + card.type.korName }
+                this.cards.getValue().joinToString(SEPARATOR) { card -> getCardStr(card) }
             println(message + cardsStr + resultScoreMessage)
         }
     }
 
-    private fun getName(gamePlayer: GamePlayer): String {
-        if (gamePlayer is Player) return gamePlayer.name.value
-        return "딜러"
+    private fun getScoreMessage(score: Int): String {
+        return if (isResult) " - 결과: $score" else ""
+    }
+
+    private fun getCardStr(card: Card): String {
+        return card.denom.symbol + card.type.korName
     }
 
     companion object {
