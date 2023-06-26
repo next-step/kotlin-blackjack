92278f3e641f0d0e9cb1508168582a97a96c6be2
goofy-919 <goofy.919@kakaocorp.com>
2023-06-26 16:01:44 +0900

    feat: OutputView 딜러 기능 구현

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,11 +1,13 @@
 package blackjack.view
 
+import blackjack.domain.player.Dealer
 import blackjack.domain.player.Participants
+import blackjack.domain.player.Player
 import blackjack.domain.player.PlayerImpl
 
 object OutputView {
     fun printGameStart(names: List<String>, initialDraw: Int) {
-        println("\n${names.joinToString(", ")}에게 ${initialDraw}장을 나누었습니다.")
+        println("\n딜러와 ${names.joinToString(", ")}에게 ${initialDraw}장을 나누었습니다.")
     }
 
     fun printPlayerCard(players: Participants) {
@@ -13,6 +15,14 @@ object OutputView {
             printPlayerCard(it)
         }
         println()
+    }
+
+    fun printDealerCard(dealer: Dealer) {
+        println("${dealer.name}카드: ${dealer.cardHold.cards[0].rank.mark}${dealer.cardHold.cards[0].shape.mark}")
+    }
+
+    fun printDealerGetAdditionalCard() {
+        println("딜러는 16이하라 한장의 카드를 더 받았습니다.\n")
     }
 
     fun printPlayerCard(player: PlayerImpl) {
@@ -26,7 +36,7 @@ object OutputView {
         }
     }
 
-    private fun printPlayerResult(player: PlayerImpl) {
+    private fun printPlayerResult(player: Player) {
         println("${player.name}카드: ${player.cardHold.cards.joinToString(", ") { it.rank.mark + it.shape.mark } } - 결과: ${player.getPoints()}")
     }
 }
