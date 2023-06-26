913add52eb52edb3740ad1bc0e76b8c0f8a10474
sjparkk <sjparkk0820@naver.com>
2023-06-26 20:46:35 +0900

    test : 딜러 관련 테스트 케이스 추가

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -1,12 +1,33 @@
 package blackjack.domain
 
+import blackjack.domain.enums.Condition
+
 class Dealer(
     val name: String = "딜러",
     val cards: Cards,
-    val deck: Deck
+    val deck: Deck,
+    private var condition: Condition = Condition.STAY
 ) {
+
+    init {
+        var total = 0
+        cards.cards.forEach {
+            total += it.rank.value
+        }
+        if( STANDARD_CARD_SCORE >= total) {
+            condition = Condition.PLAY
+        }
+    }
 
     fun draw(count: Int): Cards {
         return deck.drawCard(count)
+    }
+
+    fun currentCondition(): Condition {
+        return this.condition
+    }
+
+    companion object {
+        private const val STANDARD_CARD_SCORE = 16
     }
 }
--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -1,5 +1,8 @@
 package blackjack.domain
 
+import blackjack.domain.enums.Condition
+import blackjack.enums.Rank
+import blackjack.enums.Symbol
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -14,6 +17,22 @@ class DealerTest {
         dealer.cards.cards.size shouldBe BASIC_CARD_COUNT
         dealer.deck.cardCount shouldBe TOTAL_CARD_COUNT - BASIC_CARD_COUNT
     }
+
+    @Test
+    fun `딜러도 마찬가지로 카드를 받기 위한 응답 여부의 조건값을 가진다`() {
+        val deck = Deck()
+        val dealer = Dealer(name = "딜러", cards = deck.drawCard(2), deck = deck)
+        dealer.currentCondition() shouldBe Condition.STAY
+    }
+
+    @Test
+    fun `딜러는 처음 받은 2장의 카드의 합이 16 이하면 1장의 카드를 추가로 받기 위한 상태로 변한다`() {
+        val deck = Deck()
+        val cards = Cards(listOf(Card(rank = Rank.TWO, symbol = Symbol.SPADES), Card(rank = Rank.KING, symbol = Symbol.SPADES)))
+        val dealer = Dealer(name = "딜러", cards = cards, deck = deck)
+        dealer.currentCondition() shouldBe Condition.PLAY
+    }
+
 
     companion object {
         const val TOTAL_CARD_COUNT = 52
