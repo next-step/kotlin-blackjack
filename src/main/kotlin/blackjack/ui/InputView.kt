b99ff916a6cbce315a0bee6ffba8bf245d7a29eb
jonghyunhub <chuwhdgus@gmail.com>
2023-06-26 21:44:08 +0900

    refact : 카드 뽑기 질문시 'y' 도 인식하도록 수정

--- src/main/kotlin/blackjack/ui/InputView.kt
+++ src/main/kotlin/blackjack/ui/InputView.kt
@@ -1,7 +1,5 @@
 package blackjack.ui
 
-import blackjack.domain.Player
-
 class InputView {
     fun getPlayerNames(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
@@ -15,7 +13,7 @@ class InputView {
 
     fun askPlayersWantToDrawCard(): Boolean {
         val playerAnswer = readln()
-        if (playerAnswer == "Y") {
+        if (playerAnswer == "Y" || playerAnswer == "y") {
             return true
         }
         return false
