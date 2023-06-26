ae23aca30d6db95fba1328fadfc1a85c908b2e4b
oliveunji <eunjikim2u@gmail.com>
2023-06-26 17:40:00 +0900

    feat: 각 메서드에 대한 테스트 함수 구현

---
+++ src/test/kotlin/blackjack/domain/CardsTest.kt
@@ -0,0 +1,19 @@
+package blackjack.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class CardsTest {
+    @Test
+    fun initiateCardListTest() {
+        val cards = Cards()
+        assertThat(cards.getCards().count()).isEqualTo(52)
+    }
+
+    @Test
+    fun takeCardTest(){
+        val cards = Cards()
+        cards.takeCard(2)
+        assertThat(cards.getCards().count()).isEqualTo(50)
+    }
+}
