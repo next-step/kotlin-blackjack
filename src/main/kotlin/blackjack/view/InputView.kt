d8532fb05df7f0a4123b13dcf95403b410da372d
waveofmymind <sjun0913@gmail.com>
2023-06-27 10:58:19 +0900

    Refactor: 입력은 출력을 하지 않도록 print() 제거

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -6,7 +6,6 @@ object InputView {
     fun readPlayers(): List<String> {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
         val players = readLine()?.split(",")?.map { it.trim() } ?: throw IllegalArgumentException("입력이 잘못되었습니다.")
-        players.map { println(it) }
         return players
     }
 
---
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -0,0 +1,22 @@
+package blackjack.view
+
+import blackjack.player.Player
+
+object ResultView {
+
+    fun printPlayerList(players: List<Player>) {
+        println("딜러와 ${players.joinToString { it.name }}에게 2장의 카드를 나누었습니다.")
+        players.map { printPlayerStatus(it) }
+    }
+
+    fun printPlayerStatus(player: Player) {
+        println("${player.name}카드: ${player.displayHand()}")
+    }
+
+    fun printFinalPlayerStatus(players: List<Player>) {
+        println()
+        players.forEach { player ->
+            println("${player.name}카드: ${player.displayHand()} - 결과: ${player.getTotalValue()}")
+        }
+    }
+}
