1460d19bb6d2ae244ae6924a58f5e15439af6113
genos-lee <genos.lee@kakaocorp.com>
2023-06-28 01:18:38 +0900

    feature : 게임 베팅 결과 추상화하고 테스트코드 작성

---
+++ src/test/kotlin/next/step/blackjack/domain/betting/BettingResultsTest.kt
@@ -0,0 +1,16 @@
+package next.step.blackjack.domain.betting
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class BettingResultsTest : DescribeSpec({
+
+    describe("BettingResults") {
+        context("플레이어 결과를 넘기면") {
+            it("딜러 결과까지 계산") {
+                BettingResults.from(mapOf("dj" to -20000, "dj2" to 10000)).dealerResult shouldBe 10000
+            }
+        }
+    }
+
+})
