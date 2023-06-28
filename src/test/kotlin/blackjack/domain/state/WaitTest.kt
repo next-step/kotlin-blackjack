3031ca198bf72bace415a923eb6364ad8f90c75f
chad.yoon <chad.yoon@kakaocorp.com>
2023-06-28 19:59:26 +0900

    test: Wait 테스트 코드 작성

---
+++ src/test/kotlin/blackjack/domain/state/WaitTest.kt
@@ -0,0 +1,11 @@
+package blackjack.domain.state
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class WaitTest : StringSpec({
+
+    "카드 목록이 비어있다" {
+        Wait().cards.isEmpty() shouldBe true
+    }
+})
