c6f29e10f83db21b66172bd60c7087e159c3100d
박세준 <sjpark@selab.co.kr>
2023-06-28 16:53:55 +0900

    refactor: 무승부일 경우들을 따로 분리

--- src/main/kotlin/blackjack/common/service/BlackJackDetermine.kt
+++ src/main/kotlin/blackjack/common/service/BlackJackDetermine.kt
@@ -10,10 +10,10 @@ object BlackJackDetermine {
 
     fun determineWinType(player: BetPlayer, dealerValue: Int): WinType {
         val playerValue = player.optimalValue()
-        if (player.isInitialBlackjack()) return WinType.PLAYER_BLACK_JACK
 
         return when {
-            dealerValue > BLACK_JACK_NUMBER && playerValue > BLACK_JACK_NUMBER -> WinType.DRAW
+            dealerValue.isDraw(player, playerValue) -> WinType.DRAW
+            player.isInitialBlackjack() -> WinType.PLAYER_BLACK_JACK
             playerValue > BLACK_JACK_NUMBER -> WinType.DEALER_WIN
             dealerValue > BLACK_JACK_NUMBER -> WinType.DEALER_BUST
             playerValue > dealerValue -> WinType.PLAYER_WIN
@@ -21,6 +21,13 @@ object BlackJackDetermine {
             else -> WinType.DRAW
         }
     }
+
+    private fun Int.isDraw(
+        player: BetPlayer,
+        playerValue: Int,
+    ) = player.isInitialBlackjack() && this == BLACK_JACK_NUMBER ||
+        this > BLACK_JACK_NUMBER && playerValue > BLACK_JACK_NUMBER ||
+        this == BLACK_JACK_NUMBER && playerValue == BLACK_JACK_NUMBER
 
     fun determineWinType(player: ScorePlayer, dealerValue: Int): WinType {
         val playerValue = player.optimalValue()
