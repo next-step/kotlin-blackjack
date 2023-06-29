1a46d813686546a78b630e9aa7f18bdfb0874fd4
konempty <jameswebb@naver.com>
2023-06-29 18:20:34 +0900

    refactor: 매직넘버 수정

--- src/main/kotlin/blackjack/domain/ScoreCalculator.kt
+++ src/main/kotlin/blackjack/domain/ScoreCalculator.kt
@@ -2,11 +2,12 @@ package blackjack.domain
 
 object ScoreCalculator {
     const val BLACKJACK_LIMIT = 21
+    private const val ACE_BONUS_SCORE = 10
 
     fun calculateScore(cards: Cards): Int {
         var sum = cards.sumOf { getCardScore(it) }
-        if (cards.hasAce() && sum <= 11) {
-            sum += 10
+        if (cards.hasAce() && sum + ACE_BONUS_SCORE <= BLACKJACK_LIMIT) {
+            sum += ACE_BONUS_SCORE
         }
         return sum
     }
