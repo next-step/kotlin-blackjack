daff49d10e01e53587e5d5f91697b0bf362c97ee
sjjeong <sjjeong1225@gmail.com>
2023-07-01 00:28:33 +0900

    step1: 주석 제거

--- src/test/kotlin/dsl/DslTest.kt
+++ src/test/kotlin/dsl/DslTest.kt
@@ -5,19 +5,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-//introduce {
-//    name("박재성")
-//    company("우아한형제들")
-//    skills {
-//        soft("A passion for problem solving")
-//        soft("Good communication skills")
-//        hard("Kotlin")
-//    }
-//    languages {
-//        "Korean" level 5
-//        "English" level 3
-//    }
-//}
 class DslTest {
 
     @ValueSource(strings = ["정석준", "Dino"])
