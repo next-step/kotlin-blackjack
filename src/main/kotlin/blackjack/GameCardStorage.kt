330391b835ed7321999ed428184dfd5660e97aa4
Minsung Kim <epicblue@hanmail.net>
2023-06-30 18:54:43 +0900

    feat(GameCardStorage): 게임에서 사용할 카드는 1장씩 순서대로 뽑을 수 있다.

--- src/main/kotlin/blackjack/GameCardStorage.kt
+++ src/main/kotlin/blackjack/GameCardStorage.kt
@@ -1,9 +1,18 @@
 package blackjack
 
 class GameCardStorage(
-    val cards: Set<Card>,
+    cardList: Set<Card>,
 ) {
+    private val cards = cardList.toMutableList()
+
+    val currentCards: List<Card>
+        get() = cards.toList()
+
     init {
         require(cards.size == 52) { "게임에서 사용될 카드들은 조커를 제외한 모든 트럼프 카드 52장이어야 합니다." }
+    }
+
+    fun drawCard(): Card {
+        return cards.removeFirst()
     }
 }
--- src/test/kotlin/blackjack/GameCardStorageTest.kt
+++ src/test/kotlin/blackjack/GameCardStorageTest.kt
@@ -20,8 +20,8 @@ class GameCardStorageTest : FreeSpec({
 
         val sut = GameCardStorage(allPossibleUniqueCards)
 
-        sut.cards shouldContainExactly allPossibleUniqueCards
-        sut.cards shouldHaveSize 52
+        sut.currentCards shouldContainExactly allPossibleUniqueCards
+        sut.currentCards shouldHaveSize 52
     }
 
     "게임에서 사용될 카드가 충분하지 못할 경우 예외를 던진다" {
@@ -34,4 +34,21 @@ class GameCardStorageTest : FreeSpec({
         exception.message shouldBe "게임에서 사용될 카드들은 조커를 제외한 모든 트럼프 카드 52장이어야 합니다."
     }
 
+    "게임에서 사용할 카드는 1장씩 입력한 순서대로 뽑을 수 있다." {
+        val allPossibleUniqueCards = CardNumber
+            .values()
+            .flatMap { number ->
+                Suit.values().map { suit ->
+                    Card(suit, number)
+                }
+            }
+            .shuffled()
+            .toSet()
+
+        val sut = GameCardStorage(allPossibleUniqueCards)
+
+        allPossibleUniqueCards.forEach {
+            sut.drawCard() shouldBe it
+        }
+    }
 })
