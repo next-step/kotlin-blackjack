de9a9034a020bdd3c80dcda8a4a22f880069cd1b
Songyi Kim <ksl2950@naver.com>
2023-07-02 02:54:12 +0900

    test: player 테스트 코드 fixture 사용

--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -1,5 +1,6 @@
 package blackjack.domain
 
+import blackjack.fixture.PlayerFixture.PLAYER_SONG2_NAME
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -8,7 +9,7 @@ class PlayerTest : FunSpec({
     test("플레이어는 자신의 이름과 카드 리스트를 가진다.") {
         // given
         val cards = Cards(listOf(Card(Rank.ACE, Suit.HEART), Card(Rank.JACK, Suit.SPADE)))
-        val name = "song2"
+        val name = PLAYER_SONG2_NAME
 
         // when
         val actual = Player(name, cards)
