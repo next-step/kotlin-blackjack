ae6f66f46c9dc62f1339a62fcc3c5c520341be0c
Songyi Kim <ksl2950@naver.com>
2023-07-02 01:10:22 +0900

    feat: 각 플레이어가 가지고 있는 카드의 계산 결과를 반환한다.

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -3,8 +3,11 @@ package blackjack.domain
 class Dealer(
     private val cardGenerator: CardGenerator = RandomCardGenerator()
 ) {
+    fun dealInitialCard(): List<Card> {
+        return List(Player.DEFAULT_CARD_SIZE) { dealCard() }.toList()
+    }
+
     fun dealCard(): Card {
         return cardGenerator.generate()
     }
-
 }
--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,10 +1,62 @@
 package blackjack.domain
 
-data class Player(
+import blackjack.domain.Rank.Companion.ACE_MAX
+import blackjack.domain.Rank.Companion.ACE_MIN
+
+class Player(
     val name: String,
-    val cards: Cards
+    cards: Cards = Cards()
 ) {
+    var cards: Cards = cards
+        private set
+
     init {
         require(name.isNotEmpty() && name.isNotBlank())
+    }
+
+    fun receiveCards(newCards: List<Card>): Player {
+        cards = Cards(cards.values + newCards)
+        return this
+    }
+
+    fun receiveCard(newCard: Card): Player {
+        cards = Cards(cards.values + newCard)
+        return this
+    }
+
+    fun calculateScore(): Int {
+        var score = cards.sumOfScoreWithAceAsOne()
+
+        repeat(getNumberOfAce()) {
+            if (isValidBlackjackScoreWithAceAsEleven(score)) {
+                score += ACE_MAX - ACE_MIN
+            }
+        }
+
+        return score
+    }
+
+    private fun isValidBlackjackScoreWithAceAsEleven(score: Int) = score + ACE_MAX - ACE_MIN <= MAX_SCORE
+
+    private fun getNumberOfAce() = cards.values.count { it.rank == Rank.ACE }
+
+    companion object {
+        const val DEFAULT_CARD_SIZE = 2
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Player) return false
+        val o = other as Player
+
+        return this.name == o.name && this.cards == o.cards
+    }
+
+    override fun hashCode(): Int {
+        return 31 * name.hashCode() + cards.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Player(name='$name', cards=$cards)"
     }
 }
