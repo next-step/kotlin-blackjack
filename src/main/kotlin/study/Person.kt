e9faedfd8c6b490e6bfd7bec87b92aaa2ba3493d
hyunju-seo <hyunju6889@naver.com>
2023-07-03 00:43:21 +0900

    refactor: step1리뷰 반영

--- src/main/kotlin/study/Person.kt
+++ src/main/kotlin/study/Person.kt
@@ -1,34 +1,3 @@
 package study
 
 class Person(val name: String, val company: String?, val skills: List<Skill>, val languages: List<Language>)
-
-fun introduce(block: PersonBuilder.() -> Unit): Person {
-    return PersonBuilder().apply(block).build()
-}
-
-class PersonBuilder {
-    private lateinit var name: String
-    private var company: String? = null
-    private var skills: List<Skill> = emptyList()
-    private var language: List<Language> = emptyList()
-
-    fun name(value: String) {
-        this.name = value
-    }
-
-    fun company(value: String) {
-        this.company = value
-    }
-
-    fun skills(block: SkillsBuilder.() -> Unit) {
-        this.skills = SkillsBuilder().apply(block).build()
-    }
-
-    fun languages(block: LanguagesBuilder.() -> Unit) {
-        this.language = LanguagesBuilder().apply(block).build()
-    }
-
-    fun build(): Person {
-        return Person(name, company, skills, language)
-    }
-}
--- src/test/kotlin/study/DslBuilderTest.kt
+++ src/test/kotlin/study/DslBuilderTest.kt
@@ -1,11 +1,12 @@
 package study
 
+import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 
-class PersonTest : StringSpec({
-    "person 생성 테스트" {
+class DslBuilderTest : StringSpec({
+    "person을 생성한다." {
         val person = introduce {
             name("홍길동")
             company("홍길동 회사")
@@ -32,4 +33,22 @@ class PersonTest : StringSpec({
             Language.english(3)
         )
     }
+
+    "person 생성 시 지정하지 않으면 예외가 발생한다." {
+        shouldThrowAny {
+            introduce {
+                company("홍길동 회사")
+                skills {
+                    soft("A passion for problem solving")
+                    soft("Good communication skills")
+                    hard("Kotlin")
+                }
+                languages {
+                    "Korean" level 5
+                    "English" level 3
+                }
+            }
+        }
+    }
+
 })
