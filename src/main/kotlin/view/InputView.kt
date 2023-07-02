e96771c53a85a07d9c46e9fb48b551d00ae7a45f
jeff.42(박제헌) <jeff.42@kakaocorp.com>
2023-07-02 18:36:08 +0900

    블랙잭: Step2 뷰작성

---
+++ src/main/kotlin/view/InputView.kt
@@ -0,0 +1,18 @@
+package view
+
+private const val PLAYER_NAME_DELIMITER = ","
+
+fun inputPlayerView(): List<String> {
+    println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+    return readln().replace(" ", "").split(PLAYER_NAME_DELIMITER)
+}
+
+fun askDraw(playerName: String): YesOrNo {
+    println("${playerName}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+    return YesOrNo.of(readln()) ?: askAgain(playerName)
+}
+
+private fun askAgain(playerName: String): YesOrNo {
+    println("옳지 않은 커맨드입니다.")
+    return askDraw(playerName)
+}
