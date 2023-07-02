4c23c5487f087f4abb8834de51667005da034f6c
waveofmymind <sjun0913@gmail.com>
2023-07-03 00:06:27 +0900

    Refactor: property-based testing으로 변경

--- src/test/kotlin/blackjack/player/ScoreCalculatorTest.kt
+++ src/test/kotlin/blackjack/player/ScoreCalculatorTest.kt
@@ -4,41 +4,48 @@ import blackjack.card.Card
 import blackjack.card.CardNumber
 import blackjack.card.CardSuit
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class ScoreCalculatorTest : StringSpec({
 
-    "손에 Ace와 King이 있을 때 점수는 21점이어야 한다" {
-        val hand = Hand()
-        hand.addCard(Card(CardNumber.ACE, CardSuit.HEART))
-        hand.addCard(Card(CardNumber.KING, CardSuit.CLUB))
-
-        ScoreCalculator.calculateScore(hand) shouldBe 21
-    }
-
-    "손에 Ace 두 장이 있을 때 점수는 12점이어야 한다" {
-        val hand = Hand()
-        hand.addCard(Card(CardNumber.ACE, CardSuit.HEART))
-        hand.addCard(Card(CardNumber.ACE, CardSuit.CLUB))
-
-        ScoreCalculator.calculateScore(hand) shouldBe 12
-    }
-
-    "손에 Ace, King, Queen이 있을 때 점수는 21점이어야 한다" {
-        val hand = Hand()
-        hand.addCard(Card(CardNumber.ACE, CardSuit.HEART))
-        hand.addCard(Card(CardNumber.KING, CardSuit.CLUB))
-        hand.addCard(Card(CardNumber.QUEEN, CardSuit.DIAMOND))
-
-        ScoreCalculator.calculateScore(hand) shouldBe 21
-    }
-
-    "손에 9, 10, Ace가 있을 때 점수는 20점이어야 한다" {
-        val hand = Hand()
-        hand.addCard(Card(CardNumber.NINE, CardSuit.HEART))
-        hand.addCard(Card(CardNumber.TEN, CardSuit.CLUB))
-        hand.addCard(Card(CardNumber.ACE, CardSuit.DIAMOND))
-
-        ScoreCalculator.calculateScore(hand) shouldBe 20
+    "손에 카드들을 가질 때 올바른 점수가 계산되어야 한다" {
+        forAll(
+            row(
+                listOf(
+                    Card(CardNumber.ACE, CardSuit.HEART),
+                    Card(CardNumber.KING, CardSuit.CLUB)
+                ),
+                21
+            ),
+            row(
+                listOf(
+                    Card(CardNumber.ACE, CardSuit.HEART),
+                    Card(CardNumber.ACE, CardSuit.CLUB)
+                ),
+                12
+            ),
+            row(
+                listOf(
+                    Card(CardNumber.ACE, CardSuit.HEART),
+                    Card(CardNumber.KING, CardSuit.CLUB),
+                    Card(CardNumber.QUEEN, CardSuit.DIAMOND)
+                ),
+                21
+            ),
+            row(
+                listOf(
+                    Card(CardNumber.NINE, CardSuit.HEART),
+                    Card(CardNumber.TEN, CardSuit.CLUB),
+                    Card(CardNumber.ACE, CardSuit.DIAMOND)
+                ),
+                20
+            )
+        ) { cards, expectedScore ->
+            val hand = Hand()
+            cards.forEach { hand.addCard(it) }
+            ScoreCalculator.calculateScore(hand) shouldBe expectedScore
+        }
     }
 })
