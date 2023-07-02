b56d0e5987be730172c10606ec94daa5c162dcfc
antaeseon <ants412@naver.com>
2023-07-03 08:31:58 +0900

    refactor: 함수로만 접근 가능하도록 수정(step1)

--- src/main/kotlin/study/SkillsBuilder.kt
+++ src/main/kotlin/study/SkillsBuilder.kt
@@ -1,8 +1,8 @@
 package study
 
 class SkillsBuilder {
-    var softSkills: MutableList<String> = mutableListOf()
-    var hardSkills: MutableList<String> = mutableListOf()
+    private var softSkills: MutableList<String> = mutableListOf()
+    private var hardSkills: MutableList<String> = mutableListOf()
 
     fun soft(value: String) {
         softSkills.add(value)
