d4bf8d57ef8424aac309cc852c310099ecc249a6
ggam-nyang <v44ads@naver.com>
2023-07-03 17:19:17 +0900

    feature: 카드 숫자는 Ace ~ King까지 있다.

---
+++ src/test/kotlin/blackjack/CardNumberTest.kt
@@ -0,0 +1,24 @@
+package blackjack
+
+import blackjack.domain.CardNumber
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class CardNumberTest {
+
+    @DisplayName("카드의 숫자는 1(Ace)부터 King까지 있다")
+    @Test
+    fun cardNumber() {
+        val actual = CardNumber.values()
+
+        actual.size shouldBe 13
+        actual.shouldContainAll(listOf(
+            CardNumber.ACE, CardNumber.TWO, CardNumber.THREE,
+            CardNumber.FOUR, CardNumber.FIVE, CardNumber.SIX,
+            CardNumber.SEVEN, CardNumber.EIGHT, CardNumber.NINE, CardNumber.TEN,
+            CardNumber.JACK, CardNumber.QUEEN, CardNumber.KING
+        ))
+    }
+}
