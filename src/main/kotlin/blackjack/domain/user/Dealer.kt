0573f10e2117cf75044d426ec40cde8fcd0f353d
pooh <pooh.duck@kakaocorp.com>
2023-07-03 22:58:01 +0900

    reafcotr: step2 리뷰 반영 - player가 card를 받으면 자동으로 status가 갱신하도록 변경

--- src/main/kotlin/blackjack/domain/user/Dealer.kt
+++ src/main/kotlin/blackjack/domain/user/Dealer.kt
@@ -5,6 +5,7 @@ import blackjack.domain.card.Deck
 class Dealer(private val deck: Deck = Deck.create(), name: String = "dealer") : Player(name) {
     fun giveCardTo(player: Player, cardCount: Int = 1) {
         repeat(cardCount) { player.addCard(deck.getNextCard()) }
+        player.updateStatus()
     }
 
     fun giveCardIfPlayerWantHit(player: Player) {
