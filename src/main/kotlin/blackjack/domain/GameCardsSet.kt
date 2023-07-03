5157f6681b2acc3cdf24654ad51e7d6e1734ec13
yoo-jaein <y_jaein@naver.com>
2023-07-03 15:40:04 +0900

    feat: GameCardsSet에서 원하는 카드를 뽑는 함수 추가

--- src/main/kotlin/blackjack/domain/GameCardsSet.kt
+++ src/main/kotlin/blackjack/domain/GameCardsSet.kt
@@ -1,6 +1,7 @@
 package blackjack.domain
 
 import blackjack.domain.Card.Companion.ALL_CARDS
+import blackjack.error.BlackjackErrorMessage.CAN_NOT_DRAW_THIS_CARD
 import blackjack.error.BlackjackErrorMessage.NO_MORE_REMAINING_CARDS
 import kotlin.random.Random
 
@@ -16,5 +17,13 @@ class GameCardsSet {
         val drawnCard = remainingCards[randomIndex]
         drawnCards.add(drawnCard)
         return drawnCard
+    }
+
+    fun drawSpecificCard(card: Card): Card {
+        check(remainingCards.isNotEmpty()) { NO_MORE_REMAINING_CARDS }
+        check(remainingCards.contains(card)) { CAN_NOT_DRAW_THIS_CARD }
+
+        drawnCards.add(card)
+        return card
     }
 }
--- src/test/kotlin/blackjack/GameCardsSetTest.kt
+++ src/test/kotlin/blackjack/GameCardsSetTest.kt
@@ -2,6 +2,8 @@ package blackjack
 
 import blackjack.domain.Card
 import blackjack.domain.GameCardsSet
+import blackjack.domain.Ranks
+import blackjack.domain.Suits
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,6 +21,23 @@ class GameCardsSetTest {
     fun `게임 카드 세트에서 카드를 뽑을 수 있다`() {
         val draw = gameCardsSet.drawRandomCard()
         assertThat(draw).isInstanceOf(Card::class.java)
+    }
+
+    @Test
+    fun `게임 카드 세트에서 원하는 카드를 뽑을 수 있다`() {
+        val card = Card.createCard(Ranks.TWO, Suits.HEARTS)
+        val drawnCard = gameCardsSet.drawSpecificCard(card)
+        assertThat(card).isEqualTo(drawnCard)
+    }
+
+    @Test
+    fun `게임 카드 세트에서 이미 뽑은 카드를 다시 뽑을 경우 IllegalStateException`() {
+        val card = Card.createCard(Ranks.TWO, Suits.HEARTS)
+        val drawnCard = gameCardsSet.drawSpecificCard(card)
+
+        assertThrows<IllegalStateException> {
+            val drawnCard2 = gameCardsSet.drawSpecificCard(card)
+        }
     }
 
     @Test
