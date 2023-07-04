dc9935edfd8d2f6665719e807dd005f50de9b780
maenguin-h <maenguin.h@kakaocorp.com>
2023-07-04 23:49:38 +0900

    step2 : 플레이가 소지한 카드를 표현하는 Deck 구현

---
+++ src/main/kotlin/blackjack/Deck.kt
@@ -0,0 +1,21 @@
+package blackjack
+
+/**
+ * ### 플레이어가 소지한 카드를 표현하는 객체 입니다.
+ *
+ * 카드를 추가할 수 있고, 소지한 카드들에 대한 점수를 확인할 수 있습니다.
+ */
+class Deck(cards: List<Card> = listOf()) {
+
+    private val _cards: MutableList<Card> = cards.toMutableList()
+
+    val size : Int
+        get() = _cards.size
+
+    val point : Int
+        get() = BlackjackCardPointCalculator.calculate(_cards)
+
+    fun add(card: Card) {
+        _cards.add(card)
+    }
+}
---
+++ src/test/kotlin/blackjack/DeckTest.kt
@@ -0,0 +1,23 @@
+package blackjack
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class DeckTest {
+    @Test
+    internal fun `덱에 카드를 추가할 수 있다`() {
+        val sut = Deck()
+        sut.size shouldBe 0
+
+        sut.add(Card(CardRank.NINE, CardSuit.HEART))
+        sut.size shouldBe 1
+    }
+
+    @Test
+    internal fun `덱에 있는 카드의 점수 합계를 알 수 있다`() {
+        val sut = Deck()
+        sut.add(Card(CardRank.JACK, CardSuit.HEART))
+        sut.add(Card(CardRank.ACE, CardSuit.SPADE))
+        sut.point shouldBe 21
+    }
+}
