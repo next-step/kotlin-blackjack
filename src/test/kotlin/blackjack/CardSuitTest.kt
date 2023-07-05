6b736c61e3ae0115e44951931c21e89bb9036c5a
ggam-nyang <v44ads@naver.com>
2023-07-05 14:10:32 +0900

    feature: 카드 문양은 CardSuit enum으로 나타낸다

---
+++ src/test/kotlin/blackjack/CardSuitTest.kt
@@ -0,0 +1,18 @@
+package blackjack
+
+import blackjack.domain.CardSuit
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class CardSuitTest {
+
+    @DisplayName("CardSuit은 SPADE, HEART, DIAMOND, CLUB 네가지이다.")
+    @Test
+    fun cardSuitType() {
+        val actual = CardSuit.values()
+        val expect = arrayOf(CardSuit.SPADE, CardSuit.HEART, CardSuit.DIAMOND, CardSuit.CLUB)
+
+        actual shouldBe expect
+    }
+}
