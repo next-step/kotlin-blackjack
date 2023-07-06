3f323a9ad981600b7495238d23a9c49fc77e57ce
choihwan2 <choihwan2@naver.com>
2023-07-06 21:51:00 +0900

    추가 : A 는 먼저 1의 값을 가진다, K,Q,J 은 10 으로 계산한다. 카드는 숫자와 문양을 가진다.

--- src/test/kotlin/blackjack/card/CardTest.kt
+++ src/test/kotlin/blackjack/card/CardTest.kt
@@ -1,11 +1,41 @@
 package blackjack.card
 
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
 enum class Pattern {
-    CLOVER,
-    DIAMOND,
-    HEART,
-    SPADE,
+    CLOVER, DIAMOND, HEART, SPADE,
 }
 
-data class Card(val number: String, val pattern: Pattern)
-class CardTest
+data class Card(private val value: String, val pattern: Pattern) {
+
+    fun getValue(): Int {
+        if (value == "A") return 1
+        if (value in listOf("K", "Q", "J")) return 10
+        return value.toInt()
+    }
+
+    infix operator fun plus(other: Card): Int = this.getValue() + other.getValue()
+}
+
+class CardTest : StringSpec({
+    "카드는 숫자 1~ 9를 가질수 있다." {
+        listOf("1", "2", "3", "4", "5", "6", "7", "8", "9").forAll {
+            val card = Card(it, Pattern.CLOVER)
+            card.getValue() shouldBe it.toInt()
+        }
+    }
+
+    "카드 K, Q , J 는 10으로 계산된다." {
+        listOf("K", "Q", "J").forAll {
+            val card = Card(it, Pattern.DIAMOND)
+            card.getValue() shouldBe 10
+        }
+    }
+
+    "카드 A 는 일단 1의 값을 가진다." {
+        val card = Card("A", Pattern.HEART)
+        card.getValue() shouldBe 1
+    }
+})
