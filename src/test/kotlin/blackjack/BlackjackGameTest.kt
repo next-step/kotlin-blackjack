af0e8c1db158f0ae15f27fcad69d6fd06ca3358d
maenguin-h <maenguin.h@kakaocorp.com>
2023-07-06 22:34:55 +0900

    step2 : 아직은 플레이어에게 카드를 주기만 하는 BlackjackGame 구현

---
+++ src/test/kotlin/blackjack/BlackjackGameTest.kt
@@ -0,0 +1,28 @@
+package blackjack
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class BlackjackGameTest {
+    @Test
+    internal fun `게임이 시작되면 플레이어는 두장의 카드를 받는다`() {
+        val playerA = Player("a")
+        val playerB = Player("b")
+        val sut = BlackjackGame(Players(listOf(playerA, playerB)))
+
+        sut.dealInitialHand()
+
+        playerA.deck.size shouldBe 2
+        playerB.deck.size shouldBe 2
+    }
+
+    @Test
+    internal fun `플레이어에게 추가로 카드를 전달한다`() {
+        val playerA = Player("a")
+        val sut = BlackjackGame(Players(listOf(playerA)))
+
+        sut.dealCardTo(playerA)
+
+        playerA.deck.size shouldBe 1
+    }
+}
