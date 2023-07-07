bdffc7ae6d7c19e8c5e816e2d581527151b267db
konempty <jameswebb@naver.com>
2023-07-07 13:37:20 +0900

    refactor: 베팅 금액 테스트 로직 수정

--- src/test/kotlin/blackjack/util/DefaultInterfaceImplements.kt
+++ src/test/kotlin/blackjack/util/DefaultInterfaceImplements.kt
@@ -1,7 +1,12 @@
 package blackjack.util
 
+import blackjack.domain.user.UserBetMoneyGetter
 import blackjack.domain.user.UserDrawChecker
 
 val TEST_USER_DRAW_CHECKER = UserDrawChecker { true }
 
-val TEST_USER_BET_MONEY_GETTER = { _: String -> 10000 }
+class FixedUserBetMoneyGetter(private val betMoney: Int = 10000) : UserBetMoneyGetter {
+    override fun getBetMoney(userName: String): Int {
+        return betMoney
+    }
+}
