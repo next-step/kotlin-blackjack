8d60595a8a4787b95df0c25cd5f31f3c2e1ca2f3
theo-94 <theo.94@kakaocorp.com>
2023-07-08 23:59:57 +0900

    피드백 반영 : Players 변경
    - players 불변으로 변경
    - convention 맞추기
    - ktlint pre-commit hook에 추가

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -3,5 +3,5 @@ package blackjack.domain
 data class Players(
     val names: List<String>
 ) {
-    var players: List<Player> = names.map{ Player(it) }
+    val players: List<Player> = names.map { Player(it) }
 }
