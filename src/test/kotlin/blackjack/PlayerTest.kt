9e356b7645fef35726307151012455db35f4201e
theo-94 <theo.94@kakaocorp.com>
2023-07-09 01:09:23 +0900

    피드백 반영 : PlayerTest 변경
    StringSpec 사용

--- src/test/kotlin/blackjack/PlayerTest.kt
+++ src/test/kotlin/blackjack/PlayerTest.kt
@@ -2,33 +2,30 @@ package blackjack
 
 import blackjack.domain.Player
 import blackjack.domain.Players
-import blackjack.domain.card.Card
-import blackjack.domain.card.CardNumber
-import blackjack.domain.card.CardSymbol
-import blackjack.domain.card.Cards
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 
-class PlayerTest {
-
-    @ParameterizedTest
-    @ValueSource(strings = ["pobi", "jason"])
-    fun `Player 객체에 이름을 부여할 수 있다`(name: String) {
-        val player = Player(name)
-        player.name shouldBe name
+class PlayerTest : StringSpec({
+    "Player 객체에 이름을 부여할 수 있다" {
+        forAll(
+            row("pobi"),
+            row("jason")
+        ) { name ->
+            val player = Player(name)
+            player.name shouldBe name
+        }
     }
 
-    @Test
-    fun `쉼표 기준으로 분리된 이름을 받아 Player들을 생성한다`() {
+    "쉼표 기준으로 분리된 이름을 받아 Player들을 생성한다" {
         val nameList = listOf("pobi", "jason")
         val players = Players(nameList)
 
-        nameList.all{ name ->
+        nameList.all { name ->
             players.players.any { player ->
                 player.name == name
             }
         } shouldBe true
     }
-}
+})
