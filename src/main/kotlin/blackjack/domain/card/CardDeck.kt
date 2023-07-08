eeb5ab2b2e467c982688430c646bd2c45c09c4a0
theo-94 <theo.94@kakaocorp.com>
2023-07-08 17:50:37 +0900

    피드백 반영 : 카드덱을 모두 소진하면 새로운 카드 세트 가져오기

--- src/main/kotlin/blackjack/domain/card/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/card/CardDeck.kt
@@ -1,13 +1,22 @@
 package blackjack.domain.card
 
 class CardDeck {
-    val cards: MutableList<Card> = CardNumber.values().flatMap { number ->
-        CardSymbol.values().map { symbol ->
-            Card(number, symbol)
-        }
-    }.toMutableList()
+    lateinit var cards: MutableList<Card>
+
+    init {
+        setNewCards()
+    }
 
     fun peekCard(index: Int): Card {
+        if (cards.isEmpty()) setNewCards()
         return cards.removeAt(index)
+    }
+
+    private fun setNewCards() {
+        cards = CardNumber.values().flatMap { number ->
+            CardSymbol.values().map { symbol ->
+                Card(number, symbol)
+            }
+        }.toMutableList()
     }
 }
