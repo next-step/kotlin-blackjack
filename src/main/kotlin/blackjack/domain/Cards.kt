fb6b5e015f745cded85563134ec970659fc93f0c
june.kim <junho85@gmail.com>
2023-07-08 23:50:48 +0900

    PR리뷰 적용 - Cards에도 backing property 사용 https://github.com/next-step/kotlin-blackjack/pull/549#discussion_r1257236100

--- src/main/kotlin/blackjack/domain/Cards.kt
+++ src/main/kotlin/blackjack/domain/Cards.kt
@@ -1,14 +1,17 @@
 package blackjack.domain
 
 class Cards(vararg cards: Card) {
-    val cards: MutableList<Card> = mutableListOf()
+    private val _cards: MutableList<Card> = mutableListOf()
+
+    val cards: List<Card>
+        get() = _cards.toList()
 
     init {
-        this.cards.addAll(cards)
+        _cards.addAll(cards)
     }
 
     fun add(card: Card) {
-        cards.add(card)
+        _cards.add(card)
     }
 
     fun canDrawMoreCard(): Boolean {
