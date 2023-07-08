2deac03d334d158a88fd00cce6198f8f0b8d564f
jonghyunhub <chuwhdgus@gmail.com>
2023-07-09 00:59:09 +0900

    test : 딜러가 게임에서 패배하면, 플레이어의 베팅금만큼 돈을 잃는다

---
+++ src/test/kotlin/blackjack/domain/gamer/DealerTest.kt
@@ -0,0 +1,27 @@
+package blackjack.domain.gamer
+
+import blackjack.dto.GeneratePlayerRequest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class DealerTest {
+
+    @Test
+    fun `딜러가 게임에서 패배하면, 플레이어의 베팅금만큼 돈을 잃는다`() {
+        // given
+        val bettingMoney = 10000
+        val playerName = "name"
+        val player = GeneratePlayerRequest(playerName, bettingMoney)
+            .generatePlayer()
+
+        val dealer = Dealer()
+
+        // when
+        player.proceedGameRecord(GameRecordType.WIN)
+        val playerWinMoney = player.winMoney()
+        dealer.loseMoney(playerWinMoney)
+
+        // then
+        Assertions.assertThat(dealer.money).isEqualTo(-bettingMoney)
+    }
+}
