112dc8cf9324b778c8cc20b524a492405d16a0d3
june.kim <junho85@gmail.com>
2023-07-08 15:47:03 +0900

    PR리뷰 적용 - 의미있는 상수명 https://github.com/next-step/kotlin-blackjack/pull/549#discussion_r1255667817

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -8,16 +8,23 @@ class Player(val name: String) {
     }
 
     fun canDrawMoreCard(): Boolean {
-        return calculateScore() < 21
+        return calculateScore() < BLACKJACK
     }
 
     fun calculateScore(): Int {
         val hasAce = cards.any { it.denomination == Denomination.ACE }
 
         val totalScore = cards.sumOf { it.denomination.score }
-        if (hasAce && totalScore <= 11) {
-            return totalScore + 10
+        if (hasAce && totalScore <= BLACKJACK - ACE_SCORE_DIFF) {
+            return totalScore + ACE_SCORE_DIFF
         }
         return totalScore
+    }
+
+    companion object {
+        const val BLACKJACK = 21
+        private const val ACE_SCORE_ONE = 1
+        private const val ACE_SCORE_ELEVEN = 11
+        const val ACE_SCORE_DIFF = ACE_SCORE_ELEVEN - ACE_SCORE_ONE
     }
 }
