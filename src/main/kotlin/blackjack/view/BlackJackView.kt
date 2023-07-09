ddf40c6f2c007d3c2e3e27b12c5e2c4bfb81d86c
choihwan2 <choihwan2@naver.com>
2023-07-10 03:03:17 +0900

    추가 : BlackJackView 추가

--- src/main/kotlin/blackjack/view/BlackJackView.kt
+++ src/main/kotlin/blackjack/view/BlackJackView.kt
@@ -1,7 +1,17 @@
 package blackjack.view
 
+import blackjack.domain.player.Player
+
 object BlackJackView {
     fun printPlayerInputView() {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+    }
+
+    fun printPlayerCardsView(player: Player) {
+        println("${player.name}카드: ${player.cards.joinToString { ", " }}")
+    }
+
+    fun printPlayerMoreCardView(player: Player) {
+        println("${player.name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n")
     }
 }
