c9d2f6f4d68647acdc783816f671135f16013792
hyunju-seo <hyunju6889@naver.com>
2023-07-09 18:06:55 +0900

    feat: 출력 이름을 관리하는 DisplayName클래스 생성

--- src/main/kotlin/blackjack/domain/result/GameResultGenerator.kt
+++ src/main/kotlin/blackjack/domain/result/GameResultGenerator.kt
@@ -4,14 +4,6 @@ import blackjack.domain.participant.Dealer
 import blackjack.domain.participant.Players
 
 class GameResultGenerator(val dealer: Dealer, val players: Players) {
-    fun getDealerResult(): Map<Result, Int> {
-        val matchResults = getMatchResults()
-        return matchResults
-            .map { it.ofDealer() }
-            .groupingBy { it.result }
-            .eachCount()
-    }
-
     fun getDealerEarningAmount(): Double {
         return getMatchResults()
             .sumOf {
--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,11 +1,8 @@
 package blackjack.view
 
 import blackjack.domain.card.Card
-import blackjack.domain.card.CardNumber
-import blackjack.domain.card.CardShape
 import blackjack.domain.participant.Dealer
 import blackjack.domain.participant.Participant
-import blackjack.domain.result.Result
 
 object OutputView {
     fun printStart(participants: List<Participant>) {
@@ -25,45 +22,16 @@ object OutputView {
     }
 
     fun printPlayerResult(participant: Participant) {
-        println("${resultMessage(participant.name, participant.cards())}- 결과: ${participant.score().value}")
+        println("${resultMessage(participant.name, participant.cards())} - 결과: ${participant.score().value}")
     }
 
     private fun resultMessage(name: String, cards: List<Card>): String {
-        val cardsMessage = cards.joinToString(", ") { it.number.displayName() + it.shape.displayName() }
+        val cardsMessage =
+            cards.joinToString(", ") { DisplayName.cardNumber(it.number) + DisplayName.cardShape(it.shape) }
         return "${name}의 카드: $cardsMessage"
-    }
-
-    fun printResult(name: String, result: Result) {
-        println("$name: ${result.displayName()}")
-    }
-
-    fun printResult(name: String, results: Map<Result, Int>) {
-        val message = results.map { (result, count) -> count.toString() + result.displayName() }.joinToString(" ")
-        println("$name: $message")
     }
 
     fun printProfit(name: String, profit: Double) {
         println("$name: $profit")
     }
-}
-
-private fun CardNumber.displayName() = when (this) {
-    CardNumber.ACE -> "A"
-    CardNumber.JACK -> "J"
-    CardNumber.QUEEN -> "Q"
-    CardNumber.KING -> "K"
-    else -> this.cardScore.primary.toString()
-}
-
-private fun CardShape.displayName() = when (this) {
-    CardShape.CLUB -> "클로버"
-    CardShape.DIAMOND -> "다이아"
-    CardShape.HEART -> "하트"
-    CardShape.SPADE -> "스페이드"
-}
-
-private fun Result.displayName() = when (this) {
-    Result.WIN -> "승"
-    Result.DRAW -> "무"
-    Result.LOSE -> "패"
 }
