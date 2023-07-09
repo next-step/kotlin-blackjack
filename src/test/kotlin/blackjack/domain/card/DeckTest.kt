61b5f2c0dba0b35be46230fe561f63cbf6d1af68
choihwan2 <choihwan2@naver.com>
2023-07-10 00:34:51 +0900

    추가 : 카드 뭉치는 처음에 하트,다이아몬드,스페이드,클로버로 한 문양당 13(A~9, K,Q,J)장으로 이루어져 있다.

--- src/test/kotlin/blackjack/domain/card/DeckTest.kt
+++ src/test/kotlin/blackjack/domain/card/DeckTest.kt
@@ -1,11 +1,22 @@
 package blackjack.domain.card
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 
 class DeckTest : StringSpec({
     "카드 뭉치의 크기는 초기에 52장이다." {
         val deck = Deck.makeDeck()
         deck.cards.size shouldBe 52
+    }
+
+    "카드의 네가지 문양으로 13장의 카드를 다 가지고 있어야한다." {
+        val deck = Deck.makeDeck()
+        Pattern.values().forAll { pattern ->
+            NumberShape.values().forAll { number ->
+                deck.cards shouldContain Card(number, pattern)
+            }
+        }
     }
 })
