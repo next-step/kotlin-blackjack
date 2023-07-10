5d3c3c44075df99d35e3378d405fa02023f37dfb
jonghyunhub <chuwhdgus@gmail.com>
2023-07-10 18:05:36 +0900

    test : BlackJackGamer는 뽑은 카드를 한번에 한장씩 추가할 수 있다

--- src/test/kotlin/blackjack/domain/gamer/BlackJackGamerTest.kt
+++ src/test/kotlin/blackjack/domain/gamer/BlackJackGamerTest.kt
@@ -1,5 +1,9 @@
 package blackjack.domain.gamer
 
+import blackjack.domain.card.Card
+import blackjack.domain.card.CardNumber
+import blackjack.domain.card.CardShape
+import blackjack.dto.GeneratePlayerRequest
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
@@ -42,4 +46,38 @@ class BlackJackGamerTest {
         // then
         Assertions.assertThat(blackJackGamer.money).isEqualTo(money - takenMoney)
     }
+
+
+    @Test
+    fun `BlackJackGamer는 뽑은 카드를 한번에 한장씩 추가할 수 있다`() {
+        // given
+        val blackJackGamer = BlackJackGamer(name = "name", money = 1)
+
+        val drawCard = Card(shape = CardShape.CLOVER, number = CardNumber.A)
+
+        // when
+        blackJackGamer.addCard(drawCard)
+        val answer = listOf(Card(shape = CardShape.CLOVER, number = CardNumber.A))
+
+        // then
+        Assertions.assertThat(blackJackGamer.getCards()).isEqualTo(answer)
+    }
+
+//    @Test
+//    fun `블랙잭 플레이어는 뽑은 카드를 한번에 여러장 씩 추가할 수 있다`() {
+//        val drawCards = mutableListOf<Card>()
+//        drawCards.add(Card(shape = CardShape.DIAMOND, number = CardNumber.TWO))
+//        drawCards.add(Card(shape = CardShape.HEART, number = CardNumber.THREE))
+//        drawCards.add(Card(shape = CardShape.CLOVER, number = CardNumber.A))
+//
+//        player.addCards(drawCards)
+//
+//        val answer = listOf(
+//            Card(shape = CardShape.DIAMOND, number = CardNumber.TWO),
+//            Card(shape = CardShape.HEART, number = CardNumber.THREE),
+//            Card(shape = CardShape.CLOVER, number = CardNumber.A)
+//        )
+//
+//        Assertions.assertThat(player.getCards()).isEqualTo(answer)
+//    }
 }
