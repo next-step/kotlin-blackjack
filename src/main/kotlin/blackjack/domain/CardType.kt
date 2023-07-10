32477a96a21d2d26d3e2c81da976c63aa2fcc5de
antaeseon <ants412@naver.com>
2023-07-10 21:19:27 +0900

    refactor: view 로직 분리

--- src/main/kotlin/blackjack/domain/CardType.kt
+++ src/main/kotlin/blackjack/domain/CardType.kt
@@ -1,8 +1,8 @@
 package blackjack.domain
 
-enum class CardType(val label: String) {
-    DIAMOND("다이아몬드"),
-    HEART("하트"),
-    SPADE("스페이드"),
-    CLOVER("클로버")
+enum class CardType {
+    DIAMOND,
+    HEART,
+    SPADE,
+    CLOVER
 }
--- src/main/kotlin/blackjack/view/ResultView.kt
+++ src/main/kotlin/blackjack/view/ResultView.kt
@@ -1,5 +1,7 @@
 package blackjack.view
 
+import blackjack.domain.CardType
+import blackjack.domain.Denomination
 import blackjack.domain.Player
 import blackjack.domain.Players
 
@@ -26,5 +28,20 @@ object ResultView {
     }
 
     private fun playerCards(player: Player) =
-        player.cards.cards.joinToString(", ") { it -> "${it.cardType.label}${it.denomination.label}" }
+        player.cards.cards.joinToString(", ") { it -> "${it.cardType.display()}${it.denomination.display()}" }
+}
+
+private fun CardType.display() = when (this) {
+    CardType.CLOVER -> "클로버"
+    CardType.DIAMOND -> "다이아"
+    CardType.HEART -> "하트"
+    CardType.SPADE -> "스페이드"
+}
+
+private fun Denomination.display() = when (this) {
+    Denomination.ACE -> "A"
+    Denomination.KING -> "K"
+    Denomination.QUEEN -> "Q"
+    Denomination.JACK -> "J"
+    else -> this.score.toString()
 }
