f0ab30d64a5140e191c957fa2583ef9bd5b1542e
jonghyunhub <chuwhdgus@gmail.com>
2023-07-10 17:21:22 +0900

    refact : 딜러 승리, 패배 여부 저장 맴버 삭제

--- src/main/kotlin/blackjack/domain/gamer/Dealer.kt
+++ src/main/kotlin/blackjack/domain/gamer/Dealer.kt
@@ -1,28 +1,8 @@
 package blackjack.domain.gamer
 
-import blackjack.domain.gamer.GameRecordType.DRAW
-import blackjack.domain.gamer.GameRecordType.LOSE
-import blackjack.domain.gamer.GameRecordType.WIN
-
 class Dealer : BlackJackGamer(name = DEALER_NAME) {
-    private var winRecord = (WIN to 0)
-    private var loseRecord = (LOSE to 0)
-    private var drawRecord = (DRAW to 0)
-
     override fun getGamerType(): GamerType {
         return GamerType.DEALER
-    }
-
-    override fun proceedGameRecord(gameRecordType: GameRecordType) {
-        when (gameRecordType) {
-            WIN -> winRecord = Pair(WIN, winRecord.second + 1)
-            LOSE -> loseRecord = Pair(LOSE, loseRecord.second + 1)
-            DRAW -> drawRecord = Pair(DRAW, drawRecord.second + 1)
-        }
-    }
-
-    fun getTotalGameRecord(): List<Pair<GameRecordType, Int>> {
-        return listOf(winRecord, loseRecord, drawRecord)
     }
 
     fun winMoney(winMoney: Int) {
