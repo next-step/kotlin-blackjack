625783e15e837262a1bf3c3d2831d8ced341325b
Songyi Kim <ksl2950@naver.com>
2023-07-11 00:46:12 +0900

    refactor: 검증 함수 가독성 있게 수정

--- src/main/kotlin/blackjack/domain/ScoreCalculator.kt
+++ src/main/kotlin/blackjack/domain/ScoreCalculator.kt
@@ -1,11 +1,13 @@
 package blackjack.domain
 
+import blackjack.domain.ScoreValidator.isValidBlackjackScoreWithAceAsEleven
+
 object ScoreCalculator {
     fun calculateScore(cards: Cards): Int {
         var score = cards.sumOfScoreWithAceAsOne
 
         repeat(cards.numberOfAce) {
-            if (ScoreValidator.isValidBlackjackScoreWithAceAsEleven(score)) {
+            if (isValidBlackjackScoreWithAceAsEleven(score)) {
                 score += Rank.ACE_MAX - Rank.ACE_MIN
             }
         }
--- src/main/kotlin/blackjack/domain/ScoreValidator.kt
+++ src/main/kotlin/blackjack/domain/ScoreValidator.kt
@@ -2,6 +2,6 @@ package blackjack.domain
 
 object ScoreValidator {
     fun isValidBlackjackScoreWithAceAsEleven(score: Int): Boolean {
-        return score + Rank.ACE_MAX - Rank.ACE_MIN <= BLACK_JACK
+        return score + (Rank.ACE_MAX - Rank.ACE_MIN) <= BLACK_JACK
     }
 }
