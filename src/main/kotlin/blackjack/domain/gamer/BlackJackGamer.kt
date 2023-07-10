e9bd8ae128aae4f07ebe06b033186841dd583660
jonghyunhub <chuwhdgus@gmail.com>
2023-07-10 17:21:47 +0900

    refact : 승자,패배 결정 매서드 인터페이스 삭제

--- src/main/kotlin/blackjack/domain/gamer/BlackJackGamer.kt
+++ src/main/kotlin/blackjack/domain/gamer/BlackJackGamer.kt
@@ -37,8 +37,6 @@ open class BlackJackGamer(val name: String, money: Int = initMoney) {
         return GamerType.PLAYER
     }
 
-    open fun proceedGameRecord(gameRecordType: GameRecordType) = Unit
-
     companion object {
         const val initMoney = 0
     }
