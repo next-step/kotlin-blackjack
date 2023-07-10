40bed68746bf81ac532753232d765f9ed97f2326
jonghyunhub <chuwhdgus@gmail.com>
2023-07-10 18:01:38 +0900

    test : Blackjackgamer는 외부에서 돈을 입력받아서 가지고 있는 돈에 더한다

--- src/test/kotlin/blackjack/domain/gamer/BlackJackGamerTest.kt
+++ src/test/kotlin/blackjack/domain/gamer/BlackJackGamerTest.kt
@@ -8,11 +8,24 @@ class BlackJackGamerTest {
     @Test
     fun `BlackJackGamer는 money를 가진다`() {
         // given
-        val gamerName = "name"
         val money = 0
-        val blackJackGamer = BlackJackGamer(gamerName, money)
+        val blackJackGamer = BlackJackGamer("name", money)
 
         // then
         Assertions.assertThat(blackJackGamer.money).isEqualTo(money)
+    }
+
+    @Test
+    fun `외부에서 돈을 입력받아서 가지고 있는 돈에 더한다`() {
+        // given
+        val money = 0
+        val blackJackGamer = BlackJackGamer("name", money)
+
+        // when
+        val addMoney = 10000
+        blackJackGamer.takeMoney(addMoney)
+
+        // then
+        Assertions.assertThat(blackJackGamer.money).isEqualTo(money + addMoney)
     }
 }
