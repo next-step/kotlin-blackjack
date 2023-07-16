8b63780228b2bfc271439a3da38a5e85509096cc
maenguin-h <maenguin.h@kakaocorp.com>
2023-07-16 21:49:32 +0900

    step4 : Hit 상태 구현

--- src/main/kotlin/blackjack/domain/state/Begin.kt
+++ src/main/kotlin/blackjack/domain/state/Begin.kt
@@ -7,6 +7,6 @@ class Begin {
     fun drawInitialHands(card1: Card, card2: Card): State {
         val deck = Deck(listOf(card1, card2))
         if (deck.isBlackjack()) return Blackjack()
-        return Hit()
+        return Hit(deck)
     }
 }
--- src/main/kotlin/blackjack/domain/state/Hit.kt
+++ src/main/kotlin/blackjack/domain/state/Hit.kt
@@ -1,3 +1,17 @@
 package blackjack.domain.state
 
-class Hit : State
+import blackjack.domain.Card
+import blackjack.domain.Deck
+
+class Hit(private val deck: Deck) : State {
+
+    init {
+        require(deck.size >= 2) { "deck size must be greater than equal to 2" }
+    }
+
+    fun draw(card: Card): State {
+        deck.add(card)
+        if (deck.isBurst()) return Burst()
+        return Hit(deck)
+    }
+}
