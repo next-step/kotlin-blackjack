7888479d145beaadbd1e82f447cb61ea74a3340f
dong <dong.22@kakaoenterprise.com>
2023-11-07 23:13:12 +0900

    test : 코틀린DSL 을 학습한다

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -1,19 +1,36 @@
 package study
 
-import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class DslTest : StringSpec({
-
+class DslTest {
 
     @ValueSource(strings = ["김자바", "최코틀"])
     @ParameterizedTest
     fun nameTest(input: String) {
         val person = introduce {
             name(input)
+            company("양산박")
         }
         person.name shouldBe input
+        person.company shouldBe "양산박"
     }
-})
+}
+
+fun introduce(block: Person.() -> Unit): Person {
+    return Person().apply(block)
+}
+
+data class Person(
+    var name: String = "",
+    var company: String = "",
+) {
+    fun name(value: String) {
+        this.name = value
+    }
+
+    fun company(value: String) {
+        this.company = value
+    }
+}
