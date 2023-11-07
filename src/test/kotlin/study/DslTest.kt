c92606e5409f755f6d235e9541127d47b0ea5fd3
dean <sodp5@naver.com>
2023-11-08 02:48:11 +0900

    불필요한 주석 제거

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -3,21 +3,6 @@ package study
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-/**
- * introduce {
- *   name("홍길동")
- *   company("활빈당")
- *   skills {
- *     soft("A passion for problem solving")
- *     soft("Good communication skills")
- *     hard("Kotlin")
- *   }
- *   languages {
- *     "Korean" level 5
- *     "English" level 3
- *   }
- * }
- */
 class DslTest {
     @Test
     fun personDsl() {
