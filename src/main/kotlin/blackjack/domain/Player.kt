de13911bb4deaa36016f3dc5012d85726e65926f
원동재 <wdj0717@midasit.com>
2023-11-08 20:56:55 +0900

    step2: 블랙잭 - Player Class 카드 리스트 프로퍼티 추가

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,3 +1,3 @@
 package blackjack.domain
 
-class Player(val name: String)
+class Player(val name: String, val cards: List<Card> = emptyList())
--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -13,4 +13,18 @@ class PlayerTest : BehaviorSpec({
             }
         }
     }
+
+    given("플레이어 이름과 카드가 2장 주어지면") {
+        val name = "원동재"
+        val cards = listOf(Card(Suit.SPADE, Rank.ACE), Card(Suit.HEART, Rank.TWO))
+        `when`("플레이어 생성을 하면") {
+            val player = Player(name, cards)
+            then("플레이어의 이름은 원동재가 된다.") {
+                player.name shouldBe "원동재"
+            }
+            then("플레이어의 카드는 2장이 된다.") {
+                player.cards.size shouldBe 2
+            }
+        }
+    }
 })
