a420281bd5081009c6cf2d54d8ffad5730238480
원동재 <wdj0717@midasit.com>
2023-11-08 21:16:31 +0900

    step2: 블랙잭 - TrumpCard 예외 처리

--- src/main/kotlin/blackjack/domain/TrumpCard.kt
+++ src/main/kotlin/blackjack/domain/TrumpCard.kt
@@ -1,3 +1,13 @@
 package blackjack.domain
 
-class TrumpCard(val cards: List<Card> = emptyList())
+@JvmInline
+value class TrumpCard(val cards: List<Card> = emptyList()) {
+    init {
+        require(cards.size == CARD_COUNT) { ERROR_MESSAGE }
+    }
+
+    companion object {
+        private const val CARD_COUNT = 52
+        private const val ERROR_MESSAGE = "카드는 52장이어야 합니다."
+    }
+}
--- src/test/kotlin/blackjack/dsl/BlackJackDslTest.kt
+++ src/test/kotlin/blackjack/dsl/BlackJackDslTest.kt
@@ -4,18 +4,20 @@ import blackjack.domain.Card
 import blackjack.domain.Rank
 import blackjack.domain.Suit
 import blackjack.dsl.BlackJackDsl.initTrumpCard
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 class BlackJackDslTest : BehaviorSpec({
     given("블랙잭 DSL을 사용하면") {
-        `when`("카드를 생성하면") {
-            val trumpCard = initTrumpCard {
-                card(Card(Suit.SPADE, Rank.ACE))
-                card(Card(Suit.HEART, Rank.TWO))
-            }
-            then("카드는 2장이 된다.") {
-                trumpCard.cards.size shouldBe 2
+        `when`("트럼프 카드를 생성하는데 52장이 아니면") {
+            then("예외가 발생한다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    initTrumpCard {
+                        card(Card(Suit.SPADE, Rank.ACE))
+                    }
+                }
+                exception.message shouldBe "카드는 52장이어야 합니다."
             }
         }
     }
