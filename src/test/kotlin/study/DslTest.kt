ed8f038c85e01867fc85fe059cb44a9ec80c9ce7
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-11-09 18:50:09 +0900

    [step1] Languages 구현

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -52,6 +52,18 @@ class DslTest {
 
         assertThat(person.skills).containsExactly("A passion for problem solving", "Good communication skills", "Kotlin")
     }
+
+    @Test
+    fun languages() {
+        val person = introduce {
+            name("조재연")
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        assertThat(person.languages).containsExactlyEntriesOf(mapOf("Korean" to 5, "English" to 3))
+    }
 }
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
@@ -72,10 +84,21 @@ class SkillBuilder {
     fun build(): List<String> = skills.toList()
 }
 
+class LanguagesBuilder {
+    private val languages = mutableMapOf<String, Int>()
+
+    infix fun String.level(level: Int) {
+        languages[this] = level
+    }
+
+    fun build(): Map<String, Int> = languages.toMap()
+}
+
 class PersonBuilder {
     private lateinit var name: String
     private var company: String? = null
     private val skills = SkillBuilder()
+    private val languages = LanguagesBuilder()
 
     fun name(value: String) {
         name = value
@@ -89,8 +112,12 @@ class PersonBuilder {
         skills.apply(block)
     }
 
+    fun languages(block: LanguagesBuilder.() -> Unit) {
+        languages.apply(block)
+    }
+
     fun build(): Person {
-        return Person(name, company, skills.build())
+        return Person(name, company, skills.build(), languages.build())
     }
 }
 
@@ -98,4 +125,5 @@ data class Person(
     val name: String,
     val company: String?,
     val skills: List<String>,
+    val languages: Map<String, Int>
 )
