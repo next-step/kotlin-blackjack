8f2c6cf69998b86a03e0b1063717198d01669792
seongmo-jeon <oeeen3@gmail.com>
2023-11-11 16:23:59 +0900

    feat: 카드는 A, 2-9, J, Q, K 로 구성된다.

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -1,4 +1,11 @@
 package blackjack.domain
 
-class Card(val character: Char, val shape: String) {
+class Card(private val character: Char, val shape: String) {
+    init {
+        require(VALID_CHARACTER.contains(character)) { "유효한 카드 문자가 아닙니다." }
+    }
+
+    companion object {
+        private val VALID_CHARACTER = setOf('A', 'J', 'Q', 'K', '2', '3', '4', '5', '6', '7', '8', '9')
+    }
 }
--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -1,23 +1,37 @@
 package blackjack.domain
 
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.lang.IllegalArgumentException
 
 class CardTest {
 
     @ParameterizedTest
     @MethodSource("createCardData")
-    fun `카드 생성`(pair: Pair<Char, String>) {
+    fun `카드 전체 생성`(pair: Pair<Char, String>) {
         assertDoesNotThrow { Card(pair.first, pair.second) }
+    }
+
+    @Test
+    fun `카드가 A, J, Q, K, 2-9 까지의 문자가 아니면 IllegalArgumentException 이 발생한다`() {
+        assertThrows<IllegalArgumentException> { Card('T', "SPADE") }
     }
 
     companion object {
         @JvmStatic
         fun createCardData(): List<Arguments> {
-            return listOf(Arguments.of('A' to "SPADE"), Arguments.of('2' to "CLUB"))
+            val shapes = listOf("HEART", "CLUB", "DIAMOND", "SPADE")
+            val characters = listOf('A', 'K', 'Q', 'J') + ('2'..'9')
+            return shapes.flatMap { suit ->
+                (characters).map { num ->
+                    Arguments.of(num to suit)
+                }
+            }
+
         }
     }
-
 }
