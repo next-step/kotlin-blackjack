822438eb0012fe174fb0577d663df262f8a93782
ALbertIM0427 <guang7226961@naver.com>
2023-11-11 20:53:11 +0900

    feat: languages 구현

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -43,12 +43,49 @@ class DslTest {
     @Test
     fun skills() {
         val person = introduce {
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+        person.skills.size shouldBe 3
+        person.skills[0].skillType shouldBe SkillType.SOFT
+        person.skills[0].message shouldBe "A passion for problem solving"
+        person.skills[1].skillType shouldBe SkillType.SOFT
+        person.skills[1].message shouldBe "Good communication skills"
+        person.skills[2].skillType shouldBe SkillType.HARD
+        person.skills[2].message shouldBe "Kotlin"
+    }
+
+    @Test
+    fun languages() {
+        val person = introduce {
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+        person.languages.size shouldBe 2
+        person.languages[0].name shouldBe "Korean"
+        person.languages[0].level shouldBe 5
+        person.languages[1].name shouldBe "English"
+        person.languages[1].level shouldBe 3
+    }
+
+    @Test
+    fun `전체 테스트`() {
+        val person = introduce {
             name("홍길동")
             company("활빈당")
             skills {
                 soft("A passion for problem solving")
                 soft("Good communication skills")
                 hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
             }
         }
         person.name shouldBe "홍길동"
@@ -60,17 +97,13 @@ class DslTest {
         person.skills[1].message shouldBe "Good communication skills"
         person.skills[2].skillType shouldBe SkillType.HARD
         person.skills[2].message shouldBe "Kotlin"
-    }
-
-    @Test
-    fun pair() {
-        val pair1 = Pair(1, "one")
-        val pair2 = 1 to "one"
-        val pair3 = 1 of "one"
+        person.languages.size shouldBe 2
+        person.languages[0].name shouldBe "Korean"
+        person.languages[0].level shouldBe 5
+        person.languages[1].name shouldBe "English"
+        person.languages[1].level shouldBe 3
     }
 }
-
-infix fun Int.of(value: String): Pair<Int, String> = Pair(this, value)
 
 fun introduce(block: Person.() -> Unit): Person = Person().apply(block)
 
@@ -78,6 +111,7 @@ class Person {
     lateinit var name: String
     lateinit var company: String
     lateinit var skills: List<Skill>
+    lateinit var languages: List<Language>
 
     fun name(value: String) {
         name = value
@@ -87,24 +121,41 @@ class Person {
         company = value
     }
 
-    fun skills(block: SkillsBuilder.() -> Unit) = SkillsBuilder().apply(block).build().let { skills = it }
+    fun skills(block: SkillsBuilder.() -> Unit) = SkillsBuilder()
+        .apply(block).build().let { skills = it }
+
+    fun languages(block: LanguagesBuilder.() -> Unit) = LanguagesBuilder().apply(block).build().let { languages = it }
+}
+
+class LanguagesBuilder {
+    private val languagesList = mutableListOf<Language>()
+
+    infix fun String.level(level: Int) {
+        languagesList.add(Language(this, level))
+    }
+
+    fun build(): List<Language> = languagesList
 }
 
 class SkillsBuilder {
     private val skillsList = mutableListOf<Skill>()
 
     fun soft(value: String) {
-        skillsList.add(Skill().apply {
-            skillType = SkillType.SOFT
-            message = value
-        })
+        skillsList.add(
+            Skill().apply {
+                skillType = SkillType.SOFT
+                message = value
+            }
+        )
     }
 
     fun hard(value: String) {
-        skillsList.add(Skill().apply {
-            skillType = SkillType.HARD
-            message = value
-        })
+        skillsList.add(
+            Skill().apply {
+                skillType = SkillType.HARD
+                message = value
+            }
+        )
     }
 
     fun build(): List<Skill> = skillsList
@@ -114,6 +165,8 @@ class Skill {
     lateinit var skillType: SkillType
     lateinit var message: String
 }
+
+class Language(val name: String, val level: Int)
 
 enum class SkillType {
     SOFT, HARD;
