44baad904e73ca81e94ed103362f0c061992f9db
dokdogalmaegi <clik202012@gmail.com>
2023-11-12 18:37:10 +0900

    Add: Skills

--- src/test/kotlin/DslTest.kt
+++ src/test/kotlin/DslTest.kt
@@ -1,3 +1,4 @@
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -23,7 +24,7 @@ class DslTest {
     @ValueSource(strings = ["홍길동", "dokdogalmaegi"])
     @ParameterizedTest
     fun name(name: String) {
-        val person = introduce {
+        val person: Person = introduce {
             name(name)
         }
 
@@ -32,13 +33,60 @@ class DslTest {
 
     @Test
     fun company() {
-        val person = introduce {
+        val person: Person = introduce {
             name("홍길동")
             company("활빈당")
         }
 
         person.name shouldBe "홍길동"
         person.company shouldBe "활빈당"
+    }
+
+    @ValueSource(strings = ["Kotlin", "Go", "Java"])
+    @ParameterizedTest
+    fun hard(skill: String) {
+        val person: Person = introduce {
+            name("홍길동")
+            company("활빈당")
+            skills {
+                hard(skill)
+            }
+        }
+
+        person.skills shouldContainExactly listOf(Skill(SkillType.Hard, skill))
+    }
+
+    @ValueSource(strings = ["A passion for problem solving", "Good communication skills", "Good voice"])
+    @ParameterizedTest
+    fun soft(skill: String) {
+        val person: Person = introduce {
+            name("홍길동")
+            company("활빈당")
+            skills {
+                soft(skill)
+            }
+        }
+
+        person.skills shouldContainExactly listOf(Skill(SkillType.Soft, skill))
+    }
+
+    @Test
+    fun hardAndSoft() {
+        val person: Person = introduce {
+            name("홍길동")
+            company("활빈당")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+
+        person.skills shouldContainExactly listOf(
+            Skill(SkillType.Soft, "A passion for problem solving"),
+            Skill(SkillType.Soft, "Good communication skills"),
+            Skill(SkillType.Hard, "Kotlin"),
+        )
     }
 
     @Test
@@ -55,19 +103,45 @@ fun introduce(block: PersonBuilder.() -> Unit): Person {
     return PersonBuilder().apply(block).build()
 }
 
-data class Person(val name: String, val company: String?)
+data class Person(val name: String, val company: String?, val skills: List<Skill>?)
 
 class PersonBuilder {
-   private lateinit var name: String
-   private var company: String? = null
+    private lateinit var name: String
+    private var company: String? = null
+    private var skills: List<Skill>? = null
 
     fun name(value: String) {
-       name = value
-   }
+        name = value
+    }
 
-   fun company(value: String) {
-       company = value
-   }
+    fun company(value: String) {
+        company = value
+    }
 
-    fun build(): Person = Person(this.name, this.company)
+    fun skills(block: SkillBuilder.() -> Unit) {
+        this.skills = SkillBuilder().apply(block).build()
+    }
+
+    fun build(): Person = Person(this.name, this.company, skills)
+}
+
+enum class SkillType {
+    Hard,
+    Soft
+}
+
+data class Skill(val skill: SkillType, val content: String)
+
+class SkillBuilder {
+    private val skills: MutableList<Skill> = mutableListOf()
+
+    fun hard(content: String) = setSkills(SkillType.Hard, content)
+
+    fun soft(content: String) = setSkills(SkillType.Soft, content)
+
+    private fun setSkills(skill: SkillType, content: String) {
+        skills.add(Skill(skill, content))
+    }
+
+    fun build(): List<Skill> = this.skills
 }
