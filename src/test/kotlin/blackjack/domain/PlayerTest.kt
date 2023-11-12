8dce459bac668d7a8bacd5e6af967069d851095d
wooddy.kim <wooddy.kim@kakaocorp.com>
2023-11-12 16:48:37 +0900

    test: 카드 상태에 따른 플레이어 검증

--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -1,17 +1,43 @@
 package blackjack.domain
 
 import blackjack.test.TestDeckGenerator
-import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
-class PlayerTest : ExpectSpec({
+class PlayerTest : BehaviorSpec({
 
-    expect("플레이어는 카드를 한 장 받을 수 있다.") {
-        val dealer = Dealer(TestDeckGenerator.generate())
-        val player = Player(Nickname("name"), dealer)
+    given("ACE, JACK 카드룰 순서대로 발급하는 덱이 주어졌을 때") {
+        val nickname = Nickname("플레이어")
+        val getDeck: () -> Deck = { TestDeckGenerator.generate(Symbol.SPADE of Rank.ACE, Symbol.SPADE of Rank.JACK) }
 
-        player.receiveCard()
+        `when`("플레이어가 카드를 한번 발급하면") {
+            val player = Player(nickname, Dealer(getDeck()))
 
-        player.cards.size shouldBe 1
+            then("총 점수는 11점이다.") {
+                player.receiveCard()
+                player.calculateScore() shouldBe 11
+            }
+        }
+
+        `when`("플레이어가 카드를 두번 발급하면") {
+            val player = Player(nickname, Dealer(getDeck()))
+
+            then("총 점수는 21점이다.") {
+                player.receiveCard()
+                player.receiveCard()
+                player.calculateScore() shouldBe 21
+            }
+        }
+
+        `when`("플레이어가 카드를 세번 발급하면") {
+            val player = Player(nickname, Dealer(getDeck()))
+
+            then("예외가 발생한다.") {
+                player.receiveCard()
+                player.receiveCard()
+                player.receiveCard()
+                player.calculateScore() shouldBe 12
+            }
+        }
     }
 })
--- src/test/kotlin/blackjack/test/TestDeckGenerator.kt
+++ src/test/kotlin/blackjack/test/TestDeckGenerator.kt
@@ -4,11 +4,18 @@ import blackjack.domain.*
 
 object TestDeckGenerator {
 
-    fun generate(): Deck = deck(FakeShuffleStrategy) {
+    fun generate(vararg cards: Card): Deck = deck(FakeShuffleStrategy) {
+        val repeatedCards = generateSequence { cards.iterator() }.flatMap { it.asSequence() }.take(52)
+        repeatedCards.forEach { +it }
+    }
+
+    private fun generateFullDeck(): List<Card> {
+        val fullDeck = mutableListOf<Card>()
         for (suit in Symbol.values()) {
             for (value in Rank.values()) {
-                +(suit of value)
+                fullDeck.add(Card(suit, value))
             }
         }
+        return fullDeck
     }
 }
