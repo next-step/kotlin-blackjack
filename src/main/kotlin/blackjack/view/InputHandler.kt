70c91b1365afe629a9e4c9ac5747286f615c90a0
ALbertIM0427 <guang7226961@naver.com>
2023-11-12 18:47:54 +0900

    feat: 카드를 더 받는지 명령을 입력받는 기능 구현

--- src/main/kotlin/blackjack/view/InputHandler.kt
+++ src/main/kotlin/blackjack/view/InputHandler.kt
@@ -6,4 +6,9 @@ class InputHandler {
         println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
         return readlnOrNull() ?: throw IllegalArgumentException("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
     }
+
+    fun askForOneMore(playerName: String): String {
+        println("{$playerName}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        return readlnOrNull() ?: throw IllegalArgumentException("잘못된 입력입니다.")
+    }
 }
