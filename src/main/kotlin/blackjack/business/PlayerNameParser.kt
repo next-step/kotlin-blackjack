5643597606aa24deeaaecff0241f5a7bdef74e91
ALbertIM0427 <guang7226961@naver.com>
2023-11-12 16:03:00 +0900

    feat: 입력받은 사람의 이름을 파싱하는 기능 구현

---
+++ src/main/kotlin/blackjack/business/PlayerNameParser.kt
@@ -0,0 +1,9 @@
+package blackjack.business
+
+object PlayerNameParser {
+    private const val DELIMITER = ","
+
+    fun parse(input: String): List<String> {
+        return input.split(DELIMITER)
+    }
+}
