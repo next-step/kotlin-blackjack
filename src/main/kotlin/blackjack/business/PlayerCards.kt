7a17da419df301a7d5101af7c3a9fcfc1119a208
ALbertIM0427 <guang7226961@naver.com>
2023-11-12 17:33:15 +0900

    feat: 플레이어의 카드의 합을 구하는 기능 구현

--- src/main/kotlin/blackjack/business/PlayerCards.kt
+++ src/main/kotlin/blackjack/business/PlayerCards.kt
@@ -12,4 +12,14 @@ class PlayerCards {
     operator fun get(i: Int): Card {
         return _cards[i]
     }
+
+    fun sum(): Int {
+        var sum = _cards.sumOf { it.rank.score }
+        var aceCount = _cards.count { it.rank == Rank.ACE }
+        while (sum <= 11 && aceCount > 0) {
+            sum += 10
+            aceCount.minus(1)
+        }
+        return sum
+    }
 }
--- src/test/kotlin/blackjack/business/PlayerCardsTest.kt
+++ src/test/kotlin/blackjack/business/PlayerCardsTest.kt
@@ -2,6 +2,10 @@ package blackjack.business
 
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class PlayerCardsTest {
 
@@ -16,5 +20,75 @@ class PlayerCardsTest {
         // then
         playerCards.size shouldBe 1
         playerCards[0] shouldBe Card(Suit.SPADE, Rank.ACE)
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCards")
+    fun `카드의 합을 구한다`(cards: List<Card>, expected: Int) {
+        // given
+        val playerCards = PlayerCards()
+        cards.forEach { playerCards.add(it) }
+
+        // when
+        val actual = playerCards.sum()
+
+        // then
+        actual shouldBe expected
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideCards(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    listOf(
+                        Card(Suit.SPADE, Rank.ACE), Card(Suit.SPADE, Rank.EIGHT)
+                    ),
+                    19
+                ),
+                Arguments.of(
+                    listOf(
+                        Card(Suit.SPADE, Rank.ACE),
+                        Card(Suit.SPADE, Rank.ACE),
+                        Card(Suit.SPADE, Rank.EIGHT),
+                        Card(Suit.SPADE, Rank.ACE)
+                    ),
+                    21
+                ),
+                Arguments.of(
+                    listOf(
+                        Card(Suit.SPADE, Rank.ACE),
+                        Card(Suit.SPADE, Rank.EIGHT),
+                        Card(Suit.SPADE, Rank.TWO)
+                    ),
+                    21
+                ),
+                Arguments.of(
+                    listOf(
+                        Card(Suit.SPADE, Rank.ACE),
+                        Card(Suit.SPADE, Rank.EIGHT),
+                        Card(Suit.SPADE, Rank.TWO),
+                        Card(Suit.SPADE, Rank.TEN)
+                    ),
+                    21
+                ),
+                Arguments.of(
+                    listOf(
+                        Card(Suit.SPADE, Rank.ACE),
+                        Card(Suit.SPADE, Rank.EIGHT),
+                        Card(Suit.SPADE, Rank.THREE)
+                    ),
+                    12
+                ),
+                Arguments.of(
+                    listOf(
+                        Card(Suit.SPADE, Rank.ACE),
+                        Card(Suit.SPADE, Rank.NINE),
+                        Card(Suit.SPADE, Rank.TEN)
+                    ),
+                    20
+                ),
+            )
+        }
     }
 }
