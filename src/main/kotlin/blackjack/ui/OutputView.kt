af05348cf5a3662177afc6b6ae8ee4fd8378f5a0
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-14 03:01:43 +0900

    [refactoring] step2: 잘못된 print문 수정

--- src/main/kotlin/blackjack/ui/OutputView.kt
+++ src/main/kotlin/blackjack/ui/OutputView.kt
@@ -6,14 +6,10 @@ object OutputView {
     }
 
     fun printParticipantsName(names: String) {
-        val participants = names.split(NAME_DELIMITERS)
-        val participantsCount = participants.count()
-
-        val divideCardString = PRINT_DIVIDE_CARD_TO_PARTICIPANTS.format(names, participantsCount)
+        val divideCardString = PRINT_DIVIDE_CARD_TO_PARTICIPANTS.format(names)
         println(divideCardString)
     }
 
     private const val ENTER_PARTICIPANTS_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
-    private const val NAME_DELIMITERS = ", "
-    private const val PRINT_DIVIDE_CARD_TO_PARTICIPANTS = "%s 에게 %d장의 카드를 나누었습니다."
+    private const val PRINT_DIVIDE_CARD_TO_PARTICIPANTS = "%s 에게 각각 2장의 카드를 나누었습니다."
 }
