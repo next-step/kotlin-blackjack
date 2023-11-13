8f4e18b8eba640d1a073746d5b40f7578d1b5eff
yeongun <gjduddns@gmail.com>
2023-11-13 22:34:05 +0900

    feat: 게임이 시작하면 플레이어들이 카드 2장을 나눠 가질 수 있도록 구현

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,6 +1,12 @@
 package blackjack.domain
 
 data class Players(val players: List<Player>) {
+    fun initCard(deck: Deck): Players {
+        val newPlayers = players.map {
+            it.copy(hand = Hand(deck.init()))
+        }
+        return Players(newPlayers)
+    }
 
     companion object {
         fun init(names: List<String>): Players {
