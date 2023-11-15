57cac3274dafe094908ea15eb8eb69067953cd78
seongmo-jeon <seongmo.jeon@navercorp.com>
2023-11-15 23:36:49 +0900

    refactor: Person 클래스에 불필요한 nullable type 제거

--- src/main/kotlin/dsl/Person.kt
+++ src/main/kotlin/dsl/Person.kt
@@ -3,6 +3,6 @@ package dsl
 data class Person(
     val name: String,
     val company: String?,
-    val skills: Skills?,
-    val languages: Languages?
+    val skills: Skills = Skills(listOf(), listOf()),
+    val languages: Languages = Languages(listOf())
 )
