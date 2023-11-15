19a4d3269d4f80c585d36f052a331424fdb36442
dong <dong.22@kakaoenterprise.com>
2023-11-15 16:45:55 +0900

    refactor : ace 를 1,11 로 해석하게 수정

--- src/test/kotlin/blackjack/model/CardsTest.kt
+++ src/test/kotlin/blackjack/model/CardsTest.kt
@@ -8,13 +8,36 @@ class CardsTest : StringSpec({
     "Cards 의 모든 카드의 점수의 합을 반환해야 한다" {
         val actual = Cards(
             listOf(
-                Card(Suit.SPADE, CardRank.ACE),
                 Card(Suit.CLOVER, CardRank.THREE),
                 Card(Suit.DIAMOND, CardRank.FIVE),
                 Card(Suit.HEART, CardRank.EIGHT),
                 Card(Suit.HEART, CardRank.KING)
             )
         ).totalScore()
-        actual shouldBe (1 + 3 + 5 + 8 + 10)
+        actual shouldBe (3 + 5 + 8 + 10)
+    }
+
+    "합계점수가 21를 초과 하는 경우, ACE 가 1로 인식되어야 한다" {
+        val actual = Cards(
+            listOf(
+                Card(Suit.HEART, CardRank.ACE),
+                Card(Suit.DIAMOND, CardRank.FIVE),
+                Card(Suit.HEART, CardRank.EIGHT),
+                Card(Suit.HEART, CardRank.KING)
+            )
+        ).totalScore()
+        actual shouldBe (1 + 5 + 8 + 10)
+    }
+
+    "합계점수가 21를 이하인 경우, ACE 가 11로 인식되어야 한다" {
+        val actual = Cards(
+            listOf(
+                Card(Suit.HEART, CardRank.ACE),
+                Card(Suit.HEART, CardRank.THREE),
+                Card(Suit.DIAMOND, CardRank.FIVE),
+            )
+        ).totalScore()
+        actual shouldBe (11 + 3 + 5)
+
     }
 })
