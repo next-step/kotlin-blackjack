7f36e6a010a2730ea6840d8b673c5cda550ea8d1
yeongun <gjduddns@gmail.com>
2023-11-15 23:47:26 +0900

    refactor: RandomDeck 카드를 처음 생성할 때 shuffle을 한 뒤, 카드를 뽑는 방식으로 변경

--- src/main/kotlin/blackjack/domain/RandomDeck.kt
+++ src/main/kotlin/blackjack/domain/RandomDeck.kt
@@ -1,8 +1,12 @@
 package blackjack.domain
 
+import java.util.Random
+
 class RandomDeck private constructor(
     private val cards: List<Card> = listOf()
 ) : Deck {
+    private val random = Random()
+
     override fun init(): List<Card> {
         return listOf(getNewCard(), getNewCard())
     }
@@ -11,10 +15,11 @@ class RandomDeck private constructor(
         return getNewCard()
     }
 
-    private fun getNewCard() = cards.shuffled()
-        .first()
+    private fun getNewCard(): Card = cards[random.nextInt(ONE_DECK_CARD_COUNT)]
 
     companion object {
+        private const val ONE_DECK_CARD_COUNT = 52
+
         fun from(): Deck {
             val cards = CardSuit.values()
                 .flatMap { cardSuit ->
@@ -22,7 +27,7 @@ class RandomDeck private constructor(
                         .map { Card(cardSuit, it) }
                         .toList()
                 }
-                .toList()
+                .shuffled()
             return RandomDeck(cards)
         }
     }
