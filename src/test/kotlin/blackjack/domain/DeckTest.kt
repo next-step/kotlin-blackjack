047538c9c339a3a8e7a8c877a76cde438214f5e0
jihyun-kim13 <jihyun.kim13@linecorp.com>
2023-11-17 00:12:21 +0900

    test: add Deck tests

--- src/test/kotlin/blackjack/domain/DeckTest.kt
+++ src/test/kotlin/blackjack/domain/DeckTest.kt
@@ -7,11 +7,30 @@ import org.junit.jupiter.api.Test
 
 class DeckTest {
     @Test
-    fun `덱 하나의 초기 카드 수는 52장이다`() {
+    fun `덱 하나에는 서로 다른 카드 52장이 들어있다`() {
         val deck = Deck()
+        val actual = mutableSetOf<Card>()
 
-        val cards = deck.cards
+        repeat(52) {
+            actual.add(deck.draw())
+        }
 
-        assertThat(cards).hasSize(52)
+        assertThat(actual).hasSize(52)
+    }
+
+    @Test
+    fun `덱에 있는 카드를 다 쓰면 52장의 카드를 새로 충전한다`() {
+        val deck = Deck()
+        val actualSet = mutableSetOf<Card>()
+        val actualList = mutableListOf<Card>()
+
+        repeat(53) {
+            val card = deck.draw()
+            actualSet.add(card)
+            actualList.add(card)
+        }
+
+        assertThat(actualSet).hasSize(52)
+        assertThat(actualList).hasSize(53)
     }
 }
