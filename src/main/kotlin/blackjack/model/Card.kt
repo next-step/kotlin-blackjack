985508038182cac7e32143eb13b15c6588dda21c
dong <dong.22@kakaoenterprise.com>
2023-11-16 12:47:50 +0900

    refactor : 자료구조를 1차원 Map 구조로 수정

--- src/main/kotlin/blackjack/model/Card.kt
+++ src/main/kotlin/blackjack/model/Card.kt
@@ -10,22 +10,22 @@ data class Card(
 
         private val random = Random(System.currentTimeMillis())
 
-        private val CARDS: Map<Suit, Map<CardRank, Card>> by lazy {
-            Suit.values().associateWith { suit ->
-                CardRank.values().associateWith { rank ->
-                    Card(suit, rank)
+        private val CARDS: Map<Pair< Suit,CardRank>, Card> by lazy {
+            Suit.values().flatMap { suit ->
+                CardRank.values().map { rank ->
+                    (suit to rank) to Card(suit, rank)
                 }
-            }
+            }.toMap()
         }
 
         fun of(): Card {
-            val s = Suit.values()[random.nextInt(0, Int.MAX_VALUE) % Suit.values().size]
-            val r = CardRank.values()[random.nextInt(0, Int.MAX_VALUE) % (CardRank.values().size)]
-            return CARDS[s]!![r]!!
+            val suit = Suit.values()[random.nextInt(0, Int.MAX_VALUE) % Suit.values().size]
+            val cardRank = CardRank.values()[random.nextInt(0, Int.MAX_VALUE) % (CardRank.values().size)]
+            return requireNotNull(CARDS[s to r]) {"입력된 suit=[$s] , cardRank=[$r] 는 찾을수 없습니다"}
         }
 
         fun of(suit: Suit, cardRank: CardRank): Card {
-            return CARDS[suit]!![cardRank]!!
+            return requireNotNull(CARDS[suit to cardRank]) {"입력된 suit=[$suit] , cardRank=[$cardRank] 는 찾을수 없습니다"}
         }
     }
 }
