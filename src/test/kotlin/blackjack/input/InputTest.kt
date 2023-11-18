cff3400a3b1400eda0082b0ad1c8908756fa1fde
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-19 05:18:46 +0900

    [refactoring] step2: 누락 테스트 코드 수정

--- src/test/kotlin/blackjack/input/InputTest.kt
+++ src/test/kotlin/blackjack/input/InputTest.kt
@@ -7,21 +7,21 @@ import io.kotest.core.spec.style.StringSpec
 class InputTest : StringSpec({
     "구분자인 ,가 아닌 다른 문자로 구분했을 때" {
         val input = "pita;haero,dol"
-        shouldThrow<IllegalStateException> {
+        shouldThrow<IllegalArgumentException> {
             InputView.checkParticipants(input)
         }
     }
 
     "참가자가 ,로 시작하였을 때" {
         val input = ",pita,dol"
-        shouldThrow<IllegalStateException> {
+        shouldThrow<IllegalArgumentException> {
             InputView.checkParticipants(input)
         }
     }
 
     "참가자가 ,로 끝났을 때" {
         val input = "haero,dol,"
-        shouldThrow<IllegalStateException> {
+        shouldThrow<IllegalArgumentException> {
             InputView.checkParticipants(input)
         }
     }
