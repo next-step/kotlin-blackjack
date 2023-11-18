d0f6baa38b3be894baef6afddb978403e29cd9dc
Celina <luv.mins99@gmail.com>
2023-11-18 21:53:54 +0900

    feat: 플레이어 수 제한을 변경한다
    - 2명 -> 1명 ~ 8명까지로 변경

--- src/main/kotlin/blackjack/view/InputView.kt
+++ src/main/kotlin/blackjack/view/InputView.kt
@@ -1,9 +1,11 @@
 package blackjack.view
 
 object InputView {
-    private const val INPUT_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     private const val DELIMITER = ","
-    private const val INVALID_PLAYER_NAME_EXCEPTION = "플레이어 이름을 구분자 기준으로 두 명 입력하세요."
+    private const val MINIMUM_PLAYER = 1
+    private const val MAXIMUM_PLAYER = 8
+    private const val INPUT_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val INVALID_PLAYER_NAME_EXCEPTION = "플레이어 수는 구분자 기준 ${MINIMUM_PLAYER}명에서 ${MAXIMUM_PLAYER}명까지 가능합니다"
 
     fun inputPlayers(): List<String> {
         println(INPUT_PLAYER_NAME_MESSAGE)
@@ -12,7 +14,7 @@ object InputView {
 
     private fun validateInputPlayer(players: String): List<String> {
         val result = players.split(DELIMITER).map { it.trim() }
-        require(result.size == 2) { INVALID_PLAYER_NAME_EXCEPTION }
+        require(result.size in MINIMUM_PLAYER..MAXIMUM_PLAYER) { INVALID_PLAYER_NAME_EXCEPTION }
         return result
     }
 }
