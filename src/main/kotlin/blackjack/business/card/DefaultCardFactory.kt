c5292f7cdcd2be04b88d686b39d7f5f183753889
ALbertIM0427 <guang7226961@naver.com>
2023-11-19 12:25:15 +0900

    refactor: map 함수로 로직 변경

--- src/main/kotlin/blackjack/business/card/DefaultCardFactory.kt
+++ src/main/kotlin/blackjack/business/card/DefaultCardFactory.kt
@@ -1,16 +1,10 @@
 package blackjack.business.card
 
 class DefaultCardFactory : CardFactory {
-    private val allCards: List<Card>
-
-    init {
-        val cards = mutableListOf<Card>()
-        for (suit in Suit.values()) {
-            for (rank in Rank.values()) {
-                cards.add(Card(suit, rank))
-            }
+    private val allCards: List<Card> = Suit.values().flatMap { suit ->
+        Rank.values().map { rank ->
+            Card(suit, rank)
         }
-        allCards = cards
     }
 
     override fun getCards(): List<Card> = allCards
