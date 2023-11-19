768e5e88db9a742b03a9b9c93f21d4d786a46143
dong <dong.22@kakaoenterprise.com>
2023-11-20 01:57:47 +0900

    learn : 미완성 학습코드를 완성시킴 : 방어적 복사 관련 예제코드

--- src/test/kotlin/study/DefensiveCopyLearn.kt
+++ src/test/kotlin/study/DefensiveCopyLearn.kt
@@ -1,21 +1,46 @@
 package study
 
-import blackjack.model.Card
-import blackjack.model.Cards
-import blackjack.model.pack.ShuffledPack
 import blackjack.view.Console.present
 import io.kotest.core.spec.style.StringSpec
 
 class DefensiveCopyLearn : StringSpec({
-    "sdf" {
 
-        val list = mutableListOf<Card>()
-        list.add(ShuffledPack.pickCard())
-        list.add(ShuffledPack.pickCard())
-        list.add(ShuffledPack.pickCard())
-        val cards = Cards(list)
-        println("BEFORE : ${cards.present()}")
+    "방어적 복사를 하지 않았을 때의 문제가 되는 코드 예제" {
+        val list = mutableListOf<String>()
+        list.add("goodNight")
+        list.add("goodMorning")
+        list.add("niceYoMeetYou")
+        val nonDefensiveClass = NonDefensiveClass(list)
+        println("BEFORE : ${nonDefensiveClass.present()}")
         list.clear()
-        println("AFTER : ${cards.present()}")
+        println("AFTER : ${nonDefensiveClass.present()}")
+    }
+
+    "방어적 복사를 적용한 코드 예제" {
+        val list = mutableListOf<String>()
+        list.add("goodNight")
+        list.add("goodMorning")
+        list.add("niceYoMeetYou")
+        val nonDefensiveClass = NonDefensiveClass(list)
+        println("BEFORE : ${nonDefensiveClass.present()}")
+        list.clear()
+        println("AFTER : ${nonDefensiveClass.present()}")
     }
 })
+
+class NonDefensiveClass(
+    val strings: MutableList<String>
+) {
+    fun present(): String {
+        return strings.toString()
+    }
+}
+
+class DefensiveClass {
+    private val _strings: MutableList<String> = strings.toMutableList()
+    val strings: List<String> get() = _strings.toList()
+
+    fun present(): String {
+        return strings.toString()
+    }
+}
