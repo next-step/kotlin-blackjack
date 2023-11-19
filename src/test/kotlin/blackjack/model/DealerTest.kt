2e6ed517ceac74be53fb9f4492c9e37bf751ff73
dong <dong.22@kakaoenterprise.com>
2023-11-20 02:20:56 +0900

    test : 딜러 클래스를 추가하고, 딜러클래스에서 책임저야할 역할에 대해 테스트코드 작성

---
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -0,0 +1,18 @@
+package blackjack.model
+
+import io.kotest.core.spec.style.StringSpec
+
+class DealerTest : StringSpec({
+
+    "딜러는 처음에 받은 2장의 카드 점수 합계가 16이하이면 반드시 1장의 카드를 추가로 받아야 한다" {
+        TODO()
+    }
+
+    "딜러는 카드의 점수 합계가 17점 이상이면 추가로 받을 수 없다" {
+        TODO()
+    }
+
+    "딜러가 21을 초과하면 그 시점까지 남아 있던 플레이어들은 가지고 있는 패에 상관 없이 승리" {
+
+    }
+})
