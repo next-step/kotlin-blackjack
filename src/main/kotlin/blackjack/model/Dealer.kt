2f9e2f2ddf960bfb703e1021ce16f8b26220815b
dong <dong.22@kakaoenterprise.com>
2023-11-20 03:09:26 +0900

    refactor : 딜러의 역할에 대한 테스트코드 작성

--- src/main/kotlin/blackjack/model/Dealer.kt
+++ src/main/kotlin/blackjack/model/Dealer.kt
@@ -1,5 +1,12 @@
 package blackjack.model
 
+import blackjack.model.pack.Pack
+
 data class Dealer(
     val cards: Cards = Cards.emptyCards(),
-)
+) {
+    fun run(pack: Pack) {
+        cards.totalScore() > 17
+        TODO()
+    }
+}
--- src/test/kotlin/blackjack/model/DealerTest.kt
+++ src/test/kotlin/blackjack/model/DealerTest.kt
@@ -1,14 +1,34 @@
 package blackjack.model
 
+import blackjack.model.pack.ShuffledPack
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 
 class DealerTest : StringSpec({
 
     "딜러는 처음에 받은 2장의 카드 점수 합계가 16이하이면 반드시 1장의 카드를 추가로 받아야 한다" {
-        TODO()
+        val dealer = Dealer(
+            Cards(
+                listOf(
+                    Card.of(Suit.CLOVER, CardRank.KING),
+                    Card.of(Suit.HEART, CardRank.SIX),
+                )
+            )
+        )
+        dealer.run(ShuffledPack)
+        dealer.cards.count() shouldBe 3
     }
 
     "딜러는 카드의 점수 합계가 17점 이상이면 추가로 받을 수 없다" {
-        TODO()
+        val dealer = Dealer(
+            Cards(
+                listOf(
+                    Card.of(Suit.CLOVER, CardRank.KING),
+                    Card.of(Suit.HEART, CardRank.SEVEN),
+                )
+            )
+        )
+        dealer.run(ShuffledPack)
+        dealer.cards.count() shouldBe 2
     }
 })
