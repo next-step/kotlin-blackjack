399e103bd210e6941e8e2b8e8690a59aea0d5812
bongbong <kbk5190@naver.com>
2023-11-20 08:12:00 +0900

    [step2] chore: 사용하지 않는 코드 삭제

--- src/main/kotlin/blackjack/domain/card/CardDeck.kt
+++
@@ -1,5 +0,0 @@
-package blackjack.domain.card
-
-data class CardDeck(
-    val cards: List<Card>,
-)
--- src/main/kotlin/blackjack/domain/player/PlayerName.kt
+++ src/main/kotlin/blackjack/domain/player/PlayerName.kt
@@ -5,7 +5,6 @@ value class PlayerName(
     val value: String,
 ) {
     init {
-        value.isBlank()
         require(value.isNotBlank()) {
             "플레이어 이름은 공백이 될 수 없습니다"
         }
