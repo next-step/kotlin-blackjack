93e10b12d7841214dcfaa86c65f5eab01a2aba31
Celina <luv.mins99@gmail.com>
2023-11-21 00:25:09 +0900

    test: 테스트 코드 추가

---
+++ src/test/kotlin/blackjack/model/card/CardTest.kt
@@ -0,0 +1,22 @@
+package blackjack.model.card
+
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+import org.junit.jupiter.api.Test
+
+class CardTest {
+
+    @Test
+    fun `카드 번호와 모양을 가지는 카드 객체를 생성한다`() {
+        val card = Card(CardNumber.ACE, CardSuit.다이아몬드)
+
+        card.shouldBeInstanceOf<Card>()
+    }
+
+    @Test
+    fun `카드 객체는 카드의 점수를 가진다`() {
+        val card = Card(CardNumber.ACE, CardSuit.다이아몬드)
+
+        card.score shouldBe 11
+    }
+}
