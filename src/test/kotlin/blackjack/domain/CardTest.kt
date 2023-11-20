aff45deec96c8d38106e471d4296433b36e759ba
Ted1_Lee <ted@spoonradio.co>
2023-11-20 09:46:20 +0900

    feat: card는 숫자와 무늬를 가진다

---
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -0,0 +1,22 @@
+package blackjack.domain
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class NormalCard(
+    val number: Int,
+    val pattern: String
+)
+
+class CardTest {
+
+    @Test
+    fun `카드는 숫자와 무늬를 가진다`() {
+        val cardNumber = 1
+        val cardPattern = "Diamond"
+        val card = NormalCard(cardNumber, cardPattern)
+
+        card.number shouldBe cardNumber
+        card.pattern shouldBe cardPattern
+    }
+}
