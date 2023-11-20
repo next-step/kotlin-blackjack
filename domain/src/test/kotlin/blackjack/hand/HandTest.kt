2de8d2788de93d45331581dddb67c284da7f149b
bmsk <vv99911@gmail.com>
2023-11-20 18:45:24 +0900

    [Feat] Hand 클래스: 현재 손 패를 의미

---
+++ domain/src/test/kotlin/blackjack/hand/HandTest.kt
@@ -0,0 +1,32 @@
+package blackjack.hand
+
+import blackjack.card.Card
+import blackjack.card.CardRank
+import blackjack.card.CardSuit
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class HandTest : FunSpec({
+    test("손패가 Ace, 10으로 이뤄져 있다면 21로 계산한다.") {
+        val hand = Hand(
+            cards = listOf(
+                Card(suit = CardSuit.CLUBS, rank = CardRank.ACE),
+                Card(suit = CardSuit.HEARTS, rank = CardRank.TEN)
+            )
+        )
+
+        hand.calculateBestValue() shouldBe 21
+    }
+
+    test("손패가 Ace, 10, 2으로 이뤄져 있다면 13으로 계산한다.") {
+        val hand = Hand(
+            cards = listOf(
+                Card(suit = CardSuit.CLUBS, rank = CardRank.ACE),
+                Card(suit = CardSuit.HEARTS, rank = CardRank.TEN),
+                Card(suit = CardSuit.HEARTS, rank = CardRank.TWO),
+            )
+        )
+
+        hand.calculateBestValue() shouldBe 13
+    }
+})
