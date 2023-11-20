84eb9852ebfefdec72ec831dec34b20933446f2f
chacha.roni <chacha.roni@kakaocorp.com>
2023-11-20 21:11:10 +0900

    step2 블랙잭 구현 - 추가 드로우

--- src/main/kotlin/blackjack/controller/BlackjackMain.kt
+++ src/main/kotlin/blackjack/controller/BlackjackMain.kt
@@ -3,6 +3,7 @@ package blackjack.controller
 import blackjack.domain.Dealer
 import blackjack.domain.GameManager
 import blackjack.domain.Player
+import blackjack.view.askForDraw
 import blackjack.view.inputNames
 import blackjack.view.printInitialSupply
 import blackjack.view.printUserCardInfo
@@ -20,4 +21,22 @@ fun main() {
     // 초기 카드 분배결과 출력
     printInitialSupply(players, GameManager.INITIAL_CARD_COUNT)
     players.forEach { printUserCardInfo(it) }
+
+    // 각 사용자의 추가 draw 진행
+    players.forEach { drawWhileUserWants(it, dealer) }
+}
+
+private fun drawWhileUserWants(player: Player, dealer: Dealer) {
+    var isPrinted = false
+
+    while (askForDraw(player.name)) {
+        isPrinted = true
+
+        dealer.supplyCard(player)
+        printUserCardInfo(player)
+    }
+
+    if (!isPrinted) {
+        printUserCardInfo(player)
+    }
 }
--- src/main/kotlin/blackjack/view/StringHandler.kt
+++ src/main/kotlin/blackjack/view/StringHandler.kt
@@ -6,3 +6,11 @@ fun tokenizeUserNames(input: String): List<String> {
         .filterNot { it.isBlank() }
         .map { it.trim() }
 }
+
+fun inputToBoolean(input: String): Boolean {
+    return when (input) {
+        "y", "Y" -> true
+        "n", "N" -> false
+        else -> throw IllegalArgumentException("y 또는 n만 입력 가능합니다.")
+    }
+}
