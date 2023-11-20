560212b924b338c1407a793e21eb7ca3e945bf04
silvio.oivlis <silvio.oivlis@kakaocorp.com>
2023-11-20 16:34:51 +0900

    refactor: INITIAL_CARD_COUNT private 로 변경 및 컨벤션 반영

--- src/main/kotlin/blackjack/domain/card/Deck.kt
+++ src/main/kotlin/blackjack/domain/card/Deck.kt
@@ -6,12 +6,12 @@ import blackjack.model.Suit
 
 class Deck(val deckCards: DeckCards) {
     init {
-        require(deckCards.size == initialCardCount) { "Invalid initial Card count ${deckCards.size}" }
+        require(deckCards.size == INITIAL_CARD_COUNT) { "Invalid initial Card count ${deckCards.size}" }
         require(deckCards.cardList.count { it.suit == Suit.Spade } == Character.values().size) { "Invalid suit count ${Suit.Spade}" }
         require(deckCards.cardList.count { it.suit == Suit.Diamond } == Character.values().size) { "Invalid suit count ${Suit.Diamond}" }
         require(deckCards.cardList.count { it.suit == Suit.Heart } == Character.values().size) { "Invalid suit count ${Suit.Heart}" }
         require(deckCards.cardList.count { it.suit == Suit.Clover } == Character.values().size) { "Invalid suit count ${Suit.Clover}" }
-        require(deckCards.cardList.toSet().size == initialCardCount) { "Duplicate cards ${deckCards.cardList}" }
+        require(deckCards.cardList.toSet().size == INITIAL_CARD_COUNT) { "Duplicate cards ${deckCards.cardList}" }
     }
 
     fun draw(): Card = deckCards.drawTop()
@@ -23,7 +23,7 @@ class Deck(val deckCards: DeckCards) {
     fun cardCount(): Int = deckCards.size
 
     companion object {
-        val initialCardCount = Suit.values().size * Character.values().size
+        private val INITIAL_CARD_COUNT = Suit.values().size * Character.values().size
 
         fun fullDeck(): Deck {
             return Deck(
