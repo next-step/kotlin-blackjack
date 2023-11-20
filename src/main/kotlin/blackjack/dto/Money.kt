8bd26ef809280da2f9b841e93ce181d00ba357a8
loren-hyuni <loren.hyuni@kakaocorp.com>
2023-11-20 20:59:21 +0900

    mission: step4 블랙잭(베팅) 리뷰 반영

--- src/main/kotlin/blackjack/dto/Money.kt
+++ src/main/kotlin/blackjack/dto/Money.kt
@@ -3,11 +3,11 @@ package blackjack.dto
 @JvmInline
 value class Money(val money: Int) {
 
-    infix operator fun times(times: Double): Money = Money((money * times).toInt())
+    operator fun times(times: Double): Money = Money((money * times).toInt())
 
-    infix operator fun times(times: Int): Money = Money(money * times)
+    operator fun times(times: Int): Money = Money(money * times)
 
-    infix operator fun minus(minus: Money): Money = Money(money - minus.money)
+    operator fun minus(minus: Money): Money = Money(money - minus.money)
 
     companion object {
         val ZERO = Money(0)
