0197a84c73452bb1a26cffe6702e27e5a7bc4d8f
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-20 11:59:27 +0900

    BlackjackGame 구현 - [x] BlackjackGame 컨트롤러 객체를 생성한다.   - [x] `BlackjackGame`은 `Dealer`, `Players`를 가진다.   - [x] 게임 초기화시 `Dealer`와 `Player`와 `Deck`을 세팅한다.   - [x] 게임이 시작되면 `Player`에게 두장의 카드를 제공하고, 게임을 진행한다. 이후 `Player`별 결과를 반환한다.

--- src/main/kotlin/blackjack/BlackjackGame.kt
+++ src/main/kotlin/blackjack/BlackjackGame.kt
@@ -1,4 +1,66 @@
 package blackjack
 
+import blackjack.domain.Cards
+import blackjack.domain.Dealer
+import blackjack.domain.Player
+import blackjack.domain.Players
+import blackjack.view.InputView
+import blackjack.view.OutputView
+
 class BlackjackGame {
+
+    private var dealer: Dealer
+    private var players: Players
+    private var nicknames: List<String> = InputView.getNicknames()
+
+    init {
+        players = Players(nicknames.map { Player(it) })
+        dealer = Dealer()
+        dealer.ready()
+    }
+
+    fun start() {
+        repeat(Cards.INITIAL_DEAL_SIZE) {
+            players.values.forEach {
+                it.getCard(dealer.dealCard())
+            }
+        }
+        OutputView.printCardDealingHeader(nicknames.joinToString(", "), Cards.INITIAL_DEAL_SIZE)
+        players.values.forEach {
+            OutputView.printCardDealing(
+                it.name,
+                it.cards.value.joinToString(", ") { card -> card.toString() })
+        }
+
+        process(dealer, players)
+
+        players.values.forEach {
+            OutputView.printGameScore(
+                nickname = it.name,
+                cards = it.cards.value.joinToString(", ") { card -> card.toString() },
+                score = it.getScore()
+            )
+        }
+    }
+
+    private fun process(dealer: Dealer, players: Players) {
+        println()
+        while (players.withHit().isNotEmpty()) {
+            players.withHit().first().playGame(dealer)
+        }
+        println()
+    }
+
+    private fun Player.playGame(dealer: Dealer) {
+        if (InputView.askHitOrStand(this.name)) {
+            this.getCard(dealer.dealCard())
+            OutputView.printCardDealing(this.name, this.cards.value.joinToString(",") { card -> card.toString() })
+        } else {
+            this.turnStand()
+        }
+    }
+}
+
+fun main() {
+    BlackjackGame().start()
 }
