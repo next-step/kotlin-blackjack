58e06e6de635531cb99893bbc31eec0c017dc402
jaylene shin <jaylene.shin@kakaocorp.com>
2023-11-20 11:59:02 +0900

    뷰 구현 - [x] 입출력 조건에 맞게 `InputView`, `OutputView`를 생성한다.

--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,4 +1,21 @@
 package blackjack.view
 
 object OutputView {
+    fun printCardDealingHeader(
+        nicknames: String, numOfCards: Int
+    ) {
+        println("\n${nicknames}에게 ${numOfCards}장의 나누었습니다.")
+    }
+
+    fun printCardDealing(nickname: String, cards: String) {
+        println("${nickname}카드: $cards")
+    }
+
+    fun printGameScore(
+        nickname: String,
+        cards: String,
+        score: Int
+    ) {
+        println("${nickname}카드: $cards - 결과: $score")
+    }
 }
