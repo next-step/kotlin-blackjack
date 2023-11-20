868504b6cf87d28fb354b31be4ab696db1ca29c7
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-21 03:14:02 +0900

    [refacotirng] step3: 상태 INITIAL 추가

--- src/main/kotlin/blackjack_dealer/entity/GamerCurrentState.kt
+++ src/main/kotlin/blackjack_dealer/entity/GamerCurrentState.kt
@@ -1,6 +1,7 @@
 package blackjack_dealer.entity
 
 sealed class GamerCurrentState {
+    object INITIAL : GamerCurrentState()
     object HIT : GamerCurrentState()
     object STAND : GamerCurrentState()
     object BUST : GamerCurrentState()
