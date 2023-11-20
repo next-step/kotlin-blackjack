3725e394cbcc81cecb1a22fba6311bc8c44f7f74
ALbertIM0427 <guang7226961@naver.com>
2023-11-21 03:05:00 +0900

    main: Main 테스트 구현

--- src/test/kotlin/blackjack/GameManagerTest.kt
+++ src/test/kotlin/blackjack/GameManagerTest.kt
@@ -7,7 +7,11 @@ import blackjack.business.participants.PlayerResult
 import blackjack.business.util.Money
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class GameManagerTest {
 
@@ -36,5 +40,63 @@ class GameManagerTest {
             PlayerResult("pobi", Money(20000)),
             PlayerResult("jason", Money(-10000))
         )
+    }
+}
+
+class MainTest {
+
+    private val standardOut = System.out
+    private val outputStreamCaptor = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
+    @Test
+    fun main() {
+        // given
+        val gameManager = GameManager(
+            MainTestGameView(),
+            AlwaysDrawStrategy(),
+            CardDesk(GameManagerTestCardFactory(), FixSelectionStrategy())
+        )
+
+        // when
+        gameManager.start()
+
+        // then
+        outputStreamCaptor.toString() shouldBe """
+            게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)
+            pobi, jason
+            pobi 의 배팅 금액은?
+            10000
+            jason 의 배팅 금액은?
+            10000
+            
+            pobi, jason에게 2장의 카드를 나누었습니다.
+            딜러카드: A스페이드, 3스페이드
+            pobi카드: 9스페이드, 3스페이드
+            jason카드: 10스페이드, 3스페이드
+            
+            pobi카드: 9스페이드, 3스페이드, 9스페이드
+            jason카드: 10스페이드, 3스페이드, 10스페이드
+            
+            딜러는 16이하라 한장의 카드를 더 받았습니다.
+            딜러카드: A스페이드, 3스페이드, 5스페이드 - 결과: 19
+            pobi카드: 9스페이드, 3스페이드, 9스페이드 - 결과: 21
+            jason카드: 10스페이드, 3스페이드, 10스페이드 - 결과: 23
+            
+            ## 최종 수익
+            딜러: -10000
+            pobi: 20000
+            jason: -10000
+            
+            """.trimIndent()
     }
 }
---
+++ src/test/kotlin/blackjack/MainTestGameView.kt
@@ -0,0 +1,60 @@
+package blackjack
+
+import blackjack.business.participants.GameResult
+import blackjack.business.participants.Player
+import blackjack.business.participants.PlayerResult
+import blackjack.business.util.Money
+import blackjack.view.GameView
+
+class MainTestGameView : GameView {
+    override fun displayGameStartAnnouncement(playerNames: List<String>) {
+        println()
+        println("${playerNames.joinToString(", ")}에게 2장의 카드를 나누었습니다.")
+    }
+
+    override fun displayPlayerCards(player: Player) {
+        println("${player.name}카드: ${player.cards.joinToString(", ")}")
+    }
+
+    override fun displayPlayerResult(player: Player) {
+        println("${player.name}카드: ${player.cards.joinToString(", ")} - 결과: ${player.score}")
+    }
+
+    override fun displayGameResult(gameResult: GameResult) {
+        println()
+        println("## 최종 수익")
+        printPlayerResult(gameResult.dealerResult)
+        gameResult.playerResults.forEach(::printPlayerResult)
+    }
+
+    override fun displayDealerDrawCardAnnouncement() {
+        println()
+        println("딜러는 16이하라 한장의 카드를 더 받았습니다.")
+    }
+
+    override fun printNewLine() {
+        println()
+    }
+
+    override fun askForOneMore(playerName: String): String {
+        println("$playerName 는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        println("y")
+        return "y"
+    }
+
+    override fun askForPlayerNames(): List<String> {
+        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+        println("pobi, jason")
+        return listOf("pobi", "jason")
+    }
+
+    override fun askForBettingMoney(playerName: String): Money {
+        println("$playerName 의 배팅 금액은?")
+        println(10000)
+        return Money(10000)
+    }
+
+    private fun printPlayerResult(playerResult: PlayerResult) {
+        println("${playerResult.name}: ${playerResult.money.value}")
+    }
+}
