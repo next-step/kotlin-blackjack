8b62786b8b1c4c5d11d0e531eaaae78f35f652b2
Ahn-seokjoo <tjrwn0716@naver.com>
2023-11-22 01:39:04 +0900

    [feature] step3: 딜러 추가

--- src/main/kotlin/blackjack_dealer/domain/Dealer.kt
+++ src/main/kotlin/blackjack_dealer/domain/Dealer.kt
@@ -1,12 +1,46 @@
 package blackjack_dealer.domain
 
+import blackjack_dealer.CardGenerator
+import blackjack_dealer.entity.CardDeque
 import blackjack_dealer.entity.GamerCards
 import blackjack_dealer.entity.GamerCurrentState
+import blackjack_dealer.entity.toGamerCards
 
 data class Dealer(
-    val name: String,
-    val cards: GamerCards,
-    val currentState: GamerCurrentState,
+    private val name: String = DEALER_NAME
 ) {
+    private lateinit var gamerCards: GamerCards
+    private var currentState: GamerCurrentState = GamerCurrentState.INITIAL
 
+    fun getDealerName(): String = name
+    fun getCurrentCards(): GamerCards = gamerCards
+    fun getCurrentGamerState(): GamerCurrentState = currentState
+    fun canKeepPlayingGame(): Boolean = currentState is GamerCurrentState.HIT
+
+    fun getOneMoreCardIfHit(cardDeque: CardDeque) {
+        if (currentState is GamerCurrentState.HIT) {
+            gamerCards = gamerCards.plus(listOf(CardGenerator.generateSingleCard(cardDeque))).toGamerCards()
+        }
+    }
+
+    companion object {
+        private const val DEALER_NAME = "딜러"
+        fun newInstance(cards: GamerCards): Dealer {
+            val initialState = findDealerInitialState(cards)
+            return Dealer().apply {
+                gamerCards = cards
+                currentState = initialState
+            }
+        }
+
+        private fun findDealerInitialState(cards: GamerCards): GamerCurrentState {
+            val cardScores = cards.getCurrentScore()
+            return when (cardScores) {
+                in 4..16 -> GamerCurrentState.HIT
+                in 17..20 -> GamerCurrentState.STAND
+                21 -> GamerCurrentState.BLACKJACK
+                else -> GamerCurrentState.BUST
+            }
+        }
+    }
 }
--- src/test/kotlin/blackjack_dealer/DealerTest.kt
+++ src/test/kotlin/blackjack_dealer/DealerTest.kt
@@ -1,24 +1,66 @@
 package blackjack_dealer
 
+import blackjack_dealer.domain.Dealer
+import blackjack_dealer.entity.Card
+import blackjack_dealer.entity.CardDeque
+import blackjack_dealer.entity.CardNumber
+import blackjack_dealer.entity.CardShape
+import blackjack_dealer.entity.toGamerCards
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.ints.shouldBeLessThan
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import io.kotest.matchers.ints.shouldBeLessThanOrEqual
+import io.kotest.matchers.shouldBe
 
 class DealerTest : BehaviorSpec({
-    Given("딜러는 처음 두장을 받는다") {
-        When("받은 두장의 합계가 16 이하면 1장을 더 받는다") {
+    Given("딜러가 생성되면") {
+        val cardDeque = CardDeque.create()
+        cardDeque.cardDeque.removeIf { it == Card(CardNumber.J, CardShape.HEART) }
+        cardDeque.cardDeque.removeIf { it == Card(CardNumber.FIVE, CardShape.CLOVER) }
 
+        val customCards =
+            listOf(Card(CardNumber.J, CardShape.HEART), Card(CardNumber.J, CardShape.CLOVER)).toGamerCards()
+
+        val dealer = Dealer.newInstance(cards = customCards)
+        Then("이름은 딜러이다") {
+            val expected = "딜러"
+            dealer.getDealerName() shouldBe expected
+        }
+
+        When("카드 두장을 받고 받은 두장의 합계가 16 이하면") {
+            val cardDequeLessThanSixteen = CardDeque.create()
+            cardDequeLessThanSixteen.cardDeque.removeIf { it == Card(CardNumber.J, CardShape.HEART) }
+            cardDequeLessThanSixteen.cardDeque.removeIf { it == Card(CardNumber.FIVE, CardShape.CLOVER) }
+
+            val customCardsLessThanSixteen =
+                listOf(Card(CardNumber.J, CardShape.HEART), Card(CardNumber.FIVE, CardShape.CLOVER)).toGamerCards()
             val lessThanScoreExpected = 16
             val cardCountExpected = 3
-            dealer.gamerCards.getCurrentScore() shouldBeLessThan lessThanScoreExpected
-            dealer.gamerCards.count() shouldBe expected
+            val dealerLessThanSixteen = Dealer.newInstance(customCardsLessThanSixteen)
+
+            Then("1장을 더 받는다 (16이하인지도 체크)") {
+                dealerLessThanSixteen.getCurrentCards().getCurrentScore() shouldBeLessThanOrEqual lessThanScoreExpected
+
+                dealerLessThanSixteen.getOneMoreCardIfHit(cardDequeLessThanSixteen)
+                dealerLessThanSixteen.getCurrentCards().count() shouldBe cardCountExpected
+            }
         }
 
-        When("받은 두장의 합계가 17 이상이면 추가로 받을 수 없다") {
-            
+        When("카드 두장을 받고 받은 두장의 합계가 17 이상이면 추가로 받을 수 없다") {
+            val cardDequeGreaterThanSixteen = CardDeque.create()
+            cardDequeGreaterThanSixteen.cardDeque.removeIf { it == Card(CardNumber.J, CardShape.HEART) }
+            cardDequeGreaterThanSixteen.cardDeque.removeIf { it == Card(CardNumber.SEVEN, CardShape.CLOVER) }
+
+            val customCardsGreaterThanSixteen =
+                listOf(Card(CardNumber.J, CardShape.HEART), Card(CardNumber.SEVEN, CardShape.CLOVER)).toGamerCards()
             val moreThanScoreExpected = 17
             val cardCountExpected = 2
-            dealer.gamerCards.getCurrentScore() shouldBeMoreThan moreThanScoreExpected
-            dealer.gamerCards.count() shouldBe cardCountExpected
+            val dealerGreaterThanSixteen = Dealer.newInstance(customCardsGreaterThanSixteen)
+
+            Then("추가로 받을 수 없다 (17 이상인지도 체크)") {
+                dealerGreaterThanSixteen.getCurrentCards().getCurrentScore() shouldBeGreaterThanOrEqual moreThanScoreExpected
+                dealerGreaterThanSixteen.getCurrentCards().count() shouldBe cardCountExpected
+            }
         }
     }
+
 })
