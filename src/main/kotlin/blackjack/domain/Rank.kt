0630506beb62d1eaf5493d288f6dca2451dad3c7
dean <sodp5@naver.com>
2023-11-22 01:19:55 +0900

    숫자 표기할 때 카드이름 표기하도록 변경

--- src/main/kotlin/blackjack/domain/Rank.kt
+++ src/main/kotlin/blackjack/domain/Rank.kt
@@ -1,17 +1,17 @@
 package blackjack.domain
 
-enum class Rank(val value: Int) {
-    Ace(1),
-    Two(2),
-    Three(3),
-    Four(4),
-    Five(5),
-    Six(6),
-    Seven(7),
-    Eight(8),
-    Nine(9),
-    Ten(10),
-    Jack(10),
-    Queen(10),
-    King(10),
+enum class Rank(val value: Int, val nameValue: String) {
+    Ace(1, "1"),
+    Two(2, "2"),
+    Three(3, "3"),
+    Four(4, "4"),
+    Five(5, "5"),
+    Six(6, "6"),
+    Seven(7, "7"),
+    Eight(8, "8"),
+    Nine(9, "9"),
+    Ten(10, "10"),
+    Jack(10, "J"),
+    Queen(10, "Q"),
+    King(10, "K"),
 }
--- src/main/kotlin/blackjack/view/OutputView.kt
+++ src/main/kotlin/blackjack/view/OutputView.kt
@@ -1,16 +1,15 @@
 package blackjack.view
 
-import blackjack.domain.HardAcePointStrategy
 import blackjack.domain.Player
 
 object OutputView {
     fun printCards(player: Player) {
-        println("${player.name}카드: ${player.cards.value.joinToString { "${it.rank.value}${it.suit.value}" }}")
+        println("${player.name}카드: ${player.cards.value.joinToString { "${it.rank.nameValue}${it.suit.value}" }}")
     }
 
     fun printResult(player: Player) {
         println(
-            "${player.name}카드: ${player.cards.value.joinToString { "${it.rank.value}${it.suit.value}" }} - 결과: ${
+            "${player.name}카드: ${player.cards.value.joinToString { "${it.rank.nameValue}${it.suit.value}" }} - 결과: ${
                 player.cards.getPoints()
             }"
         )
