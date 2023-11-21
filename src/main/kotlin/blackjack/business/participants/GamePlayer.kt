a2c550c6bfa8b77dd368a4004cc7cead33d42e50
ALbertIM0427 <guang7226961@naver.com>
2023-11-22 07:16:48 +0900

    refactor: 불필요한 getter setter 제거

--- src/main/kotlin/blackjack/business/participants/GamePlayer.kt
+++ src/main/kotlin/blackjack/business/participants/GamePlayer.kt
@@ -3,18 +3,10 @@ package blackjack.business.participants
 import blackjack.business.util.BlackJackConst
 import blackjack.business.util.Money
 
-class GamePlayer(name: String, playerCards: PlayerCards = PlayerCards(), money: Money = Money(MIN_BETTING_MONEY)) :
+class GamePlayer(name: String, playerCards: PlayerCards = PlayerCards(), val money: Money = Money(MIN_BETTING_MONEY)) :
     Player(name, playerCards) {
-    private var _money: Money = money
-
-    val money: Money
-        get() = _money.copy()
 
     override fun canDrawCard(): Boolean = playerCards.sum() < BlackJackConst.BLACKJACK
-
-    fun setBettingMoney(money: Money) {
-        this._money = money
-    }
 
     companion object {
         const val MIN_BETTING_MONEY = 0
