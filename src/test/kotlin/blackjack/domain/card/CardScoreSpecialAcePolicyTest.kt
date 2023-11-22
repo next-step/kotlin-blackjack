6beecd526b317bd6d7d03f21ab5aee54382e4824
korjun1993 <corojoon93@naver.com>
2023-11-22 19:07:06 +0900

    feat: King, Queen, Jack은 10으로 계산한다.

---
+++ src/test/kotlin/blackjack/domain/card/CardScoreSpecialAcePolicyTest.kt
@@ -0,0 +1,38 @@
+package blackjack.domain.card
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.row
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class CardScoreSpecialAcePolicyTest : FunSpec({
+    test("Ace는 11로 계산한다") {
+        val card = Card(CardKind.DIAMOND, CardNumber.ACE)
+        val actual = CardScoreSpecialAcePolicy.getScore(card)
+        actual shouldBe CardScore(11)
+    }
+
+    context("일반적인 숫자는 해당 값으로 계산한다") {
+        withData(
+            row(CardNumber.TWO, 2),
+            row(CardNumber.THREE, 3),
+            row(CardNumber.FOUR, 4),
+            row(CardNumber.FIVE, 5),
+            row(CardNumber.SIX, 6),
+            row(CardNumber.SEVEN, 7),
+            row(CardNumber.EIGHT, 8),
+            row(CardNumber.NINE, 9),
+            row(CardNumber.TEN, 10),
+        ) { (cardNumber, score) ->
+            val card = Card(CardKind.DIAMOND, cardNumber)
+            CardScoreSpecialAcePolicy.getScore(card) shouldBe CardScore(score)
+        }
+    }
+
+    context("King, Queen, Jack은 10으로 계산한다.") {
+        withData(CardNumber.JACK, CardNumber.QUEEN, CardNumber.KING) { cardNumber ->
+            val card = Card(CardKind.DIAMOND, cardNumber)
+            CardScoreSpecialAcePolicy.getScore(card) shouldBe CardScore(10)
+        }
+    }
+})
