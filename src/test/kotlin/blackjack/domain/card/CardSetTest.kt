52c2329d759beaa7667c615ccd8a0140bc2fc827
korjun1993 <corojoon93@naver.com>
2023-11-22 19:31:55 +0900

    refactor: CardSetTest 코테스트 변경

--- src/test/kotlin/blackjack/domain/card/CardSetTest.kt
+++ src/test/kotlin/blackjack/domain/card/CardSetTest.kt
@@ -1,13 +1,11 @@
 package blackjack.domain.card
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 
-class CardSetTest {
-
-    @Test
-    fun `카드 뭉치에 새로운 카드를 추가할 수 있다`() {
+class CardSetTest : FunSpec({
+    test("카드 뭉치에 새로운 카드를 추가할 수 있다") {
         val cardSet = CardSet.empty()
         val newCardSet = cardSet.addCard(Card.of(CardKind.DIAMOND, CardNumber.EIGHT))
         newCardSet.cards shouldBe listOf(
@@ -15,8 +13,7 @@ class CardSetTest {
         )
     }
 
-    @Test
-    fun `카드 뭉치에 새로운 카드뭉치를 추가할 수 있다`() {
+    test("카드 뭉치에 새로운 카드뭉치를 추가할 수 있다") {
         val cardSet = CardSet.empty()
         val addedCardSet = CardSet.of(
             Card.of(CardKind.DIAMOND, CardNumber.EIGHT),
@@ -31,8 +28,7 @@ class CardSetTest {
         )
     }
 
-    @Test
-    fun `카드 뭉치에 중복된 카드가 있으면 예외가 발생한다`() {
+    test("카드 뭉치에 중복된 카드가 있으면 예외가 발생한다") {
         shouldThrow<IllegalArgumentException> {
             CardSet.of(
                 Card.of(CardKind.DIAMOND, CardNumber.EIGHT),
@@ -41,20 +37,16 @@ class CardSetTest {
         }
     }
 
-    @Test
-    fun `카드 뭉치에 중복된 카드를 추가하면 예외가 발생한다`() {
+    test("카드 뭉치에 중복된 카드를 추가하면 예외가 발생한다") {
         val cardSet = CardSet.of(Card.of(CardKind.DIAMOND, CardNumber.EIGHT))
         shouldThrow<IllegalArgumentException> { cardSet.addCard(Card.of(CardKind.DIAMOND, CardNumber.EIGHT)) }
     }
-
-    @Test
-    fun `카드 뭉치에 몇 장의 카드가 들어있는지 알 수 있다`() {
+    test("카드 뭉치에 몇 장의 카드가 들어있는지 알 수 있다") {
         val cardSet = CardSet.of(Card.of(CardKind.DIAMOND, CardNumber.EIGHT))
         cardSet.size shouldBe 1
     }
 
-    @Test
-    fun `카드 뭉치의 점수를 합산 할 수 있다`() {
+    test("카드 뭉치의 점수를 합산 할 수 있다") {
         val cardSet = CardSet.of(
             Card.of(CardKind.DIAMOND, CardNumber.EIGHT),
             Card.of(CardKind.DIAMOND, CardNumber.NINE)
@@ -73,4 +65,4 @@ class CardSetTest {
         }
         cardSet.sum(cardScorePolicy) shouldBe CardScore(17)
     }
-}
+})
