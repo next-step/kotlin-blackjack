ae2f0466aef062b843c1fc8f672e2f5c259e7704
korjun1993 <corojoon93@naver.com>
2023-11-22 19:16:16 +0900

    feat: 두 개의 카드의 점수를 더할 수 있다.

--- src/main/kotlin/blackjack/domain/card/CardScore.kt
+++ src/main/kotlin/blackjack/domain/card/CardScore.kt
@@ -1,3 +1,7 @@
 package blackjack.domain.card
 
-data class CardScore(val score: Int)
+data class CardScore(val score: Int) {
+    operator fun plus(other: CardScore): CardScore {
+        return CardScore(score + other.score)
+    }
+}
