52f4c83158b07a0c08d7a604de447e2e5d7f660b
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-22 22:44:21 +0900

    (update) controller 수정

--- src/main/kotlin/blackJack/controller/BlackJackController.kt
+++ src/main/kotlin/blackJack/controller/BlackJackController.kt
@@ -1,5 +1,7 @@
 package blackJack.controller
 
+import blackJack.domain.CardDeck
+import blackJack.domain.Dealer
 import blackJack.domain.Player
 import blackJack.domain.Players
 import blackJack.dto.PlayerDto
@@ -8,41 +10,41 @@ import blackJack.view.InputView
 import blackJack.view.OutputView
 
 fun main() {
+    val cardDeck = CardDeck.createShuffledDeck()
+    val dealer = Dealer(cardDeck)
+
     OutputView.printEnterName()
     val inputNames = InputView.inputNames()
-    val splitNames = Player.splitNames(inputNames)
-    OutputView.printPlayer(splitNames)
+    val playerList = Player.splitNames(inputNames)
+    OutputView.printPlayer(playerList)
 
-    val players: Players = Players.initBettings(splitNames)
+    val players: Players = Players.createPlayers(playerList, dealer)
     val playersDto = PlayersDto(players)
     OutputView.printPlayerCards(playersDto)
 
     val finishGamePlayers = players.players.map { player ->
-        playGame(player)
+        playGame(player, dealer)
     }
 
     val playersResult = PlayersDto(Players(finishGamePlayers))
     OutputView.printResult(playersResult)
 }
 
-private fun playGame(player: Player): Player {
+private fun playGame(player: Player, dealer: Dealer): Player {
     var currentPlayer = player
 
-    while (isContinueGame(currentPlayer)) {
-        currentPlayer = currentPlayer.addCard()
+    while (currentPlayer.isHit()) {
+        val playerDto = PlayerDto(currentPlayer)
+        OutputView.printQuestionYesOrNo(playerDto)
+        val answer = InputView.answerYesOrNo()
+
+        if (answer == "n") {
+            OutputView.printPlayerCard(PlayerDto(currentPlayer))
+            break
+        }
+
+        currentPlayer.addCard(dealer, answer)
         OutputView.printPlayerCard(PlayerDto(currentPlayer))
     }
-    OutputView.printPlayerCard(PlayerDto(currentPlayer))
     return currentPlayer
-}
-
-private fun isContinueGame(currentPlayer: Player): Boolean {
-    if (currentPlayer.cards.cards.sumOf { it.rank.score } > 21) {
-        return false
-    }
-
-    val playerDto = PlayerDto(currentPlayer)
-    OutputView.printQuestionYesOrNo(playerDto)
-    val answer = InputView.answerYesOrNo()
-    return answer == "y"
 }
