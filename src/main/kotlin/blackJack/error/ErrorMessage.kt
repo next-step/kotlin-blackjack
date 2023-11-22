63b326558725cd46bde8fc8fc7d950b025587429
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-22 20:24:14 +0900

    코드리뷰 반영

--- src/main/kotlin/blackJack/error/ErrorMessage.kt
+++ src/main/kotlin/blackJack/error/ErrorMessage.kt
@@ -5,13 +5,9 @@ enum class ErrorMessage(val message: String) {
     WRONG_SUIT("잘못된 카드 모양입니다."),
     WRONG_RANK("잘못된 카드 숫자입니다."),
     EMPTY_NAME("이름이 비어 있습니다!"),
-    EMPTY_CARD("카드가 비어 있습니다!"),
     EMPTY_CARDS("카드리스트가 비어 있습니다!"),
     WRONG_INPUT("y or n 만 입력 가능합니다."),
-    DUPLICATE_CARDS("중복된 카드가 있습니다."),
     EMPTY_PLAYERS("플레이어가 비어 있습니다."),
     CARD_DECK_SIZE("CardDeck은 52장이어야 합니다."),
-    WRONG_STATUS("잘못된 Status 값입니다."),
-    ALREADY_FINISHED("이미 승패가 결정된 플레이어입니다.")
     ;
 }
