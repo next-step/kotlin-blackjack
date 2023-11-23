fd08d2e4eaca2d3918022ea528a7c761ce645cf5
dong <dong.22@kakaoenterprise.com>
2023-11-24 06:39:17 +0900

    feat : 딜러 출력 기능 구현

--- src/main/kotlin/blackjack/model/PlayersResults.kt
+++ src/main/kotlin/blackjack/model/PlayersResults.kt
@@ -18,4 +18,12 @@ class PlayersResults(
     fun resultOfPlayer(player: Player): PlayerResult {
         return requireNotNull(value[player]) { "플레이어를 찾을 수 없음" }
     }
+
+    fun playerResult(): List<Pair<String, PlayerResult>> {
+        return value.keys.map { playerNameAndResult(it) }.toList()
+    }
+
+    private fun playerNameAndResult(it: Player): Pair<String, PlayerResult> {
+        return it.name to requireNotNull(value[it]) { "플레이어 ${it.name} 을 찾을 수 없습니다" }
+    }
 }
