6b1ab614b9ae5f85f6d13cd0cac2773871247337
hjjeong <hjjeong@wert.co.kr>
2023-11-24 17:14:55 +0900

    refactor: 플레이어가 버스트라면 항상 진다

--- src/main/kotlin/blackjack/GameParticipants.kt
+++ src/main/kotlin/blackjack/GameParticipants.kt
@@ -10,18 +10,10 @@ data class GameParticipants(
         return GameParticipantResults(compareScore())
     }
 
-    private fun compareScore(): List<GameParticipantPlayerResult> {
-        if (dealer.isBust) return players.map {
-            GameParticipantPlayerResult(
-                it.name,
-                MatchResult.WIN
-            )
-        }
-
-        return players.map {
+    private fun compareScore(): List<GameParticipantPlayerResult> =
+        players.map {
             if (it.isBust) GameParticipantPlayerResult(it.name, MatchResult.LOSS)
             else if (it.isBlackjack()) GameParticipantPlayerResult(it.name, MatchResult.WIN)
             else GameParticipantPlayerResult(it.name, MatchResult.of(it, dealer))
         }
-    }
 }
