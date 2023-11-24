8ef13bf3107ad5152fc88ca0fc57ff1ff25dab11
bmsk <vv99911@gmail.com>
2023-11-24 16:49:57 +0900

    [Docs] 사전 구현 계획

--- presenter/src/main/kotlin/ui/input/InputView.kt
+++ presenter/src/main/kotlin/ui/input/InputView.kt
@@ -2,15 +2,4 @@ package ui.input
 
 class InputView {
 
-    fun inputPlayerNames(): List<String> {
-        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
-        val input = readln()
-        return input.split(",").map { it.trim() }
-    }
-
-    fun askForAdditionalCard(playerName: String): Boolean {
-        println("${playerName}은 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
-        val input = readln()
-        return input.equals("y", ignoreCase = true)
-    }
 }
