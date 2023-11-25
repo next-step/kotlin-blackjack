bc3876e52646b5cdb62b5748ab4d35f1bb7b961e
parker1609 <psbum1609@gmail.com>
2023-11-25 11:36:17 +0900

    feat: 딜러와 참가자들의 게임 결과들을 저장하는 GameResults

---
+++ src/main/kotlin/blackjack/domain/GameResults.kt
@@ -0,0 +1,22 @@
+package blackjack.domain
+
+import blackjack.domain.player.Dealer
+import blackjack.domain.player.GameResult
+import blackjack.domain.player.Participant
+
+object GameResults {
+    fun results(dealer: Dealer, participants: List<Participant>): Map<Participant, GameResult> {
+        val results = mutableMapOf<Participant, GameResult>()
+
+        participants.forEach { participant ->
+            val dealerGameResult = dealer.compareWith(participant)
+            results[participant] = when (dealerGameResult) {
+                GameResult.WIN -> GameResult.LOSE
+                GameResult.LOSE -> GameResult.WIN
+                GameResult.TIE -> GameResult.TIE
+            }
+        }
+
+        return results
+    }
+}
