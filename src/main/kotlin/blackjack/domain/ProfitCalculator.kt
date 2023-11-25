6a00438ceb7948f150119b45d815c1120c9d5805
yeongun <gjduddns@gmail.com>
2023-11-26 08:46:35 +0900

    [Step4] feat: 에러메시지 추가

--- src/main/kotlin/blackjack/domain/ProfitCalculator.kt
+++ src/main/kotlin/blackjack/domain/ProfitCalculator.kt
@@ -5,7 +5,7 @@ import blackjack.domain.state.Blackjack
 class ProfitCalculator(private val inputNameAndBets: Map<String, BettingMoney>) {
 
     fun getPlayerProfit(player: Player, dealer: Dealer): Int {
-        val bettingMoney = inputNameAndBets[player.name] ?: throw IllegalArgumentException()
+        val bettingMoney = inputNameAndBets[player.name] ?: throw IllegalArgumentException(PLAYER_BETTING_MONEY_NOT_FOUND_ERROR_MESSAGE)
 
         if (player.isBlackjack() && dealer.isBlackjack()) {
             return 0
@@ -17,7 +17,7 @@ class ProfitCalculator(private val inputNameAndBets: Map<String, BettingMoney>) 
     }
 
     private fun calculatePlayerProfit(dealer: Dealer, player: Player): Int {
-        val bettingMoney = inputNameAndBets[player.name] ?: throw IllegalArgumentException()
+        val bettingMoney = inputNameAndBets[player.name] ?: throw IllegalArgumentException(PLAYER_BETTING_MONEY_NOT_FOUND_ERROR_MESSAGE)
 
         return when {
             dealer.isBust() -> bettingMoney.value
@@ -31,5 +31,9 @@ class ProfitCalculator(private val inputNameAndBets: Map<String, BettingMoney>) 
 
     fun getDealerProfit(player: Player, dealer: Dealer): Int {
         return -getPlayerProfit(player, dealer)
+    }
+
+    companion object {
+        private const val PLAYER_BETTING_MONEY_NOT_FOUND_ERROR_MESSAGE = "해당 플레이어의 베팅 금액을 찾을 수 없습니다."
     }
 }
