8f1e0b5ab7563d579a8e1a43c76d0edb3d1a2a62
verus.j <verus.j@kakaocorp.com>
2023-11-25 19:42:02 +0900

    refactor: Ace 카드 합산 로직 및 테스트 정리

--- src/main/kotlin/blackjack/Cards.kt
+++ src/main/kotlin/blackjack/Cards.kt
@@ -1,15 +1,13 @@
 package blackjack
 
-import kotlin.math.abs
-
 class Cards(
     vararg cards: String,
 ) {
     private val cards = cards.toList()
 
     fun sum(): Int {
-        val maximum = sumOfMaximum()
         val minimum = sumOfMinimum()
+        val maximum = sumOfMaximum()
 
         if (BLACKJACK < maximum) {
             return minimum
@@ -17,13 +15,20 @@ class Cards(
         return maximum
     }
 
-    private fun sumOfMinimum() = sumOfMaximum() - 10
+    private fun sumOfMaximum(): Int {
+        if (cards.any { it.equals("A") }) {
+            return sumOfMinimum() + 10
+        }
+        return sumOfMinimum()
+    }
 
-    private fun sumOfMaximum() = cards.sumOf { cardValue(it) }
+    private fun sumOfMinimum(): Int {
+        return cards.sumOf { cardValue(it) }
+    }
 
     private fun cardValue(card: String): Int {
         if (card.equals("A")) {
-            return 11
+            return 1
         }
         return if (isCharacterCard(card)) 10 else card.toInt()
     }
--- src/test/kotlin/blackjack/BlackjackTest.kt
+++ src/test/kotlin/blackjack/BlackjackTest.kt
@@ -1,7 +1,6 @@
 package blackjack
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvSource
@@ -23,29 +22,31 @@ class BlackjackTest {
     @ParameterizedTest
     @CsvSource(value = ["K, 5, 15", "J, 3, 13", "Q, 8, 18"])
     fun `문자 카드는 10으로 계산`(card1: String, card2: String, expect: Int) {
-        val cards = Cards(card1, card2)
+        val sut = Cards(card1, card2)
 
-        val actual = cards.sum()
+        val actual = sut.sum()
 
         assertThat(actual).isEqualTo(expect)
     }
 
-    @Test
-    fun `최대 합이 21이 넘지 않는 경우 최대 합으로 반환`() {
-        val cards = Cards("A", "10")
+    @ParameterizedTest
+    @MethodSource("includeOneAceCards")
+    fun `Ace가 한장인 경우 최적의 값으로 합산`(expect: Int, cards: List<String>) {
+        val sut = Cards(*cards.toTypedArray())
 
-        val actual = cards.sum()
+        val actual = sut.sum()
 
-        assertThat(actual).isEqualTo(21)
+        assertThat(actual).isEqualTo(expect)
     }
 
-    @Test
-    fun `최대 합이 21을 넘는 경우 최소 합으로 반환`() {
-        val cards = Cards("A", "9", "J")
+    @ParameterizedTest
+    @MethodSource("includeMultipleAceCards")
+    fun `Ace가 여러장인 경우 최적의 값으로 합산`(expect: Int, cards: List<String>) {
+        val sut = Cards(*cards.toTypedArray())
 
-        val actual = cards.sum()
+        val actual = sut.sum()
 
-        assertThat(actual).isEqualTo(20)
+        assertThat(actual).isEqualTo(expect)
     }
 
     companion object {
@@ -55,6 +56,22 @@ class BlackjackTest {
                 Arguments.of(15, listOf("7", "8")),
                 Arguments.of(11, listOf("6", "5")),
                 Arguments.of(15, listOf("6", "5", "4")),
+            )
+        }
+
+        @JvmStatic
+        fun includeOneAceCards(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(21, listOf("A", "10")),
+                Arguments.of(20, listOf("A", "9", "J")),
+            )
+        }
+
+        @JvmStatic
+        fun includeMultipleAceCards(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(21, listOf("A", "A", "A", "8")),
+                Arguments.of(21, listOf("A", "A", "A", "10", "8")),
             )
         }
     }
