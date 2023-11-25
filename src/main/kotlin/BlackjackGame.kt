c5b5adfd3396bd08cd7fc6ebd15cdc90fb1ebc7d
OYJ <oyj7677@gmail.com>
2023-11-26 02:23:36 +0900

    Refactor: 블랙젝 게임 디폴트 파라미터 추가 - playerList: List<Player>
    Feat: 추가된 사용자 검증 로직 추가.

--- src/main/kotlin/BlackjackGame.kt
+++ src/main/kotlin/BlackjackGame.kt
@@ -1,12 +1,14 @@
 import card.CardPack
 import card.PlayingCard
+import player.Player
 
-class BlackjackGame(private val cardPack: CardPack) {
+class BlackjackGame(private val cardPack: CardPack, private val playerList: List<Player>) {
 
     private val index = CardIndex(maxIndex = cardPack.cardList.size)
 
     init {
         validateCardPack()
+        validatePlayer()
     }
 
     fun hit(): PlayingCard {
@@ -28,6 +30,10 @@ class BlackjackGame(private val cardPack: CardPack) {
         require(cardPack.cardList.size == cardPack.cardList.distinct().size) { ERR_MSG_DUPLICATE_CARD_PACK }
     }
 
+    private fun validatePlayer() {
+        require(playerList.size in MIN_PLAYER_CNT..MAX_PLAYER_CNT) { "참여 가능한 플레이어의 범위를 넘어섰습니다." }
+    }
+
     private fun increaseCardIndex() {
         index.increaseIndex()
     }
@@ -36,5 +42,7 @@ class BlackjackGame(private val cardPack: CardPack) {
         private const val CARD_PACK_SIZE = 52
         private const val ERR_MSG_CARD_PACK_SIZE = "카드는 52개로 구성되어야 합나다."
         private const val ERR_MSG_DUPLICATE_CARD_PACK = "중복되는 카드는 없어야 합니다."
+        private const val MIN_PLAYER_CNT = 2
+        private const val MAX_PLAYER_CNT = CARD_PACK_SIZE / 2
     }
 }
--- src/test/kotlin/blackJack/BlackjackGameTest.kt
+++ src/test/kotlin/blackJack/BlackjackGameTest.kt
@@ -3,17 +3,21 @@ package blackJack
 import BlackjackGame
 import card.CardPack
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.internal.Classes
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import player.Player
 
 class BlackjackGameTest {
-
+    private val defaultPlayers = listOf(Player(), Player())
     @Test
     fun `정상적인 카드팩을 받고, 게임에 주입되었을 때, 예외 검증을 통과한다`() {
         // given : 정상적인 카드팩을 받는다.
         val cardPack = CardPack.getCardPack()
 
         // when : 게임에 카드를 주입한다.
-        val actual = runCatching { BlackjackGame(cardPack) }.exceptionOrNull()
+        val actual = runCatching { BlackjackGame(cardPack, defaultPlayers) }.exceptionOrNull()
 
         // then : 예외를 던지지 않는다.
         assertThat(actual).isNull()
@@ -23,7 +27,7 @@ class BlackjackGameTest {
     fun `게임이 시작되고, 카드 배포를 요청할 때, 카드팩의 카드가 반환된다`() {
         // given : 카드팩 생성, 게임이 시작된다.
         val cardPack = CardPack.getCardPack()
-        val game = BlackjackGame(cardPack)
+        val game = BlackjackGame(cardPack, defaultPlayers)
 
         // when : 카드 배포를 한다.
         val actual = game.hit()
@@ -37,7 +41,7 @@ class BlackjackGameTest {
     fun `게임이 시작되고, 카드 배포를 반복할 때, 동일하지 않은 카드를 배포한다`() {
         // given : 카드팩 생성, 게임 시작
         val cardPack = CardPack.getCardPack()
-        val game = BlackjackGame(cardPack)
+        val game = BlackjackGame(cardPack, defaultPlayers)
 
         // when : 카드 배포 2회한다.
         val card1 = game.hit()
@@ -45,5 +49,21 @@ class BlackjackGameTest {
 
         // then : 동일하지 않은 카드가 배포된다.
         assertThat(card1).isNotEqualTo(card2)
+    }
+
+    @ParameterizedTest
+    fun `게임을 생성한다면, 플레이어가 2명 이상 26명 이하의 범위를 넘어설때, 예외를 던진다`() {
+        // given : 카드팩과  범위를 넘어서는 플레이어 리스트가 생성된다.
+        val cardPack = CardPack.getCardPack()
+        val singlePlayerList = listOf(Player())
+        val overPlayerList = List(27) { Player() }
+
+        // when : 게임에 플레이어를 주입한다
+        val actualSingle = runCatching { BlackjackGame(cardPack, singlePlayerList) }.exceptionOrNull()
+        val actualOver = runCatching { BlackjackGame(cardPack, overPlayerList) }.exceptionOrNull()
+
+        // then : 예외를 던진다.
+        assertThat(actualSingle).isInstanceOf(IllegalArgumentException::class.java)
+        assertThat(actualOver).isInstanceOf(IllegalArgumentException::class.java)
     }
 }
