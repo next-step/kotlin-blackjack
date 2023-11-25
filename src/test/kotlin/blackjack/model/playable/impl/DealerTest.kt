b90b454122351e8b8ac8a422d1b632fb1731799d
dong <dong.22@kakaoenterprise.com>
2023-11-25 17:00:32 +0900

    refactor : 검증 단언문을 추가함

--- src/test/kotlin/blackjack/model/playable/impl/DealerTest.kt
+++ src/test/kotlin/blackjack/model/playable/impl/DealerTest.kt
@@ -1,39 +1,33 @@
 package blackjack.model.playable.impl
 
-import blackjack.model.card.Card
-import blackjack.model.card.CardRank
-import blackjack.model.card.Cards
-import blackjack.model.card.Suit
+import blackjack.model.card.CardFixture
 import blackjack.model.pack.impl.ShuffledPack
 import blackjack.model.playblestrategy.impl.DealerStrategy
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
 
 class DealerTest : StringSpec({
 
     "딜러는 처음에 받은 2장의 카드 점수 합계가 16이하이면 반드시 1장의 카드를 추가로 받아야 한다" {
         val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card.of(Suit.CLOVER, CardRank.KING),
-                    Card.of(Suit.HEART, CardRank.SIX),
-                )
-            )
+            CardFixture.makeCards(CardFixture.king, CardFixture.six)
         )
         dealer.playing(DealerStrategy(16), ShuffledPack)
+
+        dealer.cards.cards shouldContain CardFixture.king
+        dealer.cards.cards shouldContain CardFixture.six
         dealer.countOfCards() shouldBe 3
     }
 
     "딜러는 카드의 점수 합계가 17점 이상이면 추가로 받을 수 없다" {
         val dealer = Dealer(
-            Cards(
-                listOf(
-                    Card.of(Suit.CLOVER, CardRank.KING),
-                    Card.of(Suit.HEART, CardRank.SEVEN),
-                )
-            )
+            CardFixture.makeCards(CardFixture.king, CardFixture.seven)
         )
         dealer.playing(DealerStrategy(17), ShuffledPack)
+
+        dealer.cards.cards shouldContain CardFixture.king
+        dealer.cards.cards shouldContain CardFixture.seven
         dealer.countOfCards() shouldBe 2
     }
 })
