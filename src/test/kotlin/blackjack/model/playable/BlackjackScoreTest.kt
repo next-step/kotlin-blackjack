9a47953a3117ec3cbd03f6aa9473115a94fe9c69
dong <dong.22@kakaoenterprise.com>
2023-11-26 13:48:15 +0900

    refactor : 출력 로직 점검

---
+++ src/test/kotlin/blackjack/model/playable/BlackjackScoreTest.kt
@@ -0,0 +1,18 @@
+package blackjack.model.playable
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class BlackjackScoreTest : StringSpec({
+    "이겨야한다" {
+        BlackjackScore(4) vs BlackjackScore(2) shouldBe PlayableResult.WIN
+    }
+
+    "져야한다" {
+        BlackjackScore(1) vs BlackjackScore(2) shouldBe PlayableResult.LOSE
+    }
+
+    "비겨야한다" {
+        BlackjackScore(7) vs BlackjackScore(7) shouldBe PlayableResult.DRAW
+    }
+})
