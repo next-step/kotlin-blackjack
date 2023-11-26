9df4b9787d625881b64babde5bb96232412edc88
dong <dong.22@kakaoenterprise.com>
2023-11-26 10:41:30 +0900

    refactor : 늘어난 player, Dealer 클래스의 책임과 역할을 검증하기 위해 단위테스트도 추가되었습니다

--- src/test/kotlin/blackjack/model/playable/impl/DealerTest.kt
+++ src/test/kotlin/blackjack/model/playable/impl/DealerTest.kt
@@ -1,5 +1,6 @@
 package blackjack.model.playable.impl
 
+import blackjack.model.Players
 import blackjack.model.card.CardFixture
 import blackjack.model.pack.impl.ShuffledPack
 import blackjack.model.playable.BlackjackScore
@@ -30,5 +31,30 @@ class DealerTest : StringSpec({
         dealer.cards.cards shouldContain CardFixture.king
         dealer.cards.cards shouldContain CardFixture.seven
         dealer.countOfCards() shouldBe 2
+    }
+
+    "딜러가 21을 초과하면 그 시점까지 남아 있던 플레이어들은 가지고 있는 패에 상관 없이 승리해야 한다" {
+        val player1 = Player(
+            "seoul",
+            CardFixture.makeCards(CardFixture.ace1, CardFixture.king)
+
+        )
+        val player2 = Player(
+            "wonju",
+            CardFixture.makeCards(CardFixture.queen, CardFixture.king)
+
+        )
+        val dealer = Dealer(
+            CardFixture.makeCards(CardFixture.five, CardFixture.seven, CardFixture.king)
+        )
+
+        val actualDealerResult = dealer.dealerResult(
+            Players(player1, player2)
+        )
+
+        actualDealerResult.score shouldBe BlackjackScore(22)
+        actualDealerResult.winningCount shouldBe 0
+        actualDealerResult.drawingCount shouldBe 0
+        actualDealerResult.losingCount shouldBe 2
     }
 })
--- src/test/kotlin/blackjack/model/playable/impl/PlayerTest.kt
+++ src/test/kotlin/blackjack/model/playable/impl/PlayerTest.kt
@@ -1,6 +1,8 @@
 package blackjack.model.playable.impl
 
+import blackjack.model.card.CardFixture
 import blackjack.model.pack.impl.ShuffledPack
+import blackjack.model.playable.BlackjackScore
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -21,5 +23,23 @@ class PlayerTest : StringSpec({
             player.hit(ShuffledPack)
             player.cards.count() shouldBe 1
         }
+    }
+
+    "ACE+ACE+KING 이 들어온 경우 ACE 를 1로 인식해, 스코어가 12 이어야 만 한다" {
+        val player = Player(
+            "kim",
+            CardFixture.makeCards(CardFixture.ace1, CardFixture.ace2, CardFixture.king)
+        )
+        player.isBurst() shouldBe false
+        player.score() shouldBe BlackjackScore(12)
+    }
+
+    "ACE+KING 이 들어온 경우 ACE 를 11로 인식해, 블랙잭 점수인 21 로 계산 되어야 한다" {
+        val player = Player(
+            "kong",
+            CardFixture.makeCards(CardFixture.ace1, CardFixture.king)
+        )
+        player.isBurst() shouldBe false
+        player.score() shouldBe BlackjackScore(21)
     }
 })
