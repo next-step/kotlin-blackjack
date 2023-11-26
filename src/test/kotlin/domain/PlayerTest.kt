8fcf0c193b8c001458eac149117b4a45b669d8b0
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-11-27 00:25:17 +0900

    [step4] (test) PlayerTest 추가

--- src/test/kotlin/domain/PlayerTest.kt
+++ src/test/kotlin/domain/PlayerTest.kt
@@ -5,6 +5,7 @@ import enum.Suit
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class PlayerTest {
 
@@ -28,5 +29,26 @@ class PlayerTest {
 
         player.receiveCard(Card(Suit.CLUBS, Rank.ACE))
         assertEquals(14, player.calculateScore())
+    }
+
+    @Test
+    @DisplayName("플레이어가 베팅 금액을 설정할 수 있어야 함")
+    fun `플레이어의 베팅 금액 설정`() {
+        val player = Player("Jaeyeon")
+        val bettingAmount = 10000
+
+        player.bettingAmount = bettingAmount
+        assertEquals(bettingAmount, player.bettingAmount)
+    }
+
+    @Test
+    @DisplayName("플레이어의 베팅 금액은 음수가 될 수 없음")
+    fun `플레이어의 베팅 금액 음수 설정 불가`() {
+        val player = Player("Jaeyeon")
+        val invalidAmount = -10000
+
+        assertThrows<IllegalArgumentException> {
+            player.bettingAmount = invalidAmount
+        }
     }
 }
