a3316a801e1fed642d236e2820a30e8146bcb7cd
Ted1_Lee <ted@spoonradio.co>
2023-11-27 10:18:20 +0900

    test: test코드 추가

--- src/test/kotlin/blackjack/domain/PlayerTest.kt
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -1,11 +1,18 @@
 package blackjack.domain
 
 import blackjack.Player
+import blackjack.ScoreCalculator
+import blackjack.card.AceCard
 import blackjack.card.CardDeck
+import blackjack.card.CardPattern
+import blackjack.card.CardPicture
+import blackjack.card.NormalCard
+import blackjack.card.PictureCard
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
+    private val scoreCalculator: ScoreCalculator = ScoreCalculator()
 
     @Test
     fun `플레이어는 이름을 가질 수 있다`() {
@@ -23,5 +30,33 @@ class PlayerTest {
 
         player.drawCard(cardDeck.draw(2))
         player.cards.size shouldBe 2
+    }
+
+    @Test
+    fun `플레이어는 카드 점수 합계가 21미만이면 카드를 뽑을 수 있다`() {
+        val name = "홍길동"
+        val player = Player(name)
+        player.drawCard(
+            listOf(
+                NormalCard(9, CardPattern.CLOVER),
+            )
+        )
+
+        player.shouldDraw(scoreCalculator) shouldBe true
+    }
+
+    @Test
+    fun `플레이어는 카드 점수 합계가 21이상이면 카드를 뽑을 수 없다`() {
+        val name = "홍길동"
+        val player = Player(name)
+        player.drawCard(
+            listOf(
+                NormalCard(9, CardPattern.CLOVER),
+                PictureCard(CardPicture.KING, CardPattern.CLOVER),
+                PictureCard(CardPicture.KING, CardPattern.SPADE)
+            )
+        )
+
+        player.shouldDraw(scoreCalculator) shouldBe false
     }
 }
