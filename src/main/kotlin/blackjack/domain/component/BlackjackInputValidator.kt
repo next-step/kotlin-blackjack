8f43bdb89aced93b89370565da7a47fe4db9247e
yw9594 <kyw9509@gmail.com>
2023-11-27 21:34:03 +0900

    feat(step2): 플레이어 이름 검증 로직 추가

---
+++ src/main/kotlin/blackjack/domain/component/BlackjackInputValidator.kt
@@ -0,0 +1,15 @@
+package blackjack.domain.component
+
+class BlackjackInputValidator {
+    fun validatePlayerNamesString(playerNames: String?): String {
+        require(!playerNames.isNullOrBlank()) { "플레이어 이름은 null 또는 공백일 수 없습니다." }
+
+        return playerNames
+    }
+
+    fun validatePlayerNamesSize(playerNames: List<String>): List<String> {
+        require(playerNames.isNotEmpty()) { "게임에는 1명 이상의 플레이어가 참가해야합니다." }
+
+        return playerNames
+    }
+}
