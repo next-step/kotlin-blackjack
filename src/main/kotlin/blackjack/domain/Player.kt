6cdd3de575b6395e6d04e6cd69b87e7313296fd8
david-sang <david.sang@kakaocorp.com>
2023-11-27 16:15:59 +0900

    style(step2): ktlint

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -5,10 +5,9 @@ data class Player(val name: String)
 @JvmInline
 value class Players(val values: List<Player>) {
 
-    constructor(vararg names: String): this(names.map { Player(it) })
+    constructor(vararg names: String) : this(names.map { Player(it) })
 
     init {
         require(values.size == values.map { it.name }.toSet().size) { "duplicate name has been used" }
     }
 }
-
