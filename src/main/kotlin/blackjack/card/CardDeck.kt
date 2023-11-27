ade3e47aa77b6994079aed2bf05bffb13851672b
JunHyun Lee <dlwnsgus777@naver.com>
2023-11-27 22:50:18 +0900

    refactor: 절차지향적 초기화 스타일 리팩터링

--- src/main/kotlin/blackjack/card/CardDeck.kt
+++ src/main/kotlin/blackjack/card/CardDeck.kt
@@ -7,13 +7,8 @@ object CardDeck {
     private const val LAST_NORMAL_CARD_NUMBER: Int = 10
 
     init {
-        val list = mutableListOf<BlackJackCard>()
-        initNormalCard(list)
-        initPictureCard(list)
-        initAceCard(list)
-
-        list.shuffle()
-        deck = ArrayDeque(list)
+        val cards: List<BlackJackCard> = normalCard() + pictureCard() + aceCard()
+        deck = ArrayDeque(cards.shuffled())
     }
 
     fun getSize(): Int {
@@ -29,6 +24,22 @@ object CardDeck {
         CardPattern.values().forEach {
             (FIRST_NORMAL_CARD_NUMBER..LAST_NORMAL_CARD_NUMBER).forEach { number -> list.add(NormalCard(number, it)) }
         }
+    }
+
+    private fun normalCard(): List<BlackJackCard> {
+        return CardPattern.values().flatMap {
+            (FIRST_NORMAL_CARD_NUMBER..LAST_NORMAL_CARD_NUMBER).map { number -> NormalCard(number, it) }
+        }
+    }
+
+    private fun pictureCard(): List<BlackJackCard> {
+        return CardPattern.values().flatMap {
+            CardPicture.values().map { picture -> PictureCard(picture, it) }
+        }
+    }
+
+    private fun aceCard(): List<BlackJackCard> {
+        return CardPattern.values().map(::AceCard)
     }
 
     private fun initPictureCard(list: MutableList<BlackJackCard>) {
