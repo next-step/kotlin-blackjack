bde2e2fd00cf2331ea3ef2b3618a7ff344f03731
yw9594 <kyw9509@gmail.com>
2023-11-27 21:37:42 +0900

    feat(step2): main 함수 추가

---
+++ src/main/kotlin/blackjack/BlackjackApplication.kt
@@ -0,0 +1,14 @@
+package blackjack
+
+import blackjack.domain.component.BlackjackInputValidator
+import blackjack.domain.model.PlayerName
+import blackjack.view.BlackjackInputView
+
+fun main() {
+    val blackjackController = BlackjackController(
+        BlackjackInputView(),
+        BlackjackInputValidator()
+    )
+
+    val playerNames: List<PlayerName> = blackjackController.getPlayerNames()
+}
