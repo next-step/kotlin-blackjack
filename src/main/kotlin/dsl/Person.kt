e931175d18a347348a6949ff151db1c45f7d7775
didrlgus <rlgusdid@naver.com>
2023-11-29 11:50:29 +0900

    feat: 코틀린 DSL 실습하기

---
+++ src/main/kotlin/dsl/Person.kt
@@ -0,0 +1,9 @@
+package dsl
+
+class Person(
+    val name: String,
+    val company: String,
+    val softSkills: List<String>,
+    val hardSkills: List<String>,
+    val languages: Map<String, Int>,
+)
