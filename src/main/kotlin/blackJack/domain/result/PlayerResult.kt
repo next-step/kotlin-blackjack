82fbdb7e6ac5b20f1856b6b902d8902d4e40635b
Ho-Ya-Ho <rkdgh1056@gmail.com>
2023-11-30 21:29:28 +0900

    (update) result 출력 함수 blackjack 조건 추가

--- src/main/kotlin/blackJack/domain/result/PlayerResult.kt
+++ src/main/kotlin/blackJack/domain/result/PlayerResult.kt
@@ -4,13 +4,16 @@ import blackJack.domain.enums.BlackjackResult
 import blackJack.domain.player.Dealer
 import blackJack.domain.player.Player
 
-class PlayerResult(val name: String, val result: BlackjackResult) {
+class PlayerResult(val name: String, val bettingPrice: Int, val result: BlackjackResult) {
     companion object {
         fun calculateResult(player: Player, dealer: Dealer): PlayerResult {
             val dealerScore = dealer.cards.calculateTotalScore()
             val playerScore = player.cards.calculateTotalScore()
 
             val result = when {
+                player.isBlackJack() && dealer.isBlackJack() -> BlackjackResult.DRAW
+                player.isBlackJack() -> BlackjackResult.BLACKJACK
+                dealer.isBlackJack() -> BlackjackResult.LOSE
                 playerScore > 21 -> BlackjackResult.LOSE
                 dealerScore > 21 -> BlackjackResult.WIN
                 playerScore > dealerScore -> BlackjackResult.WIN
@@ -18,7 +21,7 @@ class PlayerResult(val name: String, val result: BlackjackResult) {
                 else -> BlackjackResult.LOSE
             }
 
-            return PlayerResult(player.name, result)
+            return PlayerResult(player.name, player.bettingPrice, result)
         }
     }
 }
