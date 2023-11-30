335541a9000d1fb9eb22fa0e0886537646881148
OYJ <oyj7677@gmail.com>
2023-12-01 00:23:13 +0900

    Feat: 플레이어 카드 점수 계산 기능 추가.

--- src/main/kotlin/card/deck/PlayerDeck.kt
+++ src/main/kotlin/card/deck/PlayerDeck.kt
@@ -1,14 +1,39 @@
 package card.deck
 
+import card.CardRank
 import card.PlayingCard
 
-class PlayerDeck(override val cardDeck: MutableList<PlayingCard>) : CardDeck {
+class PlayerDeck(override val cardDeck: MutableList<PlayingCard> = mutableListOf()) : CardDeck {
 
     fun addCard(playingCard: PlayingCard) {
         cardDeck.add(playingCard)
     }
 
+    fun getResultPoint(): Int {
+        var point = cardDeck.sumOf { it.getPoint() }
+
+        if (isContainAce()) {
+            point += addAcePoint(point)
+        }
+
+        return point
+    }
+
+    private fun isContainAce(): Boolean {
+        return cardDeck.any { it.getCardRank() == CardRank.ACE }
+    }
+
+    private fun addAcePoint(point: Int): Int {
+        return if (point < 11) {
+            ADD_ACE_POINT
+        } else {
+            ADD_ACE_POINT_NONE
+        }
+    }
+
     companion object {
         fun create() = PlayerDeck(mutableListOf())
+        private const val ADD_ACE_POINT = 10
+        private const val ADD_ACE_POINT_NONE = 0
     }
 }
--- src/test/kotlin/blackJack/card/deck/PlayerDeckTest.kt
+++ src/test/kotlin/blackJack/card/deck/PlayerDeckTest.kt
@@ -1,6 +1,9 @@
 package blackJack.card.deck
 
 import card.CardPack
+import card.CardRank
+import card.PlayingCard
+import card.Suit
 import card.deck.PlayerDeck
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -16,5 +19,20 @@ class PlayerDeckTest {
         val actual = playerDeck.cardDeck[0]
 
         assertThat(actual).isEqualTo(CardPack.cards[0])
+    }
+
+    @Test
+    fun `카드 점수 반환`() {
+        // given : 플레이어 덱을 생성하고 카드를 추가한다.
+        val playerDeck = PlayerDeck()
+        playerDeck.addCard(PlayingCard(Suit.DIAMOND, CardRank.ACE))
+        playerDeck.addCard(PlayingCard(Suit.DIAMOND, CardRank.TEN))
+        playerDeck.addCard(PlayingCard(Suit.DIAMOND, CardRank.TEN))
+
+        // when : 점수 계산 요청
+        val actual: Int = playerDeck.getResultPoint()
+
+        // then :
+        assertThat(actual).isEqualTo(21)
     }
 }
