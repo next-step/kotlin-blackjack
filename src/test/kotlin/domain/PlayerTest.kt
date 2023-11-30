01ba12d7b88a24aebbfeddeafd9744d0ab920902
Jaeyeon Cho <cjyeon1022@gmail.com>
2023-11-30 22:18:14 +0900

    [step4] (refactor) 프로덕션 코드 변경에 따른 테스트코드의 일관성 유지를 위한 리팩토링

--- src/test/kotlin/domain/PlayerTest.kt
+++ src/test/kotlin/domain/PlayerTest.kt
@@ -12,7 +12,7 @@ class PlayerTest {
     @Test
     @DisplayName("플레이어가 카드를 받으면 손패에 추가되어야 함")
     fun `플레이어가 카드를 받을 때`() {
-        val player = Player("Jaeyeon")
+        val player = Player("Jaeyeon", 10000)
         val card = Card(Suit.HEARTS, Rank.TWO)
         player.receiveCard(card)
 
@@ -22,7 +22,7 @@ class PlayerTest {
     @Test
     @DisplayName("플레이어의 점수 계산")
     fun `플레이어의 점수 계산`() {
-        val player = Player("Jaeyeon")
+        val player = Player("Jaeyeon", 10000)
         player.receiveCard(Card(Suit.HEARTS, Rank.KING))
         player.receiveCard(Card(Suit.SPADES, Rank.THREE))
         assertEquals(13, player.calculateScore())
@@ -34,32 +34,28 @@ class PlayerTest {
     @Test
     @DisplayName("플레이어가 베팅 금액을 설정할 수 있어야 함")
     fun `플레이어의 베팅 금액 설정`() {
-        val player = Player("Jaeyeon")
-        val bettingAmount = 10000
+        val player = Player("Jaeyeon", 10000)
 
-        player.bettingAmount = bettingAmount
-        assertEquals(bettingAmount, player.bettingAmount)
+        assertEquals(10000, player.bettingAmount.amount())
     }
 
     @Test
     @DisplayName("플레이어의 베팅 금액은 음수가 될 수 없음")
     fun `플레이어의 베팅 금액 음수 설정 불가`() {
-        val player = Player("Jaeyeon")
         val invalidAmount = -10000
 
         assertThrows<IllegalArgumentException> {
-            player.bettingAmount = invalidAmount
+            Player("Jaeyeon", invalidAmount)
         }
     }
 
     @Test
     @DisplayName("플레이어가 블랙잭으로 승리할 경우 수익 계산")
     fun `플레이어의 블랙잭 승리 수익 계산`() {
-        val player = Player("Jaeyeon")
-        player.bettingAmount = 10000
+        val player = Player("Jaeyeon", 10000)
         player.receiveCard(Card(Suit.HEARTS, Rank.ACE))
         player.receiveCard(Card(Suit.DIAMONDS, Rank.KING))
-        player.determineResult(BlackjackRules.MAXIMUM_SCORE - 1)
+        player.determineResult(BlackjackRules.MAXIMUM_SCORE - 1, emptyList())
 
         val profit = player.calculateFinalProfit()
         assertEquals(15000, profit)
@@ -68,11 +64,10 @@ class PlayerTest {
     @Test
     @DisplayName("딜러와 플레이어 모두 블랙잭인 경우 베팅 금액 돌려받음")
     fun `딜러와 플레이어 모두 블랙잭인 경우 수익 계산`() {
-        val player = Player("Jaeyeon")
-        player.bettingAmount = 10000
+        val player = Player("Jaeyeon", 10000)
         player.receiveCard(Card(Suit.HEARTS, Rank.ACE))
         player.receiveCard(Card(Suit.DIAMONDS, Rank.KING))
-        player.determineResult(BlackjackRules.MAXIMUM_SCORE)
+        player.determineResult(BlackjackRules.MAXIMUM_SCORE, listOf(Card(Suit.HEARTS, Rank.ACE), Card(Suit.DIAMONDS, Rank.KING)))
 
         val profit = player.calculateFinalProfit()
         assertEquals(0, profit)
@@ -81,11 +76,10 @@ class PlayerTest {
     @Test
     @DisplayName("플레이어가 승리할 경우 베팅 금액만큼 수익을 얻음")
     fun `플레이어 승리 시 수익 계산`() {
-        val player = Player("Jaeyeon")
-        player.bettingAmount = 10000
+        val player = Player("Jaeyeon", 10000)
         player.receiveCard(Card(Suit.HEARTS, Rank.TEN))
         player.receiveCard(Card(Suit.DIAMONDS, Rank.NINE))
-        player.determineResult(18)
+        player.determineResult(18, emptyList())
 
         val profit = player.calculateFinalProfit()
         assertEquals(10000, profit)
@@ -94,12 +88,11 @@ class PlayerTest {
     @Test
     @DisplayName("플레이어가 패배하거나 버스트인 경우 베팅 금액을 잃음")
     fun `플레이어 패배 또는 버스트 시 수익 계산`() {
-        val player = Player("Jaeyeon")
-        player.bettingAmount = 10000
+        val player = Player("Jaeyeon", 10000)
         player.receiveCard(Card(Suit.HEARTS, Rank.TEN))
         player.receiveCard(Card(Suit.DIAMONDS, Rank.NINE))
         player.receiveCard(Card(Suit.CLUBS, Rank.FOUR))
-        player.determineResult(20)
+        player.determineResult(20, emptyList())
 
         val profit = player.calculateFinalProfit()
         assertEquals(-10000, profit)
--- src/test/kotlin/enum/GameResultTest.kt
+++ src/test/kotlin/enum/GameResultTest.kt
@@ -1,5 +1,6 @@
 package enum
 
+import domain.Card
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -11,9 +12,10 @@ class GameResultTest {
     fun `플레이어 점수가 21을 초과하면 패배`() {
         val playerScore = 22
         val dealerScore = 20
-        val playerHasBlackjack = false
+        val playerCards = listOf(Card(Suit.HEARTS, Rank.TEN), Card(Suit.DIAMONDS, Rank.QUEEN), Card(Suit.CLUBS, Rank.THREE))
+        val dealerCards = listOf(Card(Suit.SPADES, Rank.ACE), Card(Suit.HEARTS, Rank.NINE))
 
-        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerHasBlackjack)
+        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerCards, dealerCards)
         assertEquals(GameResult.LOSE, result)
     }
 
@@ -22,9 +24,10 @@ class GameResultTest {
     fun `딜러 점수가 21을 초과하면 승리`() {
         val playerScore = 20
         val dealerScore = 22
-        val playerHasBlackjack = false
+        val playerCards = listOf(Card(Suit.HEARTS, Rank.NINE), Card(Suit.CLUBS, Rank.ACE))
+        val dealerCards = listOf(Card(Suit.DIAMONDS, Rank.QUEEN), Card(Suit.SPADES, Rank.KING), Card(Suit.HEARTS, Rank.THREE))
 
-        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerHasBlackjack)
+        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerCards, dealerCards)
         assertEquals(GameResult.WIN, result)
     }
 
@@ -33,9 +36,10 @@ class GameResultTest {
     fun `플레이어 점수가 딜러 점수보다 높으면 승리`() {
         val playerScore = 20
         val dealerScore = 18
-        val playerHasBlackjack = false
+        val playerCards = listOf(Card(Suit.SPADES, Rank.TEN), Card(Suit.CLUBS, Rank.QUEEN))
+        val dealerCards = listOf(Card(Suit.HEARTS, Rank.SEVEN), Card(Suit.DIAMONDS, Rank.JACK))
 
-        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerHasBlackjack)
+        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerCards, dealerCards)
         assertEquals(GameResult.WIN, result)
     }
 
@@ -44,9 +48,10 @@ class GameResultTest {
     fun `플레이어 점수와 딜러 점수가 같으면 무승부`() {
         val playerScore = 20
         val dealerScore = 20
-        val playerHasBlackjack = false
+        val playerCards = listOf(Card(Suit.DIAMONDS, Rank.TEN), Card(Suit.HEARTS, Rank.QUEEN))
+        val dealerCards = listOf(Card(Suit.CLUBS, Rank.KING), Card(Suit.SPADES, Rank.JACK))
 
-        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerHasBlackjack)
+        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerCards, dealerCards)
         assertEquals(GameResult.DRAW, result)
     }
 
@@ -55,9 +60,10 @@ class GameResultTest {
     fun `플레이어 점수가 딜러 점수보다 낮으면 패배`() {
         val playerScore = 17
         val dealerScore = 20
-        val playerHasBlackjack = false
+        val playerCards = listOf(Card(Suit.HEARTS, Rank.EIGHT), Card(Suit.DIAMONDS, Rank.NINE))
+        val dealerCards = listOf(Card(Suit.CLUBS, Rank.TEN), Card(Suit.SPADES, Rank.QUEEN))
 
-        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerHasBlackjack)
+        val result = GameResult.determineForResultOfPlayer(playerScore, dealerScore, playerCards, dealerCards)
         assertEquals(GameResult.LOSE, result)
     }
 }
