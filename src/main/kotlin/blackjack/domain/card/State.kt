26bf370d7e5416f85a6d6d2ba97f33392f798644
parker1609 <psbum1609@gmail.com>
2023-12-01 18:59:03 +0900

    chore: 사용하지 않는 코드 삭제

--- src/main/kotlin/blackjack/domain/card/State.kt
+++ src/main/kotlin/blackjack/domain/card/State.kt
@@ -2,31 +2,9 @@ package blackjack.domain.card
 
 enum class State(val isFinished: Boolean) {
 
-    BEGIN(false) {
-        override fun profit(bet: Int): Int {
-            return 0
-        }
-    },
-    HIT(false) {
-        override fun profit(bet: Int): Int {
-            return 0
-        }
-    },
-    STAY(true) {
-        override fun profit(bet: Int): Int {
-            return bet
-        }
-    },
-    BLACKJACK(true) {
-        override fun profit(bet: Int): Int {
-            return (bet * 1.5).toInt()
-        }
-    },
-    BUST(true) {
-        override fun profit(bet: Int): Int {
-            return -bet
-        }
-    };
-
-    abstract fun profit(bet: Int): Int
+    BEGIN(false),
+    HIT(false),
+    STAY(true),
+    BLACKJACK(true),
+    BUST(true);
 }
