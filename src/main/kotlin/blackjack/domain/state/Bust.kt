62a4ac4b9c552943796e4cc3cef9ebef707de3e5
seongmo-jeon <oeeen3@gmail.com>
2023-12-02 18:03:23 +0900

    refactor: 불필요한 함수 제거

--- src/main/kotlin/blackjack/domain/state/Bust.kt
+++ src/main/kotlin/blackjack/domain/state/Bust.kt
@@ -1,10 +1,5 @@
 package blackjack.domain.state
 
-import blackjack.domain.card.Cards
 import blackjack.domain.card.Hands
 
-class Bust(override val rate: Double = 0.0, override val hands: Hands) : Finished() {
-    override fun getCards(): Cards {
-        return Cards(hands.cards)
-    }
-}
+class Bust(override val rate: Double = 0.0, override val hands: Hands) : Finished()
