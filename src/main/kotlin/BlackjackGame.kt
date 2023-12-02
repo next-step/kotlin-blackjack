07ed6eb87bedb79695208ef0f9f94111ad48ce87
OYJ <oyj7677@gmail.com>
2023-12-02 15:05:47 +0900

    Refactor: 블랙잭 게임 로직 재구현

--- src/main/kotlin/BlackjackGame.kt
+++ src/main/kotlin/BlackjackGame.kt
@@ -2,41 +2,54 @@ import card.deck.GameDeck
 import player.Player
 import player.PlayerGroup
 import player.Status
+import view.InputView
+import view.OutputView
 
-class BlackjackGame(private val cardDeck: GameDeck, private val playerGroup: PlayerGroup) {
+class BlackjackGame(private val cardDeck: GameDeck, val playerGroup: PlayerGroup) {
 
     init {
-        playerGroup.playerList.forEach {
-            distributeTwoCards(it)
+        for (player in playerGroup.playerList) {
+            settingCard(player)
         }
     }
 
-    fun hit() {
-        val player = playerGroup.getCurrentPlayer()
-        player.saveCard(cardDeck.getCardWithIncrease())
-        player.updateStatus()
-    }
-
-    fun getCurrentPlayer(): Player {
-        return playerGroup.getCurrentPlayer()
-    }
-
-    // 턴오버.
-    fun playerTurnOver() {
-        val player = playerGroup.getCurrentPlayer()
-        if (player.status == Status.STAND) {
-            playerGroup.turnOverPlayer()
+    fun start() {
+        for (player in playerGroup.playerList) {
+            gamePlay(player)
         }
     }
 
-    fun hitDone() {
-        val player = playerGroup.getCurrentPlayer()
-        player.playDone()
+    private fun gamePlay(player: Player) {
+        while (player.status == Status.PLAYING) {
+            val response = InputView.askForHit(player.name)
+            handleForResponse(response, player)
+            OutputView.showPlayingCard(player)
+        }
     }
 
-    private fun distributeTwoCards(player: Player) {
+    private fun handleForResponse(response: String, player: Player) {
+        when (response.uppercase()) {
+            TEXT_ANSWER_YES-> {
+                player.saveCard(cardDeck.getCardWithIncrease())
+            }
+            TEXT_ANSWER_NO -> {
+                player.playDone()
+            }
+            else -> {
+                println(TEXT_RETRY_INPUT)
+            }
+        }
+    }
+
+    private fun settingCard(player: Player) {
         repeat(2) {
             player.saveCard(cardDeck.getCardWithIncrease())
         }
+    }
+
+    companion object {
+        private const val TEXT_RETRY_INPUT = "Y 혹은 N만 입력 가능합니다."
+        private const val TEXT_ANSWER_YES = "Y"
+        private const val TEXT_ANSWER_NO = "N"
     }
 }
