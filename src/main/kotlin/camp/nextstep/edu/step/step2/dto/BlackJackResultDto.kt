8ee2074338958f66bbeef05d0f5fe4854c2bfe4f
kimueibin <at365@naver.com>
2023-12-04 21:59:25 +0900

    feat : View 시, 필요한 Dto 생성

---
+++ src/main/kotlin/camp/nextstep/edu/step/step2/dto/BlackJackResultDto.kt
@@ -0,0 +1,8 @@
+package camp.nextstep.edu.step.step2.dto
+
+data class BlackJackResultDto(
+    val playerName: String,
+    val playerCards: String,
+    val playerScore: Int
+) {
+}
---
+++ src/main/kotlin/camp/nextstep/edu/step/step2/dto/PlayerCardDto.kt
@@ -0,0 +1,6 @@
+package camp.nextstep.edu.step.step2.dto
+
+data class PlayerCardDto(
+    val name: String,
+    val cards: String
+)
