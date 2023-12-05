60c5695431a92d773b58ae4381ad003729a982eb
JunHyun Lee <dlwnsgus777@naver.com>
2023-12-05 22:57:56 +0900

    feat: 점수 계산 버그 수정

--- src/main/kotlin/blackjack/GameResult.kt
+++ src/main/kotlin/blackjack/GameResult.kt
@@ -12,24 +12,35 @@ class GameResult(
     val resultMap: Map<Name, Result>
 
     init {
-
-        val (winCount, loseCount) = when {
-            dealer.isBust -> players.partition { !it.isBust }
-            else -> players.partition { it.resultScore() < dealer.resultScore() }
-        }
+        val winner = findWinner(players, dealer)
+        val loser = findLoser(players, dealer)
 
         val playerResult = mutableMapOf<Name, Result>().apply {
-            winCount.map { this[it.name] = Result.Win() }
-            loseCount.map { this[it.name] = Result.Lose() }
+            loser.map { this[it.name] = Result.Lose() }
+            winner.map { this[it.name] = Result.Win() }
         }
 
         resultMap = mapOf(
-            Name("딜러") to Result.DealerResult(winCount.size, loseCount.size)
+            Name("딜러") to Result.DealerResult(loser.size, winner.size)
         ) + playerResult
     }
 
-    companion object {
-        private const val WIN: String = "승"
-        private const val LOSE: String = "패"
+    private fun findWinner(players: List<Player>, dealer: Dealer): List<Player> {
+        if (dealer.isBust) {
+            return players.filter { !it.isBust }
+        }
+
+        return players.filter { !it.isBust }.filter {  it.resultScore() > dealer.resultScore() }
+    }
+
+    private fun findLoser(players: List<Player>, dealer: Dealer): List<Player> {
+        if (dealer.isBust) {
+            return players.filter { it.isBust }
+        }
+
+        val bustPlayer = players.filter { it.isBust }
+        val loser = players.filter { !it.isBust }.filter { it.resultScore() < dealer.resultScore() }
+
+        return bustPlayer + loser
     }
 }
