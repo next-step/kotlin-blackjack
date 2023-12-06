ff53d9990283d6f723994681ea000b44137361a2
OYJ <oyj7677@gmail.com>
2023-12-07 02:12:55 +0900

    BlackjackGame 생성자로 ViewInterFace 추가.

--- src/main/kotlin/BlackjackGame.kt
+++ src/main/kotlin/BlackjackGame.kt
@@ -5,7 +5,12 @@ import player.Status
 import view.InputViewInterface
 import view.OutputViewInterface
 
-class BlackjackGame(private val cardDeck: CardDeck, val playerGroup: PlayerGroup, private val inputView: InputViewInterface, private val outputView: OutputViewInterface) {
+class BlackjackGame(
+    private val cardDeck: CardDeck,
+    val playerGroup: PlayerGroup,
+    private val inputView: InputViewInterface,
+    private val outputView: OutputViewInterface,
+) {
 
     init {
         for (player in playerGroup.playerList) {
@@ -51,6 +56,5 @@ class BlackjackGame(private val cardDeck: CardDeck, val playerGroup: PlayerGroup
         private const val TEXT_RETRY_INPUT = "Y 혹은 N만 입력 가능합니다."
         private const val TEXT_ANSWER_YES = "Y"
         private const val TEXT_ANSWER_NO = "N"
-
     }
 }
