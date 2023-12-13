4f96e2fc8c0c41e0af159ae66a9782043f99a298
Alex <kpmyung@gmail.com>
2023-12-14 00:57:32 +0900

    feat: 게임이 종료되면 각 플레이어의 현재 패와 결과 점수를 출력한다

--- src/main/kotlin/game/blackjack/BlackjackApplication.kt
+++ src/main/kotlin/game/blackjack/BlackjackApplication.kt
@@ -3,26 +3,6 @@ package game.blackjack
 import game.blackjack.domain.Deck
 import game.blackjack.domain.Participant
 
-/*
-    게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)
-    pobi,jason
-    
-    pobi, jason에게 2장의 나누었습니다.
-    pobi카드: 2하트, 8스페이드
-    jason카드: 7클로버, K스페이드
-    
-    pobi는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)
-    y
-    pobi카드: 2하트, 8스페이드, A클로버
-    pobi는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)
-    n
-    jason은 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)
-    n
-    jason카드: 7클로버, K스페이드
-    
-    pobi카드: 2하트, 8스페이드, A클로버 - 결과: 21
-    jason카드: 7클로버, K스페이드 - 결과: 17
-*/
 fun main() {
     // 게임에 참여할 사람을 입력받는다.
     val participants = readln().split(",").map { Participant(it) }
@@ -47,5 +27,10 @@ fun main() {
             participant.drawCard(deck.drawOnce())
             println(participant)
         }
+    }
+
+    participants.forEach {
+        print(it)
+        println(" - 결과: ${it.getScore()}")
     }
 }
