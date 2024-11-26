d0d8214591e7f04fd9509c6e3b96e7635fc8f79c
SeokHo-Ham <forkyy.dev@gmail.com>
2024-11-26 23:30:03 +0900

    step1: DslTest 완성

---
+++ src/test/kotlin/study/DslTest.kt
@@ -0,0 +1,84 @@
+package study
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+/**
+ * introduce {
+ *   name("홍길동")
+ *   company("활빈당")
+ *   skills {
+ *     soft("A passion for problem solving")
+ *     soft("Good communication skills")
+ *     hard("Kotlin")
+ *   }
+ *   languages {
+ *     "Korean" level 5
+ *     "English" level 3
+ *   }
+ * }
+ *
+ */
+class DslTest {
+    @ValueSource(strings = ["홍길동", "홍"])
+    @ParameterizedTest
+    fun name(name: String) {
+        val person =
+            introduce {
+                name(name)
+            }
+        person.name shouldBe name
+    }
+
+    @Test
+    fun company() {
+        val person =
+            introduce {
+                name("홍길동")
+                company("활빈당")
+            }
+        person.company shouldBe "활빈당"
+    }
+
+    @Test
+    fun skills() {
+        val person =
+            introduce {
+                name("홍길동")
+                company("활빈당")
+                skills {
+                    soft("A passion for problem solving")
+                    soft("Good communication skills")
+                    hard("Kotlin")
+                }
+            }
+
+        person.skills?.soft shouldBe listOf("A passion for problem solving", "Good communication skills")
+        person.skills?.hard shouldBe listOf("Kotlin")
+    }
+
+    @Test
+    fun languages() {
+        val person =
+            introduce {
+                name("홍길동")
+                company("활빈당")
+                skills {
+                    soft("A passion for problem solving")
+                    soft("Good communication skills")
+                    hard("Kotlin")
+                }
+                languages {
+                    "Korean" level 5
+                    "English" level 3
+                }
+            }
+
+        person.languages!!.languages[0].language shouldBe "Korean"
+        person.languages!!.languages[0].level shouldBe 5
+        person.languages!!.languages[1].language shouldBe "English"
+        person.languages!!.languages[1].level shouldBe 3
+    }
+}
