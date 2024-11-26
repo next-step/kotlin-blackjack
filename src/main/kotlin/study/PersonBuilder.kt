773fc605566959d10146d2785c1ea1e43cd7ef9f
boradol2 <boradol0902@gmail.com>
2024-11-26 23:08:19 +0900

    feat(Person): Person : name, company - DSL 구현

---
+++ src/main/kotlin/study/PersonBuilder.kt
@@ -0,0 +1,17 @@
+package study
+class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+
+    fun name(value: String) {
+        name = value
+    }
+
+    fun company(value: String) {
+        company = value
+    }
+
+    fun build(): Person {
+        return Person(name, company)
+    }
+}
