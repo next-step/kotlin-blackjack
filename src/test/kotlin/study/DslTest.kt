52a690b181dd3a3a45e7e2ec17363dd8aa852b5a
saerang <lsg10119@gamail.com>
2024-11-26 23:20:34 +0900

    feat(step1): skills 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -40,17 +40,34 @@ class DslTest {
             }
         person.company shouldBe "활빈당"
     }
+
+    @Test
+    fun skills() {
+        val person =
+            introduce {
+                name("홍길동")
+                company("활빈당")
+                skills {
+                    soft("A passion for problem solving")
+                    soft("Good communication skills")
+                    hard("Kotlin")
+                }
+            }
+        person.skills.soft shouldBe listOf("A passion for problem solving", "Good communication skills")
+        person.skills.hard shouldBe listOf("Kotlin")
+    }
 }
 
 fun introduce(block: PersonBuilder.() -> Unit): Person {
     return PersonBuilder().apply(block).build()
 }
 
-data class Person(val name: String, val company: String?)
+data class Person(val name: String, val company: String?, val skills: Skills)
 
 class PersonBuilder {
     private lateinit var name: String
     private var company: String? = null
+    private var skills: Skills = Skills()
 
     fun name(value: String) {
         this.name = value
@@ -60,7 +77,33 @@ class PersonBuilder {
         this.company = value
     }
 
+    fun skills(value: SkillsBuilder.() -> Unit) {
+        this.skills = SkillsBuilder().apply(value).build()
+    }
+
     fun build(): Person {
-        return Person(name, company)
+        return Person(name, company, skills)
+    }
+}
+
+data class Skills(
+    val soft: List<String> = emptyList(),
+    val hard: List<String> = emptyList(),
+)
+
+class SkillsBuilder {
+    private val soft = mutableListOf<String>()
+    private val hard = mutableListOf<String>()
+
+    fun soft(skill: String) {
+        soft.add(skill)
+    }
+
+    fun hard(skill: String) {
+        hard.add(skill)
+    }
+
+    fun build(): Skills {
+        return Skills(soft, hard)
     }
 }
