ba8df642189fc2404d370d8f31be3e074c9dd14e
leekyuwon <kyuwoon369@gmail.com>
2024-11-26 23:13:06 +0900

    test: 테스트 케이스 추가

--- src/test/kotlin/study/DslTest.kt
+++ src/test/kotlin/study/DslTest.kt
@@ -1,10 +1,12 @@
 package study
 
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 import study.dsl.introduce
+import kotlin.streams.asStream
 
 class DslTest {
     @ValueSource(strings = ["홍길동", "이규원"])
@@ -29,41 +31,54 @@ class DslTest {
         person.company shouldBe company
     }
 
-    @Test
-    fun skills() {
+    @CsvSource(
+        "A passion for problem solving, Kotlin",
+        "Good communication skills, Kotlin",
+        "Critical thinking, Java",
+    )
+    @ParameterizedTest
+    fun skills(
+        softSkill: String,
+        hardSkill: String,
+    ) {
         val person =
             introduce {
                 name("이규원")
                 company("한빛앤")
                 skills {
-                    soft("A passion for problem solving")
-                    soft("Good communication skills")
-                    hard("Kotlin")
+                    soft(softSkill)
+                    hard(hardSkill)
                 }
             }
 
-        person.skills?.soft shouldBe listOf("A passion for problem solving", "Good communication skills")
-        person.skills?.hard shouldBe listOf("Kotlin")
+        person.skills?.soft shouldBe listOf(softSkill)
+        person.skills?.hard shouldBe listOf(hardSkill)
     }
 
-    @Test
-    fun languages() {
+    companion object {
+        @JvmStatic
+        fun languageData() =
+            sequenceOf(
+                mapOf("Korean" to 5, "English" to 3),
+                mapOf("Japanese" to 4, "Spanish" to 2),
+                mapOf("French" to 3, "German" to 4),
+            ).asStream()
+    }
+
+    @MethodSource("languageData")
+    @ParameterizedTest
+    fun languages(languages: Map<String, Int>) {
         val person =
             introduce {
                 name("이규원")
                 company("한빛앤")
-                skills {
-                    soft("A passion for problem solving")
-                    soft("Good communication skills")
-                    hard("Kotlin")
-                }
                 languages {
-                    "Korean" level 5
-                    "English" level 3
+                    languages.forEach { (lang, level) ->
+                        lang level level
+                    }
                 }
             }
 
-        person.skills?.soft shouldBe listOf("A passion for problem solving", "Good communication skills")
-        person.skills?.hard shouldBe listOf("Kotlin")
+        person.languages?.languages shouldBe languages
     }
 }
