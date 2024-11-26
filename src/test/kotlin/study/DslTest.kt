d45faaa42d3af3b229c98cfa42fda4f8a0bff79d
Park GyeongCheol <dev.pkch@gmail.com>
2024-11-26 23:56:11 +0900

    [step1] 코틀린 DSL (#710)
    * [step1] hand-on DSL 실습
    
    * [step1] 스킬 정보 DSL 추가
    
    * [step1] 언어 정보 DSL 추가

---
+++ src/test/kotlin/study/DslTest.kt
@@ -0,0 +1,145 @@
+package study
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.data.forAll
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+
+/**
+ * introduce {
+ *   name("박경철")
+ *   company("우아한형제들")
+ *   skills {
+ *     soft("A passion for problem solving")
+ *     soft("Good communication skills")
+ *     hard("Kotlin")
+ *   }
+ *   languages {
+ *     "Korean" level 5
+ *     "English" level 3
+ *   }
+ * }
+ */
+class DslTest : StringSpec({
+    "사람 이름을 설정한다." {
+        listOf(
+            "박경철" to "박경철",
+            "홍길동" to "홍길동",
+        )
+            .forAll { (name, expected) ->
+                introduce { name(name) }.name shouldBe expected
+            }
+    }
+
+    "사람, 회사 이름을 설정한다." {
+        val person = introduce {
+            name("박경철")
+            company("우아한형제들")
+        }
+
+        person.company shouldBe "우아한형제들"
+    }
+
+    "스킬 정보를 설정한다." {
+        val person = introduce {
+            name("박경철")
+            company("우아한형제들")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+        }
+
+        person.softSkills shouldBe listOf("A passion for problem solving", "Good communication skills")
+        person.hardSkills shouldBe listOf("Kotlin")
+    }
+
+    "언어 정보를 설정한다." {
+        val person = introduce {
+            name("박경철")
+            company("우아한형제들")
+            skills {
+                soft("A passion for problem solving")
+                soft("Good communication skills")
+                hard("Kotlin")
+            }
+            languages {
+                "Korean" level 5
+                "English" level 3
+            }
+        }
+
+        person.languages["Korean"] shouldBe 5
+        person.languages["English"] shouldBe 3
+    }
+})
+
+private fun introduce(builder: PersonBuilder.() -> Unit): Person =
+    PersonBuilder().apply(builder).build()
+
+private class Person(
+    val name: String,
+    val company: String?,
+    val softSkills: List<String>,
+    val hardSkills: List<String>,
+    val languages: Map<String, Int>,
+)
+
+private class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+    private var softSkills: List<String> = emptyList()
+    private var hardSkills: List<String> = emptyList()
+    private var languages: MutableMap<String, Int> = mutableMapOf()
+
+    fun name(value: String) {
+        this.name = value
+    }
+
+    fun company(value: String) {
+        this.company = value
+    }
+
+    fun skills(builder: SkillsBuilder.() -> Unit) {
+        val skillsBuilder = SkillsBuilder().apply(builder)
+        this.softSkills = skillsBuilder.softSkills
+        this.hardSkills = skillsBuilder.hardSkills
+    }
+
+    fun languages(builder: LanguageBuilder.() -> Unit) {
+        val languageBuilder = LanguageBuilder().apply(builder)
+        this.languages = languageBuilder.languages
+    }
+
+    fun build(): Person =
+        Person(
+            name = this.name,
+            company = this.company,
+            softSkills = this.softSkills,
+            hardSkills = this.hardSkills,
+            languages = this.languages,
+        )
+}
+
+private class SkillsBuilder {
+    val softSkills = mutableListOf<String>()
+    val hardSkills = mutableListOf<String>()
+
+    fun soft(value: String) {
+        softSkills.add(value)
+    }
+
+    fun hard(value: String) {
+        hardSkills.add(value)
+    }
+}
+
+private class LanguageBuilder {
+    val languages = mutableMapOf<String, Int>()
+
+    infix fun String.level(level: Int) {
+        languages[this] = level
+    }
+}
+
