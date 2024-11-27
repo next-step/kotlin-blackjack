60a83996a2ac2cf9c2d692980644f6178dae5df9
eunice0035 <56648464+eunice0035@users.noreply.github.com>
2024-11-27 23:36:15 +0900

    [Step1] 코틀린 DSL (#709)
    * feat: add dsl test
    
    * feat: lint check

---
+++ src/test/kotlin/study/builder/SkillsBuilder.kt
@@ -0,0 +1,24 @@
+package study.builder
+
+data class Skill(val name: String, val type: SkillType) {
+    companion object {
+        enum class SkillType {
+            SOFT,
+            HARD,
+        }
+    }
+}
+
+class SkillsBuilder {
+    private val skills = mutableListOf<Skill>()
+
+    fun soft(skill: String) {
+        skills.add(Skill(skill, Skill.Companion.SkillType.SOFT))
+    }
+
+    fun hard(skill: String) {
+        skills.add(Skill(skill, Skill.Companion.SkillType.HARD))
+    }
+
+    fun build(): List<Skill> = skills
+}
