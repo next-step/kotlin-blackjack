b31149c4db19cebdbc47ca0e84e5834559a4fff3
pkch93 <dev.pkch@gmail.com>
2024-11-27 20:20:23 +0900

    [step2] 플레이어가 카드를 뽑을때 카드의 총 합계가 21 이상이면 카드 뽑지 않는 테스트 케이스 추가

--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -76,6 +76,21 @@ class PlayerTest : FreeSpec({
         sut.currentCards shouldBe listOf(DrawCard(CardSuit.HEARTS, CardNumber.TWO))
     }
 
+    "카드를 핸드에 추가할 때 카드의 총 합이 21 이상이면 더이상 카드를 뽑지 않는다." - {
+        val sut = Player(
+            name = PlayerName("테스트"),
+            cards = mutableListOf(
+                Card(CardSuit.HEARTS, CardNumber.TEN),
+                Card(CardSuit.HEARTS, CardNumber.TEN),
+            ),
+            draw = true,
+        )
+
+        sut.addCard(Card(CardSuit.HEARTS, CardNumber.TWO))
+
+        sut.canDraw() shouldBe false
+    }
+
     "카드를 더이상 뽑지 않는다." - {
         val sut = Player(PlayerName("테스트"))
 
