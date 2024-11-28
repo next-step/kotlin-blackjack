50b0b14a7bff8f837838b6f87a38e425f8a8169a
SeongminKimKR <gvf156@gmail.com>
2024-11-28 13:32:26 +0900

    feat: 카드들의 숫자합은 카드들에 에이스가 포함되어있으면 21을 넘지 않는 선에서 1이아닌 대체값(11)을 합산한 값을 반환한다.

--- src/main/kotlin/blackjack/domain/card/Cards.kt
+++ src/main/kotlin/blackjack/domain/card/Cards.kt
@@ -7,7 +7,18 @@ class Cards(private val cards: MutableList<Card> = mutableListOf()) {
 
     fun getCards(): List<Card> = cards
 
-    fun sum(): Int = cards.sumOf { it.number.value }
+    fun sum(): Int {
+        var totalSum = cards.sumOf { it.number.value }
+        var aceCount = cards.filter { it.number == CardNumber.Ace }.size;
+
+        while (--aceCount >= 0 && isGreaterThanMaxSum(totalSum + CardNumber.Ace.toEleven())) {
+            totalSum += CardNumber.Ace.toEleven()
+        }
+
+        return totalSum
+    }
+
+    private fun isGreaterThanMaxSum(value: Int) = value <= MAX_CARDS_NUM_SUM
 
     companion object {
         const val MAX_CARDS_NUM_SUM = 21
--- src/test/kotlin/blackjack/domain/card/CardsTest.kt
+++ src/test/kotlin/blackjack/domain/card/CardsTest.kt
@@ -7,12 +7,12 @@ class CardsTest : StringSpec({
     "카드들은 숫자의 합을 반환할 수 있다." {
         val cards = Cards(
             mutableListOf(
-                Card(shape = CardShape.Heart, number = CardNumber.Ace),
+                Card(shape = CardShape.Heart, number = CardNumber.Three),
                 Card(shape = CardShape.Heart, number = CardNumber.Two),
             )
         )
 
-        cards.sum() shouldBe 3
+        cards.sum() shouldBe 5
     }
 
     "카드들은 새로운 카드를 추가할 수 있다." {
@@ -22,5 +22,16 @@ class CardsTest : StringSpec({
 
         cards.getCards().size shouldBe 1
         cards.getCards()[0] shouldBe card
+    }
+
+    "카드들의 숫자합은 카드들에 에이스가 포함되어있으면 21을 넘지 않는 선에서 대체값(11)을 합산한 값을 반환한다." {
+        val cards = Cards(
+            mutableListOf(
+                Card(shape = CardShape.Heart, number = CardNumber.Ten),
+                Card(shape = CardShape.Spade, number = CardNumber.Ace),
+            )
+        )
+
+        cards.sum() shouldBe 21
     }
 })
