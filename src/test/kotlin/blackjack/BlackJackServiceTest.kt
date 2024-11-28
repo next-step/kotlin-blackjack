36dc490b4f5b7edf11dbacc411ecef5b3dcd48e5
KimDabomi <cdabomi@nate.com>
2024-11-28 20:36:34 +0900

    [step2] feat: 카드 생성

--- src/test/kotlin/blackjack/BlackJackServiceTest.kt
+++ src/test/kotlin/blackjack/BlackJackServiceTest.kt
@@ -9,11 +9,31 @@ class BlackJackService {
     }
 }
 
+class Card {
+    val cardList = mutableListOf<String>()
+    val cardShape = listOf("하트", "클로버", "스페이드", "다이아")
+    val cardNumber = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+
+    fun createCard() {
+        for (shape in cardShape) {
+            for (number in cardNumber) {
+                cardList.add(number + shape)
+            }
+        }
+    }
+}
+
 class BlackJackServiceTest : StringSpec ({
     "사용자의 입력값을 쉼표 기준으로 분리한다." {
         val blackJackService = BlackJackService()
         val input = "kim,da"
         val result = blackJackService.splitInput(input)
         result shouldBe listOf("kim", "da")
+    }
+
+    "카드를 생성한다." {
+        val card = Card()
+        card.createCard()
+        card.cardList.size shouldBe 52
     }
 })
