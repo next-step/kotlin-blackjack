820bd51525b6a8d60e87baadf9dbed98083f30a7
Duhan Mo <duhandv@gmail.com>
2024-11-30 00:48:18 +0900

    test: 랭크가 J,Q,K 이면 점수는 10점으로 계산된다

--- src/test/kotlin/blackjack/BlackJackTest.kt
+++ src/test/kotlin/blackjack/BlackJackTest.kt
@@ -1,5 +1,6 @@
 package blackjack
 
+import blackjack.Rank.Companion.ACE
 import blackjack.Suit.SPADE
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
@@ -13,26 +14,39 @@ enum class Suit {
     CLUB,
 }
 
-
-data class Rank(val rank: String) {
+@JvmInline
+value class Rank(private val value: String) {
     init {
-        require(rank in ranks) { "랭크는 $rank 이어야 합니다." }
+        require(value in RANK_VALUES) { "랭크는 $RANK_VALUES 에 포함되어야 합니다." }
     }
 
     val score: Int
-        get() = rank.toInt()
+        get() =
+            when (value) {
+                in NUMBER_VALUES -> value.toInt()
+                in FACE_VALUES -> 10
+                else -> throw IllegalArgumentException("에이스는 경우에 따라 점수를 합산해야 합니다")
+            }
 
     companion object {
-        private const val MIN_NUMBER = 2
-        private const val MAX_NUMBER = 10
-        private const val ACE = "A"
-        private val NUMBERS = (MIN_NUMBER..MAX_NUMBER).map { it.toString() }
-        private val FACES = listOf("J", "Q", "K")
-        private val ranks = NUMBERS + FACES + ACE
+        private const val ACE_VALUE = "A"
+
+        private val NUMBER_VALUES = listOf("2", "3", "4", "5", "6", "7", "8", "9")
+        private val FACE_VALUES = listOf("J", "Q", "K")
+        private val RANK_VALUES = NUMBER_VALUES + FACE_VALUES + ACE_VALUE
+
+        val ACE = Rank(ACE_VALUE)
     }
 }
 
 data class Card(val rank: Rank, val suit: Suit)
+
+data class Cards(val cards: List<Card>) {
+    fun calculateScore(): Int {
+        return cards.filterNot { it.rank == ACE }
+            .sumOf { it.rank.score }
+    }
+}
 
 class BlackjackTest : StringSpec({
     "카드는 랭크와 문양으로 이루어진다" {
@@ -49,7 +63,13 @@ class BlackjackTest : StringSpec({
 
     "랭크가 숫자이면 점수는 숫자의 값으로 계산된다" {
         listOf("2", "3", "4", "5", "6").forAll { number ->
-            Rank(number).score shouldBe number.toInt()
+            Cards(listOf(Card(Rank(number), SPADE))).calculateScore() shouldBe number.toInt()
+        }
+    }
+
+    "랭크가 J,Q,K 이면 점수는 10점으로 계산된다" {
+        listOf("J", "Q", "K").forAll { face ->
+            Cards(listOf(Card(Rank(face), SPADE))).calculateScore() shouldBe 10
         }
     }
 })
