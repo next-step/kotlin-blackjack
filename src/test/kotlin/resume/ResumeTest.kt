625163663ecb31cd974b63a03ff06972252924d4
Giwan Kim <giwan.kim@gmail.com>
2024-11-29 21:09:48 +0900

    feat: 이름을 세팅할 수 있는 기능을 구현했다.

---
+++ src/test/kotlin/resume/ResumeTest.kt
@@ -0,0 +1,42 @@
+package resume
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+/*
+introduce {
+  name("Elon Musk")
+  company("DOGE")
+  skills {
+    soft("A passion for problem solving")
+    soft("Posting dank memes")
+    hard("Shooting rockets into space")
+  }
+  languages {
+    "English" level 5
+    "Afrikaans" level 3
+  }
+}
+*/
+class ResumeTest {
+    @ParameterizedTest
+    @ValueSource(strings = ["Elon Musk", "홍길동"])
+    fun name(value: String) {
+        val person =
+            introduce {
+                name(value)
+            }
+        person.name shouldBe value
+    }
+}
+
+fun introduce(init: Person.() -> Unit) = Person("").apply(init)
+
+data class Person(
+    var name: String,
+) {
+    fun name(value: String) {
+        name = value
+    }
+}
