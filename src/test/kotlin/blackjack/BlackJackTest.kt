527b17c799a268931b0076cc182f5f9a31ac8415
Duhan Mo <duhandv@gmail.com>
2024-11-29 23:53:03 +0900

    test: 랭크가 숫자이면 점수는 숫자의 값으로 계산된다

--- src/test/kotlin/blackjack/BlackJackTest.kt
+++ src/test/kotlin/blackjack/BlackJackTest.kt
@@ -13,11 +13,14 @@ enum class Suit {
     CLUB,
 }
 
-@JvmInline
-value class Rank(val rank: String) {
+
+data class Rank(val rank: String) {
     init {
         require(rank in ranks) { "랭크는 $rank 이어야 합니다." }
     }
+
+    val score: Int
+        get() = rank.toInt()
 
     companion object {
         private const val MIN_NUMBER = 2
@@ -41,6 +44,12 @@ class BlackjackTest : StringSpec({
     "카드의 랭크가 2~10,J,Q,K,A가 아닌 경우 예외 발생한다" {
         listOf("0", "1", "-1", "B", "D").forAll { invalidRank ->
             shouldThrow<IllegalArgumentException> { Rank(invalidRank) }
+        }
+    }
+
+    "랭크가 숫자이면 점수는 숫자의 값으로 계산된다" {
+        listOf("2", "3", "4", "5", "6").forAll { number ->
+            Rank(number).score shouldBe number.toInt()
         }
     }
 })
