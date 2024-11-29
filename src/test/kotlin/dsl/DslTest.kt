6197f02ada5232fd1f7634eb52f786ebf0e447b1
wooobo <irooori89@gmail.com>
2024-11-29 09:37:03 +0900

    feat: introduce 테스트 추가

---
+++ src/test/kotlin/dsl/DslTest.kt
@@ -0,0 +1,58 @@
+package dsl
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class DslTest {
+    @ValueSource(strings = ["홍길동", "철수"])
+    @ParameterizedTest
+    fun introduce(value: String) {
+        val person = introduce {
+            name(value)
+        }
+        assertThat(person.name).isEqualTo(value)
+    }
+
+    @Test
+    fun company() {
+        val person = introduce {
+            name("홍길동")
+            company("우리회사")
+        }
+        assertThat(person.name).isEqualTo("홍길동")
+        assertThat(person.company).isEqualTo("우리회사")
+    }
+
+    @Test
+    fun `all test`() {
+        introduce {
+            name("박재성")
+            company("우아한형제들")
+        }
+    }
+}
+
+fun introduce(block: PersonBuilder.() -> Unit): Person {
+    return PersonBuilder().apply(block).build()
+}
+
+class PersonBuilder {
+    private lateinit var name: String
+    private var company: String? = null
+
+    fun name(value: String) {
+        name = value
+    }
+
+    fun company(value: String) {
+        company = value
+    }
+
+    fun build(): Person {
+        return Person(name, company)
+    }
+}
+
+data class Person(val name: String, val company: String?)
