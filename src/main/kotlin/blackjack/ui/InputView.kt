197cb4cc0c0a6b2d73c6bef609b19ac40937364d
eunice0035 <eunice7357@gmail.com>
2024-11-29 22:12:14 +0900

    feat: add input view test and logic

--- src/main/kotlin/blackjack/ui/InputView.kt
+++ src/main/kotlin/blackjack/ui/InputView.kt
@@ -2,14 +2,22 @@ package blackjack.ui
 
 class InputView {
 
-    val playersName: String by lazy {
+    private val playersName: String by lazy {
         println("게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리")
         val input = readln()
         checkIsValidInput(input)
         input
     }
 
+    fun getPlayers(): List<String> {
+        return playersName.replace(" ", "").split(DELIMITER)
+    }
+
     fun checkIsValidInput(input: String) {
         require(input.isNotEmpty())
+    }
+
+    companion object {
+        private const val DELIMITER = ","
     }
 }
