f42080dbab9e7ce2288b6ae4b537e26efeb9c2f9
heesang.lee <sky1994601@hanmail.net>
2024-11-29 18:31:29 +0900

    feat: step2 - 블랙잭
    요구사항 '플레이어는 가진 카드의 합이 21이 넘지 않으면 추가 카드를 뽑을 수 있다' 구현

--- src/main/kotlin/blackjack/domain/BlackJackPlayer.kt
+++ src/main/kotlin/blackjack/domain/BlackJackPlayer.kt
@@ -7,7 +7,17 @@ class BlackJackPlayer(_cards: List<BlackJackCard>) {
         require(_cards.size == DEFAULT_CARD_NUMBER) { "플레이어는 처음에 2장만 가지고 시작해야해요" }
     }
 
+    fun drawCard(blackJackCard: BlackJackCard) {
+        require(isCardNumberSumUnder21()) { "21을 넘으면 카드를 못뽑아요" }
+        cards.add(blackJackCard)
+    }
+
+    fun isCardNumberSumUnder21(): Boolean {
+        return cards.sumOf { it.number.values.min() } <= BlackJack_WIN_CARD_SUM
+    }
+
     companion object {
         private val DEFAULT_CARD_NUMBER = 2
+        private val BlackJack_WIN_CARD_SUM = 21
     }
 }
--- src/test/kotlin/blackjack/BlackJackPlayerTest.kt
+++ src/test/kotlin/blackjack/BlackJackPlayerTest.kt
@@ -38,4 +38,45 @@ class BlackJackPlayerTest {
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessage("플레이어는 처음에 2장만 가지고 시작해야해요")
     }
+
+    @Test
+    fun `플레이어는 가진 카드의 합이 21이 넘지 않으면 추가 카드를 뽑을 수 있다`() {
+        val blackJackPlayer =
+            BlackJackPlayer(
+                listOf(
+                    BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.ACE),
+                    BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.JACK),
+                ),
+            )
+        blackJackPlayer.drawCard(BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.TWO))
+        assertThat(blackJackPlayer.cards).containsExactly(
+            BlackJackCard(
+                BlackJackCardShape.HEART,
+                BlackJackCardNumber.ACE,
+            ),
+            BlackJackCard(
+                BlackJackCardShape.HEART,
+                BlackJackCardNumber.JACK,
+            ),
+            BlackJackCard(
+                BlackJackCardShape.HEART,
+                BlackJackCardNumber.TWO,
+            ),
+        )
+    }
+
+    @Test
+    fun `플레이어는 가진 카드의 합이 21이 넘으면 추가 카드를 뽑을 수 없다`() {
+        val blackJackPlayer =
+            BlackJackPlayer(
+                listOf(
+                    BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.QUEEN),
+                    BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.JACK),
+                ),
+            )
+        blackJackPlayer.drawCard(BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.TWO))
+        assertThatThrownBy { blackJackPlayer.drawCard(BlackJackCard(BlackJackCardShape.HEART, BlackJackCardNumber.THREE)) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("21을 넘으면 카드를 못뽑아요")
+    }
 }
