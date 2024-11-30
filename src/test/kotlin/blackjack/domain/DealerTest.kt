64a5c6265e424c4d829e296e05fbf0ee101bca1d
jwahoonkim <kjhoon0330@snu.ac.kr>
2024-12-01 03:54:14 +0900

    refactor: DealerTest 보완

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -2,6 +2,8 @@ package blackjack.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 class DealerTest {
 
@@ -11,15 +13,11 @@ class DealerTest {
         assertThat(dealer.canDrawCard()).isTrue()
     }
 
-    @Test
-    fun `Dealer는 16점 이상일 때 카드를 받지 않아야 한다`() {
-        val cards = Cards().apply {
-            add(Card.of(Suit.HEART, Rank.KING))
-            add(Card.of(Suit.HEART, Rank.KING))
-        }
+    @MethodSource("provideCardsAndExpectedResult")
+    @ParameterizedTest
+    fun `Dealer는 17점 이상일 때 카드를 받지 않아야 한다`(cards: Cards, expected: Boolean) {
         val dealer = Dealer(cards = cards)
-
-        assertThat(dealer.canDrawCard()).isFalse()
+        assertThat(dealer.canDrawCard()).isEqualTo(expected)
     }
 
     @Test
@@ -32,5 +30,28 @@ class DealerTest {
         val dealer = Dealer(cards = cards)
 
         assertThat(dealer.isBust()).isTrue()
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideCardsAndExpectedResult() = listOf(
+            // 16점인 경우 카드를 받을 수 있어야 한다.
+            Cards().apply {
+                add(Card.of(Suit.HEART, Rank.FIVE))
+                add(Card.of(Suit.SPADE, Rank.SIX))
+            } to true,
+
+            // 17점인 경우 카드를 받을 수 없어야 한다.
+            Cards().apply {
+                add(Card.of(Suit.HEART, Rank.KING))
+                add(Card.of(Suit.DIAMOND, Rank.SEVEN))
+            } to false,
+
+            // 21점인 경우 카드를 받을 수 없어야 한다.
+            Cards().apply {
+                add(Card.of(Suit.HEART, Rank.ACE))
+                add(Card.of(Suit.SPADE, Rank.KING))
+            } to false
+        ).map { (cards, expected) -> org.junit.jupiter.params.provider.Arguments.of(cards, expected) }
     }
 }
