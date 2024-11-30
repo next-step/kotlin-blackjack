f3db507d7ca5a4ac9b2ca4f5150ecdb369e50767
dino.shin <troas96@naver.com>
2024-11-30 10:28:26 +0900

    refactor: drawCard 로직 변경

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -4,10 +4,7 @@ class Deck {
     private val cards: MutableList<Card> = Card.ALL_CARDS.toMutableList()
 
     fun drawCard(): Card {
-        if (cards.isEmpty()) {
-            throw NoSuchElementException("더 이상 덱에서 뽑을 카드가 없습니다.")
-        }
-        return cards.removeAt(cards.size - 1)
+        return cards.removeLastOrNull() ?: throw IllegalStateException("더 이상 뽑을 카드가 없습니다.")
     }
 
     fun shuffle() {
