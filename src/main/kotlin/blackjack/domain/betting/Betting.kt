aa118444840903f6836702ae36f5fd43e867deb7
SeongminKimKR <gvf156@gmail.com>
2024-12-01 23:10:32 +0900

    feat: 베팅금액 초기화 조건 수정

--- src/main/kotlin/blackjack/domain/betting/Betting.kt
+++ src/main/kotlin/blackjack/domain/betting/Betting.kt
@@ -3,10 +3,10 @@ package blackjack.domain.betting
 @JvmInline
 value class Betting(private val amount: Int) {
     init {
-        require(amount > 0) { BETTING_VALUE_EXCEPTION_MESSAGE }
+        require(amount >= 0) { BETTING_VALUE_EXCEPTION_MESSAGE }
     }
 
     companion object {
-        private const val BETTING_VALUE_EXCEPTION_MESSAGE = "베팅 금액은 음수 일 수 없다."
+        private const val BETTING_VALUE_EXCEPTION_MESSAGE = "베팅 금액은 양수여야 합니다."
     }
 }
