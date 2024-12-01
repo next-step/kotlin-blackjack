b23e9a9c4cd6733fbe6102c2ee5957a771780cdc
KimDabomi <cdabomi@nate.com>
2024-12-01 16:20:48 +0900

    [step2] feat: 플레이어가 새로운 카드를 추가한다.

---
+++ src/test/kotlin/blackjack/domain/PlayerTest.kt
@@ -0,0 +1,12 @@
+package blackjack.domain
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class PlayerTest : StringSpec({
+    "플레이어가 새로운 카드를 추가한다." {
+        val player = Player("dabomi")
+        player.addCards(listOf("2하트", "3스페이드"))
+        player.getCardList() shouldBe listOf("2하트", "3스페이드")
+    }
+})
