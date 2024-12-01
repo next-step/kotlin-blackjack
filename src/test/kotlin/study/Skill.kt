e2042b6e6da0ba293e7b89252e0a03a555b89e99
SeolYoungKim <nasur4da@gmail.com>
2024-12-01 21:06:35 +0900

    refactor: sealed interface 사용해보기

--- src/test/kotlin/study/Skill.kt
+++ src/test/kotlin/study/Skill.kt
@@ -1,9 +1,13 @@
 package study
 
-interface Skill {
+sealed interface Skill {
     val description: String
 }
 
 data class SoftSkill(override val description: String): Skill
 
 data class HardSkill(override val description: String): Skill
+
+open class SuperHardSkill(override val description: String): Skill
+
+class SuperSuperHardSkill(description: String) : SuperHardSkill(description)
