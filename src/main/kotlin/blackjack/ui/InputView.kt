f7dd93b1faf48f1446de3a2e585881b63969d0a2
Giwan Kim <giwan.kim@gmail.com>
2024-12-01 13:30:21 +0900

    feat: 이름을 입력받는 view 기능 구현

---
+++ src/main/kotlin/blackjack/ui/InputView.kt
@@ -0,0 +1,11 @@
+package blackjack.ui
+
+object InputView {
+    private const val PLAYER_NAMES_PROMPT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
+    private const val NAME_DELIMITER = ","
+
+    fun getPlayerNames(): List<String> {
+        println(PLAYER_NAMES_PROMPT)
+        return readln().split(NAME_DELIMITER).map { it.trim() }
+    }
+}
