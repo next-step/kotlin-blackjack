eb3d64c20485573ad85b4222dd7a80e473b615a5
KimDabomi <cdabomi@nate.com>
2024-12-01 16:23:42 +0900

    [step2] feat: 플레이어를 생성한다.

--- src/test/kotlin/blackjack/service/BlackJackServiceTest.kt
+++ src/test/kotlin/blackjack/service/BlackJackServiceTest.kt
@@ -1,13 +1,19 @@
-package blackjack.service
-
+import blackjack.service.BlackJackService
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-class BlackJackServiceTest : StringSpec ({
-    "사용자의 입력값을 쉼표 기준으로 분리한다." {
-        val blackJackService = BlackJackService()
-        val input = "kim,da"
-        val result = blackJackService.splitInput(input)
-        result shouldBe listOf("kim", "da")
+class BlackJackServiceTest : StringSpec({
+    val blackjackService = BlackJackService()
+    val players = blackjackService.createPlayers(listOf("kim", "da"))
+
+    "플레이어 이름을 쉼표로 분리한다." {
+        val names = blackjackService.splitPlayerNames("kim, da, bo, mi")
+        names shouldBe listOf("kim", "da", "bo", "mi")
+    }
+
+    "플레이어를 생성한다." {
+        players.size shouldBe 2
+        players[0].name shouldBe "kim"
+        players[1].name shouldBe "da"
     }
 })
