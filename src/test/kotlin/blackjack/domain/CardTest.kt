c5ac19466224c1f9011eed178bdd2c0b78a1e1ac
KimDabomi <cdabomi@nate.com>
2024-12-01 16:16:46 +0900

    [step2] feat: 카드를 한 장 뽑는다.

--- src/test/kotlin/blackjack/domain/CardTest.kt
+++ src/test/kotlin/blackjack/domain/CardTest.kt
@@ -1,12 +1,21 @@
 package blackjack.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class CardTest : StringSpec({
     "카드를 생성한다." {
         val card = Card()
-        card.createCard()
         card.cardList.size shouldBe 52
     }
+
+    "카드를 한 장 뽑는다." {
+        val card = Card()
+        val drawnCards = card.drawCards(1)
+        drawnCards.size shouldBe 1
+        card.cardList.size shouldBe 51
+    }
+
+
 })
