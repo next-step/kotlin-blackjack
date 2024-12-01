149090417c2fd80b35f19efaa95fc7793a4e6fdf
KimDabomi <cdabomi@nate.com>
2024-12-01 16:19:15 +0900

    [step2] feat: Card class 추가

--- src/main/kotlin/blackjack/domain/Card.kt
+++ src/main/kotlin/blackjack/domain/Card.kt
@@ -3,9 +3,9 @@ package blackjack.domain
 import kotlin.random.Random
 
 class Card {
-    private val cardList = mutableListOf<String>()
     private val cardShape = listOf("하트", "클로버", "스페이드", "다이아")
     private val cardNumber = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    val cardList = mutableListOf<String>()
 
     init {
         createCard()
@@ -21,11 +21,20 @@ class Card {
 
     fun drawCards(count: Int): List<String> {
         val drawnCards = mutableListOf<String>()
+
+        if (count <= 0) {
+            throw IllegalArgumentException(DRAW_CARD_EXCEPTION_MESSAGE)
+        }
+
         repeat(count) {
             val randomIndex = Random.nextInt(cardList.size)
             val card = cardList.removeAt(randomIndex)
             drawnCards.add(card)
         }
         return drawnCards
+    }
+
+    companion object {
+        private const val DRAW_CARD_EXCEPTION_MESSAGE = "1장 이상의 카드를 뽑아야 합니다."
     }
 }
