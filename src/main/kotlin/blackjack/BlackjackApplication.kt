ed927577d68fe29f925833ab4a4b04976ff82b68
seongmin <gvf156@gmail.com>
2024-12-01 13:18:52 +0900

    refactor: dealer, participants 외부 접근 허용

--- src/main/kotlin/blackjack/BlackjackApplication.kt
+++ src/main/kotlin/blackjack/BlackjackApplication.kt
@@ -3,6 +3,7 @@ package blackjack
 import blackjack.domain.BlackjackGame
 import blackjack.domain.deck.DefaultDeckGenerator
 import blackjack.domain.player.Dealer
+import blackjack.domain.player.Player
 import blackjack.view.input.CreatePlayerInputView
 import blackjack.view.input.PlayerTurnInputView
 import blackjack.view.output.DealerDrawView
@@ -20,35 +21,30 @@ fun main() {
     val playersDto = blackjackGame.getParticipants()
     StartPlayersView.print(dealer, playersDto)
 
-    drawPlayers(names, blackjackGame)
-    drawDealer(blackjackGame, dealer)
+    drawPlayers(blackjackGame)
+    drawDealer(blackjackGame)
 
     ResultView.print(blackjackGame.getParticipants())
     RecordView.print(blackjackGame.getRecords())
 }
 
-private fun drawPlayers(
-    names: List<String>,
-    blackjackGame: BlackjackGame,
-) {
-    names.forEach {
-        while (blackjackGame.canDraw(it)) {
-            if (!PlayerTurnInputView.continueDraw(it)) {
+private fun drawPlayers(blackjackGame: BlackjackGame) {
+    val players = blackjackGame.participants.participants.filterIsInstance<Player>()
+    players.forEach {
+        while (blackjackGame.canDraw(it.name)) {
+            if (!PlayerTurnInputView.continueDraw(it.name)) {
                 break
             }
-            blackjackGame.dealCard(it)
+            blackjackGame.dealCard(it.name)
         }
-        val playerDto = blackjackGame.getParticipant(it)
+        val playerDto = blackjackGame.getParticipant(it.name)
         PlayerView.print(playerDto)
     }
 }
 
-private fun drawDealer(
-    blackjackGame: BlackjackGame,
-    dealer: Dealer,
-) {
-    if (blackjackGame.canDraw(dealer.name)) {
-        blackjackGame.dealCard(dealer.name)
-        DealerDrawView.print(dealer.name)
+private fun drawDealer(blackjackGame: BlackjackGame) {
+    if (blackjackGame.canDraw(blackjackGame.dealer.name)) {
+        blackjackGame.dealCard(blackjackGame.dealer.name)
+        DealerDrawView.print(blackjackGame.dealer.name)
     }
 }
--- src/main/kotlin/blackjack/domain/BlackjackGame.kt
+++ src/main/kotlin/blackjack/domain/BlackjackGame.kt
@@ -3,20 +3,19 @@ package blackjack.domain
 import blackjack.domain.deck.Deck
 import blackjack.domain.deck.DeckGenerator
 import blackjack.domain.player.Dealer
-import blackjack.domain.player.Participant
 import blackjack.domain.player.Participants
 import blackjack.domain.player.Player
-import blackjack.view.dto.ParticiapntsDto
 import blackjack.view.dto.ParticipantDto
+import blackjack.view.dto.ParticipantsDto
 import blackjack.view.dto.RecordDto
 
 class BlackjackGame(
-    private val dealer: Dealer,
+    val dealer: Dealer,
     names: List<String>,
     deckGenerator: DeckGenerator,
 ) {
     private val deck: Deck = deckGenerator.generate()
-    private val participants = Participants(listOf(dealer) + names.map { Player(name = it) })
+    val participants = Participants(listOf(dealer) + names.map { Player(name = it) })
 
     fun start() {
         participants.participants.forEach { player ->
@@ -34,20 +33,16 @@ class BlackjackGame(
         player.receiveCard(deck.draw())
     }
 
-    fun getRecords(): RecordDto {
-        val players = participants.participants.filterIsInstance<Player>()
-        return RecordDto(
-            dealerName = dealer.name,
-            records = players.map { it.name to it.isWin(dealer) }.toMap(),
-        )
-    }
-
-    fun getParticipants(): ParticiapntsDto {
-        return Participants.toDto(participants)
+    fun getParticipants(): ParticipantsDto {
+        return ParticipantsDto.from(participants)
     }
 
     fun getParticipant(name: String): ParticipantDto {
-        return Participant.toDto(participants.findPlayer(name))
+        return ParticipantDto.from(participants.findPlayer(name))
+    }
+
+    fun getRecords(): RecordDto {
+        return RecordDto.of(dealer, participants)
     }
 
     companion object {
