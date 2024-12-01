0d50ec6a7940c6112354a54d64c2f1ca477101b7
Giwan Kim <giwan.kim@gmail.com>
2024-12-01 13:29:07 +0900

    refactor: 테스트 용이성을 위해서 Deck 인터페이스를 추출했다.

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -1,16 +1,7 @@
 package blackjack.domain
 
-import java.lang.IllegalStateException
-
-class Deck {
-    private val cards = ArrayDeque<Card>()
+interface Deck {
     val size: Int
-        get() = cards.size
 
-    init {
-        cards.addAll(Card.ALL_CARDS)
-        cards.shuffle()
-    }
-
-    fun draw(): Card = cards.removeFirstOrNull() ?: throw IllegalStateException("덱에 카드가 없습니다")
+    fun draw(): Card
 }
--- src/test/kotlin/blackjack/domain/ShuffledDeckTest.kt
+++ src/test/kotlin/blackjack/domain/ShuffledDeckTest.kt
@@ -8,16 +8,16 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 @Suppress("NonAsciiCharacters")
-class DeckTest {
+class ShuffledDeckTest {
     @Test
     fun `초기화 된 덱은 52개의 카드를 갖고 있다`() {
-        val deck = Deck()
+        val deck = ShuffledDeck()
         deck.size shouldBe 52
     }
 
     @Test
     fun `덱에서 카드를 뽑으면 사이즈가 1 감소한다`() {
-        val deck = Deck()
+        val deck = ShuffledDeck()
 
         deck.draw()
 
@@ -26,7 +26,7 @@ class DeckTest {
 
     @Test
     fun `덱에서 카드를 뽑으면 카드가 뽑힌다`() {
-        val deck = Deck()
+        val deck = ShuffledDeck()
 
         val card = deck.draw()
 
@@ -36,7 +36,7 @@ class DeckTest {
 
     @Test
     fun `카드를 52변 뽑으면 각기 다른 52개의 카드들이 뽑힌다`() {
-        val deck = Deck()
+        val deck = ShuffledDeck()
 
         val drawnCards =
             (1..52)
@@ -48,7 +48,7 @@ class DeckTest {
 
     @Test
     fun `카드를 뽑은 횟수가 52번을 초가하면 null 이 반환된다`() {
-        val deck = Deck()
+        val deck = ShuffledDeck()
         (1..52)
             .map { deck.draw() }
         assertThrows<IllegalStateException> { deck.draw() }
