92003853db9fc17c4636d7d3c46d6759ae968acc
leekyuwon <kyuwoon369@gmail.com>
2024-12-01 22:38:49 +0900

    refactor: 오버라이드 함수와 변수명 일치

--- src/main/kotlin/blackjack/entity/Player.kt
+++ src/main/kotlin/blackjack/entity/Player.kt
@@ -1,10 +1,10 @@
 package blackjack.entity
 
 class Player(name: String) : Participant(name) {
-    override fun calculateResult(input: ComparisonScore): GameResult {
-        require(input is ComparisonScore.Single) { "딜러 점수는 하나입니다." }
+    override fun calculateResult(score: ComparisonScore): GameResult {
+        require(score is ComparisonScore.Single) { "딜러 점수는 하나입니다." }
 
-        val dealerScore = input.score
+        val dealerScore = score.score
 
         val playerScore = calculateScore()
         return when {
