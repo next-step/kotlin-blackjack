21d7d70ed2e637008aa56782b6d4f2a1dac4e66f
wooobo <irooori89@gmail.com>
2024-12-01 09:13:40 +0900

    feat: UI 추가

---
+++ src/main/kotlin/blackjack/ui/InputView.kt
@@ -0,0 +1,17 @@
+package blackjack.ui
+
+class InputView(val inputProvider: () -> String? = { readln() }) {
+    fun inputUserNames(): List<String> {
+        println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)")
+
+        return inputProvider()?.split(",") ?: throw IllegalArgumentException("입력이 없습니다.")
+    }
+
+    fun inputMore(name: String): Boolean {
+        println("$name 는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+
+        return inputProvider()?.let {
+            it == "y"
+        } ?: throw IllegalArgumentException("입력이 없습니다.")
+    }
+}
---
+++ src/test/kotlin/blackjack/ui/ResultViewTest.kt
@@ -0,0 +1,42 @@
+package blackjack.ui
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ResultViewTest {
+    @Test
+    fun `유저의 이름과 장수를 출력한다`() {
+        // given
+        var expected = ""
+        val customOutputProvider: (String) -> Unit = { message ->  expected = message }
+        val resultView = ResultView(customOutputProvider)
+
+        resultView.printUserCardCount(listOf("userA","userB"), 2)
+
+        assertThat(expected).contains("userA, userB에게 2장의 나누었습니다.")
+    }
+
+    @Test
+    fun `유저 카드 출력`() {
+        // given
+        var expected = ""
+        val customOutputProvider: (String) -> Unit = { message ->  expected = message }
+        val resultView = ResultView(customOutputProvider)
+
+        resultView.printUserCards("userA", listOf("2하트", "2스페이드"))
+
+        assertThat(expected).contains("userA카드: 2하트, 2스페이드")
+    }
+
+    @Test
+    fun `결과 출력`() {
+        // given
+        var expected = ""
+        val customOutputProvider: (String) -> Unit = { message ->  expected = message }
+        val resultView = ResultView(customOutputProvider)
+
+        resultView.printResult("userA", listOf("2하트", "8스페이드", "A클로버"), 21)
+
+        assertThat(expected).contains("userA카드: 2하트, 8스페이드, A클로버 - 결과: 21")
+    }
+}
