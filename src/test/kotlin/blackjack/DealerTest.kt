a3fa819104b894bb0da05e8560df1e0da7613d95
y2gcoder <y2gcoder@gmail.com>
2024-12-02 23:32:34 +0900

    test(blackjack): Dealer.receive()에 대한 테스트 코드 추가

--- src/test/kotlin/blackjack/DealerTest.kt
+++ src/test/kotlin/blackjack/DealerTest.kt
@@ -2,6 +2,7 @@ package blackjack
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 
 class DealerTest : StringSpec({
     "딜러는 생성 시에 카드 2장이 필수다" {
@@ -19,5 +20,24 @@ class DealerTest : StringSpec({
                 Dealer(initialCards = initialCards)
             }
         }
+    }
+
+    "딜러는 카드 1장을 건네받아 손패에 추가할 수 있다" {
+        val initialCards =
+            listOf(
+                Card(CardNumber.Jack, Suit.SPADES),
+                Card(Number(6), Suit.SPADES),
+            )
+        val sut = Dealer(initialCards = initialCards)
+        val newCard = Card(Number(3), Suit.SPADES)
+
+        sut.receive(newCard)
+
+        sut.hand shouldContainExactlyInAnyOrder
+            listOf(
+                Card(CardNumber.Jack, Suit.SPADES),
+                Card(Number(6), Suit.SPADES),
+                Card(Number(3), Suit.SPADES),
+            )
     }
 })
