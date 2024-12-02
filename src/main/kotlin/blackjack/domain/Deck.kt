a42c3a9cb886eb3cdc3c5da1022ce8c028be0057
sian <chicori3@naver.com>
2024-12-02 21:56:48 +0900

    refactor: 덱 일급 컬렉션 정의, 생성 로직 추가

--- src/main/kotlin/blackjack/domain/Deck.kt
+++ src/main/kotlin/blackjack/domain/Deck.kt
@@ -1,16 +1,30 @@
 package blackjack.domain
 
 class Deck(
-    private val cards: Cards = Cards.fullCards(),
+    private val cards: MutableList<Card> = mutableListOf(),
 ) {
     val quantity: Int
         get() = cards.size
+    
+    init {
+        generateShuffledCards().also(cards::addAll)
+    }
+
+    private fun generateShuffledCards(): List<Card> {
+        return createDeck().shuffled()
+    }
+
+    private fun createDeck(): List<Card> {
+        return Suit.entries.flatMap { suit -> createCardsForSuit(suit) }
+    }
+
+    private fun createCardsForSuit(suit: Suit): List<Card> {
+        return Rank.entries.map { rank -> Card(suit, rank) }
+    }
 
     fun draw(): Card {
-        if (quantity == 0) {
-            throw IllegalStateException("카드가 모두 소진되었습니다.")
-        }
+        check(quantity != 0) { "카드가 모두 소진되었습니다." }
 
-        return cards.draw()
+        return cards.removeFirst()
     }
 }
