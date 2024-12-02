f630e7bb8cdba21fc6c51a618d182534453bfa65
y2gcoder <y2gcoder@gmail.com>
2024-12-02 23:56:52 +0900

    refactor(blackjack): Player, Dealer 의 공통 로직을 Sealed Class 인 Participant에 위임

--- src/main/kotlin/blackjack/Dealer.kt
+++ src/main/kotlin/blackjack/Dealer.kt
@@ -1,36 +1,16 @@
 package blackjack
 
-class Dealer(val name: String = DEALER_NAME, initialCards: List<Card>) {
-    private val _hand = Hand(initialCards)
-    val hand: List<Card>
-        get() = _hand.cards
-
-    init {
-        require(name.isNotBlank()) {
-            "이름은 빈 문자열일 수 없습니다: name=$name"
-        }
-        require(initialCards.size == INITIAL_HAND_LENGTH) {
-            "첫 패는 ${INITIAL_HAND_LENGTH}장이어야 한다: initialCards.size=${initialCards.size}"
-        }
-    }
-
-    fun isInitialState(): Boolean = _hand.cards.size == 2
-
-    fun receive(newCard: Card) {
+class Dealer(initialCards: List<Card>) : Participant(DEALER_NAME, initialCards) {
+    override fun receive(newCard: Card) {
         check(shouldDrawCard()) {
             "현재 딜러는 히트할 수 없는 상태입니다: sumOfHand=${sumOfHand()}"
         }
-        _hand.add(newCard)
+        super.receive(newCard)
     }
-
-    fun sumOfHand(): Int = _hand.sumOfHand()
-
-    fun isBust(): Boolean = _hand.isBust()
 
     fun shouldDrawCard(): Boolean = sumOfHand() <= DEALER_HIT_THRESHOLD
 
     companion object {
-        private const val INITIAL_HAND_LENGTH = 2
         private const val DEALER_NAME = "딜러"
         private const val DEALER_HIT_THRESHOLD = 16
     }
--- src/main/kotlin/blackjack/Player.kt
+++ src/main/kotlin/blackjack/Player.kt
@@ -1,30 +1,5 @@
 package blackjack
 
-class Player(val name: String, initialCards: List<Card>) {
-    private val _hand = Hand(initialCards)
-    val hand: List<Card>
-        get() = _hand.cards
-
-    init {
-        require(name.isNotBlank()) {
-            "이름은 빈 문자열일 수 없습니다: name=$name"
-        }
-        require(initialCards.size == INITIAL_HAND_LENGTH) {
-            "첫 패는 ${INITIAL_HAND_LENGTH}장이어야 한다: initialCards.size=${initialCards.size}"
-        }
-    }
-
-    fun isInitialState(): Boolean = _hand.cards.size == 2
-
-    fun receive(newCard: Card) {
-        _hand.add(newCard)
-    }
-
-    fun sumOfHand(): Int = _hand.sumOfHand()
-
-    fun isBust(): Boolean = _hand.isBust()
-
-    companion object {
-        private const val INITIAL_HAND_LENGTH = 2
-    }
+class Player(name: String, initialCards: List<Card>) : Participant(name, initialCards) {
+    fun isInitialState(): Boolean = hand.size == 2
 }
