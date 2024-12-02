55da780df80d69495c71f58ce74afe0a163fed12
ahn-sj <jaesa5221@gmail.com>
2024-12-03 00:21:38 +0900

    [step2] feat: 플레이어의 모든 선택이 끝나면 플레이어의 카드 합을 계산한다.

--- src/main/kotlin/blackjack/OutputView.kt
+++ src/main/kotlin/blackjack/OutputView.kt
@@ -4,5 +4,15 @@ object OutputView {
     fun printPlayersStartCardPack(players: List<Player>) {
         println("\n${players.joinToString { it.name.value }}에게 2장의 카드를 나누었습니다.")
         players.forEach { println("$it") }
+        println()
+    }
+
+    fun printPlayerCard(player: Player) {
+        println(player)
+    }
+
+    fun printBlackJackResult(players: List<Player>) {
+        println()
+        players.forEach { println("$it - 결과:${it.score()}") }
     }
 }
