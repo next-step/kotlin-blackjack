ba53b40bb72954156c5dc1ae934bc104a1296729
Giwan Kim <giwan.kim@gmail.com>
2024-12-02 10:33:48 +0900

    feat: 카드를 더 받을지 사용자의 입력을 읽는다.

--- src/main/kotlin/blackjack/ui/InputView.kt
+++ src/main/kotlin/blackjack/ui/InputView.kt
@@ -1,5 +1,7 @@
 package blackjack.ui
 
+import blackjack.domain.Player
+
 object InputView {
     private const val PLAYER_NAMES_PROMPT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     private const val COMMA_DELIMITER = ","
@@ -7,5 +9,10 @@ object InputView {
     fun getPlayerNames(): List<String> {
         println(PLAYER_NAMES_PROMPT)
         return readln().split(COMMA_DELIMITER).map { it.trim() }
+    }
+
+    fun getCommand(player: Player): String {
+        println("${player.name}는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)")
+        return readln()
     }
 }
