b51ff571dc636cc8d74cf0117d0e7ba9e947d21f
nooblette <min9hyuk@gmail.com>
2024-12-03 06:29:00 +0900

    refactor : 게임 진행 흐름 단순화

--- src/main/kotlin/blackjack/machine/BlackJackMachine.kt
+++ src/main/kotlin/blackjack/machine/BlackJackMachine.kt
@@ -1,6 +1,7 @@
 package blackjack.machine
 
 import blackjack.deck.Deck
+import blackjack.player.Player
 import blackjack.player.Players
 import blackjack.view.InputView
 import blackjack.view.ResultView
@@ -18,11 +19,30 @@ class BlackJackMachine(
                         updatedPlayer.hitCard(deck.draw())
                     }
                 }
-        val players = Players(players = playerList)
+        var players = Players(players = playerList)
 
         ResultView.printPlayersName(players = players)
         ResultView.printPlayersCardStatus(players = players)
+
+        while (isGameActive(players = players)) {
+            players = Players(players = players.players.map { playTurn(it) })
+            ResultView.printPlayersCardStatusAndSum(players = players)
+        }
     }
+
+    private fun playTurn(player: Player): Player =
+        when {
+            player.isBust() -> player
+            !InputView.isHitCard(player) ->
+                player
+                    .also { ResultView.printPlayerCard(it) }
+            else ->
+                player
+                    .hitCard(deck.draw())
+                    .also { ResultView.printPlayerCard(it) }
+        }
+
+    private fun isGameActive(players: Players) = players.players.any { !it.isBust() }
 
     companion object {
         private const val DEFAULT_HAND_SIZE = 2
--- src/main/kotlin/blackjack/player/Players.kt
+++ src/main/kotlin/blackjack/player/Players.kt
@@ -1,7 +1,6 @@
 package blackjack.player
 
 import blackjack.machine.BlackJackMachine
-import blackjack.round.RoundResult
 
 class Players(
     val players: List<Player>,
@@ -9,8 +8,6 @@ class Players(
     companion object {
         fun generateFromNames(playerNames: List<String>) = Players(players = playerNames.map { Player.ready(name = it) })
     }
-
-    fun updateCardStatus(roundResults: List<RoundResult>): Players = Players(players = roundResults.map { it.player })
 
     fun getWinner(): Player? =
         players
