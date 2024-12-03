28adff868882c92b7bfb53cc471de722d49de422
wooobo <irooori89@gmail.com>
2024-12-03 20:17:22 +0900

    feat: isBust 조건 변경

--- src/main/kotlin/blackjack/domain/Hand.kt
+++ src/main/kotlin/blackjack/domain/Hand.kt
@@ -2,11 +2,11 @@ package blackjack.domain
 
 class Hand(private val cards: MutableList<Card> = mutableListOf()) {
     val score: Int
-        get() = cards.sortedBy { if (it.rank.isAce()) 1 else 0 }.fold(0) { acc, card -> card.score(acc) }
+        get() = cards.fold(0) { acc, card -> card.score(acc) }
     val totalCards: List<Card>
         get() = cards.toList()
     val isBust: Boolean
-        get() = score > 21
+        get() = score >= 21
 
     fun add(otherCard: List<Card>) {
         cards.addAll(otherCard)
