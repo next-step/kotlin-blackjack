33aace9004436c8f24aa853cea63b289c0a5d7e4
leekyuwon <kyuwoon369@gmail.com>
2024-12-04 23:43:32 +0900

    feat: 버스트인 경우 베팅 금액을 모두 잃는다

--- src/main/kotlin/blackjack/entity/BettingAmount.kt
+++ src/main/kotlin/blackjack/entity/BettingAmount.kt
@@ -4,4 +4,10 @@ class BettingAmount(val amount: Int) {
     init {
         require(amount > 0) { "베팅 금액은 0보다 커야합니다." }
     }
+
+    fun loseIfBusted(isBusted: Boolean): Int {
+        return if (isBusted) loseBet() else 0
+    }
+
+    fun loseBet() = -amount
 }
--- src/test/kotlin/blackjack/entity/BettingAmountTest.kt
+++ src/test/kotlin/blackjack/entity/BettingAmountTest.kt
@@ -6,7 +6,6 @@ import io.kotest.matchers.shouldBe
 
 class BettingAmountTest : DescribeSpec({
     describe("베팅 금액은") {
-
         context("베팅 금액 생성 시") {
             it("0보다 큰 금액이면 생성된다") {
                 val bettingAmount = BettingAmount(100)
@@ -25,6 +24,24 @@ class BettingAmountTest : DescribeSpec({
                         BettingAmount(-10)
                     }
                 negativeException.message shouldBe "베팅 금액은 0보다 커야합니다."
+            }
+        }
+        context("플레이어가 BUst 상태인 경우") {
+            it("모든 금액을 잃는다") {
+                val bettingAmount = BettingAmount(100)
+                bettingAmount.loseIfBusted(true) shouldBe -100
+            }
+        }
+        context("플레이어가 Bust 상태가 아닌 경우") {
+            it("금액 변화가 없다") {
+                val bettingAmount = BettingAmount(100)
+                bettingAmount.loseIfBusted(false) shouldBe 0
+            }
+        }
+        context("플레이어가 패배한 경우") {
+            it("베팅 금액만큼 잃는다") {
+                val bettingAmount = BettingAmount(100)
+                bettingAmount.loseBet() shouldBe -100
             }
         }
     }
