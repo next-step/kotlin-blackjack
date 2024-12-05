37b85ffadc043d6a0fa72048d8c497642f36c0bc
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-05 18:26:34 +0900

    test(testcase) - testcase 추가

---
+++ src/test/kotlin/blackjack/core/CardsTest.kt
@@ -0,0 +1,77 @@
+package blackjack.core
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class CardsTest {
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    fun `포인트를 계산한다`(
+        cards: MutableSet<Card>,
+        point: Int,
+    ) {
+        Cards(cards).point() shouldBe point
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParameters2")
+    fun `카드네임을 얻는다`(
+        cards: MutableSet<Card>,
+        cardNames: String,
+    ) {
+        Cards(cards).getCardNames() shouldBe cardNames
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideParameters(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(mutableSetOf(Card(Denomination.TWO, Suit.HEARTS), Card(Denomination.THREE, Suit.HEARTS)), 5),
+                Arguments.of(mutableSetOf(Card(Denomination.ACE, Suit.HEARTS), Card(Denomination.KING, Suit.HEARTS)), 21),
+                Arguments.of(
+                    mutableSetOf(
+                        Card(Denomination.ACE, Suit.HEARTS),
+                        Card(Denomination.KING, Suit.HEARTS),
+                        Card(Denomination.KING, Suit.CLUBS),
+                    ),
+                    21,
+                ),
+                Arguments.of(
+                    mutableSetOf(
+                        Card(Denomination.ACE, Suit.HEARTS),
+                        Card(Denomination.KING, Suit.HEARTS),
+                        Card(Denomination.SEVEN, Suit.CLUBS),
+                    ),
+                    18,
+                ),
+            )
+        }
+
+        @JvmStatic
+        fun provideParameters2(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(mutableSetOf(Card(Denomination.TWO, Suit.HEARTS)), "2하트"),
+                Arguments.of(mutableSetOf(Card(Denomination.ACE, Suit.HEARTS), Card(Denomination.KING, Suit.CLUBS)), "A하트,King클로버"),
+                Arguments.of(
+                    mutableSetOf(
+                        Card(Denomination.ACE, Suit.HEARTS),
+                        Card(Denomination.KING, Suit.CLUBS),
+                        Card(Denomination.QUEEN, Suit.SPADES),
+                    ),
+                    "A하트,King클로버,Queen스페이드",
+                ),
+                Arguments.of(
+                    mutableSetOf(
+                        Card(Denomination.ACE, Suit.HEARTS),
+                        Card(Denomination.KING, Suit.CLUBS),
+                        Card(Denomination.QUEEN, Suit.SPADES),
+                    ),
+                    "A하트,King클로버,Queen스페이드",
+                ),
+            )
+        }
+    }
+}
