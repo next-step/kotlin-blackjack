3f44625c8b93b0033dbcd2b0280fe53f299b2003
wooobo <irooori89@gmail.com>
2024-12-05 12:35:12 +0900

    feat: 승리 판단 `MatchType` 으로 변경

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -41,31 +41,17 @@ class Dealer : Player(DEALER_NAME, Hand()) {
 
     private fun judgePlayer(player: Player): MatchType {
         return when {
-            isBust -> MatchType.WIN // 딜러가 버스트면 유저 승리
-            player.isBust -> MatchType.LOSE // 유저가 버스트면 딜러 승리
-            else -> compareScoreGap(player)
-        }
-    }
-
-    private fun compareScoreGap(player: Player): MatchType {
-        return when {
-            bustGap == player.bustGap -> MatchType.DRAW
-            bustGap < player.bustGap -> MatchType.LOSE
-            bustGap > player.bustGap -> MatchType.WIN
-            else -> throw IllegalArgumentException("비정상적인 상황입니다.")
+            isBust -> MatchType.WIN
+            player.isBust -> MatchType.LOSE
+            else -> MatchType.evaluate(player.bustGap, bustGap)
         }
     }
 
     private fun dealerMatch(player: Player): MatchType {
-        val dealerGap = bustGap
-        val playerGap = player.bustGap
         return when {
             isBust -> MatchType.LOSE
             player.isBust -> MatchType.WIN
-            bustGap == player.bustGap -> MatchType.DRAW
-            dealerGap < playerGap -> MatchType.WIN
-            dealerGap > playerGap -> MatchType.LOSE
-            else -> throw IllegalArgumentException("비정상적인 상황입니다.")
+            else -> MatchType.evaluate(bustGap, player.bustGap)
         }
     }
 
--- src/main/kotlin/blackjack/domain/MatchType.kt
+++ src/main/kotlin/blackjack/domain/MatchType.kt
@@ -3,5 +3,15 @@ package blackjack.domain
 enum class MatchType {
     WIN,
     LOSE,
-    DRAW,
+    DRAW, ;
+
+    companion object {
+        fun evaluate(o1: Int, o2: Int): MatchType {
+            return when {
+                o1 == o2 -> DRAW
+                o1 < o2 -> WIN
+                else -> LOSE
+            }
+        }
+    }
 }
