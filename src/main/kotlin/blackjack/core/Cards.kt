1710e7ca0fdf0988e7e3f84b025d9d01e1957c6a
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-05 18:56:55 +0900

    feat(Card) - Card 관련 Class추가

---
+++ src/main/kotlin/blackjack/core/Cards.kt
@@ -0,0 +1,30 @@
+package blackjack.core
+
+class Cards(private val cards: MutableSet<Card> = mutableSetOf()) : MutableSet<Card> by cards {
+    fun getCardNames(): String = this.joinToString(",", "", "")
+
+    fun point(): Int {
+        val sum = cards.sumOf { it.denomination.score }
+        val aces = cards.filter { it.denomination == Denomination.ACE }
+
+        if (aces.isEmpty()) {
+            return sum
+        }
+
+        if (!checkBust(sum, ACE_WEIGHT)) {
+            return sum + ACE_WEIGHT
+        }
+
+        return sum
+    }
+
+    private fun checkBust(
+        sum: Int,
+        weight: Int,
+    ): Boolean = (sum + weight) > MAX_POINT
+
+    companion object {
+        const val MAX_POINT = 21
+        private const val ACE_WEIGHT = 10
+    }
+}
