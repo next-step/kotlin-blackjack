2ad854ced59cbfa4ed35b7c52a074c5c7a343bef
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-05 18:25:12 +0900

    feat(Card) - Card 관련 Enum값과 객체 생성

---
+++ src/main/kotlin/blackjack/core/Card.kt
@@ -0,0 +1,34 @@
+package blackjack.core
+
+data class Card(val denomination: Denomination, val suit: Suit) {
+    override fun toString(): String {
+        return denomination.title + suit.title
+    }
+
+    companion object {
+        val CARD_COUNT = Denomination.entries.size * Suit.entries.size
+    }
+}
+
+enum class Denomination(val title: String, val score: Int) {
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("Jack", 10),
+    QUEEN("Queen", 10),
+    KING("King", 10),
+}
+
+enum class Suit(val title: String) {
+    CLUBS("클로버"),
+    DIAMONDS("다이아"),
+    HEARTS("하트"),
+    SPADES("스페이드"),
+}
