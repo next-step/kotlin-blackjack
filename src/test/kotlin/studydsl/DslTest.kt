81d98d2ad552e9ab22d36c3e6f4ce7144c058d15
Pablo730 <kcm1619@naver.com>
2024-12-08 06:51:46 +0900

    불필요한 테스트 코드 제거

--- src/test/kotlin/studydsl/DslTest.kt
+++ src/test/kotlin/studydsl/DslTest.kt
@@ -96,19 +96,3 @@ class DslTest {
         person.languages.languages[1] shouldBe Language(name = "English", level = 2)
     }
 }
-
-/*
-introduce {
-  name("박재성")
-  company("우아한형제들")
-  skills {
-    soft("A passion for problem solving")
-    soft("Good communication skills")
-    hard("Kotlin")
-  }
-  languages {
-    "Korean" level 5
-    "English" level 3
-  }
-}
-*/
