cccb374a7c83c2983a1849e9fea7ea540e3a1322
Pablo730 <kcm1619@naver.com>
2024-12-08 06:41:38 +0900

    블랙잭 게임 속 카드를 관리할 CardDeck 객체 구현 및 테스트

--- src/main/kotlin/blackjack/domain/CardDeck.kt
+++ src/main/kotlin/blackjack/domain/CardDeck.kt
@@ -1,11 +1,13 @@
 package blackjack.domain
 
-import blackjack.domain.Card.Companion.ALL_CARDS_SET
-
-data class AllCardMutableSet(val cardSet: MutableSet<Card>) {
+data class CardDeck(val deck: MutableList<Card>) {
     init {
-        require(cardSet.containsAll(ALL_CARDS_SET)) { "블랙잭 게임을 하기 위해서는 모든 카드가 준비되어야합니다" }
+        val defaultDeck = defaultDeckGenerator()
+        require(deck.containsAll(defaultDeck)) { "블랙잭 게임을 하기 위해서는 모든 카드가 준비되어야합니다" }
     }
 
-    
+    fun drawCard(): Card {
+        require(deck.isNotEmpty()) { "더이상 카드가 남아있지 않습니다" }
+        return deck.removeAt(deck.lastIndex)
+    }
 }
--- src/main/kotlin/blackjack/domain/DeckGenerator.kt
+++ src/main/kotlin/blackjack/domain/DeckGenerator.kt
@@ -1,4 +1,17 @@
 package blackjack.domain
 
-class DeckGenerator {
+fun defaultDeckGenerator(): MutableList<Card> {
+    return createAllCards().toMutableList()
+}
+
+fun shuffledDeckGenerator(): MutableList<Card> {
+    return createAllCards().shuffled().toMutableList()
+}
+
+private fun createAllCards(): List<Card> {
+    return Suit.entries.flatMap { createAllCardsBySuit(it) }
+}
+
+private fun createAllCardsBySuit(suit: Suit): List<Card> {
+    return Rank.entries.map { Card(suit, it) }
 }
