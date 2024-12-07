ebb3859968ceb8ef23ca361a6b44fd098c3d0aad
Pablo730 <kcm1619@naver.com>
2024-12-08 06:45:19 +0900

    블랙잭 게임에 참여하는 플레이어들을 관리할 Players 객체 구현 및 테스트

--- src/main/kotlin/blackjack/domain/Players.kt
+++ src/main/kotlin/blackjack/domain/Players.kt
@@ -1,4 +1,15 @@
 package blackjack.domain
 
-class Players {
+data class Players(val players: List<Player>) {
+    init {
+        require(players.size == PLAYER_COUNT) { "블랙잭 게임에서 플레이어는 2명이어야 합니다" }
+    }
+
+    fun toPlayerNamesString(): String {
+        return players.joinToString(", ") { it.playerName.name }
+    }
+
+    companion object {
+        const val PLAYER_COUNT = 2
+    }
 }
