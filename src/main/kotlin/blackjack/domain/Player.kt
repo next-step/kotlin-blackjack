1e4e6566ccffe35fed3eca848abc1667ec91b540
Pablo730 <kcm1619@naver.com>
2024-12-08 06:44:09 +0900

    블랙잭 플레이어 객체 구현 및 테스트

--- src/main/kotlin/blackjack/domain/Player.kt
+++ src/main/kotlin/blackjack/domain/Player.kt
@@ -1,4 +1,17 @@
 package blackjack.domain
 
-class Player {
+data class Player(val playerName: PlayerName, val mutableCards: MutableCards) {
+    fun addCard(card: Card) {
+        mutableCards.add(card)
+        require(sumCardValues() <= MutableCards.MAX_SUM_CARD_VALUES) {
+            playerName.name + " Bust! / " + mutableCards.cardsToString() }
+    }
+
+    fun cardsToString(): String {
+        return mutableCards.cardsToString()
+    }
+
+    fun sumCardValues(): Int {
+        return mutableCards.sumValues()
+    }
 }
