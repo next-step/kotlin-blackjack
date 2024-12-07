04af441a2d5d48c55152721ca071eae153da2431
Pablo730 <kcm1619@naver.com>
2024-12-08 06:46:10 +0900

    블랙잭 게임 초기 로직 구현 및 테스트

--- src/main/kotlin/blackjack/domain/BlackJack.kt
+++ src/main/kotlin/blackjack/domain/BlackJack.kt
@@ -1,4 +1,11 @@
 package blackjack.domain
 
-class BlackJack {
+class BlackJack(val players: Players, val cardDeck: CardDeck) {
+    init {
+        repeat(INIT_CARD_DRAW_REPEAT) { players.players.forEach { it.addCard(cardDeck.drawCard()) } }
+    }
+
+    companion object {
+        const val INIT_CARD_DRAW_REPEAT = 2
+    }
 }
--- src/test/kotlin/blackjack/domain/BlackJackTest.kt
+++ src/test/kotlin/blackjack/domain/BlackJackTest.kt
@@ -1,4 +1,17 @@
 package blackjack.domain
 
+import blackjack.util.createPlayers
+import io.kotest.matchers.equals.shouldBeEqual
+import org.junit.jupiter.api.Test
+
 class BlackJackTest {
+    @Test
+    fun `블랙잭 게임을 시작할 때 플레이어에게 카드 2장씩 주고 시작해야한다`() {
+        val players = createPlayers("pablo", "musk")
+        val blackJack = BlackJack(players, CardDeck(defaultDeckGenerator()))
+
+        blackJack.players.players.forEach {
+            it.mutableCards.cards.size shouldBeEqual 2
+        }
+    }
 }
