7427c8855751b54b47505275db8e717126f4f43a
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-08 19:51:54 +0900

    refactor(Turn) - Turn Condition 이름 변경 및 조건 변경.

---
+++ src/main/kotlin/blackjack/core/turn/Turn.kt
@@ -0,0 +1,25 @@
+package blackjack.core.turn
+
+import blackjack.core.CardDispenser
+import blackjack.core.player.Player
+import blackjack.presentation.ResultView
+
+class Turn(private val player: Player, private val cardDispenser: CardDispenser) {
+    fun process(turnCondition: TurnCondition) {
+        while (canGo(player, turnCondition)) {
+            dealCardToPlayer()
+        }
+    }
+
+    private fun dealCardToPlayer() {
+        cardDispenser.deal(player)
+        ResultView.printPlayerCards(player)
+    }
+
+    private fun canGo(
+        player: Player,
+        turnCondition: TurnCondition,
+    ): Boolean {
+        return cardDispenser.checkRemainCard() && player.checkBust().not() && turnCondition.carGo(player)
+    }
+}
