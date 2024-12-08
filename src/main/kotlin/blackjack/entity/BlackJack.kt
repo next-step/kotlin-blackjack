66b15b9833fdb6542fcba859d0c08cc8136a661c
janghomoon <sonyjangho@gmail.com>
2024-12-08 14:02:44 +0900

    fix:  블랙잭 A 값 버스트될 경우  1로 합산되도록 수정

--- src/main/kotlin/blackjack/entity/BlackJack.kt
+++ src/main/kotlin/blackjack/entity/BlackJack.kt
@@ -1,7 +1,7 @@
 package blackjack.entity
 
-import blackjack.enums.Card
-import blackjack.enums.CardSymbol
+import blackjack.domain.enums.Card
+import blackjack.domain.enums.CardSymbol
 
 class BlackJack {
     val card = mutableMapOf<CardSymbol, Card>()
@@ -11,6 +11,19 @@ class BlackJack {
     }
 
     fun getTotalCardValue(): Int {
-        return this.card.values.sumOf { it.getValue() }
+        val values = this.card.values.map { it.getValue() }
+        var total = values.sum()
+        val aceCount = values.count { it == Card.A.getValue() }
+        repeat(aceCount) {
+            if (total > BURST_LIMIT_VALUE) {
+                total -= MINUS_ACE_VALUE
+            }
+        }
+        return total
+    }
+
+    companion object {
+        private const val BURST_LIMIT_VALUE = 21
+        private const val MINUS_ACE_VALUE = 10
     }
 }
