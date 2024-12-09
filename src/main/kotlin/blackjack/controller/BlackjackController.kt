a2f0ee869720c1773ce8fb9236ce52919ebcdf94
Kim Dabomi <110092175+KimDabomi@users.noreply.github.com>
2024-12-09 18:34:14 +0900

    [step3] refactor: 컨트롤러 메서드 분리

--- src/main/kotlin/blackjack/controller/BlackjackController.kt
+++ src/main/kotlin/blackjack/controller/BlackjackController.kt
@@ -1,27 +1,52 @@
 package blackjack.controller
 
 import blackjack.domain.Game
+import blackjack.domain.Player
 import blackjack.view.InputView
 import blackjack.view.ResultView
 
 object BlackjackController {
     fun start() {
+        val game = initializeGame()
+        playPlayerTurns(game)
+        playDealerTurn(game)
+        displayGameResults(game)
+    }
+
+    private fun initializeGame(): Game {
         val playerNames = InputView.inputPlayerNames().split(",").map { it.trim() }
         val game = Game(playerNames)
 
         ResultView.printSplitCardResult(game.dealer, game.players)
         ResultView.printDealerInitialCard(game.dealer)
 
-        game.players.forEach { ResultView.printPlayerCards(listOf(it.name to it.cards)) }
+        displayInitialPlayerCards(game)
 
-        game.players.forEach { player ->
-            game.handlePlayerTurn(player) { playerName ->
-                InputView.inputPickCard(playerName)
-            }
-            ResultView.printPlayerCards(listOf(player.name to player.cards))
+        return game
+    }
+
+    private fun displayInitialPlayerCards(game: Game) {
+        game.players.forEach {
+            ResultView.printPlayerCards(listOf(it.name to it.cards))
         }
+    }
 
+    private fun playPlayerTurns(game: Game) {
+        game.players.forEach { player ->
+            playPlayerTurn(game, player)
+        }
+    }
+
+    private fun playPlayerTurn(game: Game, player: Player) {
+        game.handlePlayerTurn(player) { playerName ->
+            InputView.inputPickCard(playerName)
+        }
+        ResultView.printPlayerCards(listOf(player.name to player.cards))
+    }
+
+    private fun playDealerTurn(game: Game) {
         val dealerDraws = game.handleDealerTurn()
+
         if (dealerDraws) {
             ResultView.printDealerDrawMessage(true)
         } else {
@@ -29,7 +54,9 @@ object BlackjackController {
         }
 
         ResultView.printFinalScores(game.players, game.dealer)
+    }
 
+    private fun displayGameResults(game: Game) {
         val results = game.determineResults()
         val dealerResult = game.calculateDealerResult(results)
         ResultView.printGameResult(results, dealerResult)
