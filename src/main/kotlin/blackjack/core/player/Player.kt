2256d1f8a5b40c250599f73ee1e7291664f7bd42
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-09 12:43:16 +0900

    refactor(Player) - 카드 발급시 Bust상태 체크하도록 수정.

--- src/main/kotlin/blackjack/core/player/Player.kt
+++ src/main/kotlin/blackjack/core/player/Player.kt
@@ -4,13 +4,16 @@ import blackjack.core.card.Card
 import blackjack.core.card.Cards
 
 open class Player(val name: Name, val cards: Cards = Cards()) {
-    fun draw(card: Card) {
+    fun draw(card: Card): Boolean {
+        if (checkBust()) {
+            return false
+        }
         cards += card
+
+        return true
     }
 
     fun point(): Int = cards.point()
-
-    fun getCardNames(): String = cards.getCardNames()
 
     fun checkBust(): Boolean = cards.checkBust()
 }
