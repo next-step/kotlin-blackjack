81d4705ac659b8579c4730fa9f28f96661570a79
Giwan Kim <giwan.kim@gmail.com>
2024-12-10 14:16:28 +0900

    fix: 줄바꾸면서 베팅 금액을 입력받는다.

--- src/main/kotlin/blackjack/ui/InputView.kt
+++ src/main/kotlin/blackjack/ui/InputView.kt
@@ -6,6 +6,7 @@ import java.math.BigDecimal
 object InputView {
     private const val PLAYER_NAMES_PROMPT = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)"
     private const val COMMA_DELIMITER = ","
+    private val NEWLINE = System.lineSeparator()
 
     fun getPlayerNames(): List<String> {
         println(PLAYER_NAMES_PROMPT)
@@ -19,7 +20,7 @@ object InputView {
 
     fun getBets(names: List<String>): List<BigDecimal> =
         names.map {
-            println("${it}의 베팅 금액은?")
+            println("$NEWLINE${it}의 베팅 금액은?")
             readln().toBigDecimal()
         }
 }
