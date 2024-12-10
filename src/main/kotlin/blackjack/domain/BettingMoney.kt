6528c7345d257724a6773b20f27675bfab5a8bb6
dino.shin <troas96@naver.com>
2024-12-11 00:30:00 +0900

    refactor: 배팅 금액 계산 로직 수정

--- src/main/kotlin/blackjack/domain/BettingMoney.kt
+++ src/main/kotlin/blackjack/domain/BettingMoney.kt
@@ -1,4 +1,10 @@
 package blackjack.domain
 
 @JvmInline
-value class BettingMoney(val amount: Int)
+value class BettingMoney(val amount: Int) {
+    fun profitForWin(): Int = amount
+
+    fun profitForBlackjackWin(): Int = (amount * 1.5).toInt()
+
+    fun profitForLose(): Int = -amount
+}
--- src/main/kotlin/blackjack/domain/BlackjackGameResult.kt
+++ src/main/kotlin/blackjack/domain/BlackjackGameResult.kt
@@ -1,8 +1,5 @@
 package blackjack.domain
 
-import blackjack.domain.GameMatchResult.LOSE
-import blackjack.domain.GameMatchResult.WIN
-
 class BlackjackGameResult(
     private val dealer: Dealer,
     private val players: List<Player>,
@@ -17,27 +14,34 @@ class BlackjackGameResult(
     }
 
     fun calculateDealerProfit(): Int {
-        return players.sumOf { player ->
-            val betAmount = player.bettingMoney.amount
-            -calculateProfit(player, betAmount)
-        }
+        return calculatePlayerProfits().values.sumOf { -it }
     }
 
     fun calculatePlayerProfits(): Map<Player, Int> {
         return players.associateWith { player ->
-            val betAmount = player.bettingMoney.amount
+            val betAmount = player.bettingMoney
             calculateProfit(player, betAmount)
         }
     }
 
     private fun calculateProfit(
         player: Player,
-        betAmount: Int,
+        betAmount: BettingMoney,
     ): Int {
         return when (result[player]) {
-            WIN -> betAmount
-            LOSE -> -betAmount
+            GameMatchResult.WIN -> calculateWinProfit(player, betAmount)
+            GameMatchResult.LOSE -> betAmount.profitForLose()
             else -> 0
-        }.let { if (player.isBlackjack() && result[player] == WIN) it + (0.5 * betAmount).toInt() else it }
+        }
+    }
+
+    private fun calculateWinProfit(
+        player: Player,
+        betAmount: BettingMoney,
+    ): Int {
+        if (player.isBlackjack()) {
+            return betAmount.profitForBlackjackWin()
+        }
+        return betAmount.profitForWin()
     }
 }
