873b089bb470bcd940c8eaf401d25f238e4bed4b
Sara Han <sarahan774@gmail.com>
2024-12-12 08:25:24 +0900

    [step2] Cards 합산 로직 수정

--- src/main/kotlin/blackjack/domain/Cards.kt
+++ src/main/kotlin/blackjack/domain/Cards.kt
@@ -5,21 +5,24 @@ import blackjack.domain.Card.Companion.MAX_SUM
 
 data class Cards(val value: List<Card>) {
     fun sumValues(): Int {
-        var sum = 0
-        value.forEach { card ->
-            sum += getCardValue(card, sum)
-        }
-        return sum
+        val (nonAcesSum, aceCount) = calculateNonAcesAndCountAces()
+        return adjustForAces(nonAcesSum, aceCount)
     }
 
-    private fun getCardValue(
-        card: Card,
-        currentSum: Int,
-    ): Int {
-        return if (card.rank == Rank.ACE) {
-            if (currentSum + Rank.ACE.value > MAX_SUM) ACE_VALUE_ONE else Rank.ACE.value
-        } else {
-            card.rank.value
+    private fun calculateNonAcesAndCountAces(): Pair<Int, Int> {
+        var sum = 0
+        var aceCount = 0
+        value.forEach { card ->
+            if (card.rank == Rank.ACE) aceCount++ else sum += card.rank.value
         }
+        return sum to aceCount
+    }
+
+    private fun adjustForAces(currentSum: Int, aceCount: Int): Int {
+        var sum = currentSum
+        repeat(aceCount) {
+            sum += if (sum + Rank.ACE.value > MAX_SUM) ACE_VALUE_ONE else Rank.ACE.value
+        }
+        return sum
     }
 }
