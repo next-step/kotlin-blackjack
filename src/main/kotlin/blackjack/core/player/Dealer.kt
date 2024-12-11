a12372ea17dd6991a8825c666231d57932a18997
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-11 22:11:44 +0900

    feat(Dealer) - 수익율을 계산한 기능 추가.

--- src/main/kotlin/blackjack/core/player/Dealer.kt
+++ src/main/kotlin/blackjack/core/player/Dealer.kt
@@ -1,6 +1,9 @@
 package blackjack.core.player
 
-class Dealer : Player(Name(DEALER_NAME)) {
+import blackjack.core.amount.Amount
+import blackjack.core.amount.BettingAmount
+
+class Dealer : Player(Name(DEALER_NAME), BettingAmount(0)) {
     fun checkMatch(player: Player): MatchResult {
         return when {
             this.checkBust() -> MatchResult.LOSE
@@ -11,7 +14,39 @@ class Dealer : Player(Name(DEALER_NAME)) {
         }
     }
 
+    fun calculateProfit(players: Players) {
+        players.play { player -> calculateProfit(player) }
+    }
+
+    private fun calculateProfit(
+        player: Player,
+    ) {
+        when (checkMatch(player)) {
+            MatchResult.WIN -> moveAmount(player, this, player.bettingAmount)
+            MatchResult.LOSE -> moveAmount(this, player, getProfitAmount(player))
+            MatchResult.DRAW -> {}
+        }
+    }
+
+    private fun moveAmount(
+        from: Player,
+        to: Player,
+        amount: Amount,
+    ) {
+        from.profitAmount -= amount
+        to.profitAmount += amount
+    }
+
+    private fun getProfitAmount(player: Player): Amount {
+        return if (player.checkBlackJack()) {
+            player.bettingAmount * BLACKJACK_BONUS_WEIGHT
+        } else {
+            player.bettingAmount
+        }
+    }
+
     companion object {
         private const val DEALER_NAME = "딜러"
+        private const val BLACKJACK_BONUS_WEIGHT = 1.5f
     }
 }
