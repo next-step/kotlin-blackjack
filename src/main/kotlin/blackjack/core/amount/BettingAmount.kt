a02277132a4873279332912b073d919806fe2120
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-12 11:34:03 +0900

    refactor(BettingAmount) - 불필요한 유효성 체크 삭제(Amount Class와 중복)

--- src/main/kotlin/blackjack/core/amount/BettingAmount.kt
+++ src/main/kotlin/blackjack/core/amount/BettingAmount.kt
@@ -1,7 +1,3 @@
 package blackjack.core.amount
 
-class BettingAmount(amount: Int = 0) : Amount(amount) {
-    init {
-        require(amount >= 0) { ERROR_INVALID }
-    }
-}
+class BettingAmount(amount: Int = 0) : Amount(amount)
