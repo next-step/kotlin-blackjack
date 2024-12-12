85dce4c0d5ee3118b3d5680ce9977b600c3a6153
박찬희(Chanhee Park)/Data Solution개발팀/SKP <park.chanhee@sk.com>
2024-12-12 11:36:46 +0900

    refactor(Participant) - 코드 정리

--- src/main/kotlin/blackjack/core/player/Participant.kt
+++ src/main/kotlin/blackjack/core/player/Participant.kt
@@ -4,17 +4,17 @@ import blackjack.core.card.Card
 import blackjack.core.card.Cards
 
 open class Participant(val name: Name, val cards: Cards = Cards()) {
-    fun draw(card: Card) {
-        if (checkBust().not()) {
-            cards += card
-        }
-    }
-
     val point: Int
         get() = cards.point()
 
     val cardNum: Int
         get() = cards.size
+
+    fun draw(card: Card) {
+        if (checkBust().not()) {
+            cards += card
+        }
+    }
 
     fun checkBust(): Boolean = cards.checkBust()
 
