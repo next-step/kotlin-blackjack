b9f89d6d395e2a7fd0b88a5ed66ad7d484def550
janghomoon <sonyjangho@gmail.com>
2024-12-14 02:05:29 +0900

    fix: 피드백 테스트코드 추가및 수정

--- src/test/kotlin/blackjack/service/BlackJackServiceTest.kt
+++ src/test/kotlin/blackjack/service/BlackJackServiceTest.kt
@@ -3,6 +3,7 @@ package blackjack.service
 import blackjack.domain.enums.Card
 import blackjack.domain.enums.CardSymbol
 import blackjack.repository.GameRepository
+import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
@@ -15,61 +16,39 @@ class BlackJackServiceTest {
 
     @Test
     fun `사용자 이름을 받아 게임 생성 테스트`() {
-        blackJackService.initPlayers(listOf("문장호", "제이크"))
-        val games = gameRepository.findAll()
-        games.shouldHaveSize(2)
-        games[0].player shouldBe "문장호"
-        games[1].player shouldBe "제이크"
+        blackJackService.initPlayers("딜러", listOf("문장호", "제이크"))
+        val game = gameRepository.findAll()
+        game.players.shouldHaveSize(2)
+        game.dealer shouldBe "딜러"
+        game.players.first() shouldBe "문장호"
     }
 
     @Test
     fun `게임 진행중  추가 카드 발급 되었는지 확인 테스트`() {
-        val players = listOf("문장호")
-        blackJackService.initPlayers(players)
-        val games = blackJackService.startGame()
-
-        val cardSize = games.first().getPlayerBlackJack().card.size
+        blackJackService.initPlayers("딜러", listOf("문장호"))
+        val game = blackJackService.startGame()
+        val cardSize = game.players.first().getPlayerBlackJack().cards.size
         cardSize shouldBe 3
     }
 
-    @Test
-    fun `21 초과시 버스트로 게임 종료`() {
-        val players = listOf("문장호")
-        blackJackService.initPlayers(players)
-        blackJackService.startGame()
-
-        val player = gameRepository.findByName("문장호")!!
-        val playerBlackJack = player.getPlayerBlackJack()
-        playerBlackJack.addCardCount(
-            mutableListOf(
-                mutableMapOf(CardSymbol.HEART to Card.KING),
-                mutableMapOf(CardSymbol.DIAMOND to Card.QUEEN),
-                mutableMapOf(CardSymbol.SPADE to Card.A),
-            ),
-        )
-
-        val exception = runCatching { blackJackService.gameContinue("문장호") }.exceptionOrNull()
-        exception shouldNotBe null
-        exception!!.message shouldInclude "Player 문장호 has busted with a total of"
-    }
 
     @Test
     fun `딜러 버스트 시 모든 플레이어가 승리`() {
-        val players = listOf("딜러", "문장호")
-        blackJackService.initPlayers(players)
+        blackJackService.initPlayers("딜러", listOf("문장호", "제이크"))
+        val game = gameRepository.findAll()
+        val dealer = game.dealer
+        val dealerBlackJack = dealer.getDealerBlackJack()
 
-        val dealer = gameRepository.findByName("딜러")!!
-        val dealerBlackJack = dealer.getPlayerBlackJack()
         dealerBlackJack.addCardCount(
             mutableListOf(
-                mutableMapOf(CardSymbol.HEART to Card.KING),
-                mutableMapOf(CardSymbol.DIAMOND to Card.QUEEN),
-                mutableMapOf(CardSymbol.SPADE to Card.A),
+                mutableMapOf(CardSymbol.HEART to Card.KING) to true,
+                mutableMapOf(CardSymbol.DIAMOND to Card.QUEEN) to true,
+                mutableMapOf(CardSymbol.SPADE to Card.A) to true
             ),
         )
 
         val result = blackJackService.getGameResult()
-        val dealerResult = result.playerResults.first { it.playerName == "딜러" }
+        val dealerResult = result.dealerResult
         dealerResult.winCount shouldBe 0
         dealerResult.loseCount shouldBe 1
         dealerResult.drawCount shouldBe 0
@@ -83,36 +62,36 @@ class BlackJackServiceTest {
 
     @Test
     fun `딜러와 플레이어 점수 비교`() {
-        val players = listOf("딜러", "문장호", "장호")
-        blackJackService.initPlayers(players)
 
-        val dealer = gameRepository.findByName("딜러")!!
-        val dealerBlackJack = dealer.getPlayerBlackJack()
+        blackJackService.initPlayers("딜러", listOf("문장호", "장호"))
+        val game = gameRepository.findAll()
+        val dealer = game.dealer
+        val dealerBlackJack = dealer.getDealerBlackJack()
         dealerBlackJack.addCardCount(
             mutableListOf(
-                mutableMapOf(CardSymbol.HEART to Card.KING),
+                mutableMapOf(CardSymbol.HEART to Card.KING) to true,
             ),
         )
 
-        val player = gameRepository.findByName("문장호")!!
+        val player = gameRepository.findPlayerByName("문장호")
         val playerBlackJack = player.getPlayerBlackJack()
         playerBlackJack.addCardCount(
             mutableListOf(
-                mutableMapOf(CardSymbol.HEART to Card.KING),
-                mutableMapOf(CardSymbol.DIAMOND to Card.QUEEN),
+                mutableMapOf(CardSymbol.HEART to Card.KING) to true,
+                mutableMapOf(CardSymbol.DIAMOND to Card.QUEEN) to true,
             ),
         )
-        val playerTwo = gameRepository.findByName("장호")!!
+        val playerTwo = gameRepository.findPlayerByName("장호")
         val playerBlackJackTwo = playerTwo.getPlayerBlackJack()
         playerBlackJackTwo.addCardCount(
             mutableListOf(
-                mutableMapOf(CardSymbol.HEART to Card.KING),
-                mutableMapOf(CardSymbol.DIAMOND to Card.TWO),
+                mutableMapOf(CardSymbol.HEART to Card.KING) to true,
+                mutableMapOf(CardSymbol.DIAMOND to Card.TWO) to true,
             ),
         )
 
         val result = blackJackService.getGameResult()
-        val dealerResult = result.playerResults.first { it.playerName == "딜러" }
+        val dealerResult = result.dealerResult
         dealerResult.winCount shouldBe 0
         dealerResult.loseCount shouldBe 2
         dealerResult.drawCount shouldBe 0
