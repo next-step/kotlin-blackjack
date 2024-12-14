53c478ba6546b11fe03a6cc4ff13596c363f702e
eunice0035 <eunice7357@gmail.com>
2024-12-14 23:35:43 +0900

    refactor: add dealer class

--- src/main/kotlin/blackjack/domain/player/Dealer.kt
+++ src/main/kotlin/blackjack/domain/player/Dealer.kt
@@ -1,14 +1,25 @@
 package blackjack.domain.player
 
-import blackjack.domain.Card
+import blackjack.domain.CardDeck
 
-class Dealer : AbstractPlayer() {
-
-    override fun drawCard(newCard: Card) {
-        TODO("Not yet implemented")
+class Dealer(name: String = DEALER_NAME) : AbstractPlayer(name) {
+    private fun shouldDrawCard(): Boolean {
+        return calculateCard() <= 16
     }
 
-    override fun isBust(): Boolean {
-        return calculateCard() > 16
+    override fun startTurn(
+        onTurnStarted: ((AbstractPlayer) -> String)?,
+        onPrintResultCallback: (List<AbstractPlayer>) -> Unit
+    ) {
+        while (!isBust() && shouldDrawCard()) {
+            val card = CardDeck.drawCard()
+            drawCard(card)
+            onPrintResultCallback(listOf(this))
+            onTurnStarted?.invoke(this)
+        }
+    }
+
+    companion object {
+        const val DEALER_NAME = "딜러"
     }
 }
