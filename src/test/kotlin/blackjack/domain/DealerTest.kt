b787be50eecc1cc14d38b121c53551e2d745dbce
SeolYoungKim <nasur4da@gmail.com>
2024-12-14 23:32:28 +0900

    test: Dealer.dealCard() 테스트 작성

--- src/test/kotlin/blackjack/domain/DealerTest.kt
+++ src/test/kotlin/blackjack/domain/DealerTest.kt
@@ -5,12 +5,22 @@ import io.kotest.matchers.collections.shouldHaveSize
 
 class DealerTest : FreeSpec({
     "겜블러들에게 카드를 2장씩 나누어 준다" {
+        val dealer = Dealer()
         val gamblers = Gamblers.from(listOf("pobi", "jason"))
 
-        Dealer.dealTwoCardsEach(gamblers)
+        dealer.dealTwoCardsEach(gamblers)
 
         gamblers.forEach { gambler ->
             gambler.cards shouldHaveSize 2
         }
+    }
+
+    "겜블러에게 카드를 1장 나누어 준다" {
+        val dealer = Dealer()
+        val gambler = Gambler("pobi")
+
+        dealer.dealCard(gambler)
+
+        gambler.cards shouldHaveSize 1
     }
 })
