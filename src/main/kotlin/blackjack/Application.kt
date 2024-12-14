39ae612e6a908c018e2b585bfaeb604196d7ff1d
eunice0035 <eunice7357@gmail.com>
2024-12-14 23:56:38 +0900

    refactor: apply dealer and winning calculator

--- src/main/kotlin/blackjack/Application.kt
+++ src/main/kotlin/blackjack/Application.kt
@@ -1,7 +1,10 @@
 package blackjack
 
 import blackjack.domain.Game
-import blackjack.domain.Player
+import blackjack.domain.WinningCalculator
+import blackjack.domain.player.AbstractPlayer
+import blackjack.domain.player.Dealer
+import blackjack.domain.player.Player
 import blackjack.ui.InputView
 import blackjack.ui.ResultView
 
@@ -11,17 +14,27 @@ fun main() {
     val players = inputView.getPlayers().map { Player(it) }
 
     val game = Game.createGame(players)
-    val printCallback: ((List<Player>) -> Unit) = {
+    val printCallback: ((List<AbstractPlayer>) -> Unit) = {
         it.forEach { player ->
-            resultView.printPlayerResult(player)
+            if (player.name == Dealer.DEALER_NAME) {
+                resultView.showDealerCards(player)
+            } else {
+                resultView.showPlayerCards(player)
+            }
         }
     }
-    val turnCallback: ((Player) -> String) = { player ->
-        inputView.setUserAnswer(player.name)
-        inputView.getUserAnswer()
+    val turnCallback: ((AbstractPlayer) -> String) = { player ->
+        if (player.name == Dealer.DEALER_NAME) {
+            resultView.printDealerDrawExtra()
+            ""
+        } else {
+            inputView.setUserAnswer(player.name)
+            inputView.getUserAnswer()
+        }
     }
 
     resultView.printStartMessage(game.players)
     game.startGame(printCallback, turnCallback)
-    resultView.printGameResult(game.players)
+    WinningCalculator.calculatorGameResult(game.players, game.dealer)
+    resultView.printGameResult(game.players, game.dealer)
 }
