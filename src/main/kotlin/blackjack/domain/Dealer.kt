d5ff6ffb44c55640c22ab662d31b621588c10ff6
SeolYoungKim <nasur4da@gmail.com>
2024-12-14 23:32:09 +0900

    refactor: Gamblers가 카드를 받는 작업을 수행하도록 변경

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -2,9 +2,7 @@ package blackjack.domain
 
 class Dealer(private val cards: Cards = Cards.create(ShufflingStrategy.RandomShuffling)) {
     fun dealTwoCardsEach(gamblers: Gamblers) {
-        gamblers.forEach { gambler ->
-            gambler.receive(cards.draw(), cards.draw())
-        }
+        gamblers.receive(cards)
     }
 
     fun dealCard(gambler: Gambler) {
--- src/main/kotlin/blackjack/domain/Gamblers.kt
+++ src/main/kotlin/blackjack/domain/Gamblers.kt
@@ -1,6 +1,12 @@
 package blackjack.domain
 
-class Gamblers(private val gamblers: List<Gambler>) : Collection<Gambler> by gamblers {
+class Gamblers(private val gamblers: List<Gambler>): Collection<Gambler> by gamblers {
+    fun receive(cards: Cards) {
+        gamblers.forEach { gambler ->
+            gambler.receive(cards.draw(), cards.draw())
+        }
+    }
+
     companion object {
         fun from(names: List<String>): Gamblers {
             val gamblers = names.map { name -> Gambler(name) }
