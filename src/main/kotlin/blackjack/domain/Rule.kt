35695517954dec6b184182e55167717028a836e8
sian <chicori3@naver.com>
2024-12-15 00:48:33 +0900

    refactor: 21점 상수 추가

--- src/main/kotlin/blackjack/domain/Rule.kt
+++ src/main/kotlin/blackjack/domain/Rule.kt
@@ -1,9 +1,10 @@
 package blackjack.domain
 
 object Rule {
-    private const val BLACKJACK_VALUE = 21
+    private const val THRESHOLD_VALUE = 21
+    private const val BLACKJACK_SIZE = 2
 
-    fun isBurst(score: Int): Boolean = score > BLACKJACK_VALUE
+    fun isBurst(score: Int): Boolean = score > THRESHOLD_VALUE
 
-    fun isBlackjack(score: Int): Boolean = score == BLACKJACK_VALUE
+    fun isBlackjack(score: Int, size: Int): Boolean = score == THRESHOLD_VALUE && size == BLACKJACK_SIZE
 }
