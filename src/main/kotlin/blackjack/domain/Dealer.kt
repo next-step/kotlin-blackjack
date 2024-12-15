5549186978149a3d941b7540d70740923238adeb
SeolYoungKim <nasur4da@gmail.com>
2024-12-15 19:00:30 +0900

    refactor: 매직넘버 상수화

--- src/main/kotlin/blackjack/domain/Dealer.kt
+++ src/main/kotlin/blackjack/domain/Dealer.kt
@@ -3,6 +3,10 @@ package blackjack.domain
 class Dealer : Player("딜러") {
     override fun canNotReceiveCard(): Boolean {
         val totalScore = calculateTotalScore()
-        return totalScore >= 17
+        return totalScore > MAXIMUM_SCORE_TO_RECEIVE_CARD
+    }
+
+    companion object {
+        private const val MAXIMUM_SCORE_TO_RECEIVE_CARD = 16
     }
 }
