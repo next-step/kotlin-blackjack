bd1e7bee369bc0be8a70e8f893a5b072c52f5e71
wook <goodbyeyo@naver.com>
2024-12-16 23:34:18 +0900

    feat : Cards 도메인 분리, Player 도메인 추가

--- src/test/kotlin/blackjack/BlackJackTest.kt
+++ src/test/kotlin/blackjack/BlackJackTest.kt
@@ -1,23 +1,25 @@
 package blackjack
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class BlackJackTest {
     @Test
     fun `에이스가 아닌 카드의 포인트를 합산 할 수 있다`() {
         val card1 = Card(rank = Ranks.TWO, suits = Suits.SPADE)
         val card2 = Card(rank = Ranks.SIX, suits = Suits.HEART)
-        val cards = Cards(listOf(card1, card2))
-        cards.calculatePoints() shouldBe 8
+        val userCards = UserCards(mutableListOf(card1, card2))
+        userCards.calculatePoints() shouldBe 8
     }
 
     @Test
     fun `포인트를 합산 할때 21보다 크지 않다면 에이스 카드는 11점으로 계산한다`() {
         val card1 = Card(rank = Ranks.ACE, suits = Suits.SPADE)
         val card2 = Card(rank = Ranks.TEN, suits = Suits.HEART)
-        val cards = Cards(listOf(card1, card2))
-        cards.calculatePoints() shouldBe 21
+        val userCards = UserCards(mutableListOf(card1, card2))
+        userCards.calculatePoints() shouldBe 21
     }
 
     @Test
@@ -25,13 +27,56 @@ class BlackJackTest {
         val card1 = Card(rank = Ranks.ACE, suits = Suits.SPADE)
         val card2 = Card(rank = Ranks.EIGHT, suits = Suits.HEART)
         val card3 = Card(rank = Ranks.TEN, suits = Suits.HEART)
-        val cards = Cards(listOf(card1, card2, card3))
-        cards.calculatePoints() shouldBe 19
+        val userCards = UserCards(mutableListOf(card1, card2, card3))
+        userCards.calculatePoints() shouldBe 19
     }
 
     @Test
     fun `전체 카드의 수는 52이다`() {
-        Cards.create().size shouldBe 52
+        val gameCards = GameCards.create()
+        gameCards.size() shouldBe 52
+    }
+
+    @Test
+    fun `게임 카드를 모두 드로우 하면 전체 덱의 사이즈는 0이다`() {
+        val gameCards = GameCards.create()
+        repeat(gameCards.size()) {
+            gameCards.drawCard()
+        }
+        gameCards.size() shouldBe 0
+    }
+
+    @Test
+    fun `플레이어는 드로우 된 카드를 가질 수 있다`() {
+        val gameCards = GameCards.create()
+        val card = gameCards.drawCard()
+        val player = Player("lee")
+        player.receiveCard(Card(rank = Ranks.TEN, suits = Suits.CLUB))
+        player.receiveCard(Card(rank = Ranks.TWO, suits = Suits.DIAMOND))
+        player.receiveCard(Card(rank = Ranks.FIVE, suits = Suits.HEART))
+        player.cardSize() shouldBe 3
+    }
+
+    @Test
+    fun `플레이어의 카드 점수를 합산 할 수 있다`() {
+        val gameCards = GameCards.create()
+        val card = gameCards.drawCard()
+        val player = Player("lee")
+        player.receiveCard(Card(rank = Ranks.TEN, suits = Suits.CLUB))
+        player.receiveCard(Card(rank = Ranks.TWO, suits = Suits.DIAMOND))
+        player.receiveCard(Card(rank = Ranks.FIVE, suits = Suits.HEART))
+        player.calculateCardPoints() shouldBe 17
+    }
+
+    @Test
+    fun `카드 드로우 중지 요청하면 플레이어는 카드를 받을수 없다`() {
+        val player = Player("lee")
+        player.stopCardDraw()
+        shouldThrow<IllegalArgumentException> {
+            player.receiveCard(Card(rank = Ranks.SIX, suits = Suits.CLUB))
+        }.also {
+            it.message shouldBe "카드를 받을 수 없습니다"
+        }
     }
 }
 
@@ -60,7 +105,7 @@ enum class Suits {
     CLUB,
 }
 
-class Cards(private val cards: List<Card>) : Collection<Card> by cards {
+class UserCards(private val cards: MutableList<Card>) : Collection<Card> by cards {
     fun calculatePoints(): Int {
         val basePoints = cards.sumOf { it.rank.points[0] }
         val hasAce = cards.any { it.rank == Ranks.ACE }
@@ -70,12 +115,51 @@ class Cards(private val cards: List<Card>) : Collection<Card> by cards {
         return basePoints
     }
 
+    fun addCard(card: Card) {
+        cards.add(card)
+    }
+}
+
+class GameCards private constructor(private val deck: Queue<Card>) {
+
+    fun drawCard(): Card {
+        return deck.poll()
+    }
+
+    fun size(): Int {
+        return deck.size
+    }
+
     companion object {
-        fun create(): Cards {
+        fun create(): GameCards {
             val allCards = Suits.entries.flatMap { suit ->
                 Ranks.entries.map { rank -> Card(rank, suit) }
             }
-            return Cards(allCards.shuffled())
+            return GameCards(LinkedList(allCards.shuffled()))
         }
     }
 }
+
+class Player(val name: String) {
+    private var userCards = UserCards(mutableListOf())
+    private var isDrawContinue: Boolean = true
+
+    fun receiveCard(card: Card) {
+        require(isDrawContinue) { "카드를 받을 수 없습니다" }
+            userCards.addCard(card)
+    }
+
+    fun cardSize(): Int {
+        return userCards.size
+    }
+
+    fun calculateCardPoints(): Int {
+        return userCards.calculatePoints()
+    }
+
+    fun stopCardDraw() {
+        isDrawContinue = false
+    }
+}
+
+
