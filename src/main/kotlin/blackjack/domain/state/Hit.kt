f13ee081b2e41c8e1e4f5b75b9cc47d5a3b5b041
Aimbe <qkr2435@gmail.com>
2024-12-18 22:00:13 +0900

    feat: hit 상태 구현

--- src/main/kotlin/blackjack/domain/state/Hit.kt
+++ src/main/kotlin/blackjack/domain/state/Hit.kt
@@ -2,12 +2,22 @@ package blackjack.domain.state
 
 import blackjack.domain.card.PlayingCard
 
-class Hit(override val cards: List<PlayingCard>) : State {
-    constructor(card: PlayingCard) : this(listOf(card))
-    fun draw(card: PlayingCard): State {
-        val hands = cards + card
-        if(hands.size >= 2) return Hit(hands)
-        return Ready(card)
+class Hit(private val hands: Hands) : State {
+    override val cards: List<PlayingCard>
+        get() = cards.toList()
+
+    init {
+        require(hands.size >= 2) { "Hit 상태는 최소한 2장을 갖고있어야 합니다." }
     }
+    fun draw(card: PlayingCard): State {
+        val hands = hands + card
+        if(hands.score() > 21) return Bust(hands)
+        return Hit(hands + card)
+    }
+
+    fun stay(clubsTwo: PlayingCard): State {
+        return Stay(hands)
+    }
+
 
 }
