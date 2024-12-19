527fc2f0ce0f8794d0d50bfe64a0de70db794399
Giwan Kim <giwan.kim@gmail.com>
2024-12-19 22:45:13 +0900

    refactor: Playing 추상 클래스를 추출했다.

--- src/main/kotlin/blackjack/domain/Dealing.kt
+++ src/main/kotlin/blackjack/domain/Dealing.kt
@@ -1,12 +1,8 @@
 package blackjack.domain
 
-import java.math.BigDecimal
-
 class Dealing(
     override val hand: Hand = Hand(),
-) : State {
-    override val isDone: Boolean get() = false
-
+) : Playing() {
     init {
         require(hand.size <= Hand.INITIAL_HAND_SIZE) { "카드 숫자가 두 장을 초과할 수 없습니다." }
     }
@@ -21,6 +17,4 @@ class Dealing(
     }
 
     override fun stand(): State = throw IllegalStateException("햔재 상태에서는 스탠드를 할 수 없습니다.")
-
-    override fun profit(bet: Bet): BigDecimal = throw IllegalStateException("현재 상태에서는 수익을 계산할 수 없습니다.")
 }
--- src/main/kotlin/blackjack/domain/Ready.kt
+++ src/main/kotlin/blackjack/domain/Ready.kt
@@ -1,12 +1,8 @@
 package blackjack.domain
 
-import java.math.BigDecimal
-
 class Ready(
     override val hand: Hand,
-) : State {
-    override val isDone: Boolean get() = false
-
+) : Playing() {
     init {
         require(hand.size >= Hand.INITIAL_HAND_SIZE) { "카드 두 장 이상이어야 합니다." }
     }
@@ -21,6 +17,4 @@ class Ready(
     }
 
     override fun stand(): State = Stand(hand)
-
-    override fun profit(bet: Bet): BigDecimal = throw IllegalStateException("현재 상태에서는 수익을 계산할 수 없습니다.")
 }
