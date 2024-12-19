d8b8a8a26d7010c6db9eb421ca3d14bfecb50ebb
Aimbe <qkr2435@gmail.com>
2024-12-19 11:43:26 +0900

    test: finished 구현체 테스트

--- src/test/kotlin/blackjack/domain/state/StayTest.kt
+++ src/test/kotlin/blackjack/domain/state/StayTest.kt
@@ -1,5 +1,31 @@
 package blackjack.domain.state
 
-import org.junit.jupiter.api.Assertions.*
+import blackjack.domain.CLUBS_ACE
+import blackjack.domain.CLUBS_KING
+import blackjack.domain.CLUBS_TEN
+import blackjack.domain.CLUBS_TWO
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
-class StayTest
+class StayTest{
+
+    @Test
+    fun `생성자 테스트`(){
+        assertThrows<IllegalArgumentException> {
+            Stay(Hands())
+        }
+        assertThrows<IllegalArgumentException> {
+            Stay(Hands(CLUBS_ACE))
+        }
+    }
+
+    @Test
+    fun profit(){
+        val state = Stay(Hands(CLUBS_TWO, CLUBS_TEN, CLUBS_KING))
+        val acutal = state.profit(1_000)
+
+        acutal shouldBe 1_000
+    }
+
+}
