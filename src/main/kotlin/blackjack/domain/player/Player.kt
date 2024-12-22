d726bcf85d532f19ee27d811e05e114ab0da4f66
Aimbe <qkr2435@gmail.com>
2024-12-22 19:18:24 +0900

    feat: tdd 플레이어

--- src/main/kotlin/blackjack/domain/player/Player.kt
+++ src/main/kotlin/blackjack/domain/player/Player.kt
@@ -1,4 +1,32 @@
 package blackjack.domain.player
 
-class Player {
+import blackjack.domain.card.PlayingCard
+import blackjack.domain.state.Hands
+import blackjack.domain.state.Ready
+import blackjack.domain.state.State
+
+class Player private constructor(
+    private val name: PlayerName,
+    private var state: State = Ready()
+) {
+    val hands: Hands
+        get() = state.hands
+
+    fun drawCard(card: PlayingCard) {
+        state = state.draw(card)
+    }
+
+    fun stay() {
+        state = state.stay()
+    }
+
+    fun isRunning(): Boolean = state.isRunning()
+
+    fun calculateProfit(betAmount: Int): Double = state.profit(betAmount)
+
+    companion object {
+        fun of(name: String): Player {
+            return Player(PlayerName.from(name))
+        }
+    }
 }
---
+++ src/test/kotlin/blackjack/domain/player/PlayerTest.kt
@@ -0,0 +1,68 @@
+package blackjack.domain.player
+
+import blackjack.domain.card.Denomination
+import blackjack.domain.card.PlayingCard
+import blackjack.domain.card.Suit
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class PlayerTest : StringSpec({
+    "플레이어를 생성한다" {
+        val player = Player.of("aimbe")
+        player.hands.size shouldBe 0
+        player.isRunning() shouldBe true
+    }
+
+    "플레이어의 이름이 빈 문자열이면 예외가 발생한다" {
+        shouldThrow<IllegalArgumentException> {
+            Player.of("")
+        }.message shouldBe "플레이어 이름은 비어있을 수 없습니다."
+    }
+
+    "플레이어는 카드를 한장 받을 수 있다" {
+        val player = Player.of("aimbe")
+        val card = PlayingCard.of(Suit.HEARTS, Denomination.ACE)
+
+        player.drawCard(card)
+
+        player.hands.size shouldBe 1
+        player.hands.first() shouldBe card
+    }
+
+    "플레이어가 카드를 2장 받으면 Hit 상태가 된다" {
+        val player = Player.of("aimbe")
+        player.drawCard(PlayingCard.of(Suit.HEARTS, Denomination.NINE))
+        player.drawCard(PlayingCard.of(Suit.SPADES, Denomination.THREE))
+
+        player.isRunning() shouldBe true
+    }
+
+    "플레이어가 블랙잭을 받으면 게임이 종료되고 1.5배의 수익을 얻는다" {
+        val player = Player.of("aimbe")
+        player.drawCard(PlayingCard.of(Suit.HEARTS, Denomination.ACE))
+        player.drawCard(PlayingCard.of(Suit.SPADES, Denomination.KING))
+
+        player.calculateProfit(1000) shouldBe 1500.0
+    }
+
+    "플레이어가 bust되면 게임이 종료되고 수익이 0이 된다" {
+        val player = Player.of("aimbe")
+        player.drawCard(PlayingCard.of(Suit.HEARTS, Denomination.KING))
+        player.drawCard(PlayingCard.of(Suit.SPADES, Denomination.QUEEN))
+        player.drawCard(PlayingCard.of(Suit.DIAMONDS, Denomination.JACK))
+
+        player.isRunning() shouldBe false
+        player.calculateProfit(1000) shouldBe 0.0
+    }
+
+    "플레이어가 stay하면 게임이 종료된다" {
+        val player = Player.of("aimbe")
+        player.drawCard(PlayingCard.of(Suit.HEARTS, Denomination.TEN))
+        player.drawCard(PlayingCard.of(Suit.SPADES, Denomination.EIGHT))
+
+        player.stay(PlayingCard.of(Suit.DIAMONDS, Denomination.TWO))
+
+        player.isRunning() shouldBe false
+    }
+})
