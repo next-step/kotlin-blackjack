7b5e6ef1f762e757917aa8347ba71c11e6a57264
Aimbe <qkr2435@gmail.com>
2024-12-22 19:19:05 +0900

    feat: state isRunning 메소드 추가

--- src/main/kotlin/blackjack/domain/state/Running.kt
+++ src/main/kotlin/blackjack/domain/state/Running.kt
@@ -4,4 +4,7 @@ abstract class Running : State {
     override fun profit(money: Int): Double {
         throw IllegalArgumentException("게임이 종료되지 않았습니다.")
     }
+    override fun isRunning(): Boolean {
+        return true
+    }
 }
