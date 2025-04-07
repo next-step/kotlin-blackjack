8466d4da4319a60bd730ebb1cf7336b7996eac7d
june-777 <wlwhswnsrl96@gmail.com>
2025-04-07 23:34:56 +0200

    feat: implement SkillsBuilder

---
+++ src/main/kotlin/SkillsBuilder.kt
@@ -0,0 +1,16 @@
+class SkillsBuilder {
+    private val softSkills = mutableListOf<String>()
+    private val hardSkills = mutableListOf<String>()
+
+    fun soft(value: String) {
+        softSkills.add(value)
+    }
+
+    fun hard(value: String) {
+        hardSkills.add(value)
+    }
+
+    fun build(): Skills {
+        return Skills(softSkills, hardSkills)
+    }
+}
