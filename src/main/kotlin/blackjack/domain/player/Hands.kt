5166805c568c0398b46ff4a0f0f3636ef94a4e2f
syoun602 <syoun602@gmail.com>
2025-05-01 13:45:24 +0200

    feat: add calculating score for ace values

--- src/main/kotlin/blackjack/domain/player/Hands.kt
+++ src/main/kotlin/blackjack/domain/player/Hands.kt
@@ -1,6 +1,7 @@
 package blackjack.domain.player
 
 import blackjack.domain.card.Card
+import blackjack.domain.card.CardNumber
 
 class Hands(
     val cards: List<Card> = emptyList(),
@@ -13,9 +14,22 @@ class Hands(
 
     infix operator fun plus(card: Card) = Hands(cards + card)
 
-    fun isBust(): Boolean = cards.sumOf { it.number.value } > BLACKJACK_SCORE
+    fun isBust(): Boolean = calculateScore() > BLACKJACK_SCORE
+
+    fun calculateScore(): Int {
+        val sum = cards.sumOf { it.number.value }
+
+        return when {
+            !hasAce() -> sum
+            sum + ACE_ADD_SCORE > BLACKJACK_SCORE -> sum
+            else -> sum + ACE_ADD_SCORE
+        }
+    }
+
+    fun hasAce() = cards.map { it.number }.contains(CardNumber.ACE)
 
     companion object {
         private const val BLACKJACK_SCORE = 21
+        private const val ACE_ADD_SCORE = 10
     }
 }
--- src/test/kotlin/blackjack/domain/player/HandsTest.kt
+++ src/test/kotlin/blackjack/domain/player/HandsTest.kt
@@ -3,8 +3,10 @@ package blackjack.domain.player
 import blackjack.domain.card.CardFixture.SPADES_ACE
 import blackjack.domain.card.CardFixture.SPADES_JACK
 import blackjack.domain.card.CardFixture.SPADES_QUEEN
+import blackjack.domain.card.CardFixture.SPADES_SEVEN
 import blackjack.domain.card.CardFixture.SPADES_SIX
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 
 class HandsTest : FunSpec({
@@ -55,5 +57,33 @@ class HandsTest : FunSpec({
             )
 
         hands.isBust() shouldBe true
+    }
+
+    context("score") {
+        test("score is calculated correctly") {
+            this@context.withData(
+                listOf(SPADES_JACK, SPADES_SIX) to 16,
+                listOf(SPADES_SIX, SPADES_SEVEN) to 13,
+                listOf(SPADES_QUEEN, SPADES_JACK) to 20,
+            ) { (cards, expected) ->
+                val hands = Hands(cards)
+
+                hands.calculateScore() shouldBe expected
+            }
+        }
+
+        test("ace is counted as 1 if cards + ACE(11) is over than 21") {
+            val cards = listOf(SPADES_QUEEN, SPADES_JACK, SPADES_ACE)
+            val hands = Hands(cards)
+
+            hands.calculateScore() shouldBe 21
+        }
+
+        test("ace is counted as 11 if cards + ACE(11) is below or equal to 21") {
+            val cards = listOf(SPADES_QUEEN, SPADES_ACE)
+            val hands = Hands(cards)
+
+            hands.calculateScore() shouldBe 21
+        }
     }
 })
