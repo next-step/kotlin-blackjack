f8e477a6a6adac35f07cd251c11cbfa60032ccf7
ssh1224 <ssh1224@woowahan.com>
2025-12-12 15:18:09 +0900

    feat : 플레이어, 딜러에게 카드 발급 처리

--- src/main/kotlin/BlackjackApplication.kt
+++ src/main/kotlin/BlackjackApplication.kt
@@ -1,11 +1,28 @@
-import domain.CardDeck
 import domain.Dealer
 import presentation.InputView
+import service.CardDistributorService
 
 fun main() {
+    // 딜러 초기화
     val dealer = Dealer()
-    val players = InputView.inputPlayers()
-    println(players)
 
-    val deck = CardDeck()
+    // 플레이어 입력 및 초기화
+    val players = InputView.inputPlayers()
+    println()
+
+    // 카드 분배 : 플레이어, 딜러
+    val cardDistributorService = CardDistributorService()
+    cardDistributorService.distributeCards(players, dealer)
+
+    // 분배된 카드정보 출력
+    println("딜러와 ${players.joinToString(", ") { it.name }}에게 2장의 나누었습니다.")
+    println("딜러: ${dealer.cards.joinToString(", ")}")
+    players.forEach { player ->
+        println("${player.name}카드: ${player.cards.joinToString(", ")}")
+    }
+    println()
+
+    // 플레이어에게 카드 추가 발급 여부 묻기 (Y/N)
+
+
 }
